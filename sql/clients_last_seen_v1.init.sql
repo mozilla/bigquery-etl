08e2950bfa5f8d841b1cfb518658d777fe54648e
Daniel Thorn <dthorn@mozilla.com>
2019-02-27 15:23:31 -0800

    Optimize clients_last_seen_v1.init.sql

--- sql/clients_last_seen_v1.init.sql
+++ sql/clients_last_seen_v1.init.sql
@@ -1,17 +1,20 @@
+WITH input AS (
+  SELECT
+    ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY submission_date_s3 DESC) AS _row,
+    *
+  FROM
+    clients_daily_v6
+  WHERE
+    submission_date_s3 <= @submission_date
+    AND submission_date_s3 > DATE_SUB(@submission_date, INTERVAL 28 DAY)
+)
 SELECT
   @submission_date AS submission_date,
   CURRENT_DATETIME() AS generated_time,
-  MAX(submission_date_s3) AS last_seen_date,
-  -- approximate LAST_VALUE(input).*
-  ARRAY_AGG(input
-    ORDER BY submission_date_s3
-    DESC LIMIT 1
-  )[OFFSET(0)].* EXCEPT (submission_date_s3)
+  submission_date_s3 AS last_seen_date,
+  * EXCEPT (_row,
+    submission_date_s3)
 FROM
-  clients_daily_v6 AS input
+  input
 WHERE
-  submission_date_s3 <= @submission_date
-  AND
-  submission_date_s3 > DATE_SUB(@submission_date, INTERVAL 28 DAY)
-GROUP BY
-  input.client_id
+  _row = 1

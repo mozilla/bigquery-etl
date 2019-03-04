4182b6538c808712cbef71e8baf08ffba5de2552
Daniel Thorn <dthorn@mozilla.com>
2019-03-04 15:02:37 -0800

    format with bigquery web ui

--- sql/clients_last_seen_v1.sql
+++ sql/clients_last_seen_v1.sql
@@ -4,8 +4,11 @@ WITH current_sample AS (
     -- This last_date struct is used to record the last date that a particular
     -- client met various criteria; we record a null date if the client does
     -- not meet a given criterion.
-    STRUCT(
-      IF(country IN ('US', 'FR', 'DE', 'UK', 'CA'),
+    STRUCT( IF(country IN ('US',
+          'FR',
+          'DE',
+          'UK',
+          'CA'),
         submission_date_s3,
         NULL) AS seen_in_tier1_country,
       IF(scalar_parent_browser_engagement_total_uri_count_sum >= 5,
@@ -20,8 +23,7 @@ WITH current_sample AS (
 ), previous AS (
   SELECT
     -- Drop last_date values after 28 days
-    STRUCT(
-      IF(last_date.seen_in_tier1_country > DATE_SUB(@submission_date, INTERVAL 28 DAY),
+    STRUCT( IF(last_date.seen_in_tier1_country > DATE_SUB(@submission_date, INTERVAL 28 DAY),
         last_date.seen_in_tier1_country,
         NULL) AS seen_in_tier1_country,
       IF(last_date.visited_5_or_more_uri > DATE_SUB(@submission_date, INTERVAL 28 DAY),
@@ -40,8 +42,7 @@ SELECT
   @submission_date AS submission_date,
   CURRENT_DATETIME() AS generated_time,
   -- deep merge last_date
-  STRUCT(
-    COALESCE([current_sample.last_date.seen_in_tier1_country,
+  STRUCT( COALESCE([current_sample.last_date.seen_in_tier1_country,
       previous.last_date.seen_in_tier1_country]) AS seen_in_tier1_country,
     COALESCE([current_sample.last_date.visited_5_or_more_uri,
       previous.last_date.visited_5_or_more_uri]) AS visited_5_or_more_uri) AS last_date,

6609946b2571caab5210d283edf87e5e2da5f708
Daniel Thorn <dthorn@mozilla.com>
2019-03-04 14:34:42 -0800

    counter-PR

--- sql/clients_last_seen_v1.sql
+++ sql/clients_last_seen_v1.sql
@@ -1,5 +1,21 @@
 WITH current_sample AS (
   SELECT
+
+    -- This last_date struct is used to record the last date that a particular
+    -- client met various criteria; we record a null date if the client does
+    -- not meet a given criterion.
+    STRUCT (
+      IF(
+        country IN ('US', 'FR', 'DE', 'UK', 'CA'),
+        submission_date_s3,
+        NULL
+      ) AS seen_in_tier1_country,
+      IF(
+        scalar_parent_browser_engagement_total_uri_count_sum >= 5
+        submission_date_s3,
+        NULL
+      ) AS visited_5_or_more_uris
+    ) AS last_date,
     submission_date_s3 AS last_seen_date,
     * EXCEPT (submission_date_s3)
   FROM
@@ -19,23 +35,19 @@ WITH current_sample AS (
 SELECT
   @submission_date AS submission_date,
   CURRENT_DATETIME() AS generated_time,
-
-  -- This last_date struct is used to record the last date that a particular
-  -- client met various criteria; we record a null date if the client does
-  -- not meet a given criterion.
+  
+  -- merge last_date values
   STRUCT (
-    IF(
-      country IN ('US', 'FR', 'DE', 'UK', 'CA'),
-      current_sample.submission_date,
+    COALESCE(
+      current_sample.last_date.seen_in_tier1_country,
       previous.last_date.seen_in_tier1_country
     ) AS seen_in_tier1_country,
-    IF(
-      scalar_parent_browser_engagement_total_uri_count_sum >= 5
-      current_sample.submission_date,
-      previous.last_date.visited_5_uri
-    ) AS visited_5_uri
+    COALESCE(
+      current_sample.last_date.visited_5_or_more_uris,
+      previous.last_date.visited_5_or_more_uris
+    ) AS visited_5_or_more_uris
   ) AS last_date,
-
+  
   IF(current_sample.client_id IS NOT NULL,
     current_sample,
     previous).* EXCEPT (last_date)

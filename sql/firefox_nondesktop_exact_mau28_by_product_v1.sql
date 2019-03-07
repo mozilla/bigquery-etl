76fbce9127ee21f9750a2fa6f35f8e90a58438b0
Jeff Klukas <jeff@klukas.net>
2019-03-07 18:04:26 -0500

    Rename tables with firefox_ prefix

--- sql/firefox_nondesktop_exact_mau28_by_product_v1.sql
+++ sql/firefox_nondesktop_exact_mau28_by_product_v1.sql
@@ -9,7 +9,7 @@ SELECT
   SUM(IF(country IN ('US', 'FR', 'DE', 'UK', 'CA'), wau, 0)) AS tier1_wau,
   SUM(IF(country IN ('US', 'FR', 'DE', 'UK', 'CA'), dau, 0)) AS tier1_dau
 FROM
-  nondesktop_exact_mau28_by_dimensions_v1
+  firefox_nondesktop_exact_mau28_by_product_by_dimensions_v1
 WHERE
   submission_date = @submission_date
 GROUP BY

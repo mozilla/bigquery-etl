fb42c593eb74ec22d271977cb3468e0b4e497d16
Jeff Klukas <jeff@klukas.net>
2019-03-07 17:25:03 -0500

    Add tier1 mau/wau/dau to nondesktop_exact_mau28_v1

--- sql/nondesktop_exact_mau28_v1.sql
+++ sql/nondesktop_exact_mau28_v1.sql
@@ -4,7 +4,10 @@ SELECT
   product,
   SUM(mau) AS mau,
   SUM(wau) AS wau,
-  SUM(dau) AS dau
+  SUM(dau) AS dau,
+  SUM(IF(country IN ('US', 'FR', 'DE', 'UK', 'CA'), mau, 0)) AS tier1_mau,
+  SUM(IF(country IN ('US', 'FR', 'DE', 'UK', 'CA'), wau, 0)) AS tier1_wau,
+  SUM(IF(country IN ('US', 'FR', 'DE', 'UK', 'CA'), dau, 0)) AS tier1_dau
 FROM
   nondesktop_exact_mau28_by_dimensions_v1
 WHERE

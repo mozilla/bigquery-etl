ada629cd1a037845e8e667959fbc66494bc79a01
Jeff Klukas <jeff@klukas.net>
2019-03-27 14:22:37 -0400

    Adapt FxA ETL for merging backfill data at the raw MAU stage
    We learned today that backfilled data contains hashed user_id
    while the live Stackdriver pipeline for FxA contains unhashed ids.
    Running the relevant HMAC hash would be complex, so we are instead
    defining an overlap period of 28 days where we calculate intermediate
    MAU tables for backfill and for stackdriver data separately,
    then we union the two together.
    
    We'll coordinate to do a final one-time import of backfill data
    tomorrow morning, then use Stackdriver data from 3/28 and forward.

--- sql/fxa_users_last_seen_v1.init.sql
+++ sql/fxa_users_last_seen_v1.init.sql
@@ -5,9 +5,9 @@ SELECT
   IF(country IN ('United States', 'France', 'Germany', 'United Kingdom', 'Canada'),
     submission_date,
     NULL) AS date_last_seen_in_tier1_country,
-  * EXCEPT (submission_date, generated_time)
+  * EXCEPT (submission_date, generated_time, seen_in_tier1_country)
 FROM
   fxa_users_daily_v1
 WHERE
-  -- 2017-10-01 is the first date in the Amplitude FxA project.
-  submission_date = DATE '2017-10-01'
+  -- Choose an arbitrary date that contains no records.
+  submission_date = DATE_FROM_UNIX_DATE(0)

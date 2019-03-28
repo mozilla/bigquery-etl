b355f13262182a0fb6a99643fa1d1215d420258b
Jeff Klukas <jeff@klukas.net>
2019-03-28 10:17:22 -0400

    Adapt FxA ETL for merging backfill data at the raw MAU stage (#43)
    We learned today that backfilled data contains hashed user_id
    while the live Stackdriver pipeline for FxA contains unhashed ids.
    Running the relevant HMAC hash would be complex, so we are instead
    defining an overlap period of 28 days where we calculate intermediate
    MAU tables for backfill and for stackdriver data separately,
    then we union the two together.
    
    We'll coordinate to do a final one-time import of backfill data
    tomorrow morning, then use Stackdriver data from 3/28 and forward.

---
+++ sql/static/README.md
@@ -0,0 +1,4 @@
+This directory contains definitions for derived tables in the `static` dataset.
+
+In particular, we have two tables based on a one-time export of Firefox Accounts
+extracted from Amplitude and imported to BigQuery.
--- sql/static/fxa_amplitude_export_users_daily.sql
+++ sql/static/fxa_amplitude_export_users_daily.sql
@@ -46,7 +46,7 @@ WITH
     udf_mode_last(ARRAY_AGG(country) OVER w1) AS country,
     udf_contains_tier1_country(ARRAY_AGG(country) OVER w1) AS seen_in_tier1_country
   FROM
-    fxa_all_events_v1
+    fxa_amplitude_export_event_date
   WHERE
     user_id IS NOT NULL
     AND event_type NOT IN (
@@ -55,20 +55,20 @@ WITH
       'fxa_reg - password_missing','fxa_sms - sent', 'mktg - email_click',
       'mktg - email_open','mktg - email_sent','sync - repair_success',
       'sync - repair_triggered')
-    AND EXTRACT(DATE FROM submission_timestamp) = @submission_date
+    AND event_date = @submission_date
   WINDOW
     w1 AS (
       PARTITION BY
       user_id
     ORDER BY
-      submission_timestamp DESC
+      event_time DESC
     ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING),
     -- We must provide a modified window for ROW_NUMBER which cannot accept a frame clause.
     w1_unframed AS (
     PARTITION BY
       user_id
     ORDER BY
-      submission_timestamp DESC) )
+      event_time DESC) )
 SELECT
   * EXCEPT (_n)
 FROM

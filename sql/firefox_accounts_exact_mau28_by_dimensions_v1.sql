4d30aff301549e944744adfb0c47704ee7075dd9
Jeff Klukas <jeff@klukas.net>
2019-04-02 16:52:53 -0400

    Remove misuse of REPLACE

--- sql/firefox_accounts_exact_mau28_by_dimensions_v1.sql
+++ sql/firefox_accounts_exact_mau28_by_dimensions_v1.sql
@@ -2,11 +2,11 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-derived-datasets.telemetry.firefox_accounts_exact_mau28_by_dimensions_v1`
 AS
 SELECT
-  * EXCEPT (generated_time, country)
+  * EXCEPT (generated_time, country, mau_tier1_inclusive),
     -- We rename this column here to match the new standard of prefixing _mau
     -- with the usage criterion; we can refactor to have the correct name in
     -- the raw table the next time we need to make a change and backfill.
-    REPLACE (mau_tier1_inclusive AS seen_in_tier1_country_mau),
+    mau_tier1_inclusive AS seen_in_tier1_country_mau,
   -- We normalize country to match the two-digit country codes that appear in
   -- telemetry data, so that this view is compatible with the exact_mau28 views
   -- for desktop and nondesktop.

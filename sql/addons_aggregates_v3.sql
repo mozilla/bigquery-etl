2c7f26953f33ccf02e6ecb816a9ceec48bc8b187
Allen Short <ashort@mozilla.com>
2019-04-16 12:16:17 -0500

    test

--- sql/addons_aggregates_v3.sql
+++ sql/addons_aggregates_v3.sql
@@ -4,20 +4,20 @@ SELECT
   client_id,
   normalized_channel,
   SUBSTR(app_version, 1, 2) AS app_version,
+  SAFE.PARSE_DATE("%Y-%m-%d 00:00:00",
+    profile_creation_date) AS profile_creation_date,
   locale,
   (
   SELECT
     AS STRUCT --
     COUNTIF(is_self_install) AS n_self_installed_addons,
-    COUNTIF(_addon.element.addon_id LIKE "%@shield.mozilla%") AS n_shield_addons,
-    COUNTIF(_addon.element.foreign_install) AS n_foreign_installed_addons,
-    COUNTIF(_addon.element.is_system) AS n_system_addons,
-    COUNTIF(_addon.element.is_web_extension) AS n_web_extensions,
+    COUNTIF(element.addon_id LIKE "%@shield.mozilla%") AS n_shield_addons,
+    COUNTIF(element.foreign_install) AS n_foreign_installed_addons,
+    COUNTIF(element.is_system) AS n_system_addons,
+    COUNTIF(element.is_web_extension) AS n_web_extensions,
     MIN(IF(is_self_install,
-        SAFE.DATE_FROM_UNIX_DATE(_addon.element.install_day),
-        NULL)) AS first_addon_install_date,
-    MIN(SAFE.PARSE_DATE("%Y-%m-%d 00:00:00",
-        profile_creation_date)) AS profile_creation_date
+        SAFE.DATE_FROM_UNIX_DATE(element.install_day),
+        NULL)) AS first_addon_install_date
   FROM (
     SELECT
       *,
@@ -28,9 +28,9 @@ SELECT
       AND NOT element.addon_id LIKE '%cliqz%'
       AND NOT element.addon_id LIKE '%@unified-urlbar%' AS is_self_install
     FROM
-      UNNEST(active_addons.list)) AS _addon)
+      UNNEST(active_addons.list)) AS _addon).*
 FROM
-  telemetry.clients_daily_v6
+  clients_daily_v6
 WHERE
   client_id IS NOT NULL
   AND submission_date_s3 = @submission_date

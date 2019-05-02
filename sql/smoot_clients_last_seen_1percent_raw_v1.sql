485b8ddf84f12957fe94bf1e021c1fd172c40e1a
Jeff Klukas <jeff@klukas.net>
2019-05-02 16:53:45 -0400

    filter out days_since_created profile <0 and >27

--- sql/smoot_clients_last_seen_1percent_raw_v1.sql
+++ sql/smoot_clients_last_seen_1percent_raw_v1.sql
@@ -26,26 +26,34 @@ WITH
     telemetry.smoot_clients_last_seen_1percent_raw_v1 AS cls
   WHERE
     submission_date = DATE_SUB(@submission_date, INTERVAL 1 DAY)
-    AND shift_one_day(days_seen_bits) > 0)
---
+    AND shift_one_day(days_seen_bits) > 0),
+  --
+  _joined AS (
+  SELECT
+    @submission_date AS submission_date,
+    IF(_current.client_id IS NOT NULL,
+      _current,
+      _previous).* REPLACE (
+        combine_days(_previous.days_seen_bits, _current.days_seen_bits) AS days_seen_bits,
+        combine_days(_previous.days_visited_5_uri_bits, _current.days_visited_5_uri_bits) AS days_visited_5_uri_bits,
+        combine_days(_previous.days_opened_dev_tools_bits, _current.days_opened_dev_tools_bits) AS days_opened_dev_tools_bits,
+        -- We want to base new profile creation date on the first profile_creation_date
+        -- value we observe, so we propagate a non-null previous value in preference
+        -- to a non-null value on today's observation.
+        COALESCE(_previous.days_since_created_profile + 1,
+          _current.days_since_created_profile) AS days_since_created_profile)
+  FROM
+    _current
+  FULL JOIN
+    _previous
+  USING
+    -- Include sample_id to match the clustering of the tables, which may improve
+    -- join performance.
+    (sample_id, client_id))
+  --
 SELECT
-  @submission_date AS submission_date,
-  IF(_current.client_id IS NOT NULL,
-    _current,
-    _previous).* REPLACE (
-      combine_days(_previous.days_seen_bits, _current.days_seen_bits) AS days_seen_bits,
-      combine_days(_previous.days_visited_5_uri_bits, _current.days_visited_5_uri_bits) AS days_visited_5_uri_bits,
-      combine_days(_previous.days_opened_dev_tools_bits, _current.days_opened_dev_tools_bits) AS days_opened_dev_tools_bits,
-      -- We want to base new profile creation date on the first profile_creation_date
-      -- value we observe, so we propagate a non-null previous value in preference
-      -- to a non-null value on today's observation.
-      COALESCE(_previous.days_since_created_profile + 1,
-        _current.days_since_created_profile) AS days_since_created_profile)
+  * REPLACE (
+    -- Null out any fields that may contain data leaked from beyond our 28 day window.
+    IF(days_since_created_profile BETWEEN 0 AND 27, days_since_created_profile, NULL) AS days_since_created_profile)
 FROM
-  _current
-FULL JOIN
-  _previous
-USING
-  -- Include sample_id to match the clustering of the tables, which may improve
-  -- join performance.
-  (sample_id, client_id)
+  _joined

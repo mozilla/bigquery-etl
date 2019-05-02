7f068ec7102025b7a821d0069712cae36a9c9635
Daniel Thorn <dthorn@mozilla.com>
2019-05-02 12:53:19 -0700

    Use wildcard tables instead of jinja in fxa events

--- sql/fxa_auth_bounce_events_v1.sql
+++ sql/fxa_auth_bounce_events_v1.sql
@@ -7,8 +7,10 @@ SELECT
             user_id),
           TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id) AS fields )) AS jsonPayload )
 FROM
-  `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_auth_bounces_{{ds_nodash}}`
+  `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_auth_bounces_*`
 WHERE
   jsonPayload.type = 'amplitudeEvent'
   AND jsonPayload.fields.event_type IS NOT NULL
   AND jsonPayload.fields.user_id IS NOT NULL
+  AND _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d',
+    @submission_date)

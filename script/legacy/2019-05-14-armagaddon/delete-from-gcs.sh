b8e8504b4f7f2e13a699e6279af3bab70a96087a
Jeff Klukas <jeff@klukas.net>
2019-05-13 16:37:57 -0400

    Correct time range for gs

--- script/legacy/2019-05-14-armagaddon/delete-from-gcs.sh
+++ script/legacy/2019-05-14-armagaddon/delete-from-gcs.sh
@@ -5,226 +5,76 @@
 
 ## Deletion in these buckets requires ops credentials.
 
+function delete_hours() {
+    local target_dir=$1
+
+    gsutil -m rm -r $target_dir/2019-05-04/11/
+    gsutil -m rm -r $target_dir/2019-05-04/12/
+    gsutil -m rm -r $target_dir/2019-05-04/13/
+    gsutil -m rm -r $target_dir/2019-05-04/14/
+    gsutil -m rm -r $target_dir/2019-05-04/15/
+    gsutil -m rm -r $target_dir/2019-05-04/16/
+    gsutil -m rm -r $target_dir/2019-05-04/17/
+    gsutil -m rm -r $target_dir/2019-05-04/18/
+    gsutil -m rm -r $target_dir/2019-05-04/19/
+    gsutil -m rm -r $target_dir/2019-05-04/20/
+    gsutil -m rm -r $target_dir/2019-05-04/21/
+    gsutil -m rm -r $target_dir/2019-05-04/22/
+    gsutil -m rm -r $target_dir/2019-05-04/23/
+
+    for day in $(seq -f "%02g" 05 10); do
+        gsutil -m rm -r $target_dir/2019-05-${day}/
+    done
+
+    gsutil -m rm -r $target_dir/2019-05-11/00/
+    gsutil -m rm -r $target_dir/2019-05-11/01/
+    gsutil -m rm -r $target_dir/2019-05-11/02/
+    gsutil -m rm -r $target_dir/2019-05-11/03/
+    gsutil -m rm -r $target_dir/2019-05-11/04/
+    gsutil -m rm -r $target_dir/2019-05-11/05/
+    gsutil -m rm -r $target_dir/2019-05-11/06/
+    gsutil -m rm -r $target_dir/2019-05-11/07/
+    gsutil -m rm -r $target_dir/2019-05-11/08/
+    gsutil -m rm -r $target_dir/2019-05-11/09/
+    gsutil -m rm -r $target_dir/2019-05-11/10/
+}
+
 # telemetry-raw_gcs-sink
 # Error:  gs://moz-fx-data-stage-landfill/telemetry-error (Empty)
 # Main: gs://moz-fx-data-stage-landfill/telemetry
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/telemetry/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-landfill/telemetry
 
 # telemetry-decoded_gcs-sink
 # Error:  gs://moz-fx-data-stage-data/telemetry-decoded-error (Empty)
 # Main: gs://moz-fx-data-stage-data/telemetry-decoded
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-decoded/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-data/telemetry-decoded
 
 # telemetry-error_gcs-sink
 # Error:  gs://moz-fx-data-stage-data/telemetry-error (Empty)
 # Main: gs://moz-fx-data-stage-data/telemetry
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-data/telemetry
 
 # telemetry-decoded_bq-sink
 # Main: output to BQ, so not applicable
 # Error:  gs://moz-fx-data-stage-data/telemetry-bq-sink-error
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/telemetry-bq-sink-error/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-data/telemetry-bq-sink-error
 
 # structured-raw_gcs-sink
 # Error:  gs://moz-fx-data-stage-landfill/structured-error (Empty)
 # Main: gs://moz-fx-data-stage-landfill/structured
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-landfill/structured/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-landfill/structured
 
 # structured-decoded_gcs-sink
 # Error:  gs://moz-fx-data-stage-data/structured-decoded-error (Empty)
 # Main: gs://moz-fx-data-stage-data/structured-decoded
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-decoded/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-data/structured-decoded
 
 # structured-error_gcs-sink
 # Error:  gs://moz-fx-data-stage-data/structured-error (Empty)
 # Main: gs://moz-fx-data-stage-data/structured
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-data/structured
 
 # structured-decoded_bq-sink
 # Main: output to BQ, so not applicable
 # Error:  gs://moz-fx-data-stage-data/structured-bq-sink-error
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/11/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/12/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/13/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/14/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/15/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/16/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/17/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/18/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/19/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/20/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/21/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/22/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-04/23/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/00/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/01/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/02/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/03/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/04/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/05/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/06/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/07/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/08/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/09/*'
-gsutil -m rm -r 'gs://moz-fx-data-stage-data/structured-bq-sink-error/2019-05-05/10/*'
+delete_hours gs://moz-fx-data-stage-data/structured-bq-sink-error

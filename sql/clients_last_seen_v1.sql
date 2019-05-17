c6fed78815665de51f153c93e7d8c72cf9dfe525
Jeff Klukas <jeff@klukas.net>
2019-05-17 16:29:20 -0400

    Refactor to use days_created_profile_bits
    Applying the same technique suggested by @relud in #136 to desktop.

--- sql/clients_last_seen_v1.sql
+++ sql/clients_last_seen_v1.sql
@@ -5,6 +5,7 @@ SELECT
   CAST(SAFE.LOG(days_seen_bits & -days_seen_bits, 2) AS INT64) AS days_since_seen,
   CAST(SAFE.LOG(days_visited_5_uri_bits & -days_visited_5_uri_bits, 2) AS INT64) AS days_since_visited_5_uri,
   CAST(SAFE.LOG(days_opened_dev_tools_bits & -days_opened_dev_tools_bits, 2) AS INT64) AS days_since_opened_dev_tools,
+  CAST(SAFE.LOG(days_created_profile_bits & -days_created_profile_bits, 2) AS INT64) AS days_since_created_profile,
   * REPLACE ( --
     ARRAY(
     SELECT

45e998ab87575271a8abc4b9aeeaf3c20553fea8
Jeff Klukas <jeff@klukas.net>
2019-05-21 20:27:15 -0400

    Add a test harness for UDFs
    This is the first stage for addressing #141
    
    With this change, we parse dependencies between UDFs so that each
    UDF sql file can contain just a single UDF definition.
    Furthermore, we allow additional SQL statements in the file which
    will be treated as tests; they should call ERROR to cause
    a test to fail.

--- udf/udf_mode_last.sql
+++ udf/udf_mode_last.sql
@@ -1,3 +1,12 @@
+/*
+
+Returns the most frequently occuring element in an array.
+
+In the case of multiple values tied for the highest count, it returns the value
+that appears latest in the array. Nulls are ignored.
+
+*/
+
 CREATE TEMP FUNCTION
   udf_mode_last(list ANY TYPE) AS ((
     SELECT
@@ -16,22 +25,9 @@ CREATE TEMP FUNCTION
     LIMIT
       1 ));
 
-/*
+-- Test
 
-Returns the most frequently occuring element in an array.
-
-In the case of multiple values tied for the highest count, it returns the value
-that appears latest in the array. Nulls are ignored.
-
-Examples:
-
-SELECT udf_mode_last(['foo', 'bar', 'baz', 'bar', 'fred']);
--- bar
-
-SELECT udf_mode_last(['foo', 'bar', 'baz', 'bar', 'baz', 'fred']);
--- baz
-
-SELECT udf_mode_last([null, 'foo', null]);
--- foo
-
-*/
+SELECT
+  assert_equals('bar', udf_mode_last(['foo', 'bar', 'baz', 'bar', 'fred'])),
+  assert_equals('baz', udf_mode_last(['foo', 'bar', 'baz', 'bar', 'baz', 'fred'])),
+  assert_equals('foo', udf_mode_last([null, 'foo', null]));
--- udf/udf_null_if_empty_list.sql
+++ udf/udf_null_if_empty_list.sql
@@ -2,3 +2,8 @@ CREATE TEMP FUNCTION
   udf_null_if_empty_list(list ANY TYPE) AS ( IF(ARRAY_LENGTH(list.list) > 0,
       list,
       NULL) );
+
+-- Tests
+
+SELECT
+  assert_null(udf_null_if_empty_list(struct([] AS list)));

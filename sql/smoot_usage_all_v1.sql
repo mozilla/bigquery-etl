8e8e37120283a3f042e1e06a522ea4c600b7c3c9
Jeff Klukas <jeff@klukas.net>
2019-05-24 17:05:29 -0400

    Smoot tables and views to support GUD
    These tables will support the exploratory Growth and Usage dashboard
    that we will be demo-ing at Whistler.

---
+++ sql/smoot_usage_all_v1.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-derived-datasets.telemetry.smoot_usage_all_v1`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-derived-datasets.telemetry.smoot_usage_desktop_v1`
+--
+UNION ALL
+--
+SELECT
+  *
+FROM
+  `moz-fx-data-derived-datasets.telemetry.smoot_usage_nondesktop_v1`

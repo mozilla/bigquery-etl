fdb63d8816e5266dc6b178cec743e4c68c096a54
Daniel Thorn <dthorn@mozilla.com>
2019-07-19 11:41:34 -0700

    Add views to fix sample_id, submission_date, maps, and lists (#240)

--- sql/clients_last_seen_v1.sql
+++ sql/clients_last_seen_v1.sql
@@ -9,6 +9,16 @@ SELECT
   CAST(SAFE.LOG(days_opened_dev_tools_bits & -days_opened_dev_tools_bits, 2) AS INT64) AS days_since_opened_dev_tools,
   CAST(SAFE.LOG(days_created_profile_bits & -days_created_profile_bits, 2) AS INT64) AS days_since_created_profile,
   * REPLACE ( --
+    ARRAY(SELECT * FROM UNNEST(active_addons.list)) AS active_addons,
+    ARRAY(SELECT * FROM UNNEST(environment_settings_intl_accept_languages.list)) AS environment_settings_intl_accept_languages,
+    ARRAY(SELECT * FROM UNNEST(environment_settings_intl_app_locales.list)) AS environment_settings_intl_app_locales,
+    ARRAY(SELECT * FROM UNNEST(environment_settings_intl_available_locales.list)) AS environment_settings_intl_available_locales,
+    ARRAY(SELECT * FROM UNNEST(environment_settings_intl_regional_prefs_locales.list)) AS environment_settings_intl_regional_prefs_locales,
+    ARRAY(SELECT * FROM UNNEST(environment_settings_intl_requested_locales.list)) AS environment_settings_intl_requested_locales,
+    ARRAY(SELECT * FROM UNNEST(environment_settings_intl_system_locales.list)) AS environment_settings_intl_system_locales,
+    experiments.key_value AS experiments,
+    SAFE_CAST(sample_id AS INT64) AS sample_id,
+    scalar_parent_devtools_accessibility_select_accessible_for_node_sum.key_value AS scalar_parent_devtools_accessibility_select_accessible_for_node_sum,
     ARRAY(
     SELECT
       AS STRUCT *,
---
+++ templates/addons_v2.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-derived-datasets.telemetry.addons_v2` AS
+SELECT
+  submission_date_s3 AS submission_date,
+  * REPLACE (
+    SAFE_CAST(sample_id AS INT64) AS sample_id
+  )
+FROM
+  `moz-fx-data-derived-datasets.telemetry_raw.addons_v2`

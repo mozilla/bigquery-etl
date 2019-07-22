990e08e3de6f5d696e198a5bbed63373f9c81db8
Daniel Thorn <dthorn@mozilla.com>
2019-07-22 14:01:47 -0700

    Modify views for direct2parquet clustered tables (#245)

--- sql/telemetry_mobile_event_parquet_v2.sql
+++ sql/telemetry_mobile_event_parquet_v2.sql
@@ -4,7 +4,7 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-derived-datasets.telemetry.telemetry_mobile_event_parquet_v2` AS
 SELECT
-  submission_date_s3 AS submission_date,
+  submission_date AS submission_date_s3,
   * REPLACE (
     ARRAY(SELECT AS STRUCT _0.element.* REPLACE (_0.element.extra.key_value AS extra) FROM UNNEST(events.list) AS _0) AS events,
     experiments.key_value AS experiments,
--- templates/telemetry_focus_event_parquet_v1.sql
+++ templates/telemetry_focus_event_parquet_v1.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-derived-datasets.telemetry.telemetry_focus_event_parquet_v1` AS
 SELECT
-  submission_date_s3 AS submission_date,
+  submission_date AS submission_date_s3,
   * REPLACE (
     ARRAY(SELECT AS STRUCT _0.element.* REPLACE (_0.element.extra.key_value AS extra) FROM UNNEST(events.list) AS _0) AS events,
     experiments.key_value AS experiments,

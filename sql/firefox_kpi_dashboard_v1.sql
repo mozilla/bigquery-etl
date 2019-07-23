698a43313475699b5b48ade8948e5a7a9c04500a
Jeff Klukas <jeff@klukas.net>
2019-07-23 17:03:24 -0400

    Fix edge cases in sum_buckets_with_ci

--- sql/firefox_kpi_dashboard_v1.sql
+++ sql/firefox_kpi_dashboard_v1.sql
@@ -1,14 +1,13 @@
 CREATE TEMP FUNCTION
-  udf_js_sum_buckets_with_ci(a ARRAY<INT64>)
+  udf_js_sum_buckets_with_ci(n_buckets INT64, counts_per_bucket ARRAY<INT64>)
   RETURNS STRUCT<total INT64,
   low INT64,
   high INT64,
-  pm INT64,
-  nbuckets INT64>
+  pm INT64>
   LANGUAGE js AS """
 function erfinv(x){
     var z;
-    var a  = 0.147;
+    var a = 0.147;
     var the_sign_of_x;
     if(0==x) {
         the_sign_of_x = 0;
@@ -30,6 +29,7 @@ function erfinv(x){
     }
     return z;
 }
+
 function jackknife_resamples(arr) {
     output = [];
     for (var i = 0; i < arr.length; i++) {
@@ -39,24 +39,29 @@ function jackknife_resamples(arr) {
     }
     return output;
 }
+
 function array_sum(arr) {
     return arr.reduce(function (acc, x) {
         return acc + x;
     }, 0);
 }
+
 function array_avg(arr) {
     return array_sum(arr) / arr.length;
 }
-function sum_buckets_with_ci(counts_per_bucket) {
+
+function sum_buckets_with_ci(n_buckets, counts_per_bucket) {
+    if (counts_per_bucket.every(x => x == null)) {
+      return null;
+    };
     counts_per_bucket = counts_per_bucket.map(x => parseInt(x));
-    var n = counts_per_bucket.length;
     var total = array_sum(counts_per_bucket);
-    var mean = total / n;
+    var mean = total / n_buckets;
     var resamples = jackknife_resamples(counts_per_bucket);
     var jk_means = resamples.map(x => array_avg(x));
     var mean_errs = jk_means.map(x => Math.pow(x - mean, 2));
-    var bucket_std_err = Math.sqrt((n-1) * array_avg(mean_errs));
-    var std_err = n * bucket_std_err;
+    var bucket_std_err = Math.sqrt((n_buckets-1) * array_avg(mean_errs));
+    var std_err = n_buckets * bucket_std_err;
     var z_score = Math.sqrt(2.0) * erfinv(0.90);
     var hi = total + z_score * std_err;
     var lo = total - z_score * std_err;
@@ -65,10 +70,10 @@ function sum_buckets_with_ci(counts_per_bucket) {
         "low": lo,
         "high": hi,
         "pm": hi - total,
-        "nbuckets": n,
     };
 }
-return sum_buckets_with_ci(a);
+
+return sum_buckets_with_ci(n_buckets, counts_per_bucket);
 """;
 --
 WITH
@@ -196,7 +201,7 @@ WITH
     datasource,
     type,
     submission_date,
-    udf_js_sum_buckets_with_ci(ARRAY_AGG(mau)).*
+    udf_js_sum_buckets_with_ci(20, ARRAY_AGG(mau)).*
   FROM
     per_bucket
   GROUP BY

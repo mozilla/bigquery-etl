ed65698685036ab5247afb0acdc0d8c7d3be5948
Daniel Thorn <dthorn@mozilla.com>
2019-07-24 14:23:36 -0700

    Reimplement main_summary_v4 as SQL

---
+++ udf_js/main_summary_disabled_addons.sql
@@ -0,0 +1,29 @@
+/*
+
+Report the ids of the addons which are in the addonDetails but not in the
+activeAddons.  They are the disabled addons (possibly because they are legacy).
+We need this as addonDetails may contain both disabled and active addons.
+
+https://github.com/mozilla/telemetry-batch-view/blob/ea0733c00df191501b39d2c4e2ece3fe703a0ef3/src/main/scala/com/mozilla/telemetry/views/MainSummaryView.scala#L451-L464
+
+*/
+
+CREATE TEMP FUNCTION udf_js_main_summary_disabled_addons(
+  active_addon_ids ARRAY<STRING>,
+  addon_details_json STRING
+)
+RETURNS ARRAY<STRING>
+LANGUAGE js AS """
+const addonDetails = Object.keys(JSON.parse(addon_details_json) || {});
+return addonDetails.filter(k => !(active_addon_ids || []).includes(k));
+""";
+-- Tests
+SELECT
+  assert_array_equals(
+    udf_js_main_summary_disabled_addons(["foo", "baz", "buz"], '{"foo":{}, "buz":{}, "blee":{}}'),
+    ["blee"]
+  ),
+  assert_array_equals(
+    udf_js_main_summary_disabled_addons(NULL, '{"foo":{}}'),
+    ["foo"]
+  )

0acb5f31f915bd8ea0dc6b6d15c1cb32b8aa5840
Jeff Klukas <jeff@klukas.net>
2019-08-05 11:53:04 -0400

    Alter core view to union v2 to v10

--- templates/telemetry/core.sql
+++ templates/telemetry/core.sql
@@ -1,4 +1,27 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.core`
-AS SELECT * FROM
-  `moz-fx-data-shared-prod.telemetry_stable.core_v10`
+AS
+WITH unioned AS (
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v2`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v3`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v4`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v5`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v6`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v7`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v8`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v9`
+  UNION ALL
+  SELECT * FROM `moz-fx-data-shared-prod.telemetry_stable.core_v10`)
+  --
+SELECT
+  *
+FROM
+  unioned
+

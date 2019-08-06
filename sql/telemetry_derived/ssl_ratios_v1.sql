ef1e1146a8b7f24964f0d52a01d1bef47283aebe
Allen Short <ashort@mozilla.com>
2019-08-06 12:23:11 -0500

    SSL ratios query (#259)
    * SSL ratios query
    
    * use raw table with a window in view
    
    * better match existing
    
    * use telemetry_derived
    
    * test for telemetry_derived
    
    * Fix dry run for main_summary_v4

---
+++ sql/telemetry_derived/ssl_ratios_v1.sql
@@ -0,0 +1,31 @@
+CREATE TEMP FUNCTION udf_get_key(map ANY TYPE, k ANY TYPE) AS (
+ (
+   SELECT key_value.value
+   FROM UNNEST(map) AS key_value
+   WHERE key_value.key = k
+   LIMIT 1
+ )
+);
+--
+SELECT
+  submission_date,
+  os,
+  country,
+  SUM(udf_get_key(histogram_parent_http_pageload_is_ssl, 0)) AS non_ssl_loads,
+  SUM(udf_get_key(histogram_parent_http_pageload_is_ssl, 1)) AS ssl_loads,
+  -- ratio of pings that have the probe
+  COUNTIF(ARRAY_LENGTH(histogram_parent_http_pageload_is_ssl) > 0) / COUNT(*) AS reporting_ratio
+FROM
+  telemetry.main_summary_v4
+WHERE
+  sample_id = 42
+  AND normalized_channel = 'release'
+  AND os IN ('Windows_NT', 'Darwin', 'Linux')
+  AND app_name = 'Firefox'
+  AND submission_date > DATE '2016-11-01'
+  AND (submission_date = @submission_date
+    OR @submission_date IS NULL)
+GROUP BY
+  submission_date,
+  os,
+  country

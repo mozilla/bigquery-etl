1fad5ef97990991f165169e4c9248f8e8b4ce0ce
Jeff Klukas <jeff@klukas.net>
2019-08-20 16:26:00 -0400

    Fixup outdated lockwise view for parquet unnesting

--- sql/telemetry/lockwise_mobile_events_v1.sql
+++ sql/telemetry/lockwise_mobile_events_v1.sql
@@ -10,13 +10,8 @@ WITH
         timestamp_seconds(cast(substr(cast(metadata.timestamp AS string), 1, 10) AS int64)) AS submission_timestamp,
         device,
         metadata.app_build_id,
-        e.element.timestamp,
-        e.element.category,
-        e.element.method,
-        e.element.object,
-        e.element.value,
-        e.element.extra.key_value
-    FROM `moz-fx-data-derived-datasets.telemetry.telemetry_mobile_event_parquet_v2`, UNNEST(events.list) AS e
+        e.*
+    FROM `moz-fx-data-derived-datasets.telemetry.telemetry_mobile_event_parquet_v2`, UNNEST(events) AS e
     WHERE metadata.app_name = 'Lockbox'
     AND os = 'Android'
     -- Filter out test data before the Android launch date.
@@ -31,14 +26,9 @@ WITH
         timestamp_seconds(cast(substr(cast(metadata.timestamp AS string), 1, 10) AS int64)) AS submission_timestamp,
         device,
         metadata.app_build_id,
-        e.element.timestamp,
-        e.element.category,
-        e.element.method,
-        e.element.object,
-        e.element.value,
-        e.element.extra.key_value
+        e.*
     FROM
-    `moz-fx-data-derived-datasets.telemetry.telemetry_focus_event_parquet_v1`, UNNEST(events.list) AS e
+    `moz-fx-data-derived-datasets.telemetry.telemetry_focus_event_parquet_v1`, UNNEST(events) AS e
     WHERE metadata.app_name = 'Lockbox'
     AND os = 'iOS'
     -- Filter out test data before the iOS launch date.

02af8e82e788a811428a1f02037ac322a93424f2
Jeff Klukas <jeff@klukas.net>
2019-08-22 15:24:28 -0400

    Have unversioned views hit versioned views for parquet unnesting
    flod reported in `#fx-metrics` that submission_date_s3 was missing
    for the clients_daily view. That view and other unversioned views
    were directly referencing telemetry_derived tables and missing out
    on transformations such as parquet unnesting.

--- templates/telemetry/first_shutdown_summary.sql
+++ templates/telemetry/first_shutdown_summary.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.first_shutdown_summary`
 AS SELECT * FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.first_shutdown_summary_v4`
+  `moz-fx-data-derived-datasets.telemetry.first_shutdown_summary_v4`
--- templates/telemetry/telemetry_mobile_event_parquet.sql
+++ templates/telemetry/telemetry_mobile_event_parquet.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.telemetry_mobile_event_parquet`
 AS SELECT * FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.telemetry_mobile_event_parquet_v2`
+  `moz-fx-data-derived-datasets.telemetry.telemetry_mobile_event_parquet_v2`

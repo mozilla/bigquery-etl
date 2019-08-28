5ccd7315bea6c03b01fcc4617ac0ba261085cda6
Sunah Suh <ssuh@mozilla.com>
2019-08-28 16:57:04 -0500

    fixup! Add initial legacy Presto/Athena compatibility udfs

--- udf_legacy/date_format.sql
+++ udf_legacy/date_format.sql
@@ -2,6 +2,10 @@
 
 Shim for Presto/Athena's DATE_FORMAT to BigQuery's FORMAT_DATE
 
+Note that the format strings for FORMAT_DATE vs DATE_FORMAT are not
+identical, but usages found in existing scheduled queries had format
+strings that were portable between the two
+
 */
 CREATE TEMP FUNCTION
   udf_legacy_date_format(d DATE, f STRING)

7de9f8793c0bd50c9de3dc79a8e8d7928f7688b0
Jason Thomas <jason@lithiumfox.com>
2019-09-06 17:34:08 -0400

    Create addon_aggregates views for table rename bug1579478

--- sql/telemetry/addons_aggregates/view.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.addons_aggregates`
-AS SELECT * FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.addons_aggregates_v2`
---
+++ templates/telemetry/addon_aggregates/view.sql
@@ -0,0 +1,4 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.addon_aggregates`
+AS SELECT * FROM
+  `moz-fx-data-derived-datasets.telemetry_derived.addon_aggregates_v2`

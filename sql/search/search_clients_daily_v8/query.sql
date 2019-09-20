d296ce872f4f14b6f6112dcbc4fae6a945b90a0e
Benjamin Wu <benjaminwu124@gmail.com>
2019-09-20 19:06:02 -0400

    Add additional fields to search datasets

--- sql/search/search_clients_daily_v8/query.sql
+++ sql/search/search_clients_daily_v8/query.sql
@@ -16,6 +16,37 @@ CREATE TEMP FUNCTION
     LIMIT
       1 ));
 --
+-- Return the version of the search addon if it exists, null otherwise
+CREATE TEMP FUNCTION get_search_addon_version(active_addons ANY type) AS (
+  (
+    SELECT
+      element.version
+    FROM
+      (
+        SELECT
+          list,
+          _offset_1
+        FROM
+          UNNEST(active_addons)
+        WITH
+          OFFSET AS _offset_1
+      ),
+      UNNEST(list)
+    WITH
+      OFFSET AS _offset_2
+    WHERE
+      element.addon_id = 'followonsearch@mozilla.com'
+    GROUP BY
+      element.version
+    ORDER BY
+      COUNT(element.version) DESC,
+      MAX(_offset_1) DESC,
+      MAX(_offset_2) DESC
+    LIMIT
+      1
+  )
+);
+
 WITH
   augmented AS (
   SELECT
@@ -57,7 +88,7 @@ WITH
       FROM
         UNNEST(scalar_parent_browser_search_with_ads.key_value) ) ) AS _searches
   FROM
-    main_summary_v4 ),
+    telemetry_derived.main_summary_v4 ),
   flattened AS (
   SELECT
     *
@@ -77,11 +108,12 @@ WITH
   windowed AS (
   SELECT
     ROW_NUMBER() OVER w1_unframed AS _n,
-    submission_date_s3 AS submission_date,
+    submission_date,
     client_id,
     engine,
     source,
     udf_mode_last(ARRAY_AGG(country) OVER w1) AS country,
+    get_search_addon_version(ARRAY_AGG(active_addons) OVER w1) AS addon_version,
     udf_mode_last(ARRAY_AGG(app_version) OVER w1) AS app_version,
     udf_mode_last(ARRAY_AGG(distribution_id) OVER w1) AS distribution_id,
     udf_mode_last(ARRAY_AGG(locale) OVER w1) AS locale,
@@ -140,7 +172,7 @@ WITH
   FROM
     flattened
   WHERE
-    submission_date_s3 = @submission_date
+    submission_date = @submission_date
     AND client_id IS NOT NULL
     AND (count < 10000
       OR count IS NULL)
@@ -149,7 +181,7 @@ WITH
     w1 AS (
     PARTITION BY
       client_id,
-      submission_date_s3,
+      submission_date,
       engine,
       source,
       type
@@ -160,7 +192,7 @@ WITH
     w1_unframed AS (
     PARTITION BY
       client_id,
-      submission_date_s3,
+      submission_date,
       engine,
       source,
       type

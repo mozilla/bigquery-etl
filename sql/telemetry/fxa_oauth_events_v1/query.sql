9b2613cc406f44d6ab8a470a7ade3b42a2a2dc3e
Jeff Klukas <jeff@klukas.net>
2019-10-01 21:33:35 -0400

    Add more commentary

--- sql/telemetry/fxa_oauth_events_v1/query.sql
+++ sql/telemetry/fxa_oauth_events_v1/query.sql
@@ -12,9 +12,12 @@ WHERE
   AND jsonPayload.fields.event_type IS NOT NULL
   AND jsonPayload.fields.user_id IS NOT NULL
   AND JSON_EXTRACT_SCALAR(jsonPayload.fields.event_properties,
-    '$.oauth_client_id') NOT IN ( -- Per https://github.com/mozilla/bigquery-etl/issues/348
-    '3332a18d142636cb',
-    '5882386c6d801776',
-    '1b1a3e44c54fbb58')
+    '$.oauth_client_id') NOT IN (
+    -- We filter out events associated with high-volume oauth client IDs that
+    -- are redundant with cert_signed events;
+    -- see https://github.com/mozilla/bigquery-etl/issues/348
+    '3332a18d142636cb', -- fennec sync
+    '5882386c6d801776', -- desktop sync
+    '1b1a3e44c54fbb58') -- ios sync
   AND _TABLE_SUFFIX = FORMAT_DATE('%g%m%d',
     @submission_date)

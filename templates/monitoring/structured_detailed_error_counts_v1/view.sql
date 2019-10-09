239fab252a4c40bb574e2ad2e97770b1891edcec
Frank Bertsch <fbertsch@mozilla.com>
2019-10-09 10:03:58 -0400

    Pipeline sql (#388)
    * Views for monitoring structured ingestion errors
    
    * Add UDF for extracting missing columns
    
    * Add docs for json_extract_missing_cols
    
    * Fix missing WITH clause
    
    * Add generated sql
    
    * Fix test function
    
    * Update views
    
    * Update tests
    
    * Use shared-prod
    
    * Update docstring for missing cols udf
    
    * Move schemas to structured_ingestion dataset
    
    * Don't dryrun queries on payload_bytes
    
    * Change format of payload_bytes views
    
    - Use a with_ratio to reduce duplication
    - Change the view name to match the filename
    
    * Fix missing test function
    
    * Move to monitoring dataset
    
    * Move to new test structure
    
    * Remove spaces from js udfs
    
    * Use persistent gunzip UDF

---
+++ templates/monitoring/structured_detailed_error_counts_v1/view.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.pipeline.structured_detailed_error_counts_v1`
+AS
+WITH error_examples AS (
+  SELECT
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS hour,
+    document_namespace,
+    document_type,
+    document_version,
+    error_type,
+    error_message,
+    udf_js,gunzip(ANY_VALUE(payload)) AS sample_payload,
+    COUNT(*) AS error_count
+  FROM
+    `moz-fx-data-shared-prod.payload_bytes_error.structured`
+  WHERE
+    submission_timestamp >= TIMESTAMP_SUB(current_timestamp, INTERVAL 28 * 24 HOUR)
+  GROUP BY hour, document_namespace, document_type, document_version, error_type, error_message
+), structured_detailed_hourly_errors AS (
+  SELECT
+    hour,
+    document_namespace,
+    document_type,
+    document_version,
+    structured_hourly_errors.ping_count,
+    error_examples.error_count,
+    error_message,
+    sample_payload
+  FROM
+    structured_hourly_errors
+  INNER JOIN
+    error_examples USING (hour, document_namespace, document_type, document_version, error_type)
+), with_ratio AS (
+  SELECT
+    *,
+    SAFE_DIVIDE(1.0 * error_count, ping_count) AS error_ratio
+  FROM
+    structured_detailed_hourly_errors
+)
+SELECT * FROM
+  with_ratio

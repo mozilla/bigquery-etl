f21364f3d5c277ff564bac7f7972e308d02042de
Daniel Thorn <dthorn@mozilla.com>
2019-10-16 10:55:24 -0700

    Add views for search_derived dataset

---
+++ sql/search/search_aggregates_v7/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.search.search_aggregates_v7` AS
+SELECT
+  *,
+  submission_date AS submission_date_s3
+FROM
+  `moz-fx-data-shared-prod.search_derived.search_aggregates_v7`

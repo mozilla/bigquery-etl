88f7f644688656eb5faffee392ec09b73819a0c4
Marina Samuel <msamuel@mozilla.com>
2019-10-30 13:43:31 -0400

    Add files for Glam views.

---
+++ sql/telemetry/clients_histogram_aggregates_v1/view.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.clients_histogram_aggregates`
+AS SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.clients_histogram_aggregates_v1`

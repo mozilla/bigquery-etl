c0b59db053bdbf3058baf4c2c49efa9a0d0fbfaa
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-10-31 14:09:14 -0700

    Address review; use CTE for top-k query and remove sort clause

--- templates/telemetry/origin_content_blocking/view.sql
+++ templates/telemetry/origin_content_blocking/view.sql
@@ -1,28 +1,29 @@
 CREATE
-OR REPLACE VIEW `moz-fx-data-shared-prod.telemetry.origin_content_blocking` AS
+OR REPLACE VIEW `moz-fx-data-shared-prod.telemetry.origin_content_blocking` AS WITH most_recent AS (
+  SELECT
+    * EXCEPT(_n)
+  FROM
+    (
+      SELECT
+        *,
+        ROW_NUMBER() OVER (
+          PARTITION BY
+            submission_date,
+            batch_id,
+            origin
+          ORDER BY
+            TIMESTAMP DESC
+        ) AS _n
+      FROM
+        `moz-fx-data-shared-prod.telemetry_derived.origin_content_blocking`
+    )
+  WHERE
+    _n = 1
+)
 SELECT
   submission_date,
   batch_id,
   origin,
   aggregate
 FROM
-  (
-    SELECT
-      *,
-      ROW_NUMBER() OVER (
-        PARTITION BY
-          submission_date,
-          batch_id,
-          origin
-        ORDER BY
-          TIMESTAMP DESC
-      ) AS insert_rank
-    FROM
-      `moz-fx-data-shared-prod.telemetry_derived.origin_content_blocking`
-  )
-WHERE
-  insert_rank = 1
-ORDER BY
-  submission_date DESC,
-  batch_id,
-  aggregate DESC
+  most_recent

be99d5a078239f0214801e18bf02699a3b2ed51a
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-10-31 13:50:58 -0700

    Use table_name instead of _TABLE_PREFIX

--- udf/extract_document_type.sql
+++ udf/extract_document_type.sql
@@ -3,7 +3,7 @@
 Extract the document type from a table name e.g. _TABLE_SUFFIX.
 
 */
-CREATE TEMP FUNCTION udf_extract_document_type(table_name STRING) AS (REGEXP_EXTRACT(_TABLE_SUFFIX, r"^(.*)_v.*"));
+CREATE TEMP FUNCTION udf_extract_document_type(table_name STRING) AS (REGEXP_EXTRACT(table_name, r"^(.*)_v.*"));
 -- Tests
 SELECT
   assert_equals("type", udf_extract_document_type("type_v1")),
--- udf/extract_document_version.sql
+++ udf/extract_document_version.sql
@@ -3,7 +3,7 @@
 Extract the document version from a table name e.g. _TABLE_SUFFIX.
 
 */
-CREATE TEMP FUNCTION udf_extract_document_version(table_name STRING) AS (REGEXP_EXTRACT(_TABLE_SUFFIX, r"^.*_v(.*)$"));
+CREATE TEMP FUNCTION udf_extract_document_version(table_name STRING) AS (REGEXP_EXTRACT(table_name, r"^.*_v(.*)$"));
 -- Tests
 SELECT
   assert_equals("1", udf_extract_document_type("type_v1")),

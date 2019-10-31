82151c087a0da4f7fab07147949b928d7e91a41c
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-10-31 13:47:51 -0700

    Fix missing space from format-sql and broken test case

--- udf/extract_document_type.sql
+++ udf/extract_document_type.sql
@@ -3,11 +3,11 @@
 Extract the document type from a table name e.g. _TABLE_SUFFIX.
 
 */
-CREATE TEMP FUNCTION udf_extract_document_type(table_name STRING) AS (REGEXP_EXTRACT(_TABLE_SUFFIX, r "^(.*)_v.*"));
+CREATE TEMP FUNCTION udf_extract_document_type(table_name STRING) AS (REGEXP_EXTRACT(_TABLE_SUFFIX, r"^(.*)_v.*"));
 -- Tests
 SELECT
   assert_equals("type", udf_extract_document_type("type_v1")),
   assert_equals(
     "namespace__namespace_type",
-    udf_extract_document_type("namespace__namespace_type_v20191101")
+    udf_extract_document_type("namespace__namespace_type_v20191031")
   );

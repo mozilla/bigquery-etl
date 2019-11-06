21c944563d884f072e8d138b38b36dca1d3f93b0
Sunah Suh <ssuh@mozilla.com>
2019-11-06 10:32:58 -0600

    Add convenience function to extract count histogram values

---
+++ udf/extract_count_histogram_value.sql
@@ -0,0 +1,26 @@
+CREATE TEMP FUNCTION
+  udf_extract_count_histogram_value(input STRING) AS (
+    udf_get_key(udf_json_extract_histogram(input).values, 0)
+  );
+
+-- Tests
+
+WITH
+  histogram AS (
+    SELECT AS VALUE
+      ['{"bucket_count":3,"histogram_type":4,"sum":1,"range":[1,2],"values":{"0":1,"1":0}}',
+        '{"range":[1,2],"bucket_count":3,"histogram_type":4,"values":{},"sum":0}']
+    ),
+  --
+  extracted AS (
+     SELECT
+       udf_extract_count_histogram_value(histogram[OFFSET(0)]) as has_value,
+       udf_extract_count_histogram_value(histogram[OFFSET(1)]) as null_value
+     FROM
+       histogram )
+    --
+SELECT
+  assert_equals(1, has_value),
+  assert_null(null_value)
+FROM
+  extracted

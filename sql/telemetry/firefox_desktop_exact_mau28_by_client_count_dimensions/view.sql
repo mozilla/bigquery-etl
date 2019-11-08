9ab2b0acd75600e117e7b18cd1c05637f3152009
Mark Reid <mreid@mozilla.com>
2019-11-08 13:57:37 -0500

    Add a dataset for compatibility with "client_count" (#494)
    Add datasets for compatibility with "client_count" HLL type datasets

---
+++ sql/telemetry/firefox_desktop_exact_mau28_by_client_count_dimensions/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.firefox_desktop_exact_mau28_by_client_count_dimensions`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.firefox_desktop_exact_mau28_by_client_count_dimensions_v1`

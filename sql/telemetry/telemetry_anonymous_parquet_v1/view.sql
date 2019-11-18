e4f5140e82cb258dcb0246f44df85380d7a3c07c
Jeff Klukas <jeff@klukas.net>
2019-11-18 15:35:47 -0500

    Bug 1594783: Point several parquet views at stable ping tables
    This is a subset of the direct2parquet datasets. There are various reasons
    for not including datasets; some are due to likely lack of use, others due to
    complications in schema that make creating a compatible view impossible or
    high effort.

--- sql/telemetry/telemetry_anonymous_parquet_v1/view.sql
+++ sql/telemetry/telemetry_anonymous_parquet_v1/view.sql
@@ -1,7 +1,23 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.telemetry_anonymous_parquet_v1` AS
 SELECT
-  submission_date_s3 AS submission_date,
-  *
+  DATE(submission_timestamp) AS submission_date_s3,
+  STRUCT( document_id,
+    UNIX_MICROS(submission_timestamp) * 1000 AS `timestamp`,
+    metadata.header.date AS `date`,
+    IFNULL(metadata.geo.country,
+      '??') AS geo_country,
+    IFNULL(metadata.geo.city,
+      '??') AS geo_city,
+    metadata.uri.app_build_id,
+    metadata.uri.app_name,
+    metadata.uri.app_update_channel,
+    normalized_channel ) AS metadata,
+  application,
+  creation_date,
+  id,
+  payload,
+  type,
+  version
 FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.telemetry_anonymous_parquet_v1`
+  `moz-fx-data-shared-prod.telemetry.anonymous`
--- sql/telemetry/telemetry_downgrade_parquet_v1/view.sql
+++ sql/telemetry/telemetry_downgrade_parquet_v1/view.sql
@@ -1,7 +1,16 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.telemetry_downgrade_parquet_v1` AS
 SELECT
-  submission_date_s3 AS submission_date,
-  *
+  DATE(submission_timestamp) AS submission_date,
+  `id`,
+  creation_date,
+  application,
+  client_id,
+  version,
+  STRUCT(payload.last_version,
+    payload.last_build_id,
+    payload.has_sync,
+    CAST(NULL AS boolean) AS has_binary,
+    CAST(payload.button AS int64) AS button ) AS payload
 FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.telemetry_downgrade_parquet_v1`
+  `moz-fx-data-shared-prod.telemetry.downgrade`

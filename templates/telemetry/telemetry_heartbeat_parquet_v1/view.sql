649ccae9f6356245cfbf8ade34d69d78a2b978cc
Jeff Klukas <jeff@klukas.net>
2019-11-18 15:35:47 -0500

    Bug 1594783: Point several parquet views at stable ping tables
    This is a subset of the direct2parquet datasets. There are various reasons
    for not including datasets; some are due to likely lack of use, others due to
    complications in schema that make creating a compatible view impossible or
    high effort.

--- templates/telemetry/telemetry_heartbeat_parquet_v1/view.sql
+++ templates/telemetry/telemetry_heartbeat_parquet_v1/view.sql
@@ -1,7 +1,24 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.telemetry_heartbeat_parquet_v1` AS
 SELECT
-  submission_date_s3 AS submission_date,
-  *
+  DATE(submission_timestamp) AS submission_date_s3,
+  DATE(submission_timestamp) AS submission_date,
+  type,
+  id,
+  creation_date,
+  version,
+  client_id,
+  application,
+  (SELECT AS STRUCT payload.* replace (CAST(version AS int64) AS version)) AS payload,
+  STRUCT( UNIX_MICROS(submission_timestamp) * 1000 AS `timestamp`,
+    metadata.header.date AS `date`,
+    IFNULL(metadata.geo.country,
+      '??') AS geo_country,
+    IFNULL(metadata.geo.city,
+      '??') AS geo_city,
+    document_id,
+    metadata.uri.app_build_id,
+    metadata.uri.app_name ) AS metadata,
+  engagement_type
 FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.telemetry_heartbeat_parquet_v1`
+  `moz-fx-data-shared-prod.telemetry.heartbeat`

5cdf9b4f49a6050bd3efae90ee3e860162745531
Marina Samuel <msamuel@mozilla.com>
2019-11-25 08:29:52 -0500

    Add init files for scalar and histogram aggregates.

---
+++ templates/telemetry_derived/clients_scalar_aggregates_v1/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS
+  `moz-fx-data-shared-prod.telemetry_derived.clients_scalar_aggregates_v1` (
+    client_id STRING,
+    os STRING,
+    app_version INT64,
+    app_build_id STRING,
+    channel STRING,
+    scalar_aggregates ARRAY <STRUCT<metric STRING,
+    metric_type STRING,
+    key STRING,
+    agg_type STRING,
+    value FLOAT64>>)
+PARTITION BY RANGE_BUCKET(app_version, GENERATE_ARRAY(30, 200, 1))
+CLUSTER BY app_version, channel, client_id

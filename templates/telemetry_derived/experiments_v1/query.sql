6eee1c36d4271237080ec19d3ff367325aaac567
Daniel Thorn <dthorn@mozilla.com>
2019-12-03 15:05:15 -0800

    fix generate_sql

--- templates/telemetry_derived/experiments_v1/query.sql
+++ templates/telemetry_derived/experiments_v1/query.sql
@@ -1,11 +1,12 @@
 SELECT
   main_summary_v4.*,
   key AS experiment_id,
-  value AS experiment_branch
+  udf.get_key('branch', value) AS experiment_branch,
+  udf.get_key('enrollment_id', value) AS experiment_enrollment_id
 FROM
   main_summary_v4
 CROSS JOIN
-  UNNEST(experiments)
+  UNNEST(experiments_details)
 WHERE
   submission_date = @submission_date
   AND key IN UNNEST(@experiment_list)

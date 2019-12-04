a1ceb5c306c88128a0971a94190a22fef3d2009e
Daniel Thorn <dthorn@mozilla.com>
2019-12-03 16:02:17 -0800

    Fix order of arguments for udf.get_key (#568)
    and use black to format the script

--- templates/telemetry_derived/experiments_v1/get_experiment_list.py
+++ templates/telemetry_derived/experiments_v1/get_experiment_list.py
@@ -8,7 +8,12 @@ import time
 
 import requests
 
-experiments_qualifying_actions = {"preference-experiment", "opt-out-study", "branched-addon-study", "multi-preference-experiment"}
+experiments_qualifying_actions = {
+    "preference-experiment",
+    "opt-out-study",
+    "branched-addon-study",
+    "multi-preference-experiment",
+}
 excluded_experiments = {
     None,
     "pref-flip-screenshots-release-1369150",

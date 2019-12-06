7d9782b1bac370a7ccdfe2f541515246c4845932
Ben Wu <benjaminwu124@gmail.com>
2019-12-06 13:28:53 -0500

    Bug 1543434 - Create search datasets for mobile (#559)

---
+++ sql/search/mobile_search_aggregates/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.search.mobile_search_aggregates`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.search_derived.mobile_search_aggregates_v1`
---
+++ sql/search_derived/mobile_search_aggregates_v1/query.sql
@@ -0,0 +1,33 @@
+SELECT
+  submission_date,
+  engine,
+  source,
+  app_name,
+  app_version,
+  channel,
+  country,
+  locale,
+  distribution_id,
+  default_search_engine,
+  os,
+  os_version,
+  COUNT(*) AS client_count,
+  SUM(search_count) AS search_count
+FROM
+  mobile_search_clients_daily_v1
+WHERE
+  submission_date = @submission_date
+  AND engine IS NOT NULL
+GROUP BY
+  submission_date,
+  engine,
+  source,
+  app_name,
+  app_version,
+  channel,
+  country,
+  locale,
+  distribution_id,
+  default_search_engine,
+  os,
+  os_version

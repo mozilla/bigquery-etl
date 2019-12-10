d1948fa44569c1a5b730c4e49f793d27f0c56f6f
Jeff Klukas <jeff@klukas.net>
2019-12-10 14:03:59 -0500

    Revert "Add authorized views for payload_bytes raw and error"
    This reverts commit 1d2fa74f9ea811ff279157f44e1a48b591cd3325.

--- templates/monitoring/payload_bytes_raw_structured/view.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.monitoring.payload_bytes_raw_structured`
-AS SELECT
-  * EXCEPT (remote_addr, x_forwarded_for)
-FROM
-  `moz-fx-data-shared-prod.payload_bytes_raw.structured`

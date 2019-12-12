98d14e015e5bffc465650ffd82e018e0a8030930
Frank Bertsch <fbertsch@mozilla.com>
2019-12-12 06:53:14 -0500

    Update udf/new_monthly_engine_searches_struct.sql
    Co-Authored-By: Jeff Klukas <jeff@klukas.net>

--- udf/new_monthly_engine_searches_struct.sql
+++ udf/new_monthly_engine_searches_struct.sql
@@ -3,7 +3,7 @@ This struct represents the past year's worth of searches.
 Each month has its own entry, hence 12.
 */
 
-CREATE TEMP FUNCTION  udf_new_monthly_engine_searches_struct() AS (
+CREATE TEMP FUNCTION udf_new_monthly_engine_searches_struct() AS (
   STRUCT(
     udf_12_zeroes() AS total_searches,
     udf_12_zeroes() AS tagged_searches,

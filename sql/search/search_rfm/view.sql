796e364da77a58f3c41605d15f9c7f93e7c3da8a
Frank Bertsch <fbertsch@mozilla.com>
2019-12-19 17:08:55 -0500

    Fully escape UDFs

--- sql/search/search_rfm/view.sql
+++ sql/search/search_rfm/view.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.search.search_rfm` AS
 SELECT
-  `moz-fx-data-shared-prod`.udf.days_seen_bytes_to_rfm(days_seen_bytes) AS days_seen,
-  `moz-fx-data-shared-prod`.udf.days_seen_bytes_to_rfm(days_searched_bytes) AS days_searched,
-  `moz-fx-data-shared-prod`.udf.days_seen_bytes_to_rfm(days_tagged_searched_bytes) AS days_tagged_searched,
-  `moz-fx-data-shared-prod`.udf.days_seen_bytes_to_rfm(days_searched_with_ads_bytes) AS days_searched_with_ads,
-  `moz-fx-data-shared-prod`.udf.days_seen_bytes_to_rfm(days_clicked_ads_bytes) AS days_clicked_ads,
-  `moz-fx-data-shared-prod`.udf.bits_to_first_seen_int(days_created_profile_bytes) AS days_since_created_profile,
+  `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_seen_bytes) AS days_seen,
+  `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_searched_bytes) AS days_searched,
+  `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_tagged_searched_bytes) AS days_tagged_searched,
+  `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_searched_with_ads_bytes) AS days_searched_with_ads,
+  `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_clicked_ads_bytes) AS days_clicked_ads,
+  `moz-fx-data-shared-prod.udf.bits_to_first_seen_int`(days_created_profile_bytes) AS days_since_created_profile,
   * EXCEPT (
     days_seen_bytes,
     days_searched_bytes,

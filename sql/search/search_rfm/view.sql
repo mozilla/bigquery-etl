1d7defa9f2184002a5c7119494032b5a4a4985aa
Frank Bertsch <fbertsch@mozilla.com>
2019-12-19 17:31:38 -0500

    Fix _actual_ missing UDF

--- sql/search/search_rfm/view.sql
+++ sql/search/search_rfm/view.sql
@@ -6,7 +6,7 @@ SELECT
   `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_tagged_searched_bytes) AS days_tagged_searched,
   `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_searched_with_ads_bytes) AS days_searched_with_ads,
   `moz-fx-data-shared-prod.udf.days_seen_bytes_to_rfm`(days_clicked_ads_bytes) AS days_clicked_ads,
-  `moz-fx-data-shared-prod.udf.bits_to_first_seen_int`(days_created_profile_bytes) AS days_since_created_profile,
+  `moz-fx-data-shared-prod.udf.bits_to_days_since_first_seen`(days_created_profile_bytes) AS days_since_created_profile,
   * EXCEPT (
     days_seen_bytes,
     days_searched_bytes,

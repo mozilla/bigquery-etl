005fd845a48d29ee9d8a2eaa6b6ffd019c87d791
Daniel Thorn <dthorn@mozilla.com>
2019-12-23 18:02:12 -0800

    Rewrite script/format_sql in python

---
+++ bigquery_etl/format_sql/formatter.py
@@ -0,0 +1,189 @@
+"""Format SQL."""
+
+from dataclasses import replace
+import re
+
+from .tokenizer import (
+    ClosingBracket,
+    Comment,
+    ExpressionSeparator,
+    NewlineKeyword,
+    NoSpaceOperator,
+    Operator,
+    OpeningBracket,
+    ReservedKeyword,
+    SpaceBeforeBracketKeyword,
+    StatementSeparator,
+    TopLevelKeyword,
+    Whitespace,
+    tokenize,
+)
+
+
+def simple_format(tokens, indent="  "):
+    """Format tokens in a single pass."""
+    first_token = True
+    require_newline_before_next_token = False
+    allow_space_before_next_bracket = False
+    allow_space_before_next_token = False
+    indent_types = []
+    for token in tokens:
+        # skip original whitespace tokens
+        if isinstance(token, Whitespace):
+            continue
+
+        # uppercase keywords and replace contained whitespace with single spaces
+        if isinstance(token, ReservedKeyword):
+            token = replace(token, value=re.sub(r"\s+", " ", token.value.upper()))
+
+        # update state for current token
+        if isinstance(token, ClosingBracket):
+            # decrease indent to match last OpeningBracket
+            while indent_types and indent_types.pop() is not OpeningBracket:
+                pass
+        elif isinstance(token, TopLevelKeyword):
+            # decrease indent to top of current block
+            while indent_types and indent_types[-1] is TopLevelKeyword:
+                indent_types.pop()
+
+        # yield whitespace
+        if first_token or isinstance(token, Comment):
+            pass  # no space before first token and comments contain original whitespace
+        elif require_newline_before_next_token or isinstance(
+            token, (NewlineKeyword, ClosingBracket)
+        ):
+            yield Whitespace("\n" + indent * len(indent_types))
+        elif (
+            allow_space_before_next_token
+            and (
+                allow_space_before_next_bracket or not isinstance(token, OpeningBracket)
+            )
+            and not isinstance(
+                token, (NoSpaceOperator, ExpressionSeparator, StatementSeparator)
+            )
+        ):
+            yield Whitespace(" ")
+
+        yield token
+
+        # update state for next token
+        require_newline_before_next_token = isinstance(
+            token,
+            (
+                Comment,
+                TopLevelKeyword,
+                OpeningBracket,
+                ExpressionSeparator,
+                StatementSeparator,
+            ),
+        )
+        allow_space_before_next_token = not isinstance(token, NoSpaceOperator)
+        allow_space_before_next_bracket = isinstance(
+            token, (SpaceBeforeBracketKeyword, Operator)
+        )
+        if isinstance(token, (TopLevelKeyword, OpeningBracket)):
+            # increase indent
+            indent_types.append(type(token))
+        elif isinstance(token, StatementSeparator):
+            # reset indent
+            indent_types = []
+        first_token = False
+
+
+class Line:
+    """Container for a line of tokens."""
+
+    def __init__(self, indent_token=None):
+        """Initialize."""
+        self.indent_token = indent_token
+        if indent_token is None:
+            self.indent_level = 0
+        else:
+            self.indent_level = len(indent_token.value)
+            if indent_token.value.startswith("\n"):
+                self.indent_level -= 1
+        self.inline_tokens = []
+        self.inline_length = 0
+        self.contains_comment = isinstance(indent_token, Comment)
+        self.can_start_inline_block = False
+
+    def add(self, token):
+        """Add a token to this line."""
+        self.inline_length += len(token.value)
+        self.inline_tokens.append(token)
+        self.contains_comment = self.contains_comment or isinstance(token, Comment)
+        self.can_start_inline_block = isinstance(token, OpeningBracket)
+
+    @property
+    def tokens(self):
+        """Get a list of all the tokens in this line."""
+        if self.indent_token is None:
+            return self.inline_tokens
+        else:
+            return [self.indent_token] + self.inline_tokens
+
+
+def inline_block_format(tokens, max_inline_length=100):
+    """Extend simple_format with inline blocks.
+
+    Inline a bracket block if it would be shorter than max_inline_length.
+
+    Do not inline bracket blocks that contain a comment.
+
+    Implementation assumes simple_format will put opening brackets
+    at the end of the line and closing brackets at the beginning of the line,
+    unless there is a comment between them.
+    """
+    # format tokens using simple_format, then group into lines
+    lines = [Line()]
+    for token in simple_format(tokens):
+        if token.value.startswith("\n"):
+            lines.append(Line(token))
+        else:
+            lines[-1].add(token)
+
+    # combine all lines in each bracket block if that fits in max_inline_length
+    skip_lines = 0
+    for index, line in enumerate(lines):
+        if skip_lines > 0:
+            skip_lines -= 1
+            continue
+        yield from line.tokens
+        if line.can_start_inline_block:
+            indent_level = line.indent_level
+            line_length = indent_level + line.inline_length
+            pending_lines = 0
+            pending = []
+            last_token_is_opening_bracket = True
+            index = index + 1  # start on the next line
+            for line in lines[index:]:
+                if line.contains_comment:
+                    break
+                if not last_token_is_opening_bracket and not isinstance(
+                    line.inline_tokens[0], ClosingBracket
+                ):
+                    pending.append(Whitespace(" "))
+                    line_length += 1
+                pending_lines += 1
+                pending.extend(line.inline_tokens)
+                line_length += line.inline_length
+                last_token_is_opening_bracket = isinstance(
+                    line.inline_tokens[-1], OpeningBracket
+                )
+                if line_length > max_inline_length:
+                    break
+                if line.indent_level <= indent_level:
+                    # flush pending and handle next block if present
+                    yield from pending
+                    skip_lines += pending_lines
+                    if line.can_start_inline_block:
+                        pending_lines = 0
+                        pending = []
+                    else:
+                        break
+
+
+def reformat(query):
+    """Reformat query and return as a string."""
+    tokens = inline_block_format(tokenize(query))
+    return "".join(token.value for token in tokens)
---
+++ bigquery_etl/format_sql/tokenizer.py
@@ -0,0 +1,404 @@
+"""Tokenize SQL so that it can be formatted."""
+
+from dataclasses import dataclass, field
+import re
+
+# These words get their own line followed by increased indent
+TOP_LEVEL_KEYWORDS = [
+    # DDL
+    "ALTER TABLE IF EXISTS",
+    "ALTER TABLE",
+    "CREATE OR REPLACE TABLE",
+    "CREATE TABLE IF NOT EXISTS",
+    "CREATE TEMP TABLE",
+    "CREATE TABLE",
+    "DROP TABLE",
+    "DROP VIEW",
+    "CLUSTER BY",
+    "OPTIONS",
+    # DML
+    "DELETE FROM",
+    "DELETE",
+    "INSERT INTO",
+    "INSERT",
+    "MERGE INTO",
+    "MERGE",
+    "UPDATE",
+    # SQL
+    "CROSS JOIN",
+    "EXCEPT DISTINCT",
+    "INTERSECT DISTINCT",
+    "FROM",
+    "FULL JOIN",
+    "FULL OUTER JOIN",
+    "GROUP BY",
+    "HAVING",
+    "INNER JOIN",
+    "INTERSECT",
+    "JOIN",
+    "LEFT JOIN",
+    "LEFT OUTER JOIN",
+    "LIMIT",
+    "ORDER BY",
+    "OUTER JOIN",
+    "PARTITION BY",
+    "RIGHT JOIN",
+    "RIGHT OUTER JOIN",
+    "ROLLUP",
+    "ROWS BETWEEN",
+    "ROWS",
+    "SELECT AS STRUCT",
+    "SELECT AS VALUE",
+    "SELECT",
+    "SET",
+    "UNION ALL",
+    "UNION DISTINCT",
+    "UNION",
+    "USING",
+    "VALUES",
+    "WHERE",
+    "WINDOW",
+    "WITH",
+]
+# These words start a new line at the current indent
+NEWLINE_KEYWORDS = [
+    # UDF
+    "CREATE",
+    "RETURNS",
+    "LANGUAGE",
+    # Conditional
+    "AND",
+    "BETWEEN",
+    "ELSE",
+    "OR",
+    "WHEN",
+    "XOR",
+]
+# These words, and all get capitalized
+RESERVED_KEYWORDS = [
+    "ALL",
+    "AND",
+    "ANY",
+    "ARRAY",
+    "AS",
+    "ASC",
+    "ASSERT_ROWS_MODIFIED",
+    "AT",
+    "BETWEEN",
+    "BY",
+    "CASE",
+    "CAST",
+    "COLLATE",
+    "CONTAINS",
+    "CREATE",
+    "CROSS",
+    "CUBE",
+    "CURRENT",
+    "DEFAULT",
+    "DEFINE",
+    "DESC",
+    "DISTINCT",
+    "ELSE",
+    "END",
+    "ENUM",
+    "ESCAPE",
+    "EXCEPT",
+    "EXCLUDE",
+    "EXISTS",
+    "EXTRACT",
+    "FALSE",
+    "FETCH",
+    "FOLLOWING",
+    "FOR",
+    "FROM",
+    "FULL",
+    "GROUP",
+    "GROUPING",
+    "GROUPS",
+    "HASH",
+    "HAVING",
+    "IF",
+    "IGNORE",
+    "IN",
+    "INNER",
+    "INTERSECT",
+    "INTERVAL",
+    "INTO",
+    "IS",
+    "JOIN",
+    "LATERAL",
+    "LEFT",
+    "LIKE",
+    "LIMIT",
+    "LOOKUP",
+    "MERGE",
+    "NATURAL",
+    "NEW",
+    "NO",
+    "NOT",
+    "NULL",
+    "NULLS",
+    "OF",
+    "ON",
+    "OR",
+    "ORDER",
+    "OUTER",
+    "OVER",
+    "PARTITION",
+    "PRECEDING",
+    "PROTO",
+    "RANGE",
+    "RECURSIVE",
+    "REPLACE",
+    "RESPECT",
+    "RIGHT",
+    "ROLLUP",
+    "ROWS",
+    "SELECT",
+    "SET",
+    "SOME",
+    "STRUCT",
+    "TABLESAMPLE",
+    "THEN",
+    "TO",
+    "TREAT",
+    "TRUE",
+    "UNBOUNDED",
+    "UNION",
+    "UNNEST",
+    "USING",
+    "WHEN",
+    "WHERE",
+    "WINDOW",
+    "WITH",
+    "WITHIN",
+]
+QUOTE = "\"\"\"|'''|\"|'"
+# strings contain any character, with backslash always followed by one more character
+STRING_CONTENT = r"\\.|[^\\]"
+
+
+def _keyword_pattern(words):
+    """
+    Compile a regex that will match the given list of keywords.
+
+    Match one or more whitespace characters between words patterns, and only
+    match whole words.
+    """
+    return re.compile(
+        "(?:"
+        + "|".join(pattern.replace(" ", r"\s+") for pattern in words)
+        + ")(?![A-Za-z_0-9])",
+        re.IGNORECASE,
+    )
+
+
+@dataclass
+class Token:
+    """Abstract token class."""
+
+    value: str
+    pattern: re.Pattern = field(init=False, repr=False)
+
+
+class Comment(Token):
+    """Comment abstract class.
+
+    Comments must match preceding whitespace, but not match multiple preceding
+    lines or trailing whitespace.
+    """
+
+
+class LineComment(Comment):
+    """Comment that spans to the end of the current line, sans trailing whitespace."""
+
+    pattern = re.compile(r"\n?[\t ]*(?:#|--)(?:[^\s]|[^\n][^\s])*")
+
+
+class BlockComment(Comment):
+    """Comment that may span multiple lines."""
+
+    pattern = re.compile(r"(:?\n[\t ]*)?/\*.*?\*/", re.DOTALL)
+
+
+class Whitespace(Token):
+    """One or more whitespace characters on a single line."""
+
+    pattern = re.compile(r"\s((?!\n)\s)*")
+
+
+class ReservedKeyword(Token):
+    """Token that gets capitalized and separates words with a single space."""
+
+    pattern = _keyword_pattern(RESERVED_KEYWORDS)
+
+
+class NewlineKeyword(ReservedKeyword):
+    """Keyword that should start a new line."""
+
+    pattern = _keyword_pattern(NEWLINE_KEYWORDS)
+
+
+class TopLevelKeyword(NewlineKeyword):
+    """Keyword that should get its own line followed by increased indent."""
+
+    pattern = _keyword_pattern(TOP_LEVEL_KEYWORDS)
+
+
+class AngleBracketKeyword(ReservedKeyword):
+    """Keyword indicating that if the next token is '<' it is a bracket."""
+
+    pattern = _keyword_pattern(["ARRAY", "STRUCT"])
+
+
+class SpaceBeforeBracketKeyword(ReservedKeyword):
+    """Keyword that should be separated by a space from a following opening bracket."""
+
+    pattern = _keyword_pattern(["AS", "IN", "EXCEPT", "REPLACE"])
+
+
+class Identifier(Token):
+    """Token that identifies a column, parameter, table, or other database object."""
+
+    pattern = re.compile(fr"@?[A-Za-z_][A-Za-z_0-9]*|`(?:{STRING_CONTENT})+?`")
+
+
+class Literal(Token):
+    """A constant value of a built-in data type."""
+
+    pattern = re.compile(
+        # String literal
+        fr"(?:r?b|b?r)?({QUOTE})(?:{STRING_CONTENT})*?\1"
+        # Hexadecimal integer literal
+        "|[+-]?0[xX][0-9a-fA-F]+"
+        # Decimal integer or float literal
+        r"|[+-]?\d+\.?\d*(?:[Ee][+-]?)?\d*"
+    )
+
+
+class OpeningBracket(Token):
+    """Opening bracket or parenthesis.
+
+    May be followed by no whitespace or a new line and increased indent.
+    """
+
+    pattern = re.compile(r"[([]")
+
+
+class ClosingBracket(Token):
+    """Closing bracket or parenthesis.
+
+    May be preceded by a new line and decreased indent, or no whitespace.
+    """
+
+    pattern = re.compile(r"[\])]")
+
+
+class MaybeOpeningAngleBracket(Token):
+    """Token that needs context to determine whether it is an operator or bracket."""
+
+    pattern = re.compile(r"<")
+
+
+class MaybeClosingAngleBracket(Token):
+    """Token that needs context to determine whether it is an operator or bracket."""
+
+    pattern = re.compile(r">")
+
+
+class ExpressionSeparator(Token):
+    """Token that separates expressions.
+
+    Should not be preceded by whitespace and may be followed by a space or a
+    new line.
+    """
+
+    pattern = re.compile(r",")
+
+
+class StatementSeparator(Token):
+    """Token that separates statements.
+
+    Should not be preceded by whitespace and should be followed by a new line
+    and reset indent.
+    """
+
+    pattern = re.compile(r";")
+
+
+class Operator(Token):
+    """Operator."""
+
+    pattern = re.compile(r"<<|>>|>=|<=|<>|!=|.")
+
+
+class NoSpaceOperator(Operator):
+    """Operator that should have no surrounding whitespace."""
+
+    pattern = re.compile(r"\.")
+
+
+BIGQUERY_TOKEN_PRIORITY = [
+    LineComment,
+    BlockComment,
+    Whitespace,
+    TopLevelKeyword,
+    NewlineKeyword,
+    AngleBracketKeyword,
+    SpaceBeforeBracketKeyword,
+    ReservedKeyword,
+    Identifier,
+    Literal,
+    OpeningBracket,
+    ClosingBracket,
+    MaybeOpeningAngleBracket,
+    MaybeClosingAngleBracket,
+    NoSpaceOperator,
+    ExpressionSeparator,
+    StatementSeparator,
+    Operator,
+]
+
+
+def tokenize(query, token_priority=BIGQUERY_TOKEN_PRIORITY):
+    """Split query into a series of tokens."""
+    open_angle_brackets = 0
+    sign_is_operator = False
+    angle_bracket_is_operator = True
+    while query:
+        for token_type in token_priority:
+            match = token_type.pattern.match(query)
+            if match:
+                token = token_type(match.group())
+                # handle stateful matches
+                if isinstance(token, MaybeOpeningAngleBracket):
+                    if angle_bracket_is_operator:
+                        continue  # prevent matching operator as opening bracket
+                    token = OpeningBracket(token.value)
+                    open_angle_brackets += 1
+                elif isinstance(token, MaybeClosingAngleBracket):
+                    if angle_bracket_is_operator:
+                        continue  # prevent matching operator as closing bracket
+                    token = ClosingBracket(token.value)
+                    open_angle_brackets -= 1
+                elif (
+                    isinstance(token, Literal)
+                    and token.value[:1] in {"+", "-"}
+                    and sign_is_operator
+                ):
+                    continue  # prevent number literal from consuming preceding operator
+                yield token
+                length = len(token.value)
+                query = query[length:]
+                # update stateful conditions for next token
+                if not isinstance(token, (Comment, Whitespace)):
+                    sign_is_operator = isinstance(
+                        token, (Literal, Identifier, ClosingBracket)
+                    )
+                    angle_bracket_is_operator = not (
+                        open_angle_brackets > 0
+                        or isinstance(token, AngleBracketKeyword)
+                    )
+                break
+        else:
+            raise ValueError(f"Could not determine next token in {query!r}")

372cb437f2f9284a3e191a340be5c961c57dee02
Jeff Klukas <jeff@klukas.net>
2019-12-27 10:49:37 -0500

    Revert config option name changes

--- bigquery_etl/pytest_plugin/script_lint/pydocstyle.py
+++ bigquery_etl/pytest_plugin/script_lint/pydocstyle.py
@@ -16,9 +16,9 @@ def pytest_collect_file(parent, path):
         config.getoption("pydocstyle")
         and is_python_executable(path)
         # https://github.com/PyCQA/pydocstyle/blob/2.1.1/src/pydocstyle/config.py#L163
-        and re.match(config.getini("pydocstyle_match"), path.basename)
+        and re.match(config.getini("docstyle_match"), path.basename)
     ):
         if not any(
-            path.fnmatch(pattern) for pattern in config.getini("pydocstyle_exclude")
+            path.fnmatch(pattern) for pattern in config.getini("docstyle_exclude")
         ):
             return Item(path, parent)

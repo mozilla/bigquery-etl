34850b276296994d966de9b59c200177948ab342
Sunah Suh <github@sunahsuh.com>
2020-01-09 11:15:40 -0500

    Update templates/telemetry/attitudes_daily/view.sql
    Co-Authored-By: Jeff Klukas <jeff@klukas.net>

--- templates/telemetry/attitudes_daily/view.sql
+++ templates/telemetry/attitudes_daily/view.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-derived-datasets.telemetry.attitudes_daily`
+  `moz-fx-data-shared-prod.telemetry.attitudes_daily`
 AS SELECT * FROM
   `moz-fx-data-shared-prod.telemetry_derived.attitudes_daily_v1`

e243906537084bc676fd8447c5bbc36676c855d3
Jeff Klukas <jeff@klukas.net>
2020-01-10 14:51:25 -0500

    Ensure lowercase client_id for mobile pings
    We see uppercase client_ids from some clients in core, focus_event,
    and mobile_event pings. We are fixing this in the pipeline (see
    https://github.com/mozilla/gcp-ingestion/pull/1069) and this PR ensures
    lowercase for older data in user-facing views.

--- templates/telemetry/core/view.sql
+++ templates/telemetry/core/view.sql
@@ -22,6 +22,10 @@ WITH unioned AS (
   --
 SELECT
   * REPLACE(
+    -- The pipeline ensures lowercase client_id since 2020-01-10, but we apply
+    -- LOWER here to provide continuity for older data that still contains
+    -- some uppercase IDs; see https://github.com/mozilla/gcp-ingestion/pull/1069
+    LOWER(client_id) AS client_id,
     `moz-fx-data-shared-prod.udf.normalize_metadata`(metadata) AS metadata)
 FROM
   unioned

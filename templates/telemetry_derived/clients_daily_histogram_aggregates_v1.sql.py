a06b94316a958ca5e654eabdaef4c97a8b1a8bcf
Marina Samuel <msamuel@mozilla.com>
2020-01-16 16:50:40 -0500

    Use SAFE_CAST in Glam.

--- templates/telemetry_derived/clients_daily_histogram_aggregates_v1.sql.py
+++ templates/telemetry_derived/clients_daily_histogram_aggregates_v1.sql.py
@@ -58,15 +58,15 @@ def generate_sql(opts, additional_queries, windowed_clause, select_clause):
           WITH buckets AS (
             SELECT
               string_to_arr(JSON_EXTRACT(histogram, "$.range")) AS bucket_range,
-              CAST(JSON_EXTRACT(histogram, "$.bucket_count") AS INT64) AS num_buckets
+              SAFE_CAST(JSON_EXTRACT(histogram, "$.bucket_count") AS INT64) AS num_buckets
             FROM UNNEST(histograms) AS histogram
             WHERE histogram IS NOT NULL
             LIMIT 1
           )
 
           SELECT AS STRUCT
-            CAST(bucket_range[OFFSET(0)] AS INT64) AS first_bucket,
-            CAST(bucket_range[OFFSET(1)] AS INT64) AS last_bucket,
+            SAFE_CAST(bucket_range[OFFSET(0)] AS INT64) AS first_bucket,
+            SAFE_CAST(bucket_range[OFFSET(1)] AS INT64) AS last_bucket,
             num_buckets
           FROM
             buckets));
@@ -74,7 +74,7 @@ def generate_sql(opts, additional_queries, windowed_clause, select_clause):
 
         CREATE TEMP FUNCTION udf_get_histogram_type(histograms ARRAY<STRING>) AS ((
             SELECT
-              CASE CAST(JSON_EXTRACT(histogram, "$.histogram_type") AS INT64)
+              CASE SAFE_CAST(JSON_EXTRACT(histogram, "$.histogram_type") AS INT64)
                 WHEN 0 THEN 'histogram-exponential'
                 WHEN 1 THEN 'histogram-linear'
                 WHEN 2 THEN 'histogram-boolean'
@@ -91,7 +91,7 @@ def generate_sql(opts, additional_queries, windowed_clause, select_clause):
           udf_aggregate_json_sum(histograms ARRAY<STRING>) AS (ARRAY(
               SELECT
                 AS STRUCT SPLIT(keyval, ':')[OFFSET(0)] AS key,
-                SUM(CAST(SPLIT(keyval, ':')[OFFSET(1)] AS INT64)) AS value
+                SUM(SAFE_CAST(SPLIT(keyval, ':')[OFFSET(1)] AS INT64)) AS value
               FROM
                 UNNEST(histograms) AS histogram,
                 UNNEST(get_keyval_pairs(JSON_EXTRACT(histogram, "$.values"))) AS keyval

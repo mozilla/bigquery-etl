6bf694c68cdecec749114fd39a16dca4ae0b26e9
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-02-06 15:27:16 -0800

    Add initial moustache files for scalar_aggregates

---
+++ bigquery_etl/glam/templates/clients_scalar_aggregates_v1.moustache.sql
@@ -0,0 +1,151 @@
+WITH filtered_date_channel AS (
+  SELECT
+    *
+  FROM
+    clients_daily_scalar_aggregates_v1
+  WHERE
+    submission_date = @submission_date
+),
+filtered_aggregates AS (
+  SELECT
+    submission_date,
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    value
+  FROM
+    filtered_date_channel
+  CROSS JOIN
+    UNNEST(scalar_aggregates)
+  WHERE
+    value IS NOT NULL
+),
+version_filtered_new AS (
+  SELECT
+    submission_date,
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    scalar_aggs.channel AS channel,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    value
+  FROM
+    filtered_aggregates AS scalar_aggs
+  LEFT JOIN
+    latest_versions
+  ON
+    latest_versions.channel = scalar_aggs.channel
+  WHERE
+    CAST(app_version AS INT64) >= (latest_version - 2)
+),
+scalar_aggregates_new AS (
+  SELECT
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    --format:off
+    CASE agg_type
+      WHEN 'max' THEN max(value)
+      WHEN 'min' THEN min(value)
+      WHEN 'count' THEN sum(value)
+      WHEN 'sum' THEN sum(value)
+      WHEN 'false' THEN sum(value)
+      WHEN 'true' THEN sum(value)
+    END AS value
+    --format:on
+  FROM
+    version_filtered_new
+  GROUP BY
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type
+),
+filtered_new AS (
+  SELECT
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    ARRAY_AGG((metric, metric_type, key, process, agg_type, value)) AS scalar_aggregates
+  FROM
+    scalar_aggregates_new
+  GROUP BY
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel
+),
+filtered_old AS (
+  SELECT
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    scalar_aggs.channel AS channel,
+    scalar_aggregates
+  FROM
+    clients_scalar_aggregates_v1 AS scalar_aggs
+  LEFT JOIN
+    latest_versions
+  ON
+    latest_versions.channel = scalar_aggs.channel
+  WHERE
+    app_version >= (latest_version - 2)
+),
+joined_new_old AS (
+  SELECT
+    COALESCE(old_data.client_id, new_data.client_id) AS client_id,
+    COALESCE(old_data.os, new_data.os) AS os,
+    COALESCE(old_data.app_version, CAST(new_data.app_version AS INT64)) AS app_version,
+    COALESCE(old_data.app_build_id, new_data.app_build_id) AS app_build_id,
+    COALESCE(old_data.channel, new_data.channel) AS channel,
+    old_data.scalar_aggregates AS old_aggs,
+    new_data.scalar_aggregates AS new_aggs
+  FROM
+    filtered_new AS new_data
+  FULL OUTER JOIN
+    filtered_old AS old_data
+  ON
+    new_data.client_id = old_data.client_id
+    AND new_data.os = old_data.os
+    AND CAST(new_data.app_version AS INT64) = old_data.app_version
+    AND new_data.app_build_id = old_data.app_build_id
+    AND new_data.channel = old_data.channel
+)
+SELECT
+  client_id,
+  os,
+  app_version,
+  app_build_id,
+  channel,
+  udf_merged_user_data(old_aggs, new_aggs) AS scalar_aggregates
+FROM
+  joined_new_old

3658456f4d7c0b9fb09f49b583d840f894895320
Rob Hudson <robhudson@mozilla.com>
2020-02-12 10:01:21 -0800

    Add glam views
    Each view is per channel, intended to be extracted to CSV for import
    into Glam's Postgresql db.

---
+++ sql/telemetry/glam_client_probe_counts_nightly_v1/views.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.client_probe_counts_nightly`
+AS
+WITH deduped AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(
+        PARTITION BY
+            channel, app_version, agg_type, os, app_build_id, process, metric, key, client_agg_type, metric_type
+        ORDER BY
+            total_users DESC
+    ) AS rank
+  FROM
+    `moz-fx-data-shared-prod.telemetry.client_probe_counts`
+  WHERE
+    channel="nightly" )
+
+SELECT
+  * EXCEPT(rank)
+FROM
+  deduped
+WHERE
+  rank = 1

02f8bb318b8f9152a1978c121d5af5a0068f0864
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-02-18 14:54:45 -0800

    Add latest_versions template

---
+++ bigquery_etl/glam/templates/latest_versions_v1.sql
@@ -0,0 +1,36 @@
+WITH extracted AS (
+  SELECT
+    client_info.client_id,
+    client_info.app_channel AS channel,
+    COALESCE(
+      SAFE_CAST(SPLIT(client_info.app_display_version, '.')[OFFSET(0)] AS INT64),
+      0
+    ) AS app_version,
+  FROM
+    `moz-fx-data-shared-prod.{{ source_table }}`
+  WHERE
+    DATE(submission_timestamp) > DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+    AND client_info.app_channel IS NOT NULL
+),
+transformed AS (
+  SELECT
+    channel,
+    app_version
+  FROM
+    extracted
+  GROUP BY
+    channel,
+    app_version
+  HAVING
+    COUNT(DISTINCT client_id) > 100
+  ORDER BY
+    channel,
+    app_version DESC
+)
+SELECT
+  channel,
+  MAX(app_version) AS latest_version
+FROM
+  transformed
+GROUP BY
+  channel

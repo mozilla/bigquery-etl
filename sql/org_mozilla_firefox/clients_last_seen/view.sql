fbec72b7fcec79a0fb79d8e6cb8af8ee3be680dc
Jeff Klukas <jeff@klukas.net>
2020-02-20 10:43:09 -0500

    Refactor cls to put usage criteria consistently inside baseline struct

--- sql/org_mozilla_firefox/clients_last_seen/view.sql
+++ sql/org_mozilla_firefox/clients_last_seen/view.sql
@@ -2,6 +2,13 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.org_mozilla_firefox.clients_last_seen`
 AS
 SELECT
+  `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(baseline.days_seen_bits) AS days_since_seen,
+  `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(
+    baseline.days_seen_session_start_bits
+  ) AS days_since_seen_session_start,
+  `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(
+    baseline.days_seen_session_end_bits
+  ) AS days_since_seen_session_end,
   * EXCEPT (baseline, metrics),
   baseline.*,
   metrics.*

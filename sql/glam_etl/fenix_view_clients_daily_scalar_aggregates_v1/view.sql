4f2d5dd51d84c9560d2e4ee7f9896247a3914e8b
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-02-21 11:13:20 -0800

    Add script for intitial scheduling (#757)
    * Update run script to be more generic
    
    * Update run script with parameters and avoid reusing destination
    
    * Add view for clients_daily_scalar_aggregates
    
    * Add new view to dryrun
    
    * Remove old comment

---
+++ sql/glam_etl/fenix_view_clients_daily_scalar_aggregates_v1/view.sql
@@ -0,0 +1,11 @@
+-- Manually written view that handles time-partitioning
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.glam_etl.fenix_view_clients_daily_scalar_aggregates_v1`
+AS
+SELECT
+  * EXCEPT (submission_date),
+  DATE(_PARTITIONTIME) AS submission_date
+FROM
+  `moz-fx-data-shared-prod.glam_etl.fenix_clients_daily*`
+WHERE
+  _TABLE_SUFFIX LIKE '%scalars_aggregates%'

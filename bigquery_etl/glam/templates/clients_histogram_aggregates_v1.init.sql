6bcca926301e80ad6a20c110d9ca93109f6b8284
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-02-26 14:57:14 -0800

    Format template scripts

--- bigquery_etl/glam/templates/clients_histogram_aggregates_v1.init.sql
+++ bigquery_etl/glam/templates/clients_histogram_aggregates_v1.init.sql
@@ -1,23 +1,29 @@
 CREATE TABLE IF NOT EXISTS
-  `moz-fx-data-shared-prod.telemetry_derived.clients_histogram_aggregates_v1` (
+  `moz-fx-data-shared-prod.telemetry_derived.clients_histogram_aggregates_v1`(
     sample_id INT64,
     client_id STRING,
     os STRING,
     app_version INT64,
     app_build_id STRING,
     channel STRING,
-    histogram_aggregates ARRAY <STRUCT<
-      first_bucket INT64,
-      last_bucket INT64,
-      num_buckets INT64,
-      latest_version INT64,
-      metric STRING,
-      metric_type STRING,
-      key STRING,
-      process STRING,
-      agg_type STRING,
-      aggregates ARRAY<STRUCT<key STRING, value INT64>>
-    >>
-)
-PARTITION BY RANGE_BUCKET(sample_id, GENERATE_ARRAY(0, 100, 1))
-CLUSTER BY app_version, channel, client_id
+    histogram_aggregates ARRAY<
+      STRUCT<
+        first_bucket INT64,
+        last_bucket INT64,
+        num_buckets INT64,
+        latest_version INT64,
+        metric STRING,
+        metric_type STRING,
+        key STRING,
+        process STRING,
+        agg_type STRING,
+        aggregates ARRAY<STRUCT<key STRING, value INT64>>
+      >
+    >
+  )
+PARTITION BY
+  RANGE_BUCKET(sample_id, GENERATE_ARRAY(0, 100, 1))
+CLUSTER BY
+  app_version,
+  channel,
+  client_id
--- bigquery_etl/glam/templates/clients_histogram_aggregates_v1.sql
+++ bigquery_etl/glam/templates/clients_histogram_aggregates_v1.sql
@@ -1,10 +1,13 @@
 -- old
-WITH clients_histogram_aggregates_partition AS
-  (SELECT *
-  FROM clients_histogram_aggregates_v1
-  WHERE sample_id >= @min_sample_id
-    AND sample_id <= @max_sample_id)
-
+WITH clients_histogram_aggregates_partition AS (
+  SELECT
+    *
+  FROM
+    clients_histogram_aggregates_v1
+  WHERE
+    sample_id >= @min_sample_id
+    AND sample_id <= @max_sample_id
+)
 SELECT
   sample_id,
   client_id,
@@ -14,17 +17,23 @@ SELECT
   hist_aggs.channel AS channel,
   CONCAT(client_id, os, app_version, app_build_id, hist_aggs.channel) AS join_key,
   histogram_aggregates
-FROM clients_histogram_aggregates_partition AS hist_aggs
-LEFT JOIN latest_versions
-ON latest_versions.channel = hist_aggs.channel
-WHERE app_version >= (latest_version - 2)
-
+FROM
+  clients_histogram_aggregates_partition AS hist_aggs
+LEFT JOIN
+  latest_versions
+ON
+  latest_versions.channel = hist_aggs.channel
+WHERE
+  app_version >= (latest_version - 2)
 -- new
 WITH filtered_date_channel AS (
-  SELECT *
-  FROM clients_daily_histogram_aggregates_v1
-  WHERE submission_date = @submission_date),
-
+  SELECT
+    *
+  FROM
+    clients_daily_histogram_aggregates_v1
+  WHERE
+    submission_date = @submission_date
+),
 filtered_aggregates AS (
   SELECT
     submission_date,
@@ -40,72 +49,77 @@ filtered_aggregates AS (
     process,
     agg_type,
     value
-  FROM filtered_date_channel
+  FROM
+    filtered_date_channel
   CROSS JOIN
     UNNEST(histogram_aggregates)
-  WHERE value IS NOT NULL
+  WHERE
+    value IS NOT NULL
     AND ARRAY_LENGTH(value) > 0
     AND bucket_range.num_buckets > 0
 ),
-
-version_filtered_new AS
-  (SELECT
-      submission_date,
-      client_id,
-      os,
-      app_version,
-      app_build_id,
-      hist_aggs.channel AS channel,
-      bucket_range.first_bucket AS first_bucket,
-      bucket_range.last_bucket AS last_bucket,
-      bucket_range.num_buckets AS num_buckets,
-      metric,
-      metric_type,
-      key,
-      process,
-      agg_type,
-      latest_version,
-      hist_aggs.value AS value
-  FROM filtered_aggregates AS hist_aggs
-  LEFT JOIN latest_versions
-  ON latest_versions.channel = hist_aggs.channel
-  WHERE CAST(app_version AS INT64) >= (latest_version - 2)),
-
-aggregated_histograms AS
-  (SELECT
-      client_id,
-      os,
-      app_version,
-      app_build_id,
-      channel,
-      first_bucket,
-      last_bucket,
-      num_buckets,
-      latest_version,
-      metric,
-      metric_type,
-      key,
-      process,
-      agg_type,
-      udf.map_sum(ARRAY_CONCAT_AGG(value)) AS aggregates
+version_filtered_new AS (
+  SELECT
+    submission_date,
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    hist_aggs.channel AS channel,
+    bucket_range.first_bucket AS first_bucket,
+    bucket_range.last_bucket AS last_bucket,
+    bucket_range.num_buckets AS num_buckets,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    latest_version,
+    hist_aggs.value AS value
   FROM
-      version_filtered_new
+    filtered_aggregates AS hist_aggs
+  LEFT JOIN
+    latest_versions
+  ON
+    latest_versions.channel = hist_aggs.channel
+  WHERE
+    CAST(app_version AS INT64) >= (latest_version - 2)
+),
+aggregated_histograms AS (
+  SELECT
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    latest_version,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    udf.map_sum(ARRAY_CONCAT_AGG(value)) AS aggregates
+  FROM
+    version_filtered_new
   GROUP BY
-      client_id,
-      os,
-      app_version,
-      app_build_id,
-      channel,
-      first_bucket,
-      last_bucket,
-      num_buckets,
-      metric,
-      metric_type,
-      key,
-      process,
-      agg_type,
-      latest_version)
-
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    latest_version
+)
 SELECT
   udf_js.sample_id(client_id) AS sample_id,
   client_id,
@@ -114,17 +128,19 @@ SELECT
   app_build_id,
   channel,
   CONCAT(client_id, os, app_version, app_build_id, channel) AS join_key,
-  ARRAY_AGG(STRUCT<
-    first_bucket INT64,
-    last_bucket INT64,
-    num_buckets INT64,
-    latest_version INT64,
-    metric STRING,
-    metric_type STRING,
-    key STRING,
-    process STRING,
-    agg_type STRING,
-    aggregates ARRAY<STRUCT<key STRING, value INT64>>>(
+  ARRAY_AGG(
+    STRUCT<
+      first_bucket INT64,
+      last_bucket INT64,
+      num_buckets INT64,
+      latest_version INT64,
+      metric STRING,
+      metric_type STRING,
+      key STRING,
+      process STRING,
+      agg_type STRING,
+      aggregates ARRAY<STRUCT<key STRING, value INT64>>
+    >(
       first_bucket,
       last_bucket,
       num_buckets,
@@ -135,28 +151,35 @@ SELECT
       process,
       agg_type,
       aggregates
-)) AS histogram_aggregates
-FROM aggregated_histograms
+    )
+  ) AS histogram_aggregates
+FROM
+  aggregated_histograms
 GROUP BY
   client_id,
   os,
   app_version,
   app_build_id,
   channel
-
 -- merged
-WITH old_data AS
-  (SELECT *
-  FROM clients_histogram_aggregates_old_v1
-  WHERE sample_id >= @min_sample_id
-    AND sample_id <= @max_sample_id),
-
-new_data AS
-  (SELECT *
-  FROM clients_histogram_aggregates_new_v1
-  WHERE sample_id >= @min_sample_id
-    AND sample_id <= @max_sample_id)
-
+WITH old_data AS (
+  SELECT
+    *
+  FROM
+    clients_histogram_aggregates_old_v1
+  WHERE
+    sample_id >= @min_sample_id
+    AND sample_id <= @max_sample_id
+),
+new_data AS (
+  SELECT
+    *
+  FROM
+    clients_histogram_aggregates_new_v1
+  WHERE
+    sample_id >= @min_sample_id
+    AND sample_id <= @max_sample_id
+)
 SELECT
   COALESCE(old_data.sample_id, new_data.sample_id) AS sample_id,
   COALESCE(old_data.client_id, new_data.client_id) AS client_id,
@@ -166,78 +189,94 @@ SELECT
   COALESCE(old_data.channel, new_data.channel) AS channel,
   old_data.histogram_aggregates AS old_aggs,
   new_data.histogram_aggregates AS new_aggs
-FROM old_data
-FULL OUTER JOIN new_data
-  ON new_data.join_key = old_data.join_key
-
+FROM
+  old_data
+FULL OUTER JOIN
+  new_data
+ON
+  new_data.join_key = old_data.join_key
 -- result
-CREATE TEMP FUNCTION udf_merged_user_data(old_aggs ANY TYPE, new_aggs ANY TYPE)
-  RETURNS ARRAY<STRUCT<
-    first_bucket INT64,
-    last_bucket INT64,
-    num_buckets INT64,
-    latest_version INT64,
-    metric STRING,
-    metric_type STRING,
-    key STRING,
-    process STRING,
-    agg_type STRING,
-    aggregates ARRAY<STRUCT<key STRING, value INT64>>>> AS (
-  (
-    WITH unnested AS
-      (SELECT *
-      FROM UNNEST(old_aggs)
+  CREATE TEMP FUNCTION udf_merged_user_data(old_aggs ANY TYPE, new_aggs ANY TYPE)
+  RETURNS ARRAY<
+    STRUCT<
+      first_bucket INT64,
+      last_bucket INT64,
+      num_buckets INT64,
+      latest_version INT64,
+      metric STRING,
+      metric_type STRING,
+      key STRING,
+      process STRING,
+      agg_type STRING,
+      aggregates ARRAY<STRUCT<key STRING, value INT64>>
+    >
+  > AS (
+    (
+      WITH unnested AS (
+        SELECT
+          *
+        FROM
+          UNNEST(old_aggs)
+        UNION ALL
+        SELECT
+          *
+        FROM
+          UNNEST(new_aggs)
+      ),
+      aggregated_data AS (
+        SELECT AS STRUCT
+          first_bucket,
+          last_bucket,
+          num_buckets,
+          latest_version,
+          metric,
+          metric_type,
+          key,
+          process,
+          agg_type,
+          udf.map_sum(ARRAY_CONCAT_AGG(aggregates)) AS histogram_aggregates
+        FROM
+          unnested
+        GROUP BY
+          first_bucket,
+          last_bucket,
+          num_buckets,
+          latest_version,
+          metric,
+          metric_type,
+          key,
+          process,
+          agg_type
+      )
+      SELECT
+        ARRAY_AGG(
+          (
+            first_bucket,
+            last_bucket,
+            num_buckets,
+            latest_version,
+            metric,
+            metric_type,
+            key,
+            process,
+            agg_type,
+            histogram_aggregates
+          )
+        )
+      FROM
+        aggregated_data
+    )
+  );
 
-      UNION ALL
-
-      SELECT *
-      FROM UNNEST(new_aggs)),
-
-    aggregated_data AS
-      (SELECT AS STRUCT
-        first_bucket,
-        last_bucket,
-        num_buckets,
-        latest_version,
-        metric,
-        metric_type,
-        key,
-        process,
-        agg_type,
-        udf.map_sum(ARRAY_CONCAT_AGG(aggregates)) AS histogram_aggregates
-      FROM unnested
-      GROUP BY
-        first_bucket,
-        last_bucket,
-        num_buckets,
-        latest_version,
-        metric,
-        metric_type,
-        key,
-        process,
-        agg_type)
-
-      SELECT ARRAY_AGG((
-        first_bucket,
-        last_bucket,
-        num_buckets,
-        latest_version,
-        metric,
-        metric_type,
-        key,
-        process,
-        agg_type,
-        histogram_aggregates))
-      FROM aggregated_data
-  )
-);
-
-WITH clients_histogram_merged_partition AS
-  (SELECT *
-  FROM clients_histogram_aggregates_merged_v1
-  WHERE sample_id >= @min_sample_id
-    AND sample_id <= @max_sample_id)
-
+WITH clients_histogram_merged_partition AS (
+  SELECT
+    *
+  FROM
+    clients_histogram_aggregates_merged_v1
+  WHERE
+    sample_id >= @min_sample_id
+    AND sample_id <= @max_sample_id
+)
 SELECT
   sample_id,
   client_id,
@@ -246,4 +285,5 @@ SELECT
   app_build_id,
   channel,
   udf_merged_user_data(old_aggs, new_aggs) AS histogram_aggregates
-FROM clients_histogram_merged_partition
+FROM
+  clients_histogram_merged_partition

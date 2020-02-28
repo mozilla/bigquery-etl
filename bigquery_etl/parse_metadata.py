d7a1041770453e813afc497f2391f2ffa0982792
Anna Scholtz <anna@scholtzan.net>
2020-02-28 12:42:10 -0800

    Refactor scripts to use Metadata class

--- bigquery_etl/parse_metadata.py
+++ bigquery_etl/parse_metadata.py
@@ -7,9 +7,11 @@ import yaml
 
 
 class Metadata:
-    friendly_name = None
-    description = None
-    labels = {}
+    def __init__(self, friendly_name=None, description=None, labels={}):
+        self.friendly_name = friendly_name
+        self.description = description
+        self.labels = labels
+
 
     @staticmethod
     def is_valid_label(label):
@@ -20,20 +22,25 @@ class Metadata:
         """
         return re.match(r"[\w\d_-]+", label)
 
+
     @classmethod
-    def from_file(self, metadata_file):
+    def from_file(cls, metadata_file):
+        friendly_name = None
+        description = None
+        labels = {}
+
         with open(metadata_file, "r") as yaml_stream:
             try:
                 metadata = yaml.safe_load(yaml_stream)
 
                 if "friendly_name" in metadata:
-                    self.friendly_name = metadata["friendly_name"]
+                    friendly_name = metadata["friendly_name"]
 
                 if "description" in metadata:
-                    self.description = metadata["description"]
+                    description = metadata["description"]
 
                 if "labels" in metadata:
-                    self.labels = {}
+                    labels = {}
 
                     for key, label in metadata["labels"].items():
                         if isinstance(label, bool) and Metadata.is_valid_label(
@@ -41,12 +48,12 @@ class Metadata:
                         ):
                             # key-value pair with boolean value should get published as tag
                             if label:
-                                self.labels[str(key)] = ""
+                                labels[str(key)] = ""
                         elif Metadata.is_valid_label(
                             str(key)
                         ) and Metadata.is_valid_label(str(label)):
                             # all other pairs get published as key-value pair label
-                            self.labels[str(key)] = str(label)
+                            labels[str(key)] = str(label)
                         else:
                             print(
                                 """
@@ -58,6 +65,14 @@ class Metadata:
                                 )
                             )
 
-                return self
+                return cls(friendly_name, description, labels)
             except yaml.YAMLError as e:
                 raise e
+
+
+    def is_public_bigquery(self):
+        """
+        Return true if the public_bigquery flag is set.
+        """
+
+        return "public_bigquery" in self.labels

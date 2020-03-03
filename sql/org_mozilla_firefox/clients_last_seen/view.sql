10fa6eda617d79939ee8d97975a0a13301906e94
Jeff Klukas <jeff@klukas.net>
2020-03-03 14:03:30 -0500

    Add days_since_created_profile to Fenix clients_last_seen

--- sql/org_mozilla_firefox/clients_last_seen/view.sql
+++ sql/org_mozilla_firefox/clients_last_seen/view.sql
@@ -9,6 +9,7 @@ SELECT
   `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(
     baseline.days_seen_session_end_bits
   ) AS days_since_seen_session_end,
+  DATE_DIFF(submission_date, baseline.first_run_date, DAY) AS days_since_created_profile,
   * EXCEPT (baseline, metrics),
   baseline.*,
   metrics.*

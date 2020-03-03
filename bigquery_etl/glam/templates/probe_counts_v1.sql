cbd1e3985f2762a0992384042cfdc6631a634976
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-03-02 16:30:20 -0800

    Add initial modification to probe counts

--- bigquery_etl/glam/templates/probe_counts_v1.sql
+++ bigquery_etl/glam/templates/probe_counts_v1.sql
@@ -1,5 +1,9 @@
 {{ header }}
-{% include "probe_counts_v1.udf.scalar.sql" %}
+{% if is_scalar %}
+    {% include "probe_counts_v1.udf.scalar.sql" %}
+{% else %}
+    {% include "probe_counts_v1.udf.histogram.sql" %}
+{% endif %}
 
 {% for attribute_combo in attribute_combinations %}
     SELECT
@@ -11,26 +15,37 @@
             {% endif %}
         {% endfor %}
         {{ aggregate_attributes }},
-        client_agg_type,
-        agg_type,
-        SUM(count) AS total_users,
-        CASE
-        WHEN
-            metric_type IN ({{ scalar_metric_types }})
-        THEN
+        {% if is_scalar %}
+            client_agg_type,
+            agg_type,
+            SUM(count) AS total_users,
+            CASE
+            WHEN
+                metric_type IN ({{ scalar_metric_types }})
+            THEN
+                udf_fill_buckets(
+                    udf_dedupe_map_sum(ARRAY_AGG(STRUCT<key STRING, value FLOAT64>(bucket, count))),
+                    udf_get_buckets()
+                )
+            WHEN
+                metric_type in ({{ boolean_metric_types }})
+            THEN
+                udf_fill_buckets(
+                    udf_dedupe_map_sum(ARRAY_AGG(STRUCT<key STRING, value FLOAT64>(bucket, count))),
+                    ['always', 'never', 'sometimes']
+                )
+            END
+            AS aggregates
+        {% else %}
+            {{ metric_attributes }},
+            agg_type AS client_agg_type,
+            'histogram' as agg_type,
+            CAST(ROUND(SUM(record.value)) AS INT64) AS total_users,
             udf_fill_buckets(
-                udf_dedupe_map_sum(ARRAY_AGG(STRUCT<key STRING, value FLOAT64>(bucket, count))),
-                udf_get_buckets()
-            )
-        WHEN
-            metric_type in ({{ boolean_metric_types }})
-        THEN
-            udf_fill_buckets(
-                udf_dedupe_map_sum(ARRAY_AGG(STRUCT<key STRING, value FLOAT64>(bucket, count))),
-                ['always', 'never', 'sometimes']
-            )
-        END
-        AS aggregates
+                udf_dedupe_map_sum(ARRAY_AGG(record)),
+                udf_to_string_arr(udf_get_buckets(first_bucket, last_bucket, num_buckets, metric_type))
+            ) AS aggregates
+        {% endif %}
     FROM
         {{ source_table }}
     {% if ("os", True) in grouping_attributes %}

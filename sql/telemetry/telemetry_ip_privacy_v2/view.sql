16ad5953113d328973093c46d21de7a5659957a6
Benjamin Wu <benjaminwu124@gmail.com>
2020-03-04 09:52:50 -0500

    🪓

--- sql/telemetry/telemetry_ip_privacy_v2/view.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.telemetry_ip_privacy_v2`
-AS
-SELECT
-  DATE(submission_timestamp) AS submission_date,
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.telemetry_ip_privacy_v2`

b32fe710506c75eeb79e9b5b7214458c47ab3cf5
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-03-04 17:50:11 -0800

    Add UDF for generating functional buckets

--- bigquery_etl/glam/templates/probe_counts_v1.udf.histogram.sql
+++ bigquery_etl/glam/templates/probe_counts_v1.udf.histogram.sql
@@ -33,6 +33,29 @@ AS
   return result;
 ''';
 
+CREATE TEMP FUNCTION udf_functional_buckets(log_base INT64, buckets_per_magnitude INT64, range_max INT64)
+RETURNS ARRAY<FLOAT64>
+LANGUAGE js
+AS
+  '''
+  function sample_to_bucket_index(sample) {
+    // Get the index of the sample
+    // https://github.com/mozilla/glean/blob/master/glean-core/src/histogram/functional.rs
+    let exponent = Math.pow(log_base, 1.0/buckets_per_magnitude);
+    return Math.ceil(Math.log(sample + 1) / Math.log(exponent));
+  }
+
+  let buckets = new Set([0]);
+  for (let index = 0; index < sample_to_bucket_index(range_max); index++) {
+    // Avoid re-using the exponent due to floating point issues when carrying
+    // the `pow` operation e.g. `let exponent = ...; Math.pow(exponent, index)`.
+    let bucket = Math.floor(Math.pow(log_base, index/buckets_per_magnitude));
+    buckets.add(bucket);
+  }
+
+  return [...buckets]
+''';
+
 CREATE TEMP FUNCTION udf_to_string_arr(buckets ARRAY<INT64>)
 RETURNS ARRAY<STRING> AS (
   (SELECT ARRAY_AGG(CAST(bucket AS STRING)) FROM UNNEST(buckets) AS bucket)
@@ -51,10 +74,13 @@ RETURNS ARRAY<INT64> AS (
       SELECT
         CASE
         WHEN
-          metric_type IN ('timing_distribution', 'memory_distribution')
+          metric_type = 'timing_distribution'
         THEN
-          -- TODO: implement a udf to generate bucket ranges
-          []
+          udf_functional_buckets(2, 8, range_max)
+        WHEN
+          metric_type = 'memory_distribution'
+        THEN
+          udf_functional_buckets(2, 16, range_max)
         WHEN
           metric_type = 'custom_distribution'
           AND histogram_type = 'exponential'

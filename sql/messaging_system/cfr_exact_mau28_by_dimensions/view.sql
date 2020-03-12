2d0241d8067d5bc46b4206bfbcdeba716e7ca9bc
Nan Jiang <njiang028@gmail.com>
2020-03-12 16:58:27 -0400

    Add exact_mau28 tables/views for Messaging System

---
+++ sql/messaging_system/cfr_exact_mau28_by_dimensions/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.messaging_system.cfr_exact_mau28_by_dimensions`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.messaging_system_derived.cfr_exact_mau28_by_dimensions_v1`
---
+++ sql/messaging_system/snippets_exact_mau28_by_dimensions/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.messaging_system.snippets_exact_mau28_by_dimensions`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.messaging_system_derived.snippets_exact_mau28_by_dimensions_v1`

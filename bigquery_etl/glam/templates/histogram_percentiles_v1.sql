068ebc016730fb0fa1351802c9db82b01c67d0f4
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-03-17 15:40:39 -0700

    Update histogram_percentiles with Glean specific metrics

--- bigquery_etl/glam/templates/histogram_percentiles_v1.sql
+++ bigquery_etl/glam/templates/histogram_percentiles_v1.sql
@@ -4,13 +4,18 @@ RETURNS FLOAT64
 LANGUAGE js AS
 '''
   function lastBucketUpper(histogram, type) {
-    if (histogram.length == 1) return histogram[0].key + 1;
-    if (type === "histogram-exponential") {
+    if (histogram.length == 1) {
+        return histogram[0].key + 1;
+    }
+    if (type !== "custom_distribution_linear") {
         return Math.pow(histogram[histogram.length - 1].key, 2) / histogram[histogram.length - 2].key
     } else {
+        // TODO: verify functional bucketing
+        // Determine size of exponential and functional bucketing
         return 2 * histogram[histogram.length - 1].key - histogram[histogram.length - 2].key;
     }
   }
+
   histogram = histogram.concat([{"key": parseInt(lastBucketUpper(histogram, type)), "value": 0}]);
   let linearTerm =
       histogram[histogram.length - 1].key - histogram[histogram.length - 2].key;
@@ -34,11 +39,12 @@ LANGUAGE js AS
 
   // The ratio of the hits in the percentile to the hits in the bar containing it - how far we are inside the bar
   let ratioInBar = hitsAtPercentileInBar / histogram[percentileBucketIndex].value;
-  if (type = 'histogram-exponential') { // exponential buckets
-      return histogram[percentileBucketIndex].key * Math.pow(
-      exponentialFactor, ratioInBar); // geometric interpolation within bar
-  } else { // linear buckets
-    return histogram[percentileBucketIndex].key + linearTerm * ratioInBar; // linear interpolation within bar
+  if (type !== "custom_distribution_linear") {
+      // exponential buckets - geometric interpolation within bar
+      return histogram[percentileBucketIndex].key * Math.pow(exponentialFactor, ratioInBar);
+  } else {
+    // linear buckets - linear interpolation within bar
+    return histogram[percentileBucketIndex].key + linearTerm * ratioInBar;
   }
 ''';
 

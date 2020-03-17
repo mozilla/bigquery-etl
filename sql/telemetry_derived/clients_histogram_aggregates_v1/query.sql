4e5d645c4f553390ae3fb2a41dc48a5fcbc06079
Marina Samuel <msamuel@mozilla.com>
2020-03-17 17:13:23 -0400

    Glam histogram aggregates table is partitioned by submission_date and clustered by client_id.

--- sql/telemetry_derived/clients_histogram_aggregates_v1/query.sql
+++ sql/telemetry_derived/clients_histogram_aggregates_v1/query.sql
@@ -152,7 +152,8 @@ WITH clients_histogram_aggregates_new AS
 clients_histogram_aggregates_partition AS
   (SELECT *
   FROM clients_histogram_aggregates_v1
-  WHERE sample_id >= @min_sample_id
+  WHERE submission_date = DATE_SUB(@submission_date, INTERVAL 1 DAY)
+    AND sample_id >= @min_sample_id
     AND sample_id <= @max_sample_id),
 
 clients_histogram_aggregates_old AS
@@ -185,6 +186,7 @@ merged AS
     ON new_data.join_key = old_data.join_key)
 
 SELECT
+  DATE(@submission_date) AS submission_date,
   sample_id,
   client_id,
   os,

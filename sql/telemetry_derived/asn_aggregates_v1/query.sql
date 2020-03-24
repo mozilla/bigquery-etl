1b831fe1baf64db016a9ecd452a2983670b37f13
Anna Scholtz <anna@scholtzan.net>
2020-03-24 16:35:23 -0700

    Refactor ASN aggregation query

--- sql/telemetry_derived/asn_aggregates_v1/query.sql
+++ sql/telemetry_derived/asn_aggregates_v1/query.sql
@@ -1,9 +1,4 @@
-
-CREATE TEMPORARY FUNCTION get_client_ip(
-  xff STRING, 
-  remote_address STRING,
-  pipeline_proxy STRING
-)
+CREATE TEMPORARY FUNCTION get_client_ip(xff STRING, remote_address STRING, pipeline_proxy STRING)
 RETURNS STRING
 LANGUAGE js
 AS
@@ -31,12 +26,10 @@ AS
 
   return ip;
   """;
-CREATE TEMPORARY FUNCTION cidr_range(
-  network STRING
-)
-RETURNS STRUCT<
-  start_ip FLOAT64,
-  end_ip FLOAT64>
+
+--
+CREATE TEMPORARY FUNCTION cidr_range(network STRING)
+RETURNS STRUCT<start_ip FLOAT64, end_ip FLOAT64>
 LANGUAGE js
 AS
   """
@@ -61,54 +54,72 @@ AS
     "end_ip": parseFloat(endIp)
   };
   """;
+
 --
-WITH 
-asn_ip_address_range AS (
-  SELECT 
-    NET.IPV4_FROM_INT64(CAST(cidr_range(network).start_ip AS INT64)) as start_ip,
-    NET.IPV4_FROM_INT64(CAST(cidr_range(network).end_ip AS INT64)) as end_ip,
-    NET.IP_TRUNC(NET.IPV4_FROM_INT64(CAST(cidr_range(network).start_ip AS INT64)), CAST(SPLIT(network, "/")[OFFSET(1)] AS INT64)) as prefix,
+WITH asn_ip_address_range AS (
+  SELECT
+    NET.IPV4_FROM_INT64(CAST(cidr_range(network).start_ip AS INT64)) AS start_ip,
+    NET.IPV4_FROM_INT64(CAST(cidr_range(network).end_ip AS INT64)) AS end_ip,
+    NET.IP_TRUNC(
+      NET.IPV4_FROM_INT64(CAST(cidr_range(network).start_ip AS INT64)),
+      CAST(SPLIT(network, "/")[OFFSET(1)] AS INT64)
+    ) AS prefix,
     autonomous_system_number
-  FROM 
+  FROM
     `static.geoip2_isp_blocks_ipv4`
-  ORDER BY start_ip ASC
+  ORDER BY
+    start_ip ASC
 ),
 main_summary_with_ip AS (
-  SELECT 
+  SELECT
     submission_date,
     client_id,
-    user_pref_network_trr_mode as trr,
-    NET.SAFE_IP_FROM_STRING(ip_address) as ip_address
-  FROM `moz-fx-data-shared-prod.telemetry.main_summary` as main_summary
+    user_pref_network_trr_mode AS trr,
+    NET.SAFE_IP_FROM_STRING(ip_address) AS ip_address
+  FROM
+    `moz-fx-data-shared-prod.telemetry.main_summary` AS main_summary
   LEFT JOIN
-    (SELECT 
-      submission_timestamp,
-      get_client_ip(x_forwarded_for, remote_addr, x_pipeline_proxy) as ip_address,
-      udf.parse_desktop_telemetry_uri(uri).document_id AS document_id
-    FROM `moz-fx-data-shared-prod.payload_bytes_raw.telemetry`) AS payload_bytes_raw
-  ON payload_bytes_raw.document_id = main_summary.document_id
-  WHERE submission_date = '2020-03-20' AND
-  DATE(payload_bytes_raw.submission_timestamp) = '2020-03-20'
+    (
+      SELECT
+        submission_timestamp,
+        get_client_ip(x_forwarded_for, remote_addr, x_pipeline_proxy) AS ip_address,
+        udf.parse_desktop_telemetry_uri(uri).document_id AS document_id
+      FROM
+        `moz-fx-data-shared-prod.payload_bytes_raw.telemetry`
+    ) AS payload_bytes_raw
+  ON
+    payload_bytes_raw.document_id = main_summary.document_id
+  WHERE
+    submission_date = @submission_date
+    AND DATE(payload_bytes_raw.submission_timestamp) = @submission_date
 ),
 main_summary_with_asn AS (
-  SELECT 
+  SELECT
     submission_date,
     client_id,
     trr,
     autonomous_system_number,
-  FROM main_summary_with_ip
-  JOIN asn_ip_address_range AS a
-  ON a.prefix = NET.IP_TRUNC(ip_address, 16)
-    WHERE ip_address BETWEEN a.start_ip AND a.end_ip
+  FROM
+    main_summary_with_ip
+  JOIN
+    asn_ip_address_range AS a
+  ON
+    a.prefix = NET.IP_TRUNC(ip_address, 16)
+  WHERE
+    ip_address
+    BETWEEN a.start_ip
+    AND a.end_ip
 )
-SELECT 
+SELECT
   submission_date,
   autonomous_system_number,
-  COUNT(distinct client_id) AS n_clients,
-  COUNTIF(trr > 0) as doh_enabled,
-  COUNTIF(trr = 0) as doh_disabled
-FROM main_summary_with_asn
-GROUP BY 
-  submission_date, 
+  COUNT(DISTINCT client_id) AS n_clients,
+  COUNTIF(trr > 0) AS doh_enabled,
+  COUNTIF(trr = 0) AS doh_disabled
+FROM
+  main_summary_with_asn
+GROUP BY
+  submission_date,
   autonomous_system_number
-HAVING n_clients  > 50
+HAVING
+  n_clients > @n_clients

e6d3681194d2ceb23e676c35bb2b70bb4579abf7
Anna Scholtz <anna@scholtzan.net>
2020-03-24 09:06:12 -0700

    Add UDF for checking if ip address is in subnet

---
+++ udf_js/ip_address_in_subnet.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION udf_js.ip_address_in_network(
+  ip_address STRING,
+  network STRING
+)
+RETURNS BOOL
+LANGUAGE js
+AS
+  """
+  function ipToInt(ipAddress) 
+  {
+    var d = ipAddress.split('.');
+    return ((((((+d[0])*256)+(+d[1]))*256)+(+d[2]))*256)+(+d[3]);
+  }
+
+  function ipMask(maskSize) {
+    return -1 << (32 - maskSize);
+  }
+
+  var subnetIp = network.split("/")[0];
+  var subnetMask = network.split("/")[1];
+  return (ipToInt(ip_address) & ipMask(subnetMask)) == (ipToInt(subnetIp) & ipMask(subnetMask));
+  """;
+
+-- Test
+
+SELECT
+  assert_false(udf_js.ip_address_in_network("0.0.0.0", "1.1.1.1/8")),
+  assert_true(udf_js.ip_address_in_network("1.1.1.1", "1.1.1.1/8")),
+  assert_false(udf_js.ip_address_in_network("10.128.239.50", "10.128.240.50/30")),
+  assert_true(udf_js.ip_address_in_network("10.128.240.50", "10.128.240.50/30")),
+  assert_true(udf_js.ip_address_in_network("192.168.5.1", "192.168.5.85/24")),
+  assert_false(udf_js.ip_address_in_network("192.168.4.254", "192.168.5.85/24")),
+  assert_true(udf_js.ip_address_in_network("192.168.5.254", "192.168.5.85/24"))

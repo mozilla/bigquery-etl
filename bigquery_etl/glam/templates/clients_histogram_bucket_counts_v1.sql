33160eea96d8af71908bdad3bdbeb9c0b6442590
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-03-25 10:17:21 -0700

    Add histogram percentiles for Fenix data into GLAM (#829)
    * Add initial histogram_percentiles
    
    * Update metric_type with histogram_type suffix
    
    * Add generated SQL (backwards incompatible)
    
    * Add body for copy of histogram_percentiles_v1`
    
    * Update histogram_percentiles with Glean specific metrics
    
    * Add histogram_percentiles module
    
    * Uncomment histogram percentiles
    
    * Add generated SQL
    
    * Add template to ignore section of format_sql
    
    * Add histogram percentiles to ignore of dryrun
    
    * Move udf into persistent_udf directory
    
    * Rewrite udf_js.glean_percentile
    
    * Add generated SQL

--- bigquery_etl/glam/templates/clients_histogram_bucket_counts_v1.sql
+++ bigquery_etl/glam/templates/clients_histogram_bucket_counts_v1.sql
@@ -146,7 +146,13 @@ records as (
         bucket
 )
 SELECT
-    *
+    * EXCEPT(metric_type, histogram_type),
+    -- Suffix `custom_distribution` with bucketing type
+    IF(
+      histogram_type IS NOT NULL,
+      CONCAT(metric_type, "_", histogram_type),
+      metric_type
+    ) as metric_type
 FROM
     records
 LEFT OUTER JOIN

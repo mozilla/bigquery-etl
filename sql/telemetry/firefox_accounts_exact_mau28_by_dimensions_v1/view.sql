32e24772dcfcbf9d13d51754e3cd12c8e6d1a361
Jeff Klukas <jeff@klukas.net>
2020-04-03 15:45:34 -0400

    more

--- sql/telemetry/firefox_accounts_exact_mau28_by_dimensions_v1/view.sql
+++ sql/telemetry/firefox_accounts_exact_mau28_by_dimensions_v1/view.sql
@@ -5,7 +5,7 @@ SELECT
   raw.*,
   COALESCE(cn.code, raw.country) AS country_code
 FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.firefox_accounts_exact_mau28_by_dimensions_v1` AS raw
+  `moz-fx-data-shared-prod.telemetry_derived.firefox_accounts_exact_mau28_by_dimensions_v1` AS raw
 LEFT JOIN
   `moz-fx-data-shared-prod.static.country_names_v1` cn
   ON (raw.country = cn.name)
--- sql/telemetry/firefox_desktop_exact_mau28_by_dimensions_v1/view.sql
+++ sql/telemetry/firefox_desktop_exact_mau28_by_dimensions_v1/view.sql
@@ -5,7 +5,7 @@ SELECT
   raw.*,
   COALESCE(cc.name, raw.country) AS country_name
 FROM
-  `moz-fx-data-derived-datasets.telemetry_derived.firefox_desktop_exact_mau28_by_dimensions_v1` AS raw
+  `moz-fx-data-shared-prod.telemetry_derived.firefox_desktop_exact_mau28_by_dimensions_v1` AS raw
 LEFT JOIN
   `moz-fx-data-shared-prod.static.country_codes_v1` cc
 ON

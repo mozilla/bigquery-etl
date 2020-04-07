3b71c320f2c6934cce28406da5fc04c78f2110b7
Jeff Klukas <jeff@klukas.net>
2020-04-07 15:01:38 -0400

    Update fenix_nightly generated queries

--- sql/org_mozilla_fenix_nightly/baseline_clients_last_seen/view.sql
+++ sql/org_mozilla_fenix_nightly/baseline_clients_last_seen/view.sql
@@ -5,6 +5,9 @@ AS
 SELECT
   `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(days_seen_bits) AS days_since_seen,
   `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(
+    days_created_profile_bits
+  ) AS days_since_created_profile,
+  `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(
     days_seen_session_start_bits
   ) AS days_since_seen_session_start,
   `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(

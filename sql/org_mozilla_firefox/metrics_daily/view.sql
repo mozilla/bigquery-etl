6fa908b53352c5f33312ae2497a554b52d257f03
Jeff Klukas <jeff@klukas.net>
2020-04-09 15:49:25 -0400

    Remove legacy per-app Glean usage ETL
    The incline dashboard, KPIs, and GUD are now referencing the generic glean ETL
    (baseline_clients_daily and baseline_clients_last_seen), so these other
    tables should no longer be needed.
    
    I also need to remove these from Airflow and I will need to send out a message
    to fx-data-dev to advertise this breaking change, but I doubt there's much
    usage so far of these legacy tables since they have not been well documented
    or advertised; they existed primarily to serve GUD, Incline, and KPIs.

--- sql/org_mozilla_firefox/metrics_daily/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.org_mozilla_firefox.metrics_daily`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.org_mozilla_firefox_derived.metrics_daily_v1`

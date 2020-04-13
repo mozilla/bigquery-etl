6761f2ae27a072c3e2ee44be264cb9b7db5ce36f
Daniel Thorn <dthorn@mozilla.com>
2020-04-13 09:40:25 -0700

    Bug 1629099 - Create a 1-Week Retention View

---
+++ sql/telemetry/desktop_retention_1_week/view.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW
+  `desktop_retention_1_week`
+AS
+SELECT
+  client_id,
+  DATE_SUB(submission_date, INTERVAL 13 DAY) AS submission_date,
+  -- active week 1
+  `moz-fx-data-shared-prod`.udf.active_n_weeks_ago(days_seen_bits, 0) AS retained,
+FROM
+  `moz-fx-data-shared-prod.telemetry.clients_last_seen`
+WHERE
+  -- active week 0
+  `moz-fx-data-shared-prod`.udf.active_n_weeks_ago(days_seen_bits, 1)

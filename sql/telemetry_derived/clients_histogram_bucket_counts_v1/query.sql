e5fcc6653bf92e416bd8ebbbfa13f9e144e165c5
Marina Samuel <msamuel@mozilla.com>
2020-04-13 16:38:59 -0400

    Update histogram bucket and probe counts to fix double counting bug.

--- sql/telemetry_derived/clients_histogram_bucket_counts_v1/query.sql
+++ sql/telemetry_derived/clients_histogram_bucket_counts_v1/query.sql
@@ -40,61 +40,9 @@ RETURNS ARRAY<STRUCT<key STRING, value FLOAT64>> AS (
   )
 );
 
-CREATE TEMP FUNCTION udf_normalize_histograms (
-  arrs ARRAY<STRUCT<
-    first_bucket INT64,
-    last_bucket INT64,
-    num_buckets INT64,
-    latest_version INT64,
-    metric STRING,
-    metric_type STRING,
-    key STRING,
-    process STRING,
-    agg_type STRING,
-    aggregates ARRAY<STRUCT<key STRING, value INT64>>>>)
-RETURNS ARRAY<STRUCT<
-  first_bucket INT64,
-  last_bucket INT64,
-  num_buckets INT64,
-  latest_version INT64,
-  metric STRING,
-  metric_type STRING,
-  key STRING,
-  process STRING,
-  agg_type STRING,
-  aggregates ARRAY<STRUCT<key STRING, value FLOAT64>>>> AS (
-(
-    WITH normalized AS (
-      SELECT
-        first_bucket,
-        last_bucket,
-        num_buckets,
-        latest_version,
-        metric,
-        metric_type,
-        key,
-        process,
-        agg_type,
-        udf_normalized_sum(aggregates) AS aggregates
-      FROM UNNEST(arrs))
-
-    SELECT ARRAY_AGG((first_bucket, last_bucket, num_buckets, latest_version, metric, metric_type, key, process, agg_type, aggregates))
-    FROM normalized
-));
-
-WITH normalized_histograms AS (
+WITH filtered_data AS (
   SELECT
-    client_id,
-    os,
-    app_version,
-    app_build_id,
-    channel,
-    udf_normalize_histograms(histogram_aggregates) AS histogram_aggregates
-  FROM clients_histogram_aggregates_v1
-  WHERE submission_date = @submission_date),
-
-unnested AS (
-  SELECT
+    sample_id,
     client_id,
     os,
     app_version,
@@ -106,14 +54,164 @@ unnested AS (
     latest_version,
     metric,
     metric_type,
+    key,
     process,
     agg_type,
-    histogram_aggregates.key AS key,
-    aggregates.key AS bucket,
-    aggregates.value
-  FROM normalized_histograms
-  CROSS JOIN UNNEST(histogram_aggregates) AS histogram_aggregates
-  CROSS JOIN UNNEST(aggregates) AS aggregates)
+    aggregates
+  FROM
+    clients_histogram_aggregates_v1
+  CROSS JOIN UNNEST(histogram_aggregates)
+  WHERE submission_date = @submission_date
+    AND first_bucket IS NOT NULL),
+
+aggregated_histograms AS
+  (SELECT
+    sample_id,
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    latest_version,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    aggregates
+  FROM filtered_data
+  WHERE os IS NOT NULL
+
+  UNION ALL
+
+  SELECT
+    sample_id,
+    client_id,
+    NULL AS os,
+    app_version,
+    app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    latest_version,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    udf.map_sum(ARRAY_CONCAT_AGG(aggregates)) AS aggregates
+  FROM filtered_data
+  GROUP BY
+    sample_id,
+    client_id,
+    app_version,
+    app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    latest_version
+
+  UNION ALL
+
+  SELECT
+    sample_id,
+    client_id,
+    os,
+    app_version,
+    NULL AS app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    latest_version,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    udf.map_sum(ARRAY_CONCAT_AGG(aggregates)) AS aggregates
+  FROM filtered_data
+  WHERE os IS NOT NULL
+  GROUP BY
+    sample_id,
+    client_id,
+    os,
+    app_version,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    latest_version
+
+  UNION ALL
+
+  SELECT
+    sample_id,
+    client_id,
+    NULL AS os,
+    app_version,
+    NULL AS app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    latest_version,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    udf.map_sum(ARRAY_CONCAT_AGG(aggregates)) AS aggregates
+  FROM filtered_data
+  GROUP BY
+    sample_id,
+    client_id,
+    app_version,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    latest_version),
+
+normalized_histograms AS (
+  SELECT
+    sample_id,
+    client_id,
+    os,
+    app_version,
+    app_build_id,
+    channel,
+    first_bucket,
+    last_bucket,
+    num_buckets,
+    latest_version,
+    metric,
+    metric_type,
+    key,
+    process,
+    agg_type,
+    udf_normalized_sum(aggregates) AS aggregates
+  FROM aggregated_histograms)
 
 SELECT
   os,
@@ -125,14 +223,15 @@ SELECT
   num_buckets,
   metric,
   metric_type,
-  key,
+  normalized_histograms.key AS key,
   process,
   agg_type,
   STRUCT<key STRING, value FLOAT64>(
-    CAST(bucket AS STRING),
+    CAST(aggregates.key AS STRING),
     1.0 * SUM(value)
   ) AS record
-FROM unnested
+FROM normalized_histograms
+CROSS JOIN UNNEST(aggregates) AS aggregates
 GROUP BY
   os,
   app_version,
@@ -146,4 +245,4 @@ GROUP BY
   key,
   process,
   agg_type,
-  bucket
+  aggregates.key

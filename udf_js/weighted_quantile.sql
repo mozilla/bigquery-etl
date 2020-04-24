9fb24207cb343e9545fe678aa7a6ac7afe83fdc0
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-23 17:54:11 -0700

    Modify tests with more cases

--- udf_js/weighted_quantile.sql
+++ udf_js/weighted_quantile.sql
@@ -20,27 +20,66 @@ OPTIONS
   (library = "gs://moz-fx-data-circleci-tests-bigquery-etl/wtdstats.js");
 
 SELECT
+  -- centered
   assert_equals(
-    2.0,
+    1,
     udf_js.weighted_quantile(
       50.0,
-      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("2", 1), ("3", 1)],
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 10), ("1", 10), ("2", 10)],
       "timing_distribution"
     )
   ),
+  -- skew right
   assert_equals(
-    2.5,
+    2,
     udf_js.weighted_quantile(
       50.0,
-      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("2", 2), ("3", 1)],
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 10), ("1", 10), ("2", 100)],
       "timing_distribution"
     )
-  );
+  ),
+  -- skew left
+  assert_equals(
+    0,
+    udf_js.weighted_quantile(
+      50.0,
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 100), ("1", 10), ("2", 10)],
+      "timing_distribution"
+    )
+  ),
+  -- skew right
+  assert_equals(
+    2,
+    udf_js.weighted_quantile(
+      50.0,
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 10), ("1", 10), ("2", 100)],
+      "timing_distribution"
+    )
+  ),
+    -- Bug when all weights are less than 2. This test fails with a `400
+    -- Expected "NaN" but got "NaN"`.
+  -- assert_equals(
+  --   CAST("NaN" as FLOAT64),
+  --   udf_js.weighted_quantile(
+  --     50.0,
+  --     ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("1", 1), ("2", 1)],
+  --     "timing_distribution"
+  --   )
+  -- ),
+  -- expected behavior of the above by reweighting
+  assert_equals(
+    1,
+    udf_js.weighted_quantile(
+      50.0,
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 3), ("1", 3), ("2", 3)],
+      "timing_distribution"
+    )
+  ),;
 
 #xfail
 SELECT
   udf_js.glean_percentile(
     101.0,
-    ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("2", 2), ("3", 1)],
+    ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("1", 2), ("2", 1)],
     "timing_distribution"
   );

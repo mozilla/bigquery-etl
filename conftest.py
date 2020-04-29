0f2925b883321123ab3318a9ab5ebc38d4d5c977
Anna Scholtz <anna@scholtzan.net>
2020-04-29 09:43:50 -0700

    BigQuery client fixture with random dataset

--- conftest.py
+++ conftest.py
@@ -1,7 +1,11 @@
 """PyTest configuration."""
 
+from google.cloud import bigquery
+from google.cloud import storage
+import os
 import pytest
-
+import random
+import string
 
 pytest_plugins = [
     "bigquery_etl.pytest_plugin.sql",
@@ -26,3 +30,14 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if 'integration' in item.keywords:
             item.add_marker(skip_integration)
+
+@pytest.fixture
+def bigquery_client():
+    try:
+        project_id = os.environ["GOOGLE_PROJECT_ID"]
+
+    # generate a random test dataset to avoid conflicts when running tests in parallel
+    test_dataset = "test_" + "".join(random.choice(string.ascii_lowercase) for i in range(10))
+
+def storage_client():
+    pass

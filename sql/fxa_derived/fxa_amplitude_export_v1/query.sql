8d40ab7d849da2474b8c4cdc5c47697d34515f80
Frank Bertsch <fbertsch@mozilla.com>
2020-04-29 17:38:42 -0400

    Use hmac_sha256; update for review feedback

--- sql/fxa_derived/fxa_amplitude_export_v1/query.sql
+++ sql/fxa_derived/fxa_amplitude_export_v1/query.sql
@@ -15,8 +15,7 @@ WITH
     -- to prevent weirdness from timestamp field, use provided
     -- submission date parameter as timestamp
     CAST(@submission_date AS DATETIME) AS submission_timestamp,
-    -- TODO: JS udf with HMAC
-    TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
+    TO_HEX(udf.hmac_sha256(CAST(jsonPayload.fields.user_id AS BYTES))) AS user_id,
     MIN(CONCAT(insertId, '-user')) AS insert_id,
     CAST(@submission_date AS DATETIME) AS timestamp,
     -- Amplitude properties, scalars
@@ -44,7 +43,7 @@ WITH
     `moz-fx-data-shared-prod`.udf.mode_last(ARRAY_AGG(JSON_EXTRACT_SCALAR(jsonPayload.fields.user_properties,
           "$.ua_version") IGNORE NULLS)) AS ua_version,
     `moz-fx-data-shared-prod`.udf.mode_last(ARRAY_AGG(JSON_EXTRACT_SCALAR(jsonPayload.fields.user_properties,
-          "$.ua_version") IGNORE NULLS)) AS us_browser,
+          "$.ua_version") IGNORE NULLS)) AS ua_browser,
     MAX(jsonPayload.fields.app_version) AS app_version,
   FROM
     base_events
@@ -70,6 +69,7 @@ WITH
     '$identify' AS event_type,
     timestamp,
     '' AS event_properties,
+    -- $ is not valid for a column name, so edit it into the json string
     REPLACE( TO_JSON_STRING( STRUCT( region,
           country,
           LANGUAGE,
@@ -79,7 +79,7 @@ WITH
           sync_active_devices_week,
           sync_active_devices_month,
           ua_version,
-          us_browser,
+          ua_browser,
           app_version,
           STRUCT( fxa_services_used ) AS str_dollar_sign_postInsert ) ), 'str_dollar_sign_', '$')
   FROM

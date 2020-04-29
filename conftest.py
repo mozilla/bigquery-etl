371c3b3c21d2d16dd3b910e6deb0fb1524117d44
Anna Scholtz <anna@scholtzan.net>
2020-04-29 16:13:03 -0700

    Create pytest fixtures

--- conftest.py
+++ conftest.py
@@ -7,6 +7,10 @@ import pytest
 import random
 import string
 
+
+TEST_BUCKET = "bigquery-etl-integration-test-bucket"
+
+
 pytest_plugins = [
     "bigquery_etl.pytest_plugin.sql",
     "bigquery_etl.pytest_plugin.udf",
@@ -23,21 +27,55 @@ def pytest_collection_modifyitems(config, items):
     if keywordexpr or markexpr:
         return
 
-    skip_integration = pytest.mark.skip(
-        reason='integration marker not selected'
-    )
+    skip_integration = pytest.mark.skip(reason="integration marker not selected")
 
     for item in items:
-        if 'integration' in item.keywords:
+        if "integration" in item.keywords:
             item.add_marker(skip_integration)
+
+
+@pytest.fixture
+def project_id():
+    # GOOGLE_PROJECT_ID needs to be set for integration tests to run
+    project_id = os.environ["GOOGLE_PROJECT_ID"]
+
+    return project_id
+
 
 @pytest.fixture
 def bigquery_client():
-    try:
-        project_id = os.environ["GOOGLE_PROJECT_ID"]
+    project_id = os.environ["GOOGLE_PROJECT_ID"]
+    return bigquery.Client(project_id)
 
+
+@pytest.fixture
+def temporary_dataset():
     # generate a random test dataset to avoid conflicts when running tests in parallel
-    test_dataset = "test_" + "".join(random.choice(string.ascii_lowercase) for i in range(10))
+    test_dataset = "test_" + "".join(
+        random.choice(string.ascii_lowercase) for i in range(12)
+    )
 
+    project_id = os.environ["GOOGLE_PROJECT_ID"]
+    client = bigquery.Client(project_id)
+    client.create_dataset(test_dataset)
+
+    yield test_dataset
+
+    # cleanup and remove temporary dataset
+    client.delete_dataset(test_dataset, delete_contents=True, not_found_ok=True)
+
+
+@pytest.fixture
+def test_bucket():
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(TEST_BUCKET)
+
+    yield bucket
+
+    # cleanup test bucket
+    bucket.delete_blobs(bucket.list_blobs())
+
+
+@pytest.fixture
 def storage_client():
-    pass
+    yield storage.Client()

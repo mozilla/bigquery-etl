88a0da7e2bf418eaedcdc6034d0ddb17001c2ea9
Anna Scholtz <anna@scholtzan.net>
2020-04-29 09:50:50 -0700

    Limit metrics published in deviations query

--- sql/telemetry_derived/deviations_v1/query.sql
+++ sql/telemetry_derived/deviations_v1/query.sql
@@ -6,4 +6,6 @@ SELECT
   metric
 FROM
   `moz-fx-data-shared-prod.analysis.deviations`
-WHERE date = @submission_date
+WHERE
+  metric IN ('desktop_dau', 'mean_active_hours_per_client')
+  AND date = @submission_date

1c38ee9e46885ac43f7abf1aeae19f3f6eb44a03
Frank Bertsch <fbertsch@mozilla.com>
2020-05-04 14:23:42 -0400

    Format UDF sql

--- udf/active_values_from_days_seen_map.sql
+++ udf/active_values_from_days_seen_map.sql
@@ -5,24 +5,53 @@ time period in question
 
 Begin and end are both inclusive.
 */
-CREATE OR REPLACE FUNCTION
-  udf.active_values_from_days_seen_map(
-    days_seen_bits_map ARRAY<STRUCT<key STRING, value INT64>>,
-    start_offset INT64,
-    n_bits INT64) AS (
-    ARRAY(
-      SELECT DISTINCT
-        key
-      FROM
-        UNNEST(days_seen_bits_map)
-      WHERE
+CREATE OR REPLACE FUNCTION udf.active_values_from_days_seen_map(
+  days_seen_bits_map ARRAY<STRUCT<key STRING, value INT64>>,
+  start_offset INT64,
+  n_bits INT64
+) AS (
+  ARRAY(
+    SELECT
+      DISTINCT key
+    FROM
+      UNNEST(days_seen_bits_map)
+    WHERE
         -- TODO: Use udf.bits28_active_in_range when it's available
-        BIT_COUNT(value << (64 + start_offset - 1) >> (64 - n_bits)) > 0
-    ));
+      BIT_COUNT(value << (64 + start_offset - 1) >> (64 - n_bits)) > 0
+  )
+);
 
 -- Tests
 SELECT
-  assert_array_equals(['a', 'b'], udf.active_values_from_map([STRUCT('a' AS key, 1 AS value), STRUCT('b' AS key, 3 AS value)], 0, 1)),
-  assert_array_equals(['a'], udf.active_values_from_map([STRUCT('a' AS key, 2048 AS value), STRUCT('b' AS key, 3 AS value)], -14, 7)),
-  assert_array_equals(['b'], udf.active_values_from_map([STRUCT('a' AS key, 2048 AS value), STRUCT('b' AS key, 3 AS value)], -6, 7)),
-  assert_array_equals(['a', 'b'], udf.active_values_from_map([STRUCT('a' AS key, 1 AS value), STRUCT('b' AS key, 3 AS value)], -27, 28));
+  assert_array_equals(
+    ['a', 'b'],
+    udf.active_values_from_map(
+      [STRUCT('a' AS key, 1 AS value), STRUCT('b' AS key, 3 AS value)],
+      0,
+      1
+    )
+  ),
+  assert_array_equals(
+    ['a'],
+    udf.active_values_from_map(
+      [STRUCT('a' AS key, 2048 AS value), STRUCT('b' AS key, 3 AS value)],
+      -14,
+      7
+    )
+  ),
+  assert_array_equals(
+    ['b'],
+    udf.active_values_from_map(
+      [STRUCT('a' AS key, 2048 AS value), STRUCT('b' AS key, 3 AS value)],
+      -6,
+      7
+    )
+  ),
+  assert_array_equals(
+    ['a', 'b'],
+    udf.active_values_from_map(
+      [STRUCT('a' AS key, 1 AS value), STRUCT('b' AS key, 3 AS value)],
+      -27,
+      28
+    )
+  );

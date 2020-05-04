44e4c2c3a538972c272fdb4dee16e014975f5298
Frank Bertsch <fbertsch@mozilla.com>
2020-05-04 10:16:43 -0400

    Reformat SQL files

--- sql/fxa_derived/fxa_amplitude_export_v1/init.sql
+++ sql/fxa_derived/fxa_amplitude_export_v1/init.sql
@@ -1,33 +1,34 @@
 CREATE OR REPLACE TABLE
   `moz-fx-data-shared-prod`.fxa_derived.fxa_amplitude_export_v1
-PARTITION BY (DATE(submission_timestamp))
-CLUSTER BY (user_id)
+PARTITION BY
+  (DATE(submission_timestamp))
+CLUSTER BY
+  (user_id)
 AS
 WITH columns AS (
-    SELECT
-      CAST(NULL AS TIMESTAMP) AS submission_timestamp,
-      CAST(NULL AS STRING) AS user_id,
-      CAST(NULL AS STRING) AS insert_id,
-      CAST(NULL AS DATETIME) AS timestamp,
-      CAST(NULL AS STRING) AS region,
-      CAST(NULL AS STRING) AS country,
-      CAST(NULL AS STRING) AS `language`,
-      CAST(NULL AS ARRAY<STRING>) AS services,
-      CAST(NULL AS ARRAY<STRING>) AS oauth_client_ids,
-      CAST(NULL AS ARRAY<STRING>) AS fxa_services_used,
-      CAST(NULL AS ARRAY<STRING>) AS os_used_day,
-      CAST(NULL AS ARRAY<STRUCT<arr ARRAY<STRING>>>) AS os_used_week,
-      CAST(NULL AS ARRAY<STRUCT<arr ARRAY<STRING>>>) AS os_used_month,
-      CAST(NULL AS INT64) AS sync_device_count,
-      CAST(NULL AS INT64) AS sync_active_devices_day,
-      CAST(NULL AS INT64) AS sync_active_devices_week,
-      CAST(NULL AS INT64) AS sync_active_devices_month,
-      CAST(NULL AS STRING) AS ua_version,
-      CAST(NULL AS STRING) AS ua_browser,
-      CAST(NULL AS FLOAT64) AS app_version,
-      CAST(NULL AS INT64) AS days_seen_bits,
-  )
-
+  SELECT
+    CAST(NULL AS TIMESTAMP) AS submission_timestamp,
+    CAST(NULL AS STRING) AS user_id,
+    CAST(NULL AS STRING) AS insert_id,
+    CAST(NULL AS DATETIME) AS timestamp,
+    CAST(NULL AS STRING) AS region,
+    CAST(NULL AS STRING) AS country,
+    CAST(NULL AS STRING) AS `language`,
+    CAST(NULL AS ARRAY<STRING>) AS services,
+    CAST(NULL AS ARRAY<STRING>) AS oauth_client_ids,
+    CAST(NULL AS ARRAY<STRING>) AS fxa_services_used,
+    CAST(NULL AS ARRAY<STRING>) AS os_used_day,
+    CAST(NULL AS ARRAY<STRUCT<arr ARRAY<STRING>>>) AS os_used_week,
+    CAST(NULL AS ARRAY<STRUCT<arr ARRAY<STRING>>>) AS os_used_month,
+    CAST(NULL AS INT64) AS sync_device_count,
+    CAST(NULL AS INT64) AS sync_active_devices_day,
+    CAST(NULL AS INT64) AS sync_active_devices_week,
+    CAST(NULL AS INT64) AS sync_active_devices_month,
+    CAST(NULL AS STRING) AS ua_version,
+    CAST(NULL AS STRING) AS ua_browser,
+    CAST(NULL AS FLOAT64) AS app_version,
+    CAST(NULL AS INT64) AS days_seen_bits,
+)
 SELECT
   *
 FROM
--- tests/assert/nested_array_equals.sql
+++ tests/assert/nested_array_equals.sql
@@ -4,15 +4,16 @@ That field contains the nested array.
 
 This is a workaround becuase BigQuery does not support nested arrays.
 */
-
 CREATE TEMP FUNCTION assert_nested_array_equals(expected ANY TYPE, actual ANY TYPE) AS (
   IF(
     EXISTS(
       SELECT
         assert_array_equals(e.arr, a.arr)
       FROM
-        UNNEST(expected) AS e WITH OFFSET e_offset,
-        UNNEST(actual) AS a WITH OFFSET a_offset
+        UNNEST(expected) AS e
+        WITH OFFSET e_offset,
+        UNNEST(actual) AS a
+        WITH OFFSET a_offset
       WHERE
         e_offset = a_offset
     ),

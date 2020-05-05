888ca9c383b68f6d6c3de3dc75c378fcb8f274fb
Frank Bertsch <fbertsch@mozilla.com>
2020-05-05 17:01:59 -0400

    Reformat SQL

--- udf/histogram_percentiles.sql
+++ udf/histogram_percentiles.sql
@@ -4,7 +4,7 @@ Given histogram and list of percentiles, calculate what those percentiles are fo
 If the histogram is empty, returns NULL.
 */
 CREATE OR REPLACE FUNCTION udf.histogram_percentiles(histogram ANY TYPE, percentiles ARRAY<FLOAT64>)
-  RETURNS ARRAY<INT64> AS (
+RETURNS ARRAY<INT64> AS (
 --   IF(
 --     (SELECT SUM(value) FROM UNNEST(histogram.value)) = 0
   ARRAY(
@@ -21,8 +21,10 @@ CREATE OR REPLACE FUNCTION udf.histogram_percentiles(histogram ANY TYPE, percent
         FROM
           UNNEST(histogram.values)
       )
-    INNER JOIN UNNEST(percentiles) AS percentile
-      ON cumulative_fraction >= percentile
+    INNER JOIN
+      UNNEST(percentiles) AS percentile
+    ON
+      cumulative_fraction >= percentile
     GROUP BY
       percentile
     ORDER BY
@@ -34,20 +36,25 @@ SELECT
   assert_array_equals(
     [10, 50, 90],
     udf.histogram_percentiles(
-      STRUCT(ARRAY(SELECT STRUCT(v AS key, 1 AS value) FROM UNNEST(generate_array(1, 100)) AS v) AS values),
+      STRUCT(
+        ARRAY(
+          SELECT
+            STRUCT(v AS key, 1 AS value)
+          FROM
+            UNNEST(generate_array(1, 100)) AS v
+        ) AS values
+      ),
       [.1, .5, .9]
     )
   ),
   assert_array_equals(
     [100],
     udf.histogram_percentiles(
-      STRUCT([STRUCT(99 AS key, 100 AS value), STRUCT(100, 101)] AS values), [.5]
+      STRUCT([STRUCT(99 AS key, 100 AS value), STRUCT(100, 101)] AS values),
+      [.5]
     )
   ),
   assert_array_equals(
     CAST(NULL AS ARRAY<INT64>),
-    udf.histogram_percentiles(
-      STRUCT([STRUCT(0 AS key, 0 AS value)] AS values),
-      [.2, .5]
-    )
+    udf.histogram_percentiles(STRUCT([STRUCT(0 AS key, 0 AS value)] AS values), [.2, .5])
   );

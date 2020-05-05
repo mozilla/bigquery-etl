e3b21e3da330dd9c7a3e6ee025b6212f35b49c9f
Jeff Klukas <jeff@klukas.net>
2020-05-05 14:41:02 -0400

    Add logic to protect backfill scenarios

--- sql/telemetry_derived/clients_first_seen_v1/query.sql
+++ sql/telemetry_derived/clients_first_seen_v1/query.sql
@@ -16,6 +16,11 @@ SELECT
     cfs.first_seen_date IS NULL
   THEN
     @submission_date
+  WHEN
+    -- This should only happen in backfill scenarios where we've missed a day.
+    cfs.first_seen_date > @submission_date
+  THEN
+    @submission_date
   ELSE
     cfs.first_seen_date
   END
@@ -24,11 +29,16 @@ SELECT
   -- Logic for second_seen_date
   CASE
   WHEN
-    cfs.first_seen_date IS NULL
+    cfs.first_seen_date IS NULL OR cfs.first_seen_date > @submission_date
   THEN
     NULL
   WHEN
     cfs.second_seen_date IS NULL
+  THEN
+    @submission_date
+  WHEN
+    -- This should only happen in backfill scenarios where we've missed a day.
+    cfs.second_seen_date > @submission_date
   THEN
     @submission_date
   ELSE
@@ -39,7 +49,7 @@ SELECT
   -- Only insert dimensions from clients_daily if this is the first time the
   -- client has been seen; otherwise, we copy over the existing dimensions
   -- from the first sighting.
-  IF(cfs.client_id IS NULL, today, cfs).* EXCEPT (first_seen_date, second_seen_date)
+  IF(cfs.client_id IS NULL OR cfs.first_seen_date > @submission_date, today, cfs).* EXCEPT (first_seen_date, second_seen_date)
 FROM
   clients_first_seen_v1 AS cfs
 FULL JOIN

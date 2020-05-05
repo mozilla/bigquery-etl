4109294a43d77f1d8b42a48d0207380eb894b0d3
Jeff Klukas <jeff@klukas.net>
2020-05-05 14:59:14 -0400

    Iterate over sample_id

--- sql/telemetry_derived/clients_first_seen_v1/query.sql
+++ sql/telemetry_derived/clients_first_seen_v1/query.sql
@@ -29,7 +29,8 @@ SELECT
   -- Logic for second_seen_date
   CASE
   WHEN
-    cfs.first_seen_date IS NULL OR cfs.first_seen_date > @submission_date
+    cfs.first_seen_date IS NULL
+    OR cfs.first_seen_date > @submission_date
   THEN
     NULL
   WHEN
@@ -49,7 +50,10 @@ SELECT
   -- Only insert dimensions from clients_daily if this is the first time the
   -- client has been seen; otherwise, we copy over the existing dimensions
   -- from the first sighting.
-  IF(cfs.client_id IS NULL OR cfs.first_seen_date > @submission_date, today, cfs).* EXCEPT (first_seen_date, second_seen_date)
+  IF(cfs.client_id IS NULL OR cfs.first_seen_date > @submission_date, today, cfs).* EXCEPT (
+    first_seen_date,
+    second_seen_date
+  )
 FROM
   clients_first_seen_v1 AS cfs
 FULL JOIN

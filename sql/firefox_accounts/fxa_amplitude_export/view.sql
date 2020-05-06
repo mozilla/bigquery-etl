881d662c439c5329da91f57b3cf560b99dcb4245
Frank Bertsch <fbertsch@mozilla.com>
2020-05-05 22:06:46 -0400

    Remove version suffix from view

--- sql/firefox_accounts/fxa_amplitude_export/view.sql
+++ sql/firefox_accounts/fxa_amplitude_export/view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_accounts.fxa_amplitude_export_v1`
+  `moz-fx-data-shared-prod.firefox_accounts.fxa_amplitude_export`
 AS
 WITH active_users AS (
   SELECT

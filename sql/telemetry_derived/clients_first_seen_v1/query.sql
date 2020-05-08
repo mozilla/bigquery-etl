6d9ebad09c098e581a7e5e80ae1624cc34692c8c
Jeff Klukas <jeff@klukas.net>
2020-05-08 09:56:08 -0400

    Refactor clients_first_seen
    @jmccrosky reported that the first_seen_dates were not necessarily
    correct. I neglected an important ORDER BY clause in init.sql.
    
    This also simplifies the logic in the incremental query.

--- sql/telemetry_derived/clients_first_seen_v1/query.sql
+++ sql/telemetry_derived/clients_first_seen_v1/query.sql
@@ -7,55 +7,50 @@ WITH today AS (
     clients_daily_v6
   WHERE
     submission_date = @submission_date
+),
+previous AS (
+  -- If we need to reprocess data, we have to make sure to delete all data
+  -- earlier than the passed @submission_date parameter, so we null out
+  -- invalid second_seen_date values and drop rows invalid first_seen_date.
+  SELECT
+    * REPLACE (IF(second_seen_date >= @submission_date, NULL, second_seen_date) AS second_seen_date)
+  FROM
+    clients_first_seen_v1
+  WHERE
+    first_seen_date < @submission_date
 )
 SELECT
-  --
-  -- Logic for first_seen_date
-  CASE
-  WHEN
-    cfs.first_seen_date IS NULL
-  THEN
-    @submission_date
-  WHEN
-    -- This should only happen in backfill scenarios where we've missed a day.
-    cfs.first_seen_date > @submission_date
-  THEN
-    @submission_date
-  ELSE
-    cfs.first_seen_date
-  END
-  AS first_seen_date,
-  --
-  -- Logic for second_seen_date
-  CASE
-  WHEN
-    cfs.first_seen_date IS NULL
-    OR cfs.first_seen_date > @submission_date
-  THEN
-    NULL
-  WHEN
-    cfs.second_seen_date IS NULL
-  THEN
-    @submission_date
-  WHEN
-    -- This should only happen in backfill scenarios where we've missed a day.
-    cfs.second_seen_date > @submission_date
-  THEN
-    @submission_date
-  ELSE
-    cfs.second_seen_date
-  END
-  AS second_seen_date,
-  --
   -- Only insert dimensions from clients_daily if this is the first time the
   -- client has been seen; otherwise, we copy over the existing dimensions
   -- from the first sighting.
-  IF(cfs.client_id IS NULL OR cfs.first_seen_date > @submission_date, today, cfs).* EXCEPT (
-    first_seen_date,
-    second_seen_date
+  IF(previous.client_id IS NULL, today, previous).* REPLACE (
+    -- Logic for first_seen_date
+    CASE
+    WHEN
+      previous.first_seen_date IS NULL
+    THEN
+      @submission_date
+    ELSE
+      previous.first_seen_date
+    END
+    AS first_seen_date,
+    -- Logic for second_seen_date
+    CASE
+    WHEN
+      previous.first_seen_date IS NULL
+    THEN
+      NULL
+    WHEN
+      previous.second_seen_date IS NULL
+    THEN
+      @submission_date
+    ELSE
+      previous.second_seen_date
+    END
+    AS second_seen_date
   )
 FROM
-  clients_first_seen_v1 AS cfs
+  previous
 FULL JOIN
   today
 USING

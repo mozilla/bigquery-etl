4a39520d26cc16c48bf34f797510a6672776ab8c
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-05-11 15:09:40 +0200

    Reformat the query

--- sql/internet_outages/italy_covid19_outage_v1/query.sql
+++ sql/internet_outages/italy_covid19_outage_v1/query.sql
@@ -1,5 +1,7 @@
 DECLARE country_code STRING DEFAULT "IT";
+
 DECLARE fromdate DATE DEFAULT '2020-01-01';
+
 DECLARE todate DATE DEFAULT '2020-03-31';
 
 -- The minimum size of a bucket for it to be published.
@@ -7,116 +9,175 @@ DECLARE MIN_NUMBER_CLIENTS INT64 DEFAULT 5000;
 
 -- Note: udf.udf_json_extract_int_map map doesn't work in this case as it expects an INT -> INT
 -- map, while we have a STRING->int map
-CREATE TEMP FUNCTION
-  udf_json_extract_string_to_int_map (input STRING) AS (ARRAY(
+CREATE TEMP FUNCTION udf_json_extract_string_to_int_map(input STRING) AS (
+  ARRAY(
     SELECT
-      STRUCT(CAST(SPLIT(entry, ':')[OFFSET(0)] AS STRING) AS key,
-             CAST(SPLIT(entry, ':')[OFFSET(1)] AS INT64) AS value)
+      STRUCT(
+        CAST(SPLIT(entry, ':')[OFFSET(0)] AS STRING) AS key,
+        CAST(SPLIT(entry, ':')[OFFSET(1)] AS INT64) AS value
+      )
     FROM
       UNNEST(SPLIT(REPLACE(TRIM(input, '{}'), '"', ''), ',')) AS entry
     WHERE
-      LENGTH(entry) > 0 ));
-     
+      LENGTH(entry) > 0
+  )
+);
 
 -- Get a stable source for DAUs.
 WITH DAUs AS (
-    SELECT
-        submission_date as date,
-        COUNT(*) as client_count
-    FROM
-        telemetry.clients_daily
-    WHERE
-        submission_date >= fromdate
-        AND submission_date <= todate
-        AND country = country_code
-    GROUP BY 1
-    HAVING COUNT(*) > MIN_NUMBER_CLIENTS
+  SELECT
+    submission_date AS date,
+    COUNT(*) AS client_count
+  FROM
+    telemetry.clients_daily
+  WHERE
+    submission_date >= fromdate
+    AND submission_date <= todate
+    AND country = country_code
+  GROUP BY
+    1
+  HAVING
+    COUNT(*) > MIN_NUMBER_CLIENTS
 ),
 -- Compute aggregates for the health data.
 health_data_sample AS (
-    SELECT 
-        DATE(SAFE_CAST(creation_date as TIMESTAMP)) as date,
-        client_id,
-        SUM(coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.undefined') AS INT64),0)) as eUndefined,
-        SUM(coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.timeout') AS INT64),0)) as eTimeOut,
-        SUM(coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.abort') AS INT64),0)) as eAbort,
-        SUM(coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.eUnreachable') AS INT64),0)) as eUnreachable,
-        SUM(coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.eTerminated') AS INT64),0))  as eTerminated,
-        SUM(coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.eChannelOpen') AS INT64),0))  as eChannelOpen,
-    FROM
-        telemetry.health
-    WHERE
-        date(submission_timestamp) >= fromdate
-        AND date(submission_timestamp) <= todate
-        AND normalized_country_code = country_code
-    GROUP BY 1, 2
-), health_data_aggregates AS (
-    SELECT
-        date,
-        COUNTIF(eUndefined > 0) as num_clients_eUndefined,
-        COUNTIF(eTimeOut > 0) as num_clients_eTimeOut,
-        COUNTIF(eAbort > 0) as num_clients_eAbort,
-        COUNTIF(eUnreachable > 0) as num_clients_eUnreachable,
-        COUNTIF(eTerminated > 0) as num_clients_eTerminated,
-        COUNTIF(eChannelOpen > 0) as num_clients_eChannelOpen,
-    FROM health_data_sample
-    GROUP BY date
-    HAVING
-      COUNT(*) > MIN_NUMBER_CLIENTS
-), final_health_data AS (
-    SELECT
-        h.date,
-        (num_clients_eUndefined / DAUs.client_count) as proportion_undefined,
-        (num_clients_eTimeOut / DAUs.client_count) as proportion_timeout,
-        (num_clients_eAbort / DAUs.client_count) as proportion_abort,
-        (num_clients_eUnreachable / DAUs.client_count) as proportion_unreachable,
-        (num_clients_eTerminated / DAUs.client_count) as proportion_terminated,
-        (num_clients_eChannelOpen / DAUs.client_count) as proportion_channel_open,
-    FROM health_data_aggregates AS h
-    INNER JOIN DAUs ON DAUs.date = h.date
+  SELECT
+    DATE(SAFE_CAST(creation_date AS TIMESTAMP)) AS date,
+    client_id,
+    SUM(
+      coalesce(
+        CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.undefined') AS INT64),
+        0
+      )
+    ) AS eUndefined,
+    SUM(
+      coalesce(
+        CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.timeout') AS INT64),
+        0
+      )
+    ) AS eTimeOut,
+    SUM(
+      coalesce(CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.abort') AS INT64), 0)
+    ) AS eAbort,
+    SUM(
+      coalesce(
+        CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.eUnreachable') AS INT64),
+        0
+      )
+    ) AS eUnreachable,
+    SUM(
+      coalesce(
+        CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.eTerminated') AS INT64),
+        0
+      )
+    ) AS eTerminated,
+    SUM(
+      coalesce(
+        CAST(JSON_EXTRACT(additional_properties, '$.payload.sendFailure.eChannelOpen') AS INT64),
+        0
+      )
+    ) AS eChannelOpen,
+  FROM
+    telemetry.health
+  WHERE
+    date(submission_timestamp) >= fromdate
+    AND date(submission_timestamp) <= todate
+    AND normalized_country_code = country_code
+  GROUP BY
+    1,
+    2
+),
+health_data_aggregates AS (
+  SELECT
+    date,
+    COUNTIF(eUndefined > 0) AS num_clients_eUndefined,
+    COUNTIF(eTimeOut > 0) AS num_clients_eTimeOut,
+    COUNTIF(eAbort > 0) AS num_clients_eAbort,
+    COUNTIF(eUnreachable > 0) AS num_clients_eUnreachable,
+    COUNTIF(eTerminated > 0) AS num_clients_eTerminated,
+    COUNTIF(eChannelOpen > 0) AS num_clients_eChannelOpen,
+  FROM
+    health_data_sample
+  GROUP BY
+    date
+  HAVING
+    COUNT(*) > MIN_NUMBER_CLIENTS
+),
+final_health_data AS (
+  SELECT
+    h.date,
+    (num_clients_eUndefined / DAUs.client_count) AS proportion_undefined,
+    (num_clients_eTimeOut / DAUs.client_count) AS proportion_timeout,
+    (num_clients_eAbort / DAUs.client_count) AS proportion_abort,
+    (num_clients_eUnreachable / DAUs.client_count) AS proportion_unreachable,
+    (num_clients_eTerminated / DAUs.client_count) AS proportion_terminated,
+    (num_clients_eChannelOpen / DAUs.client_count) AS proportion_channel_open,
+  FROM
+    health_data_aggregates AS h
+  INNER JOIN
+    DAUs
+  ON
+    DAUs.date = h.date
 ),
 -- Compute aggregates for histograms coming from the health ping.
 histogram_data_sample AS (
   SELECT
     client_id,
     document_id,
-    DATE(SAFE_CAST(creation_date as TIMESTAMP)) as time_slot,
-    udf.json_extract_int_map(JSON_EXTRACT(payload.histograms.dns_failed_lookup_time,'$.values')) as dns_fail,
-    udf.json_extract_int_map(JSON_EXTRACT(payload.histograms.dns_lookup_time,'$.values')) as dns_success,
-    udf.json_extract_int_map(JSON_EXTRACT(payload.histograms.ssl_cert_verification_errors,'$.values')) as ssl_cert_errors,
-    udf.json_extract_int_map(JSON_EXTRACT(payload.processes.content.histograms.http_page_tls_handshake,'$.values')) as tls_handshake,
-  FROM telemetry.main
+    DATE(SAFE_CAST(creation_date AS TIMESTAMP)) AS time_slot,
+    udf.json_extract_int_map(
+      JSON_EXTRACT(payload.histograms.dns_failed_lookup_time, '$.values')
+    ) AS dns_fail,
+    udf.json_extract_int_map(
+      JSON_EXTRACT(payload.histograms.dns_lookup_time, '$.values')
+    ) AS dns_success,
+    udf.json_extract_int_map(
+      JSON_EXTRACT(payload.histograms.ssl_cert_verification_errors, '$.values')
+    ) AS ssl_cert_errors,
+    udf.json_extract_int_map(
+      JSON_EXTRACT(payload.processes.content.histograms.http_page_tls_handshake, '$.values')
+    ) AS tls_handshake,
+  FROM
+    telemetry.main
   WHERE
     DATE(submission_timestamp) >= fromdate
     AND DATE(submission_timestamp) <= todate
     -- Additionally limit the creation date.
-    AND DATE(SAFE_CAST(creation_date as TIMESTAMP)) >= fromdate
-    AND DATE(SAFE_CAST(creation_date as TIMESTAMP)) <= todate
+    AND DATE(SAFE_CAST(creation_date AS TIMESTAMP)) >= fromdate
+    AND DATE(SAFE_CAST(creation_date AS TIMESTAMP)) <= todate
     AND metadata.geo.country = country_code
     -- Restrict to Firefox.
     AND normalized_app_name = 'Firefox'
     -- Only to pings who seem to represent an active session.
     AND payload.info.subsession_length >= 0
-), 
+),
 -- DNS_SUCCESS histogram
-dns_success_time as (
-    select
-      time_slot as date,
-      exp( sum( log(key )*count)/sum(count)) as value
-    from (
-        select
-          client_id,
-          time_slot,
-          key,
-          sum(value) as count
-        from histogram_data_sample cross join unnest(histogram_data_sample.dns_success)
-        group by time_slot, client_id,key
+dns_success_time AS (
+  SELECT
+    time_slot AS date,
+    exp(sum(log(key) * count) / sum(count)) AS value
+  FROM
+    (
+      SELECT
+        client_id,
+        time_slot,
+        key,
+        sum(value) AS count
+      FROM
+        histogram_data_sample
+      CROSS JOIN
+        UNNEST(histogram_data_sample.dns_success)
+      GROUP BY
+        time_slot,
+        client_id,
+        key
     )
-    where
-      key >0
-    group by 1
-    HAVING
-      count(distinct(client_id)) > MIN_NUMBER_CLIENTS
+  WHERE
+    key > 0
+  GROUP BY
+    1
+  HAVING
+    count(DISTINCT(client_id)) > MIN_NUMBER_CLIENTS
 ),
 -- A shared source for the DNS_FAIL histogram
 dns_failure_src AS (
@@ -125,67 +186,107 @@ dns_failure_src AS (
     time_slot,
     key,
     sum(value) AS count
-  FROM histogram_data_sample CROSS JOIN unnest(histogram_data_sample.dns_fail)
-  GROUP BY time_slot, client_id, key
+  FROM
+    histogram_data_sample
+  CROSS JOIN
+    UNNEST(histogram_data_sample.dns_fail)
+  GROUP BY
+    time_slot,
+    client_id,
+    key
 ),
 -- DNS_FAIL histogram
 dns_failure_time AS (
-    select
-      time_slot as date,
-      exp(sum(log(key) * count) / sum(count)) as value
-    from dns_failure_src
-    where
-      key > 0
-    group by 1
-    HAVING
-      count(distinct(client_id)) > MIN_NUMBER_CLIENTS
+  SELECT
+    time_slot AS date,
+    exp(sum(log(key) * count) / sum(count)) AS value
+  FROM
+    dns_failure_src
+  WHERE
+    key > 0
+  GROUP BY
+    1
+  HAVING
+    count(DISTINCT(client_id)) > MIN_NUMBER_CLIENTS
 ),
 -- DNS_FAIL counts
 dns_failure_counts AS (
   SELECT
-    time_slot as date,
-    avg(count) as value
-  FROM (
-    SELECT
-      client_id, time_slot, sum(count) as count
-    FROM dns_failure_src
-    GROUP BY time_slot, client_id
-  )
-  GROUP BY time_slot
+    time_slot AS date,
+    avg(count) AS value
+  FROM
+    (
+      SELECT
+        client_id,
+        time_slot,
+        sum(count) AS count
+      FROM
+        dns_failure_src
+      GROUP BY
+        time_slot,
+        client_id
+    )
+  GROUP BY
+    time_slot
   HAVING
-    count(distinct(client_id)) > MIN_NUMBER_CLIENTS
+    count(DISTINCT(client_id)) > MIN_NUMBER_CLIENTS
 ),
 -- TLS_HANDSHAKE histogram
-tls_handshake_time as (
-    select
-      time_slot as date,
-      exp( sum( log(key )*count)/sum(count)) as value
-    from (
-        select
-          client_id,
-          time_slot,
-          key,
-          sum(value) as count
-        from histogram_data_sample cross join unnest(histogram_data_sample.tls_handshake)
-        group by time_slot, client_id,key
+tls_handshake_time AS (
+  SELECT
+    time_slot AS date,
+    exp(sum(log(key) * count) / sum(count)) AS value
+  FROM
+    (
+      SELECT
+        client_id,
+        time_slot,
+        key,
+        sum(value) AS count
+      FROM
+        histogram_data_sample
+      CROSS JOIN
+        UNNEST(histogram_data_sample.tls_handshake)
+      GROUP BY
+        time_slot,
+        client_id,
+        key
     )
-    where
-      key >0
-    group by 1
-    HAVING
-      count(distinct(client_id)) > MIN_NUMBER_CLIENTS
+  WHERE
+    key > 0
+  GROUP BY
+    1
+  HAVING
+    count(DISTINCT(client_id)) > MIN_NUMBER_CLIENTS
 )
 SELECT
-  DAUs.date as date,
-  hd.* EXCEPT(date),
+  DAUs.date AS date,
+  hd.* EXCEPT (date),
   ds.value AS avg_dns_success_time,
   df.value AS avg_dns_failure_time,
   dfc.value AS count_dns_failure,
   tls.value AS avg_tls_handshake_time
-FROM final_health_data AS hd
-FULL OUTER JOIN DAUs ON DAUs.date = hd.date
-FULL OUTER JOIN dns_success_time AS ds ON DAUs.date = ds.date
-FULL OUTER JOIN dns_failure_time AS df ON DAUs.date = df.date
-FULL OUTER JOIN dns_failure_counts AS dfc ON DAUs.date = dfc.date
-FULL OUTER JOIN tls_handshake_time AS tls ON DAUs.date = tls.date
-ORDER by 1
+FROM
+  final_health_data AS hd
+FULL OUTER JOIN
+  DAUs
+ON
+  DAUs.date = hd.date
+FULL OUTER JOIN
+  dns_success_time AS ds
+ON
+  DAUs.date = ds.date
+FULL OUTER JOIN
+  dns_failure_time AS df
+ON
+  DAUs.date = df.date
+FULL OUTER JOIN
+  dns_failure_counts AS dfc
+ON
+  DAUs.date = dfc.date
+FULL OUTER JOIN
+  tls_handshake_time AS tls
+ON
+  DAUs.date = tls.date
+ORDER BY
+  1

b89c873d0f1c3eaa9b45e4555d1d879e453d3b1d
Anna Scholtz <anna@scholtzan.net>
2020-05-14 16:40:02 -0700

    DAG validation

--- tests/query_scheduling/test_dag.py
+++ tests/query_scheduling/test_dag.py
@@ -15,15 +15,15 @@ class TestDag:
     }
 
     def test_dag_instantiation(self):
-        dag = Dag("test_dag", "daily", self.default_args)
+        dag = Dag("bqetl_test_dag", "daily", self.default_args)
 
-        assert dag.name == "test_dag"
+        assert dag.name == "bqetl_test_dag"
         assert dag.schedule_interval == "daily"
         assert dag.tasks == []
         assert dag.default_args == self.default_args
 
     def test_add_tasks(self):
-        dag = Dag("test_dag", "daily", self.default_args)
+        dag = Dag("bqetl_test_dag", "daily", self.default_args)
 
         query_file = (
             TEST_DIR
@@ -45,7 +45,7 @@ class TestDag:
     def test_from_dict(self):
         dag = Dag.from_dict(
             {
-                "test_dag": {
+                "bqetl_test_dag": {
                     "schedule_interval": "daily",
                     "default_args": {
                         "owner": "test@example.com",
@@ -55,7 +55,7 @@ class TestDag:
             }
         )
 
-        assert dag.name == "test_dag"
+        assert dag.name == "bqetl_test_dag"
         assert dag.schedule_interval == "daily"
         assert dag.default_args.owner == "test@example.com"
         assert dag.default_args.email == ["test@example.com"]
@@ -67,8 +67,26 @@ class TestDag:
 
     def test_from_dict_multiple_dags(self):
         with pytest.raises(DagParseException):
-            Dag.from_dict({"test_dag1": {}, "test_dag2": {}})
+            Dag.from_dict({"bqetl_test_dag1": {}, "bqetl_test_dag2": {}})
 
     def test_from_dict_without_scheduling_interval(self):
         with pytest.raises(DagParseException):
-            Dag.from_dict({"test_dag": {}})
+            Dag.from_dict({"bqetl_test_dag": {}})
+
+    def test_invalid_dag_name(self):
+        with pytest.raises(ValueError):
+            Dag("test_dag", "daily", self.default_args)
+
+    def test_schedule_interval_format(self):
+        assert Dag("bqetl_test_dag", "daily", self.default_args)
+        assert Dag("bqetl_test_dag", "weekly", self.default_args)
+        assert Dag("bqetl_test_dag", "once", self.default_args)
+
+        with pytest.raises(ValueError):
+            assert Dag("bqetl_test_dag", "never", self.default_args)
+
+        assert Dag("bqetl_test_dag", "* * * * *", self.default_args)
+        assert Dag("bqetl_test_dag", "1 * * * *", self.default_args)
+
+        with pytest.raises(TypeError):
+            assert Dag("bqetl_test_dag", 323, self.default_args)

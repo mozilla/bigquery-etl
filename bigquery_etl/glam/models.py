fa994948928cf390931a1bcdcb3cb3fb27c50d1d
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-05-18 15:46:47 -0700

    Use compact notation for static combinations

--- bigquery_etl/glam/models.py
+++ bigquery_etl/glam/models.py
@@ -114,12 +114,12 @@ def histogram_bucket_counts(**kwargs):
 def probe_counts(**kwargs):
     """Variables for probe counts."""
     attributes = ["ping_type", "os", "app_version", "app_build_id", "channel"]
-
+    fixed_attributes = ["app_version", "channel"]
+    cubed_attributes = [x for x in attributes if x not in fixed_attributes]
     return dict(
         attributes=attributes,
-        attribute_combinations=compute_datacube_groupings(
-            attributes, ["app_version", "channel"]
-        ),
+        cubed_attributes=cubed_attributes,
+        attribute_combinations=compute_datacube_groupings(cubed_attributes),
         aggregate_attributes="""
             metric,
             metric_type,
@@ -145,12 +145,14 @@ def probe_counts(**kwargs):
 def scalar_percentiles(**kwargs):
     """Variables for scalar percentiles."""
     attributes = ["ping_type", "os", "app_version", "app_build_id", "channel"]
+    fixed_attributes = ["app_version", "channel"]
+    cubed_attributes = [x for x in attributes if x not in fixed_attributes]
 
     return dict(
+        # TODO: be consistent with naming of attributes (e.g. attributes_list)
         attributes=attributes,
-        attribute_combinations=compute_datacube_groupings(
-            attributes, ["app_version", "channel"]
-        ),
+        cubed_attributes=cubed_attributes,
+        attribute_combinations=compute_datacube_groupings(cubed_attributes),
         aggregate_attributes="""
             metric,
             metric_type,
@@ -163,11 +165,12 @@ def scalar_percentiles(**kwargs):
 def user_counts(**kwargs):
     """Variables for user counts."""
     attributes = ["ping_type", "os", "app_version", "app_build_id", "channel"]
+    fixed_attributes = ["app_version", "channel"]
+    cubed_attributes = [x for x in attributes if x not in fixed_attributes]
 
     return dict(
         attributes=",".join(attributes),
-        attribute_combinations=compute_datacube_groupings(
-            attributes, ["app_version", "channel"]
-        ),
+        cubed_attributes=cubed_attributes,
+        attribute_combinations=compute_datacube_groupings(cubed_attributes),
         **kwargs,
     )
--- bigquery_etl/glam/templates/view_user_counts_v1.sql
+++ bigquery_etl/glam/templates/view_user_counts_v1.sql
@@ -1,4 +1,7 @@
 {{ header }}
+
+{% from 'macros.sql' import enumerate_table_combinations %}
+
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.{{ dataset }}.{{ prefix }}__view_user_counts_v1`
 AS
@@ -14,28 +17,19 @@ WITH all_clients AS (
     client_id,
     {{ attributes }}
   FROM `moz-fx-data-shared-prod`.{{ dataset }}.{{ prefix }}__clients_histogram_aggregates_v1
-)
-
-{% for attribute_combo in attribute_combinations %}
-    SELECT
-        {% for attribute, in_grouping in attribute_combo %}
-            {% if in_grouping %}
-                {{ attribute }},
-            {% else %}
-                NULL AS {{ attribute }},
-            {% endif %}
-        {% endfor %}
-        COUNT(DISTINCT client_id) as total_users
-    FROM
-        all_clients
-    GROUP BY
-        {% for attribute, in_grouping in attribute_combo if in_grouping %}
-            {{ attribute }}
-            {% if not loop.last %}
-                ,
-            {% endif %}
-        {% endfor %}
-    {% if not loop.last %}
-        UNION ALL
-    {% endif %}
-{% endfor %}
+),
+{{
+    enumerate_table_combinations(
+        "all_clients",
+        "all_combos",
+        cubed_attributes,
+        attribute_combinations
+    )
+}}
+SELECT
+    {{ attributes }},
+    COUNT(DISTINCT client_id) as total_users
+FROM
+    all_combos
+GROUP BY
+    {{ attributes }}

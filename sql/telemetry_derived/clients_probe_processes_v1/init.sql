19426b21f3b8c03ebbe3fce79640646f1771e7c0
Benjamin Wu <benjaminwu124@gmail.com>
2020-05-22 16:05:49 -0400

    Add query to get processes found for each probe in glam

---
+++ sql/telemetry_derived/clients_probe_processes_v1/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS
+  `moz-fx-data-shared-prod.telemetry_derived.client_probe_processes_v1`(
+    os STRING,
+    channel STRING,
+    metric STRING,
+    processes ARRAY<STRING>
+  )
---
+++ sql/telemetry_derived/clients_probe_processes_v1/query.sql
@@ -0,0 +1,14 @@
+SELECT
+  metric,
+  os,
+  channel,
+  ARRAY_AGG(DISTINCT(process)) AS processes,
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.client_probe_counts_v1`
+GROUP BY
+  metric,
+  os,
+  channel
+ORDER BY
+  metric,
+  os

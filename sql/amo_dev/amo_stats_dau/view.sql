56f2d88b6a2e582db0194fe6d4e4310840b809d2
Jeff Klukas <jeff@klukas.net>
2020-06-03 10:08:49 -0400

    Move AMO stats views to amo_prod and amo_dev

---
+++ sql/amo_dev/amo_stats_dau/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.amo_dev.amo_stats_dau`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.amo_dev.amo_stats_dau_v1`
---
+++ sql/amo_prod/amo_stats_dau/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.amo_prod.amo_stats_dau`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.amo_prod.amo_stats_dau_v1`

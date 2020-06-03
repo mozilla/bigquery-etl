5b906f859c1679bd76a5969b4fbc72276469611c
Anna Scholtz <anna@scholtzan.net>
2020-06-03 10:49:11 -0700

    Add version to error_aggregates query

---
+++ dags/bqetl_error_aggregates.py
@@ -0,0 +1,39 @@
+# Generated via query_scheduling/generate_airflow_dags
+
+from airflow import DAG
+from airflow.operators.sensors import ExternalTaskSensor
+import datetime
+from utils.gcp import bigquery_etl_query
+
+default_args = {
+    "owner": "bewu@mozilla.com",
+    "start_date": datetime.datetime(2019, 11, 1, 0, 0),
+    "email": [
+        "telemetry-alerts@mozilla.com",
+        "bewu@mozilla.com",
+        "wlachance@mozilla.com",
+    ],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1200),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 1,
+}
+
+with DAG(
+    "bqetl_error_aggregates",
+    default_args=default_args,
+    schedule_interval=datetime.timedelta(seconds=10800),
+) as dag:
+
+    telemetry_derived__error_aggregates__v1 = bigquery_etl_query(
+        task_id="telemetry_derived__error_aggregates__v1",
+        destination_table="error_aggregates_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="bewu@mozilla.com",
+        email=["bewu@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )

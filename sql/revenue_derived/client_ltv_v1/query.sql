70fe2f941d564308a9b544a66211f5755baec6fb
Frank Bertsch <fbertsch@mozilla.com>
2020-06-04 12:38:34 -0400

    Add normalization step to ltv query

--- sql/revenue_derived/client_ltv_v1/query.sql
+++ sql/revenue_derived/client_ltv_v1/query.sql
@@ -202,16 +202,29 @@ with_caps AS (
     cutoffs
   USING
     (engine, country)
-)
+), with_ltv AS (
+  SELECT
+    *,
+    ad_click_days * ad_clicks_per_day_capped * avg_client_ad_click_value AS ltv_ad_clicks_current,
+    search_with_ads_days * searches_with_ads_per_day_capped * avg_client_search_with_ads_value AS ltv_search_with_ads_current,
+    search_days * searches_per_day_capped * avg_client_search_value AS ltv_search_current,
+    tagged_search_days * tagged_searches_per_day_capped * avg_client_tagged_search_value AS ltv_tagged_search_current,
+    pred_num_days_clicking_ads * ad_clicks_per_day * avg_client_ad_click_value AS ltv_ad_clicks_future,
+    pred_num_days_seeing_ads * searches_with_ads_per_day_capped * avg_client_search_with_ads_value AS ltv_search_with_ads_future,
+    pred_num_days_searching * searches_per_day_capped * avg_client_search_value AS ltv_search_future,
+    pred_num_days_tagged_searching * tagged_searches_per_day_capped * avg_client_tagged_search_value AS ltv_tagged_search_future,
+  FROM
+    with_caps
+),
+
 SELECT
   *,
-  ad_click_days * ad_clicks_per_day_capped * avg_client_ad_click_value AS ltv_ad_clicks_current,
-  search_with_ads_days * searches_with_ads_per_day_capped * avg_client_search_with_ads_value AS ltv_search_with_ads_current,
-  search_days * searches_per_day_capped * avg_client_search_value AS ltv_search_current,
-  tagged_search_days * tagged_searches_per_day_capped * avg_client_tagged_search_value AS ltv_tagged_search_current,
-  pred_num_days_clicking_ads * ad_clicks_per_day * avg_client_ad_click_value AS ltv_ad_clicks_future,
-  pred_num_days_seeing_ads * searches_with_ads_per_day_capped * avg_client_search_with_ads_value AS ltv_search_with_ads_future,
-  pred_num_days_searching * searches_per_day_capped * avg_client_search_value AS ltv_search_future,
-  pred_num_days_tagged_searching * tagged_searches_per_day_capped * avg_client_tagged_search_value AS ltv_tagged_search_future,
+  SAFE_DIVIDE(ltv_ad_clicks_current, SUM(ltv_ad_clicks_current) OVER ()) AS normalized_ltv_ad_clicks_current,
+  SAFE_DIVIDE(ltv_search_with_ads_current, SUM(ltv_search_with_ads_current) OVER ()) AS normalized_ltv_search_with_ads_current,
+  SAFE_DIVIDE(ltv_search_current, SUM(ltv_search_current) OVER ()) AS normalized_ltv_search_current,
+  SAFE_DIVIDE(ltv_ad_clicks_future, SUM(ltv_ad_clicks_future) OVER ()) AS normalized_ltv_ad_clicks_future,
+  SAFE_DIVIDE(ltv_search_with_ads_future, SUM(ltv_search_with_ads_future) OVER ()) AS normalized_ltv_search_with_ads_future,
+  SAFE_DIVIDE(ltv_search_future, SUM(ltv_search_future) OVER ()) AS normalized_ltv_search_future,
+  SAFE_DIVIDE(ltv_tagged_search_future, SUM(ltv_tagged_search_future) OVER ()) AS normalized_ltv_tagged_search_future
 FROM
-  with_caps
+  with_ltv

69d68c55c38256ebc402c76bb90672831eaad2c3
Anna Scholtz <anna@scholtzan.net>
2020-06-08 13:04:50 -0700

    bqetl_mobile_search DAG

---
+++ dags/bqetl_mobile_search.py
@@ -0,0 +1,61 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/master/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.operators.sensors import ExternalTaskSensor
+import datetime
+from utils.gcp import bigquery_etl_query
+
+default_args = {
+    "owner": "bewu@mozilla.com",
+    "start_date": datetime.datetime(2019, 7, 25, 0, 0),
+    "email": ["telemetry-alerts@mozilla.com", "bewu@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=300),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 1,
+}
+
+with DAG(
+    "bqetl_mobile_search", default_args=default_args, schedule_interval="0 1 * * *"
+) as dag:
+
+    search_derived__mobile_search_clients_daily__v1 = bigquery_etl_query(
+        task_id="search_derived__mobile_search_clients_daily__v1",
+        destination_table="mobile_search_clients_daily_v1",
+        dataset_id="search_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="bewu@mozilla.com",
+        email=["bewu@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
+    search_derived__mobile_search_aggregates__v1 = bigquery_etl_query(
+        task_id="search_derived__mobile_search_aggregates__v1",
+        destination_table="mobile_search_aggregates_v1",
+        dataset_id="search_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="bewu@mozilla.com",
+        email=["bewu@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
+    wait_for_copy_deduplicate_copy_deduplicate_all = ExternalTaskSensor(
+        task_id="wait_for_copy_deduplicate_copy_deduplicate_all",
+        external_dag_id="copy_deduplicate",
+        external_task_id="copy_deduplicate_all",
+        check_existence=True,
+        dag=dag,
+    )
+
+    search_derived__mobile_search_clients_daily__v1.set_upstream(
+        wait_for_copy_deduplicate_copy_deduplicate_all
+    )
+
+    search_derived__mobile_search_aggregates__v1.set_upstream(
+        search_derived__mobile_search_clients_daily__v1
+    )

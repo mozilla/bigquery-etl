0a00cadb956c2e6334d05f56ed647b0128335487
Anna Scholtz <anna@scholtzan.net>
2020-06-08 16:22:51 -0700

    Move smoot queries from main_summary to bqetl_gud

--- dags/bqetl_gud.py
+++ dags/bqetl_gud.py
@@ -18,9 +18,33 @@ default_args = {
 
 with DAG("bqetl_gud", default_args=default_args, schedule_interval="0 1 * * *") as dag:
 
+    telemetry_derived__smoot_usage_desktop__v2 = bigquery_etl_query(
+        task_id="telemetry_derived__smoot_usage_desktop__v2",
+        destination_table="smoot_usage_desktop_v2",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="jklukas@mozilla.com",
+        email=["jklukas@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
     telemetry_derived__smoot_usage_new_profiles__v2 = bigquery_etl_query(
         task_id="telemetry_derived__smoot_usage_new_profiles__v2",
         destination_table="smoot_usage_new_profiles_v2",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="jklukas@mozilla.com",
+        email=["jklukas@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
+    telemetry_derived__smoot_usage_desktop_compressed__v2 = bigquery_etl_query(
+        task_id="telemetry_derived__smoot_usage_desktop_compressed__v2",
+        destination_table="smoot_usage_desktop_compressed_v2",
         dataset_id="telemetry_derived",
         project_id="moz-fx-data-shared-prod",
         owner="jklukas@mozilla.com",
@@ -66,8 +90,28 @@ with DAG("bqetl_gud", default_args=default_args, schedule_interval="0 1 * * *") 
         dag=dag,
     )
 
+    wait_for_main_summary_clients_last_seen = ExternalTaskSensor(
+        task_id="wait_for_main_summary_clients_last_seen",
+        external_dag_id="main_summary",
+        external_task_id="clients_last_seen",
+        check_existence=True,
+        dag=dag,
+    )
+
+    telemetry_derived__smoot_usage_desktop__v2.set_upstream(
+        wait_for_main_summary_clients_last_seen
+    )
+
+    telemetry_derived__smoot_usage_new_profiles__v2.set_upstream(
+        telemetry_derived__smoot_usage_desktop__v2
+    )
+
     telemetry_derived__smoot_usage_new_profiles__v2.set_upstream(
         telemetry_derived__smoot_usage_nondesktop__v2
+    )
+
+    telemetry_derived__smoot_usage_desktop_compressed__v2.set_upstream(
+        telemetry_derived__smoot_usage_desktop__v2
     )
 
     telemetry_derived__smoot_usage_new_profiles_compressed__v2.set_upstream(

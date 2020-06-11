9575f7b6482943e8233807f4285d1679e11377d0
Anna Scholtz <anna@scholtzan.net>
2020-06-11 15:00:52 -0700

    Fix UDF formatting

--- mozfun/map/get_key_with_null/udf.sql
+++ mozfun/map/get_key_with_null/udf.sql
@@ -7,19 +7,28 @@ of structs instead, and this function provides map-like access to such fields.
 This version matches NULL keys as well.
 
 */
-
 CREATE OR REPLACE FUNCTION map.get_key_with_null(map ANY TYPE, k ANY TYPE) AS (
- (
-   SELECT key_value.value
-   FROM UNNEST(map) AS key_value
-   WHERE key_value.key = k
-         OR key_value.key IS NULL and k IS NULL
-   LIMIT 1
- )
+  (
+    SELECT
+      key_value.value
+    FROM
+      UNNEST(map) AS key_value
+    WHERE
+      key_value.key = k
+      OR key_value.key IS NULL
+      AND k IS NULL
+    LIMIT
+      1
+  )
 );
 
 -- Tests
-
 SELECT
   assert_equals(12, map.get_key_with_null([STRUCT('foo' AS key, 42 AS value), ('bar', 12)], 'bar')),
-  assert_equals(12, map.get_key_with_null([STRUCT('foo' AS key, 42 AS value), (CAST(NULL AS STRING), 12)], CAST(NULL AS STRING)));
+  assert_equals(
+    12,
+    map.get_key_with_null(
+      [STRUCT('foo' AS key, 42 AS value), (CAST(NULL AS STRING), 12)],
+      CAST(NULL AS STRING)
+    )
+  );

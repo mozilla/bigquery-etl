3536151883c4525705f3ec89e6f4f5e4b9036520
Jeff Klukas <jeff@klukas.net>
2020-06-12 11:58:44 -0400

    Remove deprecated amo_stats_dau_v1 queries

--- dags/bqetl_amo_stats.py
+++ dags/bqetl_amo_stats.py
@@ -20,18 +20,6 @@ with DAG(
     "bqetl_amo_stats", default_args=default_args, schedule_interval="0 1 * * *"
 ) as dag:
 
-    amo_dev__amo_stats_dau__v1 = bigquery_etl_query(
-        task_id="amo_dev__amo_stats_dau__v1",
-        destination_table="amo_stats_dau_v1",
-        dataset_id="amo_dev",
-        project_id="moz-fx-data-shared-prod",
-        owner="jklukas@mozilla.com",
-        email=["jklukas@mozilla.com"],
-        date_partition_parameter="submission_date",
-        depends_on_past=False,
-        dag=dag,
-    )
-
     amo_dev__amo_stats_installs__v1 = bigquery_etl_query(
         task_id="amo_dev__amo_stats_installs__v1",
         destination_table="amo_stats_installs_v1",
@@ -48,18 +36,6 @@ with DAG(
         task_id="amo_dev__amo_stats_dau__v2",
         destination_table="amo_stats_dau_v2",
         dataset_id="amo_dev",
-        project_id="moz-fx-data-shared-prod",
-        owner="jklukas@mozilla.com",
-        email=["jklukas@mozilla.com"],
-        date_partition_parameter="submission_date",
-        depends_on_past=False,
-        dag=dag,
-    )
-
-    amo_prod__amo_stats_dau__v1 = bigquery_etl_query(
-        task_id="amo_prod__amo_stats_dau__v1",
-        destination_table="amo_stats_dau_v1",
-        dataset_id="amo_prod",
         project_id="moz-fx-data-shared-prod",
         owner="jklukas@mozilla.com",
         email=["jklukas@mozilla.com"],
@@ -104,8 +80,6 @@ with DAG(
         dag=dag,
     )
 
-    amo_dev__amo_stats_dau__v1.set_upstream(amo_prod__amo_stats_dau__v1)
-
     amo_dev__amo_stats_installs__v1.set_upstream(amo_prod__amo_stats_installs__v1)
 
     amo_dev__amo_stats_dau__v2.set_upstream(amo_prod__amo_stats_dau__v2)
@@ -118,8 +92,6 @@ with DAG(
         mode="reschedule",
         dag=dag,
     )
-
-    amo_prod__amo_stats_dau__v1.set_upstream(wait_for_main_summary_clients_daily)
 
     amo_prod__amo_stats_installs__v1.set_upstream(wait_for_main_summary_clients_daily)
 

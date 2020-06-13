8006c1fde2ed8ec34dacf83d8a2cdf01d57fb4b2
Jeff Klukas <jeff@klukas.net>
2020-06-13 09:16:20 -0400

    Fully qualify UDF references
    Fixes #1062

--- sql/search/search_aggregates/view.sql
+++ sql/search/search_aggregates/view.sql
@@ -35,7 +35,7 @@ UNION ALL
 SELECT
   submission_date AS submission_date_s3,
   * EXCEPT (normalized_engine),
-  udf.normalize_search_engine(engine) AS normalized_engine,
+  `moz-fx-data-shared-prod`.udf.normalize_search_engine(engine) AS normalized_engine,
 FROM
   `moz-fx-data-shared-prod.search_derived.search_aggregates_v8`
 WHERE

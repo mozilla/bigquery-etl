e9f924be4b1d496682017f949643b6754492218f
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-06-16 10:56:21 -0700

    Add command line interface via click

--- etl-graph/__main__.py
+++ etl-graph/__main__.py
@@ -1,18 +1,30 @@
 from pathlib import Path
 
+import click
+
 from .config import *
 from .crawler import fetch_dataset_listing, fetch_table_listing, resolve_view_references
 from .utils import ensure_folder, ndjson_load, print_json, run
 
-run(f"gsutil ls gs://{BUCKET}")
-run(f"bq ls {PROJECT}:{DATASET}")
 
-data_root = ensure_folder(Path(__file__).parent.parent / "data")
-project = "moz-fx-data-shared-prod"
+@click.group()
+def cli():
+    pass
 
-dataset_listing = fetch_dataset_listing(project, data_root)
-tables_listing = fetch_table_listing(dataset_listing, data_root / project)
-# tables_listing = ndjson_load(data_root / project / "tables_listing.ndjson")
 
-views_listing = [row for row in tables_listing if row["table_type"] == "VIEW"]
-resolve_view_references(views_listing, data_root / project)
+@cli.command()
+def crawl():
+    run(f"gsutil ls gs://{BUCKET}")
+    run(f"bq ls {PROJECT}:{DATASET}")
+
+    data_root = ensure_folder(Path(__file__).parent.parent / "data")
+    project = "moz-fx-data-shared-prod"
+    dataset_listing = fetch_dataset_listing(project, data_root)
+    tables_listing = fetch_table_listing(dataset_listing, data_root / project)
+    # tables_listing = ndjson_load(data_root / project / "tables_listing.ndjson")
+
+    views_listing = [row for row in tables_listing if row["table_type"] == "VIEW"]
+    resolve_view_references(views_listing, data_root / project)
+
+
+cli()

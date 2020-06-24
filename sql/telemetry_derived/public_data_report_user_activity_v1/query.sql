0a543b3e3ce5c32f867bf1a7dfc302eb59a054f7
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2020-06-24 15:43:37 +0200

    Simplify query

--- sql/telemetry_derived/public_data_report_user_activity_v1/query.sql
+++ sql/telemetry_derived/public_data_report_user_activity_v1/query.sql
@@ -1,31 +1,32 @@
-WITH sample AS (
+WITH countries AS (
+  SELECT
+    code,
+    name
+  FROM
+    `moz-fx-data-shared-prod.static.country_names_v1`
+),
+sample AS (
   SELECT
     submission_date,
     DATE_TRUNC(submission_date, WEEK(MONDAY)) AS week_start,
     EXTRACT(DAYOFWEEK FROM submission_date) = 1 AS is_last_day_of_week,
     days_since_seen,
-    country,
+    COALESCE(cn.name, country_group) AS country_name,
     subsession_hours_sum,
     days_seen_bits,
     days_created_profile_bits,
     client_id,
     app_version
   FROM
-    `moz-fx-data-shared-prod.telemetry.clients_last_seen`
+    `moz-fx-data-shared-prod.telemetry.clients_last_seen`,
+    UNNEST([country, 'Worldwide']) AS country_group
+  LEFT JOIN
+    countries AS cn
+  ON
+    cn.code = country_group
   WHERE
-    -- we need the whole week for daily_usage metric
-    -- others can look at just the last day (Sunday, see `is_last_day_of_week` above)
-    submission_date >= @submission_date
-    AND submission_date < DATE_ADD(@submission_date, INTERVAL 7 DAY)
-),
-countries AS (
-  SELECT
-    code,
-    name
-  FROM
-    `moz-fx-data-shared-prod.static.country_names_v1`
-  WHERE
-    name IN (
+    COALESCE(cn.name, country_group) IN (
+      'Worldwide',
       'Brazil',
       'China',
       'France',
@@ -37,30 +38,19 @@ countries AS (
       'Russia',
       'United States'
     )
+    -- we need the whole week for daily_usage metric
+    -- others can look at just the last day (Sunday, see `is_last_day_of_week` above)
+    AND submission_date >= @submission_date
+    AND submission_date < DATE_ADD(@submission_date, INTERVAL 7 DAY)
+    AND subsession_hours_sum < 24 --remove outliers
 ),
 mau AS (
   SELECT
     week_start,
-    'Worldwide' AS country_name,
+    country_name,
     count(*) AS MAU
   FROM
     sample
-  WHERE
-    is_last_day_of_week
-    AND days_since_seen < 28
-  GROUP BY
-    week_start
-  UNION ALL
-  SELECT
-    week_start,
-    cn.name AS country_name,
-    count(*) AS MAU
-  FROM
-    sample
-  LEFT JOIN
-    countries AS cn
-  ON
-    cn.code = country
   WHERE
     is_last_day_of_week
     AND days_since_seen < 28
@@ -70,7 +60,8 @@ mau AS (
 ),
 avg_daily_usage_by_user AS (
   SELECT
-    country,
+    client_id,
+    country_name,
     avg(subsession_hours_sum) AS avg_hours_usage_daily_per_user,
     week_start
   FROM
@@ -78,64 +69,33 @@ avg_daily_usage_by_user AS (
   WHERE
     days_since_seen = 0
   GROUP BY
-    week_start,
-    country
+    client_id,
+    country_name,
+    week_start
   HAVING
     avg_hours_usage_daily_per_user < 24 --remove outliers
 ),
 daily_usage AS (
   SELECT
-    'Worldwide' AS country_name,
+    country_name,
     avg(avg_hours_usage_daily_per_user) AS avg_hours_usage_daily,
     week_start
   FROM
     avg_daily_usage_by_user
   GROUP BY
-    week_start
-  UNION ALL
-  SELECT
-    cn.name AS country_name,
-    avg(avg_hours_usage_daily_per_user) AS avg_hours_usage_daily,
-    week_start
-  FROM
-    avg_daily_usage_by_user
-  LEFT JOIN
-    countries AS cn
-  ON
-    cn.code = country
-  GROUP BY
-    name,
+    country_name,
     week_start
 ),
 intensity AS (
   SELECT
     week_start,
-    'Worldwide' AS country_name,
+    country_name,
     SAFE_DIVIDE(
       SUM(`moz-fx-data-shared-prod.udf.bitcount_lowest_7`(days_seen_bits)),
       count(*)
     ) AS intensity
   FROM
     sample
-  WHERE
-    is_last_day_of_week
-    AND days_since_seen < 7
-  GROUP BY
-    week_start
-  UNION ALL
-  SELECT
-    week_start,
-    cn.name AS country_name,
-    SAFE_DIVIDE(
-      SUM(`moz-fx-data-shared-prod.udf.bitcount_lowest_7`(days_seen_bits)),
-      count(*)
-    ) AS intensity
-  FROM
-    sample
-  LEFT JOIN
-    countries AS cn
-  ON
-    cn.code = country
   WHERE
     is_last_day_of_week
     AND days_since_seen < 7
@@ -145,36 +105,16 @@ intensity AS (
 ),
 new_profile_rate AS (
   SELECT
-    'Worldwide' AS country_name,
-    100 * countif(
-      `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(days_created_profile_bits) < 7
-    ) / -- new profiles
-    countif(
-      `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(days_seen_bits) < 7
+    country_name,
+    SAFE_DIVIDE(
+      100 * COUNTIF(
+        `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(days_created_profile_bits) < 7
+      ), -- new profiles
+      COUNTIF(`moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(days_seen_bits) < 7)
     ) AS new_profile_rate, -- active profiles
     week_start
   FROM
     sample
-  WHERE
-    is_last_day_of_week
-  GROUP BY
-    week_start
-  UNION ALL
-  SELECT
-    cn.name AS country_name,
-    100 * countif(
-      `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(days_created_profile_bits) < 7
-    ) / -- new profiles
-    countif(
-      `moz-fx-data-shared-prod.udf.pos_of_trailing_set_bit`(days_seen_bits) < 7
-    ) AS new_profile_rate, -- active profiles
-    week_start
-  FROM
-    sample
-  LEFT JOIN
-    countries AS cn
-  ON
-    cn.code = country
   WHERE
     is_last_day_of_week
   GROUP BY
@@ -183,7 +123,7 @@ new_profile_rate AS (
 ),
 active_clients_weekly AS (
   SELECT
-    country,
+    country_name,
     client_id,
     split(app_version, '.')[offset(0)] AS major_version,
     date_sub(submission_date, INTERVAL days_since_seen DAY) AS last_day_seen,
@@ -209,7 +149,7 @@ latest_releases AS (
 active_clients_with_latest_releases AS (
   SELECT
     client_id,
-    country,
+    country_name,
     major_version,
     max(latest_major_version) AS latest_major_version,
     week_start
@@ -223,30 +163,17 @@ active_clients_with_latest_releases AS (
     client_id IS NOT NULL
   GROUP BY
     client_id,
-    country,
+    country_name,
     major_version,
     week_start
 ),
 latest_version_ratio AS (
   SELECT
-    'Worldwide' AS country_name,
-    countif(major_version = latest_major_version) / count(*) AS latest_version_ratio,
+    country_name,
+    SAFE_DIVIDE(COUNTIF(major_version = latest_major_version), count(*)) AS latest_version_ratio,
     week_start
   FROM
     active_clients_with_latest_releases
-  GROUP BY
-    week_start
-  UNION ALL
-  SELECT
-    cn.name AS country_name,
-    countif(major_version = latest_major_version) / count(*) AS latest_version_ratio,
-    week_start
-  FROM
-    active_clients_with_latest_releases
-  LEFT JOIN
-    countries AS cn
-  ON
-    cn.code = country
   GROUP BY
     country_name,
     week_start
@@ -263,23 +190,20 @@ FROM
   mau
 JOIN
   daily_usage
-ON
-  mau.week_start = daily_usage.week_start
-  AND mau.country_name = daily_usage.country_name
+USING
+  (week_start, country_name)
 JOIN
   intensity
-ON
-  mau.week_start = intensity.week_start
-  AND mau.country_name = intensity.country_name
+USING
+  (week_start, country_name)
 JOIN
   new_profile_rate
-ON
-  mau.week_start = new_profile_rate.week_start
-  AND mau.country_name = new_profile_rate.country_name
+USING
+  (week_start, country_name)
 JOIN
   latest_version_ratio
-ON
-  mau.week_start = latest_version_ratio.week_start
-  AND mau.country_name = latest_version_ratio.country_name
+USING
+  (week_start, country_name)
 ORDER BY
+  week_start,
   country_name

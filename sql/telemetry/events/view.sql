mozilla/bigquery-etl

commit 6969b83f4982734f4f7cc363809199c0eba1ada8

Remove all references to derived-datasets in views (#1092)
These references are all to views that existed in both projects or to
static tables that have been copied into shared-prod.

sql/telemetry/events/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.events_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.events_v1`

sql/telemetry/clients_daily/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.clients_daily_v6`
+  `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`

sql/telemetry/crash_aggregates/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.crash_aggregates_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.crash_aggregates_v1`

sql/telemetry/crash_summary/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.crash_summary_v2`
+  `moz-fx-data-shared-prod.telemetry_derived.crash_summary_v2`

sql/telemetry/eng_workflow_build_parquet/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.eng_workflow_build_parquet_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.eng_workflow_build_parquet_v1`

sql/telemetry/experiments/view.sql
@@ -188,4 +188,4 @@ SELECT
     scalar_parent_webrtc_peerconnection_connected
   )
 FROM
-  `moz-fx-data-derived-datasets.telemetry.experiments_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.experiments_v1`

sql/telemetry/experiments_aggregates/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.experiments_aggregates_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.experiments_aggregates_v1`

sql/telemetry/first_shutdown_summary/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.first_shutdown_summary_v4`
+  `moz-fx-data-shared-prod.telemetry_derived.first_shutdown_summary_v4`

commit 0b6a4f157782048353faa897b308150f07818907

Fix to events view
Fixes a regression introduced in https://github.com/mozilla/bigquery-etl/pull/1092

sql/telemetry/events/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.events_v1`
+  `moz-fx-data-shared-prod.telemetry.events_v1`
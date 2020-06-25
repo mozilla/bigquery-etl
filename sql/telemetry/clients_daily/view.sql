mozilla/bigquery-etl

commit 6969b83f4982734f4f7cc363809199c0eba1ada8

Remove all references to derived-datasets in views (#1092)
These references are all to views that existed in both projects or to
static tables that have been copied into shared-prod.

sql/telemetry/clients_daily/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.clients_daily_v6`
+  `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`

sql/telemetry/crash_aggregates/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.crash_aggregates_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.crash_aggregates_v1`

sql/telemetry/crash_summary/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.crash_summary_v2`
+  `moz-fx-data-shared-prod.telemetry_derived.crash_summary_v2`

sql/telemetry/events/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.events_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.events_v1`

sql/telemetry/experiments/view.sql
@@ -188,4 +188,4 @@ SELECT
     scalar_parent_webrtc_peerconnection_connected
   )
 FROM
-  `moz-fx-data-derived-datasets.telemetry.experiments_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.experiments_v1`

sql/telemetry/experiments_aggregates/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.experiments_aggregates_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.experiments_aggregates_v1`

sql/telemetry/socorro_crash/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.socorro_crash_v2`
+  `moz-fx-data-shared-prod.telemetry_derived.socorro_crash_v2`

sql/telemetry/ssl_ratios/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-derived-datasets.telemetry.ssl_ratios_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.ssl_ratios_v1`

commit 371f47e55b9b7f4de9d2d1f223c94db12b9e4448

Fix view regressions (#1097)
* Fix to events view

Fixes a regression introduced in https://github.com/mozilla/bigquery-etl/pull/1092

* Additional regression fixes

sql/telemetry/clients_daily/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
+  `moz-fx-data-shared-prod.telemetry.clients_daily_v6`

sql/telemetry/crash_aggregates/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.crash_aggregates_v1`
+  `moz-fx-data-shared-prod.telemetry.crash_aggregates_v1`

sql/telemetry/crash_summary/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.crash_summary_v2`
+  `moz-fx-data-shared-prod.telemetry.crash_summary_v2`

sql/telemetry/events/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.events_v1`
+  `moz-fx-data-shared-prod.telemetry.events_v1`

sql/telemetry/experiments/view.sql
@@ -188,4 +188,4 @@ SELECT
     scalar_parent_webrtc_peerconnection_connected
   )
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.experiments_v1`
+  `moz-fx-data-shared-prod.telemetry.experiments_v1`

sql/telemetry/experiments_aggregates/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.experiments_aggregates_v1`
+  `moz-fx-data-shared-prod.telemetry.experiments_aggregates_v1`

sql/telemetry/socorro_crash/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.socorro_crash_v2`
+  `moz-fx-data-shared-prod.telemetry.socorro_crash_v2`

sql/telemetry/ssl_ratios/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.ssl_ratios_v1`
+  `moz-fx-data-shared-prod.telemetry.ssl_ratios_v1`

sql/telemetry/telemetry_core_parquet/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.telemetry_core_parquet_v3`
+  `moz-fx-data-shared-prod.telemetry.telemetry_core_parquet_v3`

sql/telemetry/telemetry_focus_event_parquet/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.telemetry_focus_event_parquet_v1`
+  `moz-fx-data-shared-prod.telemetry.telemetry_focus_event_parquet_v1`

sql/telemetry/telemetry_new_profile_parquet/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.telemetry_new_profile_parquet_v2`
+  `moz-fx-data-shared-prod.telemetry.telemetry_new_profile_parquet_v2`

sql/telemetry/telemetry_shield_study_parquet/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.telemetry_shield_study_parquet_v1`
+  `moz-fx-data-shared-prod.telemetry.telemetry_shield_study_parquet_v1`
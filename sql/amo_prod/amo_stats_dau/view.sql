64db2b4cc65c27a2da5d0cead5b0ed98c7435a74
Jeff Klukas <jeff@klukas.net>
2020-07-01 09:42:06 -0400

    Replace null keys with 'Unknown' in amo_stats_dau
    See https://github.com/mozilla/addons-server/issues/14790

--- sql/amo_prod/amo_stats_dau/view.sql
+++ sql/amo_prod/amo_stats_dau/view.sql
@@ -2,6 +2,43 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.amo_prod.amo_stats_dau`
 AS
 SELECT
-  *
+  submission_date,
+  addon_id,
+  dau,
+  ARRAY(
+    SELECT AS STRUCT
+      IFNULL(key, 'Unknown') AS key,
+      value
+    FROM
+      UNNEST(dau_by_addon_version)
+  ) AS dau_by_addon_version,
+  ARRAY(
+    SELECT AS STRUCT
+      IFNULL(key, 'Unknown') AS key,
+      value
+    FROM
+      UNNEST(dau_by_app_os)
+  ) AS dau_by_app_os,
+  ARRAY(
+    SELECT AS STRUCT
+      IFNULL(key, 'Unknown') AS key,
+      value
+    FROM
+      UNNEST(dau_by_app_version)
+  ) AS dau_by_app_version,
+  ARRAY(
+    SELECT AS STRUCT
+      IFNULL(key, 'Unknown') AS key,
+      value
+    FROM
+      UNNEST(dau_by_country)
+  ) AS dau_by_country,
+  ARRAY(
+    SELECT AS STRUCT
+      IFNULL(key, 'Unknown') AS key,
+      value
+    FROM
+      UNNEST(dau_by_locale)
+  ) AS dau_by_locale,
 FROM
   `moz-fx-data-shared-prod.amo_prod.amo_stats_dau_v2`

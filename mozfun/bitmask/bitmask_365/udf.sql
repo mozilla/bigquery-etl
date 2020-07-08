312e0ed21aa65912da06cb1ec28f5120dbdfce79
Anna Scholtz <anna@scholtzan.net>
2020-07-08 15:00:28 -0700

    Refactor migrate_to_mozfun script

--- mozfun/bitmask/bitmask_365/udf.sql
+++ mozfun/bitmask/bitmask_365/udf.sql
@@ -1,9 +1,6 @@
 /*
 A bitmask for 365 bits
 */
-
-CREATE OR REPLACE FUNCTION
-  mozfun.bitmask.bitmask_365() AS (
-    CONCAT(
-        b'\x1F',
-        REPEAT(b'\xFF', 45)));
+CREATE OR REPLACE FUNCTION mozfun.bitmask.bitmask_365() AS (
+  CONCAT(b'\x1F', REPEAT(b'\xFF', 45))
+);

e13ee14db76af331b9bf2281bd121e712d89b0a8
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-07-08 14:07:03 -0700

    Set build_date to null instead of defaulting to glob (#1145)

--- bigquery_etl/glam/templates/extract_probe_counts_v1.sql
+++ bigquery_etl/glam/templates/extract_probe_counts_v1.sql
@@ -18,7 +18,7 @@ SELECT
     ping_type,
     os,
     app_build_id as build_id,
-    COALESCE(CAST({{ build_date_udf }}(app_build_id) AS STRING), "*") as build_date,
+    SAFE_CAST({{ build_date_udf }}(app_build_id) AS STRING) as build_date,
     metric,
     metric_type,
     -- BigQuery has some null unicode characters which Postgresql doesn't like,

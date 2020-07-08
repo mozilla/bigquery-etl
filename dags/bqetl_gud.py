804ea6919a790022c3a4d449cd8e47112a879e52
Anna Scholtz <anna@scholtzan.net>
2020-07-08 13:02:40 -0700

    Fix schedule interval delta

--- dags/bqetl_gud.py
+++ dags/bqetl_gud.py
@@ -94,7 +94,7 @@ with DAG("bqetl_gud", default_args=default_args, schedule_interval="0 2 * * *") 
         task_id="wait_for_telemetry_derived__clients_last_seen__v1",
         external_dag_id="bqetl_clients_daily",
         external_task_id="telemetry_derived__clients_last_seen__v1",
-        execution_delta=datetime.timedelta(days=-1, seconds=82800),
+        execution_delta=datetime.timedelta(seconds=3600),
         check_existence=True,
         mode="reschedule",
     )

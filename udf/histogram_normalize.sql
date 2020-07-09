6d90a47bc5909f373ec8da308f677ae1947ad9e5
Anna Scholtz <anna@scholtzan.net>
2020-07-09 08:40:13 -0700

    Migrate some UDFs to mozfun

--- udf/histogram_normalize.sql
+++ udf/histogram_normalize.sql
@@ -1,7 +1,4 @@
-/*
-Normalize a histogram. Set sum to 1, and normalize to 1
-the histogram bucket counts.
-*/
+-- Legacy wrapper around a function moved to mozfun.
 CREATE OR REPLACE FUNCTION udf.histogram_normalize(
   histogram STRUCT<
     bucket_count INT64,
@@ -18,38 +15,5 @@ RETURNS STRUCT<
   `range` ARRAY<INT64>,
   `values` ARRAY<STRUCT<key INT64, value FLOAT64>>
 > AS (
-  STRUCT(
-    histogram.bucket_count AS bucket_count,
-    1 AS `sum`,
-    histogram.histogram_type AS histogram_type,
-    histogram.`range` AS `range`,
-    ARRAY(
-      SELECT AS STRUCT
-        key,
-        SAFE_DIVIDE(value, SUM(value) OVER ())
-      FROM
-        UNNEST(histogram.`values`)
-    ) AS `values`
-  )
+  mozfun.hist.normalize()
 );
-
--- Test
-SELECT
-  assert_histogram_equals(
-    STRUCT(
-      10 AS bucket_count,
-      1 AS `sum`,
-      1 AS histogram_type,
-      [0, 10] AS `range`,
-      [STRUCT(1 AS key, 0.5 AS value), STRUCT(2 AS key, 0.5 AS value)] AS `values`
-    ),
-    udf.histogram_normalize(
-      STRUCT(
-        10 AS bucket_count,
-        6 AS `sum`,
-        1 AS histogram_type,
-        [0, 10] AS `range`,
-        [STRUCT(1 AS key, 3 AS value), STRUCT(2 AS key, 3 AS value)] AS `values`
-      )
-    )
-  );
--- udf/histogram_percentiles.sql
+++ udf/histogram_percentiles.sql
@@ -1,73 +1,7 @@
-/*
-Given histogram and list of percentiles, calculate what those percentiles are for the histogram.
-
-If the histogram is empty, returns NULL.
-*/
-CREATE OR REPLACE FUNCTION udf.histogram_percentiles(histogram ANY TYPE, percentiles ARRAY<FLOAT64>)
-RETURNS ARRAY<STRUCT<percentile FLOAT64, value INT64>> AS (
-  ARRAY(
-    SELECT AS STRUCT
-      percentile,
-      MIN(key) AS value
-    FROM
-      (
-        SELECT AS STRUCT
-          key,
-          SAFE_DIVIDE(
-            1.0 * SUM(value) OVER (ORDER BY key),
-            SUM(value) OVER ()
-          ) AS cumulative_fraction
-        FROM
-          UNNEST(histogram.VALUES)
-      )
-    INNER JOIN
-      UNNEST(percentiles) AS percentile
-    ON
-      cumulative_fraction >= percentile
-    GROUP BY
-      percentile
-    ORDER BY
-      percentile ASC
-  )
+-- Legacy wrapper around a function moved to mozfun.
+CREATE OR REPLACE FUNCTION udf.histogram_percentiles(
+  histogram ANY TYPE,
+  percentiles ARRAY<FLOAT64>
+) AS (
+  mozfun.hist.percentiles(histogram ANY TYPE, percentiles ARRAY<FLOAT64>)
 );
-
-SELECT
-  assert_array_equals(
-    [10, 50, 90],
-    ARRAY(
-      SELECT
-        value
-      FROM
-        UNNEST(
-          udf.histogram_percentiles(
-            STRUCT(
-              ARRAY(
-                SELECT
-                  STRUCT(v AS key, 1 AS value)
-                FROM
-                  UNNEST(generate_array(1, 100)) AS v
-              ) AS values
-            ),
-            [.1, .5, .9]
-          )
-        )
-    )
-  ),
-  assert_array_equals(
-    [100],
-    ARRAY(
-      SELECT
-        value
-      FROM
-        UNNEST(
-          udf.histogram_percentiles(
-            STRUCT([STRUCT(99 AS key, 100 AS value), STRUCT(100, 101)] AS values),
-            [.5]
-          )
-        )
-    )
-  ),
-  assert_array_equals(
-    CAST(NULL AS ARRAY<STRUCT<percentile FLOAT64, value INT64>>),
-    udf.histogram_percentiles(STRUCT([STRUCT(0 AS key, 0 AS value)] AS values), [.2, .5])
-  );

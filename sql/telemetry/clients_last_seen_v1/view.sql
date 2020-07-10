99653370de9ca6da876f384acd9c98e8fb9662e0
Daniel Thorn <dthorn@mozilla.com>
2020-07-10 11:59:06 -0700

    Update last-seen views to use mozfun udfs

--- sql/telemetry/clients_last_seen_v1/view.sql
+++ sql/telemetry/clients_last_seen_v1/view.sql
@@ -2,17 +2,10 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.clients_last_seen_v1`
 AS
 SELECT
-  -- We cannot use UDFs in a view, so we paste the body of udf.bitpos(bits) literally here.
-  CAST(SAFE.LOG(days_seen_bits & -days_seen_bits, 2) AS INT64) AS days_since_seen,
-  CAST(
-    SAFE.LOG(days_visited_5_uri_bits & -days_visited_5_uri_bits, 2) AS INT64
-  ) AS days_since_visited_5_uri,
-  CAST(
-    SAFE.LOG(days_opened_dev_tools_bits & -days_opened_dev_tools_bits, 2) AS INT64
-  ) AS days_since_opened_dev_tools,
-  CAST(
-    SAFE.LOG(days_created_profile_bits & -days_created_profile_bits, 2) AS INT64
-  ) AS days_since_created_profile,
+  mozfun.bits28.days_since_seen(days_seen_bits) AS days_since_seen,
+  mozfun.bits28.days_since_seen(days_visited_5_uri_bits) AS days_since_visited_5_uri,
+  mozfun.bits28.days_since_seen(days_opened_dev_tools_bits) AS days_since_opened_dev_tools,
+  mozfun.bits28.days_since_seen(days_created_profile_bits) AS days_since_created_profile,
   -- Segment definitions; see https://docs.telemetry.mozilla.org/concepts/segments.html
   -- 0x0FFFFFFE is a bitmask that accepts the previous 27 days, excluding the current day (rightmost bit)
   -- 0x183060C183 == 0b000001100000110000011000001100000110000011 is a bit mask that accepts a pair of
@@ -81,7 +74,7 @@ SELECT
     ARRAY(
       SELECT AS STRUCT
         *,
-        CAST(SAFE.LOG(bits & -bits, 2) AS INT64) AS days_since_seen
+        mozfun.bits28.days_since_seen(bits) AS days_since_seen
       FROM
         UNNEST(days_seen_in_experiment)
     ) AS days_seen_in_experiment

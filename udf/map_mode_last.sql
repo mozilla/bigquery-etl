4ab4ae11c64c5772a66220ba37adc4e70198a2d9
Anna Scholtz <anna@scholtzan.net>
2020-07-14 14:50:26 -0700

    Migrate more UDFs

--- udf/map_mode_last.sql
+++ udf/map_mode_last.sql
@@ -1,17 +1,4 @@
-/*
-
-Combine entries from multiple maps, determine the value for each key using
-udf.mode_last.
-
-*/
+-- Legacy wrapper around a function moved to mozfun.
 CREATE OR REPLACE FUNCTION udf.map_mode_last(entries ANY TYPE) AS (
-  ARRAY(
-    SELECT AS STRUCT
-      key,
-      udf.mode_last(ARRAY_AGG(value)) AS value
-    FROM
-      UNNEST(entries)
-    GROUP BY
-      key
-  )
+  mozfun.map.mode_last(entries)
 );
--- udf/normalize_metadata.sql
+++ udf/normalize_metadata.sql
@@ -1,41 +1,4 @@
-/*
-
-Accepts a pipeline metadata struct as input and returns a modified struct that
-includes a few parsed or normalized variants of the input metadata fields.
-
-*/
-
-CREATE OR REPLACE FUNCTION
-  udf.normalize_metadata(metadata ANY TYPE) AS ((
-    SELECT
-      AS STRUCT metadata.* REPLACE ( (
-        SELECT
-          AS STRUCT metadata.header.*,
-            SAFE.PARSE_TIMESTAMP('%a, %d %b %Y %T %Z',
-              -- Even though it's not part of the spec, many clients append
-              -- '+00:00' to their Date headers, so we strip that suffix.
-              REPLACE(metadata.header.`date`, 'GMT+00:00', 'GMT')) AS parsed_date) AS header )) );
-
--- Tests
-
-SELECT
-assert_equals(
-  STRUCT(STRUCT(
-    'Thu, 21 Nov 2019 22:06:06 GMT' AS `date`,
-    TIMESTAMP '2019-11-21 22:06:06' AS parsed_date) AS header),
-  udf.normalize_metadata(
-    STRUCT(STRUCT(
-      'Thu, 21 Nov 2019 22:06:06 GMT' AS `date`) AS header))),
-assert_equals(
-  TIMESTAMP '2019-11-21 22:06:06',
-  udf.normalize_metadata(
-    STRUCT(STRUCT(
-    'Thu, 21 Nov 2019 22:06:06 GMT+00:00' AS `date`) AS header)).header.parsed_date),
-assert_null(
-  udf.normalize_metadata(
-    STRUCT(STRUCT(
-    'Thu, 21 Nov 2019 22:06:06 GMT-05:00' AS `date`) AS header)).header.parsed_date),
-assert_null(
-  udf.normalize_metadata(
-    STRUCT(STRUCT(
-    CAST(NULL AS STRING) AS `date`) AS header)).header.parsed_date);
+-- Legacy wrapper around a function moved to mozfun.
+CREATE OR REPLACE FUNCTION udf.normalize_metadata(metadata ANY TYPE) AS (
+  mozfun.metadata.normalize(metadata)
+);

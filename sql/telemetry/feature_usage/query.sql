61d0a55b2471c122851840cc3578854a8b816e2c
Jesse McCrosky <jmccrosky@mozilla.com>
2020-07-23 11:34:26 +0300

    Changes as recommended by reviewers.

--- sql/telemetry/feature_usage/query.sql
+++ sql/telemetry/feature_usage/query.sql
@@ -1,35 +1,42 @@
 SELECT
   events_t.client_id AS clients_id,
   events_t.submission_date AS submission_date,
-  COALESCE(protection_report, 0) AS protection_report,
-  COALESCE(pinned_tab, 0) AS pinned_tab,
-  COALESCE(default_browser, 0) AS default_browser,
-  COALESCE(many_uri, 0) AS many_uri,
-  COALESCE(pip, 0) AS pip,
-  COALESCE(filled_password, 0) AS filled_password,
-  COALESCE(remember_password, 0) AS remember_password,
-  COALESCE(sync_configured, 0) AS sync_configured
+  COALESCE(protection_report, FALSE) AS protection_report,
+  COALESCE(pinned_tab, FALSE) AS pinned_tab,
+  COALESCE(default_browser, FALSE) AS default_browser,
+  COALESCE(many_uri, FALSE) AS many_uri,
+  COALESCE(pip, FALSE) AS pip,
+  COALESCE(filled_password, FALSE) AS filled_password,
+  COALESCE(remember_password, FALSE) AS remember_password,
+  COALESCE(sync_configured, FALSE) AS sync_configured
 FROM
   (
     SELECT
       client_id,
       submission_date,
-      IF(event_method = 'show' AND event_object = 'protection_report', 1, 0) AS protection_report,
-      IF(event_category = 'pictureinpicture' AND event_method = 'create', 1, 0) AS pip,
+      LOGICAL_OR(event_method = 'show' AND event_object = 'protection_report') AS protection_report,
+      LOGICAL_OR(event_category = 'pictureinpicture' AND event_method = 'create') AS pip,
     FROM
       `moz-fx-data-shared-prod.telemetry.events`
     WHERE
       submission_date = @submission_date
-      AND normalized_channel = "release"
+    GROUP BY
+      submission_date,
+      client_id
   ) events_t
 FULL JOIN
   (
     SELECT
       client_id,
-      submission_date,
-      LOGICAL_OR(
-        IF(scalar_parent_browser_engagement_max_concurrent_tab_pinned_count > 0, 1, 0)
-      ) AS pinned_tab,
+      DATE(submission_timestamp) AS submission_date,
+      SUM(
+        payload.processes.parent.scalars.browser_engagement_max_concurrent_tab_pinned_count
+      ) > 0 AS pinned_tab,
+      LOGICAL_OR(environment.settings.is_default_browser) AS default_browser,
+      SUM(payload.processes.parent.scalars.browser_engagement_total_uri_count) > 10 AS many_uri,
+      SUM(
+        udf.histogram_max_key_with_nonzero_value(payload.histograms.weave_device_count_desktop)
+      ) > 0 AS sync_configured,
       LOGICAL_OR(
         0 IN (
           SELECT
@@ -37,96 +44,38 @@ FULL JOIN
           FROM
             UNNEST(
               ARRAY_CONCAT(
-                histogram_content_pwmgr_form_autofill_result,
-                histogram_parent_pwmgr_form_autofill_result
+                udf_null_if_empty_list(
+                  mozfun.hist.extract(
+                    payload.processes.content.histograms.pwmgr_form_autofill_result
+                  ).values
+                ),
+                udf_null_if_empty_list(
+                  mozfun.hist.extract(payload.histograms.pwmgr_form_autofill_result).values
+                )
               )
             )
         )
       ) AS filled_password,
+      LOGICAL_OR(
+        1 IN (
+          SELECT
+            key
+          FROM
+            UNNEST(
+              udf_null_if_empty_list(
+                mozfun.hist.extract(payload.histograms.pwmgr_prompt_remember_action).values
+              )
+            )
+        )
+      ) AS remember_password,
     FROM
-      `moz-fx-data-shared-prod.telemetry.main_summary`
+      `moz-fx-data-shared-prod.telemetry.main`
     WHERE
-      submission_date = @submission_date
-      AND normalized_channel = "release"
+      DATE(submission_timestamp) = @submission_date
     GROUP BY
       submission_date,
       client_id
-  ) ms_t
+  ) main_t
 ON
-  events_t.client_id = ms_t.client_id
-  AND events_t.submission_date = ms_t.submission_date
-FULL JOIN
-  (
-    SELECT
-      client_id,
-      submission_date,
-      IF(is_default_browser, 1, 0) AS default_browser,
-      IF(scalar_parent_browser_engagement_total_uri_count_sum > 10, 1, 0) AS many_uri,
-      IF(sync_count_desktop_sum > 0, 1, 0) AS sync_configured,
-    FROM
-      `moz-fx-data-shared-prod.telemetry.clients_daily`
-    WHERE
-      submission_date = @submission_date
-      AND normalized_channel = "release"
-  ) cd_t
-ON
-  events_t.client_id = cd_t.client_id
-  AND events_t.submission_date = cd_t.submission_date
-FULL JOIN
-  (
-    SELECT
-      client_id,
-      submission_date,
-      true AS filled_password
-    FROM
-      (
-        (
-          SELECT
-            client_id,
-            submission_date
-          FROM
-            `moz-fx-data-shared-prod.telemetry.main_summary` ms_t
-          CROSS JOIN
-            ms_t.histogram_content_pwmgr_form_autofill_result res_a
-          WHERE
-            submission_date = @submission_date
-            AND res_a.key = 0
-            AND normalized_channel = "release"
-        )
-        UNION ALL
-          (
-            SELECT
-              client_id,
-              submission_date
-            FROM
-              `moz-fx-data-shared-prod.telemetry.main_summary` ms_t
-            CROSS JOIN
-              ms_t.histogram_parent_pwmgr_form_autofill_result res_b
-            WHERE
-              submission_date = @submission_date
-              AND res_b.key = 0
-              AND normalized_channel = "release"
-          )
-      )
-  ) filled_password_t
-ON
-  events_t.client_id = filled_password_t.client_id
-  AND events_t.submission_date = filled_password_t.submission_date
-FULL JOIN
-  (
-    SELECT
-      client_id,
-      submission_date,
-      1 AS remember_password
-    FROM
-      `moz-fx-data-shared-prod.telemetry.main_summary` ms_t
-    CROSS JOIN
-      ms_t.histogram_parent_pwmgr_prompt_remember_action res_a
-    WHERE
-      submission_date = @submission_date
-      AND res_a.key = 1
-      AND normalized_channel = "release"
-  ) remember_password_t
-ON
-  events_t.client_id = remember_password_t.client_id
-  AND events_t.submission_date = remember_password_t.submission_date
+  events_t.client_id = main_t.client_id
+  AND events_t.submission_date = main_t.submission_date

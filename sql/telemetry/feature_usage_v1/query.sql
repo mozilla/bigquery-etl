8f15a7c86346c21e51e8203c97fbe99ee8dc650b
Jesse McCrosky <jmccrosky@mozilla.com>
2020-07-24 08:33:59 +0300

    Add value > 0 retriction for remembered_password subquery.

--- sql/telemetry/feature_usage_v1/query.sql
+++ sql/telemetry/feature_usage_v1/query.sql
@@ -1,21 +1,24 @@
 SELECT
   client_id,
   submission_date,
-  COALESCE(protection_report, FALSE) AS protection_report,
-  COALESCE(pinned_tab, FALSE) AS pinned_tab,
+  COALESCE(viewed_protection_report, FALSE) AS viewed_protection_report,
+  COALESCE(has_pinned_tab, FALSE) AS has_pinned_tab,
   COALESCE(default_browser, FALSE) AS default_browser,
-  COALESCE(many_uri, FALSE) AS many_uri,
-  COALESCE(pip, FALSE) AS pip,
-  COALESCE(filled_password, FALSE) AS filled_password,
-  COALESCE(remember_password, FALSE) AS remember_password,
+  COALESCE(visited_10_uri, FALSE) AS visited_10_uri,
+  COALESCE(used_pip, FALSE) AS used_pip,
+  COALESCE(filled_password_automatically, FALSE) AS filled_password_automatically,
+  COALESCE(remembered_password, FALSE) AS remembered_password,
   COALESCE(sync_configured, FALSE) AS sync_configured
 FROM
   (
     SELECT
       client_id,
       submission_date,
-      LOGICAL_OR(event_method = 'show' AND event_object = 'protection_report') AS protection_report,
-      LOGICAL_OR(event_category = 'pictureinpicture' AND event_method = 'create') AS pip,
+      LOGICAL_OR(
+        event_method = 'show'
+        AND event_object = 'protection_report'
+      ) AS viewed_protection_report,
+      LOGICAL_OR(event_category = 'pictureinpicture' AND event_method = 'create') AS used_pip,
     FROM
       `moz-fx-data-shared-prod.telemetry.events`
     WHERE
@@ -31,9 +34,11 @@ FULL JOIN
       DATE(submission_timestamp) AS submission_date,
       SUM(
         payload.processes.parent.scalars.browser_engagement_max_concurrent_tab_pinned_count
-      ) > 0 AS pinned_tab,
+      ) > 0 AS has_pinned_tab,
       LOGICAL_OR(environment.settings.is_default_browser) AS default_browser,
-      SUM(payload.processes.parent.scalars.browser_engagement_total_uri_count) > 10 AS many_uri,
+      SUM(
+        payload.processes.parent.scalars.browser_engagement_total_uri_count
+      ) > 10 AS visited_10_uri,
       SUM(
         `moz-fx-data-shared-prod.udf.histogram_max_key_with_nonzero_value`(
           payload.histograms.weave_device_count_desktop
@@ -61,15 +66,17 @@ FULL JOIN
           WHERE
             value > 0
         )
-      ) AS filled_password,
+      ) AS filled_password_automatically,
       LOGICAL_OR(
         1 IN (
           SELECT
             key
           FROM
             UNNEST(mozfun.hist.extract(payload.histograms.pwmgr_prompt_remember_action).values)
+          WHERE
+            value > 0
         )
-      ) AS remember_password,
+      ) AS remembered_password,
     FROM
       `moz-fx-data-shared-prod.telemetry.main`
     WHERE

306c667b2d05a2beba707e76e07d887ead875dfa
asiOvOtus <40037781+asiOvOtus@users.noreply.github.com>
2020-07-29 02:54:44 +0800

    Add unit tests and documentations for udfs (#1197)
    * Add unit tests and documentations for udfs
    
    * Auto format SQL files
    
    * fix and format
    
    Co-authored-by: Frank Bertsch <fbertsch@mozilla.com>

--- udf/shift_one_day.sql
+++ udf/shift_one_day.sql
@@ -1,3 +1,11 @@
-CREATE OR REPLACE FUNCTION
-  udf.shift_one_day(x INT64) AS (IFNULL((x << 1) & udf.bitmask_lowest_28(),
-	0));
+/*
+Returns the bitfield shifted by one day, 0 for NULL
+*/
+CREATE OR REPLACE FUNCTION udf.shift_one_day(x INT64) AS (
+  IFNULL((x << 1) & udf.bitmask_lowest_28(), 0)
+);
+
+-- Tests
+SELECT
+  assert_equals(4, udf.shift_one_day(2)),
+  assert_equals(0, udf.shift_one_day(NULL))

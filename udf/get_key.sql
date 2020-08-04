2acb30c9b044097e24607ad6a5c65cc9d632036a
asiOvOtus <40037781+asiOvOtus@users.noreply.github.com>
2020-08-05 04:26:13 +0800

    Rewrite duplicated map udfs to mozfun shims (#1211)
    * Rewrite duplicated map udfs to mozfun shims
    
    * Format get_key_with_null.sql

--- udf/get_key.sql
+++ udf/get_key.sql
@@ -1,12 +1,6 @@
-/*
-Fetch the value associated with a given key from an array of key/value structs.
-
-Because map types aren't available in BigQuery, we model maps as arrays
-of structs instead, and this function provides map-like access to such fields.
-
-*/
+-- Legacy wrapper around a function moved to mozfun.
 CREATE OR REPLACE FUNCTION udf.get_key(map ANY TYPE, k ANY TYPE) AS (
-  (SELECT key_value.value FROM UNNEST(map) AS key_value WHERE key_value.key = k LIMIT 1)
+  mozfun.map.get_key(map, k)
 );
 
 -- Tests
--- udf/get_key_with_null.sql
+++ udf/get_key_with_null.sql
@@ -1,25 +1,15 @@
-/*
-Fetch the value associated with a given key from an array of key/value structs.
-
-Because map types aren't available in BigQuery, we model maps as arrays
-of structs instead, and this function provides map-like access to such fields.
-
-This version matches NULL keys as well.
-
-*/
-
+-- Legacy wrapper around a function moved to mozfun.
 CREATE OR REPLACE FUNCTION udf.get_key_with_null(map ANY TYPE, k ANY TYPE) AS (
- (
-   SELECT key_value.value
-   FROM UNNEST(map) AS key_value
-   WHERE key_value.key = k
-         OR key_value.key IS NULL and k IS NULL
-   LIMIT 1
- )
+  mozfun.map.get_key_with_null(map, k)
 );
 
 -- Tests
-
 SELECT
   assert_equals(12, udf.get_key_with_null([STRUCT('foo' AS key, 42 AS value), ('bar', 12)], 'bar')),
-  assert_equals(12, udf.get_key_with_null([STRUCT('foo' AS key, 42 AS value), (CAST(NULL AS STRING), 12)], CAST(NULL AS STRING)));
+  assert_equals(
+    12,
+    udf.get_key_with_null(
+      [STRUCT('foo' AS key, 42 AS value), (CAST(NULL AS STRING), 12)],
+      CAST(NULL AS STRING)
+    )
+  );

8f273b197352cd186846e54e944300c18e0b8ac5
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-08-18 14:49:59 -0700

    Use UNION ALL instead of UNION

--- bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_beta__view_clients_daily_scalar_aggregates_v1.sql
+++ bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_beta__view_clients_daily_scalar_aggregates_v1.sql
@@ -7,7 +7,7 @@ FROM
   `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix__view_clients_daily_scalar_aggregates_v1
 WHERE
   `moz-fx-data-shared-prod`.udf.fenix_build_to_datetime(app_build_id) < date "2020-07-03"
-UNION
+UNION ALL
 SELECT
   *
 FROM
--- bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_nightly__view_clients_daily_scalar_aggregates_v1.sql
+++ bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_nightly__view_clients_daily_scalar_aggregates_v1.sql
@@ -7,12 +7,12 @@ FROM
   `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix__view_clients_daily_scalar_aggregates_v1
 WHERE
   `moz-fx-data-shared-prod`.udf.fenix_build_to_datetime(app_build_id) >= date "2020-07-03"
-UNION
+UNION ALL
 SELECT
   *
 FROM
   `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix_nightly__view_clients_daily_scalar_aggregates_v1
-UNION
+UNION ALL
 SELECT
   *
 FROM

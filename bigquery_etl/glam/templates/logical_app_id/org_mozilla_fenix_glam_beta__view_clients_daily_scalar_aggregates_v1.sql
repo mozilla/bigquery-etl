3cfc51f6d88aee814af5d14154858876f1a294d0
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-08-18 15:07:15 -0700

    Replace channel with *

--- bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_beta__view_clients_daily_scalar_aggregates_v1.sql
+++ bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_beta__view_clients_daily_scalar_aggregates_v1.sql
@@ -1,14 +1,21 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix_glam_beta__view_clients_daily_scalar_aggregates_v1
 AS
+WITH extracted AS (
+  SELECT
+    *
+  FROM
+    `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix__view_clients_daily_scalar_aggregates_v1
+  WHERE
+    `moz-fx-data-shared-prod`.udf.fenix_build_to_datetime(app_build_id) < date "2020-07-03"
+  UNION ALL
+  SELECT
+    *
+  FROM
+    `moz-fx-data-shared-prod`.glam_etl.org_mozilla_firefox_beta__view_clients_daily_scalar_aggregates_v1
+)
 SELECT
-  *
+  * EXCEPT (channel),
+  "*" AS channel
 FROM
-  `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix__view_clients_daily_scalar_aggregates_v1
-WHERE
-  `moz-fx-data-shared-prod`.udf.fenix_build_to_datetime(app_build_id) < date "2020-07-03"
-UNION ALL
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod`.glam_etl.org_mozilla_firefox_beta__view_clients_daily_scalar_aggregates_v1
+  extracted

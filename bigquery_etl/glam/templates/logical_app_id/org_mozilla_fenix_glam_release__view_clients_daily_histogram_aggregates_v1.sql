96b85854d2fd34be4b01ae20460c042d2190721b
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-08-18 16:15:56 -0700

    Update Glam ETL for Fenix (#1240)
    * Use UNION ALL instead of UNION
    
    * Move tests into separate directory and add test for all fenix products
    
    * Replace channel with *

--- bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_release__view_clients_daily_histogram_aggregates_v1.sql
+++ bigquery_etl/glam/templates/logical_app_id/org_mozilla_fenix_glam_release__view_clients_daily_histogram_aggregates_v1.sql
@@ -1,7 +1,14 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod`.glam_etl.org_mozilla_fenix_glam_release__view_clients_daily_histogram_aggregates_v1
 AS
+WITH extracted AS (
+  SELECT
+    *
+  FROM
+    `moz-fx-data-shared-prod`.glam_etl.org_mozilla_firefox__view_clients_daily_histogram_aggregates_v1
+)
 SELECT
-  *
+  * EXCEPT (channel),
+  "*" AS channel
 FROM
-  `moz-fx-data-shared-prod`.glam_etl.org_mozilla_firefox__view_clients_daily_histogram_aggregates_v1
+  extracted

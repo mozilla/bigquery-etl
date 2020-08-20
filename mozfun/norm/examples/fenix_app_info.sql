aa3f827d6f928a396eb8f36ccd6574c99cc043f8
Jeff Klukas <jklukas@mozilla.com>
2020-08-20 15:42:47 -0400

    More complete docs for fenix_app_info (#1244)
    Fixes #1243

--- mozfun/norm/examples/fenix_app_info.sql
+++ mozfun/norm/examples/fenix_app_info.sql
@@ -1,9 +1,34 @@
+-- Example of a query over all Fenix builds advertised as "Firefox Beta"
+CREATE TEMP FUNCTION extract_fields(app_id STRING, m ANY TYPE) AS (
+  (
+    SELECT AS STRUCT
+      m.submission_timestamp,
+      m.metrics.string.geckoview_version,
+      mozfun.norm.fenix_app_info(app_id, m.client_info.app_build).*
+  )
+);
+
+WITH base AS (
+  SELECT
+    extract_fields('org_mozilla_firefox_beta', m).*
+  FROM
+    org_mozilla_firefox_beta.metrics AS m
+  UNION ALL
+  SELECT
+    extract_fields('org_mozilla_fenix', m).*
+  FROM
+    org_mozilla_fenix.metrics AS m
+)
 SELECT
-  mozfun.norm.fenix_app_info('org_mozilla_fenix', client_info.app_build).*
+  DATE(submission_timestamp) AS submission_date,
+  geckoview_version,
+  COUNT(*)
 FROM
-  org_mozilla_fenix.metrics
+  base
 WHERE
-  submission_date = '2020-08-01'
-LIMIT
-  1
--- app_name = Fenix, channel = nightly, app_id = org.mozilla.fenix
+  app_name = 'Fenix'  -- excludes 'Firefox Preview'
+  AND channel = 'beta'
+  AND DATE(submission_timestamp) = '2020-08-01'
+GROUP BY
+  submission_date,
+  geckoview_version

a81c4b06a3271ffc9439c6630bea75aa8e1319f8
Anna Scholtz <anna@scholtzan.net>
2020-08-21 16:21:43 -0700

    Refactor CLI utility functions

--- bigquery_etl/cli/utils.py
+++ bigquery_etl/cli/utils.py
@@ -1,14 +1,18 @@
+"""Utility functions used by the CLI."""
+
 import click
 import os
 
 
 def is_valid_dir(ctx, param, value):
+    """Check if the parameter provided via click is an existing directory."""
     if not os.path.isdir(value) or not os.path.exists(value):
         raise click.BadParameter(f"Invalid directory path to {value}")
     return value
 
 
 def is_valid_file(ctx, param, value):
+    """Check if the parameter provided via click is an existing file."""
     if not os.path.isfile(value) or not os.path.exists(value):
         raise click.BadParameter(f"Invalid file path to {value}")
     return value

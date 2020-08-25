9003ec7836454d5073922ea9da53906725465cda
Ryan Harter <harterrt@gmail.com>
2020-08-25 16:56:13 -0400

    Remove excessive comments

--- sql/telemetry_derived/firefox_nondesktop_day_2_7_activation_v1/query.sql
+++ sql/telemetry_derived/firefox_nondesktop_day_2_7_activation_v1/query.sql
@@ -51,8 +51,6 @@ FROM
 WHERE
   submission_date = @submission_date
   AND product IN (
-    -- Fenix and Firefox Preview were previously excluded pending:
-    -- https://jira.mozilla.com/browse/DS-696
     'Fenix',
     'Firefox Preview',
     'Fennec Android',

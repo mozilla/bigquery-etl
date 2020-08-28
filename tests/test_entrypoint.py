3725a64e31a4d54c34837e7c56f42bd362f78647
Anna Scholtz <anna@scholtzan.net>
2020-08-28 14:49:22 -0700

    Check error output for entrypoint tests

--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -16,10 +16,27 @@ class TestEntrypoint:
         query_file = query_file_path / "query.sql"
         query_file.write_text("SELECT 1 AS a, 'abc' AS b;")
 
-        result = subprocess.check_call(
-            [ENTRYPOINT_SCRIPT, "query", "--project_id=" + project_id, str(query_file)]
-        )
-        assert result == 0
+        try:
+            result = subprocess.check_output(
+                [
+                    ENTRYPOINT_SCRIPT,
+                    "query",
+                    "--project_id=" + project_id,
+                    str(query_file),
+                ],
+                stderr=subprocess.STDOUT,
+            )
+            assert b"Current status: DONE" in result
+            assert b"No metadata.yaml found for {}" in result
+        except subprocess.CalledProcessError as e:
+            # running bq in CircleCI will fail since it's not installed
+            # but the error output can be checked for whether bq was called
+            assert b"No such file or directory: 'bq'" in e.output
+            assert b"No metadata.yaml found for {}" in e.output
+            assert (
+                b'subprocess.check_call(["bq"] + query_arguments, stdin=query_stream)'
+                in e.output
+            )
 
     @pytest.mark.integration
     def test_run_query_write_to_table(
@@ -37,25 +54,35 @@ class TestEntrypoint:
         )
         bigquery_client.create_table(table)
 
-        result = subprocess.check_call(
-            [
-                ENTRYPOINT_SCRIPT,
-                "query",
-                "--dataset_id=" + temporary_dataset,
-                "--destination_table=query_v1",
-                "--project_id=" + project_id,
-                "--replace",
-                str(query_file),
-            ]
-        )
-        assert result == 0
+        try:
+            result = subprocess.check_output(
+                [
+                    ENTRYPOINT_SCRIPT,
+                    "query",
+                    "--dataset_id=" + temporary_dataset,
+                    "--destination_table=query_v1",
+                    "--project_id=" + project_id,
+                    "--replace",
+                    str(query_file),
+                ],
+                stderr=subprocess.STDOUT,
+            )
+            assert b"Current status: DONE" in result
+            assert b"No metadata.yaml found for {}" in result
 
-        result = bigquery_client.query(
-            f"SELECT a FROM {project_id}.{temporary_dataset}.query_v1"
-        ).result()
-        assert result.total_rows == 1
-        for row in result:
-            assert row.a == "foo"
+            result = bigquery_client.query(
+                f"SELECT a FROM {project_id}.{temporary_dataset}.query_v1"
+            ).result()
+            assert result.total_rows == 1
+            for row in result:
+                assert row.a == "foo"
+        except subprocess.CalledProcessError as e:
+            assert b"No such file or directory: 'bq'" in e.output
+            assert b"No metadata.yaml found for {}" in e.output
+            assert (
+                b'subprocess.check_call(["bq"] + query_arguments, stdin=query_stream)'
+                in e.output
+            )
 
     @pytest.mark.integration
     def test_run_query_no_query_file(self):

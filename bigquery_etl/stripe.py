208c551209380a69fb0d75cff0389f5ca82bb41a
Daniel Thorn <dthorn@mozilla.com>
2020-09-17 10:06:06 -0700

    Add stripe import script (#1316)

---
+++ bigquery_etl/stripe.py
@@ -0,0 +1,171 @@
+"""Import Stripe data into BigQuery."""
+
+from datetime import datetime, timedelta, timezone
+from typing import Any, IO, Optional
+from tempfile import TemporaryFile
+import ujson
+import sys
+import warnings
+
+from google.cloud import bigquery
+from stripe.api_resources.abstract import ListableAPIResource
+import click
+import stripe
+
+
+class StripeResourceType(click.ParamType):
+    """Click parameter type for stripe listable resources."""
+
+    name = "stripe resource"
+
+    def convert(self, value, param, ctx):
+        """Get a listable stripe resource type by name."""
+        if not isinstance(value, str):
+            self.fail(f"{value!r} is not a string")
+        if value.islower():
+            value = value.capitalize()
+        try:
+            result = getattr(stripe, value)
+        except AttributeError:
+            self.fail(f"resource type {value!r} not found in stripe")
+        if not issubclass(result, ListableAPIResource):
+            self.fail("resource must be listable")
+        return result
+
+
+def bigquery_format(obj: Any, *path):
+    """Format stripe objects for BigQuery."""
+    if isinstance(obj, stripe.ListObject):
+        # recursively request any additional values for paged lists.
+        return [
+            bigquery_format(e, *path, i) for i, e in enumerate(obj.auto_paging_iter())
+        ]
+    if path[-1:] == ("metadata",) and isinstance(obj, dict):
+        # format metadata as a key-value list
+        return [
+            {"key": key, "value": bigquery_format(value)} for key, value in obj.items()
+        ]
+    if isinstance(obj, dict):
+        # recursively format and drop nulls, empty lists, and empty objects
+        return {
+            key: formatted
+            for key, value in obj.items()
+            for formatted in (bigquery_format(value, *path, key),)
+            # drop nulls, empty lists, and empty objects
+            if formatted not in (None, [], {})
+        }
+    return obj
+
+
+def _open_file(
+    api_key: Optional[str], file: Optional[str], table: Optional[str]
+) -> IO[bytes]:
+    if file is not None:
+        if api_key is None:
+            mode = "rb"
+        else:
+            mode = "w+b"
+        return open(file, mode=mode)
+    elif table is None:
+        return sys.stdout.buffer
+    elif api_key is None:
+        return sys.stdin.buffer
+    else:
+        return TemporaryFile(mode="w+b")
+
+
+@click.group(help="Commands for Stripe ETL.")
+def stripe_():
+    """Create the CLI group for stripe commands."""
+    pass
+
+
+@stripe_.command("import", help=__doc__)
+@click.option(
+    "--api-key",
+    help="Stripe API key to use for authentication; If not set resources will be read "
+    "from stdin, or --file if set",
+)
+@click.option(
+    "--date",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    help="Creation date of resources to pull from stripe API",
+)
+@click.option(
+    "--table",
+    help="BigQuery Standard SQL format table ID where resources will be written; "
+    "if not set resources will be writtent to stdout, or --file if set",
+)
+@click.option(
+    "--file",
+    help="Optional persistent file where resources are or will be stored in newline "
+    "delimited json format for uploading to BigQuery",
+)
+@click.option(
+    "--resource",
+    default="Event",
+    type=StripeResourceType(),
+    help="Type of stripe resource to export",
+)
+def import_(api_key, date, table, file, resource):
+    """Import Stripe data into BigQuery."""
+    assert resource is stripe.Event
+    if resource is stripe.Event and not date:
+        click.echo("must specify --date for --resource=Event")
+        sys.exit(1)
+    if api_key is None and table is None:
+        click.echo("must specify --api-key and/or --table")
+        sys.exit(1)
+    if table and date:
+        table = f"{table}${date:%Y%m%d}"
+    with _open_file(api_key, file, table) as file_obj:
+        if api_key:
+            stripe.api_key = api_key
+            created = {}
+            if date:
+                start = date.replace(tzinfo=timezone.utc)
+                created = {
+                    "gte": int(start.timestamp()),
+                    # make sure to use timedelta before converting to timestamp,
+                    # so that leap seconds are properly accounted for.
+                    "lt": int((start + timedelta(days=1)).timestamp()),
+                }
+            for instance in resource.list(created=created).auto_paging_iter():
+                row = {
+                    "created": datetime.utcfromtimestamp(instance.created).isoformat()
+                }
+                if resource is stripe.Event:
+                    row["data"] = {
+                        instance.data.object.object.replace(
+                            ".", "_"
+                        ): instance.data.object,
+                    }
+                for key, value in instance.items():
+                    if key not in row:
+                        row[key] = value
+                file_obj.write(ujson.dumps(bigquery_format(row)).encode("UTF-8"))
+                file_obj.write(b"\n")
+        if table:
+            if file_obj.writable():
+                file_obj.seek(0)
+            warnings.filterwarnings("ignore", module="google.auth._default")
+            job = bigquery.Client().load_table_from_file(
+                file_obj=file_obj,
+                destination=table,
+                rewind=True,
+                job_config=bigquery.LoadJobConfig(
+                    clustering_fields=["created"],
+                    ignore_unknown_values=False,
+                    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+                    time_partitioning=bigquery.TimePartitioning(field="created"),
+                    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+                ),
+            )
+            try:
+                print(f"Waiting for {job.job_id}", file=sys.stderr)
+                job.result()
+            except Exception as e:
+                print(f"{job.job_id} failed: {e}", file=sys.stderr)
+                sys.exit(1)
+            else:
+                print(f"{job.job_id} succeeded", file=sys.stderr)

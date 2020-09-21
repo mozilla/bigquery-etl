fcc2c793fb388615d2c647c04734319d88a4f26e
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2020-09-21 14:21:37 +0200

    Reformat

--- sql/telemetry_derived/fission_monitoring_main_v1/init.sql
+++ sql/telemetry_derived/fission_monitoring_main_v1/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS
-  `moz-fx-data-shared-prod.analysis.fission_monitoring_main_v1` ( -- TODO: s/analysis/telemetry_derived/
+  `moz-fx-data-shared-prod.analysis.fission_monitoring_main_v1`( -- TODO: s/analysis/telemetry_derived/
     submission_date DATE,
     document_id STRING,
     sample_id INT64
--- sql/telemetry_derived/fission_monitoring_main_v1/query.sql
+++ sql/telemetry_derived/fission_monitoring_main_v1/query.sql
@@ -6,7 +6,7 @@ FROM
   `moz-fx-data-shared-prod`.telemetry.main
 WHERE
   -- TODO: parameterize for daily runs
-  DATE(submission_timestamp)>='2020-09-01'
+  DATE(submission_timestamp) >= '2020-09-01'
   AND normalized_channel = 'nightly'
   -- TODO: specify experiment inclusion criteria using prefs
   -- to be added in https://bugzilla.mozilla.org/show_bug.cgi?id=1660057#c3

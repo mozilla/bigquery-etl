bb2aff3fda6078e3840e436b2f0e9d0df42e49be
Frank Bertsch <fbertsch@mozilla.com>
2020-09-24 15:28:14 -0400

    WIP: Event analysis UDFs

---
+++ mozfun/event_analysis_udf/escape_metachars/udf.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION event_analysis_udf.escape_metachars(s STRING)
+RETURNS STRING AS (
+  CONCAT('\\Q', s, '\\E')
+);
+
+SELECT
+  assert_equals('\\Q.*\\E', event_analysis_udf.escape_metachars('.*')),
+  assert_equals(CAST(NULL AS STRING), event_analysis_udf.escape_metachars(NULL)),
+  assert_equals('\\Q\\E', event_analysis_udf.escape_metachars('')),
+  assert_equals('$', regexp_extract('.$*', event_analysis_udf.escape_metachars('$'))),
+  assert_equals(CAST(NULL AS STRING), regexp_extract('.$*', event_analysis_udf.escape_metachars('...'))),
+  assert_equals('.$*', regexp_extract('.$*', CONCAT(event_analysis_udf.escape_metachars('.'), '.', event_analysis_udf.escape_metachars('*')))),

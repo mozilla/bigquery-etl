df843d4b12f062b0abbe04a249fbfc41db3ac13e
Anna Scholtz <anna@scholtzan.net>
2020-09-25 11:26:02 -0700

    UDF rename command

--- bigquery_etl/_version.py
+++ bigquery_etl/_version.py
@@ -1,5 +1,5 @@
 """Provides bigquery-etl version information."""
 from incremental import Version
 
-__version__ = Version("bigquery_etl/", 20, 9, 1)
+__version__ = Version("bigquery_etl/", 20, 9, 2)
 __all__ = ["__version__"]

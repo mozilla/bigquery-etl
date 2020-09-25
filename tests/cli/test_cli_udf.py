3e54455cb8c6d7a06e5b510b4296a1a81b6fa2ea
Anna Scholtz <anna@scholtzan.net>
2020-09-25 11:52:52 -0700

    CLI UDF rename command

--- tests/cli/test_cli_udf.py
+++ tests/cli/test_cli_udf.py
@@ -3,7 +3,7 @@ import pytest
 from click.testing import CliRunner
 import yaml
 
-from bigquery_etl.cli.udf import create, info
+from bigquery_etl.cli.udf import create, info, rename
 
 
 class TestUdf:
@@ -101,3 +101,106 @@ class TestUdf:
             assert result.exit_code == 0
             assert "udf.another_udf" in result.output
             assert "udf.test_udf" not in result.output
+
+    def test_udf_renaming_invalid_naming(self, runner):
+        with runner.isolated_filesystem():
+            result = runner.invoke(
+                rename, ["dataset", "-n", "something.else"], obj={"UDF_DIRS": ("udf",)}
+            )
+            assert result.exit_code == 1
+
+            result = runner.invoke(
+                rename, ["dataset.udf", "-n", "something"], obj={"UDF_DIRS": ("udf",)}
+            )
+            assert result.exit_code == 1
+
+            result = runner.invoke(rename, ["dataset.udf"])
+            assert result.exit_code == 2
+
+    def test_udf_renaming(self, runner):
+        with runner.isolated_filesystem():
+            os.mkdir("udf")
+            os.mkdir("udf/test_udf")
+            with open("udf/test_udf/udf.sql", "w") as f:
+                f.write("CREATE OR REPLACE FUNCTION udf.test_udf() AS (TRUE)")
+
+            os.mkdir("udf/another_udf")
+            with open("udf/another_udf/udf.sql", "w") as f:
+                f.write(
+                    "CREATE OR REPLACE FUNCTION udf.another_udf() AS (udf.test_udf())"
+                )
+
+            os.mkdir("sql")
+            os.mkdir("sql/telemetry_derived")
+            os.mkdir("sql/telemetry_derived/query_v1")
+            with open("sql/telemetry_derived/query_v1/query.sql", "w") as f:
+                f.write("SELECT udf.test_udf()")
+
+            result = runner.invoke(
+                rename,
+                ["udf.test_udf", "-n", "udf.renamed_udf"],
+                obj={"UDF_DIRS": ("udf",)},
+            )
+            assert result.exit_code == 0
+            assert "test_udf" not in os.listdir("udf")
+            assert "another_udf" in os.listdir("udf")
+            assert "renamed_udf" in os.listdir("udf")
+
+            with open("sql/telemetry_derived/query_v1/query.sql", "r") as f:
+                sql = f.read()
+                assert "udf.renamed_udf" in sql
+                assert "udf.test_udf" not in sql
+
+            with open("udf/another_udf/udf.sql", "r") as f:
+                sql = f.read()
+                assert "udf.renamed_udf" in sql
+                assert "udf.test_udf" not in sql
+
+            with open("udf/renamed_udf/udf.sql", "r") as f:
+                sql = f.read()
+                assert "udf.renamed_udf" in sql
+                assert "udf.test_udf" not in sql
+
+    def test_mozfun_dataset_renaming(self, runner):
+        with runner.isolated_filesystem():
+            os.mkdir("mozfun")
+            os.mkdir("mozfun/udf")
+            os.mkdir("mozfun/udf/test_udf")
+            with open("mozfun/udf/test_udf/udf.sql", "w") as f:
+                f.write("CREATE OR REPLACE FUNCTION udf.test_udf() AS (TRUE)")
+
+            os.mkdir("mozfun/udf/another_udf")
+            with open("mozfun/udf/another_udf/udf.sql", "w") as f:
+                f.write(
+                    "CREATE OR REPLACE FUNCTION udf.another_udf() AS (udf.test_udf())"
+                )
+
+            os.mkdir("sql")
+            os.mkdir("sql/telemetry_derived")
+            os.mkdir("sql/telemetry_derived/query_v1")
+            with open("sql/telemetry_derived/query_v1/query.sql", "w") as f:
+                f.write("SELECT udf.test_udf()")
+
+            result = runner.invoke(
+                rename, ["udf", "-n", "new_dataset"], obj={"UDF_DIRS": ("mozfun",)}
+            )
+            assert result.exit_code == 0
+            assert "udf" not in os.listdir("mozfun")
+            assert "new_dataset" in os.listdir("mozfun")
+            assert "another_udf" in os.listdir("mozfun/new_dataset")
+            assert "test_udf" in os.listdir("mozfun/new_dataset")
+
+            with open("sql/telemetry_derived/query_v1/query.sql", "r") as f:
+                sql = f.read()
+                assert "new_dataset.test_udf" in sql
+                assert "udf.test_udf" not in sql
+
+            with open("mozfun/new_dataset/another_udf/udf.sql", "r") as f:
+                sql = f.read()
+                assert "new_dataset.test_udf" in sql
+                assert "udf.test_udf" not in sql
+
+            with open("mozfun/new_dataset/test_udf/udf.sql", "r") as f:
+                sql = f.read()
+                assert "new_dataset.test_udf" in sql
+                assert "udf.test_udf" not in sql

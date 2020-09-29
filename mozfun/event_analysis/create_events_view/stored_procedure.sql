815f45fa38dea99a3dce7fc82e5e697f71aad314
Frank Bertsch <fbertsch@mozilla.com>
2020-09-29 13:14:47 -0400

    Add explicit count support

--- mozfun/event_analysis/create_events_view/stored_procedure.sql
+++ mozfun/event_analysis/create_events_view/stored_procedure.sql
@@ -4,16 +4,26 @@ CREATE OR REPLACE PROCEDURE
     project STRING,
     dataset STRING,
     funnels ARRAY<STRUCT<funnel_name STRING, funnel ARRAY<STRUCT<step_name STRING, events ARRAY<STRUCT<category STRING, event_name STRING>>>>>>,
-    counts ARRAY<STRUCT<count_name STRING,events ARRAY<STRUCT<category STRING, event_name STRING>>>>)
+    counts ARRAY<STRUCT<count_name STRING, events ARRAY<STRUCT<category STRING, event_name STRING>>>>)
 BEGIN
   DECLARE i INT64 DEFAULT 1;
 
   DECLARE funnel_sql STRING;
   DECLARE funnel_sqls ARRAY<STRING> DEFAULT [];
 
+  DECLARE count_sql STRING;
+  DECLARE count_sqls ARRAY<STRING> DEFAULT [];
+
   WHILE i <= ARRAY_LENGTH(funnels) DO
-    CALL get_funnel_steps_sql(project, dataset, funnels[ORDINAL(i)].funnel_name, funnels[ORDINAL(i)].funnel, funnel_sql); 
+    CALL event_analysis.get_funnel_steps_sql(project, dataset, funnels[ORDINAL(i)].funnel_name, funnels[ORDINAL(i)].funnel, funnel_sql); 
     SET funnel_sqls = ARRAY_CONCAT(funnel_sqls, [funnel_sql]);
+    SET i = i + 1;
+  END WHILE;
+
+  SET i = 1;
+  WHILE i <= ARRAY_LENGTH(counts) DO
+    CALL event_analysis.get_count_sql(project, dataset, counts[ORDINAL(i)].count_name, counts[ORDINAL(i)].events, count_sql); 
+    SET count_sqls = ARRAY_CONCAT(count_sqls, [count_sql]);
     SET i = i + 1;
   END WHILE;
 

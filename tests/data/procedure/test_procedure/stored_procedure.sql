b0b1d82ee95c628dbeb94898248c330dbe20eaba
Frank Bertsch <fbertsch@mozilla.com>
2020-09-29 13:33:22 -0400

    Reformat last file

--- tests/data/procedure/test_procedure/stored_procedure.sql
+++ tests/data/procedure/test_procedure/stored_procedure.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURES procedure.test_procedure(out STRING) 
-BEGIN 
+CREATE OR REPLACE PROCEDURES procedure.test_procedure(out STRING)
+BEGIN
   SET out = mozfun.json.parse('{}');
 END;

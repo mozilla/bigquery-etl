160a93b7cdb3cade8c614bf8c09b3af6f9e0261a
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2020-10-02 17:19:36 +0200

    Use submission_timestamp for partitioning in fission monitoring table

--- sql/telemetry_derived/fission_monitoring_main_v1/init.sql
+++ sql/telemetry_derived/fission_monitoring_main_v1/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS
   `moz-fx-data-shared-prod.telemetry_derived.fission_monitoring_main_v1`(
-    submission_date DATE,
+    submission_timestamp TIMESTAMP,
     document_id STRING,
     sample_id INT64
   )
 PARTITION BY
-  submission_date
+  DATE(submission_timestamp)
 CLUSTER BY
   sample_id

2276a27340aa344438d71699b7e61c6c3bbf8114
Frank Bertsch <fbertsch@mozilla.com>
2020-10-07 16:25:56 -0400

    Add exract_event_counts_with_properties

---
+++ tests/assert/struct_equals/udf.sql
@@ -0,0 +1,23 @@
+CREATE TEMP FUNCTION assert_struct_equals(expected ANY TYPE, actual ANY TYPE) AS (
+  IF(
+    ARRAY_LENGTH(
+      ARRAY(
+        SELECT
+          DISTINCT
+        AS
+          STRUCT *
+        FROM
+          (SELECT * FROM UNNEST([expected]) UNION ALL SELECT * FROM UNNEST([actual]))
+      )
+    ) = 1,
+    TRUE,
+    ERROR(CONCAT('Expected struct', TO_JSON_STRING(expected), ' but got ', TO_JSON_STRING(actual)))
+  )
+);
+
+SELECT
+  assert_struct_equals(STRUCT('a' AS a, 'b' AS b, 3 AS c), STRUCT('a' AS a, 'b' AS b, 3 AS c)),
+  assert_struct_equals(STRUCT('a' AS a, NULL AS b, 3 AS c), STRUCT('a' AS a, NULL AS b, 3 AS c)),
+#xfail
+SELECT
+  assert_struct_equals(STRUCT('a' AS a, 'b' AS b, 3 AS c), STRUCT('a' AS a, 'b' AS b, 4 AS c))

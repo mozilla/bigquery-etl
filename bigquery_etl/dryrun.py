dda45e35d7114ee359e6422965b99a2e5c62232a
Jeff Klukas <jeff@klukas.net>
2020-10-15 14:52:30 -0400

    Exempt from dry run

--- bigquery_etl/dryrun.py
+++ bigquery_etl/dryrun.py
@@ -50,6 +50,7 @@ SKIP = {
     "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_amplitude_export_v1/query.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_amplitude_user_ids_v1/query.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_amplitude_user_ids_v1/init.sql",  # noqa E501
+    "sql/moz-fx-data-shared-prod/regrets_reporter/regrets_reporter_update/view.sql",
     "sql/moz-fx-data-shared-prod/revenue_derived/client_ltv_v1/query.sql",
     "sql/moz-fx-data-shared-prod/shredder_state/progress/view.sql",
     "sql/moz-fx-data-shared-prod/apple_app_store/metrics_by_app_referrer/query.sql",

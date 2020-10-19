49c9bbf3407b606689712ce71f54f002da9bdc34
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-10-19 09:26:28 -0700

    Add view for geckoview versions with one row per build hour (#1450)

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix_derived/geckoview_version/view.sql
@@ -0,0 +1,14 @@
+-- Return the most likely version over many windows of the estimated major
+-- version
+WITH agg AS (
+  SELECT
+    array_agg(t ORDER BY geckoview_major_version DESC, n_pings DESC LIMIT 1)[offset(0)] AS top_row
+  FROM
+    `moz-fx-data-shared-prod`.org_mozilla_fenix_derived.geckoview_version_v1 t
+  GROUP BY
+    build_hour
+)
+SELECT
+  top_row.*
+FROM
+  agg

b66f948bdde84c22f623160e636c5cf5094b4f84
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-10-19 12:36:45 -0700

    Add CREATE OR REPLACE VIEW clause to geckoview_versions and move it to org_mozilla_fenix (#1452)
    * Add CREATE OR REPLACE VIEW clause to geckoview_versions
    
    * Move geckoview version to org_mozilla_fenix

--- sql/moz-fx-data-shared-prod/org_mozilla_fenix/geckoview_version/view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix/geckoview_version/view.sql
@@ -1,5 +1,8 @@
 -- Return the most likely version over many windows of the estimated major
 -- version
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod`.org_mozilla_fenix.geckoview_version
+AS
 WITH agg AS (
   SELECT
     array_agg(t ORDER BY geckoview_major_version DESC, n_pings DESC LIMIT 1)[offset(0)] AS top_row

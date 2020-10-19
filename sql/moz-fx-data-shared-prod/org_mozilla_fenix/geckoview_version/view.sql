bf47e43f3dd918e516b236d5deecbaeb28f1df08
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-10-19 12:19:31 -0700

    Move geckoview version to org_mozilla_fenix

--- sql/moz-fx-data-shared-prod/org_mozilla_fenix/geckoview_version/view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix/geckoview_version/view.sql
@@ -1,7 +1,7 @@
 -- Return the most likely version over many windows of the estimated major
 -- version
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod`.org_mozilla_fenix_derived.geckoview_version
+  `moz-fx-data-shared-prod`.org_mozilla_fenix.geckoview_version
 AS
 WITH agg AS (
   SELECT

9d9a7a946563bee74c82eed938b9465eb72f8b56
jailang <lynjailang@gmail.com>
2020-10-20 11:50:47 -0700

    Update and add tests

--- tests/metadata/test_parse_metadata.py
+++ tests/metadata/test_parse_metadata.py
@@ -7,6 +7,29 @@ TEST_DIR = Path(__file__).parent.parent
 
 
 class TestParseMetadata(object):
+    def test_metadata_instantiation(self):
+        metadata = Metadata("Test metadata", "test description",
+                            ["test@example.org"], {})
+
+        assert metadata.friendly_name == "Test metadata"
+        assert metadata.description == "test description"
+        assert metadata.owners == ["test@example.org"]
+        assert metadata.labels == {}
+        assert metadata.scheduling == {}
+
+    def test_invalid_owners(self):
+        with pytest.raises(ValueError):
+            Metadata("Test metadata", "test description",
+                     ["testexample.org"])
+
+    def test_invalid_label(self):
+        with pytest.raises(ValueError):
+            Metadata("Test metadata", "test description",
+                     ["test@example.org"], {"INVALID-KEY": "foo"})
+        with pytest.raises(ValueError):
+            Metadata("Test metadata", "test description",
+                     ["test@example.org"], {"foo": "INVALID-VALUE"})
+
     def test_is_valid_label(self):
         assert Metadata.is_valid_label("valid_label")
         assert Metadata.is_valid_label("valid-label1")
@@ -33,8 +56,6 @@ class TestParseMetadata(object):
         assert metadata.is_incremental()
         assert metadata.is_incremental_export()
         assert metadata.review_bug() is None
-        assert "invalid_value" not in metadata.labels
-        assert "invalid.label" not in metadata.labels
         assert "1232341234" in metadata.labels
         assert "1234_abcd" in metadata.labels
         assert "number_value" in metadata.labels

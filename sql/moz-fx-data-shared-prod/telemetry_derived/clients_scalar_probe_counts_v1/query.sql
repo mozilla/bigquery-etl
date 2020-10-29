d3a8aa999600e5835d6ea489f355d3074ca91f5b
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-10-28 17:06:57 -0700

    Revert change to query

--- sql/moz-fx-data-shared-prod/telemetry_derived/clients_scalar_probe_counts_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/clients_scalar_probe_counts_v1/query.sql
@@ -273,7 +273,7 @@ SELECT
       ANY_VALUE(buckets)
     )
     WHEN metric_type = 'boolean' OR metric_type = 'keyed-scalar-boolean'
-    THEN (
+    THEN udf_fill_buckets(
       ARRAY_AGG(STRUCT<key STRING, value FLOAT64>(bucket, user_count)),
       ['always','never','sometimes'])
    END AS aggregates

bf6bcad3c7976d695df6d233e6a20a30199b5e90
Sunah Suh <ssuh@mozilla.com>
2020-11-02 10:25:27 -0600

    fixup! Merge branch 'master' into search-probes-main-summary

--- dags/bqetl_main_summary.py
+++ dags/bqetl_main_summary.py
@@ -132,7 +132,7 @@ with DAG(
         multipart=True,
         sql_file_path="sql/moz-fx-data-shared-prod/telemetry_derived/main_summary_v4",
         priority_weight=90,
-        allow_field_addition_on_date="2020-10-30",
+        allow_field_addition_on_date="2020-11-02",
         dag=dag,
     )
 

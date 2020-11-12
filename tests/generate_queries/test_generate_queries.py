a3fb902bbebcac8ae0f7894ae8685cfe1f72feff
Frank Bertsch <fbertsch@mozilla.com>
2020-11-12 12:12:38 -0500

    Update with review feedback

--- tests/generate_queries/test_generate_queries.py
+++ tests/generate_queries/test_generate_queries.py
@@ -1,6 +1,6 @@
 import os
 
-from bigquery_etl.events_daily.generate_queries import get_query_dirs, QueryDir
+from bigquery_etl.events_daily.generate_queries import get_query_dirs, TemplatedDir
 from pathlib import Path
 
 BASE_DIR = Path(os.path.dirname(__file__)).parent
@@ -9,4 +9,6 @@ BASE_DIR = Path(os.path.dirname(__file__)).parent
 class TestGenerateQueries:
     def test_get_query_dirs(self):
         res = list(get_query_dirs(BASE_DIR / "templates"))
-        assert res == [QueryDir("event_types", BASE_DIR / "templates" / "event_types")]
+        assert res == [
+            TemplatedDir("event_types", BASE_DIR / "templates" / "event_types")
+        ]

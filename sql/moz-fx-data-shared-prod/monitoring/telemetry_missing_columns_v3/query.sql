8dd4ca59b6415d5b4b94f50ccbb8f29c8cd39b82
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-11-13 11:42:54 -0800

    Add init for telemetry_missing_columns_v3

--- sql/moz-fx-data-shared-prod/monitoring/telemetry_missing_columns_v3/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring/telemetry_missing_columns_v3/query.sql
@@ -8,7 +8,7 @@ WITH placeholder_table_names AS (
 ),
 extracted AS (
   SELECT
-    TIMESTAMP_TRUNC(submission_timestamp, DAY) AS day,
+    @submission_date AS submission_date,
     'telemetry' AS document_namespace,
     `moz-fx-data-shared-prod`.udf.extract_document_type(_TABLE_SUFFIX) AS document_type,
     `moz-fx-data-shared-prod`.udf.extract_document_version(_TABLE_SUFFIX) AS document_version,
@@ -17,21 +17,22 @@ extracted AS (
     `moz-fx-data-shared-prod.telemetry_stable.*`
   WHERE
     -- only observe full days of data
-    DATE(submission_timestamp) = DATE_SUB(current_date, INTERVAL 1 day)
+    DATE(submission_timestamp) = @submission_date
     -- https://cloud.google.com/bigquery/docs/querying-wildcard-tables#filtering_selected_tables_using_table_suffix
     -- exclude pings derived from main schema to save on space, 300GB vs 3TB
     AND _TABLE_SUFFIX NOT IN ('main_v4', 'saved_session_v4', 'first_shutdown_v4')
     AND _TABLE_SUFFIX NOT IN (SELECT * FROM placeholder_table_names)
-)
-SELECT
-  * EXCEPT (additional_properties),
-  COUNT(*) AS path_count
-FROM
-  extracted,
-  UNNEST(
-    `moz-fx-data-shared-prod`.udf_js.json_extract_missing_cols(
-      additional_properties,
-      [],
+),
+transformed AS (
+  SELECT
+    * EXCEPT (additional_properties),
+    COUNT(*) AS path_count
+  FROM
+    extracted,
+    UNNEST(
+      `moz-fx-data-shared-prod`.udf_js.json_extract_missing_cols(
+        additional_properties,
+        [],
         -- Manually curated list of known missing sections. The process to
         -- generate list is to change the project for the live table to
         -- moz-fx-data-shar-nonprod-efed to obtain a 1% sample. Run this query
@@ -40,22 +41,32 @@ FROM
         -- number of subpaths are added to the following list, e.g. activeAddons
         -- or histograms. The list is then curated such that there are
         -- approximately less than 1000 distinct paths.
-      [
+        [
           -- common environment
-        "activeAddons",
-        "userPrefs",
-        "experiments",
+          "activeAddons",
+          "userPrefs",
+          "experiments",
           -- common measures
-        "keyedScalars",
-        "scalars",
-        "histograms",
-        "keyedHistograms"
-      ]
-    )
-  ) AS path
-GROUP BY
-  day,
+          "keyedScalars",
+          "scalars",
+          "histograms",
+          "keyedHistograms"
+        ]
+      )
+    ) AS path
+  GROUP BY
+    submission_date,
+    document_namespace,
+    document_type,
+    document_version,
+    path
+)
+SELECT
+  submission_date,
   document_namespace,
   document_type,
   document_version,
-  path
+  path,
+  path_count
+FROM
+  transformed

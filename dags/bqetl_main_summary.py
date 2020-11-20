1a517f4433cf5deec332ec64457eab0993d96f61
Jeff Klukas <jeff@klukas.net>
2020-11-20 14:36:43 -0500

    Scheduling

--- dags/bqetl_main_summary.py
+++ dags/bqetl_main_summary.py
@@ -114,6 +114,24 @@ with DAG(
         dag=dag,
     )
 
+    telemetry_derived__main_1pct__v1 = bigquery_etl_query(
+        task_id="telemetry_derived__main_1pct__v1",
+        destination_table="main_1pct_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="jklukas@mozilla.com",
+        email=[
+            "dthorn@mozilla.com",
+            "frank@mozilla.com",
+            "jklukas@mozilla.com",
+            "telemetry-alerts@mozilla.com",
+        ],
+        start_date=datetime.datetime(2020, 6, 1, 0, 0),
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
     telemetry_derived__main_summary__v4 = bigquery_etl_query(
         task_id="telemetry_derived__main_summary__v4",
         destination_table="main_summary_v4",
@@ -169,6 +187,8 @@ with DAG(
         mode="reschedule",
         pool="DATA_ENG_EXTERNALTASKSENSOR",
     )
+
+    telemetry_derived__main_1pct__v1.set_upstream(wait_for_copy_deduplicate_main_ping)
 
     telemetry_derived__main_summary__v4.set_upstream(
         wait_for_copy_deduplicate_main_ping

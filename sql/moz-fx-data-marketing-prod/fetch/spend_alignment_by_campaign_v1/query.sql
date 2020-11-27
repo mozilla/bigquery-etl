44c8bfaada17450cd5bca28cd0f31b1d965983f7
Benjamin Wu <benjaminwu124@gmail.com>
2020-11-27 15:23:02 -0500

    Add queries and views for fetch marketing spend data

---
+++ sql/moz-fx-data-marketing-prod/fetch/spend_alignment_by_campaign_v1/query.sql
@@ -0,0 +1,31 @@
+WITH fetch_summary AS (
+  SELECT
+    campaign,
+    ROUND(SUM(vendorNetSpend)) AS fetch_summary_spend,
+  FROM
+    `moz-fx-data-marketing-prod.fetch.summary_*`
+  WHERE
+    _TABLE_SUFFIX = (SELECT MAX(_table_suffix) FROM `moz-fx-data-marketing-prod.fetch.summary_*`)
+  GROUP BY
+    campaign
+),
+detailed_summary AS (
+  SELECT
+    campaign,
+    ROUND(SUM(vendorNetSpend)) AS detail_vendor_net_spend,
+  FROM
+    `moz-fx-data-marketing-prod.fetch.fetch_deduped`
+  GROUP BY
+    campaign
+)
+SELECT
+  *,
+  fetch_summary_spend - detail_vendor_net_spend AS variance,
+FROM
+  fetch_summary
+LEFT JOIN
+  detailed_summary
+USING
+  (campaign)
+ORDER BY
+  fetch_summary_spend DESC

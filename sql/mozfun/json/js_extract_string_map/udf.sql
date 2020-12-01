2f4bab4a2452fc6856cda4b075916d8d9cc8b2c5
Daniel Thorn <dthorn@mozilla.com>
2020-12-01 11:59:12 -0800

    address review

--- sql/mozfun/json/js_extract_string_map/udf.sql
+++ sql/mozfun/json/js_extract_string_map/udf.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION json.extract_string_map(input STRING)
+CREATE OR REPLACE FUNCTION json.js_extract_string_map(input STRING)
 RETURNS ARRAY<STRUCT<key STRING, value STRING>>
 LANGUAGE js
 AS
@@ -26,7 +26,7 @@ SELECT
       STRUCT("d" AS key, "{}" AS value),
       STRUCT("e" AS key, "[]" AS value)
     ],
-    json.extract_string_map('{"a":"text","b":1,"c":null,"d":{},"e":[]}')
+    json.js_extract_string_map('{"a":"text","b":1,"c":null,"d":{},"e":[]}')
   ),
-  assert.equals(0, ARRAY_LENGTH(json.extract_string_map('{}'))),
-  assert.null(json.extract_string_map(NULL));
+  assert.equals(0, ARRAY_LENGTH(json.js_extract_string_map('{}'))),
+  assert.null(json.js_extract_string_map(NULL));

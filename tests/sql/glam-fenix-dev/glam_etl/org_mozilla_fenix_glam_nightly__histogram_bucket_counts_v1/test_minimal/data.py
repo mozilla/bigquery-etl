4079d9b51eabe3839d91880c06d88f9a18d9a44e
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-12-02 15:44:34 -0800

    Update minimal test for histogram bucket counts

--- tests/sql/glam-fenix-dev/glam_etl/org_mozilla_fenix_glam_nightly__histogram_bucket_counts_v1/test_minimal/data.py
+++ tests/sql/glam-fenix-dev/glam_etl/org_mozilla_fenix_glam_nightly__histogram_bucket_counts_v1/test_minimal/data.py
@@ -1,6 +1,7 @@
 """Testing data for query."""
 
 from pathlib import Path
+from itertools import product
 
 import yaml
 
@@ -9,16 +10,19 @@ ROOT = Path(__file__).parent
 UUID = "df735f02-efe5-4b07-b212-583bb99ba241"
 SUBMISSION_DATE = "2020-10-01"
 APP_BUILD_ID = "2020100100"
+OS = "Android"
+PING_TYPE = "metrics"
 
-# NOTE: what happens when channel = "*"?
+# See the scalar_bucket_counts minimal example for more details on the
+# preconditions.
 CLIENTS_HISTOGRAM_AGGREGATES = [
     {
         "sample_id": 1,
         "client_id": UUID,
-        "ping_type": "*",
-        "os": "*",
+        "ping_type": PING_TYPE,
+        "os": OS,
         "app_version": 84,
-        "app_build_id": "*",
+        "app_build_id": APP_BUILD_ID,
         "channel": "*",
         "histogram_aggregates": [
             {
@@ -27,41 +31,34 @@ CLIENTS_HISTOGRAM_AGGREGATES = [
                 "key": "",
                 "agg_type": "summed_histogram",
                 "value": [
-                    {"key": "112863206", "value": 1},
-                    {"key": "123078199", "value": 0},
+                    {"key": "1", "value": 1},
+                    {"key": "2", "value": 0},
                 ],
             }
         ],
     }
 ]
 
+BASE_ROW = {
+    "agg_type": "summed_histogram",
+    "app_build_id": "*",
+    "app_version": 84,
+    "channel": "*",
+    "key": "",
+    "metric": "network_tcp_connection",
+    "metric_type": "timing_distribution",
+    "os": "*",
+    "ping_type": "*",
+    "range_max": 2,
+    "record": {"key": "1", "value": 1.0},
+}
+
 EXPECT = [
-    {
-        "agg_type": "summed_histogram",
-        "app_build_id": "*",
-        "app_version": 84,
-        "channel": "*",
-        "key": "",
-        "metric": "network_tcp_connection",
-        "metric_type": "timing_distribution",
-        "os": "*",
-        "ping_type": "*",
-        "range_max": 123_078_199,
-        "record": {"key": "112863206", "value": 1.0},
-    },
-    {
-        "agg_type": "summed_histogram",
-        "app_build_id": "*",
-        "app_version": 84,
-        "channel": "*",
-        "key": "",
-        "metric": "network_tcp_connection",
-        "metric_type": "timing_distribution",
-        "os": "*",
-        "ping_type": "*",
-        "range_max": 123_078_199,
-        "record": {"key": "123078199", "value": 0.0},
-    },
+    {**BASE_ROW, **dict(zip(["record", "ping_type", "os", "app_build_id"], values))}
+    for values in product(
+        [{"key": "1", "value": 1.0}, {"key": "2", "value": 0.0}],
+        *zip([PING_TYPE, OS, APP_BUILD_ID], ["*"] * 3),
+    )
 ]
 
 prefix = "glam_etl"

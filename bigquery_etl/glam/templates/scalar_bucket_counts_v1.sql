af35f4f5e7f04e7a20ed998b1a23dcbd919c247a
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-12-02 12:16:18 -0800

    Fix egregious double counting in scalar bucket counts

--- bigquery_etl/glam/templates/scalar_bucket_counts_v1.sql
+++ bigquery_etl/glam/templates/scalar_bucket_counts_v1.sql
@@ -2,9 +2,6 @@
 {% include "scalar_bucket_counts_v1.udf.sql" %}
 {% from 'macros.sql' import enumerate_table_combinations %}
 
-{# TODO: remove this import by factoring it out as a proper udf #}
-{% include "clients_scalar_aggregates_v1.udf.sql" %}
-
 WITH
 {{
     enumerate_table_combinations(
@@ -14,20 +11,6 @@ WITH
         attribute_combinations
     )
 }},
--- Ensure there is a single record per client id
-deduplicated_combos AS (
-  SELECT
-    client_id,
-    {{ attributes }},
-    udf_merged_user_data(
-      ARRAY_CONCAT_AGG(scalar_aggregates)
-    ) AS scalar_aggregates
-  FROM
-    all_combos
-  GROUP BY
-    client_id,
-    {{ attributes }}
-),
 bucketed_booleans AS (
   SELECT
     client_id,
@@ -37,17 +20,17 @@ bucketed_booleans AS (
     NULL AS bucket_count,
     udf_boolean_buckets(scalar_aggregates) AS scalar_aggregates,
   FROM
-    deduplicated_combos
+    all_combos
 ),
 log_min_max AS (
   SELECT
     metric,
     key,
-    LOG(IF(MIN(value) <= 0, 1, MIN(value)), 2) range_min,
-    LOG(IF(MAX(value) <= 0, 1, MAX(value)), 2) range_max,
+    LOG(IF(MIN(value) <= 0, 1, MIN(value)), 2) as range_min,
+    LOG(IF(MAX(value) <= 0, 1, MAX(value)), 2) as range_max,
     100 as bucket_count
   FROM
-    deduplicated_combos
+    all_combos
     CROSS JOIN UNNEST(scalar_aggregates)
   WHERE
     metric_type <> "boolean"
@@ -76,7 +59,7 @@ bucketed_scalars AS (
       FORMAT("%.*f", 2, mozfun.glam.histogram_bucket_from_value(buckets, value) + 0.0001)
       AS STRING) AS bucket
   FROM
-    deduplicated_combos
+    all_combos
   CROSS JOIN UNNEST(scalar_aggregates)
   LEFT JOIN buckets_by_metric
     USING(metric, key)
@@ -119,7 +102,7 @@ SELECT
   range_max,
   bucket_count,
   bucket,
-  COUNT(*) AS count
+  COUNT(distinct client_id) AS count
 FROM
   booleans_and_scalars
 GROUP BY

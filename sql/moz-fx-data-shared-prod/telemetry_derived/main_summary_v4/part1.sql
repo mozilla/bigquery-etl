5c2647d7b2d20f1cf902aefadd76404d0fb42b98
Anna Scholtz <anna@scholtzan.net>
2020-12-03 09:19:10 -0800

    Reduce complexity in main_summary

--- sql/moz-fx-data-shared-prod/telemetry_derived/main_summary_v4/part1.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/main_summary_v4/part1.sql
@@ -45,6 +45,25 @@ CREATE TEMP FUNCTION extract_popup_notification_stats(h STRING) AS (
   )
 );
 
+WITH extracted_histograms AS (
+  SELECT
+    document_id,
+    client_id,
+    sample_id,
+    mozfun.hist.extract(payload.histograms.ssl_handshake_result) AS ssl_handshake_result,
+    mozfun.hist.extract(
+      payload.histograms.plugins_notification_shown
+    ) AS plugins_notification_shown,
+    mozfun.hist.extract(
+      payload.histograms.plugins_notification_user_action
+    ) AS plugins_notification_user_action
+  FROM
+    `moz-fx-data-shared-prod.telemetry.main`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND normalized_app_name = 'Firefox'
+    AND document_id IS NOT NULL
+)
 SELECT
   document_id,
   client_id,
@@ -355,16 +374,13 @@ SELECT
   ) AS events,
   -- bug 1339655
   SAFE_CAST(
-    mozfun.map.get_key(
-      mozfun.hist.extract(payload.histograms.ssl_handshake_result).values,
-      0
-    ) AS INT64
+    mozfun.map.get_key(extracted_histograms.ssl_handshake_result.values, 0) AS INT64
   ) AS ssl_handshake_result_success,
   (
     SELECT
       IFNULL(SUM(value), 0)
     FROM
-      UNNEST(mozfun.hist.extract(payload.histograms.ssl_handshake_result).values)
+      UNNEST(extracted_histograms.ssl_handshake_result.values)
     WHERE
       key
       BETWEEN 1
@@ -375,7 +391,7 @@ SELECT
       CAST(key AS STRING) AS key,
       value
     FROM
-      UNNEST(mozfun.hist.extract(payload.histograms.ssl_handshake_result).values)
+      UNNEST(extracted_histograms.ssl_handshake_result.values)
     WHERE
       key
       BETWEEN 0
@@ -432,35 +448,20 @@ SELECT
   ) AS blank_window_shown,
   -- bug 1362520 and 1526278 - plugin notifications
   SAFE_CAST(
-    mozfun.map.get_key(
-      mozfun.hist.extract(payload.histograms.plugins_notification_shown).values,
-      1
-    ) AS INT64
+    mozfun.map.get_key(extracted_histograms.plugins_notification_shown.values, 1) AS INT64
   ) AS plugins_notification_shown,
   SAFE_CAST(
-    mozfun.map.get_key(
-      mozfun.hist.extract(payload.histograms.plugins_notification_shown).values,
-      0
-    ) AS INT64
+    mozfun.map.get_key(extracted_histograms.plugins_notification_shown.values, 0) AS INT64
   ) AS plugins_notification_shown_false,
   STRUCT(
     SAFE_CAST(
-      mozfun.map.get_key(
-        mozfun.hist.extract(payload.histograms.plugins_notification_user_action).values,
-        0
-      ) AS INT64
+      mozfun.map.get_key(extracted_histograms.plugins_notification_user_action.values, 0) AS INT64
     ) AS allow_now,
     SAFE_CAST(
-      mozfun.map.get_key(
-        mozfun.hist.extract(payload.histograms.plugins_notification_user_action).values,
-        1
-      ) AS INT64
+      mozfun.map.get_key(extracted_histograms.plugins_notification_user_action.values, 1) AS INT64
     ) AS allow_always,
     SAFE_CAST(
-      mozfun.map.get_key(
-        mozfun.hist.extract(payload.histograms.plugins_notification_user_action).values,
-        2
-      ) AS INT64
+      mozfun.map.get_key(extracted_histograms.plugins_notification_user_action.values, 2) AS INT64
     ) AS block
   ) AS plugins_notification_user_action,
   udf.extract_histogram_sum(payload.histograms.plugins_infobar_shown) AS plugins_infobar_shown,
@@ -556,6 +557,10 @@ SELECT
   ) AS loop_activity_counter
 FROM
   `moz-fx-data-shared-prod.telemetry.main`
+LEFT JOIN
+  extracted_histograms
+USING
+  (document_id, client_id, sample_id)
 WHERE
   DATE(submission_timestamp) = @submission_date
   AND normalized_app_name = 'Firefox'

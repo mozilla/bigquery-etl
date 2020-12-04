5fbd6bdc63e1a282fc2cef45b358fcef8bcd278a
Jeff Klukas <jklukas@mozilla.com>
2020-12-04 09:34:47 -0500

    Add main_1pct view (#1571)
    * Add main_1pct view
    
    Discussed with BQ engineering in [support case 25679061](https://console.cloud.google.com/support/cases/detail/25679061?project=moz-fx-data-shared-prod)
    
    * Scheduling
    
    * Shredder
    
    * Add subsample_id
    
    * Allow field addition
    
    * Field addition
    
    * yamllint
    
    * Document expiration

---
+++ sql/moz-fx-data-shared-prod/telemetry/main_1pct/view.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.main_1pct`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    `moz-fx-data-shared-prod.udf.normalize_main_payload`(payload) AS payload
+  )
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.main_1pct_v1`
---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/main_1pct_v1/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS
+  `moz-fx-data-shared-prod.telemetry_derived.main_1pct_v1`
+PARTITION BY
+  DATE(submission_timestamp)
+CLUSTER BY
+  normalized_channel,
+  sample_id,
+  subsample_id
+OPTIONS
+  (require_partition_filter = TRUE, partition_expiration_days = 180)
+AS
+SELECT
+  CAST(NULL AS INT64) AS subsample_id,
+  *
+FROM
+  telemetry_stable.main_v4
+WHERE
+  FALSE
+  AND sample_id = 0

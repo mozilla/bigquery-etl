3a59918f62172c4cc2c7e26e1ea15587e390bbf6
Sunah Suh <ssuh@mozilla.com>
2020-12-11 12:46:02 -0600

    Add search dashboard desktop search aggregates by userstate table and schedule daily

---
+++ dags/bqetl_search_dashboard.py
@@ -0,0 +1,47 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/master/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.operators.sensors import ExternalTaskSensor
+import datetime
+from utils.gcp import bigquery_etl_query, gke_command
+
+default_args = {
+    "owner": "ssuh@mozilla.com",
+    "start_date": datetime.datetime(2020, 12, 14, 0, 0),
+    "email": ["telemetry-alerts@mozilla.com", "ssuh@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+}
+
+with DAG(
+    "bqetl_search_dashboard", default_args=default_args, schedule_interval="0 4 * * *"
+) as dag:
+
+    search_derived__desktop_search_aggregates_by_userstate__v1 = bigquery_etl_query(
+        task_id="search_derived__desktop_search_aggregates_by_userstate__v1",
+        destination_table="desktop_search_aggregates_by_userstate_v1",
+        dataset_id="search_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="xluo@mozilla.com",
+        email=["ssuh@mozilla.com", "telemetry-alerts@mozilla.com", "xluo@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
+    wait_for_telemetry_derived__clients_last_seen__v1 = ExternalTaskSensor(
+        task_id="wait_for_telemetry_derived__clients_last_seen__v1",
+        external_dag_id="bqetl_main_summary",
+        external_task_id="telemetry_derived__clients_last_seen__v1",
+        execution_delta=datetime.timedelta(seconds=7200),
+        check_existence=True,
+        mode="reschedule",
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    search_derived__desktop_search_aggregates_by_userstate__v1.set_upstream(
+        wait_for_telemetry_derived__clients_last_seen__v1
+    )

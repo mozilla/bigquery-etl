d0b2f2b37af8a58603f2512b32a122da9b3aaf0b
Xuan Luo <xluo@mozilla.com>
2020-12-11 14:20:48 -0800

    Add desktop_search_aggregates_for_searchreport

---
+++ sql/moz-fx-data-shared-prod/search/desktop_search_aggregates_for_searchreport/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.search.desktop_search_aggregates_for_searchreport`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.search_derived.desktop_search_aggregates_for_searchreport_v1`

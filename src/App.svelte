0242b057cb0741eb7d8d9b8714d7d133a13db1b3
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-12-23 16:51:36 -0800

    Move most of the logic into the svelte app

---
+++ src/App.svelte
@@ -0,0 +1,193 @@
+<script>
+  import { onMount } from "svelte";
+
+  // view-source:https://visjs.github.io/vis-network/examples/network/exampleApplications/loadingBar.html
+  function getDatasetId(name) {
+    return name.split(".")[0];
+  }
+
+  onMount(async () => {
+    let edges = await fetch("data/edges.json").then((resp) => resp.json());
+    let nodes = new Set();
+    for (let i = 0; i < edges.length; i++) {
+      nodes.add(edges[i].destination);
+      nodes.add(edges[i].referenced);
+    }
+    // datasets are nodes too now, but assigned to a different group
+    let datasets = new Set();
+    nodes.forEach((name) => {
+      datasets.add(getDatasetId(name));
+    });
+
+    // check for intersection, which will break network visualization
+    let intersect = new Set([...nodes].filter((n) => datasets.has(n)));
+    if (intersect.size > 0) {
+      console.log("intersection between nodes and datasets found");
+      console.log(intersect);
+    }
+
+    let nodeMap = new Map([...nodes, ...datasets].map((el, idx) => [el, idx]));
+
+    let data = {
+      nodes: new vis.DataSet(
+        [...nodes]
+          .map((el) => ({
+            id: nodeMap.get(el),
+            // we show the name without the project prefix to reduce clutter
+            label: el.split(":")[1],
+            group: 0,
+            title: el,
+          }))
+          .concat(
+            [...datasets].map((el) => ({
+              id: nodeMap.get(el),
+              label: el,
+              group: 1,
+              title: el,
+            }))
+          )
+      ),
+      edges: new vis.DataSet(
+        edges
+          .map((el) => ({
+            from: nodeMap.get(el.referenced),
+            to: nodeMap.get(el.destination),
+          }))
+          // .slice(0, 100)
+          .concat(
+            [...nodes].map((el) => ({
+              from: nodeMap.get(getDatasetId(el)),
+              to: nodeMap.get(el),
+            }))
+          )
+      ),
+    };
+    // create a network
+    var container = document.getElementById("mynetwork");
+    var options = {
+      nodes: {
+        shape: "dot",
+        size: 16,
+      },
+      physics: {
+        forceAtlas2Based: {
+          gravitationalConstant: -26,
+          centralGravity: 0.005,
+          springLength: 230,
+          springConstant: 0.18,
+        },
+        maxVelocity: 146,
+        solver: "forceAtlas2Based",
+        timestep: 0.35,
+        stabilization: {
+          enabled: true,
+          iterations: 200,
+          updateInterval: 10,
+        },
+      },
+      layout: {
+        improvedLayout: false,
+      },
+      interaction: {
+        tooltipDelay: 200,
+        hideEdgesOnDrag: true,
+      },
+      edges: {
+        smooth: true,
+        arrows: { to: true },
+      },
+    };
+    var network = new vis.Network(container, data, options);
+    network.on("stabilizationProgress", function (params) {
+      document.getElementById("progress").innerHTML =
+        Math.round((params.iterations / params.total) * 100) + "%";
+    });
+
+    function setSummary(root) {
+      if (root.label == null) {
+        console.log("did not set summary");
+        return;
+      }
+      let div = document.getElementById("summary");
+      // clear the summary
+      div.innerHTML = "";
+      // dump stringified json into the summary
+      let parents = data.nodes.get(network.getConnectedNodes(root.id, "from"));
+      let children = data.nodes.get(network.getConnectedNodes(root.id, "to"));
+      console.log(root);
+      console.log(parents);
+      // pretty output
+      let output = {
+        root: root.title,
+        links: {
+          references: parents.map((node) => node.title),
+          destinations: children.map((node) => node.title),
+        },
+      };
+
+      div.innerHTML =
+        `<h3>Summary of ${root.label} </h3><pre>` +
+        JSON.stringify(output, null, 2) +
+        "</pre>";
+    }
+
+    network.on("selectNode", (obj) => {
+      let root = data.nodes.get(obj.nodes)[0];
+      setSummary(root);
+    });
+    network.once("stabilizationIterationsDone", function () {
+      document.getElementById("progress").innerHTML = "100%";
+      setTimeout(function () {
+        document.getElementById("status").style.display = "none";
+        // set a default summary
+        setSummary(
+          data.nodes.get(
+            nodeMap.get("moz-fx-data-shared-prod:telemetry_stable.main_v4")
+          )
+        );
+      }, 500);
+    });
+  });
+</script>
+
+<style type="text/css">
+  #container {
+    width: 800px;
+    margin: 0 auto;
+  }
+  #mynetwork {
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+    border: 1px solid lightgray;
+  }
+</style>
+
+<div id="container">
+  <h1>BigQuery ETL Query Network</h1>
+  <i>Created 2020-06-18.</i>
+  <p>
+    This network represents the relationships between tables in BigQuery. Each
+    blue node represents a table, while each orange node represents a dataset.
+    The network was created by scraping the BigQuery TABLES and JOBS_BY_PROJECT
+    tables in the INFORMATION_SCHEMA dataset. Views are resolved using bq with
+    --dry_run.
+  </p>
+  <p>
+    Scroll and drag to navigate the network. Selecting a node by double clicking
+    will show summary information about the table or dataset.
+  </p>
+  <p>
+    The source can be found at
+    <a
+      href="https://github.com/acmiyaguchi/etl-graph">acmiyaguchi/etl-graph</a>.
+    See
+    <a
+      href="https://github.com/acmiyaguchi/etl-graph/blob/main/README.md">NOTES.md</a>
+    for a detailed overview of development. This visualizaton is powered by
+    <a href="https://visjs.github.io/vis-network/docs/network/">vis-network</a>.
+  </p>
+  <div id="mynetwork" />
+  <p id="status">Loading <span id="progress">0%</span></p>
+  <div id="summary" />
+</div>

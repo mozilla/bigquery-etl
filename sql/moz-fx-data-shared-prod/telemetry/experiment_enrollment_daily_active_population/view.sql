87fce2e846a16d8e7347ca6f50f457674e8465a6
Anna Scholtz <anna@scholtzan.net>
2021-01-07 11:52:50 -0800

    Experiment enrollment monitoring queries as views

--- sql/moz-fx-data-shared-prod/telemetry/experiment_enrollment_daily_active_population/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/experiment_enrollment_daily_active_population/view.sql
@@ -2,6 +2,15 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.experiment_enrollment_daily_active_population`
 AS
 SELECT
-  *
+  CAST(date_add(submission_date, INTERVAL 1 day) AS timestamp) AS time,
+  experiment_id,
+  SUM(active_clients) AS active_clients
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_daily_active_population_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.experiments_daily_active_clients_v1`
+WHERE
+  submission_date = @submission_date
+GROUP BY
+  1,
+  2
+ORDER BY
+  1

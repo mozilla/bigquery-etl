ee2d979987c17faf4fe364e61c21cc10e39b1231
Anna Scholtz <anna@scholtzan.net>
2021-01-07 09:13:56 -0800

    Experiment enrollment aggregates base query

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_aggregates_recents_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_aggregates_recents_v1/query.sql
@@ -1,36 +1,11 @@
 SELECT
-  event_object AS `type`,
-  event_string_value AS experiment,
-  mozfun.map.get_key(event_map_values, 'branch') AS branch,
-  TIMESTAMP_ADD(
-    TIMESTAMP_TRUNC(`timestamp`, HOUR),
-    -- Aggregates event counts over 5-minute intervals
-    INTERVAL(DIV(EXTRACT(MINUTE FROM `timestamp`), 5) * 5) MINUTE
-  ) AS window_start,
-  TIMESTAMP_ADD(
-    TIMESTAMP_TRUNC(`timestamp`, HOUR),
-    INTERVAL((DIV(EXTRACT(MINUTE FROM `timestamp`), 5) + 1) * 5) MINUTE
-  ) AS window_end,
-  COUNTIF(event_method = 'enroll') AS enroll_count,
-  COUNTIF(event_method = 'unenroll') AS unenroll_count,
-  COUNTIF(event_method = 'graduate') AS graduate_count,
-  COUNTIF(event_method = 'update') AS update_count,
-  COUNTIF(event_method = 'enrollFailed') AS enroll_failed_count,
-  COUNTIF(event_method = 'unenrollFailed') AS unenroll_failed_count,
-  COUNTIF(event_method = 'updateFailed') AS update_failed_count
+  *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.events_live`
+  `moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_aggregates_base`
 WHERE
-  event_category = 'normandy'
-  AND timestamp > (
+  timestamp > (
     SELECT
       MAX(timestamp)
     FROM
       `moz-fx-data-shared-prod.telemetry.experiment_enrollment_aggregates_hourly`
   )
-GROUP BY
-  `type`,
-  experiment,
-  branch,
-  window_start,
-  window_end

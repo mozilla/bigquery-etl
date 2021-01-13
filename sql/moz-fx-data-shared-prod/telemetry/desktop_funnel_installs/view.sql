15294b8ed56eb3b061a7dd1d2d98305ebc8615a7
William Lachance <wrlach@gmail.com>
2021-01-13 12:18:19 -0500

    WIP

---
+++ sql/moz-fx-data-shared-prod/telemetry/desktop_funnel_installs/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.desktop_funnel_installs`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.desktop_funnel_installs_v1`
---
+++ sql/moz-fx-data-shared-prod/telemetry/desktop_funnel_new_profiles/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.desktop_funnel_new_profiles`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.desktop_funnel_new_profiles_v1`

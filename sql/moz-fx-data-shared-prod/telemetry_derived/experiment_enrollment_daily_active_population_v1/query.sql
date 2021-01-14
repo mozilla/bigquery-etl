a1fd2d3556aa7a17ea229d2a09563919e21694ba
Anna Scholtz <anna@scholtzan.net>
2021-01-14 11:45:39 -0800

    Fix daily_active_population query to match schema

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_daily_active_population_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_daily_active_population_v1/query.sql
@@ -1,6 +1,6 @@
 SELECT
   CAST(date_add(submission_date, INTERVAL 1 day) AS timestamp) AS time,
-  experiment_id,
+  experiment_id AS experiment,
   SUM(active_clients) AS value
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.experiments_daily_active_clients_v1`

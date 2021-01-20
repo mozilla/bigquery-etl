355143eda5ce4af965c7120e4a34ae15ca1e5a95
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-01-20 13:55:55 -0800

    Remove survey from dryrun exceptions

--- bigquery_etl/dryrun.py
+++ bigquery_etl/dryrun.py
@@ -74,7 +74,6 @@ SKIP = {
     "sql/moz-fx-data-shared-prod/stripe_external/products_v1/query.sql",
     "sql/moz-fx-data-shared-prod/stripe_external/setup_intents_v1/query.sql",
     "sql/moz-fx-data-shared-prod/stripe_external/subscriptions_v1/query.sql",
-    *glob.glob("sql/moz-fx-data-shared-prod/mozilla_vpn/survey_*/view.sql"),
     "sql/moz-fx-data-shared-prod/mozilla_vpn_derived/add_device_events_v1/query.sql",
     "sql/moz-fx-data-shared-prod/mozilla_vpn_derived/login_flows_v1/query.sql",
     "sql/moz-fx-data-shared-prod/mozilla_vpn_derived/protected_v1/query.sql",

02adc1c10aff85322ebb93020de12429f07e11d7
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-01-20 14:36:32 -0800

    Bug 1683765 - Add views to survey tables in the derived dataset (#1661)
    * Add views to survey tables in the external dataset
    
    * Add mozilla_vpn survey as exceptions to dry run
    
    * Point the view to the correct location
    
    * Move surveys from external to derived
    
    * Move views point to derived instead of external
    
    * Remove survey from dryrun exceptions

---
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/survey_cancellation_of_service/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozilla_vpn.survey_cancellation_of_service`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod`.mozilla_vpn_derived.survey_cancellation_of_service_v1

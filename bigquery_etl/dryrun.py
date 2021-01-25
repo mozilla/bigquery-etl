60ec9f781178d2821e0b1f3c6e47267d175c0e59
Jeff Klukas <jklukas@mozilla.com>
2021-01-25 17:01:53 -0500

    Remove views for fxa_users_services_* (#1716)
    * Remove views for fxa_users_services_*
    
    See https://github.com/mozilla/bigquery-etl/issues/1713#issuecomment-767122674
    
    I will delete these views manually after merging.
    
    * Indentation fix
    
    * Add to SKIP in dryrun

--- bigquery_etl/dryrun.py
+++ bigquery_etl/dryrun.py
@@ -47,6 +47,8 @@ SKIP = {
     "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_log_auth_events_v1/query.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_log_content_events_v1/query.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_log_device_command_events_v1/query.sql",  # noqa E501
+    "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_services_first_seen_v1/query.sql",  # noqa E501
+    "sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_services_last_seen_v1/query.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/telemetry_derived/addons_daily_v1/query.sql",
     "sql/moz-fx-data-shared-prod/search_derived/search_clients_last_seen_v1/init.sql",
     "sql/moz-fx-data-shared-prod/search_derived/search_clients_last_seen_v1/query.sql",
--- sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_services_first_seen/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_accounts.fxa_users_services_first_seen`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_services_first_seen_v1`

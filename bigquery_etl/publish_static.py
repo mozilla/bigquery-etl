e64dc92151a50f47b9c04a503a3bf396d7bc4da4
Jeff Klukas <jeff@klukas.net>
2021-01-26 13:33:14 -0500

    lint fix

--- bigquery_etl/publish_static.py
+++ bigquery_etl/publish_static.py
@@ -78,7 +78,7 @@ def main():
 
     # This machinery is only compatible with
     # the sql/moz-fx-data-shared-prod/static directory.
-    projects = project_dirs('moz-fx-data-shared-prod')
+    projects = project_dirs("moz-fx-data-shared-prod")
 
     for data_dir in projects:
         for root, dirs, files in os.walk(data_dir):

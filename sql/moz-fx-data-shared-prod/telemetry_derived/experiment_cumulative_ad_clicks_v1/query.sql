6c8a7d7fddec7e0ab750d78448f35c88e44cd840
Anna Scholtz <anna@scholtzan.net>
2021-01-27 12:10:44 -0800

    Select cumulative counts from search_aggregates_live in derived search tables

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_cumulative_ad_clicks_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_cumulative_ad_clicks_v1/query.sql
@@ -2,6 +2,6 @@ SELECT
   window_start AS `time`,
   experiment,
   branch,
-  ad_clicks_count AS value
+  cumulative_ad_clicks_count AS value
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.experiment_search_aggregates_live_v1`
--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_cumulative_search_with_ads_count_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_cumulative_search_with_ads_count_v1/query.sql
@@ -2,6 +2,6 @@ SELECT
   window_start AS `time`,
   experiment,
   branch,
-  search_with_ads_count AS value
+  cumulative_search_with_ads_count AS value
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.experiment_search_aggregates_live_v1`

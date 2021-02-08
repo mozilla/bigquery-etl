b05bb61758b367a60d49d5805f1c19b1d87d94e9
Anna Scholtz <anna@scholtzan.net>
2021-02-08 12:26:54 -0800

    Add fenix to experiments search metrics monitoring

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_aggregates_base/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_aggregates_base/view.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry_derived.experiment_search_aggregates_base`
 AS
-WITH live_and_stable AS (
+WITH telemetry_live_and_stable AS (
   SELECT
     *,
     'telemetry_stable' AS dataset_id
@@ -13,49 +13,105 @@ WITH live_and_stable AS (
     'telemetry_live' AS dataset_id
   FROM
     `moz-fx-data-shared-prod.telemetry_live.main_v4`
+),
+desktop AS (
+  SELECT
+    date(submission_timestamp) AS submission_date,
+    dataset_id,
+    unnested_experiments.key AS experiment,
+    unnested_experiments.value AS branch,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      -- Aggregates event counts over 5-minute intervals
+      INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
+    ) AS window_end,
+    SUM(unnested_ad_clicks.value) AS ad_clicks_count,
+    SUM(unnested_search_with_ads.value) AS search_with_ads_count,
+    SUM(unnested_search_counts.count) AS search_count,
+  FROM
+    telemetry_live_and_stable,
+    UNNEST(
+      ARRAY(SELECT AS STRUCT key, value.branch AS value FROM UNNEST(environment.experiments))
+    ) AS unnested_experiments,
+    UNNEST(payload.processes.parent.keyed_scalars.browser_search_ad_clicks) AS unnested_ad_clicks,
+    UNNEST(
+      payload.processes.parent.keyed_scalars.browser_search_with_ads
+    ) AS unnested_search_with_ads,
+    UNNEST(
+      ARRAY(
+        SELECT AS STRUCT
+          SUBSTR(_key, 0, pos - 2) AS engine,
+          SUBSTR(_key, pos) AS source,
+          `moz-fx-data-shared-prod`.udf.extract_histogram_sum(value) AS `count`
+        FROM
+          UNNEST(payload.keyed_histograms.search_counts),
+          UNNEST([REPLACE(key, 'in-content.', 'in-content:')]) AS _key,
+          UNNEST([LENGTH(REGEXP_EXTRACT(_key, '.+[.].'))]) AS pos
+      )
+    ) AS unnested_search_counts
+  GROUP BY
+    submission_date,
+    dataset_id,
+    experiment,
+    branch,
+    window_start,
+    window_end
+),
+fenix_live_and_stable AS (
+  SELECT
+    *,
+    'org_mozilla_fenix_stable' AS dataset_id
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_fenix_stable.metrics_v1`
+  UNION ALL
+  SELECT
+    *,
+    'org_mozilla_fenix_live' AS dataset_id
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_fenix_live.metrics_v1`
+),
+fenix AS (
+  SELECT
+    date(submission_timestamp) AS submission_date,
+    "org_mozilla_fenix_live" AS dataset_id,
+    experiment.key AS experiment,
+    experiment.value.branch AS branch,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      -- Aggregates event counts over 5-minute intervals
+      INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
+    ) AS window_end,
+    SUM(unnested_ad_clicks.value) AS ad_clicks_count,
+    SUM(unnested_search_with_ads.value) AS search_with_ads_count,
+    SUM(unnested_search_counts.value) AS search_count,
+  FROM
+    fenix_live_and_stable,
+    UNNEST(metrics.labeled_counter.browser_search_ad_clicks) AS unnested_ad_clicks,
+    UNNEST(metrics.labeled_counter.browser_search_with_ads) AS unnested_search_with_ads,
+    UNNEST(metrics.labeled_counter.metrics_search_count) AS unnested_search_counts,
+    UNNEST(ping_info.experiments) AS experiment
+  GROUP BY
+    submission_date,
+    dataset_id,
+    experiment,
+    branch,
+    window_start,
+    window_end
 )
 SELECT
-  date(submission_timestamp) AS submission_date,
-  dataset_id,
-  unnested_experiments.key AS experiment,
-  unnested_experiments.value AS branch,
-  TIMESTAMP_ADD(
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR),
-    -- Aggregates event counts over 5-minute intervals
-    INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
-  ) AS window_start,
-  TIMESTAMP_ADD(
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR),
-    INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
-  ) AS window_end,
-  SUM(unnested_ad_clicks.value) AS ad_clicks_count,
-  SUM(unnested_search_with_ads.value) AS search_with_ads_count,
-  SUM(unnested_search_counts.count) AS search_count,
+  *
 FROM
-  live_and_stable,
-  UNNEST(
-    ARRAY(SELECT AS STRUCT key, value.branch AS value FROM UNNEST(environment.experiments))
-  ) AS unnested_experiments,
-  UNNEST(payload.processes.parent.keyed_scalars.browser_search_ad_clicks) AS unnested_ad_clicks,
-  UNNEST(
-    payload.processes.parent.keyed_scalars.browser_search_with_ads
-  ) AS unnested_search_with_ads,
-  UNNEST(
-    ARRAY(
-      SELECT AS STRUCT
-        SUBSTR(_key, 0, pos - 2) AS engine,
-        SUBSTR(_key, pos) AS source,
-        `moz-fx-data-shared-prod`.udf.extract_histogram_sum(value) AS `count`
-      FROM
-        UNNEST(payload.keyed_histograms.search_counts),
-        UNNEST([REPLACE(key, 'in-content.', 'in-content:')]) AS _key,
-        UNNEST([LENGTH(REGEXP_EXTRACT(_key, '.+[.].'))]) AS pos
-    )
-  ) AS unnested_search_counts
-GROUP BY
-  submission_date,
-  dataset_id,
-  experiment,
-  branch,
-  window_start,
-  window_end
+  desktop
+UNION ALL
+SELECT
+  *
+FROM
+  fenix

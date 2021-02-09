d50c2368aa0e60d4d738209b76db48acc6505086
William Lachance <wlachance@mozilla.com>
2021-02-09 09:57:39 -0500

    Make `truncate_version` return NUMERIC (#1772)
    This should work reliably for all the cases we care about and
    allows doing things like sorting by versions reliably (since one
    can't assume lexicographical ordering).
    
    Using NUMERIC should protect us from weird floating point edge cases:
    
    https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#decimal_types

--- sql/mozfun/norm/truncate_version/udf.sql
+++ sql/mozfun/norm/truncate_version/udf.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE FUNCTION norm.truncate_version(os_version STRING, truncation_level STRING)
-RETURNS STRING AS (
+RETURNS NUMERIC AS (
   CASE
   WHEN
     truncation_level = "minor"
   THEN
-    REGEXP_EXTRACT(os_version, r"^([0-9]+[.]?[0-9]+).*")
+    CAST(REGEXP_EXTRACT(os_version, r"^([0-9]+[.]?[0-9]+).*") AS NUMERIC)
   WHEN
     truncation_level = "major"
   THEN
-    REGEXP_EXTRACT(os_version, r"^([0-9]+).*")
+    CAST(REGEXP_EXTRACT(os_version, r"^([0-9]+).*") AS NUMERIC)
   ELSE
     NULL
   END
@@ -16,9 +16,10 @@ RETURNS STRING AS (
 
 -- Tests
 SELECT
-  assert.equals("16.1", norm.truncate_version("16.1.1", "minor")),
-  assert.equals("16", norm.truncate_version("16.1.1", "major")),
-  assert.equals("10", norm.truncate_version("10", "minor")),
-  assert.equals("5.1", norm.truncate_version("5.1.5-ubuntu-foobar", "minor")),
-  assert.equals(CAST(NULL AS STRING), norm.truncate_version("5.1.5-ubuntu-foobar", "patch")),
-  assert.equals(CAST(NULL AS STRING), norm.truncate_version("foo-bar", "minor"))
+  assert.equals(16.1, norm.truncate_version("16.1.1", "minor")),
+  assert.equals(16.03, norm.truncate_version("16.03.1", "minor")),
+  assert.equals(16, norm.truncate_version("16.1.1", "major")),
+  assert.equals(10, norm.truncate_version("10", "minor")),
+  assert.equals(5.1, norm.truncate_version("5.1.5-ubuntu-foobar", "minor")),
+  assert.equals(CAST(NULL AS NUMERIC), norm.truncate_version("5.1.5-ubuntu-foobar", "patch")),
+  assert.equals(CAST(NULL AS NUMERIC), norm.truncate_version("foo-bar", "minor"))

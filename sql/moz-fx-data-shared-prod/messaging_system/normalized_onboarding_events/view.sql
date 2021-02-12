df7be5e9d765d7e58573f259762f307f58f3d269
Frank Bertsch <fbertsch@mozilla.com>
2021-02-12 10:35:33 -0500

    Generate events_daily for onboarding events

---
+++ sql/moz-fx-data-shared-prod/messaging_system/normalized_onboarding_events/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.messaging_system.normalized_onboarding_events`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.messaging_system_derived.normalized_onboarding_events_v1`

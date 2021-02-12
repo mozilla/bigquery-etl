0fc399208ac2354d475b3d3923d03323309c2ce8
Daniel Thorn <dthorn@mozilla.com>
2021-02-11 16:45:30 -0800

    Use zetasql to get dependencies for dag generation

--- bigquery_etl/dependency.py
+++ bigquery_etl/dependency.py
@@ -8,9 +8,13 @@ from typing import Tuple
 import click
 import jnius_config
 
+from .view.generate_stable_views import get_stable_table_schemas
+
 # this has to run before jnius can be imported
 for path in (Path(__file__).parent.parent / "target" / "dependency").glob("*.jar"):
     jnius_config.add_classpath(path.resolve().as_posix())
+
+stable_views = None
 
 
 def extract_table_references(sql: str):
@@ -44,6 +48,37 @@ def extract_table_references(sql: str):
     return [".".join(table.toArray()) for table in result.toArray()]
 
 
+def extract_table_references_without_views(path: Path):
+    """Recursively search for non-view tables referenced in the given SQL file."""
+    global stable_views
+
+    for table in extract_table_references(path.read_text()):
+        ref_base = path.parent
+        parts = tuple(table.split("."))
+        for _ in parts:
+            ref_base = ref_base.parent
+        view_path = ref_base.joinpath(*parts, "view.sql")
+        if view_path.is_file():
+            yield from extract_table_references_without_views(view_path)
+        else:
+            # use directory structure to fully qualify table names
+            while len(parts) < 3:
+                parts = (ref_base.name, *parts)
+                ref_base = ref_base.parent
+            if parts[:-2] == ("moz-fx-data-shared-prod",):
+                if stable_views is None:
+                    # lazy read stable views
+                    stable_views = {
+                        tuple(schema.user_facing_view.split(".")): tuple(
+                            schema.stable_table.split(".")
+                        )
+                        for schema in get_stable_table_schemas()
+                    }
+                if parts[-2:] in stable_views:
+                    parts = ("moz-fx-data-shared-prod", *stable_views[parts[-2:]])
+            yield ".".join(parts)
+
+
 @click.group(help=__doc__)
 def dependency():
     """Create the CLI group for dependency commands."""
@@ -58,7 +93,13 @@ def dependency():
     nargs=-1,
     type=click.Path(file_okay=True),
 )
-def show(paths: Tuple[str, ...]):
+@click.option(
+    "--without-views",
+    "--without_views",
+    is_flag=True,
+    help="recursively resolve view references to underlying tables",
+)
+def show(paths: Tuple[str, ...], without_views: bool):
     """Show table references in sql files."""
     distinct_paths = {
         path
@@ -69,7 +110,10 @@ def show(paths: Tuple[str, ...]):
     fail = False
     for path in sorted(distinct_paths):
         try:
-            table_references = extract_table_references(path.read_text())
+            if without_views:
+                table_references = list(extract_table_references_without_views(path))
+            else:
+                table_references = extract_table_references(path.read_text())
         except CalledProcessError as e:
             raise click.ClickException(f"failed to import jnius: {e}")
         except ImportError as e:

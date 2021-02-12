7b11298a3474c45fb18959708d6eabcbdc2958f6
Frank Bertsch <fbertsch@mozilla.com>
2021-02-12 11:18:53 -0500

    Remove extraneous view; drop extra field

--- sql/moz-fx-data-shared-prod/messaging_system/normalized_onboarding_events/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.messaging_system.normalized_onboarding_events`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.messaging_system_derived.normalized_onboarding_events_v1`

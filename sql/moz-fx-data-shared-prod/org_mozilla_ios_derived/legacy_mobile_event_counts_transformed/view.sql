060a998bfaef746cfc2a4b6af80f5128eacd38ea
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-02-16 14:20:21 -0800

    Add labeled counters for bookmarks

--- sql/moz-fx-data-shared-prod/org_mozilla_ios_derived/legacy_mobile_event_counts_transformed/view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_derived/legacy_mobile_event_counts_transformed/view.sql
@@ -1,8 +1,11 @@
-CREATE TEMPORARY FUNCTION labeled_counter(`values` ARRAY<STRUCT<key STRING, value INT64>>) AS (
+CREATE TEMPORARY FUNCTION labeled_counter(
+  `values` ARRAY<STRUCT<key STRING, value INT64>>,
+  labels ARRAY<STRING>
+) AS (
   (
     WITH summed AS (
       SELECT
-        a.key AS k,
+        IF(a.key IN (select * from unnest(labels)), a.key, "__unknown__") AS k,
         SUM(a.value) AS v
       FROM
         UNNEST(`values`) AS a
@@ -25,7 +28,16 @@ WITH extracted AS (
 labeled AS (
   SELECT
     client_id,
-    ARRAY_AGG(IF(object = "bookmark", (method, value), NULL) IGNORE NULLS) AS bookmark
+    ARRAY_AGG(
+      IF(object = "bookmark" AND method = "open", (method, value), NULL) IGNORE NULLS
+    ) AS bookmarks_open,
+    ARRAY_AGG(
+      IF(object = "bookmark" AND method = "add", (method, value), NULL) IGNORE NULLS
+    ) AS bookmarks_add,
+    ARRAY_AGG(
+      IF(object = "bookmark" AND method = "delete", (method, value), NULL) IGNORE NULLS
+    ) AS bookmarks_delete,
+    -- bookmarks_view
   FROM
     extracted
   GROUP BY
@@ -33,9 +45,18 @@ labeled AS (
 )
 SELECT
   client_id,
-  -- background is can be calculated too
+  -- background can be calculated too
   SUM(IF(object = "app" AND method = "foreground", value, 0)) AS validation_foreground_count,
-  any_value(labeled_counter(bookmark)) AS labeled_counter_bookmarks,
+  any_value(
+    labeled_counter(bookmarks_open, ["awesomebar-results", "bookmarks-panel"])
+  ) AS labeled_counter_bookmarks_open,
+  -- NOTE: should share-menu actually be context-menu?
+  any_value(
+    labeled_counter(bookmarks_add, ["page-action-menu", "share-menu", "activity-stream"])
+  ) AS labeled_counter_bookmarks_add,
+  any_value(
+    labeled_counter(bookmarks_delete, ["page-action-menu", "activity-stream", "bookmarks-panel"])
+  ) AS labeled_counter_bookmarks_delete,
   SUM(IF(object = "reader-mode-open-button", value, 0)) AS counter_reader_mode_open,
   SUM(IF(object = "reader-mode-close-button", value, 0)) AS counter_reader_mode_close,
 FROM

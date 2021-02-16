a0da366fa8169af1d4d8491ef389ef32eb8c7aae
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-02-16 15:29:39 -0800

    Add gleanified structure

--- sql/moz-fx-data-shared-prod/org_mozilla_ios_derived/legacy_mobile_event_counts_transformed/view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_derived/legacy_mobile_event_counts_transformed/view.sql
@@ -49,47 +49,73 @@ labeled AS (
     extracted
   GROUP BY
     1
+),
+aggregated AS (
+  SELECT
+    client_id,
+  -- background can be calculated too
+    SUM(
+      IF(object = "app" AND method = "foreground", value, 0)
+    ) AS counter_glean_validation_foreground_count,
+    any_value(
+      labeled_counter(bookmarks_open, ["awesomebar-results", "bookmarks-panel"])
+    ) AS labeled_counter_bookmarks_open,
+  -- NOTE: should share-menu actually be context-menu?
+    any_value(
+      labeled_counter(bookmarks_add, ["page-action-menu", "share-menu", "activity-stream"])
+    ) AS labeled_counter_bookmarks_add,
+    any_value(
+      labeled_counter(bookmarks_delete, ["page-action-menu", "activity-stream", "bookmarks-panel"])
+    ) AS labeled_counter_bookmarks_delete,
+    SUM(IF(object = "reader-mode-open-button", value, 0)) AS counter_reader_mode_open,
+    SUM(IF(object = "reader-mode-close-button", value, 0)) AS counter_reader_mode_close,
+    any_value(
+      labeled_counter(
+        reading_list_add,
+        ["reader-mode-toolbar", "share-extension", "page-action-menu"]
+      )
+    ) AS labeled_counter_reading_list_add,
+    any_value(
+      labeled_counter(reading_list_delete, ["reader-mode-toolbar", "reading-list-panel"])
+    ) AS labeled_counter_reading_list_delete,
+    SUM(
+      IF(object = "reading-list-item" AND method = "open", value, 0)
+    ) AS counter_reading_list_open,
+    SUM(
+      IF(object = "reading-list-item" AND method = "mark-as-read", value, 0)
+    ) AS counter_reading_mark_read,
+    SUM(
+      IF(object = "reading-list-item" AND method = "mark-as-unread", value, 0)
+    ) AS counter_reading_mark_unread,
+    SUM(IF(object LIKE "qr-code%" AND method = "scan", value, 0)) AS counter_qr_code_scanned,
+  FROM
+    extracted
+  JOIN
+    labeled
+  USING
+    (client_id)
+  GROUP BY
+    1
 )
 SELECT
-  client_id,
-  -- background can be calculated too
-  SUM(IF(object = "app" AND method = "foreground", value, 0)) AS validation_foreground_count,
-  any_value(
-    labeled_counter(bookmarks_open, ["awesomebar-results", "bookmarks-panel"])
-  ) AS labeled_counter_bookmarks_open,
-  -- NOTE: should share-menu actually be context-menu?
-  any_value(
-    labeled_counter(bookmarks_add, ["page-action-menu", "share-menu", "activity-stream"])
-  ) AS labeled_counter_bookmarks_add,
-  any_value(
-    labeled_counter(bookmarks_delete, ["page-action-menu", "activity-stream", "bookmarks-panel"])
-  ) AS labeled_counter_bookmarks_delete,
-  SUM(IF(object = "reader-mode-open-button", value, 0)) AS counter_reader_mode_open,
-  SUM(IF(object = "reader-mode-close-button", value, 0)) AS counter_reader_mode_close,
-  any_value(
-    labeled_counter(
-      reading_list_add,
-      ["reader-mode-toolbar", "share-extension", "page-action-menu"]
-    )
-  ) AS labeled_counter_reading_list_add,
-  any_value(
-    labeled_counter(reading_list_delete, ["reader-mode-toolbar", "reading-list-panel"])
-  ) AS labeled_counter_reading_list_delete,
-  SUM(IF(object = "reading-list-item" AND method = "open", value, 0)) AS counter_reading_list_open,
-  SUM(
-    IF(object = "reading-list-item" AND method = "mark-as-read", value, 0)
-  ) AS counter_reading_mark_read,
-  SUM(
-    IF(object = "reading-list-item" AND method = "mark-as-unread", value, 0)
-  ) AS counter_reading_mark_unread,
-    SUM(
-    IF(object like "qr-code%" AND method = "scan", value, 0)
-  ) AS counter_qr_code_scanned,
+  STRUCT(client_id) AS client_info,
+  STRUCT(
+    STRUCT(
+      counter_glean_validation_foreground_count AS glean_validation_foreground_count,
+      counter_reader_mode_open AS reader_mode_open,
+      counter_reader_mode_close AS reader_mode_close,
+      counter_reading_list_open AS reading_list_open,
+      counter_reading_list_read AS reading_list_read,
+      counter_reading_list_unread AS reading_list_unread,
+      counter_qr_code_scanned AS qr_code_scanned,
+    ) AS counter,
+    STRUCT(
+      labeled_counter_bookmarks_open AS bookmarks_open,
+      labeled_counter_bookmarks_add AS bookmarks_add,
+      labeled_counter_bookmarks_delete AS bookmarks_delete,
+      labeled_counter_reading_list_add AS reading_list_add,
+      labeled_counter_reading_list_delete AS reading_list_delete
+    ) AS labeled_counter
+  ) AS metrics
 FROM
-  extracted
-JOIN
-  labeled
-USING
-  (client_id)
-GROUP BY
-  1
+  aggregated

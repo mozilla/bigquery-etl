4dd540aabc8c2d16f24f80bdb5678eeb096e2a07
Jeff Klukas <jklukas@mozilla.com>
2021-02-23 11:01:46 -0500

    Add mobile_usage_2021 for KPI reporting (CDOU) (#1824)
    * Add mobile_usage_2021 for KPI reporting (CDOU)
    
    Mobile counterpart to https://github.com/mozilla/bigquery-etl/pull/1823
    
    Note that we don't yet have `first_seen_date` ETL for mobile, so can't include
    `new_profiles` here (which we have for desktop).
    
    This is building on the existing nondesktop exact MAU table, which filters on
    `contributes_to_2020_kpi` which so far is identical to the 2021 list, so should
    be fine for now.
    
    We may need to rebuild and clean up this ETL pipeline as we improve ETL for
    Glean apps in H1.
    
    * Add comments and update interface for function changes
    
    * Use updated mozfun.norm.product_info
    
    * Update DAG
    
    * Adapt nondesktop view to new UDF interface
    
    * Add docs about shredder effect

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/mobile_usage_v1/query.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE TABLE
+  `moz-fx-data-shared-prod.telemetry_derived.mobile_usage_v1`
+PARTITION BY
+  submission_date
+AS
+SELECT
+  submission_date,
+  -- Cumulative days of use (CDOU) is a sum over the calendar year.
+  SUM(dau) OVER (
+    PARTITION BY
+      EXTRACT(YEAR FROM submission_date),
+      id_bucket,
+      product,
+      normalized_channel,
+      campaign,
+      country,
+      distribution_id
+  ) AS cdou,
+  mau,
+  wau,
+  dau,
+  id_bucket,
+  product,
+  normalized_channel,
+  campaign,
+  country,
+  distribution_id
+FROM
+  firefox_nondesktop_exact_mau28_v1
+WHERE
+  -- We completely recreate this table every night since the source table
+  -- is small and this query windows over a large time range.
+  submission_date >= '2017-01-01'

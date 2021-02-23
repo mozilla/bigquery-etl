2a87a19fb6afb781deda0615caadb86a64243828
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-02-23 14:33:33 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/mozfun/norm/product_info/generate_body.sql
+++ sql/mozfun/norm/product_info/generate_body.sql
@@ -13,16 +13,18 @@ lines AS (
     UNNEST(SPLIT(input, '\n')) AS line
   WHERE
     line NOT LIKE '%-----%'
-    AND line NOT LIKE '%canonical_name%'
+    AND line NOT LIKE '%canonical_app_name%'
 ),
 structured AS (
   SELECT AS STRUCT
-    fields[OFFSET(0)] AS app_name,
+    fields[OFFSET(0)] AS legacy_app_name,
     fields[OFFSET(1)] AS normalized_os,
-    fields[OFFSET(2)] AS product,
-    fields[OFFSET(3)] AS canonical_name,
-    fields[OFFSET(4)] AS contributes_to_2019_kpi,
-    fields[OFFSET(5)] AS contributes_to_2020_kpi,
+    fields[OFFSET(2)] AS app_name,
+    fields[OFFSET(3)] AS product,
+    fields[OFFSET(4)] AS canonical_app_name,
+    fields[OFFSET(5)] AS contributes_to_2019_kpi,
+    fields[OFFSET(6)] AS contributes_to_2020_kpi,
+    fields[OFFSET(7)] AS contributes_to_2021_kpi,
   FROM
     lines
   WHERE
@@ -37,30 +39,37 @@ unioned AS (
   -- so we duplicate some rows.
   UNION ALL
   SELECT
-    * REPLACE (product AS app_name)
+    * REPLACE (product AS legacy_app_name)
   FROM
     structured
   WHERE
-    product NOT IN (SELECT app_name FROM structured)
+    product NOT IN (SELECT legacy_app_name FROM structured)
 ),
 formatted AS (
   SELECT
     *,
     CONCAT(
       FORMAT(
-        "WHEN app_name LIKE %T AND normalized_os LIKE %T THEN STRUCT(%T AS product, %T AS canonical_name, %s AS contributes_to_2019_kpi, %s AS contributes_to_2020_kpi)",
-        app_name,
+        "WHEN legacy_app_name LIKE %T AND normalized_os LIKE %T THEN STRUCT(%T AS app_name, %T AS product, %T AS canonical_app_name, %T AS canonical_name, %s AS contributes_to_2019_kpi, %s AS contributes_to_2020_kpi, %s AS contributes_to_2021_kpi)",
+        legacy_app_name,
         normalized_os,
+        app_name,
         product,
-        canonical_name,
+        canonical_app_name,
+        canonical_app_name,
         contributes_to_2019_kpi,
-        contributes_to_2020_kpi
+        contributes_to_2020_kpi,
+        contributes_to_2021_kpi
       )
     ) AS case_stmt,
   FROM
     unioned
 )
 SELECT
-  CONCAT("CASE\n", STRING_AGG(case_stmt, "\n"), "\nELSE ('Other', 'Other', false, false) END")
+  CONCAT(
+    "CASE\n",
+    STRING_AGG(case_stmt, "\n"),
+    "\nELSE ('other', 'Other', 'Other', 'Other', FALSE, FALSE, FALSE) END"
+  )
 FROM
   formatted

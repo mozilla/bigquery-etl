e51b403bdd318ce37771928f7867fc51c49d6d90
Jeff Klukas <jeff@klukas.net>
2021-02-23 09:20:11 -0500

    Add comments and update interface for function changes

--- sql/moz-fx-data-shared-prod/telemetry/mobile_usage_2021/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/mobile_usage_2021/view.sql
@@ -4,17 +4,24 @@ AS
 WITH base AS (
   SELECT
     *,
+    -- This is a shim to get an os field based on the existing normalized
+    -- `product` field in the underlying table. We will likely need to overhaul
+    -- the mobile ETL in H1 2021 to provide a Glean-first approach, in which
+    -- case shims like this will no longer be necessary.
     IF(product LIKE '% iOS', 'iOS', 'Android') AS os,
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.mobile_usage_v1`
 ),
-with_product_info AS (
+with_pinfo AS (
   SELECT
     *,
-    mozdata.analysis.klukas_product_info(product, os).*,
+    -- FIXME Use mozfun.norm.product_info once updates are merged:
+    -- https://github.com/mozilla/bigquery-etl/pull/1824
+    mozdata.analysis.klukas_product_info(product, os) AS pinfo,
   FROM
     base
 )
+-- Names here should be consistent with the desktop_usage_2021 view.
 SELECT
   submission_date,
   cdou,
@@ -22,12 +29,12 @@ SELECT
   wau,
   dau,
   id_bucket,
-  looker_app_name AS app_name,
-  canonical_app_name,
+  pinfo.app_name,
+  pinfo.canonical_app_name,
   normalized_channel AS channel,
   os,
   campaign,
   country,
   distribution_id
 FROM
-  with_product_info
+  with_pinfo

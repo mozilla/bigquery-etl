db8f79e0bd285991e0e455e5d3f044ad9f26b7de
Frank Bertsch <fbertsch@mozilla.com>
2021-03-01 15:42:55 -0500

    Fix docs and tests

--- sql/mozfun/bytes/extract_bits/udf.sql
+++ sql/mozfun/bytes/extract_bits/udf.sql
@@ -12,9 +12,11 @@ RETURNS BYTES AS (
 
 -- Tests
 SELECT
-  extract_bits(b'\x01\xFE', 8, 8) = b'\xFF',
-  extract_bits(b'\xFF', 5, 4) = b'\xF0',
-  extract_bits(REPEAT(b'\xFF', 100), 50, 8) = b'\xFF',
-  extract_bits(b'\x0F\xF0', -12, 8) = b'\xFF',
-  extract_bits(b'\x0F\x77', 0, 8) = b'\x0F',
-  extract_bits(b'\x0F\xF0', -10, 8) = b'\xFC',
+  bytes.extract_bits(b'\x01\xFE', 8, 8) = b'\xFF',
+  bytes.extract_bits(b'\xFF', 5, 4) = b'\xF0',
+  bytes.extract_bits(REPEAT(b'\xFF', 100), 50, 8) = b'\xFF',
+  bytes.extract_bits(b'\x0F\xF0', -12, 8) = b'\xFF',
+  bytes.extract_bits(b'\x0F\x77', 0, 8) = b'\x0F',
+  bytes.extract_bits(b'\x0F\xF0', -10, 8) = b'\xFC',
+  bytes.extract_bits(b'\x0F\xF0', 5, 8) = b'\xFF',
+  bytes.extract_bits(b'\x0C\xC0', -12, 8) = b'\xCC'

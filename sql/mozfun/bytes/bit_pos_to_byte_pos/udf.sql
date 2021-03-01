adfac8fafcc5360a06505e014bff9b6f9201cb0c
Frank Bertsch <frank@franks-mbp.attlocal.net>
2021-03-01 09:49:53 -0500

    Add UDFs for bytes manipulation

--- sql/mozfun/bytes/bit_pos_to_byte_pos/udf.sql
+++ sql/mozfun/bytes/bit_pos_to_byte_pos/udf.sql
@@ -1,12 +1,14 @@
-
-                -- Definition for bytes.bit_pos_to_byte_pos
-                -- For more information on writing UDFs see:
-                -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
-CREATE OR REPLACE FUNCTION bytes.bit_pos_to_byte_pos()
-RETURNS BOOLEAN AS (
-  TRUE
+CREATE OR REPLACE FUNCTION bytes.bit_pos_to_byte_pos(bit_pos INT64)
+RETURNS INT64 AS (
+  CAST(CEIL(ABS(bit_pos) / 8) AS INT64) * SIGN(bit_pos)
 );
 
-                -- Tests
+-- Tests
 SELECT
-  assert.true(bytes.bit_pos_to_byte_pos())
+  assert.equals(bytes.bit_pos_to_byte_pos(0), 0),
+  assert.equals(bytes.bit_pos_to_byte_pos(1), 1),
+  assert.equals(bytes.bit_pos_to_byte_pos(-1), -1),
+  assert.equals(bytes.bit_pos_to_byte_pos(8), 1),
+  assert.equals(bytes.bit_pos_to_byte_pos(-8), -1),
+  assert.equals(bytes.bit_pos_to_byte_pos(9), 2),
+  assert.equals(bytes.bit_pos_to_byte_pos(-9), -2),
--- sql/mozfun/bytes/zero_right/udf.sql
+++ sql/mozfun/bytes/zero_right/udf.sql
@@ -1,12 +1,14 @@
-
-                -- Definition for bytes.zero_right
-                -- For more information on writing UDFs see:
-                -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
-CREATE OR REPLACE FUNCTION bytes.zero_right()
-RETURNS BOOLEAN AS (
-  TRUE
+CREATE OR REPLACE FUNCTION bytes.zero_right(b BYTES, length INT64)
+RETURNS BYTES AS (
+  IF(
+    length > LENGTH(b) * 8,
+    ERROR("Cannot zero out more bits than are present"),
+    b >> length << length
+  )
 );
 
-                -- Tests
+-- Tests
 SELECT
-  assert.true(bytes.zero_right())
+  assert.equals(b'\xF0', bytes.zero_right(b'\xFF', 4)),
+  assert.equals(b'\xFF', bytes.zero_right(b'\xFF', 0)),
+  assert.equals(b'\x00', bytes.zero_right(b'\xFF', 8)),

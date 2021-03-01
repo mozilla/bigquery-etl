3af85d93ebc1e095ead908223664785960b2608b
Jeff Klukas <jeff@klukas.net>
2021-03-01 09:37:55 -0500

    Add a method to fetch dataset labels from dryrun service results

--- bigquery_etl/dryrun.py
+++ bigquery_etl/dryrun.py
@@ -328,6 +328,24 @@ class DryRun:
 
         return {}
 
+    def get_dataset_labels(self):
+        """Return the labels on the default dataset by dry running the SQL file."""
+        if self.sqlfile not in SKIP and not self.is_valid():
+            raise Exception(f"Error when dry running SQL file {self.sqlfile}")
+
+        if self.sqlfile in SKIP:
+            print(f"\t...Ignoring dryrun results for {self.sqlfile}")
+            return {}
+
+        if (
+            self.dry_run_result
+            and self.dry_run_result["valid"]
+            and "datasetLabels" in self.dry_run_result
+        ):
+            return self.dry_run_result["dataset_labels"]
+
+        return {}
+
     def is_valid(self):
         """Dry run the provided SQL file and check if valid."""
         if self.dry_run_result is None:

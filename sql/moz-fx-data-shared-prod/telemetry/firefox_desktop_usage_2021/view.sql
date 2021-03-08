d7e0571816c18a0b298fbc88e543724ee8632b27
Jeff Klukas <jklukas@mozilla.com>
2021-03-08 09:47:36 -0500

    Bug: Fill in transient slices in CDOU calculations (#1873)
    * Bug: Fill in transient slices in CDOU calculations
    
    Additional bug fix on https://github.com/mozilla/bigquery-etl/pull/1872
    
    * COALESCE nulls to zero in views

--- sql/moz-fx-data-shared-prod/telemetry/firefox_desktop_usage_2021/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/firefox_desktop_usage_2021/view.sql
@@ -2,6 +2,12 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.firefox_desktop_usage_2021`
 AS
 SELECT
-  *
+  * REPLACE (
+    COALESCE(cdou, 0) AS cdou,
+    COALESCE(mau, 0) AS mau,
+    COALESCE(wau, 0) AS wau,
+    COALESCE(dau, 0) AS dau,
+    COALESCE(new_profiles, 0) AS new_profiles
+  )
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.firefox_desktop_usage_v1`

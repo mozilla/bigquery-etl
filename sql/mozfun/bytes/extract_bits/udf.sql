66812114f6aa8c67188f411e367f3bf2c186e49b
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-03-08 20:16:12 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/mozfun/bytes/extract_bits/udf.sql
+++ sql/mozfun/bytes/extract_bits/udf.sql
@@ -2,22 +2,26 @@ CREATE OR REPLACE FUNCTION bytes.extract_bits(b BYTES, `begin` INT64, length INT
 RETURNS BYTES AS (
   SUBSTR(
     mozfun.bytes.zero_right(
-      b << MOD(IF(`begin` > 0, `begin` - 1, LENGTH(b) * 8 + `begin`), 8),
-      (mozfun.bytes.bit_pos_to_byte_pos(length) * 8 - length)
+      b << IF(`begin` >= 0, GREATEST(`begin` - 1, 0), LENGTH(b) * 8 + `begin`),
+      GREATEST(8 * LENGTH(b) - length, 0)
     ),
-    mozfun.bytes.bit_pos_to_byte_pos(`begin`),
+    1,
     mozfun.bytes.bit_pos_to_byte_pos(length)
   )
 );
 
 -- Tests
 SELECT
-  bytes.extract_bits(b'\x01\xFE', 8, 8) = b'\xFF',
-  bytes.extract_bits(b'\xFF', 5, 4) = b'\xF0',
-  bytes.extract_bits(REPEAT(b'\xFF', 100), 50, 8) = b'\xFF',
-  bytes.extract_bits(b'\x0F\xF0', -12, 8) = b'\xFF',
-  bytes.extract_bits(b'\x0F\x77', 0, 8) = b'\x0F',
-  bytes.extract_bits(b'\x0F\xF0', -10, 8) = b'\xFC',
-  bytes.extract_bits(b'\x0F\xF0', 5, 8) = b'\xFF',
-  bytes.extract_bits(b'\x0C\xC0', -12, 8) = b'\xCC',
-  bytes.extract_bits(b'\xFF', -4, 1) = b'\x80'
+  assert.equals(b'\xFF', bytes.extract_bits(b'\x01\xFE', 8, 8)),
+  assert.equals(b'\xF0', bytes.extract_bits(b'\xFF', 5, 4)),
+  assert.equals(b'\xFF', bytes.extract_bits(REPEAT(b'\xFF', 100), 50, 8)),
+  assert.equals(b'\xFF', bytes.extract_bits(b'\x0F\xF0', -12, 8)),
+  assert.equals(b'\x0F', bytes.extract_bits(b'\x0F\x77', 0, 8)),
+  assert.equals(b'\xFC', bytes.extract_bits(b'\x0F\xF0', -10, 8)),
+  assert.equals(b'\xFF', bytes.extract_bits(b'\x0F\xF0', 5, 8)),
+  assert.equals(b'\xCC', bytes.extract_bits(b'\x0C\xC0', -12, 8)),
+  assert.equals(b'\x80', bytes.extract_bits(b'\xFF', -4, 1)),
+  assert.equals(b'\xC0', bytes.extract_bits(b'\xFF\xFF', 2, 2)),
+  assert.equals(b'\x80', bytes.extract_bits(b'\xFF\xFF', 6, 1)),
+  assert.equals(b'\x80', bytes.extract_bits(b'\xFF\xFF', 1, 1)),
+  assert.equals(b'\xFF', bytes.extract_bits(b'\xFF', 1, 20)),

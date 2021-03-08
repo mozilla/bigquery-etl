26c0f67ba6c71349baf3395ee9cc85e6709c9b4b
Anna Scholtz <anna@scholtzan.net>
2021-03-08 14:55:18 -0800

    Add materialized views for experiment search metrics

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_firefox_beta_derived/experiment_search_events_live_v1/init.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE MATERIALIZED VIEW `moz-fx-data-shared-prod.org_mozilla_fenix_beta_derived.experiment_search_events_live_v1`
+AS
+WITH fenix AS (
+  SELECT
+    submission_timestamp,
+    experiment.key AS experiment,
+    experiment.value.branch AS branch,
+    unnested_ad_clicks.value AS ad_clicks_count,
+    unnested_search_with_ads.value AS search_with_ads_count,
+    unnested_search_counts.value AS search_count,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_fenix_beta_live.metrics_v1`,
+    UNNEST(metrics.labeled_counter.browser_search_ad_clicks) AS unnested_ad_clicks,
+    UNNEST(metrics.labeled_counter.browser_search_with_ads) AS unnested_search_with_ads,
+    UNNEST(metrics.labeled_counter.metrics_search_count) AS unnested_search_counts,
+    UNNEST(ping_info.experiments) AS experiment
+)
+SELECT
+  date(submission_timestamp) AS submission_date,
+  experiment,
+  branch,
+  TIMESTAMP_ADD(
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+    -- Aggregates event counts over 5-minute intervals
+    INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
+  ) AS window_start,
+  TIMESTAMP_ADD(
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+    INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
+  ) AS window_end,
+  SUM(ad_clicks_count) AS ad_clicks_count,
+  SUM(search_with_ads_count) AS search_with_ads_count,
+  SUM(search_count) AS search_count,
+FROM
+  fenix
+GROUP BY
+  submission_date,
+  experiment,
+  branch,
+  window_start,
+  window_end

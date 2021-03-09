819e9a51660cac5784b260432683e6bd25f4ef82
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-03-09 18:13:06 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix_derived/experiment_search_events_live_v1/init.sql
@@ -0,0 +1,57 @@
+-- This and the following materialized view need to be kept in sync:
+-- - org_mozilla_fenix_derived.experiment_search_events_live_v1
+-- - org_mozilla_firefox_beta_derived.experiment_search_events_live_v1
+-- - org_mozilla_firefox_derived.experiment_search_events_live_v1
+CREATE MATERIALIZED VIEW
+IF
+  NOT EXISTS `moz-fx-data-shared-prod.org_mozilla_fenix_derived.experiment_search_events_live_v1`
+  OPTIONS
+    (enable_refresh = TRUE, refresh_interval_minutes = 5)
+  AS
+  WITH fenix AS (
+    SELECT
+      submission_timestamp,
+      experiment.key AS experiment,
+      experiment.value.branch AS branch,
+      unnested_ad_clicks.value AS ad_clicks_count,
+      unnested_search_with_ads.value AS search_with_ads_count,
+      unnested_search_counts.value AS search_count,
+    FROM
+      `moz-fx-data-shared-prod.org_mozilla_fenix_live.metrics_v1`
+    LEFT JOIN
+      UNNEST(metrics.labeled_counter.browser_search_ad_clicks) AS unnested_ad_clicks
+    LEFT JOIN
+      UNNEST(metrics.labeled_counter.browser_search_with_ads) AS unnested_search_with_ads
+    LEFT JOIN
+      UNNEST(metrics.labeled_counter.metrics_search_count) AS unnested_search_counts
+    LEFT JOIN
+      UNNEST(ping_info.experiments) AS experiment
+  )
+  SELECT
+    date(submission_timestamp) AS submission_date,
+    experiment,
+    branch,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+    -- Aggregates event counts over 5-minute intervals
+      INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
+    ) AS window_end,
+    SUM(ad_clicks_count) AS ad_clicks_count,
+    SUM(search_with_ads_count) AS search_with_ads_count,
+    SUM(search_count) AS search_count,
+  FROM
+    fenix
+  WHERE
+    -- Limit the amount of data the materialized view is going to backfill when created.
+    -- This date can be moved forward whenever new changes of the materialized views need to be deployed.
+    DATE(submission_timestamp) >= DATE('2021-03-01')
+  GROUP BY
+    submission_date,
+    experiment,
+    branch,
+    window_start,
+    window_end
--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_cumulative_search_with_ads_count_v1/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-  `moz-fx-data-shared-prod.telemetry_derived.experiment_cumulative_search_with_ads_count_v1`(
-    time TIMESTAMP,
-    experiment STRING,
-    branch STRING,
-    value INT64
-  )
-CLUSTER BY
-  experiment

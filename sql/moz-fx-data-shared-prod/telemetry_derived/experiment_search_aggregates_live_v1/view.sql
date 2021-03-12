1da5a7bae22ecefb3c38e1d1b2150080701b8f78
Anna Scholtz <anna@scholtzan.net>
2021-03-12 10:41:21 -0800

    Add clarifications to experiment search aggregate materialized views

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_aggregates_live_v1/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_aggregates_live_v1/view.sql
@@ -14,12 +14,13 @@ WITH all_searches AS (
       TIMESTAMP_TRUNC(submission_timestamp, HOUR),
       INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
     ) AS window_end,
+    -- Extract counts from JSON array
     SUM(
       (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_ad_clicks)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(ad_clicks)) AS value
       )
     ) AS ad_clicks_count,
     SUM(
@@ -27,7 +28,7 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_with_ads)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_with_ads)) AS value
       )
     ) AS search_with_ads_count,
     SUM(
@@ -44,18 +45,18 @@ WITH all_searches AS (
             )
           )
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_count)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_count)) AS value
       )
     ) AS search_count,
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.experiment_search_events_live_v1`
+  WHERE
+    submission_timestamp > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   GROUP BY
     branch,
     experiment,
     window_start,
     window_end
-  WHERE
-    window_start > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   UNION ALL
   SELECT
     experiment,
@@ -69,12 +70,13 @@ WITH all_searches AS (
       TIMESTAMP_TRUNC(submission_timestamp, HOUR),
       INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
     ) AS window_end,
+    -- Extract counts from JSON array
     SUM(
       (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_ad_clicks)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(ad_clicks)) AS value
       )
     ) AS ad_clicks_count,
     SUM(
@@ -82,7 +84,7 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_with_ads)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_with_ads)) AS value
       )
     ) AS search_with_ads_count,
     SUM(
@@ -90,18 +92,18 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_count)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_count)) AS value
       )
     ) AS search_count,
   FROM
     `moz-fx-data-shared-prod.org_mozilla_fenix_derived.experiment_search_events_live_v1`
+  WHERE
+    submission_timestamp > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   GROUP BY
     branch,
     experiment,
     window_start,
     window_end
-  WHERE
-    window_start > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   UNION ALL
   SELECT
     experiment,
@@ -115,12 +117,13 @@ WITH all_searches AS (
       TIMESTAMP_TRUNC(submission_timestamp, HOUR),
       INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
     ) AS window_end,
+    -- Extract counts from JSON array
     SUM(
       (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_ad_clicks)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(ad_clicks)) AS value
       )
     ) AS ad_clicks_count,
     SUM(
@@ -128,7 +131,7 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_with_ads)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_with_ads)) AS value
       )
     ) AS search_with_ads_count,
     SUM(
@@ -136,18 +139,18 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_count)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_count)) AS value
       )
     ) AS search_count,
   FROM
     `moz-fx-data-shared-prod.org_mozilla_firefox_beta_derived.experiment_search_events_live_v1`
+  WHERE
+    submission_timestamp > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   GROUP BY
     branch,
     experiment,
     window_start,
     window_end
-  WHERE
-    window_start > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   UNION ALL
   SELECT
     experiment,
@@ -161,12 +164,13 @@ WITH all_searches AS (
       TIMESTAMP_TRUNC(submission_timestamp, HOUR),
       INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
     ) AS window_end,
+    -- Extract counts from JSON array
     SUM(
       (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_ad_clicks)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(ad_clicks)) AS value
       )
     ) AS ad_clicks_count,
     SUM(
@@ -174,7 +178,7 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_with_ads)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_with_ads)) AS value
       )
     ) AS search_with_ads_count,
     SUM(
@@ -182,18 +186,18 @@ WITH all_searches AS (
         SELECT
           SUM(CAST(REPLACE(value, '"', '') AS INT64))
         FROM
-          UNNEST(JSON_EXTRACT_ARRAY(nested_search_count)) AS value
+          UNNEST(JSON_EXTRACT_ARRAY(search_count)) AS value
       )
     ) AS search_count,
   FROM
     `moz-fx-data-shared-prod.org_mozilla_firefox_derived.experiment_search_events_live_v1`
+  WHERE
+    submission_timestamp > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   GROUP BY
     branch,
     experiment,
     window_start,
     window_end
-  WHERE
-    window_start > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
   UNION ALL
   SELECT
     branch,
@@ -214,8 +218,8 @@ grouped_searches AS (
     experiment,
     window_start,
     window_end,
-    SUM(ad_clicks_count) AS ad_clicks_count_count,
-    SUM(search_with_ads_count) AS search_with_ads_count_count,
+    SUM(ad_clicks_count) AS ad_clicks_count,
+    SUM(search_with_ads_count) AS search_with_ads_count,
     SUM(search_count) AS search_count
   FROM
     all_searches
--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_events_live_v1/init.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_events_live_v1/init.sql
@@ -4,36 +4,52 @@ IF
   OPTIONS
     (enable_refresh = TRUE, refresh_interval_minutes = 5)
   AS
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    TO_JSON_STRING(
-      REGEXP_EXTRACT_ALL(
-        TO_JSON_STRING(payload.processes.parent.keyed_scalars.browser_search_ad_clicks),
-        '"value":([^,}]+)'
-      )
-    ) AS ad_clicks,
-    TO_JSON_STRING(
-      REGEXP_EXTRACT_ALL(
-        TO_JSON_STRING(payload.processes.parent.keyed_scalars.browser_search_with_ads),
-        '"value":([^,}]+)'
-      )
-    ) AS search_with_ads,
-    TO_JSON_STRING(
-      REGEXP_EXTRACT_ALL(TO_JSON_STRING(payload.keyed_histograms.search_counts), '"value":([^,}]+)')
-    ) AS search_count
-  FROM
-    `moz-fx-data-shared-prod.telemetry_live.main_v4`,
-    UNNEST(environment.experiments) AS experiment
-  WHERE
+  WITH event_counts AS (
+    SELECT
+        submission_timestamp,
+        experiment.key AS experiment,
+        experiment.value.branch AS branch,
+        -- Materialized views do not support sub-queries for UNNESTing fields
+        -- Extract the counts from the JSON of the nested struct AS single array
+        TO_JSON_STRING(
+        REGEXP_EXTRACT_ALL(
+            TO_JSON_STRING(payload.processes.parent.keyed_scalars.browser_search_ad_clicks),
+            '"value":([^,}]+)'
+        )
+        ) AS ad_clicks,
+        TO_JSON_STRING(
+        REGEXP_EXTRACT_ALL(
+            TO_JSON_STRING(payload.processes.parent.keyed_scalars.browser_search_with_ads),
+            '"value":([^,}]+)'
+        )
+        ) AS search_with_ads,
+        TO_JSON_STRING(
+        REGEXP_EXTRACT_ALL(TO_JSON_STRING(payload.keyed_histograms.search_counts), '"value":([^,}]+)')
+        ) AS search_count
+    FROM
+        `moz-fx-data-shared-prod.telemetry_live.main_v4`,
+        UNNEST(environment.experiments) AS experiment
+      WHERE
     -- Limit the amount of data the materialized view is going to backfill when created.
     -- This date can be moved forward whenever new changes of the materialized views need to be deployed.
     DATE(submission_timestamp) >= DATE('2021-03-01')
-  GROUP BY
+      GROUP BY
     1,
     2,
     3,
     4,
     5,
     6
+  ) SELECT 
+    experiment,
+    branch,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+    -- Aggregates event counts over 5-minute intervals
+      INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
+    ) AS window_end,
+

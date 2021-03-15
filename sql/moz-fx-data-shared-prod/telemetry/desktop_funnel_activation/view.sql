597822a7929bdb67934252242f2ee15beb8246aa
Anna Scholtz <anna@scholtzan.net>
2021-03-15 15:10:48 -0700

    Fix inflated numbers in desktop funnel queries

--- sql/moz-fx-data-shared-prod/telemetry/desktop_funnel_activation/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.desktop_funnel_activation`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.desktop_funnel_activation_v1`
--- sql/moz-fx-data-shared-prod/telemetry_derived/desktop_funnel_new_profiles_v1/init.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_funnel_new_profiles_v1/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS
-  `moz-fx-data-shared-prod`.telmetry_derived.desktop_funnel_new_profiles_v1(
+  `moz-fx-data-shared-prod`.telemetry_derived.desktop_funnel_new_profiles_v1(
     date DATE,
     country_name STRING,
     channel STRING,

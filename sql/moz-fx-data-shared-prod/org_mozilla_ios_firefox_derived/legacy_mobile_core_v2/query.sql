21c258db49bf429bc0255c080427eddfcc3cb0d3
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-03-15 14:48:55 -0700

    Make v2 schema a subset of the metrics ping

--- sql/moz-fx-data-shared-prod/org_mozilla_ios_firefox_derived/legacy_mobile_core_v2/query.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefox_derived/legacy_mobile_core_v2/query.sql
@@ -3,23 +3,70 @@ CREATE TEMP FUNCTION rename_search(value STRING) AS (
   CONCAT(SPLIT(value, ".")[OFFSET(1)], ".", SPLIT(value, ".")[OFFSET(0)])
 );
 
+CREATE TEMPORARY FUNCTION labeled_counter(
+  `values` ARRAY<STRUCT<key STRING, value INT64>>,
+  labels ARRAY<STRING>
+) AS (
+  (
+    WITH summed AS (
+      SELECT
+        IF(a.key IN (SELECT * FROM UNNEST(labels)), a.key, "__unknown__") AS k,
+        SUM(a.value) AS v
+      FROM
+        UNNEST(`values`) AS a
+      GROUP BY
+        k
+    )
+    SELECT
+      ARRAY_AGG(STRUCT<key STRING, value INT64>(k, v))
+    FROM
+      summed
+  )
+);
+
 WITH extracted AS (
   SELECT
-    document_id,
-    submission_timestamp,
-    client_id,
+    *,
     DATE(submission_timestamp) AS submission_date,
-    searches,
-    default_search,
-    default_mail_client,
-    default_new_tab_experience,
-    open_tab_count
   FROM
     `moz-fx-data-shared-prod.telemetry.core`
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND normalized_os = 'iOS'
     AND normalized_app_name = 'Fennec'
+),
+extracted_meta AS (
+  SELECT
+    * EXCEPT (
+      searches,
+      default_search,
+      default_mail_client,
+      default_new_tab_experience,
+      open_tab_count
+    )
+  FROM
+    extracted
+),
+meta_ranked AS (
+  SELECT
+    t AS metadata,
+    row_number() OVER (
+      PARTITION BY
+        client_id,
+        submission_date
+      ORDER BY
+        submission_timestamp DESC
+    ) AS _n
+  FROM
+    extracted_meta t
+),
+meta AS (
+  SELECT
+    metadata.*
+  FROM
+    meta_ranked
+  WHERE
+    _n = 1
 ),
 searches AS (
   SELECT
@@ -50,8 +97,6 @@ rest_grouped AS (
   SELECT
     submission_date,
     client_id,
-    ANY_VALUE(document_id) AS document_id,
-    min(submission_timestamp) AS submission_timestamp,
     mozfun.stats.mode_last(ARRAY_AGG(default_search)) AS string_search_default_engine,
     mozfun.stats.mode_last(ARRAY_AGG(default_mail_client)) AS string_preferences_mail_client,
     mozfun.stats.mode_last(
@@ -67,10 +112,44 @@ rest_grouped AS (
     2
 )
 SELECT
-  *
+  submission_timestamp,
+  document_id,
+  (SELECT AS STRUCT metadata.* EXCEPT (uri)) AS metadata,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  STRUCT(
+    client_id,
+    CAST(NULL AS string) AS android_sdk_version,
+    metadata.uri.app_build_id AS app_build,
+    metadata.uri.app_update_channel AS app_channel,
+    metadata.uri.app_version AS app_display_version,
+    arch AS architecture,
+    "Apple" AS device_manufacturer,
+    device AS device_model,
+    locale,
+    os,
+    osversion AS os_version,
+    CAST(NULL AS string) AS telemetry_sdk_build
+  ) AS client_info,
+  STRUCT(
+    STRUCT(
+      string_search_default_engine AS search_default_engine,
+      string_preferences_mail_client AS preferences_mail_client,
+      string_preferences_new_tab_experience AS preferences_new_tab_experience
+    ) AS string,
+    STRUCT(counter_tabs_cumulative_count AS tabs_cumulative_count) AS counter,
+    STRUCT(labeled_counter_search_counts AS search_counts) AS labeled_counter
+  ) AS metrics
 FROM
   searches_grouped
 JOIN
   rest_grouped
+USING
+  (submission_date, client_id)
+JOIN
+  meta
 USING
   (submission_date, client_id)

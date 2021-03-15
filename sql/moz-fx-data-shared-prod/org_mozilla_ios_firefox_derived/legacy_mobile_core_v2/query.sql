c6a018aaa657dcb15774b6b2d3c6522436683ddc
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-03-15 10:48:05 -0700

    Add copy of v1 for v2

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefox_derived/legacy_mobile_core_v2/query.sql
@@ -0,0 +1,76 @@
+CREATE TEMP FUNCTION rename_search(value STRING) AS (
+    -- in glean, this is implemented as search-engine-name.source
+  CONCAT(SPLIT(value, ".")[OFFSET(1)], ".", SPLIT(value, ".")[OFFSET(0)])
+);
+
+WITH extracted AS (
+  SELECT
+    document_id,
+    submission_timestamp,
+    client_id,
+    DATE(submission_timestamp) AS submission_date,
+    searches,
+    default_search,
+    default_mail_client,
+    default_new_tab_experience,
+    open_tab_count
+  FROM
+    `moz-fx-data-shared-prod.telemetry.core`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND normalized_os = 'iOS'
+    AND normalized_app_name = 'Fennec'
+),
+searches AS (
+  SELECT
+    submission_date,
+    client_id,
+    rename_search(s.key) AS key,
+    SUM(s.value) AS value
+  FROM
+    extracted,
+    UNNEST(searches) s
+  GROUP BY
+    1,
+    2,
+    3
+),
+searches_grouped AS (
+  SELECT
+    submission_date,
+    client_id,
+    ARRAY_AGG(STRUCT(key, value)) AS labeled_counter_search_counts
+  FROM
+    searches
+  GROUP BY
+    1,
+    2
+),
+rest_grouped AS (
+  SELECT
+    submission_date,
+    client_id,
+    ANY_VALUE(document_id) AS document_id,
+    min(submission_timestamp) AS submission_timestamp,
+    mozfun.stats.mode_last(ARRAY_AGG(default_search)) AS string_search_default_engine,
+    mozfun.stats.mode_last(ARRAY_AGG(default_mail_client)) AS string_preferences_mail_client,
+    mozfun.stats.mode_last(
+      ARRAY_AGG(default_new_tab_experience)
+    ) AS string_preferences_new_tab_experience,
+    -- NOTE: that we cannot recover the average count because we are summing this up into one ping. An
+    -- average might be more desirable, but it doesn't quite match the metric name.
+    sum(open_tab_count) AS counter_tabs_cumulative_count
+  FROM
+    extracted
+  GROUP BY
+    1,
+    2
+)
+SELECT
+  *
+FROM
+  searches_grouped
+JOIN
+  rest_grouped
+USING
+  (submission_date, client_id)
---
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefox_derived/legacy_mobile_event_counts_v2/query.sql
@@ -0,0 +1,73 @@
+WITH extracted AS (
+  SELECT
+    *,
+    date(submission_timestamp) AS submission_date,
+  FROM
+    `moz-fx-data-shared-prod.telemetry_stable.mobile_event_v1`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND normalized_app_name = "Fennec"
+    AND normalized_os = "iOS"
+),
+meta AS (
+  SELECT
+    * EXCEPT (events)
+  FROM
+    extracted
+),
+meta_ranked AS (
+  SELECT
+    t AS metadata,
+    row_number() OVER (
+      PARTITION BY
+        client_id,
+        submission_date
+      ORDER BY
+        submission_timestamp DESC
+    ) AS _n
+  FROM
+    meta t
+),
+meta_recent AS (
+  SELECT
+    metadata.*
+  FROM
+    meta_ranked
+  WHERE
+    _n = 1
+),
+unnested AS (
+  SELECT
+    * EXCEPT (events),
+    mozdata.udf.deanonymize_event(event).*
+  FROM
+    extracted,
+    UNNEST(events) AS event
+),
+counts AS (
+  SELECT
+    client_id,
+    submission_date,
+    event_category AS category,
+    event_method AS method,
+    event_object AS object,
+    event_string_value AS string_value,
+    COUNT(*) AS value
+  FROM
+    unnested
+  GROUP BY
+    client_id,
+    submission_date,
+    category,
+    method,
+    object,
+    string_value
+)
+SELECT
+  *
+FROM
+  counts
+JOIN
+  meta_recent
+USING
+  (client_id, submission_date)

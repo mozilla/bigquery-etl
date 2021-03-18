fb232cf6813a1486eaade8dd139a1e8cd7081710
Anna Scholtz <anna@scholtzan.net>
2021-03-18 09:28:57 -0700

    Script for generating experiment search metrics materialized views

---
+++ script/experiment_monitoring/generate_search_metrics_views.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""
+Generates the experiment monitoring search metrics materialized views.
+
+Materialized views currently do not support UNNESTing of fields in
+sub-queries. To ensure the experiment search metrics numbers are
+correcy, a materialized view has to be created for each dataset and
+each search metric. The materialized views are then combined in a view
+to make querying more convenient.
+"""
+from argparse import ArgumentParser
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+
+import os
+import sys
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+from bigquery_etl.metadata.parse_metadata import Metadata, METADATA_FILE  # noqa E402
+
+
+FILE_PATH = os.path.dirname(__file__)
+BASE_DIR = Path(FILE_PATH).parent.parent
+TARGET_DIR = BASE_DIR / "sql" / "moz-fx-data-shared-prod"
+MATERIALIZED_VIEW_TEMPLATE_PATH = Path(FILE_PATH) / "templates"
+MATERIALIZED_VIEW_TEMPLATE = "materialized_view.sql"
+SEARCH_METRICS = {
+    "fenix": {
+        "base_tables": [
+            "moz-fx-data-shared-prod.org_mozilla_fenix_live.metrics_v1",
+            "moz-fx-data-shared-prod.org_mozilla_firefox_beta_live.metrics_v1",
+            "moz-fx-data-shared-prod.org_mozilla_firefox_live.metrics_v1",
+        ],
+        "metrics": [
+            {"ad_clicks_count": "metrics.labeled_counter.browser_search_ad_clicks"},
+            {
+                "search_with_ads_count": "metrics.labeled_counter.browser_search_with_ads"
+            },
+            {"search_count": "metrics.labeled_counter.metrics_search_count"},
+        ],
+    },
+    "desktop": {
+        "base_tables": ["moz-fx-data-shared-prod.telemetry_live.main_v4"],
+        "metrics": [
+            {
+                "ad_clicks_count": "payload.processes.parent.keyed_scalars.browser_search_ad_clicks"  # noqa E501
+            },
+            {
+                "search_with_ads_count": "payload.processes.parent.keyed_scalars.browser_search_with_ads"  # noqa E501
+            },
+            {"search_count": "payload.keyed_histograms.search_counts"},
+        ],
+    },
+}
+
+
+def generate_materialized_views(owner):
+    """Generate the materialized experiment search metrics views."""
+    for platform, search_metrics in SEARCH_METRICS.items():
+        for base_table in search_metrics["base_tables"]:
+            for metric in search_metrics["metrics"]:
+                # generate materialized view from template
+                env = Environment(
+                    loader=FileSystemLoader(str(MATERIALIZED_VIEW_TEMPLATE_PATH))
+                )
+                template = env.get_template(MATERIALIZED_VIEW_TEMPLATE)
+                derived_dataset = base_table.split(".")[1].replace("_live", "_derived")
+                metric_name = list(metric.keys())[0]
+
+                output = template.render(
+                    platform=platform,
+                    metric=metric_name,
+                    probe=list(metric.values())[0],
+                    dataset=derived_dataset,
+                )
+
+                # create target directory
+                output_dir = (
+                    TARGET_DIR / derived_dataset / f"experiment_{metric_name}_live_v1"
+                )
+                output_dir.mkdir(parents=True, exist_ok=True)
+
+                # write materialized view to SQL file
+                output_file = output_dir / "init.sql"
+                output_file.write_text(output)
+
+                print(f"Generate {output_file}")
+
+                # Generate metadata file
+                metadata = Metadata(
+                    friendly_name=f"Experiment {metric_name} live",
+                    description=(
+                        f"Materialized view of aggregated number of {metric_name} "
+                        "of clients enrolled in experiments."
+                    ),
+                    owners=[owner],
+                    labels={"materialized_view": True},
+                )
+                metadata.write(output_dir / METADATA_FILE)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("--owner", help="Email address of owner for materialized views")
+    args = parser.parse_args()
+    generate_materialized_views(args.owner)
---
+++ script/experiment_monitoring/templates/materialized_view.sql
@@ -0,0 +1,60 @@
+-- Generated via script/experiments_monitoring/generate_search_metrics_views.py
+CREATE MATERIALIZED VIEW
+IF
+  NOT EXISTS `moz-fx-data-shared-prod.{{ dataset }}.experiment_{{ metric }}_live_v1`
+  OPTIONS
+    (enable_refresh = TRUE, refresh_interval_minutes = 5)
+  AS
+  WITH desktop AS (
+    SELECT
+      submission_timestamp,
+      experiment.key AS experiment,
+      experiment.value.branch AS branch,
+      unnested_ad_clicks.value AS ad_clicks_count,
+      unnested_search_with_ads.value AS search_with_ads_count,
+
+      {% if probe == "payload.keyed_histograms.search_counts" %}
+      -- We cannot make UDF calls in a materialized view, so we have to reimplement part of
+      -- mozfun.hist.extract here.
+      SAFE_CAST(
+        COALESCE(
+          JSON_EXTRACT_SCALAR(search_counts.value, '$.sum'),
+          SPLIT(search_counts.value, ';')[SAFE_OFFSET(2)],
+          SPLIT(search_counts.value, ',')[SAFE_OFFSET(1)],
+          search_counts.value
+        ) AS INT64
+      ) AS search_count,
+      {% else %}
+         {{ probe.split(".")[-1] }}.value AS {{ metric }}
+      {% endif %}
+    FROM
+      `{{ base_table }}`,
+      UNNEST(environment.experiments) AS experiment,
+      UNNEST({{ probe }}) AS {{ probe.split(".")[-1] }}
+  )
+  SELECT
+    date(submission_timestamp) AS submission_date,
+    experiment,
+    branch,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+    -- Aggregates event counts over 5-minute intervals
+      INTERVAL(DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) * 5) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(submission_timestamp, HOUR),
+      INTERVAL((DIV(EXTRACT(MINUTE FROM submission_timestamp), 5) + 1) * 5) MINUTE
+    ) AS window_end,
+    SUM({{ metric }}) AS {{ metric }}
+  FROM
+    desktop
+  WHERE
+    -- Limit the amount of data the materialized view is going to backfill when created.
+    -- This date can be moved forward whenever new changes of the materialized views need to be deployed.
+    DATE(submission_timestamp) >= DATE('2021-03-15')
+  GROUP BY
+    submission_date,
+    experiment,
+    branch,
+    window_start,
+    window_end

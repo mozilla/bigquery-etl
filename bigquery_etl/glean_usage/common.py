f3690642363a2d9995943362cff9181b531c76b1
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-03-31 15:22:23 -0700

    Add initial boilerplate for clients_first_seen

--- bigquery_etl/glean_usage/common.py
+++ bigquery_etl/glean_usage/common.py
@@ -83,8 +83,10 @@ def table_names_from_baseline(baseline_table):
         baseline_table=baseline_table,
         daily_table=f"{prefix}_derived.baseline_clients_daily_v1",
         last_seen_table=f"{prefix}_derived.baseline_clients_last_seen_v1",
+        first_seen_table=f"{prefix}_derived.baseline_clients_first_seen_v1",
         daily_view=f"{prefix}.baseline_clients_daily",
         last_seen_view=f"{prefix}.baseline_clients_last_seen",
+        first_seen_view=f"{prefix}.baseline_clients_first_seen",
     )
 
 
---
+++ bigquery_etl/glean_usage/templates/baseline_clients_first_seen_v1.sql
@@ -0,0 +1,56 @@
+{{ header }}
+
+{% if init %}
+
+CREATE TABLE IF NOT EXISTS
+  `{{ first_seen_table }}`
+PARTITION BY
+  submission_date
+CLUSTER BY
+  sample_id
+OPTIONS
+  (require_partition_filter = TRUE)
+AS
+SELECT
+  DATE(submission_timestamp) as submission_date,
+  sample_id,
+  submission_timestamp,
+  client_info.client_id
+FROM
+  `{{ baseline_table }}`
+WHERE
+  FALSE
+
+{% else %}
+
+WITH _current AS (
+  SELECT
+    sample_id,
+    submission_timestamp,
+    client_info.client_id
+  FROM
+    `{{ baseline_table }}`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+),
+  --
+_previous AS (
+  SELECT
+    * EXCEPT (submission_date)
+  FROM
+    `{{ first_seen_table }}`
+  WHERE
+    submission_date = DATE_SUB(@submission_date, INTERVAL 1 DAY)
+)
+  --
+SELECT
+  @submission_date AS submission_date,
+  IF(_current.client_id IS NOT NULL, _current, _previous).*
+FROM
+  _current
+FULL JOIN
+  _previous
+USING
+  (client_id)
+
+{% endif %}

0220938e42ce49f1d45776d7815e2fac7a4bd383
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-03-31 17:08:57 -0700

    [wip] Join data against legacy fennec id if applicable

--- bigquery_etl/glean_usage/templates/baseline_clients_first_seen_v1.sql
+++ bigquery_etl/glean_usage/templates/baseline_clients_first_seen_v1.sql
@@ -22,8 +22,51 @@ WHERE
 
 {% else %}
 
-WITH _current AS (
+WITH
+{% if fennec_id %}
+-- this lookup is ~13GB on release (org_mozilla_firefox) as of 2021-03-31
+_fennec_id_lookup AS (
   SELECT
+    client_info.client_id,
+    MIN(metrics.uuid.migration_telemetry_identifiers_fennec_client_id) AS fennec_client_id
+  FROM
+    `{{ migration_table }}`
+  WHERE
+    DATE(submission_timestamp) > "2010-01-01"
+    AND client_info.client_id IS NOT NULL
+    AND metrics.uuid.migration_telemetry_identifiers_fennec_client_id IS NOT NULL
+  GROUP BY 1
+),
+-- scanning this table is ~25GB
+_core_clients_first_seen AS (
+  SELECT
+    lookup.client_id,
+    first_seen_date
+  FROM
+    _fennec_id_lookup lookup
+  JOIN
+    `moz-fx-data-shared-prod.telemetry_derived.core_clients_first_seen_v1` core
+  ON lookup.fennec_client_id = core.client_id
+
+),
+_current AS (
+  SELECT
+    COALESCE(first_seen_date, @submission_date) as submission_date,
+    sample_id,
+    client_info.client_id
+  FROM
+    `{{ baseline_table }}`
+  LEFT JOIN
+    _core_clients_first_seen
+  USING
+    (client_id)
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+),
+{% else %}
+_current AS (
+  SELECT
+    @submission_date as submission_date,
     sample_id,
     client_info.client_id
   FROM
@@ -31,10 +74,11 @@ WITH _current AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
 ),
+{% endif %}
   --
 _previous AS (
   SELECT
-    * EXCEPT (submission_date)
+    *
   FROM
     `{{ first_seen_table }}`
   WHERE
@@ -42,8 +86,7 @@ _previous AS (
 )
   --
 SELECT
-  @submission_date AS submission_date,
-  IF(_current.client_id IS NOT NULL, _current, _previous).*
+  IF(_previous.client_id IS NOT NULL, _previous, _current).*
 FROM
   _current
 FULL JOIN

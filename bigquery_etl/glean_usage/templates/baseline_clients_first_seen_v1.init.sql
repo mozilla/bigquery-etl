a9035d36c22ab1b61d877904fe80fad6219808aa
Jeff Klukas <jeff@klukas.net>
2021-04-26 14:38:11 -0400

    Bug 1707640 - Calculate sample_id in baseline_clients_first_seen
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1707640#c8

--- bigquery_etl/glean_usage/templates/baseline_clients_first_seen_v1.init.sql
+++ bigquery_etl/glean_usage/templates/baseline_clients_first_seen_v1.init.sql
@@ -16,6 +16,9 @@ WITH
     SELECT
       client_info.client_id,
       sample_id,
+      -- Some Glean data from 2019 contains incorrect sample_id, so we
+      -- recalculate here; see bug 1707640
+      udf.safe_sample_id(client_info.client_id) AS sample_id,
       DATE(MIN(submission_timestamp)) as submission_date,
       DATE(MIN(submission_timestamp)) as first_seen_date,
     FROM

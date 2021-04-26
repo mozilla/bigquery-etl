705f69a8207ea2888fb512c9ec421eabcdfa20b4
Jeff Klukas <jeff@klukas.net>
2021-04-26 11:36:26 -0400

    Add emailverified and isprimary

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_auth_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_auth_events_v1/query.sql
@@ -12,6 +12,8 @@ WITH base AS (
               SELECT AS STRUCT
                 jsonPayload.fields.* EXCEPT (device_id, user_id) REPLACE(
                   -- See https://bugzilla.mozilla.org/show_bug.cgi?id=1707571
+                  CAST(NULL AS FLOAT64) AS emailverified,
+                  CAST(NULL AS FLOAT64) AS isprimary,
                   CAST(NULL AS FLOAT64) AS isverified
                 ),
                 TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id

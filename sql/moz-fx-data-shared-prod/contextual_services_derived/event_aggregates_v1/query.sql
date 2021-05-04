afd8ab333050e8194b747ee6b63f402697ccaea9
Benjamin Wu <benjaminwu124@gmail.com>
2021-05-04 16:20:44 -0400

    tests

--- sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
@@ -4,52 +4,52 @@ WITH combined AS (
     DATE(submission_timestamp) AS submission_date,
     'suggest' AS source,
     'impression' AS event_type,
-    metadata.geo.country,
+    normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
     release_channel,
     position,
   FROM
-    `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_impression_v1`
+    contextual_services_stable.quicksuggest_impression_v1
   UNION ALL
   SELECT
     context_id,
     DATE(submission_timestamp) AS submission_date,
     'suggest' AS source,
     'click' AS event_type,
-    metadata.geo.country,
+    normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
     release_channel,
     position,
   FROM
-    `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_click_v1`
+    contextual_services_stable.quicksuggest_click_v1
   UNION ALL
   SELECT
     context_id,
     DATE(submission_timestamp) AS submission_date,
     'topsites' AS source,
     'impression' AS event_type,
-    metadata.geo.country,
+    normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
     release_channel,
     position,
   FROM
-    `moz-fx-data-shared-prod.contextual_services_stable.topsites_impression_v1`
+    contextual_services_stable.topsites_impression_v1
   UNION ALL
   SELECT
     context_id,
     DATE(submission_timestamp) AS submission_date,
     'topsites' AS source,
     'click' AS event_type,
-    metadata.geo.country,
+    normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
     release_channel,
     position,
   FROM
-    `moz-fx-data-shared-prod.contextual_services_stable.topsites_click_v1`
+    contextual_services_stable.topsites_click_v1
 )
 SELECT
   * EXCEPT (context_id),

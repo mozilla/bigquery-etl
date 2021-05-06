abdb71df0f3326a20fee6b8d32c4d153b91e135a
Benjamin Wu <benjaminwu124@gmail.com>
2021-05-06 14:36:55 -0400

    Filter out high user click counts for adm derived table

--- sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
@@ -50,15 +50,31 @@ WITH combined AS (
     position,
   FROM
     contextual_services_stable.topsites_click_v1
+),
+with_event_count AS (
+  SELECT
+    *,
+    COUNT(*) OVER (
+      PARTITION BY
+        submission_date,
+        context_id,
+        source,
+        event_type
+    ) AS user_event_count,
+  FROM
+    combined
+  ORDER BY
+    context_id
 )
 SELECT
-  * EXCEPT (context_id),
+  * EXCEPT (context_id, user_event_count),
   COUNT(*) AS event_count,
   COUNT(DISTINCT(context_id)) AS user_count,
 FROM
-  combined
+  with_event_count
 WHERE
   submission_date = @submission_date
+  AND NOT (user_event_count > 50 AND event_type = 'click')
 GROUP BY
   submission_date,
   source,

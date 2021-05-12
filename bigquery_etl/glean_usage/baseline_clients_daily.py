f4e66e2bc8eb291e2ad492c07d246c6f042d863a
Anna Scholtz <anna@scholtzan.net>
2021-05-12 16:23:05 -0700

    Generate cross channel views

--- bigquery_etl/glean_usage/baseline_clients_daily.py
+++ bigquery_etl/glean_usage/baseline_clients_daily.py
@@ -1,19 +1,13 @@
 """Generating and run baseline_clients_daily queries for Glean apps."""
-from bigquery_etl.glean_usage import common
+from bigquery_etl.glean_usage.common import GleanTable
 
 BASELINE_DAILY_TABLE_ID = "baseline_clients_daily_v1"
 PREFIX = "daily"
 
 
-def generate(project_id, baseline_table, output_dir=None, output_only=False):
-    """Generate the baseline table query."""
-    common.generate(
-        project_id,
-        baseline_table,
-        PREFIX,
-        BASELINE_DAILY_TABLE_ID,
-        {},
-        no_init=False,
-        output_dir=output_dir,
-        output_only=output_only,
-    )
+class BaselineClientsDailyTable(GleanTable):
+    def __init__(self):
+        self.target_table_id = BASELINE_DAILY_TABLE_ID
+        self.prefix = PREFIX
+        self.custom_render_kwargs = {}
+        self.no_init = False
--- bigquery_etl/glean_usage/baseline_clients_last_seen.py
+++ bigquery_etl/glean_usage/baseline_clients_last_seen.py
@@ -1,25 +1,17 @@
 """Generate and run baseline_clients_last_seen queries for Glean apps."""
 
-from bigquery_etl.glean_usage import common
+from bigquery_etl.glean_usage.common import GleanTable
 
 TARGET_TABLE_ID = "baseline_clients_last_seen_v1"
 PREFIX = "last_seen"
 USAGE_TYPES = ("seen", "created_profile", "seen_session_start", "seen_session_end")
 
 
-def generate(project_id, baseline_table, output_dir=None, output_only=False):
-    """Generate the baseline table query."""
-    render_kwargs = dict(
-        usage_types=USAGE_TYPES,
-    )
-
-    common.generate(
-        project_id,
-        baseline_table,
-        PREFIX,
-        TARGET_TABLE_ID,
-        render_kwargs,
-        no_init=False,
-        output_dir=output_dir,
-        output_only=output_only,
-    )
+class BaselineClientsLastSeenTable(GleanTable):
+    def __init__(self):
+        self.target_table_id = TARGET_TABLE_ID
+        self.prefix = PREFIX
+        self.custom_render_kwargs = dict(
+            usage_types=USAGE_TYPES,
+        )
+        self.no_init = False

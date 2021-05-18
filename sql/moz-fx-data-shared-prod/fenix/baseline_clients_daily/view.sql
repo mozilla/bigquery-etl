0d86f6360719678453e2d6c87365ecdf2d138801
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-05-18 19:12:14 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/fenix/baseline_clients_daily/view.sql
@@ -0,0 +1,28 @@
+-- Generated via ./bqetl glean_usage generate
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fenix.baseline_clients_daily`
+AS
+SELECT
+  * REPLACE ("release" AS normalized_channel)
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox.baseline_clients_daily`
+UNION ALL
+SELECT
+  * REPLACE ("beta" AS normalized_channel)
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_beta.baseline_clients_daily`
+UNION ALL
+SELECT
+  * REPLACE ("nightly" AS normalized_channel)
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix.baseline_clients_daily`
+UNION ALL
+SELECT
+  * REPLACE ("nightly" AS normalized_channel)
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix_nightly.baseline_clients_daily`
+UNION ALL
+SELECT
+  * REPLACE ("nightly" AS normalized_channel)
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fennec_aurora.baseline_clients_daily`

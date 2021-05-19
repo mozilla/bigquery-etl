ff42d1bef3e8ef042d752d4c5c01a1bf82562ca4
Anna Scholtz <anna@scholtzan.net>
2021-05-19 08:43:08 -0700

    Prefix unnested event fields

--- bigquery_etl/glean_usage/templates/cross_channel_events_unnested.view.sql
+++ bigquery_etl/glean_usage/templates/cross_channel_events_unnested.view.sql
@@ -6,11 +6,32 @@ AS
 {% if not loop.first -%}
 UNION ALL
 {% endif -%}
-{% if app_name == "fenix" -%}
-SELECT e.* EXCEPT (events) REPLACE(mozfun.norm.fenix_app_info("{{ dataset }}", client_info.app_build).channel AS normalized_channel), event.*
-{% else -%}
-SELECT e.* EXCEPT (events) REPLACE("{{ channel }}" AS normalized_channel), event.*
-{% endif -%}
-FROM `{{ project_id }}.{{ dataset }}.events` e
-LEFT JOIN UNNEST(e.events) AS event
+
+SELECT 
+    e.*
+    EXCEPT (events, metrics)
+    REPLACE(
+        {% if app_name == "fenix" -%}
+        mozfun.norm.fenix_app_info("{{ dataset }}", client_info.app_build).channel AS normalized_channel,
+        {% else -%}
+        "{{ channel }}" AS normalized_channel,
+        {% endif -%}
+        -- Order of ping_info fields differs between tables; we're verbose here for compatibility
+        STRUCT (
+            ping_info.end_time,
+            ping_info.experiments,
+            ping_info.ping_type,
+            ping_info.seq,
+            ping_info.start_time,
+            ping_info.reason,
+            ping_info.parsed_start_time,
+            ping_info.parsed_end_time
+        ) AS ping_info
+    ),
+    event.timestamp AS event_timestamp,
+    event.category AS event_category,
+    event.name AS event_name,
+    event.extra AS event_extra,
+FROM `{{ project_id }}.{{ dataset }}.events` AS e
+CROSS JOIN UNNEST(e.events) AS event
 {% endfor %}

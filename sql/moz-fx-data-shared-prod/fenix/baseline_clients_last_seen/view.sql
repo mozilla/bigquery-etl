0539dc4c0d39bf722eb76c3839ded4d40ef9c41a
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-05-19 17:13:58 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/fenix/baseline_clients_last_seen/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/baseline_clients_last_seen/view.sql
@@ -3,26 +3,36 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.fenix.baseline_clients_last_seen`
 AS
 SELECT
-  * REPLACE ("release" AS normalized_channel)
+  * REPLACE (
+    mozfun.norm.fenix_app_info("org_mozilla_firefox", app_build).channel AS normalized_channel
+  )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_firefox.baseline_clients_last_seen`
 UNION ALL
 SELECT
-  * REPLACE ("beta" AS normalized_channel)
+  * REPLACE (
+    mozfun.norm.fenix_app_info("org_mozilla_firefox_beta", app_build).channel AS normalized_channel
+  )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_firefox_beta.baseline_clients_last_seen`
 UNION ALL
 SELECT
-  * REPLACE ("nightly" AS normalized_channel)
+  * REPLACE (
+    mozfun.norm.fenix_app_info("org_mozilla_fenix", app_build).channel AS normalized_channel
+  )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_fenix.baseline_clients_last_seen`
 UNION ALL
 SELECT
-  * REPLACE ("nightly" AS normalized_channel)
+  * REPLACE (
+    mozfun.norm.fenix_app_info("org_mozilla_fenix_nightly", app_build).channel AS normalized_channel
+  )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_fenix_nightly.baseline_clients_last_seen`
 UNION ALL
 SELECT
-  * REPLACE ("nightly" AS normalized_channel)
+  * REPLACE (
+    mozfun.norm.fenix_app_info("org_mozilla_fennec_aurora", app_build).channel AS normalized_channel
+  )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_fennec_aurora.baseline_clients_last_seen`

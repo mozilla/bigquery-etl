b10eeaa98a9868114fc3fa1e24608779c11eb3e5
Jeff Klukas <jklukas@mozilla.com>
2021-05-20 14:42:53 -0400

    Deschedule fxa_auth_bounce_events query (#2052)
    See https://github.com/mozilla/bigquery-etl/issues/2051#issuecomment-845362964

--- dags/bqetl_fxa_events.py
+++ dags/bqetl_fxa_events.py
@@ -61,19 +61,6 @@ with DAG(
         dag=dag,
     )
 
-    firefox_accounts_derived__fxa_auth_bounce_events__v1 = bigquery_etl_query(
-        task_id="firefox_accounts_derived__fxa_auth_bounce_events__v1",
-        destination_table="fxa_auth_bounce_events_v1",
-        dataset_id="firefox_accounts_derived",
-        project_id="moz-fx-data-shared-prod",
-        owner="jklukas@mozilla.com",
-        email=["jklukas@mozilla.com", "telemetry-alerts@mozilla.com"],
-        date_partition_parameter="submission_date",
-        depends_on_past=False,
-        arguments=["--schema_update_option=ALLOW_FIELD_ADDITION"],
-        dag=dag,
-    )
-
     firefox_accounts_derived__fxa_auth_events__v1 = bigquery_etl_query(
         task_id="firefox_accounts_derived__fxa_auth_events__v1",
         destination_table="fxa_auth_events_v1",
@@ -187,10 +174,6 @@ with DAG(
 
     firefox_accounts_derived__exact_mau28__v1.set_upstream(
         firefox_accounts_derived__fxa_users_last_seen__v1
-    )
-
-    firefox_accounts_derived__fxa_users_daily__v1.set_upstream(
-        firefox_accounts_derived__fxa_auth_bounce_events__v1
     )
 
     firefox_accounts_derived__fxa_users_daily__v1.set_upstream(

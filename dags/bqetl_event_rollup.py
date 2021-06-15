d58c325436048f9a75c7158134b0fac18a486536
Frank Bertsch <fbertsch@mozilla.com>
2021-06-14 20:18:41 -0400

    Generate DAG

--- dags/bqetl_event_rollup.py
+++ dags/bqetl_event_rollup.py
@@ -35,6 +35,43 @@ with DAG(
     doc_md=docs,
 ) as dag:
 
+    firefox_accounts_derived__event_types__v1 = bigquery_etl_query(
+        task_id="firefox_accounts_derived__event_types__v1",
+        destination_table="event_types_v1",
+        dataset_id="firefox_accounts_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="frank@mozilla.com",
+        email=["frank@mozilla.com"],
+        date_partition_parameter=None,
+        depends_on_past=False,
+        parameters=["submission_date:DATE:{{ds}}"],
+        dag=dag,
+    )
+
+    firefox_accounts_derived__event_types_history__v1 = bigquery_etl_query(
+        task_id="firefox_accounts_derived__event_types_history__v1",
+        destination_table="event_types_history_v1",
+        dataset_id="firefox_accounts_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="frank@mozilla.com",
+        email=["frank@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=True,
+        dag=dag,
+    )
+
+    firefox_accounts_derived__events_daily__v1 = bigquery_etl_query(
+        task_id="firefox_accounts_derived__events_daily__v1",
+        destination_table="events_daily_v1",
+        dataset_id="firefox_accounts_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="frank@mozilla.com",
+        email=["frank@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        dag=dag,
+    )
+
     messaging_system_derived__event_types__v1 = bigquery_etl_query(
         task_id="messaging_system_derived__event_types__v1",
         destination_table="event_types_v1",
@@ -107,6 +144,42 @@ with DAG(
         date_partition_parameter="submission_date",
         depends_on_past=False,
         dag=dag,
+    )
+
+    firefox_accounts_derived__event_types__v1.set_upstream(
+        firefox_accounts_derived__event_types_history__v1
+    )
+
+    wait_for_firefox_accounts_derived__fxa_auth_events__v1 = ExternalTaskSensor(
+        task_id="wait_for_firefox_accounts_derived__fxa_auth_events__v1",
+        external_dag_id="bqetl_fxa_events",
+        external_task_id="firefox_accounts_derived__fxa_auth_events__v1",
+        execution_delta=datetime.timedelta(seconds=5400),
+        check_existence=True,
+        mode="reschedule",
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    firefox_accounts_derived__event_types_history__v1.set_upstream(
+        wait_for_firefox_accounts_derived__fxa_auth_events__v1
+    )
+
+    wait_for_firefox_accounts_derived__fxa_content_events__v1 = ExternalTaskSensor(
+        task_id="wait_for_firefox_accounts_derived__fxa_content_events__v1",
+        external_dag_id="bqetl_fxa_events",
+        external_task_id="firefox_accounts_derived__fxa_content_events__v1",
+        execution_delta=datetime.timedelta(seconds=5400),
+        check_existence=True,
+        mode="reschedule",
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    firefox_accounts_derived__event_types_history__v1.set_upstream(
+        wait_for_firefox_accounts_derived__fxa_content_events__v1
+    )
+
+    firefox_accounts_derived__events_daily__v1.set_upstream(
+        firefox_accounts_derived__event_types__v1
     )
 
     messaging_system_derived__event_types__v1.set_upstream(

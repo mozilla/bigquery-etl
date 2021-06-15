6fa8764fad62e9a0ad780f0b93c8f0f1a0d92b1a
Frank Bertsch <fbertsch@mozilla.com>
2021-06-14 20:22:46 -0400

    Dont dryrun complex query

--- bigquery_etl/dryrun.py
+++ bigquery_etl/dryrun.py
@@ -204,6 +204,8 @@ SKIP = {
         "sql/moz-fx-data-shared-prod/*/baseline_clients_first_seen_v1/*.sql",
         recursive=True,
     ),
+    # Query too complex
+    "sql/moz-fx-data-shared-prod/firefox_accounts_derived/event_types_history_v1/query.sql",
 }
 
 

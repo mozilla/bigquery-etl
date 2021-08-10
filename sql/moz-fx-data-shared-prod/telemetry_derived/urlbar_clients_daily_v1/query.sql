d81c2536a919b32863e972aa48e9824f80422716
Teon L Brooks <teon.brooks@gmail.com>
2021-08-10 17:13:15 -0400

    remove searchmode
    searchmode only captures the entry but does not capture information about the final selection.

--- sql/moz-fx-data-shared-prod/telemetry_derived/urlbar_clients_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/urlbar_clients_daily_v1/query.sql
@@ -38,33 +38,7 @@ WITH
     STRUCT("unknown" AS type,
       scalar_parent_urlbar_picked_unknown_sum AS index),
     STRUCT("visiturl" AS type,
-      scalar_parent_urlbar_picked_visiturl_sum AS index) ] AS urlbar_picked,
-    [ STRUCT("bookmarkmenu" AS type,
-      scalar_parent_urlbar_searchmode_bookmarkmenu_sum AS index),
-    STRUCT("handoff" AS type,
-      scalar_parent_urlbar_searchmode_handoff_sum AS index),
-    STRUCT("keywordoffer" AS type,
-      scalar_parent_urlbar_searchmode_keywordoffer_sum AS index),
-    STRUCT("oneoff" AS type,
-      scalar_parent_urlbar_searchmode_oneoff_sum AS index),
-    STRUCT("other" AS type,
-      scalar_parent_urlbar_searchmode_other_sum AS index),
-    STRUCT("shortcut" AS type,
-      scalar_parent_urlbar_searchmode_shortcut_sum AS index),
-    STRUCT("tabmenu" AS type,
-      scalar_parent_urlbar_searchmode_tabmenu_sum AS index),
-    STRUCT("tabtosearch" AS type,
-      scalar_parent_urlbar_searchmode_tabtosearch_sum AS index),
-    STRUCT("tabtosearch_onboard" AS type,
-      scalar_parent_urlbar_searchmode_tabtosearch_onboard_sum AS index),
-    STRUCT("topsites_newtab" AS type,
-      scalar_parent_urlbar_searchmode_topsites_newtab_sum AS index),
-    STRUCT("topsites_urlbar" AS type,
-      scalar_parent_urlbar_searchmode_topsites_urlbar_sum AS index),
-    STRUCT("touchbar" AS type,
-      scalar_parent_urlbar_searchmode_touchbar_sum AS index),
-    STRUCT("typed" AS type,
-      scalar_parent_urlbar_searchmode_typed_sum AS index) ] AS urlbar_searchmode
+      scalar_parent_urlbar_picked_visiturl_sum AS index) ] AS urlbar_picked
   FROM
     mozdata.telemetry.clients_daily
   WHERE
@@ -90,24 +64,6 @@ WITH
   GROUP BY
     submission_date,
     client_id ),
-  temp_searchmode AS (
-  SELECT
-    submission_date,
-    client_id,
-    COALESCE(SUM(index.value), 0) as count_searchmode_total,
-    mozfun.map.sum( ARRAY_AGG( STRUCT(type AS key,
-          index.value AS value) ) ) AS count_searchmode_by_entry,
-    mozfun.map.sum( ARRAY_AGG( STRUCT(index.key AS key,
-          index.value AS value) ) ) AS count_searchmode_by_engine
-  FROM
-    temp
-  CROSS JOIN
-    UNNEST(urlbar_searchmode) AS urlbar_searchmode
-  CROSS JOIN
-    UNNEST(index) AS index
-  GROUP BY
-    submission_date,
-    client_id )
 SELECT
   submission_date,
   client_id,
@@ -117,21 +73,12 @@ SELECT
   locale,
   count_picked_total,
   count_picked_by_type,
-  count_picked_by_index,
-  count_searchmode_total,
-  count_searchmode_by_engine,
-  count_searchmode_by_entry,
-  urlbar_picked,
-  urlbar_searchmode
+  count_picked_by_index
+  urlbar_picked
 FROM
   temp
 FULL OUTER JOIN
   temp_picked
-USING
-  (submission_date,
-    client_id)
-FULL OUTER JOIN
-  temp_searchmode
 USING
   (submission_date,
     client_id)

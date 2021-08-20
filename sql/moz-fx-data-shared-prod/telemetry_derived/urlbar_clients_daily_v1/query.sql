ded8ce3c62ac89006b41059c85078d1030e420a0
Benjamin Wu <benjaminwu124@gmail.com>
2021-08-20 16:26:04 -0400

    Format sql and add scheduling

--- sql/moz-fx-data-shared-prod/telemetry_derived/urlbar_clients_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/urlbar_clients_daily_v1/query.sql
@@ -1,16 +1,16 @@
 CREATE TEMP FUNCTION oneIndex(x ANY TYPE) AS (
   CAST(IF(SAFE_CAST(x AS INT64) < 0, SAFE_CAST(x AS INT64), SAFE_CAST(x AS INT64) + 1) AS STRING)
 );
+
 CREATE TEMP FUNCTION oneIndexStruct(x STRUCT<k STRING, v INT64>) AS (
-  STRUCT(oneIndex(x.k) AS key, x.v as value)
+  STRUCT(oneIndex(x.k) AS key, x.v AS value)
 );
+
 CREATE TEMP FUNCTION oneIndexArray(x ARRAY<STRUCT<k STRING, v INT64>>) AS (
-  ARRAY(SELECT oneIndexStruct(e) FROM UNNEST(x) as e)
+  ARRAY(SELECT oneIndexStruct(e) FROM UNNEST(x) AS e)
 );
 
-
-WITH
-  combined_urlbar_picked AS (
+WITH combined_urlbar_picked AS (
   SELECT
     submission_date,
     client_id,
@@ -18,52 +18,68 @@ WITH
     app_version,
     normalized_channel,
     locale,
-[ STRUCT("autofill" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_autofill_sum) AS position),
-    STRUCT("bookmark" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_bookmark_sum) AS position),
-    STRUCT("dynamic" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_dynamic_sum) AS position),
-    STRUCT("extension" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_extension_sum) AS position),
-    STRUCT("formhistory" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_formhistory_sum) AS position),
-    STRUCT("history" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_history_sum) AS position),
-    STRUCT("keyword" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_keyword_sum) AS position),
-    STRUCT("remotetab" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_remotetab_sum) AS position),
-    STRUCT("searchengine" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_searchengine_sum) AS position),
-    STRUCT("searchsuggestion" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_searchsuggestion_sum) AS position),
-    STRUCT("switchtab" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_switchtab_sum) AS position),
-    STRUCT("tabtosearch" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_tabtosearch_sum) AS position),
-    STRUCT("tip" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_tip_sum) AS position),
-    STRUCT("topsite" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_topsite_sum) AS position),
-    STRUCT("unknown" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_unknown_sum) AS position),
-    STRUCT("visiturl" AS type,
-      oneIndexArray(scalar_parent_urlbar_picked_visiturl_sum) AS position) ] AS urlbar_picked_by_type_by_position
+    [
+      STRUCT(
+        "autofill" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_autofill_sum) AS position
+      ),
+      STRUCT(
+        "bookmark" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_bookmark_sum) AS position
+      ),
+      STRUCT("dynamic" AS type, oneIndexArray(scalar_parent_urlbar_picked_dynamic_sum) AS position),
+      STRUCT(
+        "extension" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_extension_sum) AS position
+      ),
+      STRUCT(
+        "formhistory" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_formhistory_sum) AS position
+      ),
+      STRUCT("history" AS type, oneIndexArray(scalar_parent_urlbar_picked_history_sum) AS position),
+      STRUCT("keyword" AS type, oneIndexArray(scalar_parent_urlbar_picked_keyword_sum) AS position),
+      STRUCT(
+        "remotetab" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_remotetab_sum) AS position
+      ),
+      STRUCT(
+        "searchengine" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_searchengine_sum) AS position
+      ),
+      STRUCT(
+        "searchsuggestion" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_searchsuggestion_sum) AS position
+      ),
+      STRUCT(
+        "switchtab" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_switchtab_sum) AS position
+      ),
+      STRUCT(
+        "tabtosearch" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_tabtosearch_sum) AS position
+      ),
+      STRUCT("tip" AS type, oneIndexArray(scalar_parent_urlbar_picked_tip_sum) AS position),
+      STRUCT("topsite" AS type, oneIndexArray(scalar_parent_urlbar_picked_topsite_sum) AS position),
+      STRUCT("unknown" AS type, oneIndexArray(scalar_parent_urlbar_picked_unknown_sum) AS position),
+      STRUCT(
+        "visiturl" AS type,
+        oneIndexArray(scalar_parent_urlbar_picked_visiturl_sum) AS position
+      )
+    ] AS urlbar_picked_by_type_by_position
   FROM
-   telemetry.clients_daily
+    telemetry.clients_daily
   WHERE
-    submission_date = @submission_date ),
-  count_picked AS (
+    submission_date = @submission_date
+),
+count_picked AS (
   SELECT
     submission_date,
     client_id,
-     COALESCE(SUM(position.value), 0) as count_picked_total,
-    mozfun.map.sum( ARRAY_AGG( STRUCT(type AS key,
-          position.value AS value) ) ) AS count_picked_by_type,
-    mozfun.map.sum( ARRAY_AGG(
-        STRUCT( oneIndex(position.key) AS key,
-          position.value AS value) ) ) AS count_picked_by_position
+    COALESCE(SUM(position.value), 0) AS count_picked_total,
+    mozfun.map.sum(ARRAY_AGG(STRUCT(type AS key, position.value AS value))) AS count_picked_by_type,
+    mozfun.map.sum(
+      ARRAY_AGG(STRUCT(oneIndex(position.key) AS key, position.value AS value))
+    ) AS count_picked_by_position
   FROM
     combined_urlbar_picked
   CROSS JOIN
@@ -72,7 +88,8 @@ WITH
     UNNEST(position) AS position
   GROUP BY
     submission_date,
-    client_id )
+    client_id
+)
 SELECT
   submission_date,
   client_id,
@@ -89,5 +106,4 @@ FROM
 FULL OUTER JOIN
   count_picked
 USING
-  (submission_date,
-    client_id)
+  (submission_date, client_id)

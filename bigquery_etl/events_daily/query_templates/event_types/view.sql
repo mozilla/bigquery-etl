7b4a6f730c9699c954165ee85ba48377115e47dc
Will Lachance <wlachance@mozilla.com>
2021-08-23 16:24:15 -0400

    Add or generate various app-data specific views for fenix (#2286)
    Currently release channel only.
    
    * events_daily
    * event_types
    * migrated_clients

--- bigquery_etl/events_daily/query_templates/event_types/view.sql
+++ bigquery_etl/events_daily/query_templates/event_types/view.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.{{ app_id }}.event_types`
+  `moz-fx-data-shared-prod.{{ view_dataset }}.event_types`
 AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.{{ app_id }}_derived.event_types_v1`
+  `moz-fx-data-shared-prod.{{ base_dataset }}.event_types_v1`

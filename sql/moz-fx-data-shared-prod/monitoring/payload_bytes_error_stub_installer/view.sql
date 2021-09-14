dcf9ed19b0a607a884b551dc4e1b7863114b0ed5
Jeff Klukas <jeff@klukas.net>
2021-09-14 12:38:19 -0400

    Only create an authorized view for structured

--- sql/moz-fx-data-shared-prod/monitoring/payload_bytes_error_stub_installer/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.monitoring.payload_bytes_error_stub_installer`
-AS
-SELECT
-  * EXCEPT (payload)
-FROM
-  `moz-fx-data-shared-prod.payload_bytes_error.stub_installer`

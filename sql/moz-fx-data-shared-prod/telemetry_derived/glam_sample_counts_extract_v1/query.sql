7ef1287de41aee41d762b83bb2e9d1bfd1ed366f
Alekhya <alekhya@Alekhyas-MBP.lan>
2021-09-15 13:49:02 -0400

    modified the sample counts extract query

--- sql/moz-fx-data-shared-prod/telemetry_derived/glam_sample_counts_extract_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/glam_sample_counts_extract_v1/query.sql
@@ -1,18 +1,3 @@
-WITH deduped AS (
-  SELECT
-    *,
-    ROW_NUMBER() OVER (
-      PARTITION BY
-        os,
-        app_version,
-        app_build_id,
-        channel
-      ORDER BY
-        total_sample DESC
-    ) AS rank
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1`
-)
 SELECT
   CASE
   WHEN
@@ -36,6 +21,4 @@ SELECT
   key,
   total_sample
 FROM
-  deduped
-WHERE
-  rank = 1;
+  `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1`

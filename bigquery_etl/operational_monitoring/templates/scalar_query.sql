870567d3dc6960ca2d5b8778e79f0556d120736a
Marina Samuel <msamuel@mozilla.com>
2021-09-15 10:30:08 -0400

    Bucket scalars for faster jackknife computation.

--- bigquery_etl/operational_monitoring/templates/scalar_query.sql
+++ bigquery_etl/operational_monitoring/templates/scalar_query.sql
@@ -57,19 +57,53 @@ WITH merged_scalars AS (
           {{ dimension.name }},
         {% endfor %}
         branch
-)
+),
 
-SELECT *
-FROM merged_scalars
-WHERE branch IN (
-    -- If branches are not defined, assume it's a rollout
-    -- and fall back to branches labeled as enabled/disabled
-    {% if branches %}
-    {% for branch in branches %}
-      "{{ branch }}"
-      {{ "," if not loop.last else "" }}
-    {% endfor %}
-    {% else %}
-    "enabled", "disabled"
-    {% endif %}
-)
+flattened_scalars AS (
+    SELECT *
+    FROM merged_scalars
+    CROSS JOIN UNNEST(metrics)
+    WHERE branch IN (
+        -- If branches are not defined, assume it's a rollout
+        -- and fall back to branches labeled as enabled/disabled
+        {% if branches %}
+        {% for branch in branches %}
+          "{{ branch }}"
+          {{ "," if not loop.last else "" }}
+        {% endfor %}
+        {% else %}
+        "enabled", "disabled"
+        {% endif %}
+    )
+),
+
+log_min_max AS (
+  SELECT
+    name,
+    LOG(IF(MIN(value) <= 0, 1, MIN(value)), 2) log_min,
+    LOG(IF(MAX(value) <= 0, 1, MAX(value)), 2) log_max
+  FROM
+    flattened_scalars
+  GROUP BY 1),
+
+buckets_by_metric AS (
+  SELECT name, ARRAY(SELECT FORMAT("%.*f", 2, bucket) FROM UNNEST(
+    mozfun.glam.histogram_generate_scalar_buckets(log_min, log_max, 100)
+  ) AS bucket ORDER BY bucket) AS buckets
+  FROM log_min_max)
+
+SELECT
+    submission_date,
+    client_id,
+    build_id,
+    cores_count,
+    os,
+    branch,
+    name,
+    agg_type,
+    -- Replace value with its bucket value
+    SAFE_CAST(FORMAT("%.*f", 2, COALESCE(mozfun.glam.histogram_bucket_from_value(buckets, SAFE_CAST(value AS FLOAT64)), 0) + 0.0001) AS FLOAT64) AS value
+FROM
+    flattened_scalars
+LEFT JOIN buckets_by_metric USING(name)
+

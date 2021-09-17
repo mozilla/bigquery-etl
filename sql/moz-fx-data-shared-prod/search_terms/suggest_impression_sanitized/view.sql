83041f4bbbbe265bd433350361206873c0371b3d
Jeff Klukas <jeff@klukas.net>
2021-09-17 15:23:57 -0400

    Lint

--- sql/moz-fx-data-shared-prod/search_terms/suggest_impression_sanitized/view.sql
+++ sql/moz-fx-data-shared-prod/search_terms/suggest_impression_sanitized/view.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.search_terms.suggest_impression_sanitized`
 AS
 SELECT
-  * REPLACE(mozfun.norm.metadata(metadata) AS metadata)
+  * REPLACE (mozfun.norm.metadata(metadata) AS metadata)
 FROM
   `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_impression_v1`

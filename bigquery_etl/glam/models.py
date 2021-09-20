bea779987806d5f1becbd17c95efa0a40e676573
alekhyamoz <akommasani@mozilla.com>
2021-09-20 15:20:54 -0400

    formated for black format check

--- bigquery_etl/glam/models.py
+++ bigquery_etl/glam/models.py
@@ -182,10 +182,20 @@ def user_counts(**kwargs):
         **kwargs,
     )
 
+
 def sample_counts(**kwargs):
     """Variables for sample counts."""
-    attributes = ["ping_type", "os", "app_version", "app_build_id", "channel","key","metric","value"]
-    fixed_attributes = ["app_version", "channel","metric","value","key"]
+    attributes = [
+        "ping_type",
+        "os",
+        "app_version",
+        "app_build_id",
+        "channel",
+        "key",
+        "metric",
+        "value",
+    ]
+    fixed_attributes = ["app_version", "channel", "metric", "value", "key"]
     cubed_attributes = [x for x in attributes if x not in fixed_attributes]
     return dict(
         attributes=",".join(attributes),

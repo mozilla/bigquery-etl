fff5d1fcdac5b3ac255964c7275150ff998e5971
Daniel Thorn <dthorn@mozilla.com>
2021-09-21 16:43:46 -0700

    Add window-valued join fields for vat rates to the view

--- sql/moz-fx-data-shared-prod/mozilla_vpn/vat_rates/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/vat_rates/view.sql
@@ -2,6 +2,8 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.mozilla_vpn.vat_rates`
 AS
 SELECT
-  *
+  *,
+  LAG(effective_date) OVER (PARTITION BY country ORDER BY effective_date) AS prev_effective_date,
+  LEAD(effective_date) OVER (PARTITION BY country ORDER BY effective_date) AS next_effective_date,
 FROM
   `moz-fx-data-shared-prod`.mozilla_vpn_derived.vat_rates_v1

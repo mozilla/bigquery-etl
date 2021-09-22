e4aa925df2a61815299dee01b529134c6c9b2911
Wesley Dawson <whd@mozilla.com>
2021-09-22 10:21:49 -0700

    WIP switch to monitoring tables

--- sql/moz-fx-data-shared-prod/monitoring_derived/deletion_request_volume_v1/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/deletion_request_volume_v1/query.sql
@@ -1,13 +1,14 @@
 SELECT
   DATE(submission_timestamp) AS submission_date,
+  # FIXME
   REGEXP_EXTRACT(_TABLE_SUFFIX, "(?:stub_)?(?:[^_]+_)(.*)__") AS dataset_id,
   COUNT(*) AS num_rows,
 FROM
-  `payload_bytes_decoded.*`
+  `payload_bytes_decoded_all`
 WHERE
   DATE(submission_timestamp) < CURRENT_DATE
   AND (@submission_date IS NULL OR @submission_date = DATE(submission_timestamp))
-  AND _TABLE_SUFFIX LIKE '%__deletion_request_v%'
+  AND document_type = 'deletion_request'
 GROUP BY
   submission_date,
   dataset_id

de9e687ef5bcaf22e27b925a6c1bdbba80f718dc
Jeff Klukas <jklukas@mozilla.com>
2021-09-27 09:16:46 -0400

    Bug 1729524 Add search_terms adm_weekly_aggregrates (#2373)
    * Bug 1729524 Add search_terms adm_weekly_aggregrates
    
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1729524
    
    There will be a separate PR with a new DAG for exporting this data.
    
    * Remove position
    
    * Update sql/moz-fx-data-shared-prod/search_terms_derived/adm_weekly_aggregates_v1/query.sql

---
+++ sql/moz-fx-data-shared-prod/search_terms_derived/adm_weekly_aggregates_v1/query.sql
@@ -0,0 +1,18 @@
+SELECT
+  search_query AS query,
+  block_id,
+  COUNT(*) AS impressions,
+  COUNTIF(is_clicked) AS clicks,
+FROM
+  search_terms.suggest_impression_sanitized
+WHERE
+  DATE(submission_timestamp)
+  BETWEEN @submission_date
+  AND DATE_SUB(@submission_date, INTERVAL 6 DAY)
+GROUP BY
+  query,
+  block_id
+HAVING
+  -- This filter matches aggregated_search_terms_daily, but may change; see
+  -- https://bugzilla.mozilla.org/show_bug.cgi?id=1729524
+  COUNT(DISTINCT context_id) > 30000

6c5899e27bcf1111ccfad27332b9afeecc0e644f
Anna Scholtz <anna@scholtzan.net>
2021-10-05 11:43:14 -0700

    Cleanup generated datasets

--- sql/moz-fx-data-shared-prod/firefox_ios/clients_daily_metrics/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_ios.clients_daily_metrics`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_ios_derived.clients_daily_metrics_v1`
--- sql/moz-fx-data-shared-prod/focus_ios/clients_daily_metrics/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.focus_ios.clients_daily_metrics`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.focus_ios_derived.clients_daily_metrics_v1`

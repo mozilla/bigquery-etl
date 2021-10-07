28c4acaa7e46575db5ab21d7ba59c0d41b46acd1
Jeff Klukas <jklukas@mozilla.com>
2021-10-07 11:16:11 -0400

    RS-106 Rate monitoring views for topsites (#2411)
    * RS-106 Rate monitoring views for topsites
    
    See https://mozilla-hub.atlassian.net/browse/RS-106
    
    This adds materialized views to provide counts, and then authorized views
    on top of them to make the counts available in Grafana.
    
    We will end up consuming these views in the
    [Contile ops dashboard](https://earthangel-b40313e5.influxcloud.net/d/oak1zw6Gz/contile-infrastructure?orgId=1&from=now-12h&to=now&refresh=1m

---
+++ sql/moz-fx-data-shared-prod/monitoring/topsites_click_rate_live/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.monitoring.topsites_click_rate_live`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.monitoring_derived.topsites_click_rate_live_v1`

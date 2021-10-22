cf67059d304fe97a3ae86f16b10fd45b727b8eb0
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-10-22 15:49:57 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/mlhackweek_search/baseline_clients_daily/view.sql
+++
@@ -1,8 +0,0 @@
--- Generated via bigquery_etl.glean_usage
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.mlhackweek_search.baseline_clients_daily`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.mlhackweek_search_derived.baseline_clients_daily_v1`

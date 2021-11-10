f7d2863213ff9362e39269a5bfb4088256e550be
Alekhya <88394696+alekhyamoz@users.noreply.github.com>
2021-11-10 10:25:31 -0500

    added minimun client counts for each channel (#2453)

--- sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
@@ -1,12 +1,50 @@
-SELECT
-  app_version,
-  COALESCE(os, "*") AS os,
-  COALESCE(app_build_id, "*") AS app_build_id,
-  process,
-  metric,
+WITH client_counts AS (
+  SELECT
+    channel,
+    app_version,
+    COALESCE(os, "*") AS os,
+    COALESCE(app_build_id, "*") AS app_build_id,
+    process,
+    metric,
     -- BigQuery has some null unicode characters which Postgresql doesn't like, so we remove those here.
     -- Also limit string length to 200 to match column length.
-  SUBSTR(REPLACE(key, r"\x00", ""), 0, 200) AS key,
+    SUBSTR(REPLACE(key, r"\x00", ""), 0, 200) AS key,
+    client_agg_type,
+    metric_type,
+    total_users,
+    agg_type,
+    aggregates,
+    CASE
+    WHEN
+      channel = 'release'
+    THEN
+      total_users > 625000
+    WHEN
+      channel = 'beta'
+    THEN
+      total_users > 9000
+    WHEN
+      channel = 'nightly'
+    THEN
+      total_users > 375
+    ELSE
+      total_users > 100
+    END
+    AS user_count_check
+  FROM
+    `moz-fx-data-shared-prod.telemetry.client_probe_counts`
+  WHERE
+    channel = @channel
+    AND app_version IS NOT NULL
+)
+SELECT
+  channel,
+  app_version,
+  os,
+  app_build_id,
+  process,
+  metric,
+  key,
   client_agg_type,
   metric_type,
   total_users,
@@ -16,11 +54,9 @@ SELECT
     IF(agg_type = "percentiles", mozfun.glam.histogram_cast_json(aggregates), NULL)
   ) AS percentiles
 FROM
-  `moz-fx-data-shared-prod.telemetry.client_probe_counts`
+  client_counts
 WHERE
-  channel = @channel
-  AND app_version IS NOT NULL
-  AND total_users > 100
+  user_count_check
 GROUP BY
   channel,
   app_version,

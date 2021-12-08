1aa646eb18ddaec327c89bed6ad22d4432b999b1
Alexander Nicholson <anicholson@mozilla.com>
2021-12-08 13:05:19 -0500

    Refactor derived dataset generation script - more pathlib functions

--- bigquery_etl/docs/derived_datasets/generate_derived_dataset_docs.py
+++ bigquery_etl/docs/derived_datasets/generate_derived_dataset_docs.py
@@ -1,6 +1,5 @@
 """Generate documentation for derived datasets."""
 
-import os
 from pathlib import Path
 
 import yaml
@@ -19,85 +18,99 @@ NON_USER_FACING_DATASET_SUFFIXES = (
 SOURCE_URL = "https://github.com/mozilla/bigquery-etl/blob/generated-sql"
 
 
+def _get_metadata(table_path):
+    metadata = {}
+    metadata_file = table_path / METADATA_FILE
+    if metadata_file.exists():
+        with open(metadata_file) as stream:
+            try:
+                metadata = yaml.safe_load(stream)
+            except yaml.YAMLError as error:
+                print(error)
+    return metadata
+
+
+def _get_referenced_tables_from_view(table_path):
+    referenced_tables = []
+    view_file = table_path / VIEW_FILE
+    if view_file.exists():
+        for referenced_table in extract_table_references(view_file.read_text()):
+            table_split = referenced_table.split(".")
+            if len(table_split) == 2:
+                # missing project ID, retrieve from file path
+                [dataset_id, table_id] = table_split
+                project_id = view_file.parent.parent.parent.name
+            elif len(table_split) == 3:
+                [project_id, dataset_id, table_id] = table_split
+            else:
+                continue
+
+            referenced_tables.append(
+                {
+                    "project_id": project_id,
+                    "dataset_id": dataset_id,
+                    "table_id": table_id,
+                }
+            )
+    return referenced_tables
+
+
+def _iter_table_markdown(table_paths, template):
+    for table_path in table_paths:
+        source_urls = {"Source Directory": f"{SOURCE_URL}/{str(table_path)}"}
+
+        referenced_tables = _get_referenced_tables_from_view(table_path)
+        if referenced_tables:
+            source_urls[
+                "View Definition"
+            ] = f"{SOURCE_URL}/{str(table_path / VIEW_FILE)}"
+
+        metadata = _get_metadata(table_path)
+        if metadata:
+            source_urls[
+                "Metadata File"
+            ] = f"{SOURCE_URL}/{str(table_path / METADATA_FILE)}"
+
+        output = template.render(
+            metadata=metadata,
+            table_name=table_path.name,
+            source_urls=source_urls,
+            referenced_tables=referenced_tables,
+            project_url=f"{SOURCE_URL}/sql",
+        )
+
+        yield output
+
+
 def generate_derived_dataset_docs(out_dir, project_dir):
     """Generate documentation for derived datasets."""
-    project_doc_dir = Path(out_dir) / "mozdata"
+    output_path = Path(out_dir) / "datasets"
+    project_path = Path(project_dir)
 
     # get a list of all user-facing datasets
-    data_sets = [
-        item
-        for item in os.listdir(project_dir)
-        if os.path.isdir(os.path.join(project_dir, item))
-        and all(name not in item for name in NON_USER_FACING_DATASET_SUFFIXES)
-    ]
+    dataset_paths = sorted(
+        [
+            dataset_path
+            for dataset_path in project_path.iterdir()
+            if dataset_path.is_dir()
+            and all(
+                suffix not in str(dataset_path)
+                for suffix in NON_USER_FACING_DATASET_SUFFIXES
+            )
+        ]
+    )
 
-    for table in data_sets:
-        output = []
-        source_urls = {}
-        with open(project_doc_dir / f"{table}.md", "w") as dataset_doc:
+    for dataset_path in dataset_paths:
+        table_paths = sorted([path for path in dataset_path.iterdir() if path.is_dir()])
+
+        file_loader = FileSystemLoader("bigquery_etl/docs/derived_datasets/templates")
+        env = Environment(loader=file_loader)
+        template = env.get_template("table.md")
+
+        with open(output_path / f"{dataset_path.name}.md", "w") as dataset_doc:
             # Manually set title to prevent Mkdocs from removing
             # underscores and capitalizing file names
             # https://github.com/mkdocs/mkdocs/issues/1915#issuecomment-561311801
-            dataset_doc.write(f"---\ntitle: {table}\n---\n\n")
+            dataset_doc.write(f"---\ntitle: {dataset_path.name}\n---\n\n")
 
-            for root, dirs, files in os.walk(Path(project_dir) / table):
-                # show views in an alphabetical order
-                dirs.sort()
-                if dirs:
-                    continue
-                dataset_name = root.split("/")[-1]
-                source_urls["Source Directory"] = f"{SOURCE_URL}/{root}"
-                referenced_tables = []
-
-                metadata = {}
-                if METADATA_FILE in files:
-                    source_urls[
-                        "Metadata File"
-                    ] = f"{SOURCE_URL}/{root}/{METADATA_FILE}"
-                    with open(os.path.join(root, METADATA_FILE)) as stream:
-                        try:
-                            metadata = yaml.safe_load(stream)
-                        except yaml.YAMLError as error:
-                            print(error)
-                if VIEW_FILE in files:
-                    source_urls["View Definition"] = f"{SOURCE_URL}/{root}/{VIEW_FILE}"
-                    view_file = Path(os.path.join(root, VIEW_FILE))
-                    referenced_tables = []
-
-                    for referenced_table in extract_table_references(
-                        view_file.read_text()
-                    ):
-                        table_split = referenced_table.split(".")
-                        if len(table_split) == 2:
-                            # missing project ID, retrieve from file path
-                            [dataset_id, table_id] = table_split
-                            project_id = view_file.parent.parent.parent.name
-                        elif len(table_split) == 3:
-                            [project_id, dataset_id, table_id] = table_split
-                        else:
-                            continue
-
-                        referenced_tables.append(
-                            {
-                                "project_id": project_id,
-                                "dataset_id": dataset_id,
-                                "table_id": table_id,
-                            }
-                        )
-
-                file_loader = FileSystemLoader(
-                    "bigquery_etl/docs/derived_datasets/templates"
-                )
-                # Set up a new template environment
-                env = Environment(loader=file_loader)
-                # Create template with the markdown source text
-                template = env.get_template("table.md")
-
-                output = template.render(
-                    metadata=metadata,
-                    table_name=dataset_name,
-                    source_urls=source_urls,
-                    referenced_tables=referenced_tables,
-                    project_url=f"{SOURCE_URL}/sql",
-                )
-                dataset_doc.write(output)
+            dataset_doc.write("".join(_iter_table_markdown(table_paths, template)))

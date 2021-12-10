fedc7e6e5f064b69bdfaac9de03b243b4f31a3a0
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2021-12-10 15:18:56 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
@@ -1,24 +1,61 @@
-SELECT
-  app_version,
-  COALESCE(os, "*") AS os,
-  COALESCE(app_build_id, "*") AS app_build_id,
-  process,
-  metric,
+WITH client_counts AS (
+  SELECT
+    channel,
+    app_version,
+    COALESCE(os, "*") AS os,
+    COALESCE(app_build_id, "*") AS app_build_id,
+    process,
+    metric,
     -- BigQuery has some null unicode characters which Postgresql doesn't like, so we remove those here.
     -- Also limit string length to 200 to match column length.
-  SUBSTR(REPLACE(key, r"\x00", ""), 0, 200) AS key,
+    SUBSTR(REPLACE(key, r"\x00", ""), 0, 200) AS key,
+    client_agg_type,
+    metric_type,
+    total_users,
+    agg_type,
+    aggregates,
+    CASE
+    WHEN
+      channel = 'release'
+    THEN
+      total_users > 625000
+    WHEN
+      channel = 'beta'
+    THEN
+      total_users > 9000
+    WHEN
+      channel = 'nightly'
+    THEN
+      total_users > 375
+    ELSE
+      total_users > 100
+    END
+    AS user_count_check
+  FROM
+    `moz-fx-data-shared-prod.telemetry.client_probe_counts`
+  WHERE
+    channel = @channel
+    AND app_version IS NOT NULL
+)
+SELECT
+  app_version,
+  os,
+  app_build_id,
+  process,
+  metric,
+  key,
   client_agg_type,
   metric_type,
   total_users,
+  -- Using MAX instead of COALESCE since this is not in the GROUP BY.
   MAX(IF(agg_type = "histogram", mozfun.glam.histogram_cast_json(aggregates), NULL)) AS histogram,
   MAX(
     IF(agg_type = "percentiles", mozfun.glam.histogram_cast_json(aggregates), NULL)
   ) AS percentiles
 FROM
-  `moz-fx-data-shared-prod.telemetry.client_probe_counts`
+  client_counts
 WHERE
-  channel = @channel
-  AND app_version IS NOT NULL
+  user_count_check
 GROUP BY
   channel,
   app_version,

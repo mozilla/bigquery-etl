12de4bb531be5bdab1d282808b1385024d139c36
Rebecca BurWei <rebecca.burwei@gmail.com>
2022-01-04 16:44:40 -0600

    update contextual_services.event_aggregates

--- sql/moz-fx-data-shared-prod/contextual_services/event_aggregates/view.sql
+++ sql/moz-fx-data-shared-prod/contextual_services/event_aggregates/view.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.contextual_services.event_aggregates`
 AS
 SELECT
-  *
+  * EXCEPT (advertiser),
+  LOWER(advertiser) as advertiser
 FROM
   `moz-fx-data-shared-prod.contextual_services_derived.event_aggregates_v1`

4797df9c87c0dc5bbe4a3edd1778ea2bf4c1ceac
Rebecca BurWei <rebecca.burwei@gmail.com>
2022-01-04 17:04:08 -0600

    update quicksuggest_impression

--- sql/moz-fx-data-shared-prod/contextual_services/quicksuggest_impression/view.sql
+++ sql/moz-fx-data-shared-prod/contextual_services/quicksuggest_impression/view.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.contextual_services.quicksuggest_impression`
 AS
 SELECT
-  * EXCEPT (search_query, matched_keywords) REPLACE(mozfun.norm.metadata(metadata) AS metadata)
+  * EXCEPT (search_query, matched_keywords, advertiser) REPLACE(mozfun.norm.metadata(metadata) AS metadata),
+  LOWER(advertiser) as advertiser
 FROM
   `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v1`

89c9f9d354d628efb0061e134d2559175ff5bd1b
Rebecca BurWei <rebecca.burwei@gmail.com>
2022-01-05 14:10:03 -0600

    replace left join with array_agg

--- sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_event_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_event_v1/query.sql
@@ -1,56 +1,27 @@
-WITH firefox_suggest_events AS (
-  SELECT
-    submission_date,
-    sample_id,
-    client_id,
-    ARRAY_AGG(event_object ORDER BY session_start_time DESC)[OFFSET(0)] AS firefoxsuggest_optin,
-  FROM
-    telemetry.events
-  WHERE
-    submission_date = @submission_date
-  AND 
-    event_category = 'contextservices.quicksuggest'
-  AND 
-    event_method = 'opt_in_dialog'
-  GROUP BY
-    1,
-    2,
-    3
-),
-telemetry_events AS (
-  SELECT
-    submission_date,
-    sample_id,
-    client_id,
-    COUNT(*) AS n_logged_event,
-    COUNTIF(
-      event_category = 'pictureinpicture'
-      AND event_method = 'create'
-    ) AS n_created_pictureinpicture,
-    COUNTIF(
-      event_category = 'security.ui.protections'
-      AND event_object = 'protection_report'
-    ) AS n_viewed_protection_report,
-  FROM
-    telemetry.events
-  WHERE
-    submission_date = @submission_date
-  GROUP BY
-    1,
-    2,
-    3
-)
 SELECT
   submission_date,
   sample_id,
   client_id,
-  n_logged_event,
-  n_created_pictureinpicture,
-  n_viewed_protection_report,
-  firefoxsuggest_optin,
+  COUNT(*) AS n_logged_event,
+  COUNTIF( event_category = 'pictureinpicture'
+    AND event_method = 'create' ) AS n_created_pictureinpicture,
+  COUNTIF( event_category = 'security.ui.protections'
+    AND event_object = 'protection_report' ) AS n_viewed_protection_report,
+  ARRAY_AGG(
+  IF
+    (event_category = 'contextservices.quicksuggest'
+      AND event_method = 'opt_in_dialog',
+      event_object,
+      NULL) IGNORE NULLS
+  ORDER BY
+    session_start_time DESC )[
+OFFSET
+  (0)] AS firefoxsuggest_optin,
 FROM
-  telemetry_events
-LEFT JOIN 
-  firefox_suggest_events
-USING
-  ( submission_date, sample_id, client_id )
+  telemetry.events
+WHERE
+  submission_date = @submission_date
+GROUP BY
+  1,
+  2,
+  3

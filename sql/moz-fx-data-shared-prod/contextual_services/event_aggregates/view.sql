9e513ddd345942223e0c031d76fed33853e1ce39
Rebecca BurWei <rebecca.burwei@gmail.com>
2022-01-05 09:58:51 -0600

    Update sql/moz-fx-data-shared-prod/contextual_services/event_aggregates/view.sql
    Co-authored-by: Jeff Klukas <jklukas@mozilla.com>

--- sql/moz-fx-data-shared-prod/contextual_services/event_aggregates/view.sql
+++ sql/moz-fx-data-shared-prod/contextual_services/event_aggregates/view.sql
@@ -2,7 +2,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.contextual_services.event_aggregates`
 AS
 SELECT
-  * EXCEPT (advertiser),
-  LOWER(advertiser) as advertiser
+  * REPLACE(LOWER(advertiser) AS advertiser)
 FROM
   `moz-fx-data-shared-prod.contextual_services_derived.event_aggregates_v1`

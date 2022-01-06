be6e9bd430150021e5b39642539f637634b0e6bd
akommasani@mozilla.com <akommasani@mozilla.com>
2022-01-06 12:05:36 -0500

    added minimum client count for desktop
    added minimum client count for desktop
    
    added minimum client count for desktop
    
    added minimum client count for desktop
    
    added minimum client count for desktop

--- sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
@@ -1,49 +1,10 @@
-WITH client_counts AS (
-  SELECT
-    channel,
-    app_version,
-    COALESCE(os, "*") AS os,
-    COALESCE(app_build_id, "*") AS app_build_id,
-    process,
-    metric,
-    -- BigQuery has some null unicode characters which Postgresql doesn't like, so we remove those here.
-    -- Also limit string length to 200 to match column length.
-    SUBSTR(REPLACE(key, r"\x00", ""), 0, 200) AS key,
-    client_agg_type,
-    metric_type,
-    total_users,
-    agg_type,
-    aggregates,
-    CASE
-    WHEN
-      channel = 'release'
-    THEN
-      total_users > 625000
-    WHEN
-      channel = 'beta'
-    THEN
-      total_users > 9000
-    WHEN
-      channel = 'nightly'
-    THEN
-      total_users > 375
-    ELSE
-      total_users > 100
-    END
-    AS user_count_check
-  FROM
-    `moz-fx-data-shared-prod.telemetry.client_probe_counts`
-  WHERE
-    channel = @channel
-    AND app_version IS NOT NULL
-)
 SELECT
   app_version,
-  os,
-  app_build_id,
+  COALESCE(os, "*") AS os,
+  COALESCE(app_build_id, "*") AS app_build_id,
   process,
   metric,
-  key,
+  SUBSTR(REPLACE(key, r"\x00", ""), 0, 200) AS key,
   client_agg_type,
   metric_type,
   total_users,
@@ -53,9 +14,11 @@ SELECT
     IF(agg_type = "percentiles", mozfun.glam.histogram_cast_json(aggregates), NULL)
   ) AS percentiles
 FROM
-  client_counts
+  `moz-fx-data-shared-prod.telemetry.client_probe_counts`
 WHERE
-  user_count_check
+  channel = @channel
+  AND app_version IS NOT NULL
+  AND total_users > 100
 GROUP BY
   channel,
   app_version,

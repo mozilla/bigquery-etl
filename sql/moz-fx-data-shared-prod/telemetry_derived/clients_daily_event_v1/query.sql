ba53ff8bcae911dce147c9da047848b5c32940c4
Rebecca BurWei <rburwei@mozilla.com>
2022-01-06 10:43:34 -0600

    formatting from bqetl query validate

--- sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_event_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_event_v1/query.sql
@@ -3,20 +3,24 @@ SELECT
   sample_id,
   client_id,
   COUNT(*) AS n_logged_event,
-  COUNTIF( event_category = 'pictureinpicture'
-    AND event_method = 'create' ) AS n_created_pictureinpicture,
-  COUNTIF( event_category = 'security.ui.protections'
-    AND event_object = 'protection_report' ) AS n_viewed_protection_report,
+  COUNTIF(
+    event_category = 'pictureinpicture'
+    AND event_method = 'create'
+  ) AS n_created_pictureinpicture,
+  COUNTIF(
+    event_category = 'security.ui.protections'
+    AND event_object = 'protection_report'
+  ) AS n_viewed_protection_report,
   ARRAY_AGG(
-  IF
-    (event_category = 'contextservices.quicksuggest'
+    IF(
+      event_category = 'contextservices.quicksuggest'
       AND event_method = 'opt_in_dialog',
       event_object,
-      NULL) IGNORE NULLS
-  ORDER BY
-    session_start_time DESC )[
-OFFSET
-  (0)] AS firefoxsuggest_opt_in_dialog_choice,
+      NULL
+    ) IGNORE NULLS
+    ORDER BY
+      session_start_time DESC
+  )[OFFSET(0)] AS firefoxsuggest_opt_in_dialog_choice,
 FROM
   telemetry.events
 WHERE

6ff384802058ea61e345e81a9a9764ae6a309f7d
Jeff Klukas <jeff@klukas.net>
2022-01-11 15:08:57 -0500

    bqetl format sql

--- sql/moz-fx-data-shared-prod/contextual_services/quicksuggest_impression/view.sql
+++ sql/moz-fx-data-shared-prod/contextual_services/quicksuggest_impression/view.sql
@@ -2,6 +2,9 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.contextual_services.quicksuggest_impression`
 AS
 SELECT
-  * EXCEPT (search_query, matched_keywords) REPLACE(mozfun.norm.metadata(metadata) AS metadata, LOWER(advertiser) as advertiser)
+  * EXCEPT (search_query, matched_keywords) REPLACE(
+    mozfun.norm.metadata(metadata) AS metadata,
+    LOWER(advertiser) AS advertiser
+  )
 FROM
   `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v1`

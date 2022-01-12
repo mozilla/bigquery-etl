78a7f241dbda1a4463726c93510fb675f9af41a4
akommasani@mozilla.com <akommasani@mozilla.com>
2022-01-12 13:52:57 -0500

    FOG date check

--- bigquery_etl/glam/templates/logical_app_id/firefox_desktop_glam_release__view_clients_daily_histogram_aggregates_v1.sql
+++ bigquery_etl/glam/templates/logical_app_id/firefox_desktop_glam_release__view_clients_daily_histogram_aggregates_v1.sql
@@ -8,7 +8,7 @@ WITH extracted AS (
     `{{ project }}`.glam_etl.firefox_desktop__view_clients_daily_histogram_aggregates_v1
   WHERE
     channel = 'release'
-    AND SAFE_CAST(app_build_id AS DATE) IS NOT NULL
+    AND safe.parse_datetime('%Y%m%d%H%M%S', app_build_id) IS NOT NULL
 )
 SELECT
   * EXCEPT (app_build_id, channel),

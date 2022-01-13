6b6792e7b4bbc1e8ac7253367a4fe13ca9384c98
Jeff Klukas <jeff@klukas.net>
2022-01-13 16:03:39 -0500

    SUGGEST-178 Add impression and click counts per release channel
    See https://mozilla-hub.atlassian.net/browse/SUGGEST-178

--- sql/moz-fx-data-shared-prod/monitoring/suggest_impression_rate_live/view.sql
+++ sql/moz-fx-data-shared-prod/monitoring/suggest_impression_rate_live/view.sql
@@ -7,6 +7,9 @@ FROM
   `moz-fx-data-shared-prod.monitoring_derived.suggest_impression_rate_live_v1`
 WHERE
   DATE(submission_minute) >= DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)
+-- Because the underyling quicksuggest_impression_v1 table has a short
+-- retention period, the materialized view also has a short retention period
+-- and we need to maintain a separate table for history beyond 2 days.
 UNION ALL
 SELECT
   *
--- sql/moz-fx-data-shared-prod/monitoring_derived/suggest_impression_rate_v1/init.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/suggest_impression_rate_v1/init.sql
@@ -6,7 +6,13 @@ AS
 SELECT
   TIMESTAMP_TRUNC(submission_timestamp, minute) AS submission_minute,
   COUNT(*) AS n,
+  COUNTIF(release_channel = "release") AS n_release,
+  COUNTIF(release_channel = "beta") AS n_beta,
+  COUNTIF(release_channel = "nightly") AS n_nightly,
   COUNT(request_id) AS n_merino,
+  COUNTIF(request_id IS NOT NULL AND release_channel = "release") AS n_merino_release,
+  COUNTIF(request_id IS NOT NULL AND release_channel = "beta") AS n_merino_beta,
+  COUNTIF(request_id IS NOT NULL AND release_channel = "nightly") AS n_merino_nightly,
 FROM
   -- For this initialization query, we select from the sanitized table, which contains
   -- 15 days of history rather than just 2.

b72bceca318a776500731a006c3198d5a942151f
Daniel Thorn <dthorn@mozilla.com>
2022-02-01 15:38:30 -0800

    update init

--- sql/moz-fx-data-shared-prod/mozilla_vpn_derived/fxa_attribution_v2/init.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn_derived/fxa_attribution_v2/init.sql
@@ -3,14 +3,18 @@ CREATE TABLE IF NOT EXISTS
     flow_id STRING,
     flow_started TIMESTAMP,
     fxa_uids ARRAY<STRING>,
-    attribution STRUCT<
-      `timestamp` TIMESTAMP,
-      entrypoint_experiment STRING,
-      entrypoint_variation STRING,
-      utm_campaign STRING,
-      utm_content STRING,
-      utm_medium STRING,
-      utm_source STRING,
-      utm_term STRING
+    services ARRAY<STRING>,
+    attribution ARRAY<
+      STRUCT<
+        min_timestamp TIMESTAMP,
+        max_timestamp TIMESTAMP,
+        entrypoint_experiment STRING,
+        entrypoint_variation STRING,
+        utm_campaign STRING,
+        utm_content STRING,
+        utm_medium STRING,
+        utm_source STRING,
+        utm_term STRING
+      >
     >
   )

f8649e8006ecb672178e296cc08b05d59087271c
Daniel Thorn <dthorn@mozilla.com>
2022-02-15 13:48:04 -0800

    Add views of new vpn funnel tables

---
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/channel_group_proportions/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozilla_vpn.channel_group_proportions`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod`.mozilla_vpn_derived.channel_group_proportions_v1

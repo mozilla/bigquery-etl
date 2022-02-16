b775d5322ec7bef0b036fed0e9d8d7096e724d51
Alekhya <88394696+alekhyamoz@users.noreply.github.com>
2022-02-16 14:31:02 -0500

    added 0.1 and 1 percentiles for both desktop and fenix data (#2723)

--- sql/moz-fx-data-shared-prod/telemetry_derived/histogram_percentiles_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/histogram_percentiles_v1/query.sql
@@ -1,6 +1,8 @@
 SELECT
   * EXCEPT (aggregates) REPLACE('percentiles' AS agg_type),
   ARRAY<STRUCT<key STRING, value FLOAT64>>[
+    ('0.1', mozfun.glam.percentile(0.1, aggregates, metric_type)),
+    ('1', mozfun.glam.percentile(1, aggregates, metric_type)),
     ('5', mozfun.glam.percentile(5, aggregates, metric_type)),
     ('25', mozfun.glam.percentile(25, aggregates, metric_type)),
     ('50', mozfun.glam.percentile(50, aggregates, metric_type)),

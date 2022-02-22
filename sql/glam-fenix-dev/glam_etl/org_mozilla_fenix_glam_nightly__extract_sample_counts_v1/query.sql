ded8fb3a0ebb97915a5cab3c3788e0c4b7d47fbc
akommasani@mozilla.com <akommasani@mozilla.com>
2022-02-22 14:55:20 -0500

    added sample counts for desktop and glean products (both scalars and histograms)
     added sample counts for desktop and glean products (both scalars and histograms)
    
     added sample counts for desktop and glean products (both scalars and histograms)
    
     added sample counts for desktop and glean products (both scalars and histograms)
    
    add sample counts for scalars and histogram

--- sql/glam-fenix-dev/glam_etl/org_mozilla_fenix_glam_nightly__extract_sample_counts_v1/query.sql
+++ sql/glam-fenix-dev/glam_etl/org_mozilla_fenix_glam_nightly__extract_sample_counts_v1/query.sql
@@ -1,17 +1,25 @@
--- query for org_mozilla_fenix_glam_nightly__extract_sample_counts_v1;
 SELECT
-  channel,
-  app_version,
-  metric,
-  key,
-  coalesce(ping_type, "*") AS ping_type,
-  COALESCE(app_build_id, "*") AS app_build_id,
-  IF(
-    app_build_id = "*",
-    NULL,
-    SAFE_CAST(mozfun.glam.build_hour_to_datetime(app_build_id) AS STRING)
-  ) AS build_date,
-  COALESCE(os, "*") AS os,
+  cp.channel,
+  cp.app_version,
+  cp.ping_type,
+  COALESCE(cp.app_build_id, "*") AS app_build_id,
+  COALESCE(cp.os, "*") AS os,
+  cp.metric,
+  cp.key,
+  cp.client_agg_type,
   total_sample
 FROM
-  `glam_etl.org_mozilla_fenix_glam_nightly__view_sample_counts_v1`
+  `glam_etl.org_mozilla_fenix_glam_nightly__view_probe_counts_v1` cp
+LEFT JOIN
+  `glam_etl.org_mozilla_fenix_glam_nightly__view_sample_counts_v1` sc
+ON
+  sc.os = COALESCE(cp.os, "*")
+  AND sc.app_build_id = COALESCE(cp.app_build_id, "*")
+  AND sc.app_version = cp.app_version
+  AND sc.metric = cp.metric
+  AND cp.key = sc.key
+  AND total_sample IS NOT NULL
+WHERE
+  cp.app_version IS NOT NULL
+  AND total_users > 10
+  AND cp.client_agg_type NOT IN ('sum', 'min', 'avg', 'max')

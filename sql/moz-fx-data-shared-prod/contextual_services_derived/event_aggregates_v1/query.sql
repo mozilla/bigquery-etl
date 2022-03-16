45865eeba65ce09f9cd09bdc209d72a6c873a935
Jeff Klukas <jeff@klukas.net>
2022-03-16 17:22:19 -0400

    CONSVC-1681 Add mobile data to contextual services event_aggregates
    See https://mozilla-hub.atlassian.net/browse/CONSVC-1681

--- sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
@@ -4,6 +4,7 @@ WITH combined AS (
     DATE(submission_timestamp) AS submission_date,
     'suggest' AS source,
     'impression' AS event_type,
+    'desktop' AS form_factor,
     normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
@@ -26,6 +27,7 @@ WITH combined AS (
     DATE(submission_timestamp) AS submission_date,
     'suggest' AS source,
     'click' AS event_type,
+    'desktop' AS form_factor,
     normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
@@ -48,6 +50,7 @@ WITH combined AS (
     DATE(submission_timestamp) AS submission_date,
     'topsites' AS source,
     'impression' AS event_type,
+    'desktop' AS form_factor,
     normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
@@ -70,6 +73,7 @@ WITH combined AS (
     DATE(submission_timestamp) AS submission_date,
     'topsites' AS source,
     'click' AS event_type,
+    'desktop' AS form_factor,
     normalized_country_code AS country,
     metadata.geo.subdivision1 AS subdivision1,
     advertiser,
@@ -86,6 +90,81 @@ WITH combined AS (
     AS provider,
   FROM
     contextual_services.topsites_click
+  UNION ALL
+  SELECT
+    metrics.uuid.top_sites_context_id AS context_id,
+    DATE(submission_timestamp) AS submission_date,
+    'topsites' AS source,
+    IF(events[SAFE_OFFSET(0)].name = 'contile_click', 'click', 'impression') AS event_type,
+    'mobile' AS form_factor,
+    normalized_country_code AS country,
+    metadata.geo.subdivision1 AS subdivision1,
+    metrics.string.top_sites_contile_advertiser AS advertiser,
+    'release' AS release_channel,
+    CAST(
+      (SELECT value FROM UNNEST(events[SAFE_OFFSET(0)].extra) WHERE key = 'position') AS INT64
+    ) AS position,
+    CASE
+    WHEN
+      metrics.url2.top_sites_contile_reporting_url IS NULL
+    THEN
+      'remote settings'
+    ELSE
+      'contile'
+    END
+    AS provider,
+  FROM
+    org_mozilla_firefox.topsites_impression
+  UNION ALL
+  SELECT
+    metrics.uuid.top_sites_context_id AS context_id,
+    DATE(submission_timestamp) AS submission_date,
+    'topsites' AS source,
+    IF(events[SAFE_OFFSET(0)].name = 'contile_click', 'click', 'impression') AS event_type,
+    'mobile' AS form_factor,
+    normalized_country_code AS country,
+    metadata.geo.subdivision1 AS subdivision1,
+    metrics.string.top_sites_contile_advertiser AS advertiser,
+    'beta' AS release_channel,
+    CAST(
+      (SELECT value FROM UNNEST(events[SAFE_OFFSET(0)].extra) WHERE key = 'position') AS INT64
+    ) AS position,
+    CASE
+    WHEN
+      metrics.url2.top_sites_contile_reporting_url IS NULL
+    THEN
+      'remote settings'
+    ELSE
+      'contile'
+    END
+    AS provider,
+  FROM
+    org_mozilla_firefox_beta.topsites_impression
+  UNION ALL
+  SELECT
+    metrics.uuid.top_sites_context_id AS context_id,
+    DATE(submission_timestamp) AS submission_date,
+    'topsites' AS source,
+    IF(events[SAFE_OFFSET(0)].name = 'contile_click', 'click', 'impression') AS event_type,
+    'mobile' AS form_factor,
+    normalized_country_code AS country,
+    metadata.geo.subdivision1 AS subdivision1,
+    metrics.string.top_sites_contile_advertiser AS advertiser,
+    'nightly' AS release_channel,
+    CAST(
+      (SELECT value FROM UNNEST(events[SAFE_OFFSET(0)].extra) WHERE key = 'position') AS INT64
+    ) AS position,
+    CASE
+    WHEN
+      metrics.url2.top_sites_contile_reporting_url IS NULL
+    THEN
+      'remote settings'
+    ELSE
+      'contile'
+    END
+    AS provider,
+  FROM
+    org_mozilla_fenix.topsites_impression
 ),
 with_event_count AS (
   SELECT
@@ -95,7 +174,8 @@ with_event_count AS (
         submission_date,
         context_id,
         source,
-        event_type
+        event_type,
+        form_factor
     ) AS user_event_count,
   FROM
     combined
@@ -115,6 +195,7 @@ GROUP BY
   submission_date,
   source,
   event_type,
+  form_factor,
   country,
   subdivision1,
   advertiser,

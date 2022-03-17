ad057b02d3e144b294bf97f8ff33683400e6c7bb
Jeff Klukas <jeff@klukas.net>
2022-03-17 15:20:00 -0400

    CONSVC-1681 Monitoring queries for mobile contile telemetry
    See https://mozilla-hub.atlassian.net/browse/CONSVC-1681

---
+++ sql/moz-fx-data-shared-prod/monitoring_derived/topsites_rate_fenix_beta_live_v1/init.sql
@@ -0,0 +1,14 @@
+CREATE MATERIALIZED VIEW `moz-fx-data-shared-prod.monitoring_derived.topsites_rate_fenix_beta_live_v1`
+OPTIONS
+  (enable_refresh = TRUE, refresh_interval_minutes = 5)
+AS
+SELECT
+  TIMESTAMP_TRUNC(submission_timestamp, minute) AS submission_minute,
+  COUNTIF(events[SAFE_OFFSET(0)].name = 'contile_impression') AS n_impression,
+  COUNTIF(events[SAFE_OFFSET(0)].name = 'contile_click') AS n_click,
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_beta_live.topsites_impression_v1`
+WHERE
+  DATE(submission_timestamp) >= '2010-01-01'
+GROUP BY
+  submission_minute

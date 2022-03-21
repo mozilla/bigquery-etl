63ac93d8442a25610e4f6241dd6c5dc35701d813
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-03-21 21:31:48 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/mozilla_vpn/events_unnested/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/events_unnested/view.sql
@@ -1,4 +1,6 @@
--- Generated via ./bqetl generate glean_usage
+-- Created by hand (rather than via ./bqetl generate glean_usage)
+-- because the VPN doesn't have an events ping (just a "main" one,
+-- in which there are events)
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.mozilla_vpn.events_unnested`
 AS
@@ -22,6 +24,6 @@ SELECT
   event.name AS event_name,
   event.extra AS event_extra,
 FROM
-  `moz-fx-data-shared-prod.mozillavpn.events` AS e
+  `moz-fx-data-shared-prod.mozilla_vpn.main` AS e
 CROSS JOIN
   UNNEST(e.events) AS event

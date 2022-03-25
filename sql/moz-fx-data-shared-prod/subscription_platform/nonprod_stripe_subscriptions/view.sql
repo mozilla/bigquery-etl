6e72117fb4cb51d726923af467142c46953bd414
Daniel Thorn <dthorn@mozilla.com>
2022-03-24 17:26:49 -0700

    Fix typo in view name (#2824)

--- sql/moz-fx-data-shared-prod/subscription_platform/nonprod_stripe_subscriptions/view.sql
+++ sql/moz-fx-data-shared-prod/subscription_platform/nonprod_stripe_subscriptions/view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.subscription_plaftorm.nonprod_stripe_subscriptions`
+  `moz-fx-data-shared-prod.subscription_platform.nonprod_stripe_subscriptions`
 AS
 WITH subscriptions AS (
   SELECT

af403f2f99a27c4942b87141ab7d4d34a935967a
Daniel Thorn <dthorn@mozilla.com>
2022-03-24 17:06:14 -0700

    Update stripe billing grace period (#2821)
    for stripe smart retries

--- sql/moz-fx-data-shared-prod/mozilla_vpn_derived/all_subscriptions_v1/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn_derived/all_subscriptions_v1/query.sql
@@ -72,8 +72,8 @@ stripe_subscriptions AS (
       "-",
       (plan_amount / 100)
     ) AS pricing_plan,
-    -- Stripe default billing grace period is 1 day and Paypal is billed by Stripe
-    INTERVAL 1 DAY AS billing_grace_period,
+    -- Stripe billing grace period is 7 day and Paypal is billed by Stripe
+    INTERVAL 7 DAY AS billing_grace_period,
     promotion_codes,
   FROM
     mozdata.subscription_platform.stripe_subscriptions

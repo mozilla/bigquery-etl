9cd2dd66273401d180eeb3a6287262cb51452b8d
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-03-29 19:16:47 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_accounts/fxa_content_auth_events/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/fxa_content_auth_events/view.sql
@@ -15,7 +15,7 @@ WITH content AS (
     jsonPayload.fields.user_properties,
     jsonPayload.fields.event_properties,
     `timestamp`,
-    receiveTimestamp
+    receiveTimestamp,
   FROM
     `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_content_events_v1`
 ),
@@ -33,7 +33,7 @@ auth AS (
     jsonPayload.fields.user_properties,
     jsonPayload.fields.event_properties,
     `timestamp`,
-    receiveTimestamp
+    receiveTimestamp,
   FROM
     `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_auth_events_v1`
 ),
@@ -52,50 +52,30 @@ unioned AS (
   --
 SELECT
   * EXCEPT (user_properties, event_properties),
-  REPLACE(JSON_EXTRACT(user_properties, '$.utm_term'), "\"", "") AS utm_term,
-  REPLACE(JSON_EXTRACT(user_properties, '$.utm_source'), "\"", "") AS utm_source,
-  REPLACE(JSON_EXTRACT(user_properties, '$.utm_medium'), "\"", "") AS utm_medium,
-  REPLACE(JSON_EXTRACT(user_properties, '$.utm_campaign'), "\"", "") AS utm_campaign,
-  REPLACE(JSON_EXTRACT(user_properties, '$.utm_content'), "\"", "") AS utm_content,
-  REPLACE(JSON_EXTRACT(user_properties, '$.ua_version'), "\"", "") AS ua_version,
-  REPLACE(JSON_EXTRACT(user_properties, '$.ua_browser'), "\"", "") AS ua_browser,
-  REPLACE(JSON_EXTRACT(user_properties, '$.entrypoint'), "\"", "") AS entrypoint,
-  REPLACE(
-    JSON_EXTRACT(user_properties, '$.entrypoint_experiment'),
-    "\"",
-    ""
-  ) AS entrypoint_experiment,
-  REPLACE(
-    JSON_EXTRACT(user_properties, '$.entrypoint_variation'),
-    "\"",
-    ""
-  ) AS entrypoint_variation,
-  REPLACE(JSON_EXTRACT(user_properties, '$.flow_id'), "\"", "") AS flow_id,
-  REPLACE(JSON_EXTRACT(event_properties, '$.service'), "\"", "") AS service,
-  REPLACE(JSON_EXTRACT(event_properties, '$.email_type'), "\"", "") AS email_type,
-  REPLACE(JSON_EXTRACT(event_properties, '$.email_provider'), "\"", "") AS email_provider,
-  REPLACE(JSON_EXTRACT(event_properties, '$.oauth_client_id'), "\"", "") AS oauth_client_id,
-  REPLACE(JSON_EXTRACT(event_properties, '$.connect_device_flow'), "\"", "") AS connect_device_flow,
-  REPLACE(JSON_EXTRACT(event_properties, '$.connect_device_os'), "\"", "") AS connect_device_os,
-  REPLACE(JSON_EXTRACT(user_properties, '$.sync_device_count'), "\"", "") AS sync_device_count,
-  REPLACE(
-    JSON_EXTRACT(user_properties, '$.sync_active_devices_day'),
-    "\"",
-    ""
-  ) AS sync_active_devices_day,
-  REPLACE(
-    JSON_EXTRACT(user_properties, '$.sync_active_devices_week'),
-    "\"",
-    ""
-  ) AS sync_active_devices_week,
-  REPLACE(
-    JSON_EXTRACT(user_properties, '$.sync_active_devices_month'),
-    "\"",
-    ""
-  ) AS sync_active_devices_month,
-  REPLACE(JSON_EXTRACT(event_properties, '$.email_sender'), "\"", "") AS email_sender,
-  REPLACE(JSON_EXTRACT(event_properties, '$.email_service'), "\"", "") AS email_service,
-  REPLACE(JSON_EXTRACT(event_properties, '$.email_template'), "\"", "") AS email_template,
-  REPLACE(JSON_EXTRACT(event_properties, '$.email_version'), "\"", "") AS email_version
+  JSON_VALUE(user_properties, "$.utm_term") AS utm_term,
+  JSON_VALUE(user_properties, "$.utm_source") AS utm_source,
+  JSON_VALUE(user_properties, "$.utm_medium") AS utm_medium,
+  JSON_VALUE(user_properties, "$.utm_campaign") AS utm_campaign,
+  JSON_VALUE(user_properties, "$.utm_content") AS utm_content,
+  JSON_VALUE(user_properties, "$.ua_version") AS ua_version,
+  JSON_VALUE(user_properties, "$.ua_browser") AS ua_browser,
+  JSON_VALUE(user_properties, "$.entrypoint") AS entrypoint,
+  JSON_VALUE(user_properties, "$.entrypoint_experiment") AS entrypoint_experiment,
+  JSON_VALUE(user_properties, "$.entrypoint_variation") AS entrypoint_variation,
+  JSON_VALUE(user_properties, "$.flow_id") AS flow_id,
+  JSON_VALUE(event_properties, "$.service") AS service,
+  JSON_VALUE(event_properties, "$.email_type") AS email_type,
+  JSON_VALUE(event_properties, "$.email_provider") AS email_provider,
+  JSON_VALUE(event_properties, "$.oauth_client_id") AS oauth_client_id,
+  JSON_VALUE(event_properties, "$.connect_device_flow") AS connect_device_flow,
+  JSON_VALUE(event_properties, "$.connect_device_os") AS connect_device_os,
+  JSON_VALUE(user_properties, "$.sync_device_count") AS sync_device_count,
+  JSON_VALUE(user_properties, "$.sync_active_devices_day") AS sync_active_devices_day,
+  JSON_VALUE(user_properties, "$.sync_active_devices_week") AS sync_active_devices_week,
+  JSON_VALUE(user_properties, "$.sync_active_devices_month") AS sync_active_devices_month,
+  JSON_VALUE(event_properties, "$.email_sender") AS email_sender,
+  JSON_VALUE(event_properties, "$.email_service") AS email_service,
+  JSON_VALUE(event_properties, "$.email_template") AS email_template,
+  JSON_VALUE(event_properties, "$.email_version") AS email_version,
 FROM
   unioned

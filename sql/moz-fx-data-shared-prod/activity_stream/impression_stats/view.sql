bc675a78519832e110f1f874a452eb8337de7ba7
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-03-30 16:02:17 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/activity_stream/impression_stats/view.sql
+++ sql/moz-fx-data-shared-prod/activity_stream/impression_stats/view.sql
@@ -1,8 +1,10 @@
--- Generated via ./bqetl generate stable_views
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.activity_stream.impression_stats`
 AS
 SELECT
   * REPLACE (mozfun.norm.metadata(metadata) AS metadata)
 FROM
+  -- We explicitly choose v1 for now, but will transition to a unioned view
+  -- over v1 and v2 as the new schema is rolled out; see
+  -- https://bugzilla.mozilla.org/show_bug.cgi?id=1761790
   `moz-fx-data-shared-prod.activity_stream_stable.impression_stats_v1`

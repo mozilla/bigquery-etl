200eaddcd981c7952f9e3d377c10d6dcb32754e4
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-04-04 18:41:49 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-cjms-prod-f3c7/cjms_bigquery/flows_v1/query.sql
+++ sql/moz-fx-cjms-prod-f3c7/cjms_bigquery/flows_v1/query.sql
@@ -1,53 +1,22 @@
-WITH fxa_content_auth_stdout_events_live AS (
-  SELECT
-    PARSE_DATE('%y%m%d', _TABLE_SUFFIX) AS submission_date,
-    JSON_VALUE(jsonPayload.fields.user_properties, '$.flow_id') AS flow_id,
-    `timestamp`,
-    TO_HEX(SHA256(jsonPayload.fields.user_id)) AS fxa_uid,
-  FROM
-    `moz-fx-fxa-prod-0712.fxa_stage_logs.docker_fxa_auth_20*`
-  WHERE
-    jsonPayload.type = 'amplitudeEvent'
-    AND jsonPayload.fields.event_type IS NOT NULL
-    AND jsonPayload.fields.user_id IS NOT NULL
-  UNION ALL
-  SELECT
-    PARSE_DATE('%y%m%d', _TABLE_SUFFIX) AS submission_date,
-    JSON_VALUE(jsonPayload.fields.user_properties, '$.flow_id') AS flow_id,
-    `timestamp`,
-    TO_HEX(SHA256(jsonPayload.fields.user_id)) AS fxa_uid,
-  FROM
-    `moz-fx-fxa-prod-0712.fxa_stage_logs.docker_fxa_content_20*`
-  WHERE
-    jsonPayload.type = 'amplitudeEvent'
-    AND jsonPayload.fields.event_type IS NOT NULL
-  UNION ALL
-  SELECT
-    PARSE_DATE('%y%m%d', _TABLE_SUFFIX) AS submission_date,
-    JSON_VALUE(jsonPayload.fields.user_properties, '$.flow_id') AS flow_id,
-    `timestamp`,
-    TO_HEX(SHA256(jsonPayload.fields.user_id)) AS fxa_uid,
-  FROM
-    `moz-fx-fxa-prod-0712.fxa_stage_logs.stdout_20*`
-  WHERE
-    jsonPayload.type = 'amplitudeEvent'
-    AND jsonPayload.fields.event_type IS NOT NULL
-)
 SELECT
-  submission_date,
+  DATE(`timestamp`) AS submission_date,
   flow_id,
   MIN(`timestamp`) AS flow_started,
   ARRAY_AGG(
-    IF(fxa_uid IS NULL, NULL, STRUCT(fxa_uid, `timestamp` AS fxa_uid_timestamp)) IGNORE NULLS
+    IF(
+      user_id IS NULL,
+      NULL,
+      STRUCT(user_id AS fxa_uid, `timestamp` AS fxa_uid_timestamp)
+    ) IGNORE NULLS
     ORDER BY
       `timestamp` DESC
     LIMIT
       1
   )[SAFE_OFFSET(0)].*,
 FROM
-  fxa_content_auth_stdout_events_live
+  mozdata.firefox_accounts.fxa_content_auth_stdout_events
 WHERE
-  submission_date = @submission_date
+  DATE(`timestamp`) = @submission_date
   AND flow_id IS NOT NULL
 GROUP BY
   submission_date,

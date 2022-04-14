2042fb716329da71c4076f1b545cc506f45343d6
Alexander Nicholson <anicholson@mozilla.com>
2022-04-14 16:10:00 -0400

    Coalesce country in unified metrics

--- sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.unified_metrics`
 AS
 SELECT
-  *
+  * REPLACE (COALESCE(country, '??') AS country)
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.unified_metrics_v1`

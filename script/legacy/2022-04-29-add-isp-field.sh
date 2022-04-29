7be6116fd0c84b47271a3195e7a6958ab967e5fb
Jeff Klukas <jeff@klukas.net>
2022-04-29 16:51:02 -0400

    Bug 1757216 Add ISP field to baseline_clients_daily
    Replaces #2919
    
    This will enable use to filter out clients sent by BrowserStack in downstream tables and queries.

---
+++ script/legacy/2022-04-29-add-isp-field.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# It's rare that we need to add a new field to the generated glean ETL for baseline_clients_daily
+# but it's fiddly when we do. There are dozens of tables affected and our dependency resolution for
+# schema updates with downstream detection is insufficient to cross the per-addId to per-app boundary.
+# As such, it's practical to run a custom script instead to inject the schema of affected tables.
+
+# See PR X for context.
+
+
+# This script assumes that the following has been run locally:
+
+# bqetl generate glean_usage
+# bqetl query schema update 'moz-fx-data-shared-prod.*.baseline_clients_daily_v1'
+# bqetl query schema update 'moz-fx-data-shared-prod.*.baseline_clients_last_seen_v1'
+# bqetl query schema update 'moz-fx-data-shared-prod.*.clients_last_seen_joined_v1'
+
+
+# Function to list out all target tables in project:dataset.table format
+tables_to_modify() {
+    find . -name query.sql | sort \
+        | grep -v telemetry_derived \
+        | grep -E 'baseline_clients_daily_v1|baseline_clients_last_seen_v1|clients_last_seen_joined_v1' \
+        | sed 's,./sql/\(.*\)/\(.*\)/\(.*\)/query.sql,\1:\2.\3,'
+}
+
+#for t in $(tables_to_modify)
+for t in mozdata:tmp.klukas_isp_test1
+do
+    echo "Processing $t"
+
+    # Note that if we accidentally run this twice, the bq update should fail with message
+    # "BigQuery error in update operation: Field isp already exists in schema"
+    bq show --schema "$t" \
+        | jq '. + [{"name":"isp","type":"STRING","mode":"NULLABLE"}]' \
+        | bq update --schema /dev/stdin "$t"
+done

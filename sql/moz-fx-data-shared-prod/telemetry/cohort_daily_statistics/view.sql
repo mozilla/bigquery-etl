e7b4ef45d24936b988f6a974ab42a7fe90822984
Alexander Nicholson <anicholson@mozilla.com>
2022-05-13 11:41:33 -0400

    Removed cohort daily view

--- sql/moz-fx-data-shared-prod/telemetry/cohort_daily_statistics/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.cohort_daily_statistics`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.cohort_daily_statistics_v1`

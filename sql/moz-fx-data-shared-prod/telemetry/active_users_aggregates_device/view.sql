92d21c9632fb82ba8f8d6d78aebe710a427e65b6
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-05-24 17:43:44 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/telemetry/active_users_aggregates_device/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.active_users_aggregates_device`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.active_users_aggregates_device_v1`

d1af83b4af0a42334f0ab43f0bb5afc38a38c08e
Chelsea Troy <chelsea.dommert@gmail.com>
2022-06-14 11:25:07 -0500

    Add second table to facilitate request level sanitized queries and queries passed through allow list plus merged with telemetry data

---
+++ sql/moz-fx-data-shared-prod/search_terms_derived/merino_log_sanitized_v3/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS
+  `moz-fx-data-shared-prod:search_terms_derived.merino_log_sanitized_v3`(
+    timestamp timestamp,
+    request_id string,
+    query string,
+    country string,
+    region string,
+    dma string,
+    form_factor string,
+    browser string,
+    os_family string
+  )
+PARTITION BY
+  DATE(timestamp)
+OPTIONS
+  (require_partition_filter = TRUE, partition_expiration_days = 15);
---
+++ sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/query.sql
@@ -0,0 +1,57 @@
+/*
+
+This query simply copies data from a table in the restricted suggest-searches-prod
+project to shared-prod, but throws an error on empty input in order to signal an
+upstream delay or error.
+
+In order to produce the error, it includes aggregations and join steps.
+
+*/
+WITH sanitized_impressions AS (
+  SELECT
+    *
+  FROM
+    `suggest-searches-prod-a30f.sanitized.suggest_impression_sanitized_v3`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+),
+sanitized_impressions_count AS (
+  SELECT
+    COUNT(*) AS _n,
+    COUNT(sanitized_query) AS _n_with_query,
+  FROM
+    sanitized_impressions
+),
+-- We perform a LEFT JOIN on TRUE as a workaround to attach the count to every
+-- row from the impressions table; the LEFT JOIN has the important property that
+-- if the input impressions partition is empty, we will still get a single row of
+-- output, which allows us to raise an error in the WHERE clause.
+validated_impressions AS (
+  SELECT
+    * EXCEPT (_n, _n_with_query),
+  FROM
+    sanitized_impressions_count
+  LEFT JOIN
+    sanitized_impressions
+  ON
+    TRUE
+  WHERE
+    IF(
+      _n < 1,
+      ERROR(
+        "The source partition of suggest-searches-prod-a30f.sanitized.suggest_impression_sanitized_v3 is empty; retry later or investigate upstream issues"
+      ),
+      TRUE
+    )
+    AND IF(
+      _n_with_query < 1,
+      ERROR(
+        "The source partition of suggest-searches-prod-a30f.sanitized.suggest_impression_sanitized_v3 contains rows, but none have sanitized_query populated; investigate upstream issues with log routing"
+      ),
+      TRUE
+    )
+)
+SELECT
+  *
+FROM
+  validated_impressions

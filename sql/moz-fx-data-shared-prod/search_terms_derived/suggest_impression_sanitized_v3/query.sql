4e00b7f92a71ff18660d6190b7eaeb4bd9a8d4bf
Chelsea Troy <chelsea.dommert@gmail.com>
2022-06-15 12:08:20 -0500

    Add safety check for throwing an error if the source table is empty

--- sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/query.sql
+++ sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/query.sql
@@ -42,11 +42,40 @@ sanitized_queries AS (
     `moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3`
   WHERE
     DATE(timestamp) = @submission_date
+),
+sanitized_queries_count AS (
+  SELECT
+    COUNT(*) AS _n,
+    COUNT(sanitized_query) AS _n_with_query,
+  FROM
+    sanitized_queries
+),
+-- We perform a LEFT JOIN on TRUE as a workaround to attach the count to every
+-- row from the impressions table; the LEFT JOIN has the important property that
+-- if the input impressions partition is empty, we will still get a single row of
+-- output, which allows us to raise an error in the WHERE clause.
+validated_queries AS (
+  SELECT
+    * EXCEPT (_n, _n_with_query),
+  FROM
+    sanitized_queries_count
+  LEFT JOIN
+    sanitized_queries
+  ON
+    TRUE
+  WHERE
+    IF(
+      _n < 1,
+      ERROR(
+        "The source partition of moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3 is empty; retry later or investigate upstream issues"
+      ),
+      TRUE
+    )
 )
 SELECT
   *
 FROM
-  sanitized_queries
+  validated_queries
 LEFT JOIN
   impressions
 USING

4ea5d0caac1411ff622e33295fe8f6152af0753f
Chelsea Troy <chelsea.dommert@gmail.com>
2022-06-29 11:01:06 -0500

    Reformat the view

--- sql/moz-fx-data-shared-prod/search_terms/sanitization_job_languages/view.sql
+++ sql/moz-fx-data-shared-prod/search_terms/sanitization_job_languages/view.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.search_terms.sanitization_job_languages`
 AS
 SELECT
-  started_at as job_start_time,
+  started_at AS job_start_time,
   lang.key AS language_code,
   SAFE_CAST(lang.value AS int) AS search_term_count
 FROM

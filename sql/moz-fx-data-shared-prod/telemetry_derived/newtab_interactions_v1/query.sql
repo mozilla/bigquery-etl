7b367637306925957c8603db9b6850fe0eb5d9f2
Alexander Nicholson <anicholson@mozilla.com>
2022-07-14 17:13:28 -0400

    Added aggregation, schema and metadata

--- sql/moz-fx-data-shared-prod/telemetry_derived/newtab_interactions_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/newtab_interactions_v1/query.sql
@@ -44,19 +44,18 @@ categorized_events AS (
         IF(event_name = "closed", event_timestamp, NULL) as newtab_visit_ended_at,
 
         -- Client/Session-unique attributes
-        normalized_os as os,
-        normalized_os_version as os_version,
-        normalized_country_code as country_code,
-        normalized_channel as channel,
-        client_info.app_display_version as browser_version,
-        "Firefox Desktop" as browser_name,
-        mozfun.map.get_key(event_details, "source") as newtab_open_source, --
+        normalized_os,
+        normalized_os_version,
+        normalized_country_code,
+        normalized_channel,
+        client_info.app_display_version,
+        mozfun.map.get_key(event_details, "source") as newtab_open_source,
         metrics.string.search_engine_private_engine_id as default_search_engine,
         metrics.string.search_engine_default_engine_id as default_private_search_engine,
         ping_info.experiments,
+        -- ??? private_browsing_mode
 
         -- Partially unique session attributes
-        mozfun.map.get_key(event_details, "is_sponsored") = "true" AND event_category = 'topsites' as is_sponsored_topsite_interaction,
         mozfun.map.get_key(event_details, "telemetry_id") as search_engine,
         mozfun.map.get_key(event_details, "search_access_point") as search_access_point,
         -- ??? topsite_advertiser_id
@@ -67,6 +66,49 @@ categorized_events AS (
         events_unnested
 )
 SELECT
-    *
+    newtab_visit_id,
+    client_id,
+    submission_date,
+
+    search_engine,
+    search_access_point,
+    -- topsite_advertiser_id,
+    -- topsite_position,
+
+    ANY_VALUE(experiments) as experiments,
+    ANY_VALUE(default_private_search_engine) as default_private_search_engine,
+    ANY_VALUE(default_search_engine) as default_search_engine,
+    ANY_VALUE(normalized_os) as os,
+    ANY_VALUE(normalized_os_version) as os_version,
+    ANY_VALUE(normalized_country_code) as country_code,
+    ANY_VALUE(normalized_channel) as channel,
+    ANY_VALUE(app_display_version) as browser_version,
+    "Firefox Desktop" as browser_name,
+    ANY_VALUE(newtab_open_source) as newtab_open_source,
+
+    MIN(newtab_visit_started_at) as newtab_visit_started_at,
+    MIN(newtab_visit_ended_at) as newtab_visit_ended_at,
+
+    COUNTIF(is_topsite_click) as topsite_clicks,
+    COUNTIF(is_sponsored_topsite_click) as sponsored_topsite_clicks,
+    COUNTIF(is_topsite_impression) as topsite_impressions,
+    COUNTIF(is_sponsored_topsite_impression) as sponsored_topsite_impressions,
+
+    COUNTIF(is_search_issued) as searches,
+
+    COUNTIF(is_tagged_search_ad_click) as tagged_search_ad_clicks,
+    COUNTIF(is_tagged_search_ad_impression) as tagged_search_ad_impressions,
+    COUNTIF(is_follow_on_search_ad_click) as follow_on_search_ad_clicks,
+    COUNTIF(is_follow_on_search_ad_impression) as follow_on_search_ad_impressions,
+    COUNTIF(is_tagged_search_ad_click AND is_follow_on_search_ad_click) as tagged_follow_on_search_ad_clicks,
+    COUNTIF(is_tagged_search_ad_impression AND is_follow_on_search_ad_impression) as tagged_follow_on_search_ad_impressions,
 FROM
     categorized_events
+GROUP BY
+    newtab_visit_id,
+    client_id,
+    submission_date,
+    search_engine,
+    search_access_point
+    -- topsite_advertiser_id,
+    -- topsite_position

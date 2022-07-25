6cd24bbd8229cf076faf44e1cf7c5b19958f7b36
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-07-25 21:41:20 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry/newtab_interactions/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/newtab_interactions/view.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.newtab_interactions`
 AS
 SELECT
-  *
+  * EXCEPT (search_engine),
+  `moz-fx-data-shared-prod`.udf.normalize_search_engine(search_engine) AS search_engine,
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.newtab_interactions_v1`

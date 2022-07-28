288865299fd0e93f9c287c0383273b7f65881ec3
Chelsea Troy <chelsea.dommert@gmail.com>
2022-07-28 14:13:57 -0500

    Create derived view for data validation tasks on search term sanitization

---
+++ sql/moz-fx-data-shared-prod/search_terms/sanitization_job_data_validation_metrics/view.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.search_terms.sanitization_job_data_validation_metrics`
+AS
+SELECT
+        finished_at,
+        total_search_terms_removed_by_sanitization_job / total_search_terms_analyzed AS pct_sanitized_search_terms,
+        contained_at / total_search_terms_analyzed AS pct_sanitized_contained_at,
+        contained_numbers / total_search_terms_analyzed AS pct_sanitized_contained_numbers,
+        contained_name / total_search_terms_analyzed AS pct_sanitized_contained_name,
+        sum_terms_containing_us_census_surname / total_search_terms_analyzed AS pct_terms_containing_us_census_surname,
+        sum_uppercase_chars_all_search_terms / sum_chars_all_search_terms AS pct_uppercase_chars_all_search_terms,
+        sum_words_all_search_terms / total_search_terms_analyzed AS avg_words_all_search_terms,
+        1 - languages.english_count / languages.all_languages_count AS pct_terms_non_english
+        FROM `moz-fx-data-shared-prod.search_terms_derived.sanitization_job_metadata_v2` AS metadata
+    JOIN
+    (
+        SELECT
+            job_start_time,
+            max(case when language_code = 'en' then search_term_count end) english_count,
+            sum(search_term_count) as all_languages_count,
+        FROM `moz-fx-data-shared-prod.search_terms.sanitization_job_languages`
+        GROUP BY job_start_time
+    ) AS languages
+    ON metadata.started_at = languages.job_start_time
+    WHERE status = 'SUCCESS'
+    ORDER BY finished_at DESC;

282253204a2c619c4f1edd067409fcc38566dbe8
Chelsea Troy <chelsea.dommert@gmail.com>
2022-07-28 16:22:47 -0500

    Switch to SAFE_DIVIDE from / to allow the job to succeed if dividing by 0

--- sql/moz-fx-data-shared-prod/search_terms/sanitization_job_data_validation_metrics/view.sql
+++ sql/moz-fx-data-shared-prod/search_terms/sanitization_job_data_validation_metrics/view.sql
@@ -3,14 +3,26 @@ CREATE OR REPLACE VIEW
 AS
 SELECT
   finished_at,
-  total_search_terms_removed_by_sanitization_job / total_search_terms_analyzed AS pct_sanitized_search_terms,
-  contained_at / total_search_terms_analyzed AS pct_sanitized_contained_at,
-  contained_numbers / total_search_terms_analyzed AS pct_sanitized_contained_numbers,
-  contained_name / total_search_terms_analyzed AS pct_sanitized_contained_name,
-  sum_terms_containing_us_census_surname / total_search_terms_analyzed AS pct_terms_containing_us_census_surname,
-  sum_uppercase_chars_all_search_terms / sum_chars_all_search_terms AS pct_uppercase_chars_all_search_terms,
-  sum_words_all_search_terms / total_search_terms_analyzed AS avg_words_all_search_terms,
-  1 - languages.english_count / languages.all_languages_count AS pct_terms_non_english
+  SAFE_DIVIDE(
+    total_search_terms_removed_by_sanitization_job,
+    total_search_terms_analyzed
+  ) AS pct_sanitized_search_terms,
+  SAFE_DIVIDE(contained_at, total_search_terms_analyzed) AS pct_sanitized_contained_at,
+  SAFE_DIVIDE(contained_numbers, total_search_terms_analyzed) AS pct_sanitized_contained_numbers,
+  SAFE_DIVIDE(contained_name, total_search_terms_analyzed) AS pct_sanitized_contained_name,
+  SAFE_DIVIDE(
+    sum_terms_containing_us_census_surname,
+    total_search_terms_analyzed
+  ) AS pct_terms_containing_us_census_surname,
+  SAFE_DIVIDE(
+    sum_uppercase_chars_all_search_terms,
+    sum_chars_all_search_terms
+  ) AS pct_uppercase_chars_all_search_terms,
+  SAFE_DIVIDE(
+    sum_words_all_search_terms,
+    total_search_terms_analyzed
+  ) AS avg_words_all_search_terms,
+  1 - SAFE_DIVIDE(languages.english_count, languages.all_languages_count) AS pct_terms_non_english
 FROM
   `moz-fx-data-shared-prod.search_terms_derived.sanitization_job_metadata_v2` AS metadata
 JOIN

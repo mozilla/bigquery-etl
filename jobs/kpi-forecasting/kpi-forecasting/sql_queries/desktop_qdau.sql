6567502ea7f1c60d51043235691961aad7bb9406
irrationalagent <leifdoines@gmail.com>
2022-08-02 11:56:30 -0600

    change kpi forecasts

---
+++ jobs/kpi-forecasting/kpi-forecasting/sql_queries/desktop_qdau.sql
@@ -0,0 +1,8 @@
+SELECT
+    submission_date,
+    COUNTIF(normalized_app_name = 'Firefox Desktop' AND active_hours_sum > 0 AND uri_count > 0) AS QDAU,
+FROM telemetry.unified_metrics
+WHERE `mozfun`.bits28.active_in_range(days_seen_bits, 0, 1)
+AND submission_date >= DATE(2018,1,1)
+GROUP BY 1q
+ORDER BY 1

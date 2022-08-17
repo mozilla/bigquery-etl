06dc870283dc972e0b2082f516c757557ef1ff13
Sean Rose <1994030+sean-rose@users.noreply.github.com>
2022-08-17 11:42:32 -0700

    Include `metrics` column in VPN Glean unified views. (#3156)

--- sql/moz-fx-data-shared-prod/mozilla_vpn/deletion_request/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/deletion_request/view.sql
@@ -3,11 +3,28 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.mozilla_vpn.deletion_request`
 AS
 SELECT
-  * EXCEPT (metrics)
+  *
 FROM
   `moz-fx-data-shared-prod.mozillavpn.deletion_request`
 UNION ALL
 SELECT
-  * EXCEPT (metrics)
+  * REPLACE (
+    STRUCT(
+      CAST(NULL AS ARRAY<STRUCT<key STRING, value STRING>>) AS jwe,
+      metrics.labeled_counter AS labeled_counter,
+      CAST(
+        NULL
+        AS
+          ARRAY<
+            STRUCT<
+              key STRING,
+              value ARRAY<STRUCT<key STRING, value STRUCT<denominator INT64, numerator INT64>>>
+            >
+          >
+      ) AS labeled_rate,
+      CAST(NULL AS ARRAY<STRUCT<key STRING, value STRING>>) AS url,
+      CAST(NULL AS ARRAY<STRUCT<key STRING, value STRING>>) AS text
+    ) AS metrics
+  )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_firefox_vpn.deletion_request`

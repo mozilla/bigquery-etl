dba05c78e2fad8fe0865a7279808ac8fe08220d3
Sean Rose <srose@mozilla.com>
2022-08-19 13:17:19 -0700

    Fix `events_unnested` views to consistently select the correct fields.

--- sql_generators/glean_usage/templates/cross_channel_events_unnested.view.sql
+++ sql_generators/glean_usage/templates/cross_channel_events_unnested.view.sql
@@ -15,7 +15,41 @@ SELECT
     {% else -%}
     "{{ channel }}" AS normalized_channel,
     {% endif -%}
-    -- Order of ping_info fields differs between tables; we're verbose here for compatibility
+    -- Order of some fields differs between tables; we're verbose here for compatibility
+    STRUCT(
+      client_info.android_sdk_version AS android_sdk_version,
+      client_info.app_build AS app_build,
+      client_info.app_channel AS app_channel,
+      client_info.app_display_version AS app_display_version,
+      client_info.architecture AS architecture,
+      client_info.client_id AS client_id,
+      client_info.device_manufacturer AS device_manufacturer,
+      client_info.device_model AS device_model,
+      client_info.first_run_date AS first_run_date,
+      client_info.locale AS locale,
+      client_info.os AS os,
+      client_info.os_version AS os_version,
+      client_info.telemetry_sdk_build AS telemetry_sdk_build,
+      client_info.build_date AS build_date
+    ) AS client_info,
+    (
+      SELECT AS STRUCT
+        metadata.* REPLACE (
+          STRUCT(
+            metadata.header.`date` AS `date`,
+            metadata.header.dnt AS dnt,
+            metadata.header.x_debug_id AS x_debug_id,
+            metadata.header.x_pingsender_version AS x_pingsender_version,
+            metadata.header.x_source_tags AS x_source_tags,
+            metadata.header.x_telemetry_agent AS x_telemetry_agent,
+            metadata.header.x_foxsec_ip_reputation AS x_foxsec_ip_reputation,
+            metadata.header.x_lb_tags AS x_lb_tags,
+            metadata.header.parsed_date AS parsed_date,
+            metadata.header.parsed_x_source_tags AS parsed_x_source_tags,
+            metadata.header.parsed_x_lb_tags AS parsed_x_lb_tags
+          ) AS header
+        )
+    ) AS metadata,
     STRUCT (
       ping_info.end_time,
       ping_info.experiments,

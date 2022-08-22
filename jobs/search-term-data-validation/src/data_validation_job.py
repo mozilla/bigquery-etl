a496e2e8a3ef40261f518e9b890dd30d6262f752
Perry McManis <pmcmanis@mozilla.com>
2022-08-22 15:05:44 -0400

    changed test import and updated formatting

--- jobs/search-term-data-validation/src/data_validation_job.py
+++ jobs/search-term-data-validation/src/data_validation_job.py
@@ -6,10 +6,17 @@ from collections import namedtuple
 
 from data_validation import retrieve_data_validation_metrics, record_validation_results
 
-parser = argparse.ArgumentParser(description="Validate Recent Search Input Against Historical Norms",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--data_validation_origin", help="Origin table for data validation metrics")
-parser.add_argument("--data_validation_reporting_destination", help="Table to store data validation metric test results")
+parser = argparse.ArgumentParser(
+    description="Validate Recent Search Input Against Historical Norms",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "--data_validation_origin", help="Origin table for data validation metrics"
+)
+parser.add_argument(
+    "--data_validation_reporting_destination",
+    help="Table to store data validation metric test results",
+)
 args = parser.parse_args()
 
 validation_df = retrieve_data_validation_metrics(args.data_validation_origin)

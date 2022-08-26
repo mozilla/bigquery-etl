3c2230893e2e1349737c1c2645ddb2dc6620c777
skahmann3 <16420065+skahmann3@users.noreply.github.com>
2022-08-26 09:54:37 -0700

    Add browser info to unified metrics

--- sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.unified_metrics`
 AS
 SELECT
-  * REPLACE (COALESCE(country, '??') AS country)
+  * REPLACE (COALESCE(country, '??') AS country),
+  `mozfun.norm.browser_version_info`(app_version) as browser_version_info
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.unified_metrics_v1`

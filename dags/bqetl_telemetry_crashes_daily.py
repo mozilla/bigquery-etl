34ffb949c80314f5da91a5a77cd4cf105471c403
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-08-30 18:10:39 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ dags/bqetl_telemetry_crashes_daily.py
@@ -0,0 +1,69 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, gke_command
+
+docs = """
+### bqetl_telemetry_crashes_daily
+
+Built from bigquery-etl repo, [`dags/bqetl_telemetry_crashes_daily.py`](https://github.com/mozilla/bigquery-etl/blob/main/dags/bqetl_telemetry_crashes_daily.py)
+
+#### Description
+
+Telemetry crashes daily
+#### Owner
+
+frank@mozilla.com
+"""
+
+
+default_args = {
+    "owner": "frank@mozilla.com",
+    "start_date": datetime.datetime(2022, 8, 30, 0, 0),
+    "end_date": None,
+    "email": ["telemetry-alerts@mozilla.com", "frank@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+}
+
+tags = ["impact/tier_2", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_telemetry_crashes_daily",
+    default_args=default_args,
+    schedule_interval="0 1 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+
+    crashes_daily_v1 = bigquery_etl_query(
+        task_id="crashes_daily_v1",
+        destination_table="crashes_daily_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="frank@mozilla.com",
+        email=["frank@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    wait_for_copy_deduplicate_all = ExternalTaskSensor(
+        task_id="wait_for_copy_deduplicate_all",
+        external_dag_id="copy_deduplicate",
+        external_task_id="copy_deduplicate_all",
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    crashes_daily_v1.set_upstream(wait_for_copy_deduplicate_all)

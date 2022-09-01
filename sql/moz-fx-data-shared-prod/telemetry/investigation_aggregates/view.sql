6a5ea1809a9bde0674b8466457def9c820e3339d
Lucia Vargas <lvargas@mozilla.com>
2022-09-01 11:50:42 +0200

    DENG-179 update dag and remove not required view.

--- sql/moz-fx-data-shared-prod/telemetry/investigation_aggregates/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.investigation_aggregates`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.investigation_aggregates_v1`

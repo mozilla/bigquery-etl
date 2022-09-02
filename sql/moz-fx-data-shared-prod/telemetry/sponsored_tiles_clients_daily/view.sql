bfc6a61452027b4a02519d5697c026f0ce38e3d1
skahmann3 <16420065+skahmann3@users.noreply.github.com>
2022-09-02 16:31:39 -0700

    sponsored_tiles_clients_daily
    Client-level code for desktop + mobile. Issue with UNION ALL for desktop and mobile tables as the experiments structure is different between the devices

---
+++ sql/moz-fx-data-shared-prod/telemetry/sponsored_tiles_clients_daily/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.sponsored_tiles_clients_daily`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.sponsored_tiles_clients_daily_v1`

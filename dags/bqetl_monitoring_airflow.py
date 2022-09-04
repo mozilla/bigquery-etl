8bd40a40f745f8c726e6911dc38cf5b0a0690165
kik-kik <kignasiak01@gmail.com>
2022-09-04 16:59:02 +0200

    generated bqetl_monitoring_airflow dag

---
+++ dags/bqetl_monitoring_airflow.py
@@ -0,0 +1,124 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, gke_command
+
+docs = """
+### bqetl_monitoring_airflow
+
+Built from bigquery-etl repo, [`dags/bqetl_monitoring_airflow.py`](https://github.com/mozilla/bigquery-etl/blob/main/dags/bqetl_monitoring_airflow.py)
+
+#### Description
+
+This DAG schedules queries and scripts for populating datasets
+used for monitoring of Airflow DAGs.
+
+#### Owner
+
+kignasiak@mozilla.com
+"""
+
+
+default_args = {
+    "owner": "kignasiak@mozilla.com",
+    "start_date": datetime.datetime(2022, 9, 1, 0, 0),
+    "end_date": None,
+    "email": ["kignasiak@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+}
+
+tags = ["impact/tier_2", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_monitoring_airflow",
+    default_args=default_args,
+    schedule_interval="0 2 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+
+    monitoring_airflow_derived__dag__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__dag__v1",
+        destination_table="dag_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    monitoring_airflow_derived__dag_run__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__dag_run__v1",
+        destination_table="dag_run_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    monitoring_airflow_derived__dag_tag__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__dag_tag__v1",
+        destination_table="dag_tag_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    monitoring_airflow_derived__slot_pool__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__slot_pool__v1",
+        destination_table="slot_pool_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    monitoring_airflow_derived__task_fail__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__task_fail__v1",
+        destination_table="task_fail_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    monitoring_airflow_derived__task_reschedule__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__task_reschedule__v1",
+        destination_table="task_reschedule_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    monitoring_airflow_derived__user__v1 = bigquery_etl_query(
+        task_id="monitoring_airflow_derived__user__v1",
+        destination_table="user_v1",
+        dataset_id="monitoring_airflow_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=["kignasiak@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )

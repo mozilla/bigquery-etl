9ec9bae1ca16c358c4004bf1ab45bbb67f903ff5
Anna Scholtz <anna@scholtzan.net>
2022-09-07 09:50:04 -0700

    Update sql/mozfun/glean/legacy_compatible_experiments/udf.sql

--- sql/mozfun/glean/legacy_compatible_experiments/udf.sql
+++ sql/mozfun/glean/legacy_compatible_experiments/udf.sql
@@ -1,34 +1,34 @@
-CREATE OR REPLACE FUNCTION glean.legacy_compatible_experiments(ping_info__experiments ARRAY)
-RETURNS ARRAY AS (
-  ARRAY(
-    SELECT
-      STRUCT(x.key AS key, x.value.branch AS value)
-    FROM
-      UNNEST(ping_info__experiments) x
-  ) AS experiments
+CREATE OR REPLACE FUNCTION glean.legacy_compatible_experiments(
+  ping_info__experiments ARRAY<
+    STRUCT<key STRING, value STRUCT<branch STRING, extra STRUCT<type STRING>>>
+  >
+)
+RETURNS ARRAY<STRUCT<key STRING, value STRING>> AS (
+  ARRAY(SELECT STRUCT(x.key AS key, x.value.branch AS value) FROM UNNEST(ping_info__experiments) x)
 );
 
-                -- Tests
+-- Tests
 WITH ping_info AS (
   SELECT
-    ARRAY(
+    [
       STRUCT(
         "experiment_a" AS key,
-        STRUCT("control" AS branch, STRUCT("type" AS "firefox") AS extra) AS value
+        STRUCT("control" AS branch, STRUCT("firefox" AS type) AS extra) AS value
       ),
       STRUCT(
         "experiment_b" AS key,
-        STRUCT("treatment" AS branch, STRUCT("type" AS "firefoxOS") AS extra) AS value
-      ),
-    ) AS experiments
+        STRUCT("treatment" AS branch, STRUCT("firefoxOS" AS type) AS extra) AS value
+      )
+    ] AS experiments
 ),
 expected AS (
   SELECT
-    ARRAY(
+    [
       STRUCT("experiment_a" AS key, "control" AS value),
-      STRUCT("experiment_b" AS key, "treatment" AS value),
-    ) AS experiments
-),
+      STRUCT("experiment_b" AS key, "treatment" AS value)
+    ] AS experiments
 )
 SELECT
-  assert.equals(expected, glean.legacy_compatible_experiments(ping_info.experiments))
+  assert.equals(expected, glean.legacy_compatible_experiments(experiments))
+FROM
+  ping_info

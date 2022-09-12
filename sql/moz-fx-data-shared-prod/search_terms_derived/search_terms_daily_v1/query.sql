6aa615db04f0c678a56ecc34023bcf4341063c23
Rebecca BurWei <rebecca.burwei@gmail.com>
2022-09-12 16:26:25 -0500

    Update query.sql

--- sql/moz-fx-data-shared-prod/search_terms_derived/search_terms_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/search_terms_derived/search_terms_daily_v1/query.sql
@@ -1,9 +1,22 @@
--- Query for search_terms_derived.search_terms_daily_v1
-            -- For more information on writing queries see:
-            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
-SELECT
-  *
-FROM
-  table
-WHERE
-  submission_date = @submission_date
+with query_logs as (
+    SELECT
+        TIMESTAMP_TRUNC(timestamp, DATE )AS merino_date,
+        jsonPayload.fields.*
+    FROM `suggest-searches-prod-a30f.logs.stdout`
+    WHERE
+        jsonPayload.type = "web.suggest.request"
+        and jsonPayload.fields.session_id is not null
+), terminal_queries as (
+    select 
+        array_agg(ql order by sequence_no desc limit 1)[offset(0)].*
+    from query_logs ql
+    group by ql.session_id
+)
+select
+    merino_date,
+    query as search_term,
+    count(*) search_sessions
+from terminal_queries
+group by 1,2
+-- Level 2 aggregation. See: https://wiki.mozilla.org/Data_Publishing
+having count(*) > 5000

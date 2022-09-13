a2c01103b8698960b14fd051667ec9b690574e03
Eduardo Filho <edugomfilho@gmail.com>
2022-09-13 12:25:44 -0400

    Glam dev etl - fix docstring

--- bigquery_etl/util/glam_probe_utils.py
+++ bigquery_etl/util/glam_probe_utils.py
@@ -1,6 +1,5 @@
 """Functions that retrieve allow or block lists of probes."""
 from urllib.request import Request, urlopen
-import gzip
 import json
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
@@ -22,6 +21,7 @@ def get_etl_excluded_probes_quickfix(product):
 
 
 def temp_hardcoded_most_used_probes():
+    """Provide a temporary static list of most used probes - used for testing dev etl."""
     return [
         "pdfjs_editing",
         "wr_rasterize_glyphs_time",
@@ -38,9 +38,11 @@ def temp_hardcoded_most_used_probes():
 
 def get_most_used_probes():
     """Fetch and provide probes that have been used lately in Glam.
-    Important: The API does not distinguish from different products (e.g.: fenix, desktop), so make sure the list of probes from this function goes through a filter to avoid computing probes for a different product
-    """
 
+    Important: The API does not distinguish from different products (e.g.: fenix, desktop),
+    so make sure the list of probes from this function goes through a filter
+    to avoid computing probes for a different product.
+    """
     # Temporarily returning a list of hardcoded probes, for testing
     return temp_hardcoded_most_used_probes()
     """
@@ -49,21 +51,7 @@ def get_most_used_probes():
         f"{GLAM_USAGE_SERVICE}?fromDate={from_date}&fields=probe_name&actionType=PROBE_SEARCH&agg=count",
         None,
         {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0",
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Referer": "https://glam.telemetry.mozilla.org/firefox/probe/gc_animation_ms/explore?activeBuckets=%5B%22always%22%2C%22never%22%2C%22sometimes%22%5D&channel=beta&currentPage=1",
-            "Content-Type": "application/json",
-            "Origin": "https://glam.telemetry.mozilla.org",
-            "Connection": "keep-alive",
-            "Cookie": "???",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "Pragma": "no-cache",
-            "Cache-Control": "no-cache",
-            "TE": "trailers",
+
         },
     )
     probe_names = []
@@ -77,7 +65,8 @@ def get_most_used_probes():
 
 
 def probe_is_recent_legacy(probe_data):
-    # Temporarily return false so we can only evaluate the hardcoded list of recently used probes
+    """Check probe data and returns if probe was created in the last RECENT_PROBE_DAYS."""
+    # Temporarily return false in favor of hardcoded list of recently used probes
     return False
     """
     probe_created_on = datetime.strptime(
@@ -88,7 +77,7 @@ def probe_is_recent_legacy(probe_data):
 
 
 def probe_is_recent_glean(probe, product):
-    """Return whether probe was created in the last 90 days"""
+    """Return whether probe was created in the last 90 days."""
     product_map = {
         "firefox_desktop": "firefox_desktop",
         "org_mozilla_fenix": "fenix",
@@ -98,7 +87,8 @@ def probe_is_recent_glean(probe, product):
         raise Exception(f"Product not supported: {product}")
 
     product_path = product_map[product]
-    url = f"https://dictionary.telemetry.mozilla.org/data/{product_path}/metrics/data_{probe}.json"
+    domain = "https://dictionary.telemetry.mozilla.org"
+    url = f"{domain}/data/{product_path}/metrics/data_{probe}.json"
     url_req = Request(url)
 
     with urlopen(url_req) as url:

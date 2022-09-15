16c77a4755e9c8660a92a974de0de4edcd50f434
Wil Stuckey <wstuckey@mozilla.com>
2022-09-15 12:58:30 -0500

    Clean up dag imports

--- dags/bqetl_sponsored_tiles_clients_daily.py
+++ dags/bqetl_sponsored_tiles_clients_daily.py
@@ -1,12 +1,11 @@
 # Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+import datetime
 
 from airflow import DAG
-from airflow.sensors.external_task import ExternalTaskMarker
-from airflow.sensors.external_task import ExternalTaskSensor
-from airflow.utils.task_group import TaskGroup
-import datetime
+from airflow.sensors.external_task import ExternalTaskMarker, ExternalTaskSensor
+
 from utils.constants import ALLOWED_STATES, FAILED_STATES
-from utils.gcp import bigquery_etl_query, gke_command
+from utils.gcp import bigquery_etl_query
 
 docs = """
 ### bqetl_sponsored_tiles_clients_daily

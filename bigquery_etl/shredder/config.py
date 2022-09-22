7236d01dccb9ebccaffcf707d5663aad44eb4440
Frank Bertsch <frank.bertsch@gmail.com>
2022-09-22 14:39:50 -0400

    Fix failing CI tests

--- bigquery_etl/shredder/config.py
+++ bigquery_etl/shredder/config.py
@@ -516,14 +516,12 @@ def find_pioneer_targets(pool, client, project=PIONEER_PROD, study_projects=[]):
                 return RALLY_ID_TOP_LEVEL
             else:
                 return RALLY_ID
-        elif (
-            table.dataset_id == "analysis"
-        ):
+        elif table.dataset_id == "analysis":
             # Rally analysis tables do not have schemas specified upfront,
             # analysts might decide to use either nested or top-level rally_id.
             # Shared datasets, like attention stream, may also have derived
             # datasets with rally IDs
-            # See https://github.com/mozilla-services/cloudops-infra/blob/master/projects/data-pioneer/tf/prod/envs/prod/study-projects/main.tf#L60-L67
+            # See https://github.com/mozilla-services/cloudops-infra/blob/master/projects/data-pioneer/tf/prod/envs/prod/study-projects/main.tf#L60-L67 # noqa 
             if any(_has_nested_rally_id(field) for field in table.schema):
                 return RALLY_ID
             elif any(field.name == RALLY_ID_TOP_LEVEL for field in table.schema):
@@ -612,12 +610,11 @@ def find_pioneer_targets(pool, client, project=PIONEER_PROD, study_projects=[]):
             # derived tables with pioneer_id
             DeleteTarget(
                 table=qualified_table_id(table),
-                field=field,
+                field=_get_client_id_field(table),
                 project=PIONEER_PROD,
             ): sources[table.dataset_id]
             for dataset in derived_datasets
             for table in _get_tables_with_pioneer_id(dataset)
-            if field := _get_client_id_field(table) in 
         },
         **{
             # tables with pioneer_id located in study analysis projects

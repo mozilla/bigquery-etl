71090453c645789da57a5d600cd16435a210ceba
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-10-11 17:07:36 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/org_mozilla_firefox_derived/baseline_clients_first_seen_v1/query.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_firefox_derived/baseline_clients_first_seen_v1/query.sql
@@ -2,17 +2,15 @@
 WITH
 -- this lookup is ~13GB on release (org_mozilla_firefox) as of 2021-03-31
 _fennec_id_lookup AS (
-  SELECT
+  SELECT DISTINCT
     client_info.client_id,
-    MIN(metrics.uuid.migration_telemetry_identifiers_fennec_client_id) AS fennec_client_id
+    metrics.uuid.migration_telemetry_identifiers_fennec_client_id AS fennec_client_id
   FROM
     `org_mozilla_firefox_stable.migration_v1`
   WHERE
     DATE(submission_timestamp) > "2010-01-01"
     AND client_info.client_id IS NOT NULL
     AND metrics.uuid.migration_telemetry_identifiers_fennec_client_id IS NOT NULL
-  GROUP BY
-    1
 ),
 _core AS (
   SELECT
@@ -26,22 +24,15 @@ _core AS (
 _core_clients_first_seen AS (
   SELECT
     _fennec_id_lookup.client_id,
-    first_seen_date
+    MIN(first_seen_date) AS first_seen_date,
   FROM
     _fennec_id_lookup
   JOIN
     _core
   ON
     _fennec_id_lookup.fennec_client_id = _core.client_id
-  WHERE
-    TRUE
-  QUALIFY
-    IF(
-      COUNT(*) OVER (PARTITION BY _fennec_id_lookup.client_id) > 1
-      OR COUNT(*) OVER (PARTITION BY _core.client_id) > 1,
-      ERROR("duplicate client_id detected"),
-      TRUE
-    )
+  GROUP BY
+    _fennec_id_lookup.client_id
 ),
 _baseline AS (
   -- extract the client_id into the top level for the `USING` clause

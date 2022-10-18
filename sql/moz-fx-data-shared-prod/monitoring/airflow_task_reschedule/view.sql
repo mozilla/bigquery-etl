b41fba8a50f8fffa6f929036274291c486b2b59c
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-10-18 11:33:05 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/monitoring/airflow_task_reschedule/view.sql
+++ sql/moz-fx-data-shared-prod/monitoring/airflow_task_reschedule/view.sql
@@ -2,8 +2,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.monitoring.airflow_task_reschedule`
 AS
 SELECT
-  * EXCEPT (is_deleted)
+  *
 FROM
   `moz-fx-data-shared-prod.monitoring_derived.airflow_task_reschedule_v1`
-WHERE
-  NOT is_deleted

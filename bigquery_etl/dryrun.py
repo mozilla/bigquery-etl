ea25eefb8d969485cfd6d57f8c6d92aef92aa7cb
Sean Rose <srose@mozilla.com>
2022-10-18 01:08:42 -0700

    Don't dry-run new Stripe subscriptions history views.

--- bigquery_etl/dryrun.py
+++ bigquery_etl/dryrun.py
@@ -97,7 +97,9 @@ SKIP = {
     *glob.glob("sql/moz-fx-data-shared-prod/stripe_external/**/*.sql", recursive=True),
     *glob.glob("sql/moz-fx-cjms-*/**/*.sql", recursive=True),
     "sql/moz-fx-data-shared-prod/subscription_platform/stripe_subscriptions/view.sql",
+    "sql/moz-fx-data-shared-prod/subscription_platform/stripe_subscriptions_history/view.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/subscription_platform/nonprod_stripe_subscriptions/view.sql",  # noqa E501
+    "sql/moz-fx-data-shared-prod/subscription_platform/nonprod_stripe_subscriptions_history/view.sql",  # noqa E501
     "sql/moz-fx-data-shared-prod/stripe/itemized_payout_reconciliation/view.sql",
     "sql/moz-fx-data-shared-prod/mozilla_vpn_derived/active_subscriptions_v1/query.sql",
     "sql/moz-fx-data-shared-prod/mozilla_vpn_derived/active_subscription_ids_v1/query.sql",  # noqa E501

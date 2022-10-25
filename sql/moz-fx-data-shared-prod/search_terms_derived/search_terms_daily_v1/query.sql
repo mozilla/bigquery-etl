5e720044629dbfd27a29b813e8d35c4101bf0951
Rebecca BurWei <rburwei@mozilla.com>
2022-10-25 12:33:33 -0500

    feat: add threshold

--- sql/moz-fx-data-shared-prod/search_terms_derived/search_terms_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/search_terms_derived/search_terms_daily_v1/query.sql
@@ -1,22 +1,44 @@
 WITH terminal_queries AS (
   SELECT
-    array_reverse(ARRAY_AGG(sanitized ORDER BY sequence_no, timestamp ASC))[SAFE_OFFSET(0)].*
+    ARRAY_REVERSE(ARRAY_AGG(sanitized ORDER BY sequence_no, timestamp ASC))[SAFE_OFFSET(0)].*
   FROM
     `moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3` sanitized
   WHERE
-    date(timestamp) = @submission_date
+    DATE(timestamp) > @submission_date - 7
   GROUP BY
     sanitized.session_id
+),
+daily_queries AS (
+  SELECT
+    DATE(timestamp) AS submission_date,
+    query,
+    COUNT(*) AS search_sessions
+  FROM
+    terminal_queries
+  GROUP BY
+    1,
+    2
+),
+daily_queries_with_threshold AS (
+  SELECT
+    *,
+    SUM(search_sessions) OVER (
+      PARTITION BY
+        QUERY
+      ORDER BY
+        SUBMISSION_DATE ASC
+      ROWS BETWEEN
+        6 PRECEDING
+        AND CURRENT ROW
+    ) AS search_sessions_in_last_7_days
+  FROM
+    DAILY_QUERIES
 )
 SELECT
-  date(timestamp) AS submission_date,
-  query,
-  count(*) AS search_sessions
+  * EXCEPT (search_sessions_in_last_7_days)
 FROM
-  terminal_queries
-GROUP BY
-  1,
-  2
--- Level 2 aggregation. See: https://wiki.mozilla.org/Data_Publishing
-HAVING
-  count(*) > 5000
+  daily_queries_with_threshold
+WHERE
+  --Threshold decision (not a public doc): https://docs.google.com/document/d/1xPiQP-72B4MtgdIZRbyYJcwlKh5pA9yLZ8gxC6rrwEs/edit#heading=h.ddfi2mqlhlyw
+  search_sessions_in_last_7_days > 1
+  AND submission_date = @submission_date

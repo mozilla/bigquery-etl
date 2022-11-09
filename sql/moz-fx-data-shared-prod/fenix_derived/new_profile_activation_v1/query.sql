784c919ebdee02804f3dce830cd7415245d8881f
Vanessa Sabino <vsabino@mozilla.com>
2022-11-09 14:13:32 -0500

    Fenix and Firefox iOS new profile activation metric

---
+++ sql/moz-fx-data-shared-prod/fenix_derived/new_profile_activation_v1/query.sql
@@ -0,0 +1,123 @@
+WITH client_first_seen AS (
+  SELECT
+    client_id,
+    sample_id,
+    normalized_channel,
+    app_build,
+    app_channel,
+    app_display_version,
+    device_manufacturer,
+    device_model,
+    isp,
+    locale,
+    city,
+    country,
+    MIN(first_seen_date) AS first_seen_date
+  FROM
+    mozdata.fenix.baseline_clients_first_seen
+  WHERE
+    submission_date = DATE_SUB(@submission_date, INTERVAL 6 DAY)
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12
+),
+client_search AS (
+  SELECT
+    client_id,
+    SUM(search_count) AS search_count
+  FROM
+    `moz-fx-data-shared-prod.search_derived.mobile_search_clients_daily_v1`
+  JOIN
+    client_first_seen
+  USING
+    (client_id)
+  WHERE
+    submission_date
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 3 DAY)
+    AND @submission_date
+    AND normalized_app_name = 'Fenix'
+    AND DATE_DIFF(submission_date, first_seen_date, DAY)
+    BETWEEN 4
+    AND 7
+  GROUP BY
+    1
+),
+dou AS (
+  SELECT
+    client_id,
+    submission_date,
+    ARRAY_LENGTH(
+      mozfun.bits28.to_dates(mozfun.bits28.range(days_seen_bits, -5, 6), submission_date)
+    ) AS days_2_7,
+  FROM
+    `mozdata.fenix.baseline_clients_last_seen`
+  WHERE
+    submission_date = @submission_date
+    AND date_diff(submission_date, first_seen_date, DAY) = 6
+    AND normalized_channel = 'release'
+),
+adjust_client AS (
+  SELECT
+    client_info.client_id AS client_id,
+    ARRAY_AGG(metrics.string.first_session_network)[SAFE_OFFSET(0)] AS adjust_network,
+    ARRAY_AGG(metrics.string.first_session_adgroup)[SAFE_OFFSET(0)] AS adjust_adgroup,
+    ARRAY_AGG(metrics.string.first_session_campaign)[SAFE_OFFSET(0)] AS adjust_campaign,
+    ARRAY_AGG(metrics.string.first_session_creative)[SAFE_OFFSET(0)] AS adjust_creative,
+    MIN(DATE(submission_timestamp)) AS first_session_date
+  FROM
+    `mozdata.fenix.first_session`
+  WHERE
+    DATE(submission_timestamp)
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 6 DAY)
+    AND @submission_date
+    AND metrics.string.first_session_network IS NOT NULL
+    AND metrics.string.first_session_network <> ''
+  GROUP BY
+    1
+)
+SELECT
+  client_id,
+  sample_id,
+  normalized_channel,
+  app_build,
+  app_channel,
+  app_display_version,
+  device_manufacturer,
+  device_model,
+  isp,
+  locale,
+  city,
+  country,
+  locale,
+  COALESCE(adjust_network, 'Unknown') AS adjust_network,
+  COALESCE(adjust_adgroup, 'Unknown') AS adjust_adgroup,
+  COALESCE(adjust_campaign, 'Unknown') AS adjust_campaign,
+  COALESCE(adjust_creative, 'Unknown') AS adjust_creative,
+  first_seen_date,
+  submission_date,
+  1 AS new_profile,
+  CAST(days_2_7 > 1 AND coalesce(search_count, 0) > 0 AS integer) AS activated
+FROM
+  dou
+INNER JOIN
+  client_first_seen
+USING
+  (client_id)
+LEFT JOIN
+  client_search
+USING
+  (client_id)
+LEFT JOIN
+  adjust_client
+USING
+  (client_id)
---
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/new_profile_activation_v1/query.sql
@@ -0,0 +1,97 @@
+WITH client_first_seen AS (
+  SELECT
+    client_id,
+    sample_id,
+    normalized_channel,
+    app_build,
+    app_channel,
+    app_display_version,
+    device_manufacturer,
+    device_model,
+    isp,
+    locale,
+    city,
+    country,
+    MIN(first_seen_date) AS first_seen_date
+  FROM
+    `mozdata.firefox_ios.baseline_clients_first_seen`
+  WHERE
+    submission_date = DATE_SUB(@submission_date, INTERVAL 6 DAY)
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12
+),
+client_search AS (
+  SELECT
+    client_id,
+    SUM(search_count) AS search_count
+  FROM
+    `moz-fx-data-shared-prod.search_derived.mobile_search_clients_daily_v1`
+  JOIN
+    client_first_seen
+  USING
+    (client_id)
+  WHERE
+    submission_date
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 3 DAY)
+    AND @submission_date
+    AND normalized_app_name = 'Fennec'
+    AND os = 'iOS'
+    AND DATE_DIFF(submission_date, first_seen_date, DAY)
+    BETWEEN 4
+    AND 7
+  GROUP BY
+    1
+),
+dou AS (
+  SELECT
+    client_id,
+    submission_date,
+    ARRAY_LENGTH(
+      mozfun.bits28.to_dates(mozfun.bits28.range(days_seen_bits, -5, 6), submission_date)
+    ) AS days_2_7,
+  FROM
+    `mozdata.firefox_ios.baseline_clients_last_seen`
+  WHERE
+    submission_date = @submission_date
+    AND date_diff(submission_date, first_seen_date, DAY) = 6
+    AND normalized_channel = 'release'
+)
+SELECT
+  client_id,
+  sample_id,
+  normalized_channel,
+  app_build,
+  app_channel,
+  app_display_version,
+  device_manufacturer,
+  device_model,
+  isp,
+  locale,
+  city,
+  country,
+  locale,
+  first_seen_date,
+  submission_date,
+  1 AS new_profile,
+  CAST(days_2_7 > 1 AND coalesce(search_count, 0) > 0 AS integer) AS activated
+FROM
+  dou
+INNER JOIN
+  client_first_seen
+USING
+  (client_id)
+LEFT JOIN
+  client_search
+USING
+  (client_id)

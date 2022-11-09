8a3f146e9efb33be73cbc6d6d039c07880899128
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2022-11-09 18:37:44 +0100

    DENG-434 Add focus android data from Glean to unified metrics (#3316)
    * DENG-434. Update cohorts queries to use the unified_metrics view instead of the table.
    
    * DENG-434. Make columns  (is_default_browser, uri_count) available after updating the sql generators in https://github.com/mozilla/bigquery-etl/pull/3315.
    
    * DENG-434. Document the change to use app name convention with suffix ' Glean'
    
    * Update sql/moz-fx-data-shared-prod/telemetry/unified_metrics/metadata.yaml
    
    * Update sql/moz-fx-data-shared-prod/telemetry/unified_metrics/metadata.yaml
    
    Co-authored-by: Lucia Vargas <lvargas@mozilla.com>
    Co-authored-by: Alekhya <88394696+alekhyamoz@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
@@ -6,3 +6,5 @@ SELECT
   `mozfun.norm.browser_version_info`(app_version) AS browser_version_info
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.unified_metrics_v1`
+WHERE
+  normalized_app_name != 'Focus Android Glean'

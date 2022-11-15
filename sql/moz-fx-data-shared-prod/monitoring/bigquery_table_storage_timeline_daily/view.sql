8e71c4ab7561bfe184d7992422f06da5d8262d00
Winnie Chan <10429026+wwyc@users.noreply.github.com>
2022-11-15 09:14:33 -0800

    Revert "Created dervied table for big query usage and tables reporting (#3350)"
    This reverts commit 373069835524725b8764fe6fd09f5d8ff99ae779.

--- sql/moz-fx-data-shared-prod/monitoring/bigquery_table_storage_timeline_daily/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.monitoring.bigquery_table_storage_timeline_daily`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.monitoring_derived.bigquery_table_storage_timeline_daily_v1`

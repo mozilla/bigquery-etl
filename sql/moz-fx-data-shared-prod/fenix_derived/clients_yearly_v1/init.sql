02eee68fe9669ab1006a98f4f19570efedbf12d9
Frank Bertsch <frank.bertsch@gmail.com>
2022-11-30 14:29:12 -0500

    Create clients_yearly table for Fenix clients
    This table contains a year of history for Fenix.
    It utilizes similar logic to search.search_clients_last_seen,
    but the naming specifies the year-long byte field.
    
    Currently only looks at Fenix data, which migrated
    ~2020-09-01; so full data is only available beginning 2021-09-01.

---
+++ sql/moz-fx-data-shared-prod/fenix_derived/clients_yearly_v1/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS
+  fenix_derived.clients_yearly_v1
+PARTITION BY
+  submission_date
+CLUSTER BY
+  sample_id,
+  client_id
+OPTIONS
+  (require_partition_filter = TRUE)
+AS
+SELECT
+  CAST(NULL AS BYTES) AS days_seen_bytes,
+  *,
+FROM
+  fenix.baseline_clients_daily
+WHERE
+  -- Output empty table and read no input rows
+  FALSE

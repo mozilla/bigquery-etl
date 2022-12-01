931573b4eac5f57b3a1c80a6ceba5e0549cca501
Vanessa Sabino <vsabino@mozilla.com>
2022-11-30 19:08:17 -0500

    Activation metric for mobile new profiles (#3347)
    * Fenix and Firefox iOS new profile activation metric
    
    * Update DAGs
    
    * parenthesis for readability of between statements
    
    Co-authored-by: Curtis Morales <cmorales@mozilla.com>
    
    * remove redundant filters for incremental build
    
    Co-authored-by: Curtis Morales <cmorales@mozilla.com>

---
+++ sql/moz-fx-data-shared-prod/firefox_ios/new_profile_activation/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_ios.new_profile_activation`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_ios_derived.new_profile_activation_v1`

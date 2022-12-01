76755b40d520da6d99bd4f9feaa1469fc677a6a5
Frank Bertsch <frank.bertsch@gmail.com>
2022-12-01 11:00:07 -0500

    Remove duplicates from baseline_clients_daily

--- sql/moz-fx-data-shared-prod/fenix_derived/clients_yearly_v1/query.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/clients_yearly_v1/query.sql
@@ -1,14 +1,32 @@
-WITH _current AS (
+WITH base AS (
+  -- There are duplicates now in `baseline_clients_daily` because of the join with `clients_first_seen`,
+  -- so we take the minimum
+  SELECT
+    *,
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        client_id,
+        submission_date
+      ORDER BY
+        first_seen_date ASC,
+        first_run_date ASC
+    ) AS rn,
+  FROM
+    fenix.baseline_clients_daily
+  WHERE
+    submission_date = @submission_date
+),
+_current AS (
   SELECT
     -- In this raw table, we capture the history of activity over the past
     -- 365 days for each usage criterion as an array of bytes. The
     -- rightmost bit represents whether the user was active in the current day.
     udf.bool_to_365_bits(TRUE) AS days_seen_bytes,
-    * EXCEPT (submission_date),
+    * EXCEPT (submission_date, rn),
   FROM
-    fenix.baseline_clients_daily
+    base
   WHERE
-    submission_date = @submission_date
+    rn = 1
 ),
 _previous AS (
   SELECT

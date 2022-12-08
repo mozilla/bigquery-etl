a7fcec70e1c74ae35576f94118975d56b45e777d
kik-kik <kignasiak01@gmail.com>
2022-12-08 16:03:25 +0100

    funnel_events_source_v1 updated to match the new fxa_all_events view

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/funnel_events_source_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/funnel_events_source_v1/query.sql
@@ -1,5 +1,37 @@
+WITH fxa_events AS (
+  SELECT
+    * EXCEPT (user_properties, event_properties, service),
+    JSON_VALUE(user_properties, "$.utm_term") AS utm_term,
+    JSON_VALUE(user_properties, "$.utm_source") AS utm_source,
+    JSON_VALUE(user_properties, "$.utm_medium") AS utm_medium,
+    JSON_VALUE(user_properties, "$.utm_campaign") AS utm_campaign,
+    JSON_VALUE(user_properties, "$.utm_content") AS utm_content,
+    JSON_VALUE(user_properties, "$.ua_version") AS ua_version,
+    JSON_VALUE(user_properties, "$.ua_browser") AS ua_browser,
+    JSON_VALUE(user_properties, "$.entrypoint") AS entrypoint,
+    JSON_VALUE(user_properties, "$.flow_id") AS flow_id,
+    JSON_VALUE(event_properties, "$.service") AS service,
+    JSON_VALUE(event_properties, "$.email_type") AS email_type,
+    JSON_VALUE(event_properties, "$.email_provider") AS email_provider,
+    JSON_VALUE(event_properties, "$.oauth_client_id") AS oauth_client_id,
+    JSON_VALUE(event_properties, "$.connect_device_flow") AS connect_device_flow,
+    JSON_VALUE(event_properties, "$.connect_device_os") AS connect_device_os,
+    JSON_VALUE(user_properties, "$.sync_device_count") AS sync_device_count,
+    JSON_VALUE(user_properties, "$.sync_active_devices_day") AS sync_active_devices_day,
+    JSON_VALUE(user_properties, "$.sync_active_devices_week") AS sync_active_devices_week,
+    JSON_VALUE(user_properties, "$.sync_active_devices_month") AS sync_active_devices_month,
+    JSON_VALUE(event_properties, "$.email_sender") AS email_sender,
+    JSON_VALUE(event_properties, "$.email_service") AS email_service,
+    JSON_VALUE(event_properties, "$.email_template") AS email_template,
+    JSON_VALUE(event_properties, "$.email_version") AS email_version,
+  FROM
+    `moz-fx-data-shared-prod.firefox_accounts.fxa_all_events`
+  WHERE
+    DATE(`timestamp`) = @submission_date
+    AND event_category IN ('fxa_content_event', 'fxa_auth_event', 'fxa_oauth_event')
+)
 SELECT
-  DATE(timestamp) AS submission_date,
+  DATE(`timestamp`) AS submission_date,
   user_id AS client_id,
   `moz-fx-data-shared-prod`.udf.safe_sample_id(user_id) AS sample_id,
   SPLIT(event_type, ' - ')[OFFSET(0)] AS category,
@@ -19,7 +51,4 @@ SELECT
   CAST([] AS ARRAY<STRUCT<key STRING, value STRING>>) AS experiments,
   *,
 FROM
-  `moz-fx-data-shared-prod.firefox_accounts.fxa_all_events`
-WHERE
-  DATE(`timestamp`) = @submission_date
-  AND event_category IN ('fxa_content_event', 'fxa_auth_event', 'fxa_oauth_event')
+  fxa_events

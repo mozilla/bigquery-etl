d65521bb5a02572832776b9bc2b73d945a9de11e
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-12-09 00:41:30 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry/test_query_ref/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.test_query_ref`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry.test_query`
--- sql/moz-fx-data-shared-prod/telemetry_derived/test_query_v1/query.sql
+++
@@ -1,3 +0,0 @@
-SELECT
-  DATE(@submission_date) AS submission_date,
-  1 AS num

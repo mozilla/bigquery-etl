de4ed44fac112cc4458f6cd6150f3ee944ec1707
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2022-12-16 14:27:46 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_desktop_background_update/background_update/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_background_update/background_update/view.sql
@@ -5,7 +5,22 @@ AS
 SELECT
   * REPLACE (
     mozfun.norm.metadata(metadata) AS metadata,
-    mozfun.norm.glean_ping_info(ping_info) AS ping_info
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (
+      SELECT AS STRUCT
+        metrics.* REPLACE (
+          STRUCT(
+            mozfun.glean.parse_datetime(
+              metrics.datetime.background_update_targeting_env_current_date
+            ) AS background_update_targeting_env_current_date,
+            metrics.datetime.background_update_targeting_env_current_date AS raw_background_update_targeting_env_current_date,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.background_update_targeting_env_profile_age
+            ) AS background_update_targeting_env_profile_age,
+            metrics.datetime.background_update_targeting_env_profile_age AS raw_background_update_targeting_env_profile_age
+          ) AS datetime
+        )
+    ) AS metrics
   )
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_background_update_stable.background_update_v1`

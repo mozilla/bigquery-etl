8fed000d92e69349a08d0a6a993cf342635a9006
Chelsea Troy <chelsea.dommert@gmail.com>
2022-12-16 13:40:00 -0600

    Consistentify another thing in the dag

--- dags/bqetl_campaign_cost_breakdowns.py
+++ dags/bqetl_campaign_cost_breakdowns.py
@@ -50,8 +50,8 @@ with DAG(
     doc_md=docs,
     tags=tags,
 ) as dag:
-    google_ads__campaign_cost_breakdowns__v1 = bigquery_etl_query(
-        task_id="google_ads__campaign_cost_breakdowns__v1",
+    fenix_derived__google_ads_campaign_cost_breakdowns__v1 = bigquery_etl_query(
+        task_id="fenix_derived__google_ads_campaign_cost_breakdowns__v1",
         destination_table="google_ads_campaign_cost_breakdowns_v1",
         dataset_id="fenix_derived",
         project_id="moz-fx-data-shared-prod",

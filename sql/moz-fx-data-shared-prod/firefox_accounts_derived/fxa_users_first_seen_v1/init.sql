856f10514572aa5c5e7699e12bd6aa89eea97fa7
kik-kik <kignasiak01@gmail.com>
2022-12-16 14:46:02 +0100

    Replaced init.sql for fxa_users_first_seen_v1 with schema.yaml

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_first_seen_v1/init.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_first_seen_v1/init.sql
@@ -2,13 +2,10 @@ CREATE OR REPLACE TABLE
   `firefox_accounts_derived.fxa_users_first_seen_v1`
 PARTITION BY
   first_seen_date
-CLUSTER BY
-  user_id
 AS
 SELECT
   user_id,
   DATE(MIN(`timestamp`)) AS first_seen_date,
-  ARRAY_AGG(DISTINCT service IGNORE NULLS ORDER BY service) AS services_used,
 FROM
   firefox_accounts.fxa_all_events
 WHERE

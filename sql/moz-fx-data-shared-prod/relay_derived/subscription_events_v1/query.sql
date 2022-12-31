c6b1de73e019c0335cec2657d05849cb8b0bd2b0
Yeonjoo Yoo <ysmith@mozilla.com>
2022-12-30 21:38:28 -0500

    matching date_partition_offset setting with the table
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/relay_derived/subscription_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/relay_derived/subscription_events_v1/query.sql
@@ -3,4 +3,4 @@ SELECT
 FROM
   `moz-fx-data-shared-prod`.relay_derived.subscription_events_live
 WHERE
-  IF(CAST(@date AS DATE) IS NULL, event_date < CURRENT_DATE - 7, event_date = @date)
+  IF(CAST(@date AS DATE) IS NULL, event_date < CURRENT_DATE - 8, event_date = @date)

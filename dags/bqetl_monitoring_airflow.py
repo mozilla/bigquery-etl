5c7fb1b3d8b1d23088f9d66ec9cd31a7b571699a
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-01-04 00:04:38 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_monitoring_airflow.py
+++ dags/bqetl_monitoring_airflow.py
@@ -8,8 +8,8 @@ import datetime
 from utils.constants import ALLOWED_STATES, FAILED_STATES
 from utils.gcp import bigquery_etl_query, gke_command
 
-from operators.backport.fivetran.operator import FivetranOperator
-from operators.backport.fivetran.sensor import FivetranSensor
+from fivetran_provider.operators.fivetran import FivetranOperator
+from fivetran_provider.sensors.fivetran import FivetranSensor
 
 docs = """
 ### bqetl_monitoring_airflow
@@ -142,6 +142,7 @@ with DAG(
         connector_id="{{ var.value.fivetran_airflow_metadata_import_connector_id }}",
         task_id="fivetran_airflow_metadata_import_sensor",
         poke_interval=5,
+        xcom="{{ task_instance.xcom_pull('fivetran_airflow_metadata_import_task') }}",
     )
 
     fivetran_airflow_metadata_import_sync_wait.set_upstream(

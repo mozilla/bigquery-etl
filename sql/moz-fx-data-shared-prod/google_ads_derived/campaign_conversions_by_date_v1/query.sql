94faa1f7bcc23eb33da0fc860de2ecbfeea2c6f4
Frank Bertsch <frank.bertsch@gmail.com>
2023-01-05 09:33:57 -0500

    Update query for dupes; dont dryrun

--- sql/moz-fx-data-shared-prod/google_ads_derived/campaign_conversions_by_date_v1/query.sql
+++ sql/moz-fx-data-shared-prod/google_ads_derived/campaign_conversions_by_date_v1/query.sql
@@ -1,5 +1,7 @@
 WITH campaigns AS (
-  SELECT DISTINCT
+  SELECT
+    CAST(start_date AS DATE) AS start_date,
+    CAST(end_date AS DATE) AS end_date,
     id AS campaign_id,
     name,
   FROM
@@ -8,15 +10,18 @@ WITH campaigns AS (
 SELECT
   date,
   campaigns.name,
-  campaign_id,
+  campaigns.campaign_id,
   SUM(biddable_app_install_conversions) AS installs,
   SUM(biddable_app_post_install_conversions) AS conversions,
 FROM
   `moz-fx-data-bq-fivetran`.google_ads.campaign_conversions_by_date
 JOIN
   campaigns
-USING
-  (campaign_id)
+ON
+  campaign_conversions_by_date.campaign_id = campaigns.campaign_id
+  AND campaign_conversions_by_date.date
+  BETWEEN campaigns.start_date
+  AND campaigns.end_date
 GROUP BY
   date,
   campaigns.name,

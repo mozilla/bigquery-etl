f53e073d5a2e87e5125e5fcbc58371e8a366f402
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-01-09 21:25:48 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/treeherder/clients_last_seen_joined/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.treeherder.clients_last_seen_joined`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.treeherder_derived.clients_last_seen_joined_v1`
--- sql/moz-fx-data-shared-prod/treeherder/metrics_clients_daily/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.treeherder.metrics_clients_daily`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.treeherder_derived.metrics_clients_daily_v1`

e53e114f06c3c4806846eb8d7dc0da48e2db7a64
kik-kik <kignasiak@mozilla.com>
2023-01-16 11:56:09 +0100

    reverted fxa_content_events query to use sharded table due to the partitioned version of the table missing some events

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_content_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_content_events_v1/query.sql
@@ -13,8 +13,16 @@ SELECT
     ) AS jsonPayload
   )
 FROM
-  `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_content`
+  `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_content_20*`
 WHERE
   jsonPayload.type = 'amplitudeEvent'
   AND jsonPayload.fields.event_type IS NOT NULL
-  AND DATE(`timestamp`) = @submission_date
+  -- FXA-6593: the partitioned version of the table
+  -- seems to be missing some data.
+  -- For now reverting this query to the sharded version
+  -- Once the issue has been resolves:
+  -- 1. uncomment the DATE(...) = @submission_date line
+  -- 2. Remove the _TABLE_SUFFIX line below
+  -- 3. Change source table to be `docker_fxa_content`
+  -- AND DATE(`timestamp`) = @submission_date
+  AND _TABLE_SUFFIX = FORMAT_DATE('%y%m%d', @submission_date)

cd52cc4b4ed13f1e58e4682ae10d3d21c255ae02
wwyc <wichan@mozilla.com>
2023-01-23 13:06:47 -0800

    Added normalized os columns

--- sql/moz-fx-data-shared-prod/telemetry/active_users_aggregates_device/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/active_users_aggregates_device/view.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.active_users_aggregates_device`
 AS
 SELECT
-  *
+  *,
+  `mozfun.norm.os`(os) AS normalized_os
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.active_users_aggregates_device_v1`

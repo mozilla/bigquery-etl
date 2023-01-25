d6ba192d2a4231d969133bf62adeb90fd768bba7
Lucia Vargas <lvargas@mozilla.com>
2023-01-25 21:09:49 +0100

    Implement QDAU in the active_users_aggregates tables.

--- sql/moz-fx-data-shared-prod/telemetry_derived/active_users_aggregates_attribution_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/active_users_aggregates_attribution_v1/query.sql
@@ -1,32 +1,7 @@
 -- Query for telemetry_derived.active_users_aggregates_attribution_v1
-CREATE TEMP FUNCTION normalize_adjust_network(key STRING) AS (
-  (
-    CASE
-    WHEN
-      key NOT IN (
-        '',
-        'Organic',
-        'Google Organic Search',
-        'Untrusted Devices',
-        'Product Marketing (Owned media)',
-        'Google Ads ACI'
-      )
-      AND key IS NOT NULL
-    THEN
-      'Other'
-    ELSE
-      key
-    END
-  )
-);
-
-CREATE TEMP FUNCTION normalize_install_source(key STRING) AS (
-  (CASE WHEN key NOT IN ('com.android.vending') AND key IS NOT NULL THEN 'Other' ELSE key END)
-);
-
 WITH baseline AS (
   SELECT
-    client_id,
+    unified_metrics.client_id,
     attribution_medium IS NOT NULL
     OR attribution_source IS NOT NULL AS attributed,
     attribution_campaign,
@@ -35,13 +10,15 @@ WITH baseline AS (
     attribution_medium,
     attribution_source,
     attribution_variation,
+    att.adjust_network,
+    att.install_source,
     city,
     country,
-    first_seen_date,
-    EXTRACT(YEAR FROM first_seen_date) AS first_seen_year,
+    unified_metrics.first_seen_date,
+    EXTRACT(YEAR FROM unified_metrics.first_seen_date) AS first_seen_year,
     is_default_browser,
     normalized_app_name,
-    submission_date,
+    unified_metrics.submission_date,
     days_since_seen,
     ad_click,
     organic_search_count,
@@ -50,130 +27,13 @@ WITH baseline AS (
     uri_count,
     active_hours_sum
   FROM
-    `moz-fx-data-shared-prod.telemetry.unified_metrics`
-  WHERE
-    submission_date = @submission_date
-),
-fenix_first_date_attributions AS (
-  -- Firefox Preview beta
-  SELECT
-    client_info.client_id AS client_id,
-    MIN(submission_timestamp) AS min_submission_timestamp,
-    ARRAY_AGG(metrics.string.metrics_adjust_network)[SAFE_OFFSET(0)] AS adjust_network,
-    ARRAY_AGG(metrics.string.metrics_install_source)[SAFE_OFFSET(0)] AS install_source
-  FROM
-    org_mozilla_fenix.metrics AS org_mozilla_fenix_metrics
-  WHERE
-    DATE(submission_timestamp) >= '2021-01-01'
-    AND metrics.string.metrics_install_source IS NOT NULL
-    AND metrics.string.metrics_adjust_network IS NOT NULL
-  GROUP BY
-    client_id
-  UNION ALL
-  -- Firefox Preview nightly
-  SELECT
-    client_info.client_id AS client_id,
-    MIN(submission_timestamp) AS min_submission_timestamp,
-    ARRAY_AGG(metrics.string.metrics_adjust_network)[SAFE_OFFSET(0)] AS adjust_network,
-    ARRAY_AGG(metrics.string.metrics_install_source)[SAFE_OFFSET(0)] AS install_source
-  FROM
-    org_mozilla_fenix.metrics AS org_mozilla_fenix_metrics
-  WHERE
-    DATE(submission_timestamp) >= '2021-01-01'
-    AND metrics.string.metrics_install_source IS NOT NULL
-    AND metrics.string.metrics_adjust_network IS NOT NULL
-  GROUP BY
-    client_id
-  UNION ALL
-  -- Fenix nightly
-  SELECT
-    client_info.client_id AS client_id,
-    MIN(submission_timestamp) AS min_submission_timestamp,
-    ARRAY_AGG(metrics.string.metrics_adjust_network)[SAFE_OFFSET(0)] AS adjust_network,
-    ARRAY_AGG(metrics.string.metrics_install_source)[SAFE_OFFSET(0)] AS install_source
-  FROM
-    org_mozilla_fennec_aurora.metrics AS org_mozilla_fennec_aurora_metrics
-  WHERE
-    DATE(submission_timestamp) >= '2021-01-01'
-    AND metrics.string.metrics_install_source IS NOT NULL
-    AND metrics.string.metrics_adjust_network IS NOT NULL
-  GROUP BY
-    client_id
-  UNION ALL
-  -- Fenix beta
-  SELECT
-    client_info.client_id AS client_id,
-    MIN(submission_timestamp) AS min_submission_timestamp,
-    ARRAY_AGG(metrics.string.metrics_adjust_network)[SAFE_OFFSET(0)] AS adjust_network,
-    ARRAY_AGG(metrics.string.metrics_install_source)[SAFE_OFFSET(0)] AS install_source
-  FROM
-    org_mozilla_firefox_beta.metrics AS org_mozilla_firefox_beta_metrics
-  WHERE
-    DATE(submission_timestamp) >= '2021-01-01'
-    AND metrics.string.metrics_install_source IS NOT NULL
-    AND metrics.string.metrics_adjust_network IS NOT NULL
-  GROUP BY
-    client_id
-  UNION ALL
-  -- Fenix release
-  SELECT
-    client_info.client_id AS client_id,
-    MIN(submission_timestamp) AS min_submission_timestamp,
-    ARRAY_AGG(metrics.string.metrics_adjust_network)[SAFE_OFFSET(0)] AS adjust_network,
-    ARRAY_AGG(metrics.string.metrics_install_source)[SAFE_OFFSET(0)] AS install_source
-  FROM
-    org_mozilla_firefox.metrics AS org_mozilla_firefox_metrics
-  WHERE
-    DATE(submission_timestamp) >= '2021-01-01'
-    AND metrics.string.metrics_install_source IS NOT NULL
-    AND metrics.string.metrics_adjust_network IS NOT NULL
-  GROUP BY
-    client_id
-),
-fenix_unique_attribution AS (
-  SELECT
-    client_id,
-    DATE(min_submission_timestamp) AS attribution_date,
-    ANY_VALUE(adjust_network) AS adjust_network,
-    ANY_VALUE(install_source) AS install_source
-  FROM
-    fenix_first_date_attributions
-  GROUP BY
-    client_id,
-    attribution_date
-),
-all_with_first_attribution AS (
-  SELECT
-    base.client_id,
-    base.attributed,
-    base.attribution_campaign,
-    base.attribution_content,
-    base.attribution_experiment,
-    base.attribution_medium,
-    base.attribution_source,
-    base.attribution_variation,
-    normalize_adjust_network(att.adjust_network) AS adjust_network,
-    normalize_install_source(att.install_source) AS install_source,
-    base.city,
-    base.country,
-    base.first_seen_date,
-    base.first_seen_year,
-    base.is_default_browser,
-    base.normalized_app_name,
-    base.submission_date,
-    base.days_since_seen,
-    base.ad_click,
-    base.organic_search_count,
-    base.search_count,
-    base.search_with_ads,
-    base.uri_count,
-    base.active_hours_sum
-  FROM
-    baseline AS base
+    `moz-fx-data-shared-prod.telemetry.unified_metrics` AS unified_metrics
   LEFT JOIN
-    fenix_unique_attribution AS att
+    fenix.firefox_android_clients AS att
   ON
-    att.client_id = base.client_id
+    unified_metrics.client_id = att.client_id
+  WHERE
+    unified_metrics.submission_date = @submission_date
 )
 SELECT
   attributed,
@@ -194,6 +54,7 @@ SELECT
   COUNT(DISTINCT IF(days_since_seen = 0, client_id, NULL)) AS dau,
   COUNT(DISTINCT IF(days_since_seen < 7, client_id, NULL)) AS wau,
   COUNT(DISTINCT client_id) AS mau,
+  COUNT(DISTINCT IF(days_since_seen = 0 AND normalized_app_name = 'Firefox Desktop' AND active_hours_sum > 0 AND uri_count > 0, client_id, NULL)) AS qdau,
   COUNT(DISTINCT IF(submission_date = first_seen_date, client_id, NULL)) AS new_profiles,
   SUM(ad_click) AS ad_clicks,
   SUM(organic_search_count) AS organic_search_count,
@@ -202,7 +63,7 @@ SELECT
   SUM(uri_count) AS uri_count,
   SUM(active_hours_sum) AS active_hours
 FROM
-  all_with_first_attribution
+  baseline
 GROUP BY
   attributed,
   attribution_campaign,

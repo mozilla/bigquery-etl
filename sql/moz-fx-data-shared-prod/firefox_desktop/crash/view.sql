0bb24b07ff310b0277a5e491ba99df282d10140f
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-01-25 16:27:34 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_desktop/crash/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/crash/view.sql
@@ -6,6 +6,15 @@ SELECT
   * REPLACE (
     mozfun.norm.metadata(metadata) AS metadata,
     mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (
+      SELECT AS STRUCT
+        metrics.* REPLACE (
+          STRUCT(
+            mozfun.glean.parse_datetime(metrics.datetime.crash_time) AS crash_time,
+            metrics.datetime.crash_time AS raw_crash_time
+          ) AS datetime
+        )
+    ) AS metrics,
     'Firefox' AS normalized_app_name
   )
 FROM
--- sql/moz-fx-data-shared-prod/org_mozilla_fennec_aurora/crash/view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_fennec_aurora/crash/view.sql
@@ -5,7 +5,16 @@ AS
 SELECT
   * REPLACE (
     mozfun.norm.metadata(metadata) AS metadata,
-    mozfun.norm.glean_ping_info(ping_info) AS ping_info
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (
+      SELECT AS STRUCT
+        metrics.* REPLACE (
+          STRUCT(
+            mozfun.glean.parse_datetime(metrics.datetime.crash_time) AS crash_time,
+            metrics.datetime.crash_time AS raw_crash_time
+          ) AS datetime
+        )
+    ) AS metrics
   )
 FROM
   `moz-fx-data-shared-prod.org_mozilla_fennec_aurora_stable.crash_v1`

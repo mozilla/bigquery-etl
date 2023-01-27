3e5a1c5f39561926906ba26533e6a59825d223a9
Curtis Morales <cmorales@mozilla.com>
2023-01-27 10:10:04 -0500

    Format

--- sql/mozfun/map/extract_keyed_scalar_sum/udf.sql
+++ sql/mozfun/map/extract_keyed_scalar_sum/udf.sql
@@ -7,22 +7,18 @@ RETURNS INT64 AS (
 
 SELECT
   assert.equals(
-    3, map.extract_keyed_scalar_sum(
-      [STRUCT("a" AS key, 1 AS value), STRUCT("b" AS key, 2 AS value)]
-    )
+    3,
+    map.extract_keyed_scalar_sum([STRUCT("a" AS key, 1 AS value), STRUCT("b" AS key, 2 AS value)])
   ),
   -- Some NULL values and some non-NULL values -> ignore NULLs
   assert.equals(
-    1, map.extract_keyed_scalar_sum(
+    1,
+    map.extract_keyed_scalar_sum(
       [STRUCT("a" AS key, 1 AS value), STRUCT("b" AS key, NULL AS value)]
     )
   ),
   -- All values NULL -> return NULL
-  assert.null(
-    map.extract_keyed_scalar_sum(
-      [STRUCT("a" AS key, NULL AS value)]
-    )
-  ),
+  assert.null(map.extract_keyed_scalar_sum([STRUCT("a" AS key, NULL AS value)])),
   -- Empty array -> return NULL
   assert.null(map.extract_keyed_scalar_sum([])),
   -- NULL input -> return NULL

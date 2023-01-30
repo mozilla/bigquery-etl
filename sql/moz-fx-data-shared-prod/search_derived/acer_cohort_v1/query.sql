5dfee3884b4dba04a99dd29cf6dd64ba09998e50
Curtis Morales <cmorales@mozilla.com>
2023-01-30 11:58:46 -0500

    Add acer_cohort table to moz-fx-data-shared-prod

---
+++ sql/moz-fx-data-shared-prod/search_derived/acer_cohort_v1/query.sql
@@ -0,0 +1,10 @@
+-- Query for search_derived.acer_cohort_v1
+-- Pulls all client_ids that were using an Acer distribution in late 2022
+-- Query saved for posterity; will not be re-run
+SELECT DISTINCT
+  client_id
+FROM
+  `moz-fx-data-shared-prod.telemetry.clients_daily`
+WHERE
+  submission_date >= '2022-12-15'
+  AND distribution_id IN ('acer', 'acer-001', 'acer-002', 'acer-g-003')

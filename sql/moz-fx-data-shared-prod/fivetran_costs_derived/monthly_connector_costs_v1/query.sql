f471a38640437e3a0fed074ef5cb5cc2ddd03136
Leli Schiestl <lschiestl@mozilla.com>
2023-01-31 14:25:06 +0100

    implementing suggestions

--- sql/moz-fx-data-shared-prod/fivetran_costs_derived/monthly_connector_costs_v1/query.sql
+++ sql/moz-fx-data-shared-prod/fivetran_costs_derived/monthly_connector_costs_v1/query.sql
@@ -10,10 +10,10 @@ WITH connector_mar AS (
     `moz-fx-data-shared-prod.fivetran_costs_derived.incremental_mar_v1`
   GROUP BY
     destination_id,
-    connector_name,
-    measured_month
+    measured_month,
+    connector_name
 ),
-total_mar AS (
+destinations_total_mar AS (
   SELECT
     destination_id,
     measured_month,
@@ -25,44 +25,66 @@ total_mar AS (
     destination_id,
     measured_month
 ),
-monthly_connector_costs AS (
+total_mar AS (
   SELECT
-    destinations.destination_name,
+    measured_month,
+    SUM(paid_active_rows) AS paid_active_rows,
+  FROM
+    destinations_total_mar
+  GROUP BY
+    measured_month
+),
+monthly_connector_ratios AS (
+  SELECT
+    destination_id,
     measured_month,
     connector_mar.connector_name,
     connector_mar.paid_active_rows,
     connector_mar.free_active_rows,
     connector_mar.total_active_rows,
     ROUND(
+      connector_mar.paid_active_rows / NULLIF(destinations_total_mar.paid_active_rows, 0) * 100,
+      2
+    ) AS percentage_of_destination_paid_active_rows,
+    ROUND(
       connector_mar.paid_active_rows / NULLIF(total_mar.paid_active_rows, 0) * 100,
       2
-    ) AS percentage_of_total_paid_mar,
+    ) AS percentage_of_total_active_rows
+  FROM
+    connector_mar
+  LEFT JOIN
+    destinations_total_mar
+  USING
+    (destination_id, measured_month)
+  LEFT JOIN
+    total_mar
+  USING
+    (measured_month)
+),
+monthly_connector_costs AS (
+  SELECT
+    destinations.destination_name,
+    measured_month,
+    monthly_connector_ratios.connector_name,
+    monthly_connector_ratios.paid_active_rows,
+    monthly_connector_ratios.free_active_rows,
+    monthly_connector_ratios.total_active_rows,
+    monthly_connector_ratios.percentage_of_destination_paid_active_rows,
+    monthly_connector_ratios.percentage_of_total_active_rows,
     ROUND(
-      connector_mar.paid_active_rows / NULLIF(
-        total_mar.paid_active_rows,
-        0
-      ) * monthly_costs.dollars_spent,
+      monthly_connector_ratios.percentage_of_destination_paid_active_rows * monthly_costs.dollars_spent,
       2
     ) AS cost_in_usd,
     ROUND(
-      connector_mar.paid_active_rows / NULLIF(
-        total_mar.paid_active_rows,
-        0
-      ) * monthly_costs.credits_spent,
+      monthly_connector_ratios.percentage_of_destination_paid_active_rows * monthly_costs.credits_spent,
       2
-    ) AS cost_in_credit
-    -- todo translate cost_in_credit to usd
-    -- todo get change in usage compared to the previous month for each connector
+    ) AS cost_in_credits
   FROM
-    connector_mar
+    monthly_connector_ratios
   LEFT JOIN
     `moz-fx-data-shared-prod.fivetran_costs_derived.destinations_v1` AS destinations
   USING
     (destination_id)
-  LEFT JOIN
-    total_mar
-  USING
-    (destination_id, measured_month)
   LEFT JOIN
     `moz-fx-data-shared-prod.fivetran_costs_derived.monthly_costs_v1` AS monthly_costs
   USING

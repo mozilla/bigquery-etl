dfd72d5342f3a3ae2e4eb74244bc20b130265524
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-02-13 23:41:14 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/mozilla_vpn/events/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/events/view.sql
@@ -1,15 +1,41 @@
--- Override the default glean_usage generated view to union data from all VPN clients.
+-- Generated via ./bqetl generate glean_usage
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.mozilla_vpn.events`
 AS
--- Data from VPN clients using Glean.js
 SELECT
-  *
+  "mozillavpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
 FROM
   `moz-fx-data-shared-prod.mozillavpn.events`
 UNION ALL
--- Data from VPN Android clients using Glean Kotlin SDK
 SELECT
-  *
+  "org_mozilla_firefox_vpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
 FROM
   `moz-fx-data-shared-prod.org_mozilla_firefox_vpn.events`
--- sql/moz-fx-data-shared-prod/mozilla_vpn/main/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/main/view.sql
@@ -1,69 +1,89 @@
--- Override the default glean_usage generated view to union data from all VPN clients.
+-- Generated via ./bqetl generate glean_usage
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.mozilla_vpn.main`
 AS
--- Data from VPN clients using Glean.js
 SELECT
-  *
+  "mozillavpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
 FROM
   `moz-fx-data-shared-prod.mozillavpn.main`
 UNION ALL
--- Data from VPN Android clients using Glean Kotlin SDK
 SELECT
-  * REPLACE (
+  "org_mozilla_firefox_vpn" AS normalized_app_id,
+  additional_properties,
+  STRUCT(
+    client_info.android_sdk_version,
+    client_info.app_build,
+    client_info.app_channel,
+    client_info.app_display_version,
+    client_info.architecture,
+    client_info.client_id,
+    client_info.device_manufacturer,
+    client_info.device_model,
+    client_info.first_run_date,
+    client_info.locale,
+    client_info.os,
+    client_info.os_version,
+    client_info.telemetry_sdk_build,
+    client_info.build_date,
+    client_info.windows_build_number
+  ) AS client_info,
+  document_id,
+  events,
+  STRUCT(
+    metadata.geo,
     STRUCT(
-      client_info.android_sdk_version AS android_sdk_version,
-      client_info.app_build AS app_build,
-      client_info.app_channel AS app_channel,
-      client_info.app_display_version AS app_display_version,
-      client_info.architecture AS architecture,
-      client_info.client_id AS client_id,
-      client_info.device_manufacturer AS device_manufacturer,
-      client_info.device_model AS device_model,
-      client_info.first_run_date AS first_run_date,
-      client_info.locale AS locale,
-      client_info.os AS os,
-      client_info.os_version AS os_version,
-      client_info.telemetry_sdk_build AS telemetry_sdk_build,
-      client_info.build_date AS build_date,
-      CAST(
-        NULL AS INT64
-      ) AS windows_build_number  -- will never exist on Android
-    ) AS client_info,
-    (
-      SELECT AS STRUCT
-        metadata.* REPLACE (
-          STRUCT(
-            metadata.header.`date` AS `date`,
-            metadata.header.dnt AS dnt,
-            metadata.header.x_debug_id AS x_debug_id,
-            metadata.header.x_pingsender_version AS x_pingsender_version,
-            metadata.header.x_source_tags AS x_source_tags,
-            metadata.header.x_telemetry_agent AS x_telemetry_agent,
-            metadata.header.x_foxsec_ip_reputation AS x_foxsec_ip_reputation,
-            metadata.header.x_lb_tags AS x_lb_tags,
-            metadata.header.parsed_date AS parsed_date,
-            metadata.header.parsed_x_source_tags AS parsed_x_source_tags,
-            metadata.header.parsed_x_lb_tags AS parsed_x_lb_tags
-          ) AS header
-        )
-    ) AS metadata,
+      metadata.header.date,
+      metadata.header.dnt,
+      metadata.header.x_debug_id,
+      metadata.header.x_pingsender_version,
+      metadata.header.x_source_tags,
+      metadata.header.x_telemetry_agent,
+      metadata.header.x_foxsec_ip_reputation,
+      metadata.header.x_lb_tags,
+      metadata.header.parsed_date,
+      metadata.header.parsed_x_source_tags,
+      metadata.header.parsed_x_lb_tags
+    ) AS header,
+    metadata.isp,
+    metadata.user_agent
+  ) AS metadata,
+  STRUCT(
+    STRUCT(SAFE_CAST(NULL AS STRING) AS key, SAFE_CAST(NULL AS STRING) AS value) AS jwe,
+    metrics.labeled_counter,
     STRUCT(
-      CAST(NULL AS ARRAY<STRUCT<key STRING, value STRING>>) AS jwe,
-      metrics.labeled_counter AS labeled_counter,
-      CAST(
-        NULL
-        AS
-          ARRAY<
-            STRUCT<
-              key STRING,
-              value ARRAY<STRUCT<key STRING, value STRUCT<denominator INT64, numerator INT64>>>
-            >
-          >
-      ) AS labeled_rate,
-      CAST(NULL AS ARRAY<STRUCT<key STRING, value STRING>>) AS url,
-      CAST(NULL AS ARRAY<STRUCT<key STRING, value STRING>>) AS text
-    ) AS metrics
-  )
+      SAFE_CAST(NULL AS STRING) AS key,
+      STRUCT(
+        SAFE_CAST(NULL AS STRING) AS key,
+        STRUCT(
+          SAFE_CAST(NULL AS INTEGER) AS denominator,
+          SAFE_CAST(NULL AS INTEGER) AS numerator
+        ) AS value
+      ) AS value
+    ) AS labeled_rate,
+    STRUCT(SAFE_CAST(NULL AS STRING) AS key, SAFE_CAST(NULL AS STRING) AS value) AS url,
+    STRUCT(SAFE_CAST(NULL AS STRING) AS key, SAFE_CAST(NULL AS STRING) AS value) AS text
+  ) AS metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
 FROM
   `moz-fx-data-shared-prod.org_mozilla_firefox_vpn.main`

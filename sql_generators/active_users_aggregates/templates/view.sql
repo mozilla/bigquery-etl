77b5eccf83b9394ced8147863fa726fa792ecac4
Lucia Vargas <lvargas@mozilla.com>
2023-02-23 16:40:42 +0100

    DENG-682 Refactor active_users_aggregates to be generated per-browser, change source for mobile data to clients_last_seen_joined and for desktop data to telemetry.main.

---
+++ sql_generators/active_users_aggregates/templates/view.sql
@@ -0,0 +1,8 @@
+{ {header } }
+CREATE OR REPLACE VIEW
+  `{{ project_id }}.{{ app_name }}.active_users_aggregates`
+AS
+SELECT
+  *
+FROM
+  `{{ project_id }}.{{ app_name }}_derived.active_users_aggregates_v1`

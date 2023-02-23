14f832f698cc2ae3d1e6bad388b9f28f125ee0a5
kik-kik <kignasiak@mozilla.com>
2023-02-23 11:48:46 +0100

    tweaked the query so that for seen_in_tier_1_country and registered fields all user service records for a day are considered

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_daily_v2/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_daily_v2/query.sql
@@ -1,17 +1,39 @@
+WITH base AS (
+  SELECT
+    user_id,
+    country,
+    `language`,
+    os_name,
+    os_version,
+    seen_in_tier1_country,
+    registered,
+    user_service_first_daily_flow_info,
+  FROM
+    `firefox_accounts_derived.fxa_users_services_daily_v2`
+  WHERE
+    submission_date = @submission_date
+),
+bool_fields AS (
+  SELECT
+    user_id,
+    LOGICAL_OR(seen_in_tier1_country) AS seen_in_tier1_country,
+    LOGICAL_OR(registered) AS registered,
+  FROM
+    base
+  GROUP BY
+    user_id
+)
 SELECT
-  submission_date,
-  user_id,
-  country,
-  `language`,
-  app_version,
-  os_name,
-  os_version,
-  seen_in_tier1_country,
-  registered,
+  @submission_date AS submission_date,
+  * EXCEPT (seen_in_tier1_country, registered, user_service_first_daily_flow_info),
+  bool_fields.seen_in_tier1_country,
+  bool_fields.registered,
 FROM
-  `firefox_accounts_derived.fxa_users_services_daily_v2`
-WHERE
-  submission_date = @submission_date
+  base
+LEFT JOIN
+  bool_fields
+USING
+  (user_id)
 QUALIFY
   ROW_NUMBER() OVER (
     PARTITION BY

e61eb99c6f2c982d9ca7c8b95e3701d610b73468
Anna Scholtz <anna@scholtzan.net>
2023-02-27 13:45:51 -0800

    Fix publishing UDFs with fully qualified identifiers

--- bigquery_etl/routine/publish_routines.py
+++ bigquery_etl/routine/publish_routines.py
@@ -167,6 +167,7 @@ def publish_routine(
             # ensure UDF definitions are not replaced twice as would be the case for
             # `mozfun`.stats.mode_last and `mozfun`.stats.mode_last_retain_nulls
             # since one name is a substring of the other
+            definition = definition.replace(f"`{project_id}.{udf}`", udf)
             definition = definition.replace(f"`{project_id}`.{udf}", udf)
             definition = definition.replace(f"{project_id}.{udf}", udf)
             definition = definition.replace(udf, f"`{project_id}`.{udf}")

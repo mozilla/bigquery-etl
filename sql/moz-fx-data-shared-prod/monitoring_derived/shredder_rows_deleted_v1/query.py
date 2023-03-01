12e16850fa6ad31973416f6df5ea140e22540dd3
Daniel Thorn <dthorn@mozilla.com>
2023-03-01 14:46:15 -0800

    address review

--- sql/moz-fx-data-shared-prod/monitoring_derived/shredder_rows_deleted_v1/query.py
+++ sql/moz-fx-data-shared-prod/monitoring_derived/shredder_rows_deleted_v1/query.py
@@ -1,14 +1,22 @@
 """Monitor the number of rows deleted by shredder."""
 import datetime
 from argparse import ArgumentParser
-from pathlib import Path
 from multiprocessing.pool import ThreadPool
+from pathlib import Path
 
 import yaml
-from google.api_core.exceptions import BadRequest
 from google.cloud import bigquery
 
 from bigquery_etl.util import standard_args
+
+PROJECT_IDS_QUERY = """
+SELECT DISTINCT
+  SPLIT(job_id, ":")[OFFSET(0)] AS project_id,
+FROM
+  `moz-fx-data-shredder.shredder_state.shredder_state`
+WHERE
+  DATE(job_created) BETWEEN @end_date - 1 AND @end_date
+"""
 
 JOB_INFO_QUERY = """
 WITH shredder AS (
@@ -18,24 +26,15 @@ WITH shredder AS (
     SPLIT(job_id, ".")[OFFSET(1)] AS job_id,
   FROM
     `moz-fx-data-shredder.shredder_state.shredder_state`
+  WHERE
+    DATE(job_created) BETWEEN @end_date - 1 AND @end_date
 ),
-jobs AS (
+successful_jobs AS (
   -- https://cloud.google.com/bigquery/docs/information-schema-jobs
   SELECT
     *
   FROM
-    `moz-fx-data-shredder.region-us.INFORMATION_SCHEMA.JOBS_BY_PROJECT`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `moz-fx-data-bq-batch-prod.region-us.INFORMATION_SCHEMA.JOBS_BY_PROJECT`
-),
-successful_jobs AS (
-  SELECT
-    *,
-  FROM
-    jobs
+    `{project_id}.region-us.INFORMATION_SCHEMA.JOBS_BY_PROJECT`
   WHERE
     DATE(creation_time) BETWEEN @end_date - 1 AND @end_date
     AND DATE(end_time) = @end_date
@@ -45,7 +44,9 @@ successful_jobs AS (
 SELECT
   successful_jobs.end_time,
   shredder.full_job_id AS job_id,
-  successful_jobs.destination_table.*,
+  successful_jobs.destination_table.project_id,
+  successful_jobs.destination_table.dataset_id,
+  successful_jobs.destination_table.table_id,
   successful_jobs.dml_statistics.deleted_row_count,
   UNIX_MILLIS(successful_jobs.start_time) AS start_time_millis,
   UNIX_MILLIS(successful_jobs.end_time) AS end_time_millis,
@@ -72,22 +73,31 @@ parser.add_argument(
 standard_args.add_parallelism(parser)
 
 
-def add_job_metdata(client, job):
+def get_job_info(
+    client: bigquery.Client, project_id: str, end_date: datetime.date
+) -> list[dict]:
+    query = client.query(
+        JOB_INFO_QUERY.format(project_id=project_id),
+        job_config=bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("end_date", "DATE", end_date),
+            ]
+        ),
+    )
+    return list(map(dict, query.result()))
+
+
+def add_job_metadata(client: bigquery.Client, job: dict) -> dict:
     if job["deleted_row_count"] is None:
         table = f'{job["project_id"]}.{job["dataset_id"]}.{job["table_id"]}'
-        try:
-            before = client.get_table(f'{table}@{job["start_time_millis"]}').num_rows
-            after = client.get_table(f'{table}@{job["end_time_millis"]}').num_rows
-        except BadRequest as e:
-            if "Invalid time travel timestamp" in e.message:
-                return None  # row count not available
-            raise
-        job["deleted_row_count"] = before - after
+        before = client.get_table(f'{table}@{job["start_time_millis"]}').num_rows
+        after = client.get_table(f'{table}@{job["end_time_millis"]}').num_rows
+        job["deleted_row_count"] = count = before - after
+        if count < 0:
+            raise ValueError(f"deleted_row_count must be >= 0, but got: {count}")
     # move partition id to separate column
     if "$" in job["table_id"]:
-        (job["table_id"], _, job["partition_id"],) = job[
-            "table_id"
-        ].partition("$")
+        job["table_id"], _, job["partition_id"] = job["table_id"].partition("$")
     job["end_time"] = job["end_time"].isoformat()
     return job
 
@@ -98,24 +108,28 @@ def main():
     client = bigquery.Client()
 
     query = client.query(
-        JOB_INFO_QUERY,
+        PROJECT_IDS_QUERY,
         job_config=bigquery.QueryJobConfig(
             query_parameters=[
                 bigquery.ScalarQueryParameter("end_date", "DATE", args.end_date),
             ]
         ),
     )
-
-    jobs = list(map(dict, query.result()))
+    project_ids = [row["project_id"] for row in query.result()]
 
     with ThreadPool(args.parallelism) as pool:
+        jobs = sum(
+            pool.starmap(
+                get_job_info,
+                ((client, project_id, args.end_date) for project_id in project_ids),
+                chunksize=1,
+            ),
+            start=[],
+        )
         json_rows = list(
-            filter(
-                None,
-                pool.starmap(
-                    add_job_metdata, ((client, job) for job in jobs), chunksize=1
-                ),
-            )
+            pool.starmap(
+                add_job_metadata, ((client, job) for job in jobs), chunksize=1
+            ),
         )
 
     load_job = client.load_table_from_json(

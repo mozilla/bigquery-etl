192c454bf9d1d66c06f9440e7a82a7569ef7d7c5
kik-kik <kignasiak@mozilla.com>
2023-03-06 11:33:05 +0100

    applied code change suggestion made by akkomar in PR#3607

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_daily_v2/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_users_daily_v2/query.sql
@@ -1,43 +1,22 @@
-WITH base AS (
-  SELECT
-    user_id,
-    country,
-    `language`,
-    os_name,
-    os_version,
-    seen_in_tier1_country,
-    registered,
-    user_service_first_daily_flow_info,
-  FROM
-    `firefox_accounts_derived.fxa_users_services_daily_v2`
-  WHERE
-    submission_date = @submission_date
-),
-bool_fields AS (
-  SELECT
-    user_id,
-    LOGICAL_OR(seen_in_tier1_country) AS seen_in_tier1_country,
-    LOGICAL_OR(registered) AS registered,
-  FROM
-    base
-  GROUP BY
-    user_id
-)
 SELECT
-  @submission_date AS submission_date,
-  * EXCEPT (seen_in_tier1_country, registered, user_service_first_daily_flow_info),
-  bool_fields.seen_in_tier1_country,
-  bool_fields.registered,
+  submission_date,
+  user_id,
+  country,
+  `language`,
+  os_name,
+  os_version,
+  LOGICAL_OR(seen_in_tier1_country) OVER (user_window) AS seen_in_tier1_country,
+  LOGICAL_OR(registered) OVER (user_window) AS registered,
 FROM
-  base
-LEFT JOIN
-  bool_fields
-USING
-  (user_id)
+  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_services_daily_v2`
+WHERE
+  submission_date = @submission_date
 QUALIFY
-  ROW_NUMBER() OVER (
+  ROW_NUMBER() OVER (user_window) = 1
+WINDOW
+  user_window AS (
     PARTITION BY
       user_id
     ORDER BY
-      user_service_first_daily_flow_info.`timestamp` ASC
-  ) = 1
+      user_service_first_daily_flow_info.timestamp ASC
+  )

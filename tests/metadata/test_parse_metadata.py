33a7ab0921678d9b29da1cfa137dc92e46e8c824
Winnie Chan <10429026+wwyc@users.noreply.github.com>
2023-03-06 15:08:50 -0800

    DENG-590 Added owner to labels metadata (#3637)
    * Added owner to labels metadata
    
    * Update bigquery_etl/metadata/parse_metadata.py
    
    Co-authored-by: Alexander <anicholson@mozilla.com>
    
    ---------
    
    Co-authored-by: Alexander <anicholson@mozilla.com>

--- tests/metadata/test_parse_metadata.py
+++ tests/metadata/test_parse_metadata.py
@@ -85,6 +85,8 @@ class TestParseMetadata(object):
         assert metadata.labels["number_string"] == "1234abcde"
         assert "123-432" in metadata.labels
         assert metadata.owners == ["test1@mozilla.com", "test2@example.com"]
+        assert metadata.labels["owner1"] == "test1"
+        assert metadata.labels["owner2"] == "test2"
         assert "query.sql" in metadata.references
         assert metadata.references["query.sql"] == ["project.dataset_derived.table_v1"]
 

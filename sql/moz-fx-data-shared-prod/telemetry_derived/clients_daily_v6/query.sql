2daf13fcaf9eb2ac571da9842cdc534ef14c79a7
m-d-bowerman <mbowerman@mozilla.com>
2023-03-08 14:24:09 -0800

    Update schemas with hist fields

--- sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_v6/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_v6/query.sql
@@ -1418,6 +1418,8 @@ aggregates AS (
     SUM(text_recognition_api_performance_count) AS text_recognition_api_performance_count_sum,
     SUM(text_recognition_text_length) AS text_recognition_text_length_sum,
     SUM(text_recognition_text_length_count) AS text_recognition_text_length_count_sum,
+    SUM(places_searchbar_cumulative_searches) AS places_searchbar_cumulative_searches_sum,
+    SUM(places_searchbar_cumulative_filter_count) AS places_searchbar_cumulative_filter_count_sum,
     LOGICAL_OR(
       scalar_parent_dom_parentprocess_private_window_used
     ) AS dom_parentprocess_private_window_used,

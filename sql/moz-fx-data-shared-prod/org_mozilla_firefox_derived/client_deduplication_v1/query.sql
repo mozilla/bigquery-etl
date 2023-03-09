ab78fb58cba6d74774b531b8789e4d8f11b2f0e0
Frank Bertsch <frank.bertsch@gmail.com>
2023-03-09 11:40:12 -0500

    Enable Client Deduplication ping anonymization
    The outcome here is the new tables should reflect almost
    exactly what our ping structure is. One difference is the
    addition of submission_date for ease of querying / partitioning.
    - Hash the Ad IDs with a secret key
    - Replace stable views with views on derived tables
    - Add view over all client deduplication pings

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_firefox_derived/client_deduplication_v1/query.sql
@@ -0,0 +1,38 @@
+-- Query for org_mozilla_firefox_derived.client_deduplication_v1
+WITH hmac_key AS (
+  SELECT
+    AEAD.DECRYPT_BYTES(
+      (SELECT keyset FROM `moz-fx-dataops-secrets.airflow_query_keys.org_mozilla_firefox`),
+      ciphertext,
+      CAST(key_id AS BYTES)
+    ) AS value
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_firefox.encrypted_keys_v1`
+  WHERE
+    key_id = 'ad_id'
+),
+SELECT
+  DATE(submission_timestamp) AS submission_date,
+  "org_mozilla_firefox" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics.* REPLACE (
+    metrics.string.* REPLACE (
+      TO_HEX(
+        udf.hmac_sha256((SELECT * FROM hmac_key), CAST(metrics.string.activation_identifier AS BYTES))
+      ) AS activation_identifier
+    ) AS string
+  ) AS metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_stable.client_deduplication_v1`

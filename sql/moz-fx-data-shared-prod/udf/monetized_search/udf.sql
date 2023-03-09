08bff484bd72ee4ddafcb57c2b5e4feb1dd976a2
Alexander Nicholson <anicholson@mozilla.com>
2023-03-09 17:49:45 -0500

    Stub for monetized search

---
+++ sql/moz-fx-data-shared-prod/udf/monetized_search/udf.sql
@@ -0,0 +1,10 @@
+/*
+Stub for a function that returns whether search is monetized.
+*/
+CREATE OR REPLACE FUNCTION udf.monetized_search(engine STRING, country STRING, distribution_id STRING, submission_date DATE) AS (
+    CASE
+     WHEN TRUE
+       THEN FALSE
+     ELSE FALSE
+    END
+);

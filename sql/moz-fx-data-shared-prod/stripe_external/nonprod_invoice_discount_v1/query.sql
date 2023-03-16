a9a6aa5795a2c95b7d759ba80492e67c2e0281ff
Daniel Thorn <dthorn@mozilla.com>
2023-03-16 09:40:42 -0700

    Bug 1818792 - Stop exposing fivetran datasets directly via user-facing views (#3660)

---
+++ sql/moz-fx-data-shared-prod/stripe_external/nonprod_invoice_discount_v1/query.sql
@@ -0,0 +1,14 @@
+SELECT
+  id,
+  invoice_id,
+  _fivetran_synced,
+  checkout_session,
+  coupon_id,
+  customer_id,
+  `end`,
+  invoice_item_id,
+  promotion_code,
+  start,
+  subscription_id,
+FROM
+  `dev-fivetran`.stripe_nonprod.invoice_discount

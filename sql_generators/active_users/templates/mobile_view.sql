e6a2293678eb93743a25d2951a93b4d966405d69
Lucia Vargas <lvargas@mozilla.com>
2023-03-21 13:40:50 +0100

    DENG-516 Formatting.

--- sql_generators/active_users/templates/mobile_view.sql
+++ sql_generators/active_users/templates/mobile_view.sql
@@ -3,27 +3,27 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ dataset_id }}.active_users_aggregates_mobile`
 AS
 SELECT
-  {{ view_columns }}
+  { {view_columns } }
 FROM
   `{{ project_id }}.{{ fenix_dataset }}_derived.active_users_aggregates_v1`
 UNION ALL
 SELECT
-  {{ view_columns }}
+  { {view_columns } }
 FROM
   `{{ project_id }}.{{ firefox_ios_dataset }}_derived.active_users_aggregates_v1`
 UNION ALL
 SELECT
-  {{ view_columns }}
+  { {view_columns } }
 FROM
   `{{ project_id }}.{{ focus_ios_dataset }}_derived.active_users_aggregates_v1`
 UNION ALL
 SELECT
-  {{ view_columns }}
+  { {view_columns } }
 FROM
   `{{ project_id }}.{{ klar_ios_dataset }}_derived.active_users_aggregates_v1`
 UNION ALL
 SELECT
-  {{ view_columns }}
+  { {view_columns } }
 FROM
   `{{ project_id }}.{{ focus_android_dataset }}_derived.active_users_aggregates_v1`
 WHERE

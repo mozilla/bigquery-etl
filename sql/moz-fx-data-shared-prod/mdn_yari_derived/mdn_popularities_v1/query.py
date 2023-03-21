75df70c428f76fb019d18396a0125e9e6dc10e14
Claas Augner <495429+caugner@users.noreply.github.com>
2023-03-21 15:33:43 +0100

    fix(MP-267): align query with popularitiesCron
    1. Uses column names "Page" and "Pageviews".
    2. Returns absolute page view counts.
    3. Filters only successful requests.
    4. Filters only requests by "Mozilla" user agents.

--- sql/moz-fx-data-shared-prod/mdn_yari_derived/mdn_popularities_v1/query.py
+++ sql/moz-fx-data-shared-prod/mdn_yari_derived/mdn_popularities_v1/query.py
@@ -8,33 +8,16 @@ from datetime import datetime
 from google.cloud import bigquery, storage
 
 QUERY_TEMPLATE = """\
-WITH host_stripped AS (
-  SELECT
-    SPLIT(pge.metrics.url2.page_path, 'https://developer.mozilla.org')[OFFSET(1)] AS `path`,
-    COUNT(*) AS page_hits,
-  FROM
-    `moz-fx-data-shared-prod.mdn_yari.page` AS pge
-  WHERE
-    DATE(submission_timestamp)
-    BETWEEN DATE_TRUNC(@submission_date, MONTH)
-    AND LAST_DAY(@submission_date)
-    AND CONTAINS_SUBSTR(pge.metrics.url2.page_path, 'https://developer.mozilla.org/')
-    AND client_info.app_channel = 'prod'
-  GROUP BY
-    `path`
-)
-SELECT
-  host_stripped.`path`,
-  host_stripped.page_hits / FIRST_VALUE(host_stripped.page_hits) OVER (
-    ORDER BY
-      page_hits DESC
-  ) AS popularity
-FROM
-  host_stripped
-WHERE
-  ARRAY_LENGTH(SPLIT(host_stripped.`path`, '/')) > 3
-ORDER BY
-    popularity DESC
+SELECT REGEXP_EXTRACT(pge.metrics.url2.page_path, r'^https://developer.mozilla.org(/.+?/docs/[^?#]+)') AS Page,
+       COUNT(*) AS Pageviews,
+FROM `moz-fx-data-shared-prod.mdn_yari.page` AS pge
+WHERE DATE(submission_timestamp) BETWEEN DATE_TRUNC(@submission_date, MONTH) AND LAST_DAY(@submission_date)
+  AND client_info.app_channel = 'prod'
+  AND REGEXP_CONTAINS(pge.metrics.url2.page_path, r'^https://developer.mozilla.org(/.+?/docs/[^?#]+)')
+  AND pge.metrics.string.page_http_status = '200'
+  AND pge.metrics.string.navigator_user_agent LIKE 'Mozilla%'
+GROUP BY Page
+ORDER BY Pageviews DESC
 """
 
 APP = "mdn_yari"

82a4520213dd5e0cea4316c00607a999a45a3e18
akkomar <akkomar@users.noreply.github.com>
2023-03-21 13:57:37 +0100

    Update sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
    Co-authored-by: Lucia <30448600+lucia-vargas-a@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
@@ -13,7 +13,7 @@ WHERE
     'Focus Android'
   )
   OR (
-    app_name IN ('Focus Android Glean', 'Focus Android Glean BrowserStack')
+    normalized_app_name IN ('Focus Android Glean', 'Focus Android Glean BrowserStack')
     AND submission_date >= '2023-01-01'
   )
   OR (app_name IN ('Focus Android') AND submission_date < '2023-01-01')

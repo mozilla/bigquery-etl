90b03137ad2d4dc69e459f6de8e3cdd60a67e124
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2023-03-22 19:51:12 +0100

    DENG-742 Add os_grouped to active_users views. (#3666)
    Co-authored-by: Lucia Vargas <lvargas@mozilla.com>

--- sql_generators/active_users/templates/focus_android_view.sql
+++ sql_generators/active_users/templates/focus_android_view.sql
@@ -2,10 +2,13 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ app_name }}.active_users_aggregates`
 AS
 SELECT
-  * REPLACE (COALESCE(country, '??') AS country),
-  `mozfun.norm.browser_version_info`(app_version) AS browser_version_info
+  * EXCEPT (app_version),
+  app_version,
+  `mozfun.norm.browser_version_info`(app_version).major_version AS app_version_major,
+  `mozfun.norm.browser_version_info`(app_version).minor_version AS app_version_minor,
+  `mozfun.norm.browser_version_info`(app_version).patch_revision AS app_version_patch_revision,
+  `mozfun.norm.browser_version_info`(app_version).is_major_release AS app_version_is_major_release
 FROM
   `{{ project_id }}.{{ app_name }}_derived.active_users_aggregates_v1`
 WHERE
-  app_name != 'Focus Android Glean'
-  AND app_name != 'Focus Android Glean BrowserStack'
+  app_name NOT IN ('Focus Android Glean', 'Focus Android Glean BrowserStack')

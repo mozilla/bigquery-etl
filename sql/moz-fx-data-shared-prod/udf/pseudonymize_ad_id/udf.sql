420267581ae68ae66929ee39bf5c0e2118b086fc
Frank Bertsch <frank.bertsch@gmail.com>
2023-03-22 13:49:56 -0400

    Remove trailing s from udf invocation

--- sql/moz-fx-data-shared-prod/udf/pseudonymize_ad_id/udf.sql
+++ sql/moz-fx-data-shared-prod/udf/pseudonymize_ad_id/udf.sql
@@ -1,4 +1,4 @@
--- pseudonymize_ad_ids: Only for use in Ad ID pipeline
+-- pseudonymize_ad_id: Only for use in Ad ID pipeline
 CREATE OR REPLACE FUNCTION udf.pseudonymize_ad_id(hashed_ad_id STRING, key BYTES)
 RETURNS STRING AS (
   IF( -- These two values represent the hashed all-0 Ad IDs, see bug 1823482
@@ -13,11 +13,11 @@ RETURNS STRING AS (
 
 -- Tests
 SELECT
-  assert.null(udf.pseudonymize_ad_ids("q7RZx9leE4AHjn9DRQR8kLtjO0FNwmj/61ECGxFinno=", b"\x14")),
-  assert.null(udf.pseudonymize_ad_ids("ksEEnM6/esBxl2myqrVLo31ePBoQZZnkCm81vefaF90=", b"\x14")),
-  assert.not_null(udf.pseudonymize_ad_ids("7RZx9leE4AHjn9DRQR8kLtjO0FNwmj/61ECGxFinno=", b"\x14")),
-  assert.not_null(udf.pseudonymize_ad_ids("sEEnM6/esBxl2myqrVLo31ePBoQZZnkCm81vefaF90=", b"\x14")),
+  assert.null(udf.pseudonymize_ad_id("q7RZx9leE4AHjn9DRQR8kLtjO0FNwmj/61ECGxFinno=", b"\x14")),
+  assert.null(udf.pseudonymize_ad_id("ksEEnM6/esBxl2myqrVLo31ePBoQZZnkCm81vefaF90=", b"\x14")),
+  assert.not_null(udf.pseudonymize_ad_id("7RZx9leE4AHjn9DRQR8kLtjO0FNwmj/61ECGxFinno=", b"\x14")),
+  assert.not_null(udf.pseudonymize_ad_id("sEEnM6/esBxl2myqrVLo31ePBoQZZnkCm81vefaF90=", b"\x14")),
   assert.equals(
-    udf.pseudonymize_ad_ids("abc", b"\x14"),
+    udf.pseudonymize_ad_id("abc", b"\x14"),
     TO_HEX(`moz-fx-data-shared-prod`.udf.hmac_sha256(b"\x14", CAST("abc" AS BYTES)))
   )

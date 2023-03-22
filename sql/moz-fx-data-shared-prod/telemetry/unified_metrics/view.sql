c3e0b9ce83f2d24dac5d196978356ac08c41769e
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2023-03-22 15:23:41 +0100

    Address review feedback: normalize Focus Android app name

--- sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/unified_metrics/view.sql
@@ -2,7 +2,12 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.unified_metrics`
 AS
 SELECT
-  * REPLACE (COALESCE(country, '??') AS country),
+  * EXCEPT (normalized_app_name) REPLACE(COALESCE(country, '??') AS country),
+  IF(
+    normalized_app_name IN ('Focus Android Glean', 'Focus Android'),
+    'Focus Android',
+    normalized_app_name
+  ) AS normalized_app_name,
   `mozfun.norm.browser_version_info`(app_version) AS browser_version_info
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.unified_metrics_v1`

516f1e4d0dd1ce3ddba79a83a312244f023e69ae
Lucia Vargas <lvargas@mozilla.com>
2023-03-30 17:14:24 +0200

    DENG-762 Align views and DAG.

--- sql_generators/active_users/templates/focus_android_view.sql
+++ sql_generators/active_users/templates/focus_android_view.sql
@@ -2,13 +2,8 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ app_name }}.active_users_aggregates`
 AS
 SELECT
-  * EXCEPT (app_version, app_name) REPLACE(COALESCE(country, '??') AS country),
-  IF
-  (
-    app_name IN ('Focus Android Glean', 'Focus Android'),
-    'Focus Android',
-    app_name
-  ) AS app_name,
+  * EXCEPT (app_version, app_name),
+  IF(app_name IN ('Focus Android Glean', 'Focus Android'), 'Focus Android', app_name) AS app_name,
   app_version,
   `mozfun.norm.browser_version_info`(app_version).major_version AS app_version_major,
   `mozfun.norm.browser_version_info`(app_version).minor_version AS app_version_minor,
@@ -17,11 +12,7 @@ SELECT
 FROM
   `{{ project_id }}.{{ app_name }}_derived.active_users_aggregates_v1`
 WHERE
-  app_name NOT IN (
-    'Focus Android Glean',
-    'Focus Android Glean BrowserStack',
-    'Focus Android'
-  )
+  app_name NOT IN ('Focus Android Glean', 'Focus Android Glean BrowserStack', 'Focus Android')
   OR (
     app_name IN ('Focus Android Glean', 'Focus Android Glean BrowserStack')
     AND submission_date >= '2023-01-01'

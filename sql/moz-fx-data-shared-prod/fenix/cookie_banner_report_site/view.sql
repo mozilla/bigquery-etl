bf3262ba410808f2a2cc0bdee3111edb7f4bc4f3
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-03-31 05:17:45 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/fenix/cookie_banner_report_site/view.sql
@@ -0,0 +1,98 @@
+-- Generated via ./bqetl generate glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fenix.cookie_banner_report_site`
+AS
+SELECT
+  "org_mozilla_firefox" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox.cookie_banner_report_site`
+UNION ALL
+SELECT
+  "org_mozilla_firefox_beta" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_beta.cookie_banner_report_site`
+UNION ALL
+SELECT
+  "org_mozilla_fenix" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix.cookie_banner_report_site`
+UNION ALL
+SELECT
+  "org_mozilla_fenix_nightly" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix_nightly.cookie_banner_report_site`
+UNION ALL
+SELECT
+  "org_mozilla_fennec_aurora" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fennec_aurora.cookie_banner_report_site`

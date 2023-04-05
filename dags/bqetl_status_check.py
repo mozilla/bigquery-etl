fcaaa8a13463e5e0c7425851bdbd6feddb07e298
Anna Scholtz <anna@scholtzan.net>
2023-04-05 11:46:37 -0700

    Add check for monitoring python script runs

--- dags/bqetl_status_check.py
+++ dags/bqetl_status_check.py
@@ -20,9 +20,9 @@ breakages as soon as possible.
 
 *Triage notes*
 
-None of these tasks should fail. If they do it is very likely that other/all ETL tasks will 
+None of these tasks should fail. If they do it is very likely that other/all ETL tasks will
 subsequently fail as well. Any failures should be communicated to the Data Infra Working Group
-as soon as possible. 
+as soon as possible.
 
 #### Owner
 
@@ -34,7 +34,7 @@ default_args = {
     "owner": "ascholtz@mozilla.com",
     "start_date": datetime.datetime(2023, 4, 1, 0, 0),
     "end_date": None,
-    "email": ["telemetry-alerts@mozilla.com", "data-platform-infra-wg@mozilla.com"],
+    "email": ["telemetry-alerts@mozilla.com"],
     "depends_on_past": False,
     "retry_delay": datetime.timedelta(seconds=1800),
     "email_on_failure": True,
@@ -47,10 +47,21 @@ tags = ["impact/tier_1", "repo/bigquery-etl"]
 with DAG(
     "bqetl_status_check",
     default_args=default_args,
-    schedule_interval=datetime.timedelta(seconds=1800),
+    schedule_interval=datetime.timedelta(seconds=3600),
     doc_md=docs,
     tags=tags,
 ) as dag:
+    monitoring_derived__bigquery_etl_python_run_check__v1 = gke_command(
+        task_id="monitoring_derived__bigquery_etl_python_run_check__v1",
+        command=[
+            "python",
+            "sql/moz-fx-data-shared-prod/monitoring_derived/bigquery_etl_python_run_check_v1/query.py",
+        ]
+        + ["--task_instance={{ task_instance_key_str }}", "--run_id={{ run_id }}"],
+        docker_image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
+        owner="ascholtz@mozilla.com",
+        email=["ascholtz@mozilla.com", "telemetry-alerts@mozilla.com"],
+    )
 
     monitoring_derived__bigquery_etl_sql_run_check__v1 = bigquery_etl_query(
         task_id="monitoring_derived__bigquery_etl_sql_run_check__v1",
@@ -58,11 +69,7 @@ with DAG(
         dataset_id="monitoring_derived",
         project_id="moz-fx-data-shared-prod",
         owner="ascholtz@mozilla.com",
-        email=[
-            "ascholtz@mozilla.com",
-            "data-platform-infra-wg@mozilla.com",
-            "telemetry-alerts@mozilla.com",
-        ],
+        email=["ascholtz@mozilla.com", "telemetry-alerts@mozilla.com"],
         date_partition_parameter=None,
         depends_on_past=False,
         task_concurrency=1,
--- sql/moz-fx-data-shared-prod/monitoring_derived/bigquery_etl_sql_run_check_v1/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/bigquery_etl_sql_run_check_v1/query.sql
@@ -1,4 +1,4 @@
 SELECT
-  CURRENT_TIMESTAMP() AS last_run_timestamp,
-  '@task_instance_key_str' AS task_instance,
-  '@run_id' AS run_id
+  CURRENT_TIMESTAMP() AS run_timestamp,
+  @task_instance AS task_instance,
+  @run_id AS run_id

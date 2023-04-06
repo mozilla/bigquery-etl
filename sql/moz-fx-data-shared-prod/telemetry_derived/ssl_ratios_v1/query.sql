97798f461fa03f1c9906ba82ff093a151ddaa4be
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-04-06 22:36:21 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry_derived/ssl_ratios_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/ssl_ratios_v1/query.sql
@@ -6,19 +6,47 @@ SELECT
   ssl_loads_v1 AS ssl_loads,
   http_pageload_is_ssl_ratio_v1 AS reporting_ratio
 FROM
-  {{
-  metrics.calculate(
-    metrics=["non_ssl_loads_v1", "ssl_loads_v1", "http_pageload_is_ssl_ratio_v1"],
-    platform="firefox_desktop",
-    group_by={"os": "environment.system.os.name", "country": "metadata.geo.country"},
-    where="""
-      sample_id = 42
-      AND normalized_channel = 'release'
-      AND environment.system.os.name IN ('Windows_NT', 'Darwin', 'Linux')
-      AND application.name = 'Firefox'
-      AND DATE(submission_timestamp) > DATE '2016-11-01'
-      AND (DATE(submission_timestamp) = @submission_date OR @submission_date IS NULL)
-    """,
-    group_by_client_id=False
+  (
+    WITH main AS (
+      SELECT
+        submission_date AS submission_date,
+        environment.system.os.name AS os,
+        metadata.geo.country AS country,
+        SUM(
+          mozfun.map.get_key(mozfun.hist.extract(payload.histograms.http_pageload_is_ssl).values, 0)
+        ) AS non_ssl_loads_v1,
+        SUM(
+          mozfun.map.get_key(mozfun.hist.extract(payload.histograms.http_pageload_is_ssl).values, 1)
+        ) AS ssl_loads_v1,
+        COUNT(payload.histograms.http_pageload_is_ssl) / COUNT(*) AS http_pageload_is_ssl_ratio_v1,
+      FROM
+        (
+          SELECT
+            *,
+            DATE(submission_timestamp) AS submission_date,
+            environment.experiments
+          FROM
+            `moz-fx-data-shared-prod.telemetry_stable.main_v4`
+        )
+      WHERE
+        sample_id = 42
+        AND normalized_channel = 'release'
+        AND environment.system.os.name IN ('Windows_NT', 'Darwin', 'Linux')
+        AND application.name = 'Firefox'
+        AND DATE(submission_timestamp) > DATE '2016-11-01'
+        AND (DATE(submission_timestamp) = @submission_date OR @submission_date IS NULL)
+      GROUP BY
+        os,
+        country,
+        submission_date
+    )
+    SELECT
+      main.submission_date,
+      main.os AS os,
+      main.country AS country,
+      non_ssl_loads_v1,
+      ssl_loads_v1,
+      http_pageload_is_ssl_ratio_v1,
+    FROM
+      main
   )
-}}

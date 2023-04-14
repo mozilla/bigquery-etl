7530fb70d4868454c2fc9cd45bda2c60f6c2f1c7
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-04-14 05:18:11 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/mozilla_vpn/vpnsession/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/vpnsession/view.sql
@@ -39,3 +39,22 @@ SELECT
   submission_timestamp
 FROM
   `moz-fx-data-shared-prod.org_mozilla_firefox_vpn.vpnsession`
+UNION ALL
+SELECT
+  "org_mozilla_ios_firefoxvpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn.vpnsession`

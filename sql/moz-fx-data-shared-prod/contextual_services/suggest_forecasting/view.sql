36cb286faea51ac0d2415bc85dc14df69ca7ccc7
skahmann3 <16420065+skahmann3@users.noreply.github.com>
2023-04-14 13:43:18 -0700

    Update & separate suggest forecasting from Tiles

---
+++ sql/moz-fx-data-shared-prod/contextual_services/suggest_forecasting/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.contextual_services.suggest_foreasting`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.contextual_services_derived.suggest_foreasting_v1`

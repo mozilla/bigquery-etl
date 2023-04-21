db2f8dacad3e789a0074c623daf914f271321c06
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2023-04-21 17:49:08 +0200

    DS-2850, use app_name from base table. (#3728)
    Co-authored-by: Lucia Vargas <lvargas@mozilla.com>

--- sql/moz-fx-data-shared-prod/telemetry_derived/sponsored_tiles_clients_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/sponsored_tiles_clients_daily_v1/query.sql
@@ -155,13 +155,13 @@ unified_metrics AS (
       OR (
         normalized_app_name = "Firefox iOS"
         AND (country IN UNNEST(["US"]))
-          OR (country IN UNNEST(["DE"]) AND submission_date >= "2022-12-05")
+        OR (country IN UNNEST(["DE"]) AND submission_date >= "2022-12-05")
         AND browser_version_info.major_version >= 101
       )
       OR (
         normalized_app_name = "Fenix"
         AND (country IN UNNEST(["US"]))
-          OR (country IN UNNEST(["DE"]) AND submission_date >= "2022-12-05")
+        OR (country IN UNNEST(["DE"]) AND submission_date >= "2022-12-05")
         AND browser_version_info.major_version >= 100
       )
     )

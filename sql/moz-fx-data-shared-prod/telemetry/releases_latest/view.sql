db61a7c665f98ebb9ab891a4f1cfb53ab2f76dbd
kik-kik <kignasiak@mozilla.com>
2023-04-21 14:38:25 +0200

    implemented tweaks as suggested by ncalexan and ascholtz in PR#3718

--- sql/moz-fx-data-shared-prod/telemetry/releases_latest/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/releases_latest/view.sql
@@ -2,13 +2,14 @@ WITH channels AS (
   SELECT
   "release" AS channel
   UNION ALL SELECT "beta"
+  UNION ALL SELECT "esr"
 ),
 dates_and_channels AS (
   SELECT
     `date`, channel
   FROM
     UNNEST(
-      (SELECT GENERATE_DATE_ARRAY(DATE_SUB(CURRENT_DATE, INTERVAL 1 YEAR), CURRENT_DATE, INTERVAL 1 DAY) AS dates)
+      (SELECT GENERATE_DATE_ARRAY("2016-01-01", CURRENT_DATE, INTERVAL 1 DAY) AS dates)
     ) AS `date`
     CROSS JOIN channels
 ),
@@ -30,6 +31,7 @@ releases AS (
     category,
     product,
     build_date,
+    build_number,
     COALESCE(
       channel,
       CASE
@@ -42,6 +44,8 @@ releases AS (
   LEFT JOIN builds using(`version`)
   -- we need to dedup per date, channel due to some cases where
   -- we get some versions marked as the wrong channel.
+  WHERE
+    TRUE
   QUALIFY
     ROW_NUMBER() OVER(PARTITION BY `date`, channel ORDER BY submission_timestamp DESC) = 1
 ),
@@ -52,11 +56,13 @@ joined AS (
   FIRST_VALUE(category IGNORE NULLS) OVER latest_values AS category,
   channel,
   DATE(FIRST_VALUE(build_date IGNORE NULLS) OVER latest_values) AS build_date,
+  FIRST_VALUE(build_number IGNORE NULLS) OVER latest_values AS build_number,
   FIRST_VALUE(releases.`date` IGNORE NULLS) OVER latest_values AS release_date,
   FIRST_VALUE(`version` IGNORE NULLS) OVER latest_values AS `version`,
   FROM dates_and_channels
   FULL OUTER JOIN releases USING(`date`, channel)
-  WHERE channel NOT IN ("aurora", "esr")
+  WHERE channel IN ("release", "beta", "esr")
+  AND channel IN ("beta")
   WINDOW latest_values AS (
     PARTITION BY channel ORDER BY DATE DESC
     ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
@@ -64,6 +70,7 @@ joined AS (
 )
 SELECT
   *,
+  IF(channel = "beta", SPLIT(version, "b")[SAFE_OFFSET(1)], NULL) AS beta_version,
   mozfun.norm.extract_version(version, "major") AS major_version,
   mozfun.norm.extract_version(version, "minor") AS minor_version,
   mozfun.norm.extract_version(version, "patch") AS patch_version,

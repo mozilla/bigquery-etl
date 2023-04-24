1fd76ed3536d645392cdd2ec18670d563a0331d7
Yeonjoo Yoo <ysmith@mozilla.com>
2023-04-24 17:15:08 -0400

    Update sql/moz-fx-data-shared-prod/mozilla_vpn_derived/funnel_product_page_to_subscribed_v1/query.sql
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/mozilla_vpn_derived/funnel_product_page_to_subscribed_v1/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_vpn_derived/funnel_product_page_to_subscribed_v1/query.sql
@@ -127,7 +127,7 @@ flows AS (
       AND user_id IS NOT NULL
     ) AS pay_setup_engage_with_uid,
     -- new fxa after entering the email
-    LOGICAL_OR(event_type = "fxa_pay_setup - success") AS pay_setup_complete,
+    LOGICAL_OR(event_type IN ("fxa_pay_setup - 3ds_complete", "fxa_pay_setup - success")) AS pay_setup_complete,
     LOGICAL_OR(
       event_type = "fxa_pay_setup - success"
       AND user_id IS NULL

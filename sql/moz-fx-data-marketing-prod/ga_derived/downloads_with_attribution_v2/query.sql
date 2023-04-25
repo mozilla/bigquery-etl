4188821286ee7fe2ec9677fdeb6546821b53387d
Glenda Leonard <75265513+gleonard-m@users.noreply.github.com>
2023-04-25 13:19:52 -0400

    Case changes.
    Co-authored-by: kik-kik <42538694+kik-kik@users.noreply.github.com>

--- sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/query.sql
@@ -399,6 +399,6 @@ ON
 SELECT * EXCEPT (join_result_v1), 'MISSING_GA_CLIENT_OR_SESSION_ID' AS join_result_v2, join_result_v1 as join_result_v1
 FROM v1_table
 UNION ALL
-SELECT * EXCEPT (join_result_v2), join_result_v2 AS exception_v2, null as exception_v1
+SELECT * EXCEPT (join_result_v2), join_result_v2 AS exception_v2, NULL AS exception_v1
 FROM v2_table
 WHERE join_result_v2 != 'MISSING_GA_CLIENT_OR_SESSION_ID'

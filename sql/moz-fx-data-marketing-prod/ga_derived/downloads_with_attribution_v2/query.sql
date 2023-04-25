b4d2bf8ae84029b7329e83343189f2eff14c9992
Glenda Leonard <75265513+gleonard-m@users.noreply.github.com>
2023-04-25 13:20:16 -0400

    Case changes.
    Co-authored-by: kik-kik <42538694+kik-kik@users.noreply.github.com>

--- sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/query.sql
@@ -396,7 +396,7 @@ ON
   cn.name = country
 )
 
-SELECT * EXCEPT (join_result_v1), 'MISSING_GA_CLIENT_OR_SESSION_ID' AS join_result_v2, join_result_v1 as join_result_v1
+SELECT * EXCEPT (join_result_v1), 'MISSING_GA_CLIENT_OR_SESSION_ID' AS join_result_v2, join_result_v1 AS join_result_v1
 FROM v1_table
 UNION ALL
 SELECT * EXCEPT (join_result_v2), join_result_v2 AS exception_v2, NULL AS exception_v1

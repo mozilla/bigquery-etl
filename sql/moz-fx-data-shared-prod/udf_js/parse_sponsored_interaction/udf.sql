6a3b3b40f009f6edf2c7bd04849dddb30b74681b
kik-kik <kignasiak@mozilla.com>
2023-04-27 18:58:06 +0200

    small tweaks to parse_sponsoder_interaction and extract_string_from_bytes udfs

--- sql/moz-fx-data-shared-prod/udf_js/parse_sponsored_interaction/udf.sql
+++ sql/moz-fx-data-shared-prod/udf_js/parse_sponsored_interaction/udf.sql
@@ -2,22 +2,25 @@
 The function parses the `payload` column in `moz-fx-data-shared-prod.payload_bytes_error.contextual_services`
 to extract the `source` column
 */
-CREATE OR REPLACE FUNCTION  udf_js.parse_sponsored_interaction(params string)
-returns struct<
-  source string,
-  formFactor string, 
-  scenario string,
-  interactionType string, 
-  contextId string, 
-  reportingUrl string, 
-  requestId string, 
-  submissionTimestamp timestamp,
-  parsedReportingUrl json,
-  originalDocType string,
-  originalNamespace string,
-  interactionCount integer,
-  flaggedFraud bool> 
-LANGUAGE js AS """
+CREATE OR REPLACE FUNCTION udf_js.parse_sponsored_interaction(params string)
+RETURNS STRUCT<
+  source STRING,
+  formFactor STRING,
+  scenario STRING,
+  interactionType STRING,
+  contextId STRING,
+  reportingUrl STRING,
+  requestId STRING,
+  submissionTimestamp TIMESTAMP,
+  parsedReportingUrl JSON,
+  originalDocType STRING,
+  originalNamespace STRING,
+  interactionCount INTEGER,
+  flaggedFraud BOOLEAN
+>
+LANGUAGE js
+AS
+  """
   const parseURL = (url) => {
     const [base, params] = url.split("?");
     const [scheme, _, host, ...path] = base.split("/")
@@ -49,7 +52,7 @@ LANGUAGE js AS """
         return acc;
       }, {});
 
-    
+
     if (interaction.parsedReportingUrl?.params?.impressions) {
       interaction.source = "topsites";
       interaction.formFactor = interaction.parsedReportingUrl.params.form_factor;
@@ -63,9 +66,9 @@ LANGUAGE js AS """
     }
 
     interaction.flaggedFraud = (
-      interaction.reportingUrl.includes("click-status=65") 
+      interaction.reportingUrl.includes("click-status=65")
       || interaction.reportingUrl.includes("custom-data=invalid"));
-     
+
     return interaction;
   }
   catch(err) {
@@ -75,7 +78,7 @@ LANGUAGE js AS """
 
 WITH events AS (
   SELECT AS VALUE
-   "source\u003dtopsites, formFactor\u003ddesktop, scenario\u003dnull, interactionType\u003dclick, contextId\u003d{10679079-b1cd-45a3-9e40-cdfb364d3476}, reportingUrl\u003dhttps://bridge.sfo1.ap01.net/ctp?ci\u003d1681139740815.12791\u0026country-code\u003dDE\u0026ctag\u003dpd_sl_08aeb79c14ac3da0f8e9116cdcb0afadec2e24da616da802ba033bf6\u0026dma-code\u003d\u0026form-factor\u003ddesktop\u0026key\u003d1681139740400900002.1\u0026os-family\u003dWindows\u0026product-version\u003dfirefox_111\u0026region-code\u003dNW\u0026version\u003d16.0.0, requestId\u003dnull, submissionTimestamp\u003d2023-04-10T15:41:55Z, originalDocType\u003dtopsites-click, originalNamespace\u003dcontextual-services"
+    "source\u003dtopsites, formFactor\u003ddesktop, scenario\u003dnull, interactionType\u003dclick, contextId\u003d{10679079-b1cd-45a3-9e40-cdfb364d3476}, reportingUrl\u003dhttps://bridge.sfo1.ap01.net/ctp?ci\u003d1681139740815.12791\u0026country-code\u003dDE\u0026ctag\u003dpd_sl_08aeb79c14ac3da0f8e9116cdcb0afadec2e24da616da802ba033bf6\u0026dma-code\u003d\u0026form-factor\u003ddesktop\u0026key\u003d1681139740400900002.1\u0026os-family\u003dWindows\u0026product-version\u003dfirefox_111\u0026region-code\u003dNW\u0026version\u003d16.0.0, requestId\u003dnull, submissionTimestamp\u003d2023-04-10T15:41:55Z, originalDocType\u003dtopsites-click, originalNamespace\u003dcontextual-services"
 ),
 extracted AS (
   SELECT
@@ -83,20 +86,27 @@ extracted AS (
   FROM
     events
 )
-    --
 SELECT
-  assert.array_equals("topsites",e.source),
-  assert.array_equals("desktop",e.formFactor),
-  assert.array_equals(null,e.scenario),
-  assert.array_equals("click",e.interactionType),
-  assert.array_equals("{10679079-b1cd-45a3-9e40-cdfb364d3476}",e.contextId),
-  assert.array_equals("https://bridge.sfo1.ap01.net/ctp?ci=1681139740815.12791&country-code=DE&ctag=pd_sl_08aeb79c14ac3da0f8e9116cdcb0afadec2e24da616da802ba033bf6&dma-code=&form-factor=desktop&key=1681139740400900002.1&os-family=Windows&product-version=firefox_111&region-code=NW&version=16.0.0",e.reportingUrl),
-  assert.array_equals(null,e.requestId),
-  assert.array_equals("2023-04-10 15:41:55 UTC",e.submissionTimestamp),
-  assert.array_equals("topsites-click",e.originalDocType),
-  assert.array_equals("contextual-services",e.originalNamespace),
-  assert.array_equals(1,e.interactionCount),
-  assert.array_equals(false,e.flaggedFraud),
-  assert.array_equals(JSON('{\"host\":\"bridge.sfo1.ap01.net\",\"params\":{\"ci\":\"1681139740815.12791\",\"country_code\":\"DE\",\"ctag\":\"pd_sl_08aeb79c14ac3da0f8e9116cdcb0afadec2e24da616da802ba033bf6\",\"dma_code\":\"\",\"form_factor\":\"desktop\",\"key\":\"1681139740400900002.1\",\"os_family\":\"Windows\",\"product_version\":\"firefox_111\",\"region_code\":\"NW\",\"version\":\"16.0.0\"},\"path\":\"ctp\",\"scheme\":\"https:\"}'), e.parsedReportingUrl)
+  assert.array_equals("topsites", e.source),
+  assert.array_equals("desktop", e.formFactor),
+  assert.null(e.scenario),
+  assert.array_equals("click", e.interactionType),
+  assert.array_equals("{10679079-b1cd-45a3-9e40-cdfb364d3476}", e.contextId),
+  assert.array_equals(
+    "https://bridge.sfo1.ap01.net/ctp?ci=1681139740815.12791&country-code=DE&ctag=pd_sl_08aeb79c14ac3da0f8e9116cdcb0afadec2e24da616da802ba033bf6&dma-code=&form-factor=desktop&key=1681139740400900002.1&os-family=Windows&product-version=firefox_111&region-code=NW&version=16.0.0",
+    e.reportingUrl
+  ),
+  assert.null(e.requestId),
+  assert.array_equals("2023-04-10 15:41:55 UTC", e.submissionTimestamp),
+  assert.array_equals("topsites-click", e.originalDocType),
+  assert.array_equals("contextual-services", e.originalNamespace),
+  assert.array_equals(1, e.interactionCount),
+  assert.array_equals(FALSE, e.flaggedFraud),
+  assert.array_equals(
+    JSON(
+      '{\"host\":\"bridge.sfo1.ap01.net\",\"params\":{\"ci\":\"1681139740815.12791\",\"country_code\":\"DE\",\"ctag\":\"pd_sl_08aeb79c14ac3da0f8e9116cdcb0afadec2e24da616da802ba033bf6\",\"dma_code\":\"\",\"form_factor\":\"desktop\",\"key\":\"1681139740400900002.1\",\"os_family\":\"Windows\",\"product_version\":\"firefox_111\",\"region_code\":\"NW\",\"version\":\"16.0.0\"},\"path\":\"ctp\",\"scheme\":\"https:\"}'
+    ),
+    e.parsedReportingUrl
+  )
 FROM
   extracted

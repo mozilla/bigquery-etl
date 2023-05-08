70fba5d57676c9d781722373e4ab6d66d713d904
Lucia Vargas <lvargas@mozilla.com>
2023-05-08 16:31:54 +0200

    DENG-847. Temporary comment to test change control.

--- sql_generators/active_users/templates/view.sql
+++ sql_generators/active_users/templates/view.sql
@@ -1,4 +1,5 @@
 --- User-facing view. Generated via sql_generators.active_users_aggregates.
+--- Remove the LIMIT. It's only used for change-control testing purposes.
 CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ app_name }}.active_users_aggregates`
 AS
@@ -13,3 +14,4 @@ SELECT
   `mozfun.norm.os`(os) AS os_grouped
 FROM
   `{{ project_id }}.{{ app_name }}_derived.active_users_aggregates_v1`
+LIMIT 1000

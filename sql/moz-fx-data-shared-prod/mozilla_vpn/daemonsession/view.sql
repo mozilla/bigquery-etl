c58ae1c54093d321da41ccecb8110d087e743db8
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-05-12 05:17:54 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/mozilla_vpn/daemonsession/view.sql
@@ -0,0 +1,60 @@
+-- Generated via ./bqetl generate glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozilla_vpn.daemonsession`
+AS
+SELECT
+  "mozillavpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.mozillavpn.daemonsession`
+UNION ALL
+SELECT
+  "org_mozilla_firefox_vpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_vpn.daemonsession`
+UNION ALL
+SELECT
+  "org_mozilla_ios_firefoxvpn" AS normalized_app_id,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_channel,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn.daemonsession`

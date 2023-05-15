6389e76453a3150a4ecc12dff23a19d20cc9662b
kik-kik <kignasiak@mozilla.com>
2023-05-15 15:07:57 +0200

    fixing attributable_clients view to allow for deploy_stage ci step to pass

--- sql/moz-fx-data-shared-prod/firefox_ios/attributable_clients/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios/attributable_clients/view.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW
-  firefox_ios.attributable_clients
+  `moz-fx-data-shared-prod.firefox_ios.attributable_clients`
 AS
 SELECT
   *
 FROM
-  firefox_ios_derived.attributable_clients_v1
+  `moz-fx-data-shared-prod.firefox_ios_derived.attributable_clients_v1`

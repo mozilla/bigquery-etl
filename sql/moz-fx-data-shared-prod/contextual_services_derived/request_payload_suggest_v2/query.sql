c717775cdfc7c08f8a9f8e4a9f60fa696a973fc9
Alekhya Kommasani <akommasani@mozilla.com>
2023-05-18 12:24:11 -0400

    fix format

--- sql/moz-fx-data-shared-prod/contextual_services_derived/request_payload_suggest_v2/query.sql
+++ sql/moz-fx-data-shared-prod/contextual_services_derived/request_payload_suggest_v2/query.sql
@@ -4,39 +4,42 @@ WITH parsed AS (
     udf_js.parse_sponsored_interaction(udf_js.extract_string_from_bytes(payload)) AS si
   FROM
     `moz-fx-data-shared-prod.payload_bytes_error.contextual_services`
- 
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND error_type = 'SendRequest'
 ),
-ping_data as (
-  SELECT 
-    distinct context_id,
-    "impression" as interaction_type,
-    metadata.geo.country as country_code,
-    metadata.geo.subdivision1 as region_code,
-    metadata.user_agent.os as os_family,
-    metadata.user_agent.version as product_version,
-  FROM `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_impression_v1` 
-  WHERE DATE(submission_timestamp) = @submission_date
-  AND advertiser != "wikipedia"
-  AND reporting_url IS NOT NULL
-UNION ALL 
-  SELECT
-    distinct context_id,
-    "click" as interaction_type,
-    metadata.geo.country as country_code,
-    metadata.geo.subdivision1 as region_code,
-    metadata.user_agent.os as os_family,
-    metadata.user_agent.version as product_version,
-  FROM `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_click_v1`
-  WHERE DATE(submission_timestamp) = @submission_date
-  AND advertiser != "wikipedia"
-  AND reporting_url IS NOT NULL
+ping_data AS (
+  SELECT DISTINCT
+    context_id,
+    "impression" AS interaction_type,
+    metadata.geo.country AS country_code,
+    metadata.geo.subdivision1 AS region_code,
+    metadata.user_agent.os AS os_family,
+    metadata.user_agent.version AS product_version,
+  FROM
+    `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_impression_v1`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND advertiser != "wikipedia"
+    AND reporting_url IS NOT NULL
+  UNION ALL
+  SELECT DISTINCT
+    context_id,
+    "click" AS interaction_type,
+    metadata.geo.country AS country_code,
+    metadata.geo.subdivision1 AS region_code,
+    metadata.user_agent.os AS os_family,
+    metadata.user_agent.version AS product_version,
+  FROM
+    `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_click_v1`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND advertiser != "wikipedia"
+    AND reporting_url IS NOT NULL
 ),
-quicksuggest as (
+quicksuggest AS (
   SELECT
-    si.contextId as context_id,
+    si.contextId AS context_id,
     si.interactionType AS interaction_type,
     si.formFactor AS form_factor,
     si.flaggedFraud AS flagged_fraud,
@@ -46,7 +49,7 @@ quicksuggest as (
   FROM
     parsed
   WHERE
-    si.source = 'quicksuggest'  
+    si.source = 'quicksuggest'
 )
 SELECT
   form_factor,
@@ -60,5 +63,8 @@ SELECT
   click_count
 FROM
   quicksuggest qs
-left join ping_data pings
-  on qs.context_id = pings.context_id and qs.interaction_type = pings.interaction_type
+LEFT JOIN
+  ping_data pings
+ON
+  qs.context_id = pings.context_id
+  AND qs.interaction_type = pings.interaction_type

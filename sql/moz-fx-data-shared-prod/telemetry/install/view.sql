8f89cc8805298019846b92492ff30d553f8bc2da
Glenda Leonard <gleonard@mozilla.com>
2023-05-24 14:31:16 -0400

    Added `moz-fx-data-shared-prod` prefix to udf_js.decode_uri_attribution

--- sql/moz-fx-data-shared-prod/telemetry/install/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/install/view.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod`.telemetry.install
 AS
 SELECT
-  * REPLACE (udf_js.decode_uri_attribution(attribution) AS attribution)
+  * REPLACE (`moz-fx-data-shared-prod`.udf_js.decode_uri_attribution(attribution) AS attribution)
 FROM
   `moz-fx-data-shared-prod.firefox_installer.install`

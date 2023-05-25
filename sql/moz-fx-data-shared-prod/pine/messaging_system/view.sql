a3857a9244b5f8c66167e781a62c052fa1359437
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-05-25 05:18:34 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/pine/messaging_system/view.sql
@@ -0,0 +1,11 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.pine.messaging_system`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info
+  )
+FROM
+  `moz-fx-data-shared-prod.pine_stable.messaging_system_v1`

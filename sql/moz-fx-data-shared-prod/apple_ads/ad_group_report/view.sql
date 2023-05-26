46daa24670822eaea0ce82a6ae3fcbefccb9a4bd
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-05-26 17:32:19 +0200

    feat(DENG-789): making apple ads data accessible (#3847)
    * added apple_ads_derived for copying over apple_ad data from the fivetran dataset, and apple_ads views now read from it
    
    * added bqetl_fivetran_apple_ads.py DAG responsible for copying apple_ads data from the fivetran project over to moz-fx-data-shared-prod
    
    * now dryrun skips apple_ads_derived instead of apple_ads as the query now accesses restricted dataset
    
    * added schema files for apple_ads_derived datasets
    
    * added descriptions to schema.yaml files for apple_ads_derived namespace
    
    * added dataset_metadata for apple_ads_derived to include a link to the dbt transformations
    
    * fixed apple_ads view definitions
    
    * removed application label and referenced_tables section inside metadata.yaml for apple_ads as requsted by srose in PR#3847
    
    * corrected source project for apple_ads views
    
    * renamed apple_ads_derived to apple_ads_external
    
    * added * to apple_ads_external namespace name to skip in the dryrun due to integration test deployment
    
    * made tweaks to apple_ads and apple_ads_external datasets/namespaces as requested by whd
    
    * updated apple_ads_external skip rule to the way it is meant to be defined, this will work once a fix is rolled out for dryrun
    
    * fixed dag bqetl_fivetran_apple_ads description and updated the schedule to run once a day

--- sql/moz-fx-data-shared-prod/apple_ads/ad_group_report/view.sql
+++ sql/moz-fx-data-shared-prod/apple_ads/ad_group_report/view.sql
@@ -1,9 +1,7 @@
--- This is an authorized view that allows us to read Apple Ads data
--- from a Fivetran project.
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.apple_ads.ad_group_report`
 AS
 SELECT
   *
 FROM
-  `moz-fx-data-bq-fivetran.dbt_fivetran_transformation_apple_search_ads.apple_search_ads__ad_group_report`
+  `moz-fx-data-shared-prod.apple_ads_external.ad_group_report_v1`

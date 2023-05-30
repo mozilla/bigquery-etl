d1a9909074c83396e4c5647e9052a5c0602e0411
Eduardo Filho <edugomfilho@gmail.com>
2023-05-30 18:01:09 -0400

    glam: Partition clients_histogram_aggregates by sample_id (has been running like this since April 3 from a different branch)

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/glam_sample_counts_v1/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1`
+(
+  os STRING,
+  app_version INT64,
+  app_build_id STRING,
+  channel STRING,
+  metric STRING,
+  process STRING,
+  key STRING,
+  agg_type STRING,
+  total_sample FLOAT64
+);

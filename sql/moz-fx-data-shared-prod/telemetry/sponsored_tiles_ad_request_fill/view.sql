9bc95a0cb24c7287990303dd9129f081e5135a9d
skahmann3 <16420065+skahmann3@users.noreply.github.com>
2023-05-31 14:02:40 -0700

    Create table

---
+++ sql/moz-fx-data-shared-prod/telemetry/sponsored_tiles_ad_request_fill/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.sponsored_tiles_ad_request_fill`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.sponsored_tiles_ad_request_fill_v1`

2a63dcd561abe0e75701cf94a6a46c5106002e2b
Alexander <alexj.nich@hotmail.com>
2023-06-01 15:46:45 -0400

    Revert "feat(DENG-980): fxa_users_services_first_seen view updated to use v2 (#3870)"
    This reverts commit 058ab9d98ad4a3045820ae13165318dcf53cc6c8.

--- sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_services_first_seen/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_services_first_seen/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_services_first_seen_v2`
+  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_services_first_seen_v1`

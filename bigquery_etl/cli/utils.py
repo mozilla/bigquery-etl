cd33dda9bc4b50986b5413a1612c3bd29e38da5d
wwyc <wichan@mozilla.com>
2023-06-09 11:37:51 -0700

    Refractored backfill cli commands

--- bigquery_etl/cli/utils.py
+++ bigquery_etl/cli/utils.py
@@ -3,15 +3,19 @@
 import fnmatch
 import os
 import re
+import sys
+from collections import defaultdict
 from fnmatch import fnmatchcase
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 
 import click
 from google.auth.exceptions import DefaultCredentialsError
 from google.cloud import bigquery
 
 from bigquery_etl.util.common import TempDatasetReference, project_dirs
+
+from ..backfill.parse import BACKFILL_FILE, Backfill
 
 QUERY_FILE_RE = re.compile(
     r"^.*/([a-zA-Z0-9-]+)/([a-zA-Z0-9_]+)/([a-zA-Z0-9_]+(_v[0-9]+)?)/"
@@ -136,6 +140,43 @@ def qualified_table_name_matching(qualified_table_name) -> Tuple[str, str, str]:
         )
 
     return (project_id, dataset_id, table_id)
+
+
+def backfills_matching_name_pattern(
+    sql_dir, project_id, qualified_table_name
+) -> defaultdict[Path, List[Backfill]]:
+    """Return backfills with name pattern."""
+    backfills_dict = defaultdict(list)
+
+    table_id = None
+
+    if qualified_table_name:
+        try:
+            (project_id, dataset_id, table_id) = qualified_table_name_matching(
+                qualified_table_name
+            )
+
+            path = Path(sql_dir)
+            query_path = path / project_id / dataset_id / table_id
+
+            if not query_path.exists():
+                click.echo(f"{project_id}.{dataset_id}.{table_id}" + " does not exist")
+                sys.exit(1)
+
+        except AttributeError as e:
+            click.echo(e)
+            sys.exit(1)
+
+    backfill_files = paths_matching_name_pattern(
+        table_id, sql_dir, project_id, [BACKFILL_FILE]
+    )
+
+    for file in backfill_files:
+        backfills = Backfill.entries_from_file(file)
+        if backfills:
+            backfills_dict[file].extend(backfills)
+
+    return backfills_dict
 
 
 sql_dir_option = click.option(

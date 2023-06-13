dcb609c1e7024e41aefc75fea9f0691d793c4ac9
Eduardo Filho <edugomfilho@gmail.com>
2023-06-13 19:46:58 -0400

    remove init.sql to prevent ci from trying to derive schema from it and break

--- sql/moz-fx-data-shared-prod/telemetry_derived/glam_user_counts_v1/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `moz-fx-data-shared-prod.telemetry_derived.glam_user_counts_v1`
-(
-  os STRING,
-  app_version INT64,
-  app_build_id STRING,
-  channel STRING,
-  total_users INT64
-);

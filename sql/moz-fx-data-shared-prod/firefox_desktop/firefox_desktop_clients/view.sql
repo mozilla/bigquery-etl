619e9dbe9a4530283fdea390183c0feaeebced87
Lucia Vargas <lvargas@mozilla.com>
2023-06-15 20:10:38 +0200

    DS-2947. Update dataset name to clients_first_seen_v2.

--- sql/moz-fx-data-shared-prod/firefox_desktop/firefox_desktop_clients/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_desktop.firefox_desktop_clients`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_desktop_derived.firefox_desktop_clients_v1`

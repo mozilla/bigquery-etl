5b10ab7a3f32feba1a08b1b21d5db26f560f1e48
kik-kik <kignasiak@mozilla.com>
2023-06-15 16:19:23 +0200

    added AS as requested by srose

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_server_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_server_events_v1/query.sql
@@ -12,7 +12,7 @@ SELECT
         )
     ) AS jsonPayload
   ),
-  SPLIT(jsonPayload.logger, "-")[OFFSET(1)] fxa_log,
+  SPLIT(jsonPayload.logger, "-")[OFFSET(1)] AS fxa_log,
 FROM
   `moz-fx-fxa-nonprod.gke_fxa_stage_log.stdout`
 WHERE

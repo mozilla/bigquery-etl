5f36a02db2c3842f861c8a97f19e32aa8ff9dccb
kik-kik <kignasiak@mozilla.com>
2023-06-19 14:13:40 +0200

    updated nonprod_fxa queries and schema for fxa_server_events_v1 as requested by srose, this query also pulls data for stout which now has end date

--- sql/moz-fx-data-shared-prod/firefox_accounts/nonprod_fxa_all_events/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/nonprod_fxa_all_events/view.sql
@@ -62,8 +62,11 @@ stdout_events AS (
     "payments" AS fxa_log
   FROM
     `moz-fx-data-shared-prod.firefox_accounts_derived.nonprod_fxa_stdout_events_v1`
+  WHERE
+    DATE(`timestamp`) <= "2023-05-26"
 ),
 -- New fxa event table (nonprod) includes, content and auth events
+-- this also includes "payments" events.
 server_events AS (
   SELECT
     `timestamp`,
--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_server_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_server_events_v1/query.sql
@@ -20,3 +20,28 @@ WHERE
   AND jsonPayload.logger IS NOT NULL
   AND jsonPayload.fields.event_type IS NOT NULL
   AND DATE(`timestamp`) = @submission_date
+UNION ALL
+-- Pulling data from the old stdout table too as some of the GCP
+-- fxa events are still landing inside it.
+SELECT
+  * REPLACE (
+    (
+      SELECT AS STRUCT
+        jsonPayload.* REPLACE (
+          (
+            SELECT AS STRUCT
+              jsonPayload.fields.* EXCEPT (device_id, user_id),
+              TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
+              TO_HEX(SHA256(jsonPayload.fields.device_id)) AS device_id
+          ) AS fields
+        )
+    ) AS jsonPayload
+  ),
+  SPLIT(jsonPayload.logger, "-")[OFFSET(1)] AS fxa_log,
+FROM
+  `moz-fx-fxa-nonprod-375e.fxa_stage_logs.stdout`
+WHERE
+  jsonPayload.type = 'amplitudeEvent'
+  AND jsonPayload.logger IS NOT NULL
+  AND jsonPayload.fields.event_type IS NOT NULL
+  AND DATE(`timestamp`) = @submission_date

1999653944d69f448313fe76e5b866e1e5cc9b2b
Alekhya Kommasani <akommasani@mozilla.com>
2023-06-20 21:53:49 -0400

    Generate dags to include bigquery_dq_check

--- dags/bqetl_download_funnel_attribution.py
+++ dags/bqetl_download_funnel_attribution.py
@@ -6,7 +6,7 @@ from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.utils.task_group import TaskGroup
 import datetime
 from utils.constants import ALLOWED_STATES, FAILED_STATES
-from utils.gcp import bigquery_etl_query, gke_command
+from utils.gcp import bigquery_etl_query, gke_command, bigquery_dq_check
 
 docs = """
 ### bqetl_download_funnel_attribution
@@ -43,6 +43,17 @@ with DAG(
     doc_md=docs,
     tags=tags,
 ) as dag:
+    checks__ga_derived__downloads_with_attribution__v2 = bigquery_dq_check(
+        task_id="checks__ga_derived__downloads_with_attribution__v2",
+        destination_table='downloads_with_attribution_v2${{ macros.ds_format(macros.ds_add(ds, -1), "%Y-%m-%d", "%Y%m%d") }}',
+        dataset_id="ga_derived",
+        project_id="moz-fx-data-marketing-prod",
+        owner="gleonard@mozilla.com",
+        email=["gleonard@mozilla.com", "telemetry-alerts@mozilla.com"],
+        depends_on_past=False,
+        sql_file_path="sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/checks.sql",
+    )
+
     ga_derived__downloads_with_attribution__v2 = bigquery_etl_query(
         task_id="ga_derived__downloads_with_attribution__v2",
         destination_table='downloads_with_attribution_v2${{ macros.ds_format(macros.ds_add(ds, -1), "%Y-%m-%d", "%Y%m%d") }}',
@@ -55,6 +66,9 @@ with DAG(
         parameters=["download_date:DATE:{{macros.ds_add(ds, -1)}}"],
     )
 
+    checks__ga_derived__downloads_with_attribution__v2.set_upstream(
+        ga_derived__downloads_with_attribution__v2
+    )
     wait_for_ga_derived__www_site_empty_check__v1 = ExternalTaskSensor(
         task_id="wait_for_ga_derived__www_site_empty_check__v1",
         external_dag_id="bqetl_google_analytics_derived",
@@ -64,6 +78,10 @@ with DAG(
         allowed_states=ALLOWED_STATES,
         failed_states=FAILED_STATES,
         pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    checks__ga_derived__downloads_with_attribution__v2.set_upstream(
+        wait_for_ga_derived__www_site_empty_check__v1
     )
 
     ga_derived__downloads_with_attribution__v2.set_upstream(

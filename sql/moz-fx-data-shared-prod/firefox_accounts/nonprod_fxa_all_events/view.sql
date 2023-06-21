0ee45a1e0a28f7373afab90e9943269d1982f57f
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-06-21 12:09:28 +0200

    feat(DENG-722): preparing for fxa AWS to GCP migration (nonprod) (#3882)
    * added a new table for the new nonprod fxa backend events and a fxa_all_events_nonprod view to simulate the process we will need to follow for prod
    
    * added date filters to the nonprod_fxa_all_events view as requested by akkomar and updated the metadata
    
    * added the new nonprod_fxa_server_events_v1 table to dry run skip due to permissions
    
    * improved the comment about deleting a view as requested by akkomar
    
    * tweaked date filtering as requested by srose
    
    * pulled nonprod_fxa schema from DENG-1006-fxa-log-fields
    
    * added schema.yaml for nonprod_fxa_server_events_v1
    
    * deleted init.sql and added clustering config to metadata.yaml instead
    
    * added AS as requested by srose
    
    * fixed yaml lint errors
    
    * added the ability to pass end_date param into Airflow task
    
    * updated nonprod_fxa queries and schema for fxa_server_events_v1 as requested by srose, this query also pulls data for stout which now has end date
    
    * regenerated bqetl_fxa_events DAG
    
    * renamed fxa_log to fxa_server as agreed on with srose
    
    * reverted merging of the stdout and server event etls due to incompatible schemas
    
    * removed changes related to task level end_date
    
    * removed date filter for stdout events
    
    * undoing test changes
    
    * added country to fxa_server_events_v1 schema
    
    * tweaked selected ordering as requested by srose and updated comments and metadata.yaml

--- sql/moz-fx-data-shared-prod/firefox_accounts/nonprod_fxa_all_events/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/nonprod_fxa_all_events/view.sql
@@ -1,84 +1,127 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_accounts.nonprod_fxa_content_auth_stdout_events`
+  `moz-fx-data-shared-prod.firefox_accounts.nonprod_fxa_all_events`
 AS
-  -- TODO: we should look into deleting this view
-  -- as we should use firefox_accounts.nonprod_fxa_all_events instead
-  -- to access nonprod fxa event data.
-  -- Keeping this around until we can ensure all downstream
-  -- tools are updated to use the new view (see: DENG-627).
-WITH content AS (
+-- Existing fxa event tables (stage)
+WITH auth_events AS (
   SELECT
-    jsonPayload.logger,
-    jsonPayload.fields.event_type,
+    "auth" AS fxa_server,
+    `timestamp`,
+    receiveTimestamp,
+    jsonPayload.fields.user_id,
+    jsonPayload.fields.country,
+    jsonPayload.fields.language,
     jsonPayload.fields.app_version,
     jsonPayload.fields.os_name,
     jsonPayload.fields.os_version,
-    jsonPayload.fields.country,
-    jsonPayload.fields.language,
-    jsonPayload.fields.user_id,
-    jsonPayload.fields.user_properties,
-    jsonPayload.fields.event_properties,
-    `timestamp`,
-    receiveTimestamp
-  FROM
-    `moz-fx-data-shared-prod.firefox_accounts_derived.nonprod_fxa_content_events_v1`
-),
-  --
-auth AS (
-  SELECT
-    jsonPayload.logger,
     jsonPayload.fields.event_type,
-    jsonPayload.fields.app_version,
-    jsonPayload.fields.os_name,
-    jsonPayload.fields.os_version,
-    jsonPayload.fields.country,
-    jsonPayload.fields.language,
-    jsonPayload.fields.user_id,
+    jsonPayload.logger,
     jsonPayload.fields.user_properties,
     jsonPayload.fields.event_properties,
-    `timestamp`,
-    receiveTimestamp
+    jsonPayload.fields.device_id,
   FROM
     `moz-fx-data-shared-prod.firefox_accounts_derived.nonprod_fxa_auth_events_v1`
+  WHERE
+    DATE(`timestamp`) <= "2023-05-26"
 ),
-  --
-stdout AS (
+content_events AS (
   SELECT
-    jsonPayload.logger,
+    "content" AS fxa_server,
+    `timestamp`,
+    receiveTimestamp,
+    jsonPayload.fields.user_id,
+    CAST(NULL AS STRING) AS country,
+    jsonPayload.fields.language,
+    jsonPayload.fields.app_version,
+    CAST(NULL AS STRING) AS os_name,
+    CAST(NULL AS STRING) AS os_version,
     jsonPayload.fields.event_type,
+    jsonPayload.logger,
+    jsonPayload.fields.user_properties,
+    jsonPayload.fields.event_properties,
+    CAST(NULL AS STRING) AS device_id,
+  FROM
+    `moz-fx-data-shared-prod.firefox_accounts_derived.nonprod_fxa_content_events_v1`
+  WHERE
+    DATE(`timestamp`) <= "2023-05-26"
+),
+stdout_events AS (
+  SELECT
+    "payments" AS fxa_server,
+    `timestamp`,
+    receiveTimestamp,
+    jsonPayload.fields.user_id,
+    CAST(NULL AS STRING) AS country,
+    jsonPayload.fields.language,
     jsonPayload.fields.app_version,
     jsonPayload.fields.os_name,
     jsonPayload.fields.os_version,
-    CAST(NULL AS STRING) AS country,
-    CAST(NULL AS STRING) AS language,
-    jsonPayload.fields.user_id,
+    jsonPayload.fields.event_type,
+    jsonPayload.logger,
     jsonPayload.fields.user_properties,
     jsonPayload.fields.event_properties,
-    `timestamp`,
-    receiveTimestamp
+    jsonPayload.fields.device_id,
   FROM
     `moz-fx-data-shared-prod.firefox_accounts_derived.nonprod_fxa_stdout_events_v1`
 ),
-  --
+-- New fxa event table (nonprod) includes, content and auth events
+server_events AS (
+  SELECT
+    fxa_server,
+    `timestamp`,
+    receiveTimestamp,
+    jsonPayload.fields.user_id,
+    jsonPayload.fields.country,
+    jsonPayload.fields.language,
+    jsonPayload.fields.app_version,
+    jsonPayload.fields.os_name,
+    jsonPayload.fields.os_version,
+    jsonPayload.fields.event_type,
+    jsonPayload.logger,
+    jsonPayload.fields.user_properties,
+    jsonPayload.fields.event_properties,
+    jsonPayload.fields.device_id,
+  FROM
+    `moz-fx-data-shared-prod.firefox_accounts_derived.nonprod_fxa_server_events_v1`
+  WHERE
+    DATE(`timestamp`) >= "2023-05-26"
+),
 unioned AS (
   SELECT
     *
   FROM
-    auth
+    auth_events
   UNION ALL
   SELECT
     *
   FROM
-    content
+    content_events
   UNION ALL
   SELECT
     *
   FROM
-    stdout
+    stdout_events
+  UNION ALL
+  SELECT
+    *
+  FROM
+    server_events
 )
-  --
 SELECT
-  * EXCEPT (user_properties, event_properties),
+  fxa_server,
+  `timestamp`,
+  receiveTimestamp,
+  logger,
+  event_type,
+  user_id,
+  device_id,
+  country,
+  `language`,
+  app_version,
+  os_name,
+  os_version,
+  user_properties,
+  event_properties,
+  -- extract user properties
   JSON_VALUE(user_properties, "$.utm_term") AS utm_term,
   JSON_VALUE(user_properties, "$.utm_source") AS utm_source,
   JSON_VALUE(user_properties, "$.utm_medium") AS utm_medium,
@@ -90,16 +133,17 @@ SELECT
   JSON_VALUE(user_properties, "$.entrypoint_experiment") AS entrypoint_experiment,
   JSON_VALUE(user_properties, "$.entrypoint_variation") AS entrypoint_variation,
   JSON_VALUE(user_properties, "$.flow_id") AS flow_id,
+  JSON_VALUE(user_properties, "$.sync_device_count") AS sync_device_count,
+  JSON_VALUE(user_properties, "$.sync_active_devices_day") AS sync_active_devices_day,
+  JSON_VALUE(user_properties, "$.sync_active_devices_week") AS sync_active_devices_week,
+  JSON_VALUE(user_properties, "$.sync_active_devices_month") AS sync_active_devices_month,
+  -- extract event properties
   JSON_VALUE(event_properties, "$.service") AS service,
   JSON_VALUE(event_properties, "$.email_type") AS email_type,
   JSON_VALUE(event_properties, "$.email_provider") AS email_provider,
   JSON_VALUE(event_properties, "$.oauth_client_id") AS oauth_client_id,
   JSON_VALUE(event_properties, "$.connect_device_flow") AS connect_device_flow,
   JSON_VALUE(event_properties, "$.connect_device_os") AS connect_device_os,
-  JSON_VALUE(user_properties, "$.sync_device_count") AS sync_device_count,
-  JSON_VALUE(user_properties, "$.sync_active_devices_day") AS sync_active_devices_day,
-  JSON_VALUE(user_properties, "$.sync_active_devices_week") AS sync_active_devices_week,
-  JSON_VALUE(user_properties, "$.sync_active_devices_month") AS sync_active_devices_month,
   JSON_VALUE(event_properties, "$.email_sender") AS email_sender,
   JSON_VALUE(event_properties, "$.email_service") AS email_service,
   JSON_VALUE(event_properties, "$.email_template") AS email_template,

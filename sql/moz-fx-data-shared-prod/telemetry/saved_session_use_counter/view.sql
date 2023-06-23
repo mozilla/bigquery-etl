28cdff50d35268164dd1de48a00372226d41c151
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-06-23 21:27:37 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry/saved_session_use_counter/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/saved_session_use_counter/view.sql
@@ -3,9 +3,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.saved_session_use_counter`
 AS
 SELECT
-  * REPLACE (
-    mozfun.norm.metadata(metadata) AS metadata,
-    `moz-fx-data-shared-prod`.udf.normalize_main_payload(payload) AS payload
-  )
+  * REPLACE (mozfun.norm.metadata(metadata) AS metadata)
 FROM
   `moz-fx-data-shared-prod.telemetry_stable.saved_session_use_counter_v4`

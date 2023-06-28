ca5b8810c45be94bc6e764033d6dddb029d5902c
Daniel Thorn <dthorn@mozilla.com>
2023-06-28 11:11:31 -0700

    DS-2642 - Add plan, product, and customer shipping info to stripe itemized report

--- sql/moz-fx-data-shared-prod/stripe/itemized_payout_reconciliation/view.sql
+++ sql/moz-fx-data-shared-prod/stripe/itemized_payout_reconciliation/view.sql
@@ -1,12 +1,15 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.stripe.itemized_payout_reconciliation`
 AS
-WITH customers AS (
+WITH customers_without_shipping_state AS (
   SELECT
-    id,
-    address_country AS country,
+    id AS customer_id,
+    NULLIF(address_country, "") AS country,
     NULLIF(UPPER(TRIM(address_postal_code)), "") AS postal_code,
     NULLIF(address_state, "") AS state,
+    NULLIF(shipping_address_country, "") AS shipping_address_country,
+    NULLIF(UPPER(TRIM(shipping_address_postal_code)), "") AS shipping_address_postal_code,
+    NULLIF(shipping_address_state, "") AS shipping_address_state,
   FROM
     `moz-fx-data-shared-prod`.stripe_external.customer_v1
 ),
@@ -16,14 +19,31 @@ postal_code_to_state AS (
     postal_code,
     IF(COUNT(DISTINCT state) > 1, NULL, ANY_VALUE(state)) AS state,
   FROM
-    customers
+    customers_without_shipping_state
   WHERE
     country IN ("US", "CA")
     AND postal_code IS NOT NULL
     AND state IS NOT NULL
+    AND LENGTH(state) = 2
   GROUP BY
     country,
     postal_code
+),
+customers AS (
+  SELECT
+    customers.* REPLACE (
+      COALESCE(
+        customers.shipping_address_state,
+        postal_code_to_state.state
+      ) AS shipping_address_state
+    )
+  FROM
+    customers_without_shipping_state AS customers
+  LEFT JOIN
+    postal_code_to_state
+  ON
+    customers.shipping_address_country = postal_code_to_state.country
+    AND customers.shipping_address_postal_code = postal_code_to_state.postal_code
 ),
 charge_states AS (
   SELECT
@@ -45,34 +65,96 @@ charge_states AS (
   WHERE
     cards.country IN ("US", "CA")
 ),
+subscriptions AS (
+  SELECT
+    subscription_id,
+    plan_id,
+    plan_name,
+    plan_interval,
+    plan_interval_count,
+    product_id,
+    product_name,
+  FROM
+    `moz-fx-data-shared-prod`.subscription_platform.stripe_subscriptions
+),
 enriched AS (
   SELECT
-    report.*,
-    charge_states.state,
-    charge_states.postal_code,
+    report.* EXCEPT (
+      card_country,
+      shipping_address_city,
+      shipping_address_country,
+      shipping_address_line1,
+      shipping_address_line2,
+      shipping_address_postal_code,
+      shipping_address_state
+    ),
+    {% for country, state, postal_code, dst_pre in [
+        (
+          "customers.shipping_address_country",
+          "customers.shipping_address_state",
+          "customers.shipping_address_postal_code",
+          "shipping_address"
+        ),
+        ("customers.country", "customers.state", "customers.postal_code", "address"),
+        ("card_country", "charge_states.state", "charge_states.postal_code", "card"),
+    ] -%}
+      CASE
+        -- American Samoa
+        WHEN `{{country}}` = "US"
+          AND REGEXP_CONTAINS({{postal_code}}, "^96799(-?[0-9]{4})?$")
+          THEN STRUCT("AS" AS `{{dst_pre}}_country`, NULL AS `{{dst_pre}}_state`)
+        -- Puerto Rico
+        WHEN `{{country}}` = "US"
+          AND REGEXP_CONTAINS({{postal_code}}, "^00[679][0-9]{2}(-?[0-9]{4})?$")
+          THEN STRUCT("PR" AS `{{dst_pre}}_country`, NULL AS `{{dst_pre}}_state`)
+        -- Virgin Islands
+        WHEN `{{country}}` = "US"
+          AND REGEXP_CONTAINS({{postal_code}}, "^008[0-9]{2}(-?[0-9]{4})?$")
+          THEN STRUCT("VI" AS `{{dst_pre}} country`, NULL AS `{{dst_pre}}_state`)
+        ELSE STRUCT({{country}} AS `{{dst_pre}}_country`, {{state}} AS `{{dst_pre}}_state`)
+      END.*,
+      {{postal_code}} AS `{{dst_pre}}_postal_code`,
+    {% endfor -%}
+    subscriptions.* EXCEPT (subscription_id),
   FROM
     `moz-fx-data-shared-prod`.stripe_external.itemized_payout_reconciliation_v5 AS report
   LEFT JOIN
     charge_states
   USING
     (charge_id, card_country)
+  LEFT JOIN
+    customers
+  USING
+    (customer_id)
+  LEFT JOIN
+    subscriptions
+  USING
+    (subscription_id)
 )
 SELECT
-  * EXCEPT (card_country, state),
+  *,
+  -- Use the same address hierarchy as Stripe Tax after we enabled Stripe Tax (FXA-5457).
+  -- https://stripe.com/docs/tax/customer-locations#address-hierarchy
+  -- (customer shipping address, customer billing address, payment method billing address)
   CASE
-    -- american samoa
-    WHEN card_country = "US"
-      AND REGEXP_CONTAINS(postal_code, "^96799(-?[0-9]{4})?$")
-      THEN STRUCT("AS" AS card_country, NULL AS state)
-    -- puerto rico
-    WHEN card_country = "US"
-      AND REGEXP_CONTAINS(postal_code, "^00[679][0-9]{2}(-?[0-9]{4})?$")
-      THEN STRUCT("PR" AS card_country, NULL AS state)
-    -- virgin islands
-    WHEN card_country = "US"
-      AND REGEXP_CONTAINS(postal_code, "^008[0-9]{2}(-?[0-9]{4})?$")
-      THEN STRUCT("VI" AS card_country, NULL AS state)
-    ELSE STRUCT(card_country, state)
+    WHEN shipping_address_country IS NOT NULL
+      THEN STRUCT(
+          shipping_address_country AS tax_country,
+          shipping_address_state AS tax_state,
+          shipping_address_postal_code AS tax_postal_code
+        )
+    WHEN address_country IS NOT NULL
+      THEN STRUCT(
+          address_country AS tax_country,
+          address_state AS tax_state,
+          address_postal_code AS tax_postal_code
+        )
+    WHEN card_country IS NOT NULL
+      THEN STRUCT(
+          card_country AS tax_country,
+          card_state AS tax_state,
+          card_postal_code AS tax_postal_code
+        )
   END.*,
 FROM
   enriched

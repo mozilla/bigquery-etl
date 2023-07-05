a33cc2e695921e8dbda58ff85c78547b303727d7
Marlene-M-Hirose <mhirose@mozilla.com>
2023-07-05 12:39:58 -1000

    add DAG for adjust_derived

---
+++ dags/bqetl_adjust_derived.py
@@ -0,0 +1,52 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, gke_command, bigquery_dq_check
+
+docs = """
+
+Derived tables built on Adjust data downloaded from https://api.adjust.com/kpis/v1/<app_token>".
+app_tokens are stored in the Airflow variable ADJUST_APP_TOKEN_LIST
+#### Owner
+
+mhirose@mozilla.com
+"""
+
+
+default_args = {
+    "owner": "mhirose@mozilla.com",
+    "start_date": datetime.datetime(2023, 4, 25, 0, 0),
+    "end_date": None,
+    "email": ["telemetry-alerts@mozilla.com", "mhirose@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+}
+
+tags = ["impact/tier_2", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_adjust_derived",
+    default_args=default_args,
+    schedule_interval="0 4 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+    adjust_derived__v1 = bigquery_etl_query(
+        task_id="adjust_derived__firefox_mobile_installs__v1",
+        destination_table="adjust_derived_v1",
+        dataset_id="adjust_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="mhirose@mozilla.com",
+        email=["mhirose@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date:DATE:{{ds}}",
+        depends_on_past=False,
+        task_concurrency=1,
+    )

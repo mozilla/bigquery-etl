460509e4e3587a7a5920ff773589bc2e07ead76e
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-07-06 21:51:35 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/checks.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/checks.sql
@@ -1,3 +1,12 @@
-ASSERT((SELECT COUNT(*) FROM `..` WHERE download_date = @download_date) > 250000)
+ASSERT(
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      `moz-fx-data-marketing-prod.ga_derived.downloads_with_attribution_v2`
+    WHERE
+      download_date = @download_date
+  ) > 250000
+)
 AS
-  'ETL Data Check Failed: Table .. contains less than 250,000 rows for date: .'
+  'ETL Data Check Failed: Table moz-fx-data-marketing-prod.ga_derived.downloads_with_attribution_v2 contains less than 250,000 rows for date: .'
---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/ssl_ratios_v1/checks.sql
@@ -0,0 +1,100 @@
+WITH null_checks AS (
+  SELECT
+    [
+      IF(COUNTIF(submission_date IS NULL) > 0, "submission_date", NULL),
+      IF(COUNTIF(os IS NULL) > 0, "os", NULL)
+    ] AS checks
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.ssl_ratios_v1`
+  WHERE
+    submission_date = @submission_date
+),
+non_null_checks AS (
+  SELECT
+    ARRAY_AGG(u IGNORE NULLS) AS checks
+  FROM
+    null_checks,
+    UNNEST(checks) AS u
+)
+SELECT
+  IF(
+    (SELECT ARRAY_LENGTH(checks) FROM non_null_checks) > 0,
+    ERROR(
+      CONCAT(
+        "Columns with NULL values: ",
+        (SELECT ARRAY_TO_STRING(checks, ", ") FROM non_null_checks)
+      )
+    ),
+    NULL
+  );
+
+WITH min_rows AS (
+  SELECT
+    COUNT(*) AS total_rows
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.ssl_ratios_v1`
+  WHERE
+    submission_date = @submission_date
+)
+SELECT
+  IF(
+    (SELECT total_rows FROM min_rows WHERE total_rows < 1) > 0,
+    ERROR(
+      CONCAT("Less than ", (SELECT total_rows FROM min_rows), " rows found (expected more than 1)")
+    ),
+    NULL
+  );
+
+WITH non_unique AS (
+  SELECT
+    COUNT(*) AS total_count
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.ssl_ratios_v1`
+  WHERE
+    submission_date = @submission_date
+  GROUP BY
+    submission_date,
+    os,
+    country
+  HAVING
+    total_count > 1
+)
+SELECT
+  IF(
+    (SELECT COUNT(*) FROM non_unique) > 0,
+    ERROR(
+      "Duplicates detected (Expected combined set of values for columns ['submission_date', 'os', 'country'] to be unique.)"
+    ),
+    NULL
+  );
+
+WITH ranges AS (
+  SELECT
+    [
+      IF(COUNTIF(non_ssl_loads < 0) > 0, "non_ssl_loads", NULL),
+      IF(COUNTIF(ssl_loads < 0) > 0, "ssl_loads", NULL),
+      IF(COUNTIF(reporting_ratio < 0) > 0, "reporting_ratio", NULL)
+    ] AS checks
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.ssl_ratios_v1`
+  WHERE
+    submission_date = @submission_date
+),
+range_checks AS (
+  SELECT
+    ARRAY_AGG(u IGNORE NULLS) AS checks
+  FROM
+    ranges,
+    UNNEST(checks) AS u
+)
+SELECT
+  IF(
+    (SELECT ARRAY_LENGTH(checks) FROM range_checks) > 0,
+    ERROR(
+      CONCAT(
+        "Columns with values not within defined range [0, None]: ",
+        (SELECT ARRAY_TO_STRING(checks, ", ") FROM range_checks)
+      )
+    ),
+    NULL
+  );

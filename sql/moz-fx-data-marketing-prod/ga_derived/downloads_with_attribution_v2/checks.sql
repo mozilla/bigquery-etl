bef8a116ce69188c1dcfda15e04842054831a06e
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-07-11 14:53:46 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/checks.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/downloads_with_attribution_v2/checks.sql
@@ -6,7 +6,7 @@ ASSERT(
       `moz-fx-data-marketing-prod.ga_derived.downloads_with_attribution_v2`
     WHERE
       download_date = @download_date
-  ) > 250000
+  ) > 50000
 )
 AS
-  'ETL Data Check Failed: Table moz-fx-data-marketing-prod.ga_derived.downloads_with_attribution_v2 contains less than 250,000 rows for date: .'
+  'ETL Data Check Failed: Table moz-fx-data-marketing-prod.ga_derived.downloads_with_attribution_v2 contains less than 50,000 rows for date: .'

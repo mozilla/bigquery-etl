7cb7cb008f6a022c473bd8f19cac507f83b4936d
Alekhya Kommasani <akommasani@mozilla.com>
2023-07-11 15:27:29 -0400

    correct the view name

--- sql/moz-fx-data-shared-prod/contextual_services/suggest_forecasting/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.contextual_services.suggest_foreasting`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.contextual_services_derived.suggest_foreasting_v1`

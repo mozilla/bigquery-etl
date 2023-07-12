49b6fba6e9a364d9e0138944c9ed09ebedb6bb85
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2023-07-12 13:17:04 +0200

    Add os_grouped to the view active_users_aggregates (#4044)
    * Add os_grouped to the view.
    
    * bqetl reformatting.
    
    ---------
    
    Co-authored-by: Lucia Vargas <lvargas@mozilla.com>
    Co-authored-by: Marlene Hirose <92952117+Marlene-M-Hirose@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/adjust/adjust_kpi_deliverables/view.sql
+++ sql/moz-fx-data-shared-prod/adjust/adjust_kpi_deliverables/view.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE VIEW
-    `moz-fx-data-shared-prod.adjust.adjust_kpi_deliverables`
+  `moz-fx-data-shared-prod.adjust.adjust_kpi_deliverables`
 AS
 SELECT
-    *
-FROM `moz-fx-data-shared-prod.adjust_derived.adjust_derived_v1`
+  *
+FROM
+  `moz-fx-data-shared-prod.adjust_derived.adjust_derived_v1`

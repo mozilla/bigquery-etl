51971948ca1cd01b27f84860920cdfaf6e4aa412
kik-kik <kignasiak@mozilla.com>
2023-07-18 21:10:07 +0200

    added a view for first_28_day_retention_v1 query

---
+++ sql/moz-fx-data-shared-prod/firefox_ios/first_28_day_retention/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_ios.first_28_day_retention`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_ios_derived.first_28_day_retention_v1`

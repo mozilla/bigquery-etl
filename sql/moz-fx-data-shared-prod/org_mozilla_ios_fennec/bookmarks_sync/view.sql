5e34d84b125e3b718e7936490f375e80171da9ed
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-07-18 05:21:16 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_fennec/bookmarks_sync/view.sql
@@ -0,0 +1,34 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_ios_fennec.bookmarks_sync`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (
+      SELECT AS STRUCT
+        metrics.* REPLACE (
+          STRUCT(
+            mozfun.glean.parse_datetime(
+              metrics.datetime.bookmarks_sync_finished_at
+            ) AS bookmarks_sync_finished_at,
+            metrics.datetime.bookmarks_sync_finished_at AS raw_bookmarks_sync_finished_at,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.bookmarks_sync_started_at
+            ) AS bookmarks_sync_started_at,
+            metrics.datetime.bookmarks_sync_started_at AS raw_bookmarks_sync_started_at,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.bookmarks_sync_v2_finished_at
+            ) AS bookmarks_sync_v2_finished_at,
+            metrics.datetime.bookmarks_sync_v2_finished_at AS raw_bookmarks_sync_v2_finished_at,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.bookmarks_sync_v2_started_at
+            ) AS bookmarks_sync_v2_started_at,
+            metrics.datetime.bookmarks_sync_v2_started_at AS raw_bookmarks_sync_v2_started_at
+          ) AS datetime
+        )
+    ) AS metrics
+  )
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_fennec_stable.bookmarks_sync_v1`
---
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefoxbeta/logins_sync/view.sql
@@ -0,0 +1,34 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta.logins_sync`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (
+      SELECT AS STRUCT
+        metrics.* REPLACE (
+          STRUCT(
+            mozfun.glean.parse_datetime(
+              metrics.datetime.logins_sync_finished_at
+            ) AS logins_sync_finished_at,
+            metrics.datetime.logins_sync_finished_at AS raw_logins_sync_finished_at,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.logins_sync_started_at
+            ) AS logins_sync_started_at,
+            metrics.datetime.logins_sync_started_at AS raw_logins_sync_started_at,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.logins_sync_v2_finished_at
+            ) AS logins_sync_v2_finished_at,
+            metrics.datetime.logins_sync_v2_finished_at AS raw_logins_sync_v2_finished_at,
+            mozfun.glean.parse_datetime(
+              metrics.datetime.logins_sync_v2_started_at
+            ) AS logins_sync_v2_started_at,
+            metrics.datetime.logins_sync_v2_started_at AS raw_logins_sync_v2_started_at
+          ) AS datetime
+        )
+    ) AS metrics
+  )
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta_stable.logins_sync_v1`

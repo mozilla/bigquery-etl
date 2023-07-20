5ae63c7aaa9459cdc8404dd6a2dcc790b032c598
Frank Bertsch <frank.bertsch@gmail.com>
2023-07-20 14:22:14 -0400

    Add full init script for clients_yearly

--- sql/moz-fx-data-shared-prod/fenix_derived/clients_yearly_v1/init.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/clients_yearly_v1/init.sql
@@ -1,18 +1,48 @@
-CREATE TABLE IF NOT EXISTS
-  fenix_derived.clients_yearly_v1
+CREATE OR REPLACE TABLE
+  mozdata.analysis.clients_yearly_sim_111111
 PARTITION BY
-  submission_date
+  (submission_date)
 CLUSTER BY
-  sample_id,
-  client_id
-OPTIONS
-  (require_partition_filter = TRUE)
+  (sample_id)
 AS
+WITH base AS (
+  -- There are duplicates now in `baseline_clients_daily` because of the join with `clients_first_seen`,
+  -- so we take the minimum
+  SELECT
+    * EXCEPT (normalized_app_id),
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        client_id,
+        submission_date
+      ORDER BY
+        first_seen_date ASC,
+        first_run_date ASC
+    ) AS rn,
+  FROM
+    fenix.baseline_clients_daily
+  WHERE
+    submission_date >= "2021-08-01"
+  QUALIFY
+    rn = 1
+)
 SELECT
-  CAST(NULL AS BYTES) AS days_seen_bytes,
-  * EXCEPT (normalized_app_id),
+  -- In this raw table, we capture the history of activity over the past
+  -- 365 days for each usage criterion as an array of bytes. The
+  -- rightmost bit represents whether the user was active in the current day.
+  DATE_ADD(submission_date, INTERVAL off DAY) AS submission_date,
+  client_id,
+  -- bits_from_offsets does not return a consistent number of bytes, and BQ requires that these match
+  -- So we expand out the bit pattern using LPAD, then take the last 365 bits
+  mozfun.bytes.extract_bits(
+    LPAD(udf.bits_from_offsets(ARRAY_AGG(off)), 46, b'\x00'),
+    -365,
+    365
+  ) AS days_seen_bytes,
+  ANY_VALUE(_base HAVING MIN off).* EXCEPT (submission_date, client_id),
 FROM
-  fenix.baseline_clients_daily
-WHERE
-  -- Output empty table and read no input rows
-  FALSE
+  base AS _base
+CROSS JOIN
+  UNNEST(GENERATE_ARRAY(0, 364)) AS off
+GROUP BY
+  client_id,
+  submission_date

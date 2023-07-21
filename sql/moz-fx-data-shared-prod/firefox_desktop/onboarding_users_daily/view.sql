e56d9cf56220984d0702749ccfca91ce1d7e5a39
Glenda Leonard <gleonard@mozilla.com>
2023-07-21 11:48:15 -0400

    onboarding_users_daily_v2

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/onboarding_users_daily/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.onboarding_users_daily`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.onboarding_users_daily_v2`

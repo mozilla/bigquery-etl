62f0be4741bd275980e7a655eb29239e76d87f10
Lucia Vargas <lvargas@mozilla.com>
2023-07-22 21:02:22 +0200

    DS-3037 Add is_init to calculate second_seen_date. Formatting.

--- sql/moz-fx-data-shared-prod/telemetry_derived/clients_first_seen_v2/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/clients_first_seen_v2/query.sql
@@ -605,19 +605,17 @@ _previous AS (
 SELECT
   IF(_previous.client_id IS NULL, _current, _previous).* REPLACE (
     {% if is_init() %}
-      COALESCE(
-        _previous.second_seen_date, _current.second_seen_date
-      ) AS second_seen_date
+      COALESCE(_previous.second_seen_date, _current.second_seen_date) AS second_seen_date
     {% else %}
       IF(
-      _previous.first_seen_date IS NOT NULL
-      AND _previous.second_seen_date IS NULL
-      AND _current.client_id IS NOT NULL,
-      @submission_date,
-      _previous.second_seen_date
-    ) AS second_seen_date
+        _previous.first_seen_date IS NOT NULL
+        AND _previous.second_seen_date IS NULL
+        AND _current.client_id IS NOT NULL,
+        @submission_date,
+        _previous.second_seen_date
+      ) AS second_seen_date
     {% endif %}
-    )
+  )
 FROM
   _previous
 FULL JOIN
--- sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/glam_client_probe_counts_extract_v1/query.sql
@@ -1,74 +1,73 @@
-WITH
-  final_probe_extract AS (
-    SELECT
-      app_version,
-      COALESCE(os, "*") AS os,
-      COALESCE(app_build_id, "*") AS app_build_id,
-      process,
-      metric,
-      SUBSTR(REPLACE(KEY, r"\x00", ""), 0, 200) AS KEY,
-      client_agg_type,
-      metric_type,
-      total_users,
+WITH final_probe_extract AS (
+  SELECT
+    app_version,
+    COALESCE(os, "*") AS os,
+    COALESCE(app_build_id, "*") AS app_build_id,
+    process,
+    metric,
+    SUBSTR(REPLACE(KEY, r"\x00", ""), 0, 200) AS KEY,
+    client_agg_type,
+    metric_type,
+    total_users,
       -- Using MAX instead of COALESCE since this is not in the GROUP BY.
-      MAX(
-      IF
-        (agg_type = "histogram", mozfun.glam.histogram_cast_json(aggregates), NULL)) AS histogram,
-      MAX(
-      IF
-        (agg_type = "histogram", mozfun.glam.histogram_cast_json(non_norm_aggregates), NULL)) AS non_norm_histogram,
-      MAX(
-      IF
-        (agg_type = "percentiles", mozfun.glam.histogram_cast_json(aggregates), NULL) ) AS percentiles,
-      MAX(
-      IF
-        (agg_type = "percentiles", mozfun.glam.histogram_cast_json(non_norm_aggregates), NULL) ) AS non_norm_percentiles
-    FROM
-      `moz-fx-data-shared-prod.telemetry_derived.client_probe_counts`
-    WHERE
-      channel = @channel
-      AND app_version IS NOT NULL
-      AND total_users > 375
-    GROUP BY
-      channel,
-      app_version,
-      app_build_id,
-      os,
-      metric,
-      metric_type,
-      KEY,
-      process,
-      client_agg_type,
-      total_users ),
-  glam_sample_counts AS (
-    SELECT
-      fsc1.os,
-      fsc1.app_version,
-      fsc1.app_build_id,
-      fsc1.metric,
-      fsc1.key,
-      fsc1.process,
-      fsc1.agg_type,
-      CASE
-        WHEN fsc1.agg_type IN ('max', 'min', 'sum', 'avg') AND fsc2.agg_type = 'count' THEN fsc2.total_sample
-      ELSE
-      fsc1.total_sample
-    END
-      AS total_sample
-    FROM
-      `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1` fsc1
-    INNER JOIN
-      `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1` fsc2
-    ON
-      fsc1.os = fsc2.os
-      AND fsc1.app_build_id = fsc2.app_build_id
-      AND fsc1.app_version = fsc2.app_version
-      AND fsc1.metric = fsc2.metric
-      AND fsc1.key = fsc2.key
-      AND fsc1.channel = fsc2.channel
-      AND fsc1.process = fsc2.process
-    WHERE
-      fsc1.channel = @channel )
+    MAX(IF(agg_type = "histogram", mozfun.glam.histogram_cast_json(aggregates), NULL)) AS histogram,
+    MAX(
+      IF(agg_type = "histogram", mozfun.glam.histogram_cast_json(non_norm_aggregates), NULL)
+    ) AS non_norm_histogram,
+    MAX(
+      IF(agg_type = "percentiles", mozfun.glam.histogram_cast_json(aggregates), NULL)
+    ) AS percentiles,
+    MAX(
+      IF(agg_type = "percentiles", mozfun.glam.histogram_cast_json(non_norm_aggregates), NULL)
+    ) AS non_norm_percentiles
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.client_probe_counts`
+  WHERE
+    channel = @channel
+    AND app_version IS NOT NULL
+    AND total_users > 375
+  GROUP BY
+    channel,
+    app_version,
+    app_build_id,
+    os,
+    metric,
+    metric_type,
+    KEY,
+    process,
+    client_agg_type,
+    total_users
+),
+glam_sample_counts AS (
+  SELECT
+    fsc1.os,
+    fsc1.app_version,
+    fsc1.app_build_id,
+    fsc1.metric,
+    fsc1.key,
+    fsc1.process,
+    fsc1.agg_type,
+    CASE
+      WHEN fsc1.agg_type IN ('max', 'min', 'sum', 'avg')
+        AND fsc2.agg_type = 'count'
+        THEN fsc2.total_sample
+      ELSE fsc1.total_sample
+    END AS total_sample
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1` fsc1
+  INNER JOIN
+    `moz-fx-data-shared-prod.telemetry_derived.glam_sample_counts_v1` fsc2
+  ON
+    fsc1.os = fsc2.os
+    AND fsc1.app_build_id = fsc2.app_build_id
+    AND fsc1.app_version = fsc2.app_version
+    AND fsc1.metric = fsc2.metric
+    AND fsc1.key = fsc2.key
+    AND fsc1.channel = fsc2.channel
+    AND fsc1.process = fsc2.process
+  WHERE
+    fsc1.channel = @channel
+)
 SELECT
   cp.app_version,
   cp.os,
@@ -82,11 +81,10 @@ SELECT
   histogram,
   percentiles,
   CASE
-    WHEN client_agg_type = '' THEN 0
-  ELSE
-    total_sample
-  END
-  AS total_sample,
+    WHEN client_agg_type = ''
+      THEN 0
+    ELSE total_sample
+  END AS total_sample,
   non_norm_histogram,
   non_norm_percentiles
 FROM
@@ -101,8 +99,7 @@ ON
   AND sc.key = cp.key
   AND sc.process = cp.process
   AND total_sample IS NOT NULL
-  AND (sc.agg_type = cp.client_agg_type
-    OR cp.client_agg_type = '')
+  AND (sc.agg_type = cp.client_agg_type OR cp.client_agg_type = '')
 GROUP BY
   cp.app_version,
   cp.os,

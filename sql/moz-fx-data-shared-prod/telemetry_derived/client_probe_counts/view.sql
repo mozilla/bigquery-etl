0e6a28026dca74f07b1779b23a2c8df4ea76f86e
Eduardo Filho <edugomfilho@gmail.com>
2023-07-24 13:40:58 -0400

    fix + format query

--- sql/moz-fx-data-shared-prod/telemetry_derived/client_probe_counts/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/client_probe_counts/view.sql
@@ -1,29 +1,29 @@
-CREATE OR REPLACE VIEW `moz-fx-data-shared-prod`.telemetry_derived.client_probe_counts
-(
-WITH all_counts AS (
-  SELECT
-    *,
-    aggregates AS non_norm_aggregates
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_scalar_probe_counts_v1`
-  UNION ALL
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod`.telemetry_derived.client_probe_counts AS (
+  WITH
+    all_counts AS (
+    SELECT
+      *,
+      aggregates AS non_norm_aggregates
+    FROM
+      `moz-fx-data-shared-prod.telemetry_derived.clients_scalar_probe_counts_v1`
+    UNION ALL
+    SELECT
+      *
+    FROM
+      `moz-fx-data-shared-prod.telemetry_derived.clients_histogram_probe_counts_v1`
+    UNION ALL
+    SELECT
+      *,
+      aggregates AS non_norm_aggregates
+    FROM
+      `moz-fx-data-shared-prod.telemetry_derived.scalar_percentiles_v1`
+    UNION ALL
+    SELECT
+      *
+    FROM
+      `moz-fx-data-shared-prod.telemetry_derived.histogram_percentiles_v1` )
   SELECT
     *
   FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_histogram_probe_counts_v1`
-  UNION ALL
-  SELECT
-    *,
-    aggregates AS non_norm_aggregates
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.scalar_percentiles_v1`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.histogram_percentiles_v1`
-)
-SELECT
-  *
-FROM
-  all_counts)
+    all_counts)

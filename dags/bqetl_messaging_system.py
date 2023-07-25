541295866a33ed0808471bd13318c433c8369f3e
Glenda Leonard <gleonard@mozilla.com>
2023-07-25 15:24:02 -0400

    snippets_users_last_seen_v2

--- dags/bqetl_messaging_system.py
+++ dags/bqetl_messaging_system.py
@@ -197,6 +197,21 @@ with DAG(
         depends_on_past=False,
     )
 
+    firefox_desktop_derived__snippets_users_last_seen__v2 = bigquery_etl_query(
+        task_id="firefox_desktop_derived__snippets_users_last_seen__v2",
+        destination_table="snippets_users_last_seen_v2",
+        dataset_id="firefox_desktop_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="gleonard@mozilla.com",
+        email=[
+            "gleonard@mozilla.com",
+            "najiang@mozilla.com",
+            "telemetry-alerts@mozilla.com",
+        ],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     messaging_system_derived__cfr_exact_mau28_by_dimensions__v1 = bigquery_etl_query(
         task_id="messaging_system_derived__cfr_exact_mau28_by_dimensions__v1",
         destination_table="cfr_exact_mau28_by_dimensions_v1",
@@ -340,6 +355,10 @@ with DAG(
 
     firefox_desktop_derived__snippets_users_daily__v2.set_upstream(
         firefox_desktop_derived__snippets__v2
+    )
+
+    firefox_desktop_derived__snippets_users_last_seen__v2.set_upstream(
+        firefox_desktop_derived__snippets_users_daily__v2
     )
 
     messaging_system_derived__cfr_exact_mau28_by_dimensions__v1.set_upstream(

b1504781f399fb7a74bde6d06a570f8cc1328fec
kik-kik <kignasiak@mozilla.com>
2023-08-02 16:41:15 +0200

    renamed firefox_ios_derived.funnel_v1 to firefox_ios_derived.app_store_funnel_v1

--- dags/bqetl_firefox_ios.py
+++ dags/bqetl_firefox_ios.py
@@ -43,9 +43,9 @@ with DAG(
     doc_md=docs,
     tags=tags,
 ) as dag:
-    checks__firefox_ios_derived__funnel__v1 = bigquery_dq_check(
-        task_id="checks__firefox_ios_derived__funnel__v1",
-        source_table="funnel_v1",
+    checks__firefox_ios_derived__app_store_funnel__v1 = bigquery_dq_check(
+        task_id="checks__firefox_ios_derived__app_store_funnel__v1",
+        source_table="app_store_funnel_v1",
         dataset_id="firefox_ios_derived",
         project_id="moz-fx-data-shared-prod",
         owner="kignasiak@mozilla.com",
@@ -54,6 +54,22 @@ with DAG(
             "kik@mozilla.com",
             "telemetry-alerts@mozilla.com",
         ],
+        depends_on_past=False,
+        task_concurrency=1,
+    )
+
+    firefox_ios_derived__app_store_funnel__v1 = bigquery_etl_query(
+        task_id="firefox_ios_derived__app_store_funnel__v1",
+        destination_table="app_store_funnel_v1",
+        dataset_id="firefox_ios_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kignasiak@mozilla.com",
+        email=[
+            "kignasiak@mozilla.com",
+            "kik@mozilla.com",
+            "telemetry-alerts@mozilla.com",
+        ],
+        date_partition_parameter=None,
         depends_on_past=False,
         task_concurrency=1,
     )
@@ -89,22 +105,6 @@ with DAG(
         parameters=["submission_date:DATE:{{ds}}"],
     )
 
-    firefox_ios_derived__funnel__v1 = bigquery_etl_query(
-        task_id="firefox_ios_derived__funnel__v1",
-        destination_table="funnel_v1",
-        dataset_id="firefox_ios_derived",
-        project_id="moz-fx-data-shared-prod",
-        owner="kignasiak@mozilla.com",
-        email=[
-            "kignasiak@mozilla.com",
-            "kik@mozilla.com",
-            "telemetry-alerts@mozilla.com",
-        ],
-        date_partition_parameter=None,
-        depends_on_past=False,
-        task_concurrency=1,
-    )
-
     firefox_ios_derived__new_profile_activation__v2 = bigquery_etl_query(
         task_id="firefox_ios_derived__new_profile_activation__v2",
         destination_table="new_profile_activation_v2",
@@ -133,8 +133,54 @@ with DAG(
         email=["kik@mozilla.com", "telemetry-alerts@mozilla.com"],
     )
 
-    checks__firefox_ios_derived__funnel__v1.set_upstream(
-        firefox_ios_derived__funnel__v1
+    checks__firefox_ios_derived__app_store_funnel__v1.set_upstream(
+        firefox_ios_derived__app_store_funnel__v1
+    )
+
+    wait_for_app_store_external__firefox_app_store_territory_source_type_report__v1 = ExternalTaskSensor(
+        task_id="wait_for_app_store_external__firefox_app_store_territory_source_type_report__v1",
+        external_dag_id="bqetl_fivetran_copied_tables",
+        external_task_id="app_store_external__firefox_app_store_territory_source_type_report__v1",
+        execution_delta=datetime.timedelta(seconds=3600),
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    firefox_ios_derived__app_store_funnel__v1.set_upstream(
+        wait_for_app_store_external__firefox_app_store_territory_source_type_report__v1
+    )
+    wait_for_app_store_external__firefox_downloads_territory_source_type_report__v1 = ExternalTaskSensor(
+        task_id="wait_for_app_store_external__firefox_downloads_territory_source_type_report__v1",
+        external_dag_id="bqetl_fivetran_copied_tables",
+        external_task_id="app_store_external__firefox_downloads_territory_source_type_report__v1",
+        execution_delta=datetime.timedelta(seconds=3600),
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    firefox_ios_derived__app_store_funnel__v1.set_upstream(
+        wait_for_app_store_external__firefox_downloads_territory_source_type_report__v1
+    )
+    wait_for_firefox_ios_derived__new_profile_activation__v1 = ExternalTaskSensor(
+        task_id="wait_for_firefox_ios_derived__new_profile_activation__v1",
+        external_dag_id="bqetl_mobile_activation",
+        external_task_id="firefox_ios_derived__new_profile_activation__v1",
+        execution_delta=datetime.timedelta(seconds=14400),
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    firefox_ios_derived__app_store_funnel__v1.set_upstream(
+        wait_for_firefox_ios_derived__new_profile_activation__v1
     )
 
     wait_for_baseline_clients_daily = ExternalTaskSensor(
@@ -181,52 +227,6 @@ with DAG(
 
     firefox_ios_derived__firefox_ios_clients__v1.set_upstream(
         firefox_ios_derived__new_profile_activation__v2
-    )
-
-    wait_for_app_store_external__firefox_app_store_territory_source_type_report__v1 = ExternalTaskSensor(
-        task_id="wait_for_app_store_external__firefox_app_store_territory_source_type_report__v1",
-        external_dag_id="bqetl_fivetran_copied_tables",
-        external_task_id="app_store_external__firefox_app_store_territory_source_type_report__v1",
-        execution_delta=datetime.timedelta(seconds=3600),
-        check_existence=True,
-        mode="reschedule",
-        allowed_states=ALLOWED_STATES,
-        failed_states=FAILED_STATES,
-        pool="DATA_ENG_EXTERNALTASKSENSOR",
-    )
-
-    firefox_ios_derived__funnel__v1.set_upstream(
-        wait_for_app_store_external__firefox_app_store_territory_source_type_report__v1
-    )
-    wait_for_app_store_external__firefox_downloads_territory_source_type_report__v1 = ExternalTaskSensor(
-        task_id="wait_for_app_store_external__firefox_downloads_territory_source_type_report__v1",
-        external_dag_id="bqetl_fivetran_copied_tables",
-        external_task_id="app_store_external__firefox_downloads_territory_source_type_report__v1",
-        execution_delta=datetime.timedelta(seconds=3600),
-        check_existence=True,
-        mode="reschedule",
-        allowed_states=ALLOWED_STATES,
-        failed_states=FAILED_STATES,
-        pool="DATA_ENG_EXTERNALTASKSENSOR",
-    )
-
-    firefox_ios_derived__funnel__v1.set_upstream(
-        wait_for_app_store_external__firefox_downloads_territory_source_type_report__v1
-    )
-    wait_for_firefox_ios_derived__new_profile_activation__v1 = ExternalTaskSensor(
-        task_id="wait_for_firefox_ios_derived__new_profile_activation__v1",
-        external_dag_id="bqetl_mobile_activation",
-        external_task_id="firefox_ios_derived__new_profile_activation__v1",
-        execution_delta=datetime.timedelta(seconds=14400),
-        check_existence=True,
-        mode="reschedule",
-        allowed_states=ALLOWED_STATES,
-        failed_states=FAILED_STATES,
-        pool="DATA_ENG_EXTERNALTASKSENSOR",
-    )
-
-    firefox_ios_derived__funnel__v1.set_upstream(
-        wait_for_firefox_ios_derived__new_profile_activation__v1
     )
 
     wait_for_baseline_clients_last_seen = ExternalTaskSensor(
--- sql/moz-fx-data-shared-prod/firefox_ios/funnel/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_ios.funnel`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_ios_derived.funnel_v1`

d65e213174a447e0c4c463fcf51c61ac06a83366
kik-kik <kignasiak@mozilla.com>
2023-08-02 17:04:15 +0200

    split downloads measure into new and redownloads

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/query.sql
@@ -15,7 +15,9 @@ downloads_data AS (
   SELECT
     `date`,
     territory AS country_name,
-    SUM(total_downloads) AS downloads,
+    SUM(total_downloads) AS total_downloads,
+    SUM(first_time_downloads) AS first_time_downloads,
+    SUM(redownloads) AS redownloads,
   FROM
     app_store.firefox_downloads_territory_source_type_report
   WHERE
@@ -30,7 +32,9 @@ store_stats AS (
     DATE(`date`) AS `date`,
     code AS country,
     views,
-    downloads,
+    total_downloads,
+    first_time_downloads,
+    redownloads,
   FROM
     views_data
   JOIN
@@ -42,7 +46,7 @@ store_stats AS (
   ON
     country_name = name
 ),
-activations AS (
+new_profiles_and_activations AS (
   SELECT
     first_seen_date AS `date`,
     country,
@@ -55,10 +59,7 @@ activations AS (
     AND first_seen_date >= '2022-01-01'
      -- below filter required due to untrusted devices anomaly,
      -- more information can be found in this bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1846554
-    AND NOT (
-      app_display_version = '107.2'
-      AND submission_date >= '2023-02-01'
-    )
+    AND NOT (app_display_version = '107.2' AND submission_date >= '2023-02-01')
   GROUP BY
     `date`,
     country
@@ -67,12 +68,14 @@ SELECT
   `date`,
   country,
   COALESCE(views, 0) AS views,
-  COALESCE(downloads, 0) AS downloads,
+  COALESCE(total_downloads, 0) AS total_downloads,
+  COALESCE(first_time_downloads, 0) AS first_time_downloads,
+  COALESCE(redownloads, 0) AS redownloads,
   COALESCE(new_profiles, 0) AS new_profiles,
   COALESCE(activations, 0) AS activations,
 FROM
   store_stats
 FULL OUTER JOIN
-  activations
+  new_profiles_and_activations
 USING
   (`date`, country)

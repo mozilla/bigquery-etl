b49699c2cfffbbc0da7f5adc1e14c2f8625da4cf
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-08-07 13:43:37 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/checks.sql
@@ -3,9 +3,8 @@ WITH non_unique AS (
     COUNT(*) AS total_count
   FROM
     `moz-fx-data-shared-prod.firefox_ios_derived.app_store_funnel_v1`
-  WHERE
-    `date` = @submission_date
   GROUP BY
+    `date`,
     country
   HAVING
     total_count > 1
@@ -14,7 +13,7 @@ SELECT
   IF(
     (SELECT COUNT(*) FROM non_unique) > 0,
     ERROR(
-      "Duplicates detected (Expected combined set of values for columns ['country'] to be unique.)"
+      "Duplicates detected (Expected combined set of values for columns ['`date`', 'country'] to be unique.)"
     ),
     NULL
   );

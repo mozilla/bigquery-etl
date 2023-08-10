c8681a286f8ce656c3e42575eb739cc2153478aa
kik-kik <kignasiak@mozilla.com>
2023-08-10 12:52:42 +0200

    including old Airflow data (pre-migration) as suggested by srose in PR#4171

--- sql/moz-fx-data-shared-prod/monitoring_derived/airflow_task_fail_v1/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/airflow_task_fail_v1/query.sql
@@ -9,3 +9,16 @@ FROM
   `moz-fx-data-bq-fivetran.telemetry_airflow_metadata_public.task_fail`
 WHERE
   NOT _fivetran_deleted
+UNION ALL
+-- including historical data from the pre-migration Airflow instance
+SELECT
+  dag_id,
+  task_id,
+  duration,
+  execution_date,
+  start_date,
+  end_date,
+FROM
+  `moz-fx-data-bq-fivetran.airflow_metadata_airflow_db.task_fail`
+WHERE
+  NOT _fivetran_deleted

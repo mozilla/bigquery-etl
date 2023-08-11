aa49fd4c383b2348ec1726672549c6e1d71b4b7f
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-08-11 16:04:21 +0200

    bug(DENG-1358): fixing ETL errors for airflow metadata queries (#4177)
    * fixing ETL errors for airflow metadata queries
    
    * made a change as requested by lelilia

--- sql/moz-fx-data-shared-prod/monitoring_derived/airflow_dag_v1/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/airflow_dag_v1/query.sql
@@ -1,13 +1,21 @@
 SELECT
   dag_id,
-  IF(is_subdag, SPLIT(dag_id, ".")[OFFSET(0)], dag_id) AS root_dag_id,
+  COALESCE(root_dag_id, dag_id) AS root_dag_id,
   IF(is_subdag, SPLIT(dag_id, ".")[OFFSET(1)], NULL) AS subdag_id,
   is_subdag,
+  description,
+  schedule_interval,
   owners,
+  is_active,
   is_paused,
   has_task_concurrency_limits,
-  concurrency,
   max_active_runs,
+  max_active_tasks,
+  next_dagrun,
+  next_dagrun_create_after,
+  next_dagrun_data_interval_start,
+  next_dagrun_data_interval_end,
+  last_parsed_time,
 FROM
   `moz-fx-data-bq-fivetran.telemetry_airflow_metadata_public.dag`
 WHERE

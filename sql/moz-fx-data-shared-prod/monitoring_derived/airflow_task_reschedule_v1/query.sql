097e3b28670c2bbd058f7c37d5c0fef63a0a34ee
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-08-11 14:19:53 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/monitoring_derived/airflow_task_reschedule_v1/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/airflow_task_reschedule_v1/query.sql
@@ -1,7 +1,8 @@
 SELECT
   dag_id,
   task_id,
-  execution_date,
+  run_id,
+  try_number,
   reschedule_date,
   start_date,
   end_date,
@@ -11,11 +12,12 @@ FROM
 WHERE
   NOT _fivetran_deleted
 UNION ALL
--- including historical data from the pre-migration Airflow instance
+  -- including historical data from the pre-migration Airflow instance
 SELECT
   dag_id,
   task_id,
-  execution_date,
+  run_id,
+  try_number,
   reschedule_date,
   start_date,
   end_date,

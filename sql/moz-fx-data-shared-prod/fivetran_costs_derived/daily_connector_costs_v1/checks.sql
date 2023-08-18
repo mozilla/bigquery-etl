ac69c6cfaa94b52d8ba1e4cf5c25ada09bf1d092
Leli Schiestl <lschiestl@mozilla.com>
2023-08-18 18:26:10 +0200

    infraday data-checks: add data checks to fivetran_costs_derived

---
+++ sql/moz-fx-data-shared-prod/fivetran_costs_derived/daily_connector_costs_v1/checks.sql
@@ -0,0 +1,5 @@
+{{ is_unique(columns=["destination", "measured_date", "connector", "billing_type"]) }}
+
+{{ not_null(columns=["destination", "measured_date", "connector", "billing_type", "active_rows", "cost_in_usd"]) }}
+
+{{ min_rows(1) }}

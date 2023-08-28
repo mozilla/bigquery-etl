dd80ffb71b76288f0eac17d04a456e09081d433c
Alekhya Kommasani <akommasani@mozilla.com>
2023-08-28 15:43:56 -0400

    Initial draft for adding deprecation support to bqetl

--- bigquery_etl/cli/metadata.py
+++ bigquery_etl/cli/metadata.py
@@ -1,30 +1,18 @@
 """bigquery-etl CLI metadata command."""
-import re
-import subprocess
-import sys
-import tempfile
+import os
 from pathlib import Path
-from subprocess import CalledProcessError
-from typing import List, Optional, Union
+from typing import Optional
 
 import click
-import sqlparse
 
-from bigquery_etl.format_sql.formatter import reformat
+from bigquery_etl.metadata.parse_metadata import DatasetMetadata, Metadata
 
-from ..cli.utils import (
-    is_authenticated,
-    paths_matching_checks_pattern,
-    project_id_option,
-    sql_dir_option,
-)
-from ..util.common import render as render_template
-
+from ..cli.utils import paths_matching_name_pattern, project_id_option, sql_dir_option
 
 
 @click.group(
     help="""
-        Commands for managing metadata.
+        Commands for managing bqetl metadata.
         \b
 
         UNDER ACTIVE DEVELOPMENT See https://mozilla-hub.atlassian.net/browse/DENG-1381
@@ -35,9 +23,10 @@ def metadata(ctx):
     """Create the CLI group for the metadata command."""
     pass
 
-@check.command(
+
+@metadata.command(
     help="""
-    update metadata yaml files.
+    Update table level metadata yaml files.
    \b
 
     Example:
@@ -51,24 +40,36 @@ def metadata(ctx):
 @click.argument("name")
 @project_id_option()
 @sql_dir_option
-@click.pass_context
-def update(
-    ctx: click.Context, name: str, project_id: Optional[str], sql_dir: Optional[str]
-) -> None:
-    """Update metadata yaml file """
+def update(name: str, sql_dir: Optional[str], project_id: Optional[str]) -> None:
+    """Update metadata yaml file."""
     table_metadata_files = paths_matching_name_pattern(
         name, sql_dir, project_id=project_id, files=["metadata.yaml"]
     )
     for table_metadata_file in table_metadata_files:
-        dataset_metadata_path = Path(table_metadata_file).parent.parent / "dataset_metadata.yaml"
-        dataset_metadata = DatasetMetadata.from_file(dataset_metadata_path)
-        table_metadata = Metadata.from_file(table_metadata_file)
-        if table_metadata.deprecated:
-            table_metadata.workgroup_access = [dict(role="roles/bigquery.metadataViewer", members=["workgroup:deprecated"])]
-        else:
-            if dataset_metadata.default_table_workgroup_access and (table_metadata.workgroup_access == [] or table_metadata.workgroup_access is None):
-                table_metadata.workgroup_access = dataset_metadata.default_table_workgroup_access
-        table_metadata.write(table_metadata_file)
+        dataset_metadata_path = (
+            Path(table_metadata_file).parent.parent / "dataset_metadata.yaml"
+        )
+        if os.path.exists(dataset_metadata_path):
+            dataset_metadata = DatasetMetadata.from_file(dataset_metadata_path)
+            table_metadata = Metadata.from_file(table_metadata_file)
+            if table_metadata.deprecated:
+                table_metadata.workgroup_access = [
+                    dict(
+                        role="roles/bigquery.metadataViewer",
+                        members=["workgroup:deprecated"],
+                    )
+                ]
+            else:
+                if dataset_metadata.default_table_workgroup_access and (
+                    table_metadata.workgroup_access == []
+                    or table_metadata.workgroup_access is None
+                ):
+                    table_metadata.workgroup_access = (
+                        dataset_metadata.default_table_workgroup_access
+                    )
+            table_metadata.write(table_metadata_file)
 
-        click.echo(f"Updated {table_metadata_file}")
+            click.echo(f"Updated {table_metadata_file}")
+        else:
+            click.echo("Dataset metadata yaml does not exist")
     return None

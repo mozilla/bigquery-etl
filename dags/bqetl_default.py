27262acdfd1d9c27050fcaba2ebe9eb3f3ee0470
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2023-08-29 14:32:52 +0200

    Default DAG for bqetl queries (#4143)
    * DENG-1314 Implement changes to bqetl and create default DAG.
    
    * DENG-1314. Update Documentation.
    
    * DENG-1314. Dummy query to enable generating DAG and run tests.
    
    * DENG-1314. Update tests.
    
    * Update bigquery_etl/cli/query.py
    
    Raise exception when scheduling information is missing.
    
    Co-authored-by: Daniel Thorn <dthorn@mozilla.com>
    
    * DENG-1314. Update tests.
    
    * DS-3054. Update query creation to set bqetl_default as default value for --dag. Update tests.
    
    * Default task and tests update.
    
    * Default task and tests update.
    
    * 3650 - Remove default DAG option, update DAG template comment & tests.
    
    * 3650 - Condition for DAG warning.
    
    * 3650 - Update docs.
    
    * Clarification on sql/moz-fx-data-shared-prod/analysis/bqetl_default_task_v1/metadata.yaml
    
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>
    
    * Update docs/cookbooks/creating_a_derived_dataset.md
    
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>
    
    ---------
    
    Co-authored-by: Lucia Vargas <lvargas@mozilla.com>
    Co-authored-by: Daniel Thorn <dthorn@mozilla.com>
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>

---
+++ dags/bqetl_default.py
@@ -0,0 +1,56 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, gke_command, bigquery_dq_check
+
+docs = """
+### bqetl_default
+
+Built from bigquery-etl repo, [`dags/bqetl_default.py`](https://github.com/mozilla/bigquery-etl/blob/main/dags/bqetl_default.py)
+
+#### Description
+
+This is a default DAG to schedule tasks with lower business impact or that don't require a new or existing DAG. Queries can be automatically scheduled in this DAG using the option --use_default_dag. See [related documentation in the cookbooks](https://mozilla.github.io/bigquery-etl/cookbooks/creating_a_derived_dataset/)
+#### Owner
+
+telemetry-alerts@mozilla.com
+"""
+
+
+default_args = {
+    "owner": "telemetry-alerts@mozilla.com",
+    "start_date": datetime.datetime(2023, 9, 1, 0, 0),
+    "end_date": None,
+    "email": ["telemetry-alerts@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+}
+
+tags = ["impact/tier_3", "repo/bigquery-etl", "triage/no_triage"]
+
+with DAG(
+    "bqetl_default",
+    default_args=default_args,
+    schedule_interval="0 4 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+    analysis__bqetl_default_task__v1 = bigquery_etl_query(
+        #### WARNING: This task has been scheduled in the default DAG. It can be moved to a more suitable DAG using `bqetl query schedule`.
+        task_id="analysis__bqetl_default_task__v1",
+        destination_table="bqetl_default_task_v1",
+        dataset_id="analysis",
+        project_id="moz-fx-data-shared-prod",
+        owner="telemetry-alerts@mozilla.com",
+        email=["telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )

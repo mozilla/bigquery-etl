a4fa1e84e38144207dfadb94220e3a3260b09174
Marlene-M-Hirose <mhirose@mozilla.com>
2023-09-01 06:49:44 -1000

    add date fitler for partitioning and backfilling in future

--- sql/moz-fx-data-shared-prod/monitoring_derived/jobs_by_organization_v1/query.py
+++ sql/moz-fx-data-shared-prod/monitoring_derived/jobs_by_organization_v1/query.py
@@ -72,9 +72,10 @@ def create_query(date, project):
         FROM
           `{project}.region-us.INFORMATION_SCHEMA.JOBS_BY_ORGANIZATION`
         WHERE
-          (project_id IN UNNEST({DEFAULT_PROJECTS})
-          OR referenced_tables.project_id IN UNNEST({DEFAULT_PROJECTS})
-          OR destination_table.project_id IN UNNEST({DEFAULT_PROJECTS}))
+          DATE(creation_time) = '{date}'
+          AND (t1.project_id IN UNNEST({DEFAULT_PROJECTS})
+            OR referenced_tables.project_id IN UNNEST({DEFAULT_PROJECTS})
+            OR destination_table.project_id IN UNNEST({DEFAULT_PROJECTS}))
     """
 
 

6c3619cfeb2bcd918a4295310db0d72ee6f797db
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-09-07 16:35:27 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/checks.sql
@@ -17,3 +17,20 @@ SELECT
     ),
     NULL
   );
+
+WITH min_rows AS (
+  SELECT
+    COUNT(*) AS total_rows
+  FROM
+    `moz-fx-data-shared-prod.firefox_ios_derived.app_store_funnel_v1`
+  WHERE
+    `date` = DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)
+)
+SELECT
+  IF(
+    (SELECT COUNTIF(total_rows < 1) FROM min_rows) > 0,
+    ERROR(
+      CONCAT("Less than ", (SELECT total_rows FROM min_rows), " rows found (expected more than 1)")
+    ),
+    NULL
+  );

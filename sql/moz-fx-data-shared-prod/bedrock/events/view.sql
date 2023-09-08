17a9311624f6570e9a42c4cf27f0bda475db1d5b
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-09-08 05:22:44 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/bedrock/events/view.sql
+++ sql/moz-fx-data-shared-prod/bedrock/events/view.sql
@@ -5,7 +5,16 @@ AS
 SELECT
   * REPLACE (
     mozfun.norm.metadata(metadata) AS metadata,
-    mozfun.norm.glean_ping_info(ping_info) AS ping_info
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (
+      SELECT AS STRUCT
+        metrics.* REPLACE (
+          STRUCT(
+            mozfun.glean.parse_datetime(metrics.datetime.page_viewed) AS page_viewed,
+            metrics.datetime.page_viewed AS raw_page_viewed
+          ) AS datetime
+        )
+    ) AS metrics
   )
 FROM
   `moz-fx-data-shared-prod.bedrock_stable.events_v1`

45f8884dc039e57e8aee7800094f57be756a753e
kik-kik <kignasiak@mozilla.com>
2023-09-11 19:43:56 +0200

    tweaks made to fxa_server_events_v1 query as suggested by srose and partition filter corrected

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_server_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_server_events_v1/query.sql
@@ -1,14 +1,17 @@
 SELECT
-  SPLIT(jsonPayload.logger, "-")[OFFSET(1)] AS fxa_server,  -- example expected input: fxa-auth-server
+  SPLIT(jsonPayload.logger, "-")[
+    OFFSET(1)
+  ] AS fxa_server,  -- example expected input: fxa-auth-server
   * REPLACE (
     (
       SELECT AS STRUCT
         jsonPayload.* REPLACE (
           (
             SELECT AS STRUCT
-              jsonPayload.fields.* EXCEPT (device_id, user_id),
-              TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
-              TO_HEX(SHA256(jsonPayload.fields.device_id)) AS device_id
+              jsonPayload.fields.* REPLACE (
+                TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
+                TO_HEX(SHA256(jsonPayload.fields.device_id)) AS device_id
+              )
           ) AS fields
         )
     ) AS jsonPayload
@@ -16,7 +19,9 @@ SELECT
 FROM
   `moz-fx-fxa-prod.gke_fxa_prod_log.stdout`
 WHERE
-  jsonPayload.type = 'amplitudeEvent'
+  -- TODO: Looks like partition field is different as expected, need to confirm with SRE that this is intended.
+  -- DATE(`timestamp`) = @submission_date
+  _PARTITIONTIME = @submission_date
+  AND jsonPayload.type = 'amplitudeEvent'
   AND jsonPayload.logger IS NOT NULL
   AND jsonPayload.fields.event_type IS NOT NULL
-  AND DATE(`timestamp`) = @submission_date

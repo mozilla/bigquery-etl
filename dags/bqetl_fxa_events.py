94b5361da920642e3aa543cfd32a77bb01478c52
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-09-13 19:09:38 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_fxa_events.py
+++ dags/bqetl_fxa_events.py
@@ -174,6 +174,74 @@ with DAG(
         depends_on_past=False,
     )
 
+    firefox_accounts_derived__fxa_gcp_stderr_events__v1 = bigquery_etl_query(
+        task_id="firefox_accounts_derived__fxa_gcp_stderr_events__v1",
+        destination_table="fxa_gcp_stderr_events_v1",
+        dataset_id="firefox_accounts_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kik@mozilla.com",
+        email=["dthorn@mozilla.com", "kik@mozilla.com", "telemetry-alerts@mozilla.com"],
+        start_date=datetime.datetime(2023, 9, 7, 0, 0),
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        arguments=["--schema_update_option=ALLOW_FIELD_ADDITION"],
+    )
+
+    with TaskGroup(
+        "firefox_accounts_derived__fxa_gcp_stderr_events__v1_external"
+    ) as firefox_accounts_derived__fxa_gcp_stderr_events__v1_external:
+        ExternalTaskMarker(
+            task_id="bqetl_subplat__wait_for_firefox_accounts_derived__fxa_gcp_stderr_events__v1",
+            external_dag_id="bqetl_subplat",
+            external_task_id="wait_for_firefox_accounts_derived__fxa_gcp_stderr_events__v1",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=85500)).isoformat() }}",
+        )
+
+        ExternalTaskMarker(
+            task_id="bqetl_event_rollup__wait_for_firefox_accounts_derived__fxa_gcp_stderr_events__v1",
+            external_dag_id="bqetl_event_rollup",
+            external_task_id="wait_for_firefox_accounts_derived__fxa_gcp_stderr_events__v1",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=81000)).isoformat() }}",
+        )
+
+        firefox_accounts_derived__fxa_gcp_stderr_events__v1_external.set_upstream(
+            firefox_accounts_derived__fxa_gcp_stderr_events__v1
+        )
+
+    firefox_accounts_derived__fxa_gcp_stdout_events__v1 = bigquery_etl_query(
+        task_id="firefox_accounts_derived__fxa_gcp_stdout_events__v1",
+        destination_table="fxa_gcp_stdout_events_v1",
+        dataset_id="firefox_accounts_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kik@mozilla.com",
+        email=["dthorn@mozilla.com", "kik@mozilla.com", "telemetry-alerts@mozilla.com"],
+        start_date=datetime.datetime(2023, 9, 7, 0, 0),
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+        arguments=["--schema_update_option=ALLOW_FIELD_ADDITION"],
+    )
+
+    with TaskGroup(
+        "firefox_accounts_derived__fxa_gcp_stdout_events__v1_external"
+    ) as firefox_accounts_derived__fxa_gcp_stdout_events__v1_external:
+        ExternalTaskMarker(
+            task_id="bqetl_subplat__wait_for_firefox_accounts_derived__fxa_gcp_stdout_events__v1",
+            external_dag_id="bqetl_subplat",
+            external_task_id="wait_for_firefox_accounts_derived__fxa_gcp_stdout_events__v1",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=85500)).isoformat() }}",
+        )
+
+        ExternalTaskMarker(
+            task_id="bqetl_event_rollup__wait_for_firefox_accounts_derived__fxa_gcp_stdout_events__v1",
+            external_dag_id="bqetl_event_rollup",
+            external_task_id="wait_for_firefox_accounts_derived__fxa_gcp_stdout_events__v1",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=81000)).isoformat() }}",
+        )
+
+        firefox_accounts_derived__fxa_gcp_stdout_events__v1_external.set_upstream(
+            firefox_accounts_derived__fxa_gcp_stdout_events__v1
+        )
+
     firefox_accounts_derived__fxa_log_auth_events__v1 = bigquery_etl_query(
         task_id="firefox_accounts_derived__fxa_log_auth_events__v1",
         destination_table="fxa_log_auth_events_v1",
@@ -423,6 +491,14 @@ with DAG(
     )
 
     firefox_accounts_derived__fxa_users_daily__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stderr_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_daily__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stdout_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_daily__v1.set_upstream(
         firefox_accounts_derived__fxa_stdout_events__v1
     )
 
@@ -432,6 +508,14 @@ with DAG(
 
     firefox_accounts_derived__fxa_users_first_seen__v1.set_upstream(
         firefox_accounts_derived__fxa_content_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_first_seen__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stderr_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_first_seen__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stdout_events__v1
     )
 
     firefox_accounts_derived__fxa_users_first_seen__v1.set_upstream(
@@ -451,6 +535,14 @@ with DAG(
     )
 
     firefox_accounts_derived__fxa_users_services_daily__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stderr_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_services_daily__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stdout_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_services_daily__v1.set_upstream(
         firefox_accounts_derived__fxa_stdout_events__v1
     )
 
@@ -463,6 +555,14 @@ with DAG(
     )
 
     firefox_accounts_derived__fxa_users_services_daily__v2.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stderr_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_services_daily__v2.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stdout_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_services_daily__v2.set_upstream(
         firefox_accounts_derived__fxa_stdout_events__v1
     )
 
@@ -472,6 +572,14 @@ with DAG(
 
     firefox_accounts_derived__fxa_users_services_devices_daily__v1.set_upstream(
         firefox_accounts_derived__fxa_content_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_services_devices_daily__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stderr_events__v1
+    )
+
+    firefox_accounts_derived__fxa_users_services_devices_daily__v1.set_upstream(
+        firefox_accounts_derived__fxa_gcp_stdout_events__v1
     )
 
     firefox_accounts_derived__fxa_users_services_devices_daily__v1.set_upstream(

51fc5ffc5faf677bda680dbf4cd0e534e60f5a43
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-09-15 11:40:59 +0200

    Apply suggestions from code review provided by srose
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_gcp_stderr_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_gcp_stderr_events_v1/query.sql
@@ -7,9 +7,10 @@ SELECT
         jsonPayload.* REPLACE (
           (
             SELECT AS STRUCT
-              jsonPayload.fields.* EXCEPT (device_id, user_id),
-              TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
-              TO_HEX(SHA256(jsonPayload.fields.device_id)) AS device_id
+              jsonPayload.fields.* REPLACE (
+                TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
+                TO_HEX(SHA256(jsonPayload.fields.device_id)) AS device_id
+              )
           ) AS fields
         )
     ) AS jsonPayload
@@ -24,4 +25,5 @@ WHERE
   )
   AND DATE(`timestamp`) = @submission_date
   AND jsonPayload.type = 'amplitudeEvent'
+  AND jsonPayload.logger IN ("fxa-auth-server", "fxa-content-server")
   AND jsonPayload.fields.event_type IS NOT NULL
--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_gcp_stdout_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_gcp_stdout_events_v1/query.sql
@@ -24,4 +24,5 @@ WHERE
   )
   AND DATE(`timestamp`) = @submission_date
   AND jsonPayload.type = 'amplitudeEvent'
+  AND jsonPayload.logger IN ("fxa-auth-server", "fxa-content-server")
   AND jsonPayload.fields.event_type IS NOT NULL

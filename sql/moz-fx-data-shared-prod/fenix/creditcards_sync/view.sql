8c78039061e25363afa87f72f17d332a72a84d83
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-09-18 18:20:03 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/fenix/creditcards_sync/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/creditcards_sync/view.sql
@@ -69,6 +69,28 @@ FROM
   `moz-fx-data-shared-prod.org_mozilla_fenix.creditcards_sync`
 UNION ALL
 SELECT
+  "org_mozilla_fenix_nightly" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_fenix_nightly",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix_nightly.creditcards_sync`
+UNION ALL
+SELECT
   "org_mozilla_fennec_aurora" AS normalized_app_id,
   mozfun.norm.fenix_app_info(
     "org_mozilla_fennec_aurora",

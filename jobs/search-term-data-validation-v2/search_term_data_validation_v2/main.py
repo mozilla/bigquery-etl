29915dc833b922f6a88a1f0b3680508bed89adcd
Chelsea Troy <chelseatroy@users.noreply.github.com>
2023-09-19 19:37:26 -0500

    Add more print statements to figure out when the job is hanging (#150)
    * Add more print statements to figure out when the job is hanging
    
    * Add yet another print

--- jobs/search-term-data-validation-v2/search_term_data_validation_v2/main.py
+++ jobs/search-term-data-validation-v2/search_term_data_validation_v2/main.py
@@ -1,3 +1,5 @@
+print("Look at me! I'm running a Python command! WOW!")
+
 import argparse
 import pandas as pd
 
@@ -5,6 +7,8 @@ from datetime import date, timedelta
 from collections import namedtuple
 
 from data_validation import retrieve_data_validation_metrics, record_validation_results
+
+print("Dependencies successfully imported!")
 
 parser = argparse.ArgumentParser(
     description="Validate Recent Search Input Against Historical Norms",
@@ -17,13 +21,17 @@ parser.add_argument(
     "--data_validation_reporting_destination",
     help="Table to store data validation metric test results",
 )
+print("Parser successfully instantiated!")
+
 args = parser.parse_args()
-print("Args successfully parsed")
+print("Args successfully parsed!")
 print(f"Data Validation Origin: {args.data_validation_origin}")
 print(f"Data Validation Reporting Destination: {args.data_validation_reporting_destination}")
 
+print("Retrieving Data Validation Metrics Now...")
 validation_df = retrieve_data_validation_metrics(args.data_validation_origin)
 print(f"Input Dataframe Shape: {validation_df.shape}")
 
+print("Recording validation results...")
 record_validation_results(validation_df, args.data_validation_reporting_destination)
 

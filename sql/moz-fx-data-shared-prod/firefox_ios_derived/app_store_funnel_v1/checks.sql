c2ae0e28042edfbd98c8114fc78d5e457257888a
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-09-21 10:47:27 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/checks.sql
@@ -1,3 +1,5 @@
+
+#fail
 WITH non_unique AS (
   SELECT
     COUNT(*) AS total_count
@@ -18,6 +20,9 @@ SELECT
     NULL
   );
 
+-- min_row_count helps us detect if we're seeing delays in the data arriving
+-- could also be an indicator of an upstream issue.
+#fail
 WITH min_row_count AS (
   SELECT
     COUNT(*) AS total_rows
@@ -38,3 +43,67 @@ SELECT
     ),
     NULL
   );
+
+#fail
+WITH _aua_new_profiles AS (
+  SELECT
+    SUM(new_profiles) AS new_profiles,
+  FROM
+    `moz-fx-data-shared-prod.firefox_ios.active_users_aggregates`
+  WHERE
+    submission_date = @submission_date
+    AND channel = "release"
+),
+_new_funnel_new_profiles AS (
+  SELECT
+    SUM(new_profiles) AS new_funnel_new_profiles,
+  FROM
+    `moz-fx-data-shared-prod.firefox_ios_derived.app_store_funnel_v1`
+  WHERE
+    `date` = @submission_date
+)
+SELECT
+  IF(
+    (SELECT * FROM _aua_new_profiles) - (SELECT * FROM _new_funnel_new_profiles) <> 0,
+    ERROR("There's a 'new_profiles' mismatch between active_users_aggregates and the funnel table"),
+    NULL
+  );
+
+#warn
+WITH base AS (
+  SELECT
+    SUM(total_downloads) AS total_downloads,
+    SUM(first_time_downloads) AS first_time_downloads,
+    SUM(redownloads) AS redownloads,
+  FROM
+    `moz-fx-data-shared-prod.firefox_ios_derived.app_store_funnel_v1`
+  WHERE
+    `date` = @submission_date
+)
+SELECT
+  IF(
+    total_downloads <> (first_time_downloads + redownloads),
+    ERROR("The sum of first time downloads and redownloads does not match that of total_downloads"),
+    NULL
+  )
+FROM
+  base;
+
+#warn
+WITH base AS (
+  SELECT
+    SUM(new_profiles) AS new_funnel_new_profiles,
+    SUM(total_downloads) AS total_downloads,
+  FROM
+    `moz-fx-data-shared-prod.firefox_ios_derived.app_store_funnel_v1`
+  WHERE
+    `date` = @submission_date
+)
+SELECT
+  IF(
+    new_funnel_new_profiles > total_downloads,
+    ERROR("There are more new_profiles than app downloads."),
+    NULL
+  )
+FROM
+  base;

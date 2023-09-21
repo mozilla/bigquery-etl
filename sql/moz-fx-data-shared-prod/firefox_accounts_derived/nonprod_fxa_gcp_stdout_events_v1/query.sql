69592dab81fcc45e261effc70e0568ce259f101a
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-09-21 16:54:17 +0200

    # feat(): updated fxa nonprod queries updated to be in line with production queries (#4297)
    * updated fxa nonprod/staging queries to be in line with what production queries look like
    
    * Apply suggestions from code review provided by srose
    
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>
    
    * tweaks made as suggested by srose in PR#4297
    
    ---------
    
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>

---
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/nonprod_fxa_gcp_stdout_events_v1/query.sql
@@ -0,0 +1,29 @@
+SELECT
+  -- example logger expected input: fxa-auth-server
+  SPLIT(jsonPayload.logger, "-")[OFFSET(1)] AS fxa_server,
+  * REPLACE (
+    (
+      SELECT AS STRUCT
+        jsonPayload.* REPLACE (
+          (
+            SELECT AS STRUCT
+              jsonPayload.fields.* REPLACE (
+                TO_HEX(SHA256(jsonPayload.fields.user_id)) AS user_id,
+                TO_HEX(SHA256(jsonPayload.fields.device_id)) AS device_id
+              )
+          ) AS fields
+        )
+    ) AS jsonPayload
+  )
+FROM
+  `moz-fx-fxa-nonprod.gke_fxa_stage_log.stdout`
+WHERE
+  (
+    DATE(_PARTITIONTIME)
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 1 DAY)
+    AND DATE_ADD(@submission_date, INTERVAL 1 DAY)
+  )
+  AND DATE(`timestamp`) = @submission_date
+  AND jsonPayload.type = 'amplitudeEvent'
+  AND jsonPayload.logger IN ("fxa-auth-server", "fxa-content-server")
+  AND jsonPayload.fields.event_type IS NOT NULL

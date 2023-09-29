38f9c26e0217a032fcf7bbf6aa51735f86980ff6
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-09-29 10:51:36 +0200

    feat(DENG-1578): docker_fxa_customs_sanitized_v2 query added and view updated (#4315)
    * added docker_fxa_customs_sanitized_v2 query to pull from the new fxa log table and updated the view to union v1 and v2
    
    * updated schema file for docker_fxa_customs_sanitized_v2
    
    * tweaks made as sugested by srose in PR#4315
    
    * added docker_fxa_customs_sanitized_v2 to skip list due to permissions
    
    * once again scheduling fxa.docker_fxa_customs_sanitized_v1 as AWS events appear to still be arriving
    
    * added schema for docker_fxa_customs_sanitized_v1
    
    * updated date filter to represent when we stopped receiving relevant events and descheduled v1

---
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/docker_fxa_customs_sanitized_v2/query.sql
@@ -0,0 +1,23 @@
+-- Query for firefox_accounts_derived.docker_fxa_customs_sanitized_v2
+SELECT
+  DATE(@submission_date) AS `date`,
+  * REPLACE (
+    (
+      SELECT AS STRUCT
+        jsonPayload.* REPLACE (SHA256(jsonPayload.email) AS email, SHA256(jsonPayload.ip) AS ip)
+    ) AS jsonPayload
+  )
+FROM
+  `moz-fx-fxa-prod.gke_fxa_prod_log.stderr`
+WHERE
+  (
+    DATE(_PARTITIONTIME)
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 1 DAY)
+    AND DATE_ADD(@submission_date, INTERVAL 1 DAY)
+  )
+  AND DATE(`timestamp`) = @submission_date
+  -- v2 consumes events from GCP based fxa service,
+  -- the date indicates when the migration to GCP started.
+  AND DATE(`timestamp`) >= "2023-09-07"
+  AND labels.k8s_pod_deployment = "customs"
+  AND resource.labels.container_name = "customs"

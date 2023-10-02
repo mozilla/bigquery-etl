d771b8ba0f925d082cb8f5b801a3bb708c3b4c7f
Daniel Thorn <dthorn@mozilla.com>
2023-10-02 14:25:22 -0700

    improve description and add context comment

--- sql/moz-fx-data-shared-prod/telemetry_derived/hcm_clients_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/hcm_clients_v1/query.sql
@@ -1,20 +1,33 @@
 SELECT
-  DATE(submission_timestamp) AS submission_date,
-  normalized_channel AS channel,
-  environment.system.os.name AS os,
+  DATE(main.submission_timestamp) AS submission_date,
+  main.normalized_channel AS channel,
+  main.environment.system.os.name AS os,
   COUNT(DISTINCT client_id) AS num_clients,
 FROM
-  `moz-fx-data-shared-prod`.telemetry_stable.main_v5
+  `moz-fx-data-shared-prod`.telemetry_stable.main_v5 AS main
 CROSS JOIN
-  UNNEST(payload.processes.parent.keyed_scalars.a11y_theme)
+  UNNEST(main.payload.processes.parent.keyed_scalars.a11y_theme) AS a11y_theme
 WHERE
   {% if is_init() %}
-    DATE(submission_timestamp) >= '2021-01-01'
+    DATE(main.submission_timestamp) >= '2021-01-01'
   {% else %}
-    DATE(submission_timestamp) = @submission_date
+    DATE(main.submission_timestamp) = @submission_date
   {% endif %}
-  AND value IS TRUE
-  AND key != "always"
+  /*
+  a11y_theme.value is TRUE when an OS high contrast mode (HCM) is present and FALSE otherwise,
+
+  a11y_theme.key is derived from the config pref `browser.display.document_color_use" which is has the
+  following values / meanings:
+  * "always" = always use document colors, regardless of OS HCM or colors chosen in the colors
+    dialog (about:preferences > manage colors)
+  * "never" = never use document colors (this is equivalent to "always use HCM colors")
+  * "default" = use document colors when OS HCM is off, and use OS HCM colors when OS HCM is on.
+
+  This condition selects users who have OS HCM on and who have not forced document colors everywhere
+  (ie. the OS HCM colors should propagate to web content).
+  */
+  AND a11y_theme.value IS TRUE
+  AND a11y_theme.key != "always"
 GROUP BY
   submission_date,
   channel,

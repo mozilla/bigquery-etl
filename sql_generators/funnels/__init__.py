714903759bded9e569325f45738c91071a98e70e
Anna Scholtz <anna@scholtzan.net>
2023-10-02 11:19:16 -0700

    Add funnel generation logic

---
+++ sql_generators/funnels/__init__.py
@@ -0,0 +1,69 @@
+"""Funnel generation."""
+import os
+from pathlib import Path
+import click
+import toml
+import re
+from jinja2 import Environment, FileSystemLoader
+
+from bigquery_etl.cli.utils import use_cloud_function_option
+from bigquery_etl.format_sql.formatter import reformat
+from bigquery_etl.util.common import write_sql
+from sql_generators.funnels.config import ConfigParser
+
+
+FILE_PATH = Path(os.path.dirname(__file__))
+TEMPLATES_PATH = FILE_PATH / "templates"
+
+def bq_normalize_name(name: str) -> str:
+    return re.sub(r"[^a-zA-Z0-9_]", "_", name)
+
+def generate_funnels(target_project, path, output_dir):
+    output_dir = Path(output_dir) / target_project
+
+    for config_file in path.glob("*.toml"):
+        config = toml.loads(config_file)
+        table_name = bq_normalize_name(config_file.stem)
+
+        env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
+        sql_template = env.get_template("funnel.sql")
+        funnel_sql = reformat(sql_template.render(
+            funnels=config["funnels"],
+            steps=config["steps"],
+            data_sources=config["data_sources"]
+        ))
+        write_sql(
+            output_dir=output_dir,
+            full_table_id=f"{target_project}.{config['destination_dataset']}.{table_name}",
+            basename="query.sql",
+            sql=funnel_sql,
+            skip_existing=False
+        )
+
+
+@click.command("generate")
+@click.option(
+    "--target-project",
+    "--target_project",
+    help="Which project the queries should be written to.",
+    default="moz-fx-data-shared-prod",
+)
+@click.option(
+    "--path",
+    help="Where query directories will be searched for.",
+    default="sql_generators/funnels/configs",
+    required=False,
+    type=click.Path(file_okay=False),
+)
+@click.option(
+    "--output-dir",
+    "--output_dir",
+    help="The location to write to. Defaults to sql/.",
+    default=Path("sql"),
+    type=click.Path(file_okay=False),
+)
+@use_cloud_function_option
+def generate(target_project, path, output_dir, use_cloud_function):
+    """Generate the experiment monitoring views."""
+    output_dir = Path(output_dir)
+    generate_funnels(target_project, path, output_dir)
---
+++ sql_generators/funnels/templates/funnel.sql
@@ -0,0 +1,62 @@
+{% for funnel_name, funnel in funnels %}
+{% if loop.first %}WITH{%endif%}
+{% for step_name in funnel["steps"] %}
+{{ funnel_name }}_{{ step_name }} AS (
+  SELECT
+    {% if steps[step_name]["join_key"] %}
+    {{ steps[step_name]["join_key"] }} AS join_key,
+    {% endif %}
+    {{ data_sources[steps[step_name]["data_source"]]["submission_date_column"] }} AS submission_date,
+    {{ steps[step_name].column }} AS column
+  FROM 
+    {{ data_sources[steps[step_name]["data_source"]]["select_expression"] }}
+  {% if not loop.first and steps[step_name]["depends_on_previous_step"] %}
+  INNER JOIN {{ funnel_name }}_{{ loop.previtem[0] }} AS prev
+  ON 
+    prev.submission_date = submission_date AND
+    prev["join_key"] = join_key
+  {% endif %}
+  WHERE
+    {{ data_sources[steps[step_name]["data_source"]]["submission_date_column"] }} = @submission_date
+    {% if steps[step_name]["where_expression"] %}
+    AND {{ steps[step_name]["where_expression"] }}
+    {% endif %}
+),
+{% endfor %}
+{% endfor %}
+
+{% for funnel_name, funnel in funnels %}
+{% for step_name in funnel["steps"] %}
+{{ funnel_name }}_{{ step_name }}_aggregated AS (
+  SELECT
+    submission_date,
+    "{{ funnel_name }}" AS funnel,
+    {{ steps[step_name]["aggregation"] }}(column) AS aggregated
+  FROM
+    {{ funnel_name }}_{{ step_name }}
+  GROUP BY
+    submission_date, 
+    funnel
+){% if not loop.last %},{% endif %}
+{% endfor %}
+{% endfor %}
+
+SELECT
+  submission_date,
+  funnel,
+  {% for funnel_name, funnel in funnels %}
+    {% for step_name in funnel["steps"] %}
+    {{ funnel_name }}_{{ step_name }}_aggregated.aggregated AS {{ step_name }},
+    {% endfor %}
+  {% endfor %}
+FROM
+{% for funnel_name, funnel in funnels %}
+  {% for step_name in funnel["steps"] %}
+    {% if loop.first %}
+    {{ funnel_name }}_{{ step_name }}_aggregated
+    {% else %}
+    FULL OUTER JOIN {{ funnel_name }}_{{ step_name }}_aggregated
+    USING (submission_date, funnel) 
+    {% endif %}
+  {% endfor %}
+{% endfor %}

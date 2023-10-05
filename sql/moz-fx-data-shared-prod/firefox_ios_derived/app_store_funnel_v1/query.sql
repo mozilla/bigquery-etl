34dc9447feb8fe86417e16f5dd0c06fc449e34dd
kik-kik <kignasiak@mozilla.com>
2023-10-05 14:18:36 +0200

    modified firefox_ios_derived.app_store_funnel_v1 to be an incremental job

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/query.sql
@@ -1,3 +1,4 @@
+-- TODO: should we run this job with 7 day delay to make sure all data landed (a wider window to be on the safe side).
 WITH views_data AS (
   SELECT
     `date`,
@@ -6,7 +7,7 @@ WITH views_data AS (
   FROM
     app_store.firefox_app_store_territory_source_type_report
   WHERE
-    `date` >= '2022-01-01'
+    `date` = DATE_SUB(@submission_date, INTERVAL 7 DAY)
   GROUP BY
     `date`,
     country_name
@@ -21,7 +22,7 @@ downloads_data AS (
   FROM
     app_store.firefox_downloads_territory_source_type_report
   WHERE
-    `date` >= '2022-01-01'
+    `date` = DATE_SUB(@submission_date, INTERVAL 7 DAY)
     AND source_type <> 'Institutional Purchase'
   GROUP BY
     `date`,
@@ -50,19 +51,20 @@ store_stats AS (
 -- instead to avoid the other funnel query using a different source.
 _new_profiles AS (
   SELECT
-    submission_date AS `date`,
-    country,
-    SUM(new_profiles) AS new_profiles,
+    first_seen_date AS `date`,
+    first_reported_country AS country,
+    COUNT(*) AS new_profiles,
   FROM
-    firefox_ios.active_users_aggregates
+    firefox_ios.firefox_ios_clients
   WHERE
-    submission_date >= '2022-01-01'
+    first_seen_date = DATE_SUB(@submission_date, INTERVAL 7 DAY)
     AND channel = "release"
   GROUP BY
     `date`,
     country
 )
 SELECT
+  @submission_date AS submission_date,
   `date`,
   country,
   COALESCE(views, 0) AS views,

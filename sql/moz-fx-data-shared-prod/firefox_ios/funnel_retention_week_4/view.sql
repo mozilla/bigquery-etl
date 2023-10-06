d95cf7e58b678eef525c81cc4a943fa0451eb02b
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-10-06 15:46:37 +0200

    feat(): renaming funnel_retention week_2 and week_4 and aggregates tables (#4386)
    * renaming funnel_retention week_2 and week_4 to include clients (client level table) and removing aggregates from funnel_retention_week_4_aggregates_v1
    
    * fixed typo in one of the retention queries
    
    * made tweaks to field descriptions as suggested by lvargas

---
+++ sql/moz-fx-data-shared-prod/firefox_ios/funnel_retention_week_4/view.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_ios.funnel_retention_week_4`
+AS
+SELECT
+  *
+FROM `moz-fx-data-shared-prod.firefox_ios_derived.funnel_retention_week_4_v1`
--- sql/moz-fx-data-shared-prod/firefox_ios_derived/funnel_retention_week_4_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/funnel_retention_week_4_v1/query.sql
@@ -1,61 +1,25 @@
-WITH clients_retention AS (
-  SELECT
-    submission_date,
-    client_id,
-    sample_id,
-    days_seen_bits,
-  FROM
-    firefox_ios.baseline_clients_last_seen
-  WHERE
-    submission_date = @submission_date
-    AND normalized_channel = "release"
-),
-clients_first_seen AS (
-  SELECT
-    first_seen_date,
-    client_id,
-    sample_id,
-    first_reported_country,
-    first_reported_isp,
-    adjust_ad_group,
-    adjust_campaign,
-    adjust_creative,
-    adjust_network,
-  FROM
-    firefox_ios.firefox_ios_clients
-  WHERE
-    -- 28 days need to elapse before calculating the week 4 and day 28 retention metrics
-    first_seen_date = DATE_SUB(@submission_date, INTERVAL 27 DAY)
-    AND channel = "release"
-    -- filtering out suspicious devices on iOS, for more info see: bug-1846554
-    AND NOT is_suspicious_device_client
-),
-retention_calculation AS (
-  SELECT
-    clients_first_seen.*,
-    BIT_COUNT(clients_retention.days_seen_bits) AS days_seen_in_first_28_days,
-    mozfun.bits28.retention(clients_retention.days_seen_bits, @submission_date) AS retention,
-  FROM
-    clients_retention
-  INNER JOIN
-    clients_first_seen
-  USING
-    (client_id, sample_id)
-)
 SELECT
-  @submission_date AS submission_date,
-  * EXCEPT (retention) REPLACE(
-    -- metric date should align with first_seen_date, if that is not the case then the query will fail.
-    IF(
-      retention.day_27.metric_date <> first_seen_date,
-      ERROR("Metric date misaligned with first_seen_date"),
-      first_seen_date
-    ) AS first_seen_date
-  ),
-  days_seen_in_first_28_days > 1 AS repeat_first_month_user,
-  -- retention UDF works on 0 index basis, that's why for example week_1 is aliased as week 2 to make it a bit more user friendly.
-  -- retained_week_2 added for testing.
-  retention.day_27.active_in_week_1 AS retained_week_2,
-  retention.day_27.active_in_week_3 AS retained_week_4,
+  submission_date,
+  first_seen_date,
+  first_reported_country,
+  first_reported_isp,
+  adjust_ad_group,
+  adjust_campaign,
+  adjust_creative,
+  adjust_network,
+  COUNT(*) AS new_profiles,
+  COUNTIF(repeat_first_month_user) AS repeat_user,
+  COUNTIF(retained_week_4) AS retained_week_4,
 FROM
-  retention_calculation
+  firefox_ios_derived.funnel_retention_clients_week_4_v1
+WHERE
+  submission_date = @submission_date
+GROUP BY
+  submission_date,
+  first_seen_date,
+  first_reported_country,
+  first_reported_isp,
+  adjust_ad_group,
+  adjust_campaign,
+  adjust_creative,
+  adjust_network

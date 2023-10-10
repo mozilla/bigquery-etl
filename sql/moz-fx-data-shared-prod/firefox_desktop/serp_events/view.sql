08d70e8639e7aa6ee539d1706f7c6f306125fc4c
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-10-10 18:56:37 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/serp_events/view.sql
@@ -0,0 +1,8 @@
+--- User-facing view. Generated via sql_generators.active_users.
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.serp_events`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.serp_events_v1`

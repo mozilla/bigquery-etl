63e2b97daffad34aa45cd11943d7485ba0ff01ba
kik-kik <kignasiak@mozilla.com>
2023-10-10 13:30:23 +0200

    added v2 of docker_fxa_admin_server_events

--- sql/moz-fx-data-shared-prod/firefox_accounts/docker_fxa_admin_server_sanitized/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/docker_fxa_admin_server_sanitized/view.sql
@@ -2,6 +2,13 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_accounts.docker_fxa_admin_server_sanitized`
 AS
 SELECT
-  *
+  * EXCEPT(resource, labels, jsonPayload)
 FROM
   `moz-fx-data-shared-prod.firefox_accounts_derived.docker_fxa_admin_server_sanitized_v1`
+UNION ALL
+
+-- TODO, try alligning those fields as much as possible
+SELECT
+  * EXCEPT(resource, labels, jsonPayload)
+FROM
+  `moz-fx-data-shared-prod.firefox_accounts_derived.docker_fxa_admin_server_sanitized_v2`

109b9f0835f050632f6136b16883c4433736bc25
kik-kik <42538694+kik-kik@users.noreply.github.com>
2023-10-13 16:42:02 +0200

    feat(DENG-1599): added ETL checks related to firefox_ios_derived.new_profile_activation_v2 (#4411)
    * added ETL checks related to firefox_ios_derived.new_profile_activation_v2
    
    * regenerated bqetl_firefox_ios DAG

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/firefox_ios_clients_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/firefox_ios_clients_v1/checks.sql
@@ -12,6 +12,23 @@ SELECT
     NULL
   )
 FROM
-  `moz-fx-data-shared-prod.firefox_ios_derived.firefox_ios_clients_v1`
+  `{{ project_id }}.{{ dataset_id }}.{{ table_name }}`
 WHERE
   first_seen_date = @submission_date;
+#warn
+WITH base AS (
+  SELECT COUNTIF(is_activated)
+  FROM `{{ project_id }}.{{ dataset_id }}.{{ table_name }}`
+  WHERE first_seen_date = @submission_date
+),
+upstream AS (
+  SELECT COUNTIF(is_activated)
+  FROM `{{ project_id }}.{{ dataset_id }}.new_profile_activation_v2`
+  WHERE first_seen_date = @submission_date
+)
+SELECT
+  IF(
+    (SELECT * FROM base) <> (SELECT * FROM upstream),
+    ERROR(CONCAT("Number of activations does not match up that of the upstream table. Upstream count: ", (SELECT * FROM upstream), ", base count: ", (SELECT * FROM base))),
+    NULL
+  );

6275a9c7c26342b84ea2697e6e21955b5fb023d9
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-10-17 21:19:50 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop_background_defaultagent_derived/baseline_clients_last_seen_v1/checks.sql
@@ -0,0 +1,23 @@
+
+-- Generated via bigquery_etl.glean_usage
+#fail
+WITH non_unique AS (
+  SELECT
+    COUNT(*) AS total_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_background_defaultagent_derived.baseline_clients_last_seen_v1`
+  WHERE
+    submission_date = @submission_date
+  GROUP BY
+    client_id
+  HAVING
+    total_count > 1
+)
+SELECT
+  IF(
+    (SELECT COUNT(*) FROM non_unique) > 0,
+    ERROR(
+      "Duplicates detected (Expected combined set of values for columns ['client_id'] to be unique.)"
+    ),
+    NULL
+  );

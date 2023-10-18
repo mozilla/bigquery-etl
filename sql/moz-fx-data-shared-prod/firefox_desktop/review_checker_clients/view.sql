00ede22dc75cbbbed17928e7c4fb53bb13578a54
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-10-18 21:13:29 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/review_checker_clients/view.sql
@@ -0,0 +1,8 @@
+--- User-facing view. Generated via sql_generators.active_users.
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.review_checker_clients`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.review_checker_clients_v1`
---
+++ sql/moz-fx-data-shared-prod/org_mozilla_firefox_vpn/baseline_clients_last_seen/view.sql
@@ -0,0 +1,18 @@
+-- Generated via bigquery_etl.glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_firefox_vpn.baseline_clients_last_seen`
+AS
+SELECT
+  `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(days_seen_bits) AS days_since_seen,
+  `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(
+    days_created_profile_bits
+  ) AS days_since_created_profile,
+  `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(
+    days_seen_session_start_bits
+  ) AS days_since_seen_session_start,
+  `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(
+    days_seen_session_end_bits
+  ) AS days_since_seen_session_end,
+  *
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_vpn_derived.baseline_clients_last_seen_v1`

7d22a1baea2d011deb1c8277253384a8a70d81fb
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-10-19 05:22:38 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix/fx_suggest/view.sql
@@ -0,0 +1,12 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_fenix.fx_suggest`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (SELECT AS STRUCT metrics.*, metrics.url2 AS url) AS metrics
+  )
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix_stable.fx_suggest_v1`

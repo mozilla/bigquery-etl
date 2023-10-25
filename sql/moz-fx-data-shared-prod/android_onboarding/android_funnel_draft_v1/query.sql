007f519989fb54e23e48865a143f8f7e2f8b53b1
irrationalagent <leifdoines@gmail.com>
2023-10-25 10:42:33 -0600

    add completed event

--- sql/moz-fx-data-shared-prod/android_onboarding/android_funnel_draft_v1/query.sql
+++ sql/moz-fx-data-shared-prod/android_onboarding/android_funnel_draft_v1/query.sql
@@ -66,6 +66,7 @@ WITH onboarding_funnel_first_card AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND mozfun.map.get_key(extra, 'sequence_position') = '1'
+    AND e.name != 'completed'
 ),
 onboarding_funnel_second_card AS (
   SELECT
@@ -139,6 +140,7 @@ onboarding_funnel_second_card AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND mozfun.map.get_key(extra, 'sequence_position') = '2'
+    AND e.name != 'completed'
 ),
 onboarding_funnel_third_card AS (
   SELECT
@@ -212,6 +214,80 @@ onboarding_funnel_third_card AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND mozfun.map.get_key(extra, 'sequence_position') = '3'
+    AND e.name != 'completed'
+),
+onboarding_funnel_onboarding_completed AS (
+  SELECT
+    client_info.client_id AS join_key,
+    mozfun.map.get_key(extra, 'sequence_id') AS funnel_id,
+    onboarding_funnel_funnel_id.action AS action,
+    onboarding_funnel_action.element_type AS element_type,
+    DATE(submission_timestamp) AS submission_date,
+    client_info.client_id AS client_id,
+    client_info.client_id AS column
+  FROM
+    (
+      SELECT
+        *
+      FROM
+        mozdata.fenix.events ev
+      CROSS JOIN
+        UNNEST(events) e
+      WHERE
+        e.category = 'onboarding'
+    )
+  INNER JOIN
+    onboarding_funnel_third_card AS prev
+  ON
+    prev.submission_date = DATE(submission_timestamp)
+    AND prev.join_key = client_info.client_id
+  LEFT JOIN
+    (
+      SELECT
+        DATE(submission_timestamp) AS submission_date,
+        client_info.client_id AS client_id,
+        mozfun.map.get_key(extra, 'action') AS action
+      FROM
+        (
+          SELECT
+            *
+          FROM
+            mozdata.fenix.events ev
+          CROSS JOIN
+            UNNEST(events) e
+          WHERE
+            e.category = 'onboarding'
+        )
+      WHERE
+        DATE(submission_timestamp) = @submission_date
+    ) AS dimension_source_action
+  ON
+    dimension_source_action.client_id = client_id
+  LEFT JOIN
+    (
+      SELECT
+        DATE(submission_timestamp) AS submission_date,
+        client_info.client_id AS client_id,
+        mozfun.map.get_key(extra, 'element_type') AS element_type
+      FROM
+        (
+          SELECT
+            *
+          FROM
+            mozdata.fenix.events ev
+          CROSS JOIN
+            UNNEST(events) e
+          WHERE
+            e.category = 'onboarding'
+        )
+      WHERE
+        DATE(submission_timestamp) = @submission_date
+    ) AS dimension_source_element_type
+  ON
+    dimension_source_element_type.client_id = client_id
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND e.name = 'completed'
 ),
 -- aggregate each funnel step value
 onboarding_funnel_first_card_aggregated AS (
@@ -265,6 +341,23 @@ onboarding_funnel_third_card_aggregated AS (
     submission_date,
     funnel
 ),
+onboarding_funnel_onboarding_completed_aggregated AS (
+  SELECT
+    submission_date,
+    "onboarding_funnel" AS funnel,
+    funnel_id,
+    action,
+    element_type,
+    COUNT(DISTINCT column) AS aggregated
+  FROM
+    onboarding_funnel_onboarding_completed
+  GROUP BY
+    funnel_id,
+    action,
+    element_type,
+    submission_date,
+    funnel
+),
 -- merge all funnels so results can be written into one table
 merged_funnels AS (
   SELECT
@@ -276,6 +369,7 @@ merged_funnels AS (
     COALESCE(onboarding_funnel_first_card_aggregated.aggregated) AS first_card,
     COALESCE(onboarding_funnel_second_card_aggregated.aggregated) AS second_card,
     COALESCE(onboarding_funnel_third_card_aggregated.aggregated) AS third_card,
+    COALESCE(onboarding_funnel_onboarding_completed_aggregated.aggregated) AS onboarding_completed,
   FROM
     onboarding_funnel_first_card_aggregated
   FULL OUTER JOIN
@@ -284,6 +378,10 @@ merged_funnels AS (
     (submission_date, funnel)
   FULL OUTER JOIN
     onboarding_funnel_third_card_aggregated
+  USING
+    (submission_date, funnel)
+  FULL OUTER JOIN
+    onboarding_funnel_onboarding_completed_aggregated
   USING
     (submission_date, funnel)
 )

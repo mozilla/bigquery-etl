dd6d372092bb1af09f92e186ee28baea1c1b3915
Alekhya Kommasani <akommasani@mozilla.com>
2023-10-25 12:00:57 -0400

    Incorporate feedback by Curtis
    Incorporate feedback from Curtis

--- sql/moz-fx-data-shared-prod/firefox_desktop/urlbar_events_temp_v2/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/urlbar_events_temp_v2/view.sql
@@ -1,6 +1,6 @@
 --- User-facing view. Generated via sql_generators.active_users.
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_desktop.urlbar_events_view_v2`
+  `moz-fx-data-shared-prod.firefox_desktop.urlbar_events_temp_v2`
 AS
 SELECT
   *
--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/urlbar_events_v2/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/urlbar_events_v2/query.sql
@@ -1,41 +1,27 @@
 CREATE TEMP FUNCTION enumerated_array(results ARRAY<STRING>, _groups ARRAY<STRING>) AS (
-  ARRAY(SELECT STRUCT(
-    off + 1 AS position,
-    r AS result_type,
-    `mozfun.norm.result_type_to_product_name`(r) as product_result_type,
-    _groups[SAFE_OFFSET(off)] AS result_group
-  ) FROM UNNEST(results) AS r WITH OFFSET off)
+  ARRAY(
+    SELECT
+      STRUCT(
+        off + 1 AS position,
+        r AS result_type,
+        `mozfun.norm.result_type_to_product_name`(r) AS product_result_type,
+        _groups[SAFE_OFFSET(off)] AS result_group
+      )
+    FROM
+      UNNEST(results) AS r
+      WITH OFFSET off
+  )
 );
 
 CREATE TEMP FUNCTION get_event_action(event_name STRING, engagement_type STRING) AS (
- CASE
+  CASE
     WHEN event_name = 'engagement'
-      AND (
-        (
-          engagement_type IN (
-            "click",
-            "drop_go",
-            "enter",
-            "go_button",
-            "paste_go"
-          )
-        )
-      )
+      AND (engagement_type IN ("click", "drop_go", "enter", "go_button", "paste_go"))
       THEN 'engaged'
     WHEN event_name = 'abandonment'
       THEN 'abandoned'
     WHEN event_name = 'engagement'
-      AND (
-        (
-          engagement_type NOT IN (
-            "click",
-            "drop_go",
-            "enter",
-            "go_button",
-            "paste_go"
-          )
-        )
-      )
+      AND (engagement_type NOT IN ("click", "drop_go", "enter", "go_button", "paste_go"))
       THEN "annoyance"
     ELSE NULL
   END
@@ -63,37 +49,42 @@ CREATE TEMP FUNCTION get_is_terminal(selected_result STRING, engagement_type STR
 );
 
 WITH events_unnested AS (
-SELECT
-  DATE(submission_timestamp) AS submission_date,
-  client_info.client_id AS glean_client_id,
-  metrics.uuid.legacy_telemetry_client_id AS legacy_telemetry_client_id,
-  sample_id,
-  name AS event_name,
-  timestamp AS event_timestamp,
-  GENERATE_UUID() AS event_id,
-  ping_info.experiments,
-  ping_info.seq,
-  normalized_channel,
-  normalized_country_code,
-  `moz-fx-data-shared-prod`.udf.normalize_search_engine(mozfun.map.get_key(extra, "search_engine_default_id")) AS normalized_engine,
-  COALESCE(metrics.boolean.urlbar_pref_suggest_data_collection, FALSE) AS pref_data_collection,
-  COALESCE(metrics.boolean.urlbar_pref_suggest_sponsored, FALSE) AS pref_sponsored_suggestions,
-  COALESCE(metrics.boolean.urlbar_pref_suggest_nonsponsored, FALSE) AS pref_fx_suggestions,
-  mozfun.map.get_key(extra, "engagement_type") AS engagement_type,
-  mozfun.map.get_key(extra, "interaction") AS interaction,
-  CAST(mozfun.map.get_key(extra, "n_chars") AS int) AS num_chars_typed,
-  CAST(mozfun.map.get_key(extra, "n_results") AS int) AS num_total_results,
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    client_info.client_id AS glean_client_id,
+    metrics.uuid.legacy_telemetry_client_id AS legacy_telemetry_client_id,
+    sample_id,
+    name AS event_name,
+    timestamp AS event_timestamp,
+    GENERATE_UUID() AS event_id,
+    ping_info.experiments,
+    ping_info.seq,
+    normalized_channel,
+    normalized_country_code,
+    `moz-fx-data-shared-prod`.udf.normalize_search_engine(
+      mozfun.map.get_key(extra, "search_engine_default_id")
+    ) AS normalized_engine,
+    COALESCE(metrics.boolean.urlbar_pref_suggest_data_collection, FALSE) AS pref_data_collection,
+    COALESCE(metrics.boolean.urlbar_pref_suggest_sponsored, FALSE) AS pref_sponsored_suggestions,
+    COALESCE(metrics.boolean.urlbar_pref_suggest_nonsponsored, FALSE) AS pref_fx_suggestions,
+    mozfun.map.get_key(extra, "engagement_type") AS engagement_type,
+    mozfun.map.get_key(extra, "interaction") AS interaction,
+    CAST(mozfun.map.get_key(extra, "n_chars") AS int) AS num_chars_typed,
+    CAST(mozfun.map.get_key(extra, "n_results") AS int) AS num_total_results,
   --If 0, then no result was selected.
-  NULLIF(CAST(mozfun.map.get_key(extra, "selected_position") AS int), 0) AS selected_position,
-  mozfun.map.get_key(extra, "selected_result") AS selected_result,
-  enumerated_array(SPLIT(mozfun.map.get_key(extra, "results"), ','), SPLIT(mozfun.map.get_key(extra, "groups"), ',')) AS results,
+    NULLIF(CAST(mozfun.map.get_key(extra, "selected_position") AS int), 0) AS selected_position,
+    mozfun.map.get_key(extra, "selected_result") AS selected_result,
+    enumerated_array(
+      SPLIT(mozfun.map.get_key(extra, "results"), ','),
+      SPLIT(mozfun.map.get_key(extra, "groups"), ',')
+    ) AS results,
   FROM
     `moz-fx-data-shared-prod.firefox_desktop_stable.events_v1`,
-    UNNEST(events)
+    UNNEST(events) AS event
   WHERE
     DATE(submission_timestamp) = @submission_date
-    AND category = 'urlbar'
-    AND name IN ('engagement', 'abandonment')
+    AND event.category = 'urlbar'
+    AND event.name IN ('engagement', 'abandonment')
 )
 SELECT
   *,
@@ -111,7 +102,7 @@ SELECT
     ELSE NULL
   END AS product_engaged_result_type,
   CASE
-    WHEN  get_event_action(event_name, engagement_type) = 'annoyance'
+    WHEN get_event_action(event_name, engagement_type) = 'annoyance'
       THEN engagement_type
     ELSE NULL
   END AS annoyance_signal_type,

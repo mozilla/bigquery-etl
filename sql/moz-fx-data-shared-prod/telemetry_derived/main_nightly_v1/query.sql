57670281e10b15e6e04b148a13464fa78fedca1a
Daniel Thorn <dthorn@mozilla.com>
2023-10-26 09:44:57 -0700

    DENG-476 - Update sampled main ping tables to reference main_v5 (#4433)

--- sql/moz-fx-data-shared-prod/telemetry_derived/main_nightly_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/main_nightly_v1/query.sql
@@ -8,7 +8,7 @@ SELECT
     `moz-fx-data-shared-prod.udf.normalize_main_payload`(payload) AS payload
   )
 FROM
-  telemetry_stable.main_v4
+  telemetry_stable.main_v5
 WHERE
   normalized_channel = 'nightly'
   AND DATE(submission_timestamp) = @submission_date

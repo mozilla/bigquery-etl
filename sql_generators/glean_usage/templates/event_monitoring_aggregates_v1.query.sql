5712712fd713a91635ffd6b4574d61d10b4db4fe
Anna Scholtz <anna@scholtzan.net>
2023-11-01 14:20:20 -0700

    Materialized views and aggregated tables for event monitoring (#4478)
    * WIP event monitoring
    
    * Add FxA custom events to view definition (#4483)
    
    * Add FxA custom events to view definition
    
    * Update sql_generators/event_monitoring/templates/event_monitoring_live.init.sql
    
    * Update sql_generators/event_monitoring/templates/event_monitoring_live.init.sql
    
    * Update sql_generators/event_monitoring/templates/event_monitoring_live.init.sql
    
    * Update sql_generators/event_monitoring/templates/event_monitoring_live.init.sql
    
    ---------
    
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>
    
    * Move event monitoring to glean_usage generator
    
    * Add cross-app event monitoring view
    
    * Generate cross app monitoring
    
    * Simplyfy event monitoring aggregation
    
    ---------
    
    Co-authored-by: akkomar <akkomar@users.noreply.github.com>

---
+++ sql_generators/glean_usage/templates/event_monitoring_aggregates_v1.query.sql
@@ -0,0 +1,112 @@
+-- Generated via ./bqetl generate glean_usage
+{% for app in apps %}
+{% set outer_loop = loop -%}
+{% for dataset in app -%}
+{% if dataset['bq_dataset_family'] not in ["telemetry", "accounts_frontend", "accounts_backend"] %}
+  {% if not outer_loop.first -%}
+  UNION ALL
+  {% endif -%}
+  SELECT
+    @submission_date AS submission_date,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time), HOUR),
+    -- Aggregates event counts over 30-minute intervals
+      INTERVAL(
+        DIV(
+          EXTRACT(MINUTE FROM SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time)),
+          60
+        ) * 60
+      ) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time), HOUR),
+      INTERVAL(
+        (
+          DIV(
+            EXTRACT(MINUTE FROM SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time)),
+            60
+          ) + 1
+        ) * 60
+      ) MINUTE
+    ) AS window_end,
+    event.category AS event_category,
+    event.name AS event_name,
+    event_extra.key AS event_extra_key,
+    normalized_country_code AS country,
+    "{{ dataset['canonical_app_name'] }}" AS normalized_app_name,
+    {% if app_name == "fenix" -%}
+    mozfun.norm.fenix_app_info("{{ dataset['bq_dataset_family'] }}", app_build).channel AS normalized_channel,
+    {% else %}
+    "{{ dataset.get('app_channel', 'release') }}" AS normalized_channel,
+    {% endif %}
+    client_info.app_display_version AS version,
+    COUNT(*) AS total_events
+  FROM
+    `{{ project_id }}.{{ dataset['bq_dataset_family'] }}_stable.events_v1`
+  CROSS JOIN
+    UNNEST(events) AS event,
+    UNNEST(event.extra) AS event_extra
+  WHERE DATE(submission_timestamp) = @submission_date
+  GROUP BY
+    submission_date,
+    window_start,
+    window_end,
+    event_category,
+    event_name,
+    event_extra_key,
+    country,
+    normalized_app_name,
+    normalized_channel,
+    version
+{% elif dataset in ["accounts_frontend", "accounts_backend"] %}
+  {% if not outer_loop.first -%}
+  UNION ALL
+  {% endif -%}
+      -- FxA uses custom pings to send events without a category and extras.
+  SELECT
+    @submission_date AS submission_date,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time), HOUR),
+      -- Aggregates event counts over 30-minute intervals
+      INTERVAL(
+        DIV(
+          EXTRACT(MINUTE FROM SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time)),
+          60
+        ) * 60
+      ) MINUTE
+    ) AS window_start,
+    TIMESTAMP_ADD(
+      TIMESTAMP_TRUNC(SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time), HOUR),
+      INTERVAL(
+        (
+          DIV(
+            EXTRACT(MINUTE FROM SAFE.PARSE_TIMESTAMP('%FT%H:%M%Ez', ping_info.start_time)),
+            60
+          ) + 1
+        ) * 60
+      ) MINUTE
+    ) AS window_end,
+    NULL AS event_category,
+    metrics.string.event_name,
+    NULL AS event_extra_key,
+    normalized_country_code AS country,
+    "{{ dataset['canonical_app_name'] }}" AS normalized_app_name,
+    normalized_channel,
+    client_info.app_display_version AS VERSION,
+    COUNT(*) AS total_events
+  FROM
+    `{{ project_id }}.{{ dataset['bq_dataset_family'] }}_stable.accounts_events_v1`
+  WHERE DATE(submission_timestamp) = @submission_date
+  GROUP BY
+    window_start,
+    window_end,
+    event_category,
+    event_name,
+    event_extra_key,
+    country,
+    normalized_app_name,
+    normalized_channel,
+    version
+{% endif %}
+{% endfor %}
+{% endfor %}

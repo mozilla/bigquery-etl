47f9051596dd7a0de34a0a6009912cf91c655566
Lucia Vargas <lvargas@mozilla.com>
2023-11-01 17:20:43 +0100

    Update dags.

--- dags/bqetl_firefox_ios.py
+++ dags/bqetl_firefox_ios.py
@@ -353,6 +353,21 @@ with DAG(
     firefox_ios_derived__attributable_clients__v1.set_upstream(
         checks__fail_firefox_ios_derived__firefox_ios_clients__v1
     )
+    wait_for_copy_deduplicate_all = ExternalTaskSensor(
+        task_id="wait_for_copy_deduplicate_all",
+        external_dag_id="copy_deduplicate",
+        external_task_id="copy_deduplicate_all",
+        execution_delta=datetime.timedelta(seconds=10800),
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    firefox_ios_derived__attributable_clients__v1.set_upstream(
+        wait_for_copy_deduplicate_all
+    )
 
     firefox_ios_derived__firefox_ios_clients__v1.set_upstream(
         wait_for_baseline_clients_daily

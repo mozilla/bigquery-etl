b4d3fbc3d1d67588695cccfefe6516d4f943941f
Leli Schiestl <lschiestl@mozilla.com>
2023-11-06 16:48:42 +0100

    DENG-1662 move from google_ads connector to ads_google_mmc connector

--- sql/moz-fx-data-shared-prod/google_ads_derived/campaign_conversions_by_date_v1/query.sql
+++ sql/moz-fx-data-shared-prod/google_ads_derived/campaign_conversions_by_date_v1/query.sql
@@ -1,17 +1,21 @@
 WITH conversion_counts AS (
   SELECT
     date,
+    customer_id as account_id,
     campaign_id,
     SUM(biddable_app_install_conversions) AS installs,
     SUM(biddable_app_post_install_conversions) AS conversions,
   FROM
-    `moz-fx-data-bq-fivetran`.google_ads.campaign_conversions_by_date
+    `moz-fx-data-bq-fivetran`.ads_google_mmc.campaign_conversions_by_date
   GROUP BY
     date,
+    account_id,
     campaign_id
 )
 SELECT
   date,
+  account_name,
+  account_id,
   campaign_name,
   campaign_id,
   installs,
@@ -21,4 +25,4 @@ FROM
 JOIN
   `moz-fx-data-shared-prod`.google_ads_derived.campaign_names_map_v1
 USING
-  (campaign_id)
+  (campaign_id, account_id)

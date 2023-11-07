3db89bf42d90fe754bab555e9d066fcd73064ece
Sean Rose <srose@mozilla.com>
2023-11-07 15:03:39 -0800

    Incorporate PayPal refunds in `has_refunds` column logic.

--- sql/moz-fx-data-shared-prod/subscription_platform_derived/stripe_subscriptions_history_v2/query.sql
+++ sql/moz-fx-data-shared-prod/subscription_platform_derived/stripe_subscriptions_history_v2/query.sql
@@ -56,15 +56,7 @@ subscriptions_history AS (
     AND subscriptions_history.valid_from < customers_history.valid_from
     AND subscriptions_history.valid_to > customers_history.valid_from
 ),
-paypal_subscriptions AS (
-  SELECT DISTINCT
-    subscription_id
-  FROM
-    `moz-fx-data-shared-prod.stripe_external.invoice_v1`
-  WHERE
-    JSON_VALUE(metadata, '$.paypalTransactionId') IS NOT NULL
-),
-subscriptions_history_charge_summaries AS (
+subscriptions_history_invoice_summaries AS (
   SELECT
     history.id AS subscriptions_history_id,
     ARRAY_AGG(
@@ -76,7 +68,11 @@ subscriptions_history_charge_summaries AS (
       LIMIT
         1
     )[SAFE_ORDINAL(1)] AS latest_card_country,
-    LOGICAL_OR(refunds.status = 'succeeded') AS has_refunds,
+    LOGICAL_OR(JSON_VALUE(invoices.metadata, '$.paypalTransactionId') IS NOT NULL) AS uses_paypal,
+    LOGICAL_OR(
+      refunds.status = 'succeeded'
+      OR JSON_VALUE(invoices.metadata, '$.paypalRefundTransactionId') IS NOT NULL
+    ) AS has_refunds,
     LOGICAL_OR(
       charges.fraud_details_user_report = 'fraudulent'
       OR (
@@ -86,16 +82,16 @@ subscriptions_history_charge_summaries AS (
       OR (refunds.reason = 'fraudulent' AND refunds.status = 'succeeded')
     ) AS has_fraudulent_charges
   FROM
-    `moz-fx-data-shared-prod.stripe_external.charge_v1` AS charges
-  JOIN
     `moz-fx-data-shared-prod.stripe_external.invoice_v1` AS invoices
-  ON
-    charges.invoice_id = invoices.id
   JOIN
     subscriptions_history AS history
   ON
     invoices.subscription_id = history.subscription.id
-    AND charges.created < history.valid_to
+    AND invoices.created < history.valid_to
+  LEFT JOIN
+    `moz-fx-data-shared-prod.stripe_external.charge_v1` AS charges
+  ON
+    invoices.id = charges.invoice_id
   LEFT JOIN
     `moz-fx-data-shared-prod.stripe_external.card_v1` AS cards
   ON
@@ -116,7 +112,7 @@ SELECT
   (
     SELECT AS STRUCT
       history.subscription.*,
-      IF(paypal_subscriptions.subscription_id IS NOT NULL, 'PayPal', 'Stripe') AS payment_provider,
+      IF(invoice_summaries.uses_paypal, 'PayPal', 'Stripe') AS payment_provider,
       CASE
         -- Use the same address hierarchy as Stripe Tax after we enabled Stripe Tax (FXA-5457).
         -- https://stripe.com/docs/tax/customer-locations#address-hierarchy
@@ -128,24 +124,20 @@ SELECT
           THEN COALESCE(
               NULLIF(history.customer.shipping.address.country, ''),
               NULLIF(history.customer.address.country, ''),
-              charge_summaries.latest_card_country
+              invoice_summaries.latest_card_country
             )
         -- SubPlat copies the PayPal billing agreement country to the customer's address.
-        WHEN paypal_subscriptions.subscription_id IS NOT NULL
+        WHEN invoice_summaries.uses_paypal
           THEN NULLIF(history.customer.address.country, '')
-        ELSE charge_summaries.latest_card_country
+        ELSE invoice_summaries.latest_card_country
       END AS country_code,
-      COALESCE(charge_summaries.has_refunds, FALSE) AS has_refunds,
-      COALESCE(charge_summaries.has_fraudulent_charges, FALSE) AS has_fraudulent_charges
+      COALESCE(invoice_summaries.has_refunds, FALSE) AS has_refunds,
+      COALESCE(invoice_summaries.has_fraudulent_charges, FALSE) AS has_fraudulent_charges
   ) AS subscription,
   history.customer
 FROM
   subscriptions_history AS history
 LEFT JOIN
-  paypal_subscriptions
+  subscriptions_history_invoice_summaries AS invoice_summaries
 ON
-  history.subscription.id = paypal_subscriptions.subscription_id
-LEFT JOIN
-  subscriptions_history_charge_summaries AS charge_summaries
-ON
-  history.id = charge_summaries.subscriptions_history_id
+  history.id = invoice_summaries.subscriptions_history_id

fcbd37242198ca7f849dd4205f43a23addc2e869
Alekhya <88394696+alekhyamoz@users.noreply.github.com>
2023-11-09 15:20:30 -0500

    Add missing clients view to fenix review checker (#4540)

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix/review_checker_clients/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_fenix.review_checker_clients`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix_derived.review_checker_clients_v1`

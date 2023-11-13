e26d8eb8c8a7b7fea444184aa6b794dd76336c54
Alexander Nicholson <anicholson@mozilla.com>
2023-11-13 13:31:07 -0500

    Query typo

--- sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_v6/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/clients_daily_v6/query.sql
@@ -1454,7 +1454,7 @@ aggregates AS (
       OFFSET(0)
     ] AS startup_profile_selection_reason_first,
     mozfun.stats.mode_last(
-        ARRAY_AGG(IF(subsession_counter = 1, startup_profile_selection_reason, NULL) ORDER BY submission_timestamp)
+        ARRAY_AGG(IF(subsession_counter = 1, startup_profile_selection_reason, NULL) ORDER BY submission_timestamp ASC)
     ) as startup_profile_selection_first_ping_only,
     SUM(
       scalar_parent_browser_ui_interaction_textrecognition_error

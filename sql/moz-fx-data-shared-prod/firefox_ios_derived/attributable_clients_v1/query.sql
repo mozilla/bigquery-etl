9a7ed996d53c7ff77155fd6da3a88253a03ef399
Frank Bertsch <frank.bertsch@gmail.com>
2023-11-13 08:30:20 -0500

    iOS attributable_clients use metrics adclicks (#4543)
    * iOS attributable_clients use metrics adclicks
    
    * Remove project id from table name
    
    Co-authored-by: kik-kik <42538694+kik-kik@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: kik-kik <42538694+kik-kik@users.noreply.github.com>

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/attributable_clients_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/attributable_clients_v1/query.sql
@@ -3,22 +3,51 @@ RETURNS INT64 AS (
   (SELECT SUM(value) FROM UNNEST(map))
 );
 
-WITH client_search_activity AS (
+WITH client_days AS (
   SELECT
     DATE(submission_timestamp) AS submission_date,
     sample_id,
     client_info.client_id,
+    LOGICAL_OR(mozfun.norm.extract_version(app_version, 'major') >= 108) AS has_search_data,
     SUM(sum_map_values(metrics.labeled_counter.search_in_content)) AS searches,
     SUM(sum_map_values(metrics.labeled_counter.browser_search_with_ads)) AS searches_with_ads,
     SUM(sum_map_values(metrics.labeled_counter.browser_search_ad_clicks)) AS ad_clicks,
   FROM
     firefox_ios.baseline
   WHERE
-    DATE(submission_timestamp) = @submission_date
+    {% if is_init() %}
+      submission_date >= "2020-05-01"
+    {% else %}
+      submission_date = @submission_date
+    {% endif %}
   GROUP BY
     submission_date,
     sample_id,
     client_id
+),
+metrics_searches AS (
+  SELECT
+    submission_date,
+    client_id,
+    sample_id,
+    LOGICAL_OR(mozfun.norm.extract_version(app_version, 'major') < 108) AS has_search_data,
+    SUM(search_count) AS searches,
+    SUM(search_with_ads) AS searches_with_ads,
+    SUM(ad_click) AS ad_clicks
+  FROM
+    search_derived.mobile_search_clients_daily_v1
+  WHERE
+    {% if is_init() %}
+      submission_date >= "2020-05-01"
+    {% else %}
+      submission_date = @submission_date
+    {% endif %}
+    AND normalized_app_name = 'Fennec'
+    AND os = 'iOS'
+  GROUP BY
+    submission_date,
+    client_id,
+    sample_id
 ),
 adjust_client_info AS (
   SELECT
@@ -32,8 +61,6 @@ adjust_client_info AS (
     metadata.is_reported_first_session_ping,
   FROM
     firefox_ios.firefox_ios_clients
-  WHERE
-    adjust_network NOT IN ("Unknown", "Other")
 )
 SELECT
   submission_date,
@@ -45,11 +72,33 @@ SELECT
   adjust_campaign,
   adjust_creative,
   first_seen_date = submission_date AS is_new_profile,
-  COALESCE(searches, 0) AS searches,
-  COALESCE(searches_with_ads, 0) AS searches_with_ads,
-  COALESCE(ad_clicks, 0) AS ad_clicks,
+  CASE
+    WHEN client_days.has_search_data
+      THEN client_days.searches
+    WHEN metrics_searches.has_search_data
+      THEN metrics_searches.searches
+    ELSE 0
+  END AS searches,
+  CASE
+    WHEN client_days.has_search_data
+      THEN client_days.searches_with_ads
+    WHEN metrics_searches.has_search_data
+      THEN metrics_searches.searches_with_ads
+    ELSE 0
+  END AS searches_with_ads,
+  CASE
+    WHEN client_days.has_search_data
+      THEN client_days.ad_clicks
+    WHEN metrics_searches.has_search_data
+      THEN metrics_searches.ad_clicks
+    ELSE 0
+  END AS ad_clicks,
 FROM
-  client_search_activity
+  client_days
+FULL OUTER JOIN
+  metrics_searches
+USING
+  (client_id, sample_id, submission_date)
 INNER JOIN
   adjust_client_info
 USING

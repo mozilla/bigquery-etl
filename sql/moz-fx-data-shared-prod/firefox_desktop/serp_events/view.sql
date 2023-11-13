f3d57f74af6972264e08eca59589c0b68b86b0f9
Alekhya <88394696+alekhyamoz@users.noreply.github.com>
2023-11-13 17:28:22 -0500

    Delete sql/moz-fx-data-shared-prod/firefox_desktop/serp_events/view.sql

--- sql/moz-fx-data-shared-prod/firefox_desktop/serp_events/view.sql
+++
@@ -1,8 +0,0 @@
---- User-facing view. Generated via sql_generators.active_users.
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_desktop.serp_events`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_desktop_derived.serp_events_v1`

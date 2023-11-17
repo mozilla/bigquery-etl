f7bf99892c0c91a62e903f9ac34c87b28c90ec14
Frank Bertsch <frank.bertsch@gmail.com>
2023-11-17 14:48:32 -0500

    Use correct dataset name in view

--- sql/moz-fx-data-shared-prod/stub_attribution_service/dl_token_ga_attribution_lookup/view.sql
+++ sql/moz-fx-data-shared-prod/stub_attribution_service/dl_token_ga_attribution_lookup/view.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.mozilla_org.dl_token_ga_attribution_lookup`
+  `moz-fx-data-shared-prod.stub_attribution_service.dl_token_ga_attribution_lookup`
 AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.mozilla_org_derived.dl_token_ga_attribution_lookup_v1`
+  `moz-fx-data-shared-prod.stub_attribution_service_derived.dl_token_ga_attribution_lookup_v1`

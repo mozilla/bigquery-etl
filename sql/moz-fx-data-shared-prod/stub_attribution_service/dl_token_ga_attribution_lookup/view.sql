104ece82d95d36f708ee99adc749d68a9ca7ae02
Frank Bertsch <frank.bertsch@gmail.com>
2023-11-17 16:36:48 -0500

    Add derived stub attribution logs (#4557)
    * Add derived stub attribution logs
    
    This table keeps triplets from the stub attribution logs.
    The triplet of (dl_token, ga_client_id, stub_session_id)
    will only ever appear once here.
    
    See the associated decision brief:
    https://docs.google.com/document/d/1L4vOR0nCGawwSRPA9xiR8Hmu_8ozCGUecXAtBWmGGA0/edit
    
    * Move stub attribution table to new dataset
    
    In order to ensure limited access to the stub attribution service
    data without significantly decreasing developer velocity, we
    move these tables to a new dataset. That dataset has the defaults
    we want for all stub attribution log data:
    - Defaults to just read access to data-science/DUET workgroup
    - No read/write access for DE
    
    We will backfill via the bqetl_backfill DAG.
    
    * Rename view
    
    * Use correct dataset name in view
    
    * Skip dryrun; no access

---
+++ sql/moz-fx-data-shared-prod/stub_attribution_service/dl_token_ga_attribution_lookup/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.stub_attribution_service.dl_token_ga_attribution_lookup`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.stub_attribution_service_derived.dl_token_ga_attribution_lookup_v1`

0eddeaab6f81283e7c04284acac5837f2cb26e34
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2023-11-17 15:33:43 +0100

    SVCSE-1595 Setup import of tables from staging FxA databases
    Per [SVCSE-1595](https://mozilla-hub.atlassian.net/browse/SVCSE-1595) we'd like to import all the tables from FxA databases to BigQuery. There are 37 tables per environment (stage and production). If we follow the current approach we'd end up having all those tables in `accounts_backend_external` dataset, and their views in `accounts_backend` together with telemetry data. This layout will make finding data hard.
    
    To better organize this data I propose to:
    * introduce two new external datasets: `accounts_db_nonprod_external` and `accounts_db_external` to replace `accounts_backend_external` and store data imported from FxA databases
    * introduce two datasets to store user-facing views to these tables: `accounts_db_nonprod` and `accounts_db`
    * remove `accounts_backend_external` dataset
    * remove previously created user-facing views: `accounts_backend.accounts` and `accounts_backend.emails`

---
+++ sql/moz-fx-data-shared-prod/accounts_db_nonprod_external/fxa_oauth_clients_v1/query.sql
@@ -0,0 +1,29 @@
+SELECT
+  id,
+  name,
+  imageUri,
+  redirectUri,
+  canGrant,
+  publicClient,
+  createdAt,
+  SAFE_CAST(trusted AS BOOL) AS trusted,
+  allowedScopes,
+  notes,
+FROM
+  EXTERNAL_QUERY(
+    "moz-fx-fxa-nonprod.us.fxa-oauth-nonprod-stage-fxa-oauth",
+    """SELECT
+         id,
+         name,
+         imageUri,
+         redirectUri,
+         canGrant,
+         publicClient,
+         createdAt,
+         trusted,
+         allowedScopes,
+         notes
+       FROM
+         fxa_oauth.clients
+    """
+  )
---
+++ sql/moz-fx-data-shared-prod/accounts_db_nonprod_external/fxa_oauth_scopes_v1/query.sql
@@ -0,0 +1,13 @@
+SELECT
+  scope,
+  hasScopedKeys,
+FROM
+  EXTERNAL_QUERY(
+    "moz-fx-fxa-nonprod.us.fxa-oauth-nonprod-stage-fxa-oauth",
+    """SELECT
+         scope,
+         hasScopedKeys
+       FROM
+         fxa_oauth.scopes
+    """
+  )

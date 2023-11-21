73b86960fca31a07364b8f81c3ec95486312fc2c
Frank Bertsch <frank.bertsch@gmail.com>
2023-11-21 08:55:03 -0500

    Account for NULL handling in joins (#4590)
    Previously, NULL values in the join keys didn't join, resulting
    in duplicate rows. This change will coalesce those to empty
    strings and NULLIFY them in the view.

--- sql/moz-fx-data-shared-prod/stub_attribution_service_derived/dl_token_ga_attribution_lookup_v1/query.sql
+++ sql/moz-fx-data-shared-prod/stub_attribution_service_derived/dl_token_ga_attribution_lookup_v1/query.sql
@@ -1,17 +1,17 @@
 WITH historical_triplets AS (
   SELECT
-    dl_token,
-    ga_client_id,
-    stub_session_id,
+    IFNULL(dl_token, "") AS dl_token,
+    IFNULL(ga_client_id, "") AS ga_client_id,
+    IFNULL(stub_session_id, "") AS stub_session_id,
     first_seen_date,
   FROM
     stub_attribution_service_derived.dl_token_ga_attribution_lookup_v1
 ),
 new_downloads AS (
   SELECT DISTINCT
-    mozfun.ga.nullify_string(jsonPayload.fields.dltoken) AS dl_token,
-    mozfun.ga.nullify_string(jsonPayload.fields.visit_id) AS ga_client_id,
-    mozfun.ga.nullify_string(jsonPayload.fields.session_id) AS stub_session_id,
+    IFNULL(mozfun.ga.nullify_string(jsonPayload.fields.dltoken), "") AS dl_token,
+    IFNULL(mozfun.ga.nullify_string(jsonPayload.fields.visit_id), "") AS ga_client_id,
+    IFNULL(mozfun.ga.nullify_string(jsonPayload.fields.session_id), "") AS stub_session_id,
     @download_date AS first_seen_date,
   FROM
     `moz-fx-stubattribut-prod-32a5`.stubattribution_prod.stdout
@@ -19,6 +19,8 @@ new_downloads AS (
     DATE(timestamp) = @download_date
 )
 SELECT
+  -- We can't store these as NULL, since joins on NULL keys don't match.
+  -- Since they don't match, we end up with dupes
   dl_token,
   ga_client_id,
   stub_session_id,

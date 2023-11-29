d297d749c08eefaddc1c210f9d9d6180ce758b93
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2023-11-29 19:47:05 +0100

    Add user-facing view to fxa_oauth.clients

---
+++ sql/moz-fx-data-shared-prod/accounts_db/fxa_oauth_clients/view.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.accounts_db.fxa_oauth_clients`
+AS
+SELECT
+  TO_HEX(id) AS id,
+  name
+FROM
+  `moz-fx-data-shared-prod.accounts_db_external.fxa_oauth_clients_v1`

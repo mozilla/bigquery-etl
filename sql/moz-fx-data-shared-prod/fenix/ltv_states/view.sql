ecbedd6253bd228f000d1277d72136726f0d4b7d
Frank Bertsch <frank.bertsch@gmail.com>
2023-11-30 16:41:34 -0500

    Create tables that have state values per day

---
+++ sql/moz-fx-data-shared-prod/fenix/ltv_states/view.sql
@@ -0,0 +1,75 @@
+DECLARE max_weeks INT64 DEFAULT 32;
+
+DECLARE death_time INT64 DEFAULT 168;
+
+DECLARE lookback INT64 DEFAULT 28;
+
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fenix.ltv_states`
+AS
+WITH extracted_fields AS (
+  SELECT
+    *,
+    BIT_COUNT(
+      `mozfun`.bytes.extract_bits(days_seen_bytes, -1 * lookback, lookback)
+    ) AS activity_pattern,
+    BIT_COUNT(`mozfun`.bytes.extract_bits(days_seen_bytes, -1, 1)) AS active_on_this_date,
+  FROM
+    `moz-fx-data-shared-prod.fenix_derived.ltv_states_v1`
+  WHERE
+    -- BrowserStack clients are bots, we don't want to accidentally report on them
+    first_reported_isp != "BrowserStack"
+    -- Remove clients who are new on this day, but have more/less than 1 day of activity
+    AND NOT (days_since_first_seen = 0 AND BIT_COUNT(days_seen_bytes) != 1)
+)
+SELECT
+  client_id,
+  submission_date,
+  [
+    STRUCT(
+      "android_states_v1" AS state_function_name,
+      mozfun.ltv.android_states_v1(
+        adjust_network,
+        days_since_first_seen,
+        days_since_seen,
+        death_time,
+        submission_date,
+        first_seen_date,
+        activity_pattern,
+        active_on_this_date,
+        max_weeks,
+        first_reported_country
+      ) AS state
+    ),
+    STRUCT(
+      "android_states_with_paid_v1" AS state_function_name,
+      mozfun.ltv.android_states_with_paid_v1(
+        adjust_network,
+        days_since_first_seen,
+        submission_date,
+        first_seen_date,
+        activity_pattern,
+        active_on_this_date,
+        max_weeks,
+        first_reported_country
+      ) AS state
+    ),
+    STRUCT(
+      "android_states_with_paid_v2" AS state_function_name,
+      mozfun.ltv.android_states_with_paid_v2(
+        adjust_network,
+        days_since_first_seen,
+        days_since_seen,
+        death_time,
+        submission_date,
+        first_seen_date,
+        activity_pattern,
+        active_on_this_date,
+        max_weeks,
+        first_reported_country
+      ) AS state
+    )
+  ] AS states,
+  * EXCEPT (client_id, submission_date)
+FROM
+  extracted_fields
---
+++ sql/moz-fx-data-shared-prod/fenix_derived/ltv_states_v1/query.sql
@@ -0,0 +1,55 @@
+WITH clients_yearly AS (
+  SELECT
+    client_id,
+    sample_id,
+    submission_date,
+    first_seen_date,
+    days_since_seen,
+    days_seen_bytes,
+    days_since_first_seen,
+  FROM
+    fenix.clients_yearly
+  WHERE
+    {% if is_init() %}
+      submission_date >= "2010-01-01"
+    {% else %}
+      submission_date = @submission_date
+    {% endif %}
+)
+SELECT
+  client_id,
+  sample_id,
+  clients_yearly.submission_date,
+  clients_yearly.first_seen_date,
+  clients_yearly.days_since_first_seen,
+  clients_yearly.days_since_seen,
+  clients_yearly.days_seen_bytes,
+  (
+    SELECT
+      LEAST(value, 10000)
+    FROM
+      UNNEST(ad_click_history)
+    WHERE
+      key = clients_yearly.submission_date
+  ) AS ad_clicks_on_date,
+  (
+    SELECT
+      SUM(LEAST(value, 10000))
+    FROM
+      UNNEST(ad_click_history)
+    WHERE
+      key <= clients_yearly.submission_date
+  ) AS total_historic_ad_clicks,
+  firefox_android_clients.first_reported_country,
+  firefox_android_clients.first_reported_isp,
+  firefox_android_clients.adjust_network,
+FROM
+  clients_yearly
+JOIN
+  fenix.firefox_android_clients
+USING
+  (sample_id, client_id)
+LEFT JOIN
+  fenix.client_adclicks_history
+USING
+  (sample_id, client_id)

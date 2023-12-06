5e43fbc42d824dc58151dce83b4d82c1216272c0
Frank Bertsch <frank.bertsch@gmail.com>
2023-12-06 08:23:05 -0500

    Use jinja vars instead of sql variables

--- sql/moz-fx-data-shared-prod/fenix/ltv_states/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/ltv_states/view.sql
@@ -1,9 +1,6 @@
-DECLARE max_weeks INT64 DEFAULT 32;
-
-DECLARE death_time INT64 DEFAULT 168;
-
-DECLARE lookback INT64 DEFAULT 28;
-
+{% set max_weeks = 32 %}
+{% set death_time = 168 %}
+{% set lookback = 28 %}
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.fenix.ltv_states`
 AS
@@ -11,7 +8,7 @@ WITH extracted_fields AS (
   SELECT
     *,
     BIT_COUNT(
-      `mozfun`.bytes.extract_bits(days_seen_bytes, -1 * lookback, lookback)
+      `mozfun`.bytes.extract_bits(days_seen_bytes, -1 * {{ lookback }}, {{ lookback }})
     ) AS activity_pattern,
     BIT_COUNT(`mozfun`.bytes.extract_bits(days_seen_bytes, -1, 1)) AS active_on_this_date,
   FROM
@@ -25,12 +22,12 @@ SELECT
       adjust_network,
       days_since_first_seen,
       days_since_seen,
-      death_time,
+      {{ death_time }},
       submission_date,
       first_seen_date,
       activity_pattern,
       active_on_this_date,
-      max_weeks,
+      {{ max_weeks }},
       first_reported_country
     ) AS android_states_v1,
     mozfun.ltv.android_states_with_paid_v1(
@@ -40,19 +37,19 @@ SELECT
       first_seen_date,
       activity_pattern,
       active_on_this_date,
-      max_weeks,
+      {{ max_weeks }},
       first_reported_country
     ) AS android_states_with_paid_v1,
     mozfun.ltv.android_states_with_paid_v2(
       adjust_network,
       days_since_first_seen,
       days_since_seen,
-      death_time,
+      {{ death_time }},
       submission_date,
       first_seen_date,
       activity_pattern,
       active_on_this_date,
-      max_weeks,
+      {{ max_weeks }},
       first_reported_country
     ) AS android_states_with_paid_v2
   ) AS markov_states,

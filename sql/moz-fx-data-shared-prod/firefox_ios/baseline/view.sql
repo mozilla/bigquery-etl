7f74d089fa4c7eeae85691d4b8e9a535c15020d1
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-12-08 00:36:26 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_ios/baseline/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios/baseline/view.sql
@@ -39,3 +39,22 @@ SELECT
   submission_timestamp
 FROM
   `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta.baseline`
+UNION ALL
+SELECT
+  "org_mozilla_ios_fennec" AS normalized_app_id,
+  "nightly" AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_fennec.baseline`

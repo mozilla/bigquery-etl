0223c0cc8c545136fd39d3d363cef04a12c1e9bc
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-12-11 18:35:24 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/mozillavpn_cirrus/baseline_clients_daily/view.sql
@@ -0,0 +1,8 @@
+-- Generated via bigquery_etl.glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozillavpn_cirrus.baseline_clients_daily`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.mozillavpn_cirrus_derived.baseline_clients_daily_v1`

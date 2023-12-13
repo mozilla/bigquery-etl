180e95e6b2337b8fdd993d872328d257294b75c9
Katie Windau <kwindau@mozilla.com>
2023-12-13 16:13:25 -0600

    Bug 1866834 - creating 2 new use counter derived tables & a view combining them

---
+++ sql/moz-fx-data-shared-prod/fenix/use_counters/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fenix.use_counters`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.fenix_derived.use_counters_v1`
---
+++ sql/moz-fx-data-shared-prod/telemetry/firefox_use_counters/view.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.firefox_use_counters`
+SELECT
+  submission_date,
+  version_major,
+  geo_country,
+  platform,
+  metric,
+  rate
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.use_counters_v1`
+UNION ALL
+SELECT
+  submission_date,
+  version_major,
+  geo_country,
+  platform,
+  metric,
+  rate
+FROM
+  `moz-fx-data-shared-prod.fenix_derived.use_counters_v1`

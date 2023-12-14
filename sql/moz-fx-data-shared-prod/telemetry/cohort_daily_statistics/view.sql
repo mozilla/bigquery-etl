c1c3fe30eff5fb3b556e347dfa0fbb88606b279d
Marlene-M-Hirose <mhirose@mozilla.com>
2023-12-14 08:51:53 -1000

    add data from desktop_cohort_daily_retention to cohort_daily_stats, normalize desktop normalized_app_name to 'Firefox Desktop'

--- sql/moz-fx-data-shared-prod/telemetry/cohort_daily_statistics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/cohort_daily_statistics/view.sql
@@ -2,8 +2,39 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.cohort_daily_statistics`
 AS
 SELECT
-  *
+  activity_date,
+  cohort_date,
+  normalized_app_name,
+  normalized_channel,
+  normalized_os,
+  normalized_os_version,
+  attribution_source,
+  attribution_medium,
+  attribution_campaign,
+  attribution_content,
+  attribution_experiment,
+  attribution_variation,
+  num_clients_active_on_day,
+  num_clients_in_cohort
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.cohort_daily_statistics_v1`
 WHERE
   normalized_app_name != "Firefox Desktop"
+UNION ALL
+SELECT
+  submission_date AS activity_date,
+  first_seen_date AS cohort_date,
+  "Firefox Desktop" AS normalized_app_name,
+  normalized_channel,
+  normalized_os,
+  normalized_os_version,
+  attribution_source,
+  attribution_medium,
+  attribution_campaign,
+  attribution_content,
+  attribution_experiment,
+  NULL AS attribution_variation,
+  num_clients_active_on_day,
+  num_clients_in_cohort
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.desktop_cohort_daily_retention_v1`

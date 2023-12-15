d5ffd4fe35d2e924af577b262707e180a393f153
Katie Windau <153020235+kwindau@users.noreply.github.com>
2023-12-15 13:13:36 -0600

    Fb 1866834 new use counter data (#4713)
    * 1866834 - add new DAG bqetl_use_counter_analysis
    
    * 1866834 - adding fenix & firefox use_counters_v1 tables and telemetry use_counters view
    
    * 1866834 - fixing yaml file formatting
    
    * 1866834 - fix dags.yaml format - remove trailing space
    
    * 1866834 - add cast to numeric for rate to match schema for rate
    
    * 1866834 - Remove unnecessary alias
    
    * 1866834 - remove unnecessary alias
    
    * Update and rename view.sql to view.sql
    
    * 1866834 - test removing view
    
    * 1866834 - update table names
    
    * 1866834 - fixing fenix_and_firefox_use_counters view name and source table references

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/firefox_desktop_use_counters/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.firefox_desktop_use_counters`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.firefox_desktop_use_counters_v1`

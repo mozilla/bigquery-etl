2da1be51fe47400269658e1a8883d8245eec2ca8
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-12-15 20:13:24 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry/cohort_daily_statistics/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/cohort_daily_statistics/view.sql
@@ -2,20 +2,30 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.cohort_daily_statistics`
 AS
 SELECT
-  activity_date,
   cohort_date,
+  activity_date,
+  activity_segment,
+  app_version,
+  attribution_campaign,
+  attribution_content,
+  attribution_experiment,
+  attribution_medium,
+  attribution_source,
+  attribution_variation,
+  city,
+  country,
+  device_model,
+  distribution_id,
+  is_default_browser,
+  locale,
   normalized_app_name,
   normalized_channel,
   normalized_os,
   normalized_os_version,
-  attribution_source,
-  attribution_medium,
-  attribution_campaign,
-  attribution_content,
-  attribution_experiment,
-  attribution_variation,
-  num_clients_active_on_day,
-  num_clients_in_cohort
+  os_version_major,
+  os_version_minor,
+  num_clients_in_cohort,
+  num_clients_active_on_day
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.cohort_daily_statistics_v1`
 WHERE
@@ -24,17 +34,27 @@ UNION ALL
 SELECT
   submission_date AS activity_date,
   first_seen_date AS cohort_date,
+  CAST(NULL AS STRING) AS activity_segment,
+  app_version,
+  attribution_campaign,
+  attribution_content,
+  attribution_experiment,
+  attribution_medium,
+  attribution_source,
+  CAST(NULL AS STRING) AS attribution_variation,
+  city,
+  country,
+  CAST(NULL AS STRING) AS device_model,
+  distribution_id,
+  CAST(NULL AS BOOL) AS is_default_browser,
+  locale,
   "Firefox Desktop" AS normalized_app_name,
   normalized_channel,
   normalized_os,
   normalized_os_version,
-  attribution_source,
-  attribution_medium,
-  attribution_campaign,
-  attribution_content,
-  attribution_experiment,
-  CAST(NULL AS STRING) AS attribution_variation,
-  num_clients_active_on_day,
-  num_clients_in_cohort
+  os_version_major,
+  os_version_minor,
+  num_clients_in_cohort,
+  num_clients_active_on_day
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.desktop_cohort_daily_retention_v1`

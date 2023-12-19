05d16b5c5048340a554a9cd0bba4bf91f6d8d8e6
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2023-12-19 16:50:51 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/firefox_ios/fx_suggest/view.sql
@@ -0,0 +1,60 @@
+-- Generated via ./bqetl generate glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_ios.fx_suggest`
+AS
+SELECT
+  "org_mozilla_ios_firefox" AS normalized_app_id,
+  "release" AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefox.fx_suggest`
+UNION ALL
+SELECT
+  "org_mozilla_ios_firefoxbeta" AS normalized_app_id,
+  "beta" AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta.fx_suggest`
+UNION ALL
+SELECT
+  "org_mozilla_ios_fennec" AS normalized_app_id,
+  "nightly" AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  metrics,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_fennec.fx_suggest`

70720bf3d0ec3b9fad0d186654420a14f3b244e3
Leli Schiestl <lschiestl@mozilla.com>
2023-12-20 14:54:42 +0100

    status quo

---
+++ sql/moz-fx-data-shared-prod/telemetry_dev_cycle/telemetry_probes/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry_dev_cycle.telemetry_probes`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_dev_cycle_derived.telemetry_probes_v1`
---
+++ sql/moz-fx-data-shared-prod/telemetry_dev_cycle_derived/telemetry_probes_v1/query.sql
@@ -0,0 +1,9 @@
+-- Query for telemetry_dev_cycle_derived.telemetry_probes_v1
+            -- For more information on writing queries see:
+            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+SELECT
+  *
+FROM
+  table
+WHERE
+  submission_date = @submission_date

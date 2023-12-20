6bf11164a521b422772dde850e0152e8449b03fc
Leli Schiestl <lschiestl@mozilla.com>
2023-12-20 22:38:23 +0100

    reformat query

--- sql/moz-fx-data-shared-prod/telemetry_dev_cycle_derived/telemetry_probes_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_dev_cycle_derived/telemetry_probes_v1/query.sql
@@ -1,42 +1,47 @@
-with
-
-telemetry_probes_with_expiration as (
-  select
+WITH telemetry_probes_with_expiration AS (
+  SELECT
     channel,
     probe,
     type,
     release_version,
     last_version,
     expiry_version,
-    case
-      when expiry_version = "never" then last_version + 1
-      else if( last_version + 1 < cast(expiry_version as int64), last_version + 1, cast(expiry_version as int64))
-    end as expired_version_helper,
-    first_added_date as release_date
-  from `telemetry_dev_cycle_external.telemetry_probes_stats_v1` as probes
+    CASE
+      WHEN expiry_version = "never"
+        THEN last_version + 1
+      ELSE IF(
+          last_version + 1 < CAST(expiry_version AS int64),
+          last_version + 1,
+          CAST(expiry_version AS int64)
+        )
+    END AS expired_version_helper,
+    first_added_date AS release_date
+  FROM
+    `telemetry_dev_cycle_external.telemetry_probes_stats_v1` AS probes
 ),
-
-final as (
-  select
+final AS (
+  SELECT
     probes.channel,
     probes.probe,
     probes.type,
     probes.expiry_version,
     probes.release_version,
-    --coalesce(release_dates.version as expired_version,
-    release_dates.version as expired_version,
-    --probes.expired_version_helper,
-    --probes.last_version,
+    release_dates.version AS expired_version,
     probes.release_date,
-    case
-      when probes.expired_version_helper < 100 then coalesce(release_dates.publish_date, release_date)
-      else release_dates.publish_date
-    end as expired_date
-  from telemetry_probes_with_expiration as probes
-  left join `telemetry_dev_cycle_derived.firefox_major_release_dates_v1` as release_dates
-  on probes.channel = release_dates.channel and probes.expired_version_helper = release_dates.version
+    CASE
+      WHEN probes.expired_version_helper < 100
+        THEN COALESCE(release_dates.publish_date, release_date)
+      ELSE release_dates.publish_date
+    END AS expired_date
+  FROM
+    telemetry_probes_with_expiration AS probes
+  LEFT JOIN
+    `telemetry_dev_cycle_derived.firefox_major_release_dates_v1` AS release_dates
+  ON
+    probes.channel = release_dates.channel
+    AND probes.expired_version_helper = release_dates.version
 )
-
-select * from final
-
-
+SELECT
+  *
+FROM
+  final

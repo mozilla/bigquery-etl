15801e7935dbe59a3f1ef2db8b58b708effcd0f4
Katie Windau <kwindau@mozilla.com>
2024-01-02 09:08:27 -0600

    1866834 - update usage rate formula for V1 tables

--- sql/moz-fx-data-shared-prod/fenix_derived/fenix_use_counters_v1/query.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/fenix_use_counters_v1/query.sql
@@ -8260,7 +8260,7 @@ fenix_staging AS (
       ELSE NULL
     END AS doc_rate,
     CASE
-      WHEN metric LIKE 'use_counter_css_page_%'
+      WHEN metric LIKE 'use_counter_css_page_%' OR metric LIKE 'use_counter_page_%'
         THEN SAFE_DIVIDE(cnt, use_counter_top_level_content_documents_destroyed)
       ELSE NULL
     END AS page_rate,

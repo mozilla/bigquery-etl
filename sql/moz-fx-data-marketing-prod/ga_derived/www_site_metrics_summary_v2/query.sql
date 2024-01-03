d1152d63e4e281dfe8cf8dd9bc9ecba90183912c
Katie Windau <kwindau@mozilla.com>
2024-01-03 13:42:15 -0600

    Adding downloads column

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_metrics_summary_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_metrics_summary_v2/query.sql
@@ -14,8 +14,8 @@ WITH site_data AS (
     collected_traffic_source.manual_campaign_name AS campaign,
     collected_traffic_source.manual_content AS ad_content,
     sum(CASE WHEN event_name = 'session_start' THEN 1 ELSE 0 END) AS sessions,
-    sum(CASE WHEN event_name = 'session_start' AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser) THEN 1 ELSE 0 END) AS non_fx_sessions
-    --downloads
+    sum(CASE WHEN event_name = 'session_start' AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser) THEN 1 ELSE 0 END) AS non_fx_sessions,
+    sum(CASE WHEN event_name = 'product_download' THEN 1 ELSE 0 END) AS downloads
     --non_fx_downloads
   FROM
     `moz-fx-data-marketing-prod.analytics_313696158.events_*`
@@ -48,7 +48,7 @@ SELECT
   s.ad_content
   s.sessions,
   s.non_fx_sessions,
-  --s.downloads,
+  s.downloads,
   --s.non_fx_downloads
 FROM
   site_data s

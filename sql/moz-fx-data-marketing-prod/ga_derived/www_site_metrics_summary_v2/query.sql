dff3264c97f0afbdc3ffcf8a42fc19ef98b3b8a3
Katie Windau <kwindau@mozilla.com>
2024-01-03 13:48:03 -0600

    reformat the SQL

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_metrics_summary_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_metrics_summary_v2/query.sql
@@ -3,7 +3,7 @@
             -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
 WITH site_data AS (
   SELECT
-    event_date as date,
+    event_date AS date,
     device.category AS device_category,
     device.operating_system AS operating_system,
     device.web_info.browser AS browser,
@@ -13,13 +13,27 @@ WITH site_data AS (
     traffic_source.medium AS medium,
     collected_traffic_source.manual_campaign_name AS campaign,
     collected_traffic_source.manual_content AS ad_content,
-    sum(CASE WHEN event_name = 'session_start' THEN 1 ELSE 0 END) AS sessions,
-    sum(CASE WHEN event_name = 'session_start' AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser) THEN 1 ELSE 0 END) AS non_fx_sessions,
-    sum(CASE WHEN event_name = 'product_download' THEN 1 ELSE 0 END) AS downloads,
-    sum(CASE WHEN event_name = 'product_download' AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser) THEN 1 ELSE 0 END) AS non_fx_downloads
+    SUM(CASE WHEN event_name = 'session_start' THEN 1 ELSE 0 END) AS sessions,
+    SUM(
+      CASE
+        WHEN event_name = 'session_start'
+          AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+          THEN 1
+        ELSE 0
+      END
+    ) AS non_fx_sessions,
+    SUM(CASE WHEN event_name = 'product_download' THEN 1 ELSE 0 END) AS downloads,
+    SUM(
+      CASE
+        WHEN event_name = 'product_download'
+          AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+          THEN 1
+        ELSE 0
+      END
+    ) AS non_fx_downloads
   FROM
     `moz-fx-data-marketing-prod.analytics_313696158.events_*`
-  where
+  WHERE
     event_date = FORMAT_DATE('%Y%m%d', @submission_date)
   GROUP BY
     event_date,
@@ -33,7 +47,6 @@ WITH site_data AS (
     collected_traffic_source.manual_campaign_name,
     collected_traffic_source.manual_content
 )
-
 SELECT
   s.date,
   s.device_category,
@@ -45,8 +58,7 @@ SELECT
   s.source,
   s.medium,
   s.campaign,
-  s.ad_content
-  s.sessions,
+  s.ad_content s.sessions,
   s.non_fx_sessions,
   s.downloads,
   s.non_fx_downloads
@@ -54,4 +66,5 @@ FROM
   site_data s
 LEFT JOIN
   `moz-fx-data-shared-prod.static.third_party_standardized_country_names` std_cntry_nms
-ON s.country = std_cntry_nms.raw_country
+ON
+  s.country = std_cntry_nms.raw_country

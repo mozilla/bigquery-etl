b79025418fb56d550257ee01c07dfb57d793c445
Katie Windau <kwindau@mozilla.com>
2024-01-03 10:36:22 -0600

    Initial draft

---
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_metrics_summary_v2/query.sql
@@ -0,0 +1,9 @@
+-- Query for ga_derived.www_site_metrics_summary_v2
+            -- For more information on writing queries see:
+            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+SELECT
+  *
+FROM
+  table
+WHERE
+  submission_date = @submission_date

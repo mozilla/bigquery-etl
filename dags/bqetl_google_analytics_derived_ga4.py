d7117cad47ac0404e052f77eca616246830d19ac
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-01-04 21:24:13 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ dags/bqetl_google_analytics_derived_ga4.py
@@ -0,0 +1,61 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, gke_command, bigquery_dq_check
+
+docs = """
+### bqetl_google_analytics_derived_ga4
+
+Built from bigquery-etl repo, [`dags/bqetl_google_analytics_derived_ga4.py`](https://github.com/mozilla/bigquery-etl/blob/generated-sql/dags/bqetl_google_analytics_derived_ga4.py)
+
+#### Description
+
+Daily aggregations of data exported from Google Analytics 4
+
+#### Owner
+
+kwindau@mozilla.com
+
+#### Tags
+
+* impact/tier_2
+* repo/bigquery-etl
+"""
+
+
+default_args = {
+    "owner": "kwindau@mozilla.com",
+    "start_date": datetime.datetime(2024, 1, 3, 0, 0),
+    "end_date": None,
+    "email": ["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 2,
+}
+
+tags = ["impact/tier_2", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_google_analytics_derived_ga4",
+    default_args=default_args,
+    schedule_interval="0 12 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+    ga_derived__www_site_metrics_summary__v2 = bigquery_etl_query(
+        task_id="ga_derived__www_site_metrics_summary__v2",
+        destination_table="www_site_metrics_summary_v2",
+        dataset_id="ga_derived",
+        project_id="moz-fx-data-marketing-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
---
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_metrics_summary_v2/query.sql
@@ -0,0 +1,68 @@
+WITH site_data AS (
+  SELECT
+    PARSE_DATE('%Y%m%d', event_date) AS date,
+    device.category AS device_category,
+    device.operating_system AS operating_system,
+    device.web_info.browser AS browser,
+    device.language AS language,
+    geo.country AS country,
+    collected_traffic_source.manual_source AS source,
+    collected_traffic_source.manual_medium AS medium,
+    collected_traffic_source.manual_campaign_name AS campaign,
+    collected_traffic_source.manual_content AS ad_content,
+    COUNTIF(event_name = 'session_start') AS sessions,
+    SUM(
+      CASE
+        WHEN event_name = 'session_start'
+          AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+          THEN 1
+        ELSE 0
+      END
+    ) AS non_fx_sessions,
+    SUM(CASE WHEN event_name = 'product_download' THEN 1 ELSE 0 END) AS downloads,
+    SUM(
+      CASE
+        WHEN event_name = 'product_download'
+          AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+          THEN 1
+        ELSE 0
+      END
+    ) AS non_fx_downloads
+  FROM
+    `moz-fx-data-marketing-prod.analytics_313696158.events_*`
+  WHERE
+    event_date = FORMAT_DATE('%Y%m%d', @submission_date)
+  GROUP BY
+    event_date,
+    device.category,
+    device.operating_system,
+    device.web_info.browser,
+    device.language,
+    geo.country,
+    collected_traffic_source.manual_source,
+    collected_traffic_source.manual_medium,
+    collected_traffic_source.manual_campaign_name,
+    collected_traffic_source.manual_content
+)
+SELECT
+  s.date,
+  s.device_category,
+  s.operating_system,
+  s.browser,
+  s.language,
+  s.country,
+  std_cntry_nms.standardized_country AS standardized_country_name,
+  s.source,
+  s.medium,
+  s.campaign,
+  s.ad_content,
+  s.sessions,
+  s.non_fx_sessions,
+  s.downloads,
+  s.non_fx_downloads
+FROM
+  site_data AS s
+LEFT JOIN
+  `moz-fx-data-shared-prod.static.third_party_standardized_country_names` AS std_cntry_nms
+ON
+  s.country = std_cntry_nms.raw_country

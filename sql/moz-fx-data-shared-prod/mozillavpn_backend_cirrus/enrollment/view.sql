fdd112664a2881983eb64fc339c7f553a16c5605
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-01-06 05:26:00 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/mozillavpn_backend_cirrus/enrollment/view.sql
@@ -0,0 +1,11 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozillavpn_backend_cirrus.enrollment`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info
+  )
+FROM
+  `moz-fx-data-shared-prod.mozillavpn_backend_cirrus_stable.enrollment_v1`
---
+++ sql/moz-fx-data-shared-prod/mozillavpn_backend_cirrus/enrollment_status/view.sql
@@ -0,0 +1,11 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozillavpn_backend_cirrus.enrollment_status`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info
+  )
+FROM
+  `moz-fx-data-shared-prod.mozillavpn_backend_cirrus_stable.enrollment_status_v1`

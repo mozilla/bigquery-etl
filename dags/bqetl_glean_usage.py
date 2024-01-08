6a7865a2ba419eef0d37e7251fe1257384b042f4
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-01-08 21:26:56 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_glean_usage.py
+++ dags/bqetl_glean_usage.py
@@ -283,20 +283,6 @@ with DAG(
         task_group=task_group_firefox_desktop,
     )
 
-    checks__warn_mozillavpn_backend_cirrus_derived__baseline_clients_last_seen__v1 = bigquery_dq_check(
-        task_id="checks__warn_mozillavpn_backend_cirrus_derived__baseline_clients_last_seen__v1",
-        source_table="baseline_clients_last_seen_v1",
-        dataset_id="mozillavpn_backend_cirrus_derived",
-        project_id="moz-fx-data-shared-prod",
-        is_dq_check_fail=False,
-        owner="ascholtz@mozilla.com",
-        email=["ascholtz@mozilla.com", "telemetry-alerts@mozilla.com"],
-        depends_on_past=False,
-        parameters=["submission_date:DATE:{{ds}}"],
-        retries=0,
-        task_group=task_group_mozillavpn_backend_cirrus,
-    )
-
     checks__warn_org_mozilla_connect_firefox_derived__baseline_clients_last_seen__v1 = bigquery_dq_check(
         task_id="checks__warn_org_mozilla_connect_firefox_derived__baseline_clients_last_seen__v1",
         source_table="baseline_clients_last_seen_v1",
@@ -3477,10 +3463,6 @@ with DAG(
 
     checks__warn_firefox_desktop_derived__baseline_clients_last_seen__v1.set_upstream(
         firefox_desktop_derived__baseline_clients_last_seen__v1
-    )
-
-    checks__warn_mozillavpn_backend_cirrus_derived__baseline_clients_last_seen__v1.set_upstream(
-        mozillavpn_backend_cirrus_derived__baseline_clients_last_seen__v1
     )
 
     checks__warn_org_mozilla_connect_firefox_derived__baseline_clients_last_seen__v1.set_upstream(

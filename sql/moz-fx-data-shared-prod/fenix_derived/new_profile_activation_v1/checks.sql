d9bd0ea6d628a3522e9277d31962f95ef3fece22
Frank Bertsch <frank.bertsch@gmail.com>
2024-01-08 12:49:10 -0500

    Add deps & checks for new_profile_activations

---
+++ sql/moz-fx-data-shared-prod/fenix_derived/new_profile_activation_v1/checks.sql
@@ -0,0 +1,12 @@
+#fail
+{{ is_unique("client_id", "submission_date = @submission_date") }}
+
+#fail
+{{ min_row_count(10000, "submission_date = @submission_date") }}
+
+#fail
+{{ min_row_count(10000, "submission_date = @submission_date AND first_seen_date = @submission_date") }}
+
+#warn
+{{ min_row_count(1000, "submission_date = @submission_date AND activated") }}
+

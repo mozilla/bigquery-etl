0690e6a6b5582acfa2ad9bd6a5879df2bdec373f
Frank Bertsch <frank.bertsch@gmail.com>
2024-01-09 08:36:13 -0500

    Remove failing check

--- sql/moz-fx-data-shared-prod/fenix_derived/new_profile_activation_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/new_profile_activation_v1/checks.sql
@@ -4,9 +4,6 @@
 #fail
 {{ min_row_count(10000, "submission_date = @submission_date") }}
 
-#fail
-{{ min_row_count(10000, "submission_date = @submission_date AND first_seen_date = @submission_date") }}
-
 #warn
-{{ min_row_count(1000, "submission_date = @submission_date AND activated") }}
+{{ min_row_count(1000, "submission_date = @submission_date AND activated = 1") }}
 

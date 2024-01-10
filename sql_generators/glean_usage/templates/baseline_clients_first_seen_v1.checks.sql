a27191362fa968c767849a7eb5b8fed6ecccd800
Anna Scholtz <anna@scholtzan.net>
2024-01-10 11:38:56 -0800

    Add checks for baseline_clients_daily and baseline_clients_last_seen queries

---
+++ sql_generators/glean_usage/templates/baseline_clients_first_seen_v1.checks.sql
@@ -0,0 +1,19 @@
+{{ header }}
+
+{#
+   We use raw here b/c the first pass is rendered to create the checks.sql
+   files, and the second pass is rendering of the checks themselves.
+
+   For example, the header above is rendered for every checks file
+   when we create the checks file, when `bqetl generate glean_usage`
+   is called.
+
+   However the second part, where we render the check is_unique() below,
+   is rendered when we _run_ the check, during `bqetl query backfill`
+   (you can also run them locally with `bqetl check run`).
+#}
+{% raw -%}
+#warn
+{{ is_unique(["client_id"], where="submission_date = @submission_date") }}
+{% endraw %}
+

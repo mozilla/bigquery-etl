4e32ef9f6e44fe8da911464d16470f10bea2b8ff
Sean Rose <srose@mozilla.com>
2024-01-12 09:51:43 -0800

    Quote column names containing Jinja expressions to prevent `bqetl format` causing invalid SQL.

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/baseline_clients_yearly_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/baseline_clients_yearly_v1/query.sql
@@ -12,7 +12,7 @@
   AS
   SELECT
     {% for usage_type, _ in usage_types %}
-      CAST(NULL AS BYTES) AS days_ {{ usage_type }} _bytes,
+      CAST(NULL AS BYTES) AS `days_{{ usage_type }}_bytes`,
     {% endfor %}
   -- We make sure to delay * until the end so that as new columns are added
   -- to the daily table we can add those columns in the same order to the end
@@ -31,7 +31,7 @@
     -- 365 days for each usage criterion as an array of bytes. The
     -- rightmost bit represents whether the user was active in the current day.
       {% for usage_type, criterion in usage_types %}
-        udf.bool_to_365_bits({{ criterion }}) AS days_ {{ usage_type }} _bytes,
+        udf.bool_to_365_bits({{ criterion }}) AS `days_{{ usage_type }}_bytes`,
       {% endfor %}
       * EXCEPT (submission_date),
     FROM
@@ -58,9 +58,9 @@
     IF(_current.client_id IS NOT NULL, _current, _previous).* REPLACE (
       {% for usage_type, _ in usage_types %}
         udf.combine_adjacent_days_365_bits(
-          _previous.days_ {{ usage_type }} _bytes,
-          _current.days_ {{ usage_type }} _bytes
-        ) AS days_ {{ usage_type }} _bytes {{ "," if not loop.last }}
+          _previous.`days_{{ usage_type }}_bytes`,
+          _current.`days_{{ usage_type }}_bytes`
+        ) AS `days_{{ usage_type }}_bytes` {{ "," if not loop.last }}
 
       {% endfor %}
     )

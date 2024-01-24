dfa1208d8167ed71c8cf5d5c7121fac50be997da
Frank Bertsch <frank.bertsch@gmail.com>
2024-01-24 14:41:59 -0500

    Add android_state_v2 to view (#4876)
    * Add android_state_v2 to view
    
    * Add new state function to client_ltv view

--- sql/moz-fx-data-shared-prod/fenix/ltv_states/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/ltv_states/view.sql
@@ -29,6 +29,18 @@ SELECT
       {{ max_weeks }},
       first_reported_country
     ) AS android_states_v1,
+    mozfun.ltv.android_states_v2(
+      adjust_network,
+      days_since_first_seen,
+      days_since_seen,
+      {{ death_time }},
+      submission_date,
+      first_seen_date,
+      activity_pattern,
+      active_on_this_date,
+      {{ max_weeks }},
+      first_reported_country
+    ) AS android_states_v2,
     mozfun.ltv.android_states_with_paid_v1(
       adjust_network,
       days_since_first_seen,

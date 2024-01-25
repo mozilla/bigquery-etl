7926aeaeca446769e13d210bd29d0fd543416eae
Katie Windau <kwindau@mozilla.com>
2024-01-25 16:38:05 -0600

    Making aliases more meaningful

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_hits_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_hits_v2/query.sql
@@ -174,124 +174,124 @@ first_and_last_interaction AS (
 ),
 final_staging AS (
   SELECT
-    a.date,
-    a.visit_identifier,
-    a.full_visitor_id,
-    a.visit_start_time,
-    REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', '') AS page_path,
-    SPLIT(REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', ''), '/')[
+    all_sessions.date,
+    all_sessions.visit_identifier,
+    all_sessions.full_visitor_id,
+    all_sessions.visit_start_time,
+    REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', '') AS page_path,
+    SPLIT(REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', ''), '/')[
       SAFE_OFFSET(1)
     ] AS page_path_level1,
     CASE
-      WHEN b.event_name = 'page_view'
+      WHEN all_events.event_name = 'page_view'
         THEN 'PAGE'
       ELSE 'EVENT'
     END AS hit_type,
-    CAST(COALESCE(c.is_exit, 0) AS bool) AS is_exit,
-    CAST(b.is_entrance AS bool) AS is_entrance,
-    b.hit_number,
-    b.event_timestamp AS hit_timestamp,
+    CAST(COALESCE(exits.is_exit, 0) AS bool) AS is_exit,
+    CAST(all_events.is_entrance AS bool) AS is_entrance,
+    all_events.hit_number,
+    all_events.event_timestamp AS hit_timestamp,
     CAST(NULL AS string) AS event_category, --GA4 has no notion of event_label, unlike GA3 (UA360)
-    b.event_name,
+    all_events.event_name,
     CAST(NULL AS string) AS event_label, --GA4 has no notion of event_label, unlike GA3 (UA360)
     CAST(NULL AS string) AS event_action, --GA4 has no notion of event_action, unlike GA3 (UA360)
-    b.device_category,
-    b.operating_system,
-    b.language,
-    b.browser,
-    b.browser_version,
-    b.country,
-    b.source,
-    b.medium,
-    b.campaign,
-    b.ad_content,
-    d.session_had_an_engaged_event AS visits, --this is the equivalent logic to totals.visits in UA
+    all_events.device_category,
+    all_events.operating_system,
+    all_events.language,
+    all_events.browser,
+    all_events.browser_version,
+    all_events.country,
+    all_events.source,
+    all_events.medium,
+    all_events.campaign,
+    all_events.ad_content,
+    engmgt.session_had_an_engaged_event AS visits, --this is the equivalent logic to totals.visits in UA
     CASE
-      WHEN c.nbr_page_view_events = 1
+      WHEN exits.nbr_page_view_events = 1
         THEN 1
       ELSE 0
     END AS bounces, --this is the equivalent logic to totals.bounces in UA
     SAFE_DIVIDE(engagement_time_msec, 1000) AS hit_time,
-    d.first_interaction,
-    CAST(d.last_interaction AS float64) AS last_interaction,
-    b.is_entrance AS entrances,
-    COALESCE(c.is_exit, 0) AS exits,
+    engmgt.first_interaction,
+    CAST(engmgt.last_interaction AS float64) AS last_interaction,
+    all_events.is_entrance AS entrances,
+    COALESCE(exits.is_exit, 0) AS exits,
     CAST(
       NULL AS string
     ) AS event_id, --old table defined this from event category, action, and label, which no longer exist in GA4
-    SPLIT(REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', ''), '/')[
+    SPLIT(REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', ''), '/')[
       SAFE_OFFSET(1)
     ] AS page_level_1,
-    SPLIT(REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', ''), '/')[
+    SPLIT(REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', ''), '/')[
       SAFE_OFFSET(2)
     ] AS page_level_2,
-    SPLIT(REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', ''), '/')[
+    SPLIT(REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', ''), '/')[
       SAFE_OFFSET(3)
     ] AS page_level_3,
-    SPLIT(REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', ''), '/')[
+    SPLIT(REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', ''), '/')[
       SAFE_OFFSET(4)
     ] AS page_level_4,
-    SPLIT(REGEXP_REPLACE(b.page_location, '^https://www.mozilla.org', ''), '/')[
+    SPLIT(REGEXP_REPLACE(all_events.page_location, '^https://www.mozilla.org', ''), '/')[
       SAFE_OFFSET(5)
     ] AS page_level_5
   FROM
-    get_session_start_time a
+    get_session_start_time all_sessions
   LEFT OUTER JOIN
-    get_all_events_in_each_session b
-    ON a.date = b.date
-    AND a.visit_identifier = b.visit_identifier
-    AND a.full_visitor_id = b.full_visitor_id
+    get_all_events_in_each_session all_events
+    ON all_sessions.date = all_events.date
+    AND all_sessions.visit_identifier = all_events.visit_identifier
+    AND all_sessions.full_visitor_id = all_events.full_visitor_id
   LEFT OUTER JOIN
-    session_exits c
-    ON b.visit_identifier = c.visit_identifier
-    AND b.row_nbr = c.row_nbr_to_count_as_exit
+    session_exits exits
+    ON all_events.visit_identifier = exits.visit_identifier
+    AND all_events.row_nbr = exits.row_nbr_to_count_as_exit
   LEFT OUTER JOIN
-    first_and_last_interaction d
-    ON a.visit_identifier = d.visit_identifier
+    first_and_last_interaction engmgt
+    ON all_sessions.visit_identifier = engmgt.visit_identifier
 )
 SELECT
-  a.date,
-  a.visit_identifier,
-  a.full_visitor_id,
-  a.visit_start_time,
-  a.page_path,
-  a.page_path_level1,
-  a.hit_type,
-  a.is_exit,
-  a.is_entrance,
-  a.hit_number,
-  a.hit_timestamp,
-  a.event_category,
-  a.event_name,
-  a.event_label,
-  a.event_action,
-  a.device_category,
-  a.operating_system,
-  a.language,
-  a.browser,
-  a.browser_version,
-  a.country,
-  a.source,
-  a.medium,
-  a.campaign,
-  a.ad_content,
-  a.visits,
-  a.bounces,
-  a.hit_time,
-  a.first_interaction,
-  a.last_interaction,
-  a.entrances,
-  a.exits,
-  a.event_id,
-  a.page_level_1,
-  a.page_level_2,
-  a.page_level_3,
-  a.page_level_4,
-  a.page_level_5,
+  final.date,
+  final.visit_identifier,
+  final.full_visitor_id,
+  final.visit_start_time,
+  final.page_path,
+  final.page_path_level1,
+  final.hit_type,
+  final.is_exit,
+  final.is_entrance,
+  final.hit_number,
+  final.hit_timestamp,
+  final.event_category,
+  final.event_name,
+  final.event_label,
+  final.event_action,
+  final.device_category,
+  final.operating_system,
+  final.language,
+  final.browser,
+  final.browser_version,
+  final.country,
+  final.source,
+  final.medium,
+  final.campaign,
+  final.ad_content,
+  final.visits,
+  final.bounces,
+  final.hit_time,
+  final.first_interaction,
+  final.last_interaction,
+  final.entrances,
+  final.exits,
+  final.event_id,
+  final.page_level_1,
+  final.page_level_2,
+  final.page_level_3,
+  final.page_level_4,
+  final.page_level_5,
   IF(
     page_level_2 IS NULL,
     CONCAT('/', page_level_1, '/'),
     ARRAY_TO_STRING(['', page_level_1, page_level_2, page_level_3, page_level_4, page_level_5], '/')
   ) AS page_name,
 FROM
-  final_staging a
+  final_staging final

7256df0cbea8db69811334bd8c6d1084427f710b
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-01-26 21:50:51 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_google_analytics_derived_ga4.py
+++ dags/bqetl_google_analytics_derived_ga4.py
@@ -48,6 +48,17 @@ with DAG(
     doc_md=docs,
     tags=tags,
 ) as dag:
+    ga_derived__www_site_downloads__v2 = bigquery_etl_query(
+        task_id="ga_derived__www_site_downloads__v2",
+        destination_table="www_site_downloads_v2",
+        dataset_id="ga_derived",
+        project_id="moz-fx-data-marketing-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     ga_derived__www_site_hits__v2 = bigquery_etl_query(
         task_id="ga_derived__www_site_hits__v2",
         destination_table="www_site_hits_v2",
---
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
@@ -0,0 +1,52 @@
+-- Query for ga_derived.www_site_downloads_v2
+-- For more information on writing queries see:
+-- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+SELECT
+  PARSE_DATE('%Y%m%d', event_date) AS date,
+  a.user_pseudo_id || '-' || CAST(
+    (
+      SELECT
+        `value`
+      FROM
+        UNNEST(event_params)
+      WHERE
+        key = 'ga_session_id'
+      LIMIT
+        1
+    ).int_value AS STRING
+  ) AS visit_identifier,
+  device.category AS device_category,
+  device.operating_system AS operating_system,
+  device.language AS language,
+  geo.country AS country,
+  collected_traffic_source.manual_source AS source,
+  collected_traffic_source.manual_medium AS medium,
+  collected_traffic_source.manual_campaign_name AS campaign,
+  collected_traffic_source.manual_content AS ad_content,
+  device.web_info.browser AS browser,
+  --note: the 2 columns are the same because in GA4, there is no logic saying you can only count 1 download per session, unlike GA3
+  COUNTIF(event_name = 'product_download') AS download_events,
+  COUNTIF(event_name = 'product_download') AS downloads,
+  COUNTIF(
+    event_name = 'product_download'
+    AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+  ) AS non_fx_downloads,
+FROM
+  `moz-fx-data-marketing-prod.analytics_313696158.events_*` a
+JOIN
+  UNNEST(event_params) e
+WHERE
+  _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d', @submission_date)
+  AND e.key = 'ga_session_id'
+GROUP BY
+  date,
+  visit_identifier,
+  device_category,
+  operating_system,
+  LANGUAGE,
+  country,
+  source,
+  medium,
+  campaign,
+  ad_content,
+  browser

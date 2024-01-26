24803fb6e799bcf096c30afd7cc9bdfc6f8199cf
kik-kik <kignasiak@mozilla.com>
2024-01-26 11:18:05 +0100

    Update fxa_users_first_seen view to use v2 of the corresponding table

--- sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_first_seen/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_first_seen/view.sql
@@ -2,6 +2,8 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_accounts.fxa_users_first_seen`
 AS
 SELECT
-  *
+  *,
+  -- TODO: remove the `services_used` field once we confirm no downstream usage.
+  [CAST(NULL AS STRING)] AS services_used,
 FROM
-  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_first_seen_v1`
+  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_first_seen_v2`

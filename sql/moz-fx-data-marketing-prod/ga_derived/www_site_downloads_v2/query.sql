0d905c81c18ee0d61233e76396f9be27c689b62a
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-01-27 20:14:35 -0600

    Update query.sql

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
@@ -11,11 +11,10 @@ SELECT
   collected_traffic_source.manual_content AS ad_content,
   device.web_info.browser AS browser,
   --note: the 2 columns are the same because in GA4, there is no logic saying you can only count 1 download per session, unlike GA3
-  COUNTIF(event_name = 'product_download') AS download_events,
-  COUNTIF(event_name = 'product_download') AS downloads,
+  COUNT(1) AS download_events,
+  COUNT(1) AS downloads,
   COUNTIF(
-    event_name = 'product_download'
-    AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+    NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
   ) AS non_fx_downloads,
 FROM
   `moz-fx-data-marketing-prod.analytics_313696158.events_*` AS a

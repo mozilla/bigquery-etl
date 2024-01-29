3e12c5a3cb0de6dc07abd3b40b7aa4e1a9b7ae25
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-01-29 09:02:22 -0600

    Minor updates to www_site_downloads_v2 DENG-2492 (#4902)
    * Minor updates to www_site_downloads_v2 DENG-2492
    
    * Update schema.yaml
    
    * Update query.sql
    
    * Update query.sql
    
    * Update sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
    
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>
    
    * Update query.sql
    
    * Update query.sql
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Sean Rose <1994030+sean-rose@users.noreply.github.com>

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
@@ -1,23 +1,9 @@
--- Query for ga_derived.www_site_downloads_v2
--- For more information on writing queries see:
--- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
 SELECT
   PARSE_DATE('%Y%m%d', event_date) AS date,
-  a.user_pseudo_id || '-' || CAST(
-    (
-      SELECT
-        `value`
-      FROM
-        UNNEST(event_params)
-      WHERE
-        key = 'ga_session_id'
-      LIMIT
-        1
-    ).int_value AS STRING
-  ) AS visit_identifier,
+  a.user_pseudo_id || '-' || CAST(e.value.int_value AS string) AS visit_identifier,
   device.category AS device_category,
   device.operating_system AS operating_system,
-  device.language AS language,
+  device.language AS `language`,
   geo.country AS country,
   collected_traffic_source.manual_source AS source,
   collected_traffic_source.manual_medium AS medium,
@@ -25,25 +11,25 @@ SELECT
   collected_traffic_source.manual_content AS ad_content,
   device.web_info.browser AS browser,
   --note: the 2 columns are the same because in GA4, there is no logic saying you can only count 1 download per session, unlike GA3
-  COUNTIF(event_name = 'product_download') AS download_events,
-  COUNTIF(event_name = 'product_download') AS downloads,
+  COUNT(1) AS download_events,
+  COUNT(1) AS downloads,
   COUNTIF(
-    event_name = 'product_download'
-    AND NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
+    NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(device.web_info.browser)
   ) AS non_fx_downloads,
 FROM
-  `moz-fx-data-marketing-prod.analytics_313696158.events_*` a
+  `moz-fx-data-marketing-prod.analytics_313696158.events_*` AS a
 JOIN
-  UNNEST(event_params) e
+  UNNEST(event_params) AS e
 WHERE
   _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d', @submission_date)
   AND e.key = 'ga_session_id'
+  AND a.event_name = 'product_download'
 GROUP BY
   date,
   visit_identifier,
   device_category,
   operating_system,
-  LANGUAGE,
+  `language`,
   country,
   source,
   medium,

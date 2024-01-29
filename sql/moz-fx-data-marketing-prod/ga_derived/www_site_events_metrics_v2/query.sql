5c887867e313cb673a0b73dece9542c4e6808d3d
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-01-29 11:20:45 -0600

    DENG-2492 - create new table www_site_events_metrics_v2 (#4911)
    * DENG-2492 - initial commit for new table www_site_events_metrics_v2
    
    * DENG-2492 - updating query
    
    * DENG-2492 - updating schema column descriptions
    
    * DENG-2492 - remove extra space in yaml
    
    * DENG-2492 - Remove unnecessary auto-generated comment
    
    * Adding column descriptions for language and locale
    
    * Adding additional clustering fields

---
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_events_metrics_v2/query.sql
@@ -0,0 +1,52 @@
+SELECT
+  date,
+  event_name AS event_category,
+  event_name AS event_action,
+  event_name AS event_label,
+  page_path,
+  page_path_level1 AS locale,
+  page_level_1,
+  page_level_2,
+  page_level_3,
+  page_level_4,
+  page_level_5,
+  page_name,
+  device_category,
+  operating_system,
+  `language`,
+  browser,
+  country,
+  source,
+  medium,
+  campaign,
+  ad_content,
+  COUNT(*) AS total_events,
+  COUNT(DISTINCT visit_identifier) AS unique_events,
+FROM
+  `moz-fx-data-marketing-prod.ga_derived.www_site_hits_v2`
+WHERE
+  date = @submission_date
+  AND hit_type = 'EVENT'
+  AND visits = 1
+GROUP BY
+  date,
+  event_category,
+  event_action,
+  event_label,
+  page_path,
+  locale,
+  page_level_1,
+  page_level_2,
+  page_level_3,
+  page_level_4,
+  page_level_5,
+  device_category,
+  operating_system,
+  `language`,
+  browser,
+  country,
+  source,
+  medium,
+  campaign,
+  ad_content,
+  page_name

ec8a2d5f680e43cefb347ffbccf2f4012c75e2ba
kik-kik <kignasiak@mozilla.com>
2024-02-01 15:59:20 +0100

    No logic changes required, added a note to the views to for clarification

--- sql/moz-fx-data-shared-prod/apple_ads/ad_group_report/view.sql
+++ sql/moz-fx-data-shared-prod/apple_ads/ad_group_report/view.sql
@@ -1,6 +1,11 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.apple_ads.ad_group_report`
 AS
+-- As per the Fivetran connector docs (https://fivetran.com/docs/applications/apple-search-ads#utcconversion):
+-- "We don't convert source timestamps to Universal Time Coordinated (UTC)
+-- but use the Apple Search Ads account's time zone to store the data in your destination."
+--
+-- ! We should be careful about this and make sure our account is set to UTC and not changed.
 SELECT
   *
 FROM

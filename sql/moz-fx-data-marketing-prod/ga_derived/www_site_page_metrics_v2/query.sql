3e4b877dc1a9d5db7bd1033771e3a44d9e7ef333
Katie Windau <kwindau@mozilla.com>
2024-02-01 16:43:26 -0600

    work in progress

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_page_metrics_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_page_metrics_v2/query.sql
@@ -1,3 +1,71 @@
+--first, calculate the next page view's start time relative to when the session started in seconds using lead
+with page_view_staging AS (
+
+    SELECT
+    *,
+    LEAD(hit_time) OVER (
+      PARTITION BY
+        full_visitor_id,
+        visit_start_time
+      ORDER BY
+        hit_time
+    ) AS next_pageview,
+  FROM
+    `moz-fx-data-marketing-prod.ga_derived.www_site_hits_v2`
+  WHERE
+    date = @submission_date
+    AND hit_type = 'PAGE'
+),
+
+--now, subtract current page view start time from next page view start time to get time on current page
+page_views_only  AS (
+  SELECT 
+  a.date,
+  a.page_path AS page,
+  a.page_path_level1 AS locale,
+  a.page_name,
+  a.page_level_1,
+  a.page_level_2,
+  a.page_level_3,
+  a.page_level_4,
+  a.page_level_5,
+  a.device_category,
+  a.operating_system,
+  a.language,
+  a.browser,
+  a.browser_version,
+  a.country,
+  a.source,
+  a.medium,
+  a.campaign,
+  a.ad_content,
+  sum(a.next_pageview - a.hit_time) AS total_time_on_page
+  FROM page_view_staging
+  GROUP BY 
+  a.date,
+  a.page_path,
+  a.page_path_level1,
+  a.page_name,
+  a.page_level_1,
+  a.page_level_2,
+  a.page_level_3,
+  a.page_level_4,
+  a.page_level_5,
+  a.device_category,
+  a.operating_system,
+  a.language,
+  a.browser,
+  a.browser_version,
+  a.country,
+  a.source,
+  a.medium,
+  a.campaign,
+  a.ad_content
+)
+
+
+
+all_events_staging AS (
 SELECT
   a.date,
   a.page_path AS page,
@@ -25,7 +93,6 @@ SELECT
   SUM(a.entrances) AS entrances,
   SUM(a.exits) AS exits,
   COUNTIF(event_name = 'page_view' AND is_exit IS FALSE) AS non_exit_pageviews,
-  ? AS total_time_on_page,
   COUNTIF(hit_type = 'EVENT') AS total_events,
   COUNT(
     DISTINCT(CASE WHEN hit_type = 'EVENT' THEN visit_identifier ELSE NULL END)
@@ -60,3 +127,28 @@ GROUP BY
   a.medium,
   a.campaign,
   a.ad_content
+) 
+
+--join it all together to get everything plus total time on each page 
+
+SELECT 
+FROM 
+all_events_staging a 
+FULL OUTER JOIN 
+page_views_only b 
+ON a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?
+AND a.? = b.?

f9b675fcbb51529879f144e5194abd080ab89c54
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-02-02 05:22:09 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_tiktokreporter/download_data/view.sql
@@ -0,0 +1,12 @@
+-- Generated via ./bqetl generate stable_views
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_tiktokreporter.download_data`
+AS
+SELECT
+  * REPLACE (
+    mozfun.norm.metadata(metadata) AS metadata,
+    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
+    (SELECT AS STRUCT metrics.*, metrics.text2 AS text) AS metrics
+  )
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_tiktokreporter_stable.download_data_v1`

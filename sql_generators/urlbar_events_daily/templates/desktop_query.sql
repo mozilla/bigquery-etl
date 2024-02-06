f95c0985ca9e6498371194eaf1206648ac1938e0
Teon L Brooks <teon.brooks@gmail.com>
2024-02-06 14:15:57 -0500

    Update query.sql

--- sql_generators/urlbar_events_daily/templates/desktop_query.sql
+++ sql_generators/urlbar_events_daily/templates/desktop_query.sql
@@ -1,13 +1,13 @@
 WITH
   temp_unnested AS (
   SELECT
+    event_id,
     submission_date,
     normalized_channel,
     normalized_country_code,
-    mozfun.norm.result_type_to_product_name(res.result_type) AS type,
-    is_terminal,
+    res.product_result_type AS type,
     CASE
-      WHEN selected_result = res.result_type THEN 1
+      WHEN product_selected_result = res.product_result_type THEN 1
     ELSE
     0
   END
@@ -18,28 +18,31 @@ WITH
     UNNEST (results) AS res
   WHERE
     submission_date = @submission_date
+    AND event_action = 'engaged'
+    AND is_terminal = TRUE
+  )
+  WITH temp_session AS (
+    SELECT
+      event_id,
+      submission_date,
+      normalized_channel,
+      normalized_country_code,
+      type,
+      COUNTIF(is_clicked) AS is_clicked
+      COUNT(*) as n_impressions,
+    FROM temp_unnested
+    GROUP BY
+    1,2
   )
 SELECT
   submission_date,
-  COUNT(*) AS n_impressions,
-  SUM(is_clicked) AS n_clicks,
-  -- SAFE_DIVIDE(SUM(is_clicked), COUNT(*)) AS CTR,
+  SUM(n_impressions) AS n_impressions,
+  COUNTIF(is_clicked) AS n_clicks,
   normalized_channel,
   normalized_country_code,
-  type,
-  CASE
-  -- is 'xchannels_add_on' considered addon or separate?
-    WHEN type IN ('history', 'bookmark', 'open_tab', 'admarketplace_sponsored', 'navigational', 'suggest_add_on', 'wikipedia_enhanced', 'wikipedia_dynamic', 'weather', 'quick_action', 'pocket_collection') THEN TRUE
-    WHEN type IN ('default_partner_search_suggestion',
-    'search_engine',
-    'trending_suggestion',
-    'tab_to_search') THEN FALSE
-  ELSE
-  NULL
-END
-  AS is_fx_suggest
+  type
 FROM
-  temp_unnested
+  temp_session
 GROUP BY
   submission_date,
   normalized_country_code,
@@ -51,3 +54,4 @@ ORDER BY
   n_impressions DESC,
   type ASC,
   normalized_channel
+  

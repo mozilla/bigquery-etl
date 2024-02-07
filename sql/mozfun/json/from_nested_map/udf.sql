a142bf641ecd721d6fbae6c7370baa8d0c5b720b
Jan-Erik Rediger <jrediger@mozilla.com>
2024-02-07 12:33:07 +0100

    New UDF: json.from_nested_map

---
+++ sql/mozfun/json/from_nested_map/udf.sql
@@ -0,0 +1,34 @@
+-- Definition for json.from_nested_map
+CREATE OR REPLACE function json.from_nested_map(input JSON)
+RETURNS json
+LANGUAGE js
+AS
+  """
+  if (input && Object.keys(input).length) {
+    for (const k in input) {
+      if (input[k] && input[k].length) {
+        input[k] = input[k].reduce((acc, {key, value}) => {
+          acc[key] = value;
+          return acc;
+        }, {});
+      }
+    }
+  }
+  return input;
+""";
+
+-- Tests
+SELECT
+  assert.null(json.from_nested_map(NULL)),
+  assert.json_equal(JSON '{ "metric": [] }', json.from_nested_map(JSON '{ "metric": [] }')),
+  assert.json_equal(JSON '{ "metric": {} }', json.from_nested_map(JSON '{ "metric": {} }')),
+  assert.json_equal(
+    JSON '{ "metric": { "extra": 2 } }',
+    json.from_nested_map(JSON '{ "metric": [ {"key": "extra", "value": 2 } ] }')
+  ),
+  assert.json_equal(JSON '{ "metric": {}}', json.from_nested_map(JSON '{ "metric": [1, 2, 3]}')),
+  assert.json_equal(JSON '{ "metric": "value"}', json.from_nested_map(JSON '{ "metric": "value"}')),
+  assert.json_equal(
+    JSON '{ "metric": {}}',
+    json.from_nested_map(JSON '{ "metric": [{"key": "value"}]}')
+  ),

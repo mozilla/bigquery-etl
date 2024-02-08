1364b2af17d54b97168224dfc11935145320dd67
Winnie Chan <10429026+wwyc@users.noreply.github.com>
2024-02-08 10:28:24 -0800

    Fixed array concat if null (#4975)

--- sql/moz-fx-data-shared-prod/monitoring_derived/bigquery_usage_v2/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/bigquery_usage_v2/query.sql
@@ -55,16 +55,21 @@ jobs_by_project AS (
         ARRAY_CONCAT(
           referenced_tables,
           (
-            SELECT
-              ARRAY_AGG(
-                STRUCT(
-                  materialized_view.table_reference.project_id AS project_id,
-                  materialized_view.table_reference.dataset_id AS dataset_id,
-                  materialized_view.table_reference.table_id AS table_id
-                )
-              )
-            FROM
-              UNNEST(materialized_view_statistics.materialized_view) AS materialized_view
+            IFNULL(
+              (
+                SELECT
+                  ARRAY_AGG(
+                    STRUCT(
+                      materialized_view.table_reference.project_id AS project_id,
+                      materialized_view.table_reference.dataset_id AS dataset_id,
+                      materialized_view.table_reference.table_id AS table_id
+                    )
+                  )
+                FROM
+                  UNNEST(materialized_view_statistics.materialized_view) AS materialized_view
+              ),
+              []
+            )
           )
         )
       ) AS referenced_table

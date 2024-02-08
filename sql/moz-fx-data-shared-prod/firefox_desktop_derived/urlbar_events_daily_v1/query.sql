ce3eedc62e0d7e7cf3a80de3bcc40087e2702d1b
Teon L Brooks <teon.brooks@gmail.com>
2024-02-08 16:18:13 -0500

    file movement and cleanup

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/urlbar_events_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/urlbar_events_daily_v1/query.sql
@@ -1,8 +1,8 @@
 WITH
   temp_unnested AS (
   SELECT
-    event_id,
     submission_date,
+    event_id,
     normalized_channel,
     normalized_country_code,
     res.product_result_type AS product_result_type,
@@ -20,37 +20,37 @@ WITH
     submission_date = @submission_date
     AND event_action = 'engaged'
     AND is_terminal = TRUE
-  )
-  WITH temp_session AS (
+  ),
+  temp_session AS (
     SELECT
-      event_id,
       submission_date,
-      normalized_channel,
-      normalized_country_code,
+      event_id,
       product_result_type,
-      COUNTIF(is_clicked) AS is_clicked
+      mozdata.udf.mode_last(ARRAY_AGG(normalized_channel)) AS normalized_channel,
+      mozdata.udf.mode_last(ARRAY_AGG(normalized_country_code)) AS normalized_country_code,
+      COUNTIF(is_clicked > 0) AS is_clicked,
       COUNT(*) as n_impressions,
     FROM temp_unnested
     GROUP BY
-    1,2
+    submission_date,
+    event_id,
+    product_result_type
   )
 SELECT
   submission_date,
-  SUM(n_impressions) AS n_impressions,
-  COUNTIF(is_clicked) AS n_clicks,
-  normalized_channel,
   normalized_country_code,
-  product_result_type
+  normalized_channel,
+  product_result_type,
+  SUM(n_impressions) AS n_impressions,
+  COUNTIF(is_clicked > 0) AS n_clicks
 FROM
   temp_session
 GROUP BY
   submission_date,
   normalized_country_code,
-  type,
+  product_result_type,
   normalized_channel
 ORDER BY
   submission_date DESC,
   normalized_country_code DESC,
-  n_impressions DESC,
-  type ASC,
-  normalized_channel
+  n_impressions DESC

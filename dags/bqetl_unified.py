6593598cf804f068e7db1b89af45c45042ac5769
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-02-08 15:58:34 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_unified.py
+++ dags/bqetl_unified.py
@@ -163,6 +163,20 @@ with DAG(
         depends_on_past=False,
     )
 
+    with TaskGroup(
+        "telemetry_derived__rolling_cohorts__v1_external",
+    ) as telemetry_derived__rolling_cohorts__v1_external:
+        ExternalTaskMarker(
+            task_id="bqetl_analytics_aggregations__wait_for_telemetry_derived__rolling_cohorts__v1",
+            external_dag_id="bqetl_analytics_aggregations",
+            external_task_id="wait_for_telemetry_derived__rolling_cohorts__v1",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=81900)).isoformat() }}",
+        )
+
+        telemetry_derived__rolling_cohorts__v1_external.set_upstream(
+            telemetry_derived__rolling_cohorts__v1
+        )
+
     telemetry_derived__unified_metrics__v1 = bigquery_etl_query(
         task_id="telemetry_derived__unified_metrics__v1",
         destination_table="unified_metrics_v1",

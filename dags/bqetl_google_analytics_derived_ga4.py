cd2a6be886b353ef926a28d64e4d7a27926956ae
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-02-09 20:38:04 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_google_analytics_derived_ga4.py
+++ dags/bqetl_google_analytics_derived_ga4.py
@@ -49,6 +49,17 @@ with DAG(
     doc_md=docs,
     tags=tags,
 ) as dag:
+    ga_derived__firefox_whatsnew_summary__v2 = bigquery_etl_query(
+        task_id="ga_derived__firefox_whatsnew_summary__v2",
+        destination_table="firefox_whatsnew_summary_v2",
+        dataset_id="ga_derived",
+        project_id="moz-fx-data-marketing-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     ga_derived__www_site_downloads__v2 = bigquery_etl_query(
         task_id="ga_derived__www_site_downloads__v2",
         destination_table="www_site_downloads_v2",
@@ -140,6 +151,8 @@ with DAG(
         mozilla_org_derived__ga_sessions__v2_external.set_upstream(
             mozilla_org_derived__ga_sessions__v2
         )
+
+    ga_derived__firefox_whatsnew_summary__v2.set_upstream(ga_derived__www_site_hits__v2)
 
     ga_derived__www_site_events_metrics__v2.set_upstream(ga_derived__www_site_hits__v2)
 

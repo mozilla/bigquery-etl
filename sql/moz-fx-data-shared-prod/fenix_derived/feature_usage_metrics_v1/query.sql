b793d599bebf58a1f6c34d387a368880b1111a5c
rhythmofrain63 <ruoxi63@gmail.com>
2024-02-12 01:03:10 -0800

    update fenix feature usage metrics using ping parse time logic

--- sql/moz-fx-data-shared-prod/fenix_derived/feature_usage_metrics_v1/query.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/feature_usage_metrics_v1/query.sql
@@ -1,18 +1,53 @@
-WITH _metrics_ping_distinct_client_count AS (
+WITH baseline_clients AS (
   SELECT
-    DATE(submission_timestamp) AS submission_date,
-    COUNT(DISTINCT client_info.client_id) AS metrics_ping_distinct_client_count
-  FROM
-    fenix.metrics
-  WHERE
-    DATE(submission_timestamp) = @submission_date
-  GROUP BY
-    submission_date
-),
-client_product_feature_usage AS (
-  SELECT
+    DATE(
+      DATETIME(LEAST(ping_info.parsed_start_time, ping_info.parsed_end_time), 'UTC')
+    ) AS ping_date,
     client_info.client_id,
-    DATE(submission_timestamp) AS submission_date,
+    normalized_channel AS channel,
+    normalized_country_code AS country
+  FROM
+    fenix.baseline
+  WHERE
+    metrics.timespan.glean_baseline_duration.value > 0
+    AND LOWER(metadata.isp.name) <> "browserstack"
+    AND DATE(submission_timestamp)
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 4 DAY)
+    AND @submission_date
+    AND DATE(
+      DATETIME(LEAST(ping_info.parsed_start_time, ping_info.parsed_end_time), 'UTC')
+    ) = DATE_SUB(@submission_date, INTERVAL 4 DAY)
+  QUALIFY
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        client_info.client_id
+      ORDER BY
+        DATE(DATETIME(LEAST(ping_info.parsed_start_time, ping_info.parsed_end_time), 'UTC'))
+    ) = 1
+),
+client_attribution AS (
+  SELECT
+    client_id,
+    channel,
+    adjust_network,
+  FROM
+    fenix.firefox_android_clients
+),
+metric_ping_clients_feature_usage AS (
+  SELECT
+    -- In rare cases we can have an end_time that is earlier than the start_time, we made the decision
+    -- to attribute the metrics to the earlier date of the two.
+    DATE(
+      DATETIME(LEAST(ping_info.parsed_start_time, ping_info.parsed_end_time), 'UTC')
+    ) AS ping_date,
+    client_info.client_id,
+    normalized_channel AS channel,
+    normalized_country_code AS country,
+    IF(
+      SUM(CASE WHEN metrics.boolean.metrics_default_browser THEN 1 ELSE 0 END) > 0,
+      TRUE,
+      FALSE
+    ) AS is_default_browser,
     --Credential Management: Logins
     COALESCE(SUM(metrics.counter.logins_deleted), 0) AS logins_deleted,
     COALESCE(SUM(metrics.counter.logins_modified), 0) AS logins_modified,
@@ -187,321 +222,263 @@ client_product_feature_usage AS (
   LEFT JOIN
     UNNEST(metrics.labeled_counter.metrics_bookmarks_open) AS metrics_bookmarks_open_table
   WHERE
-    DATE(submission_timestamp) = @submission_date
+    LOWER(metadata.isp.name) <> "browserstack"
+    -- we need to work with a larger time window as some metrics ping arrive with a multi day delay
+    AND DATE(submission_timestamp)
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 4 DAY)
+    AND @submission_date
+    AND DATE(
+      DATETIME(LEAST(ping_info.parsed_start_time, ping_info.parsed_end_time), 'UTC')
+    ) = DATE_SUB(@submission_date, INTERVAL 4 DAY)
   GROUP BY
+    ping_date,
     client_id,
-    submission_date
-),
-product_features_agg AS (
-  SELECT
-    submission_date,
+    channel,
+    country
+)
+-- Aggregated feature usage
+SELECT
+  @submission_date AS submission_date,
+  ping_date,
+  channel,
+  country,
+  adjust_network,
+  is_default_browser,
     /*Logins*/
     --logins deleted
-    COUNT(DISTINCT CASE WHEN logins_deleted > 0 THEN client_id END) AS logins_deleted_users,
-    SUM(logins_deleted) AS logins_deleted,
+  COUNT(DISTINCT CASE WHEN logins_deleted > 0 THEN client_id END) AS logins_deleted_users,
+  SUM(logins_deleted) AS logins_deleted,
     --logins modified
-    COUNT(DISTINCT CASE WHEN logins_modified > 0 THEN client_id END) AS logins_modified_users,
-    SUM(logins_modified) AS logins_modified,
+  COUNT(DISTINCT CASE WHEN logins_modified > 0 THEN client_id END) AS logins_modified_users,
+  SUM(logins_modified) AS logins_modified,
     --logins currently stored
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN currently_stored_logins > 0
-          THEN client_id
-      END
-    ) AS currently_stored_logins_users,
-    SUM(currently_stored_logins) AS currently_stored_logins,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN currently_stored_logins > 0
+        THEN client_id
+    END
+  ) AS currently_stored_logins_users,
+  SUM(currently_stored_logins) AS currently_stored_logins,
     /*Credit Card*/
     --credit card deleted
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN credit_cards_deleted > 0
-          THEN client_id
-      END
-    ) AS credit_cards_deleted_users,
-    SUM(credit_cards_deleted) AS credit_cards_deleted,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN credit_cards_deleted > 0
+        THEN client_id
+    END
+  ) AS credit_cards_deleted_users,
+  SUM(credit_cards_deleted) AS credit_cards_deleted,
     --credit card currently stored
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN currently_stored_credit_cards > 0
-          THEN client_id
-      END
-    ) AS currently_stored_credit_cards_users,
-    SUM(currently_stored_credit_cards) AS currently_stored_credit_cards,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN currently_stored_credit_cards > 0
+        THEN client_id
+    END
+  ) AS currently_stored_credit_cards_users,
+  SUM(currently_stored_credit_cards) AS currently_stored_credit_cards,
     /*Address*/
     --address deleted
-    COUNT(DISTINCT CASE WHEN addresses_deleted > 0 THEN client_id END) AS addresses_deleted_users,
-    SUM(addresses_deleted) AS addresses_deleted,
+  COUNT(DISTINCT CASE WHEN addresses_deleted > 0 THEN client_id END) AS addresses_deleted_users,
+  SUM(addresses_deleted) AS addresses_deleted,
     --address modified
-    COUNT(DISTINCT CASE WHEN addresses_modified > 0 THEN client_id END) AS addresses_modified_users,
-    SUM(addresses_modified) AS addresses_modified,
+  COUNT(DISTINCT CASE WHEN addresses_modified > 0 THEN client_id END) AS addresses_modified_users,
+  SUM(addresses_modified) AS addresses_modified,
     -- addresses currently stored
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN currently_stored_addresses > 0
-          THEN client_id
-      END
-    ) AS currently_stored_addresses_users,
-    SUM(currently_stored_addresses) AS currently_stored_addresses,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN currently_stored_addresses > 0
+        THEN client_id
+    END
+  ) AS currently_stored_addresses_users,
+  SUM(currently_stored_addresses) AS currently_stored_addresses,
     /*Bookmark*/
     --bookmarks_add
-    COUNT(DISTINCT CASE WHEN bookmarks_add > 0 THEN client_id END) AS bookmarks_add_users,
-    SUM(bookmarks_add) AS bookmarks_add,
+  COUNT(DISTINCT CASE WHEN bookmarks_add > 0 THEN client_id END) AS bookmarks_add_users,
+  SUM(bookmarks_add) AS bookmarks_add,
     --bookmarks_delete
-    COUNT(DISTINCT CASE WHEN bookmarks_delete > 0 THEN client_id END) AS bookmarks_delete_users,
-    SUM(bookmarks_delete) AS bookmarks_delete,
+  COUNT(DISTINCT CASE WHEN bookmarks_delete > 0 THEN client_id END) AS bookmarks_delete_users,
+  SUM(bookmarks_delete) AS bookmarks_delete,
     --bookmarks_edit
-    COUNT(DISTINCT CASE WHEN bookmarks_edit > 0 THEN client_id END) AS bookmarks_edit_users,
-    SUM(bookmarks_edit) AS bookmarks_edit,
+  COUNT(DISTINCT CASE WHEN bookmarks_edit > 0 THEN client_id END) AS bookmarks_edit_users,
+  SUM(bookmarks_edit) AS bookmarks_edit,
     --bookmarks_open
-    COUNT(DISTINCT CASE WHEN bookmarks_open > 0 THEN client_id END) AS bookmarks_open_users,
-    SUM(bookmarks_open) AS bookmarks_open,
+  COUNT(DISTINCT CASE WHEN bookmarks_open > 0 THEN client_id END) AS bookmarks_open_users,
+  SUM(bookmarks_open) AS bookmarks_open,
     --metrics_desktop_bookmarks_count
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_desktop_bookmarks_count > 0
-          THEN client_id
-      END
-    ) AS metrics_desktop_bookmarks_count_users,
-    SUM(metrics_desktop_bookmarks_count) AS metrics_desktop_bookmarks_count,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_desktop_bookmarks_count > 0
+        THEN client_id
+    END
+  ) AS metrics_desktop_bookmarks_count_users,
+  SUM(metrics_desktop_bookmarks_count) AS metrics_desktop_bookmarks_count,
     --metrics_mobile_bookmarks_count
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_mobile_bookmarks_count > 0
-          THEN client_id
-      END
-    ) AS metrics_mobile_bookmarks_count_users,
-    SUM(metrics_mobile_bookmarks_count) AS metrics_mobile_bookmarks_count,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_mobile_bookmarks_count > 0
+        THEN client_id
+    END
+  ) AS metrics_mobile_bookmarks_count_users,
+  SUM(metrics_mobile_bookmarks_count) AS metrics_mobile_bookmarks_count,
     --metrics_has_desktop_bookmarks
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_has_desktop_bookmarks > 0
-          THEN client_id
-      END
-    ) AS metrics_has_desktop_bookmarks_users,
-    SUM(metrics_has_desktop_bookmarks) AS metrics_has_desktop_bookmarks,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_has_desktop_bookmarks > 0
+        THEN client_id
+    END
+  ) AS metrics_has_desktop_bookmarks_users,
+  SUM(metrics_has_desktop_bookmarks) AS metrics_has_desktop_bookmarks,
     --metrics_has_mobile_bookmarks
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_has_mobile_bookmarks > 0
-          THEN client_id
-      END
-    ) AS metrics_has_mobile_bookmarks_users,
-    SUM(metrics_has_mobile_bookmarks) AS metrics_has_mobile_bookmarks,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_has_mobile_bookmarks > 0
+        THEN client_id
+    END
+  ) AS metrics_has_mobile_bookmarks_users,
+  SUM(metrics_has_mobile_bookmarks) AS metrics_has_mobile_bookmarks,
     /*Privacy*/
     --etp_standard
-    COUNT(DISTINCT CASE WHEN etp_standard > 0 THEN client_id END) AS etp_standard_users,
-    SUM(etp_standard) AS etp_standard,
+  COUNT(DISTINCT CASE WHEN etp_standard > 0 THEN client_id END) AS etp_standard_users,
+  SUM(etp_standard) AS etp_standard,
     --etp_strict
-    COUNT(DISTINCT CASE WHEN etp_strict > 0 THEN client_id END) AS etp_strict_users,
-    SUM(etp_strict) AS etp_strict,
+  COUNT(DISTINCT CASE WHEN etp_strict > 0 THEN client_id END) AS etp_strict_users,
+  SUM(etp_strict) AS etp_strict,
     --etp_custom
-    COUNT(DISTINCT CASE WHEN etp_custom > 0 THEN client_id END) AS etp_custom_users,
-    SUM(etp_custom) AS etp_custom,
+  COUNT(DISTINCT CASE WHEN etp_custom > 0 THEN client_id END) AS etp_custom_users,
+  SUM(etp_custom) AS etp_custom,
     --etp_disabled
-    COUNT(DISTINCT CASE WHEN etp_disabled > 0 THEN client_id END) AS etp_disabled_users,
-    SUM(etp_disabled) AS etp_disabled,
+  COUNT(DISTINCT CASE WHEN etp_disabled > 0 THEN client_id END) AS etp_disabled_users,
+  SUM(etp_disabled) AS etp_disabled,
     /*Tab Count*/
     --metrics_private_tabs_open_count
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_private_tabs_open_count > 0
-          THEN client_id
-      END
-    ) AS metrics_private_tabs_open_count_users,
-    SUM(metrics_private_tabs_open_count) AS metrics_private_tabs_open_count,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_private_tabs_open_count > 0
+        THEN client_id
+    END
+  ) AS metrics_private_tabs_open_count_users,
+  SUM(metrics_private_tabs_open_count) AS metrics_private_tabs_open_count,
     --metrics_tabs_open_count
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_tabs_open_count > 0
-          THEN client_id
-      END
-    ) AS metrics_tabs_open_count_users,
-    SUM(metrics_tabs_open_count) AS metrics_tabs_open_count,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_tabs_open_count > 0
+        THEN client_id
+    END
+  ) AS metrics_tabs_open_count_users,
+  SUM(metrics_tabs_open_count) AS metrics_tabs_open_count,
     /*Default Browser*/
     --metrics_default_browser
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_default_browser > 0
-          THEN client_id
-      END
-    ) AS metrics_default_browser_users,
-    SUM(metrics_default_browser) AS metrics_default_browser,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_default_browser > 0
+        THEN client_id
+    END
+  ) AS metrics_default_browser_users,
+  SUM(metrics_default_browser) AS metrics_default_browser,
     /*Awesomebar Location*/
     --awesomebar_top
-    COUNT(DISTINCT CASE WHEN awesomebar_top > 0 THEN client_id END) AS awesomebar_top_users,
-    SUM(awesomebar_top) AS awesomebar_top,
+  COUNT(DISTINCT CASE WHEN awesomebar_top > 0 THEN client_id END) AS awesomebar_top_users,
     --awesomebar_bottom
-    COUNT(DISTINCT CASE WHEN awesomebar_bottom > 0 THEN client_id END) AS awesomebar_bottom_users,
-    SUM(awesomebar_bottom) AS awesomebar_bottom,
+  COUNT(DISTINCT CASE WHEN awesomebar_bottom > 0 THEN client_id END) AS awesomebar_bottom_users,
     --awesomebar_null
-    COUNT(DISTINCT CASE WHEN awesomebar_null > 0 THEN client_id END) AS awesomebar_null_users,
-    SUM(awesomebar_null) AS awesomebar_null,
+  COUNT(DISTINCT CASE WHEN awesomebar_null > 0 THEN client_id END) AS awesomebar_null_users,
     /*Notificaion*/
     --metrics_notifications_allowed
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN metrics_notifications_allowed > 0
-          THEN client_id
-      END
-    ) AS metrics_notifications_allowed_users,
-    SUM(metrics_notifications_allowed) AS metrics_notifications_allowed,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN metrics_notifications_allowed > 0
+        THEN client_id
+    END
+  ) AS metrics_notifications_allowed_users,
+  SUM(metrics_notifications_allowed) AS metrics_notifications_allowed,
     --events_marketing_notification_allowed
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN events_marketing_notification_allowed > 0
-          THEN client_id
-      END
-    ) AS events_marketing_notification_allowed_users,
-    SUM(events_marketing_notification_allowed) AS events_marketing_notification_allowed,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN events_marketing_notification_allowed > 0
+        THEN client_id
+    END
+  ) AS events_marketing_notification_allowed_users,
+  SUM(events_marketing_notification_allowed) AS events_marketing_notification_allowed,
     /*Customize Homepage*/
     --customize_home_contile
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN customize_home_contile > 0
-          THEN client_id
-      END
-    ) AS customize_home_contile_users,
-    SUM(customize_home_contile) AS customize_home_contile,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN customize_home_contile > 0
+        THEN client_id
+    END
+  ) AS customize_home_contile_users,
+  SUM(customize_home_contile) AS customize_home_contile,
     --customize_home_jump_back_in_users
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN customize_home_jump_back_in > 0
-          THEN client_id
-      END
-    ) AS customize_home_jump_back_in_users,
-    SUM(customize_home_jump_back_in) AS customize_home_jump_back_in,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN customize_home_jump_back_in > 0
+        THEN client_id
+    END
+  ) AS customize_home_jump_back_in_users,
+  SUM(customize_home_jump_back_in) AS customize_home_jump_back_in,
     --customize_home_most_visited_sites_users
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN customize_home_most_visited_sites > 0
-          THEN client_id
-      END
-    ) AS customize_home_most_visited_sites_users,
-    SUM(customize_home_most_visited_sites) AS customize_home_most_visited_sites,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN customize_home_most_visited_sites > 0
+        THEN client_id
+    END
+  ) AS customize_home_most_visited_sites_users,
+  SUM(customize_home_most_visited_sites) AS customize_home_most_visited_sites,
     --customize_home_pocket_users
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN customize_home_pocket > 0
-          THEN client_id
-      END
-    ) AS customize_home_pocket_users,
-    SUM(customize_home_pocket) AS customize_home_pocket,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN customize_home_pocket > 0
+        THEN client_id
+    END
+  ) AS customize_home_pocket_users,
+  SUM(customize_home_pocket) AS customize_home_pocket,
     --customize_home_recently_saved_users
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN customize_home_recently_saved > 0
-          THEN client_id
-      END
-    ) AS customize_home_recently_saved_users,
-    SUM(customize_home_recently_saved) AS customize_home_recently_saved,
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN customize_home_recently_saved > 0
+        THEN client_id
+    END
+  ) AS customize_home_recently_saved_users,
+  SUM(customize_home_recently_saved) AS customize_home_recently_saved,
     --customize_home_recently_visited_users
-    COUNT(
-      DISTINCT
-      CASE
-        WHEN customize_home_recently_visited > 0
-          THEN client_id
-      END
-    ) AS customize_home_recently_visited_users,
-    SUM(customize_home_recently_visited) AS customize_home_recently_visited,
-  FROM
-    client_product_feature_usage
-  GROUP BY
-    submission_date
-)
-SELECT
-  submission_date,
-  metrics_ping_distinct_client_count,
-/*logins*/
-  logins_deleted_users,
-  logins_deleted,
-  logins_modified_users,
-  logins_modified,
-  currently_stored_logins_users,
-  currently_stored_logins,
-/*credit card*/
-  credit_cards_deleted_users,
-  credit_cards_deleted,
-  currently_stored_credit_cards_users,
-  currently_stored_credit_cards,
-/*addresses*/
-  addresses_deleted_users,
-  addresses_deleted,
-  addresses_modified_users,
-  addresses_modified,
-  currently_stored_addresses_users,
-  currently_stored_addresses,
-/*bookmark*/
-  bookmarks_add_users,
-  bookmarks_add,
-  bookmarks_delete_users,
-  bookmarks_delete,
-  bookmarks_edit_users,
-  bookmarks_edit,
-  bookmarks_open_users,
-  bookmarks_open,
-  metrics_desktop_bookmarks_count_users,
-  metrics_desktop_bookmarks_count,
-  metrics_mobile_bookmarks_count_users,
-  metrics_mobile_bookmarks_count,
-  metrics_has_desktop_bookmarks_users,
-  metrics_has_desktop_bookmarks,
-  metrics_has_mobile_bookmarks_users,
-  metrics_has_mobile_bookmarks,
-/*privacy*/
-  etp_standard_users,
-  etp_standard,
-  etp_strict_users,
-  etp_strict,
-  etp_custom_users,
-  etp_custom,
-  etp_disabled_users,
-  etp_disabled,
-/*Tab count*/
-  metrics_private_tabs_open_count_users,
-  metrics_private_tabs_open_count,
-  metrics_tabs_open_count_users,
-  metrics_tabs_open_count,
-/*Default browser*/
-  metrics_default_browser_users,
-  metrics_default_browser,
-/*Notification*/
-  metrics_notifications_allowed_users,
-  metrics_notifications_allowed,
-  events_marketing_notification_allowed_users,
-  events_marketing_notification_allowed,
-/*Customize Home*/
-  customize_home_contile_users,
-  customize_home_contile,
-  customize_home_jump_back_in_users,
-  customize_home_jump_back_in,
-  customize_home_most_visited_sites_users,
-  customize_home_most_visited_sites,
-  customize_home_pocket_users,
-  customize_home_pocket,
-  customize_home_recently_saved_users,
-  customize_home_recently_saved,
-  customize_home_recently_visited_users,
-  customize_home_recently_visited
+  COUNT(
+    DISTINCT
+    CASE
+      WHEN customize_home_recently_visited > 0
+        THEN client_id
+    END
+  ) AS customize_home_recently_visited_users,
+  SUM(customize_home_recently_visited) AS customize_home_recently_visited
 FROM
-  _metrics_ping_distinct_client_count
+  metric_ping_clients_feature_usage
+INNER JOIN
+  baseline_clients
+  USING (ping_date, client_id, channel, country)
 LEFT JOIN
-  product_features_agg
-  USING (submission_date)
+  client_attribution
+  USING (client_id, channel)
+GROUP BY
+  submission_date,
+  ping_date,
+  channel,
+  country,
+  adjust_network,
+  is_default_browser

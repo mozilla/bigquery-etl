28a976e3171bc84fc6b62f2dc7490120235b0d58
Leli Schiestl <lschiestl@mozilla.com>
2024-02-22 17:01:18 +0100

    DENG-2831 create UDF to extract UTM parameters from URL

---
+++ sql/mozfun/utils/extract_utm_from_url/README.md
@@ -0,0 +1,4 @@
+This UDF extracts UTM parameters from a URL string.
+
+UTM (Urchin Tracking Module) parameters are URL parameters used by marketing to track the effectiveness of online marketing campaigns.
+
---
+++ sql/mozfun/utils/extract_utm_from_url/udf.sql
@@ -0,0 +1,59 @@
+
+                -- Definition for utils.extract_utm_from_url
+                -- For more information on writing UDFs see:
+                -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+CREATE OR REPLACE FUNCTION utils.extract_utm_from_url(url STRING)
+RETURNS STRUCT<
+  utm_source STRING,
+  utm_medium STRING,
+  utm_campaign STRING,
+  utm_content STRING,
+  utm_term STRING
+> AS (
+  STRUCT(
+    REGEXP_EXTRACT(url, r"utm_source=([^&\'\"\]]+)"),   -- utm_source
+    REGEXP_EXTRACT(url, r"utm_medium=([^&\'\"\]]+)"),   -- utm_medium
+    REGEXP_EXTRACT(url, r"utm_campaign=([^&\'\"\]]+)"), -- utm_campaign
+    REGEXP_EXTRACT(url, r"utm_content=([^&\'\"\]]+)"),  -- utm_content
+    REGEXP_EXTRACT(url, r"utm_term=([^&\'\"\]]+)")      -- utm_term
+  )
+);
+
+-- Tests
+SELECT
+  mozfun.assert.equals("google-rsa", utils.extract_utm_from_url(url_1).utm_source),
+  mozfun.assert.equals("paidsearch", utils.extract_utm_from_url(url_1).utm_medium),
+  mozfun.assert.equals("fxeu", utils.extract_utm_from_url(url_1).utm_campaign),
+  mozfun.assert.equals("A123_A234_7890", utils.extract_utm_from_url(url_1).utm_content),
+  mozfun.assert.null(utils.extract_utm_from_url(url_1).utm_term),
+  mozfun.assert.equals("bar", utils.extract_utm_from_url(url_2).utm_source),
+  mozfun.assert.equals("baz", utils.extract_utm_from_url(url_2).utm_medium),
+  mozfun.assert.null(utils.extract_utm_from_url(url_2).utm_campaign),
+  mozfun.assert.null(utils.extract_utm_from_url(url_2).utm_content),
+  mozfun.assert.equals("foo", utils.extract_utm_from_url(url_2).utm_term),
+  mozfun.assert.null(utils.extract_utm_from_url(url_3).utm_source),
+  mozfun.assert.null(utils.extract_utm_from_url(url_3).utm_medium),
+  mozfun.assert.null(utils.extract_utm_from_url(url_3).utm_campaign),
+  mozfun.assert.null(utils.extract_utm_from_url(url_3).utm_content),
+  mozfun.assert.null(utils.extract_utm_from_url(url_3).utm_term),
+  mozfun.assert.null(utils.extract_utm_from_url(url_4).utm_source),
+  mozfun.assert.null(utils.extract_utm_from_url(url_4).utm_medium),
+  mozfun.assert.null(utils.extract_utm_from_url(url_4).utm_campaign),
+  mozfun.assert.null(utils.extract_utm_from_url(url_4).utm_content),
+  mozfun.assert.null(utils.extract_utm_from_url(url_4).utm_term),
+  mozfun.assert.equals("foo", utils.extract_utm_from_url(url_5).utm_term),
+  mozfun.assert.equals("bar", utils.extract_utm_from_url(url_5).utm_source),
+  mozfun.assert.equals("foo", utils.extract_utm_from_url(url_6).utm_term),
+  mozfun.assert.equals("bar", utils.extract_utm_from_url(url_6).utm_source),
+  mozfun.assert.equals("bar", utils.extract_utm_from_url(url_7).utm_source)
+FROM
+  (
+    SELECT
+      "https://www.mozilla.org/fr/firefox/new/?utm_medium=paidsearch&utm_source=google-rsa&utm_campaign=fxeu&utm_content=A123_A234_7890" AS url_1,
+      "http://some-url.com/?utm_term=foo&utm_source=bar&utm_medium=baz" AS url_2,
+      "https://www.mozilla.org" AS url_3,
+      "" AS url_4,
+      "'http://some-url.com/?utm_term=foo&utm_source=bar'" AS url_5,
+      '"http://some-url.com/?utm_term=foo&utm_source=bar"' AS url_6,
+      "[http://some-url.com/?utm_source=bar]" AS url_7
+  )

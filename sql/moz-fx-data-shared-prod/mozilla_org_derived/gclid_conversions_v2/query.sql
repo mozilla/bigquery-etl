5e15213443b64e67bf203eae4d38815f8ccf19b4
Marlene-M-Hirose <mhirose@mozilla.com>
2024-02-22 09:13:27 -1000

    reformat sql

--- sql/moz-fx-data-shared-prod/mozilla_org_derived/gclid_conversions_v2/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org_derived/gclid_conversions_v2/query.sql
@@ -1,62 +1,61 @@
-WITH gclids_to_ga_ids_stg AS(
+WITH gclids_to_ga_ids_stg
+AS
+  (
+    SELECT DISTINCT
+      gclid,
+      ga_client_id,
+      stub_session_id,
+    FROM
+      `moz-fx-data-shared-prod.mozilla_org_derived.ga_sessions_v2`,
+      UNNEST(gclid_array) AS gclid
+    CROSS JOIN
+      UNNEST(all_reported_stub_session_ids) AS stub_session_id
+    WHERE
+      session_date >= DATE_SUB(@activity_date, INTERVAL @conversion_window DAY)
+      AND gclid IS NOT NULL
+  ),
+  gclids_to_ga_ids AS (
+    SELECT DISTINCT
+      gclid,
+      ga_client_id,
+      stub_session_id,
+    FROM
+      `moz-fx-data-shared-prod.mozilla_org_derived.ga_sessions_v2`,
+      UNNEST(gclid_array) AS gclid
+    CROSS JOIN
+      UNNEST(all_reported_stub_session_ids) AS stub_session_id
+  ),
+  ga_ids_to_dl_token AS (
     SELECT
-    distinct gclid,
-    ga_client_id,
-    stub_session_id,
-  FROM
-    `moz-fx-data-shared-prod.mozilla_org_derived.ga_sessions_v2`,
-    UNNEST(gclid_array) AS gclid
-  CROSS JOIN
-    UNNEST(all_reported_stub_session_ids) AS stub_session_id
-  WHERE
-    session_date >= DATE_SUB(@activity_date, INTERVAL @conversion_window DAY)
-    AND gclid IS NOT NULL
-
-),
-
-gclids_to_ga_ids AS (
-  SELECT
-    distinct gclid,
-    ga_client_id,
-    stub_session_id,
-  FROM
-    `moz-fx-data-shared-prod.mozilla_org_derived.ga_sessions_v2`,
-    UNNEST(gclid_array) AS gclid
-  CROSS JOIN
-    UNNEST(all_reported_stub_session_ids) AS stub_session_id
-),
-
-ga_ids_to_dl_token AS (
-  SELECT
-    ga_client_id,
-    stub_session_id,
-    dl_token,
-  FROM
-    `moz-fx-data-shared-prod.stub_attribution_service_derived.dl_token_ga_attribution_lookup_v1`
-  WHERE
-    ga_client_id IS NOT NULL
-    AND stub_session_id IS NOT NULL
-),
-dl_token_to_telemetry_id AS (
-  SELECT
-    client_id AS telemetry_client_id,
-    first_seen_date,
-    attribution_dltoken AS dl_token,
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_first_seen_v2`
-),
-telemetry_id_to_activity AS (
-  SELECT
-    client_id AS telemetry_client_id,
-    submission_date AS activity_date,
-    search_count_all > 0 AS did_search,
-    ad_clicks_count_all > 0 AS did_click_ad,
-    TRUE AS was_active,
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
-  WHERE
-    submission_date = @activity_date
-)
+      ga_client_id,
+      stub_session_id,
+      dl_token,
+    FROM
+      `moz-fx-data-shared-prod.stub_attribution_service_derived.dl_token_ga_attribution_lookup_v1`
+    WHERE
+      ga_client_id IS NOT NULL
+      AND stub_session_id IS NOT NULL
+  ),
+  dl_token_to_telemetry_id AS (
+    SELECT
+      client_id AS telemetry_client_id,
+      first_seen_date,
+      attribution_dltoken AS dl_token,
+    FROM
+      `moz-fx-data-shared-prod.telemetry_derived.clients_first_seen_v2`
+  ),
+  telemetry_id_to_activity AS (
+    SELECT
+      client_id AS telemetry_client_id,
+      submission_date AS activity_date,
+      search_count_all > 0 AS did_search,
+      ad_clicks_count_all > 0 AS did_click_ad,
+      TRUE AS was_active,
+    FROM
+      `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
+    WHERE
+      submission_date = @activity_date
+  )
 SELECT
   activity_date,
   gclid,

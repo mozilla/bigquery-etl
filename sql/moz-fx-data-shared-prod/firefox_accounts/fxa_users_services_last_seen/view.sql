f8d35f7a2e61069a919f8d61e4922f7208c38511
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-02-26 13:52:43 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_services_last_seen/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts/fxa_users_services_last_seen/view.sql
@@ -1,7 +1,1 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_accounts.fxa_users_services_last_seen`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_accounts_derived.fxa_users_services_last_seen_v1`
+

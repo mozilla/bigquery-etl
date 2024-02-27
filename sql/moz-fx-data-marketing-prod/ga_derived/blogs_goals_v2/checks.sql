88ed5440f1bce197ca727f6572f90eed33c9bdd9
Katie Windau <kwindau@mozilla.com>
2024-02-27 16:46:05 -0600

    switch to both be same type

--- sql/moz-fx-data-marketing-prod/ga_derived/blogs_goals_v2/checks.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/blogs_goals_v2/checks.sql
@@ -1,5 +1,5 @@
 #fail
 {{ is_unique(["date", "visit_identifier"], "date = @submission_date") }}
 
-#warn
+#fail
 {{ matches_pattern(column="visit_identifier", pattern="^[0-9]+\\.{1}[0-9]+\\-{1}[0-9]+$", where="date = @submission_date", threshold_fail_percentage=0, message="Warn - some visit_identifier not matching expected pattern") }}

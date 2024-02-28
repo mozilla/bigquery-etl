98c72c6704cb69b0379ed20eb180ab01a0d7e8e9
Sean Rose <1994030+sean-rose@users.noreply.github.com>
2024-02-28 15:39:48 -0800

    Prepare for FxA payments server moving to our GCPv2 setup (OPST-296) (#4903)
    After which its production logs should end up in `moz-fx-fxa-prod.gke_fxa_prod_log.stderr` or `moz-fx-fxa-prod.gke_fxa_prod_log.stdout`, and its stage logs should end up in `moz-fx-fxa-nonprod.gke_fxa_stage_log.stderr` or `moz-fx-fxa-nonprod.gke_fxa_stage_log.stdout`.

--- sql/moz-fx-cjms-prod-f3c7/cjms_bigquery/flows_v1/query.sql
+++ sql/moz-fx-cjms-prod-f3c7/cjms_bigquery/flows_v1/query.sql
@@ -7,7 +7,7 @@ WITH fxa_events AS (
     `moz-fx-data-shared-prod.firefox_accounts.fxa_all_events`
   WHERE
     DATE(`timestamp`) = @submission_date
-    AND fxa_log IN ('content', 'auth', 'stdout')
+    AND fxa_log IN ('content', 'auth', 'stdout', 'payments')
 )
 SELECT
   DATE(`timestamp`) AS submission_date,

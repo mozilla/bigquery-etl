e2b2514ea287f501432669bb1d5d1a0933d8d81d
Katie Windau <kwindau@mozilla.com>
2024-03-01 11:02:23 -0600

    DENG-2924 fix typo

--- sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
+++ sql/moz-fx-data-marketing-prod/ga_derived/www_site_downloads_v2/query.sql
@@ -35,63 +35,49 @@ WITH staging AS (
     ) AS download_events
   FROM
     (
-      (
-        SELECT
-          PARSE_DATE('%Y%m%d', event_date) AS `date`,
-          user_pseudo_id || '-' || CAST(e.value.int_value AS string) AS visit_identifier,
-          device.category AS device_category,
-          device.operating_system AS operating_system,
-          device.language AS `language`,
-          geo.country AS country,
-          collected_traffic_source.manual_source AS source,
-          collected_traffic_source.manual_medium AS medium,
-          collected_traffic_source.manual_campaign_name AS campaign,
-          collected_traffic_source.manual_content AS ad_content,
-          device.web_info.browser AS browser,
-          event_name,
-          (
-            SELECT
-              `value`
-            FROM
-              UNNEST(event_params)
-            WHERE
-              key = 'product'
-            LIMIT
-              1
-          ).string_value AS product_type,
-          (
-            SELECT
-              `value`
-            FROM
-              UNNEST(event_params)
-            WHERE
-              key = 'platform'
-            LIMIT
-              1
-          ).string_value AS platform_type
-        FROM
-          `moz-fx-data-marketing-prod.analytics_313696158.events_*`
-        JOIN
-          UNNEST(event_params) AS e
-        WHERE
-          _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d', @submission_date)
-          AND e.key = 'ga_session_id'
-          AND e.value.int_value IS NOT NULL
-      ) staging
-      GROUP BY
-        `date`,
-        visit_identifier,
-        device_category,
-        operating_system,
-        `language`,
-        country,
-        source,
-        medium,
-        campaign,
-        ad_content,
-        browser
-    )
-  SELECT
+      SELECT
+        PARSE_DATE('%Y%m%d', event_date) AS `date`,
+        user_pseudo_id || '-' || CAST(e.value.int_value AS string) AS visit_identifier,
+        device.category AS device_category,
+        device.operating_system AS operating_system,
+        device.language AS `language`,
+        geo.country AS country,
+        collected_traffic_source.manual_source AS source,
+        collected_traffic_source.manual_medium AS medium,
+        collected_traffic_source.manual_campaign_name AS campaign,
+        collected_traffic_source.manual_content AS ad_content,
+        device.web_info.browser AS browser,
+        event_name,
+        (
+          SELECT
+            `value`
+          FROM
+            UNNEST(event_params)
+          WHERE
+            key = 'product'
+          LIMIT
+            1
+        ).string_value AS product_type,
+        (
+          SELECT
+            `value`
+          FROM
+            UNNEST(event_params)
+          WHERE
+            key = 'platform'
+          LIMIT
+            1
+        ).string_value AS platform_type
+      FROM
+        `moz-fx-data-marketing-prod.analytics_313696158.events_*`
+      JOIN
+        UNNEST(event_params) AS e
+      WHERE
+        _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d', @submission_date)
+        AND e.key = 'ga_session_id'
+        AND e.value.int_value IS NOT NULL
+    ) staging
+  GROUP BY
     `date`,
     visit_identifier,
     device_category,
@@ -102,13 +88,26 @@ WITH staging AS (
     medium,
     campaign,
     ad_content,
-    browser,
-    download_events,
-    download_events AS downloads,
-    CASE
-      WHEN NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(browser)
-        THEN download_events
-      ELSE 0
-    END AS non_fx_downloads
-  FROM
-    staging
+    browser
+)
+SELECT
+  `date`,
+  visit_identifier,
+  device_category,
+  operating_system,
+  `language`,
+  country,
+  source,
+  medium,
+  campaign,
+  ad_content,
+  browser,
+  download_events,
+  download_events AS downloads,
+  CASE
+    WHEN NOT `moz-fx-data-shared-prod.udf.ga_is_mozilla_browser`(browser)
+      THEN download_events
+    ELSE 0
+  END AS non_fx_downloads
+FROM
+  staging

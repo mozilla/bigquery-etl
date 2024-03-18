e1b81d445dff85363be00a7dcc75b83471f0d6c3
lucia-vargas-a <alvargasa@gmail.com>
2024-03-18 19:25:23 +0100

    Add days_active_bits to baseline_clients_last_seen tables for Mobile apps.

--- sql_generators/glean_usage/templates/baseline_clients_last_seen_v1.query.sql
+++ sql_generators/glean_usage/templates/baseline_clients_last_seen_v1.query.sql
@@ -14,6 +14,7 @@ OPTIONS
 AS
 SELECT
   CAST(NULL AS INT64) AS days_seen_bits,
+  CAST(NULL AS INT64) AS days_active_bits,
   CAST(NULL AS INT64) AS days_created_profile_bits,
   -- We make sure to delay * until the end so that as new columns are added
   -- to the daily table we can add those columns in the same order to the end
@@ -32,8 +33,11 @@ WITH _current AS (
   SELECT
     -- In this raw table, we capture the history of activity over the past
     -- 28 days for each usage criterion as a single 64-bit integer. The
-    -- rightmost bit represents whether the user was active in the current day.
+    -- rightmost bit in 'days_since_seen' represents whether the user sent a
+    -- baseline ping in the submission_date and the similarly in days_active_bits
+    -- it represents whether the user was active on the same date.
     CAST(TRUE AS INT64) AS days_seen_bits,
+    CAST(TRUE AS INT64) & CAST(durations > 0  AS INT64) AS days_active_bits,
     udf.days_since_created_profile_as_28_bits(
       DATE_DIFF(submission_date, first_run_date, DAY)
     ) AS days_created_profile_bits,

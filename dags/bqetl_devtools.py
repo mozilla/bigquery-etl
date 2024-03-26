c4d9b73be594e91380eca25ad493cb989061d8ce
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-03-26 22:29:14 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_devtools.py
+++ dags/bqetl_devtools.py
@@ -63,6 +63,18 @@ with DAG(
         pool="DATA_ENG_EXTERNALTASKSENSOR",
     )
 
+    wait_for_telemetry_derived__clients_daily_joined__v1 = ExternalTaskSensor(
+        task_id="wait_for_telemetry_derived__clients_daily_joined__v1",
+        external_dag_id="bqetl_main_summary",
+        external_task_id="telemetry_derived__clients_daily_joined__v1",
+        execution_delta=datetime.timedelta(seconds=3600),
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
     telemetry_derived__devtools_accessiblility_panel_usage__v1 = bigquery_etl_query(
         task_id="telemetry_derived__devtools_accessiblility_panel_usage__v1",
         destination_table="devtools_accessiblility_panel_usage_v1",
@@ -93,4 +105,8 @@ with DAG(
 
     telemetry_derived__devtools_accessiblility_panel_usage__v1.set_upstream(
         wait_for_copy_deduplicate_main_ping
+    )
+
+    telemetry_derived__devtools_panel_usage__v1.set_upstream(
+        wait_for_telemetry_derived__clients_daily_joined__v1
     )

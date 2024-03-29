4cc93fc8929b6a220c75cb245d7364242875e445
Anna Scholtz <anna@scholtzan.net>
2024-03-29 11:27:01 -0700

    Remove init.sql files for fenix and use is_init() instead

--- sql/moz-fx-data-shared-prod/fenix_derived/meta_attribution_country_counts_v1/query.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/meta_attribution_country_counts_v1/query.sql
@@ -1,25 +1,54 @@
+{% if is_init() %}
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    CASE
+      WHEN (metrics.string.meta_attribution_app = '382348575493443')
+        THEN 'Firefox Focus for Android'
+      WHEN (metrics.string.meta_attribution_app = '979253712091854')
+        THEN 'Firefox Browser for Android'
+      WHEN (metrics.string.meta_attribution_app = '697946762208244')
+        THEN 'Firefox Nightly for Android'
+      ELSE NULL
+    END AS meta_attribution_app,
+    normalized_channel,
+    metadata.geo.country AS country,
+    COUNT(*) AS ping_count
+  FROM
+    fenix.first_session
+  WHERE
+    DATE(submission_timestamp) >= '2023-11-01'
+    AND client_info.client_id IS NOT NULL
+  GROUP BY
+    submission_date,
+    meta_attribution_app,
+    normalized_channel,
+    country;
+
+{% else %}
 -- Aggregate meta attribution counts by country and channel.
-SELECT
-  DATE(submission_timestamp) AS submission_date,
-  CASE
-    WHEN (metrics.string.meta_attribution_app = '382348575493443')
-      THEN 'Firefox Focus for Android'
-    WHEN (metrics.string.meta_attribution_app = '979253712091854')
-      THEN 'Firefox Browser for Android'
-    WHEN (metrics.string.meta_attribution_app = '697946762208244')
-      THEN 'Firefox Nightly for Android'
-    ELSE CAST(NULL AS STRING)
-  END AS meta_attribution_app,
-  normalized_channel,
-  metadata.geo.country AS country,
-  COUNT(*) AS ping_count
-FROM
-  fenix.first_session
-WHERE
-  DATE(submission_timestamp) = @submission_date
-  AND client_info.client_id IS NOT NULL
-GROUP BY
-  submission_date,
-  meta_attribution_app,
-  normalized_channel,
-  country;
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    CASE
+      WHEN (metrics.string.meta_attribution_app = '382348575493443')
+        THEN 'Firefox Focus for Android'
+      WHEN (metrics.string.meta_attribution_app = '979253712091854')
+        THEN 'Firefox Browser for Android'
+      WHEN (metrics.string.meta_attribution_app = '697946762208244')
+        THEN 'Firefox Nightly for Android'
+      ELSE CAST(NULL AS STRING)
+    END AS meta_attribution_app,
+    normalized_channel,
+    metadata.geo.country AS country,
+    COUNT(*) AS ping_count
+  FROM
+    fenix.first_session
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+  GROUP BY
+    submission_date,
+    meta_attribution_app,
+    normalized_channel,
+    country;
+
+{% endif %}

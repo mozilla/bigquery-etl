dbfeae5cedd31b64f783ca7474726314d087c0ad
Katie Windau <kwindau@mozilla.com>
2024-04-02 16:49:50 -0500

    DENG-3265 new OS cloudflare table

---
+++ sql/moz-fx-data-shared-prod/cloudflare_derived/os_usage_v1/query.sql
@@ -0,0 +1,9 @@
+-- Query for cloudflare_derived.os_usage_v1
+            -- For more information on writing queries see:
+            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+SELECT
+  *
+FROM
+  table
+WHERE
+  submission_date = @submission_date

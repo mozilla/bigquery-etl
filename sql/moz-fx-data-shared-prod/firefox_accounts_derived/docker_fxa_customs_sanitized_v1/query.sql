8dc0b1d7a6dcc77bab5d5c77ff0e591353246a52
Anna Scholtz <anna@scholtzan.net>
2024-04-04 10:34:05 -0700

    Simplify init statements

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/docker_fxa_customs_sanitized_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/docker_fxa_customs_sanitized_v1/query.sql
@@ -1,28 +1,18 @@
-{% if is_init() %}
-  SELECT
-    DATE(timestamp) AS date,
-    * REPLACE (
-      (
-        SELECT AS STRUCT
-          jsonPayload.* REPLACE (SHA256(jsonPayload.email) AS email, SHA256(jsonPayload.ip) AS ip)
-      ) AS jsonPayload
-    )
-  FROM
-    `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_customs`
-  WHERE
-    DATE(`timestamp`) = "2020-01-01"
-{% else %}
+
 -- Query for firefox_accounts_derived.docker_fxa_customs_sanitized_v1
-  SELECT
-    @submission_date AS date,
-    * REPLACE (
-      (
-        SELECT AS STRUCT
-          jsonPayload.* REPLACE (SHA256(jsonPayload.email) AS email, SHA256(jsonPayload.ip) AS ip)
-      ) AS jsonPayload
-    )
-  FROM
-    `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_customs`
-  WHERE
+SELECT
+  @submission_date AS date,
+  * REPLACE (
+    (
+      SELECT AS STRUCT
+        jsonPayload.* REPLACE (SHA256(jsonPayload.email) AS email, SHA256(jsonPayload.ip) AS ip)
+    ) AS jsonPayload
+  )
+FROM
+  `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_customs`
+WHERE
+  {% if is_init() %}
+    DATE(`timestamp`) = "2020-01-01"
+  {% else %}
     DATE(`timestamp`) = @submission_date
-{% endif %}
+  {% endif %}

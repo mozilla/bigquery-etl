7568d9ef6c33afeb585a3aa25b35a90cab24933f
Katie Windau <kwindau@mozilla.com>
2024-04-04 15:25:45 -0500

    DENG-3281 add test for new UDF partner_org_ga_metrics

--- sql/mozfun/norm/partner_org_ga_metrics/udf.sql
+++ sql/mozfun/norm/partner_org_ga_metrics/udf.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE FUNCTION norm.partner_org_ga_metrics()
 RETURNS STRING AS (
   (SELECT 'non-distribution' AS partner_org)
-)
+);
+
+SELECT
+  mozfun.assert.equals(mozfun.norm.partner_org_ga_metrics(), 'non-distribution');

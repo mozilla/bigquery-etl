1882b8607d0b9195940b77537cf7795606cde9d5
Katie Windau <kwindau@mozilla.com>
2024-04-04 15:23:48 -0500

    DENG-3281 add test for norm.distribution_model_ga_metrics UDF

--- sql/mozfun/norm/distribution_model_ga_metrics/udf.sql
+++ sql/mozfun/norm/distribution_model_ga_metrics/udf.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE FUNCTION norm.distribution_model_ga_metrics()
 RETURNS STRING AS (
   (SELECT 'non-distribution' AS distribution_model)
-)
+);
+
+SELECT
+  mozfun.assert.equals(mozfun.norm.distribution_model_ga_metrics(), 'non-distribution');

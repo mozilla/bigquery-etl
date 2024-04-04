6d66206f724eee6320f564afaa1b40779a9ae48c
Chelsey Beck <cbeck@mozilla.com>
2024-04-04 10:28:04 -0700

    adding sql for products subscriptions and waitlists

---
+++ sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
@@ -0,0 +1,13 @@
+SELECT
+  fxa_uid AS fxa_id_sha256,
+  ARRAY_AGG(STRUCT(plan_id,
+      product_id,
+      status,
+      plan_started_at,
+      plan_ended_at,
+      plan_interval,
+      plan_interval_count)) AS products
+FROM
+  `moz-fx-data-shared-prod.subscription_platform_derived.stripe_subscriptions_v1`
+GROUP BY
+  fxa_id_sha256;
---
+++ sql/moz-fx-data-shared-prod/braze_derived/waitlists_v1/query.sql
@@ -0,0 +1,14 @@
+SELECT
+  w.email_id AS external_id,
+  ARRAY_AGG(STRUCT(w.name AS waitlist_name,
+      JSON_EXTRACT_SCALAR(w.fields, '$.geo') AS waitlist_geo,
+      JSON_EXTRACT_SCALAR(w.fields, '$.platform') AS waitlist_platform,
+      w.source AS waitlist_source,
+      w.create_timestamp,
+      w.subscribed,
+      w.unsub_reason,
+      w.update_timestamp)) AS waitlists
+FROM
+  `moz-fx-data-shared-prod.ctms_braze.ctms_waitlists` AS w
+GROUP BY
+  w.email_id;

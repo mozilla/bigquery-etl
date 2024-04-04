20f588bcaefbd912447bd17e18bc558af4136768
Anna Scholtz <anna@scholtzan.net>
2024-04-04 10:34:05 -0700

    Simplify init statements

--- sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_delete_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_accounts_derived/fxa_delete_events_v1/query.sql
@@ -1,40 +1,25 @@
-{% if is_init() %}
-  WITH columns AS (
-    SELECT
-      CAST(NULL AS TIMESTAMP) AS submission_timestamp,
-      CAST(NULL AS STRING) AS user_id,
-      CAST(NULL AS STRING) AS hmac_user_id,
-  )
+WITH hmac_key AS (
   SELECT
-    *
+    AEAD.DECRYPT_BYTES(
+      (SELECT keyset FROM `moz-fx-dataops-secrets.airflow_query_keys.fxa_prod`),
+      ciphertext,
+      CAST(key_id AS BYTES)
+    ) AS value
   FROM
-    columns
+    `moz-fx-data-shared-prod.firefox_accounts_derived.encrypted_keys_v1`
   WHERE
-    FALSE
-{% else %}
-  WITH hmac_key AS (
-    SELECT
-      AEAD.DECRYPT_BYTES(
-        (SELECT keyset FROM `moz-fx-dataops-secrets.airflow_query_keys.fxa_prod`),
-        ciphertext,
-        CAST(key_id AS BYTES)
-      ) AS value
-    FROM
-      `moz-fx-data-shared-prod.firefox_accounts_derived.encrypted_keys_v1`
-    WHERE
-      key_id = 'fxa_hmac_prod'
-  )
-  SELECT
-    `timestamp` AS submission_timestamp,
-    TO_HEX(SHA256(jsonPayload.fields.uid)) AS user_id,
-    TO_HEX(
-      udf.hmac_sha256((SELECT * FROM hmac_key), CAST(jsonPayload.fields.uid AS BYTES))
-    ) AS hmac_user_id,
-  FROM
-    `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_auth`
-  WHERE
-    DATE(`timestamp`) = @submission_date
-    AND jsonPayload.type = 'activityEvent'
-    AND jsonPayload.fields.event = 'account.deleted'
-    AND jsonPayload.fields.uid IS NOT NULL
-{% endif %}
+    key_id = 'fxa_hmac_prod'
+)
+SELECT
+  `timestamp` AS submission_timestamp,
+  TO_HEX(SHA256(jsonPayload.fields.uid)) AS user_id,
+  TO_HEX(
+    udf.hmac_sha256((SELECT * FROM hmac_key), CAST(jsonPayload.fields.uid AS BYTES))
+  ) AS hmac_user_id,
+FROM
+  `moz-fx-fxa-prod-0712.fxa_prod_logs.docker_fxa_auth`
+WHERE
+  DATE(`timestamp`) = @submission_date
+  AND jsonPayload.type = 'activityEvent'
+  AND jsonPayload.fields.event = 'account.deleted'
+  AND jsonPayload.fields.uid IS NOT NULL

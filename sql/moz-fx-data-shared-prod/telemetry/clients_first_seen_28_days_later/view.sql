5090f6c9462d871fc03179a35d6ea18f934329af
Katie Windau <kwindau@mozilla.com>
2024-04-09 12:46:30 -0500

    DENG-3355 add distribution model to clients first seen 28 days later

--- sql/moz-fx-data-shared-prod/telemetry/clients_first_seen_28_days_later/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/clients_first_seen_28_days_later/view.sql
@@ -12,6 +12,7 @@ SELECT
     attribution_ua,
     startup_profile_selection_reason,
     distribution_id
-  ) AS funnel_derived
+  ) AS funnel_derived,
+  mozdata.udf.distribution_model_clients(distribution_id) AS distribution_model
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.clients_first_seen_28_days_later_v1` a

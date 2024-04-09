ffc6b8296e5222d4cee34d0802a82aaefe0d509a
Katie Windau <kwindau@mozilla.com>
2024-04-09 12:42:29 -0500

    DENG-3355 add funnel_derived_clients UDF to clients_first_seen_28_days_later view

--- sql/moz-fx-data-shared-prod/telemetry/clients_first_seen_28_days_later/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/clients_first_seen_28_days_later/view.sql
@@ -2,6 +2,16 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.clients_first_seen_28_days_later`
 AS
 SELECT
-  *
+  a.*,
+  mozdata.udf.funnel_derived_clients(
+    channel,
+    os,
+    first_seen_date,
+    build_id,
+    attribution_source,
+    attribution_ua,
+    startup_profile_selection_reason,
+    distribution_id
+  ) AS funnel_derived
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.clients_first_seen_28_days_later_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.clients_first_seen_28_days_later_v1` a

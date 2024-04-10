669b0284d353b0d282e26faf7be2c4af8397cd0e
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-04-10 23:19:50 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/public_data_report_hardware_v1/query.py
@@ -0,0 +1,4 @@
+"""
+This is a stub to allow bqetl to deploy the table schema.
+Code is in https://github.com/mozilla/firefox-public-data-report-etl/tree/main/public_data_report/hardware_report
+"""

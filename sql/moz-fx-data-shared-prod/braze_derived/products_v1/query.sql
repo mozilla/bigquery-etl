361ea826fbd4759060d1f80daa09f9abb043c640
Chelsey Beck <cbeck@mozilla.com>
2024-04-10 14:11:35 -0700

    adding products model

---
+++ sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
@@ -0,0 +1,22 @@
+SELECT
+  fxa_uid AS fxa_id_sha256,
+  ARRAY_AGG( STRUCT( plan_id,
+      product_id,
+      status,
+      plan_started_at,
+      plan_ended_at,
+      plan_interval,
+      plan_interval_count,
+      event_timestamp AS update_timestamp ) ) AS products,
+  @submission_date AS last_modified_timestamp,
+  DATE(@submission_date) AS last_modified_date
+FROM
+  `moz-fx-data-shared-prod.subscription_platform_derived.stripe_subscriptions_v1`
+WHERE
+  event_timestamp > (
+  SELECT
+    MAX(last_modified_timestamp)
+  FROM
+    `moz-fx-data-shared-prod.braze_external.products_v1` )
+GROUP BY
+  fxa_id_sha256;

21cdc3c6a258f498dd901d12a831a7170e1ca686
Chelsey Beck <cbeck@mozilla.com>
2024-04-10 21:25:47 -0700

    removing create statement and adding modified timestamp

--- sql/moz-fx-data-shared-prod/braze_derived/suppressions_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/suppressions_v1/query.sql
@@ -1,22 +1,29 @@
-CREATE OR REPLACE TABLE `moz-fx-data-shared-prod.braze_external.dev_suppressions` AS
+WITH
+  suppressions AS (
+  SELECT
+    LOWER(email) AS email,
+    external_id AS email_id
+  FROM
+    `moz-fx-data-shared-prod.acoustic_external.suppression_list_v1`
+  UNION DISTINCT
+  SELECT
+    LOWER(primary_email) AS email,
+    email_id
+  FROM
+    `moz-fx-data-shared-prod.ctms_braze.ctms_emails`
+  WHERE
+    has_opted_out_of_email = TRUE
+  UNION DISTINCT
+  SELECT
+    LOWER(email) AS email,
+    email_id
+  FROM
+    `moz-fx-data-shared-prod.acoustic_external.contact_raw_v1`
+  WHERE
+    has_opted_out_of_email = 1)
 SELECT
-  LOWER(email) AS email,
-  external_id AS email_id
+  *,
+  @submission_date AS last_modified_timestamp
 FROM
-  `moz-fx-data-shared-prod.acoustic_external.suppression_list_v1`
-UNION DISTINCT
-SELECT
-  LOWER(primary_email) AS email,
-  email_id
-FROM
-  `moz-fx-data-shared-prod.ctms_braze.ctms_emails`
-WHERE
-  has_opted_out_of_email = TRUE
-UNION DISTINCT
-SELECT
-  LOWER(email) AS email,
-  email_id
-FROM
-  `moz-fx-data-shared-prod.acoustic_external.contact_raw_v1`
-WHERE
-  has_opted_out_of_email = 1;
+  suppressions
+  

dd6ec671cbea621fc0b3706ccf613e804ec75e41
lucia-vargas-a <alvargasa@gmail.com>
2024-04-14 21:06:23 +0200

    Revert "View updates." This reverts commit c2ab9b0677eda934993673afea9a90f1d91cfa85.

--- sql/moz-fx-data-shared-prod/telemetry/active_users/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/active_users/view.sql
@@ -11,7 +11,7 @@ WITH fenix_distribution_id AS (
   FROM
     `moz-fx-data-shared-prod.fenix.firefox_android_clients`
 )
--- Firefox Desktop (Legacy)
+-- Firefox Desktop
 SELECT
   submission_date,
   client_id,
@@ -39,7 +39,7 @@ SELECT
   ) AS is_desktop,
   FALSE AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.telemetry_derived_clients_last_seen_v2_20240322_view`
+  `moz-fx-data-shared-prod.telemetry_derived.clients_last_seen_v2`
 UNION ALL
 -- Fenix
 SELECT
@@ -69,7 +69,7 @@ SELECT
     FALSE
   ) AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.fenix_baseline_clients_last_seen_20240325`
+  `moz-fx-data-shared-prod.fenix.baseline_clients_last_seen`
 LEFT JOIN
   fenix_distribution_id
   USING (client_id, sample_id)
@@ -95,7 +95,7 @@ SELECT
   FALSE AS is_desktop,
   IF(LOWER(IFNULL(isp, '')) <> "browserstack", TRUE, FALSE) AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.firefox_ios_baseline_clients_last_seen_20240325`
+  `moz-fx-data-shared-prod.firefox_ios.baseline_clients_last_seen`
 UNION ALL
 -- Klar Android
 SELECT
@@ -118,7 +118,7 @@ SELECT
   FALSE AS is_desktop,
   FALSE AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.klar_android_baseline_clients_last_seen_20240325`
+  `moz-fx-data-shared-prod.klar_android.baseline_clients_last_seen`
 UNION ALL
 -- Klar iOS
 SELECT
@@ -141,7 +141,7 @@ SELECT
   FALSE AS is_desktop,
   FALSE AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.klar_ios_baseline_clients_last_seen_20240325`
+  `moz-fx-data-shared-prod.klar_ios.baseline_clients_last_seen`
 UNION ALL
 -- Focus Android
 SELECT
@@ -157,14 +157,14 @@ SELECT
   days_active_bits,
   IFNULL(mozfun.bits28.days_since_seen(days_active_bits) = 0, FALSE) AS is_dau,
   IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 7, FALSE) AS is_wau,
-  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) <28, FALSE) AS is_mau,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 28, FALSE) AS is_mau,
   IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) = 0, FALSE) AS is_daily_user,
   IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 7, FALSE) AS is_weekly_user,
   IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) < 28, FALSE) AS is_monthly_user,
   FALSE AS is_desktop,
   IF(LOWER(IFNULL(isp, '')) <> "browserstack", TRUE, FALSE) AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.focus_android_baseline_clients_last_seen_20240325`
+  `moz-fx-data-shared-prod.focus_android.baseline_clients_last_seen`
 UNION ALL
 -- Focus iOS
 SELECT
@@ -178,13 +178,13 @@ SELECT
   END AS app_name,
   days_seen_bits,
   days_active_bits,
-  CAST(mozfun.bits28.days_since_seen(days_active_bits) = 0 AS BOOLEAN) AS is_dau,
-  CAST(mozfun.bits28.days_since_seen(days_active_bits) < 7 AS BOOLEAN) AS is_wau,
-  CAST(mozfun.bits28.days_since_seen(days_active_bits) < 28 AS BOOLEAN) AS is_mau,
-  CAST(mozfun.bits28.days_since_seen(days_seen_bits) = 0 AS BOOLEAN) AS is_daily_user,
-  CAST(mozfun.bits28.days_since_seen(days_seen_bits) < 7 AS BOOLEAN) AS is_weekly_user,
-  CAST(mozfun.bits28.days_since_seen(days_seen_bits) < 28 AS BOOLEAN) AS is_monthly_user,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) = 0, FALSE) AS is_dau,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 7, FALSE) AS is_wau,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 28, FALSE) AS is_mau,
+  IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) = 0, FALSE) AS is_daily_user,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 7, FALSE) AS is_weekly_user,
+  IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) < 28, FALSE) AS is_monthly_user,
   FALSE AS is_desktop,
-  IF(LOWER(isp) <> "browserstack", TRUE, FALSE) AS is_mobile
+  IF(LOWER(IFNULL(isp, '')) <> "browserstack", TRUE, FALSE) AS is_mobile
 FROM
-  `moz-fx-data-shared-prod.backfills_staging_derived.focus_ios_baseline_clients_last_seen_20240325`
+  `moz-fx-data-shared-prod.focus_ios.baseline_clients_last_seen`

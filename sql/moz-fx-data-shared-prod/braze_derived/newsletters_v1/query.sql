a6b9b1b95cc6ac36a26bd13df09a5cfd957079b6
Chelsey Beck <64881557+chelseybeck@users.noreply.github.com>
2024-04-15 19:27:31 -0700

    Create Braze derived models (#5362)
    * DENG-3008 removing misspelled file
    
    * DENG-3008 adding suppressions model
    
    * adding products model
    
    * updating dataset
    
    * adding newsletter model
    
    * adding waitlist model
    
    * removing create statement and adding modified timestamp
    
    * updating name and description for newsletters
    
    * updating description
    
    * adding users model
    
    * adding braze dag
    
    * changing subscribed to boolean
    
    * updating type to date
    
    * updating to full refresh
    
    * updating email id to external id for standardization
    
    * updating format
    
    * updating nullable fields
    
    * updating format
    
    * updating nullable fields
    
    * adding subscription map model
    
    * adding subscriptions model
    
    * adding user profiles model
    
    * updating format
    
    * formatting
    
    * removing last modified date in favor of only timestamp
    
    * updating table name

---
+++ sql/moz-fx-data-shared-prod/braze_derived/newsletters_v1/query.sql
@@ -0,0 +1,16 @@
+SELECT
+  email_id AS external_id,
+  ARRAY_AGG(
+    STRUCT(
+      name AS newsletter_name,
+      subscribed,
+      lang AS newsletter_lang,
+      create_timestamp,
+      update_timestamp
+    )
+  ) AS newsletters,
+  @submission_date AS last_modified_timestamp,
+FROM
+  `moz-fx-data-shared-prod.ctms_braze.ctms_newsletters`
+GROUP BY
+  email_id;

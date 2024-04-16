4b3c7e74ee6f686fe8de0cae9f01601d43e8e938
Sean Rose <srose@mozilla.com>
2024-04-15 18:26:27 -0700

    Use new brand keyword regular expressions from Adria Kyne in `classify_query` UDF.

--- sql/mozfun/google_search_console/classify_query/udf.sql
+++ sql/mozfun/google_search_console/classify_query/udf.sql
@@ -3,105 +3,47 @@ RETURNS STRING AS (
   CASE
     WHEN query IS NULL
       THEN 'Anonymized'
-    -- Brand keywords source: https://docs.google.com/document/d/1cjUQrPxvUG_A-hFTNUg-3MvRvssieVhk8tkJ8HBrtw4/edit
     WHEN REGEXP_CONTAINS(
         query,
         ARRAY_TO_STRING(
           [
-            r'f5ref6',
-            r'fair box',
-            r'fairbox',
-            r'faiya-fokkusu',  -- fire fox (Japanese)
-            r'feier',
-            r'ff browser',
-            r'ff download',
-            r'ff',
+            r'f.ref.x',
             r'fier',
-            r'fiferox',
-            r'fir',
-            r'ｆｉｒｅｆｏｘ',
-            r'forefpx',
+            r'fire',
+            r'firf',
+            r'focus',
+            r'fokku',
             r'fox',
-            r'frefx',
-            r'frox',
-            r'furefix',
-            r'huohu',  -- firefox (Chinese)
-            r'illa',
-            r'ireox',
-            r'm o z i l l a \. o r g',
-            r'marzil',
-            r'mazola',
-            r'mizolla',
-            r'modgila',
-            r'modjila',
-            r'modsila',
-            r'modzil',
-            r'mogil',
-            r'moill',
-            r'moizlla',
-            r'mojala',
-            r'mojila',
-            r'mojira',
-            r'mojula',
-            r'moliza',
-            r'molliza',
-            r'monjila',
-            r'morgila',
-            r'morjila',
-            r'morzil',
-            r'mosil',
-            r'moxil',
-            r'moyila',
-            r'moyyila',
-            r'moz',
+            r'il*a$',
+            r'm.z*',
             r'nightly',
-            r'pocket',
             r'quantum',
-            r'rise 25',
-            r'thunderbird',
-            r'vox',
-            r'zila',
-            r'μοζζιλα',  -- mozilla (Greek)
-            r'μοζζιλλα',  -- mozilla (Greek)
-            r'μοζιλα',  -- mozilla (Greek)
-            r'μοζιλλα',  -- mozilla (Greek)
-            r'μονζιλα',  -- monzilla (Greek)
-            r'ашкуащч',  -- ashkuashtch (Cyrillic)
+            r'μοζ*ιλ*α',  -- moz*il*a (Greek)
+            r'μοτζιλα',  -- motzila (Greek)
             r'лиса',  -- fox (Cyrillic)
-            r'мазила',  -- mazila (Cyrillic)
-            r'мазилла',  -- mazilla (Cyrillic)
-            r'мазилу',  -- mazilu (Cyrillic)
-            r'мазіла',  -- mazila (Cyrillic)
-            r'мазілу',  -- mazilu (Cyrillic)
-            r'моззила',  -- mozilla (Cyrillic)
-            r'мозила',  -- mozila (Cyrillic)
-            r'мозилла',  -- mozilla (Cyrillic)
-            r'мозилу',  -- mozilu (Cyrillic)
-            r'мозилы',  -- mozily (Cyrillic)
-            r'мозіла',  -- mozilla (Cyrillic)
-            r'мозілла',  -- mozilla (Cyrillic)
-            r'мозілу',  -- mozilu (Cyrillic)
-            r'музила',  -- muzila (Cyrillic)
+            r'мази',  -- mazi (Cyrillic)
+            r'мазі',  -- mazi (Cyrillic)
+            r'моз',  -- moz (Cyrillic)
+            r'муз',  -- muz (Cyrillic)
+            r'фаер',  -- fire (Cyrillic)
+            r'фаир',  -- fair (Cyrillic)
+            r'файер',  -- fire (Cyrillic)
+            r'файр',  -- fire (Cyrillic)
+            r'фире',  -- fire (Cyrillic)
             r'фокс',  -- fox (Cyrillic)
-            r'фох',  -- foh (Cyrillic)
-            r'фоь',  -- fo (Cyrillic)
-            r'ьфяшддф',  -- fyashddf (Cyrillic)
-            r'ьфяшдф',  -- fyashdf (Cyrillic)
-            r'ьщяшддф',  -- shsyashddf (Cyrillic)
-            r'ьщяшддфющкп',  -- shtyashddfyshkp (Cyrillic)
-            r'ьщяшдф',  -- yshchyashdf (Cyrillic)
+            r'фох',  -- fox (Cyrillic)
             r'כןרקכםס',  -- yes, rakhems (Hebrew)
             r'מוזילה',  -- mozilla (Hebrew)
             r'פיירפוקס',  -- firefox (Hebrew)
-            r'فاكس',  -- fax (Arabic)
-            r'فاير',  -- fire (Arabic)
-            r'فایرفاکس',  -- firefox (Arabic)
+            r'فاکس',  -- fax (Arabic)
+            r'فایر',  -- fire (Arabic)
             r'فكس',  -- fx (Arabic)
             r'فوكس',  -- fox (Arabic)
             r'فير',  -- fir (Arabic)
+            r'لفاير',  -- lafayette (Arabic)
             r'موزلا',  -- mozilla (Arabic)
-            r'موزيل',  -- moselle (Arabic)
-            r'موزیلا',  -- mozilla (Arabic)
+            r'موزيلا',  -- mozilla (Arabic)
+            r'يروكس',  -- yerox (Arabic)
             r'फायरफक्स',  -- firefox (Indic)
             r'फायरफॉक्स',  -- firefox (Indic)
             r'फ़ायरफ़ॉक्स',  -- firefox (Indic)
@@ -110,9 +52,8 @@ RETURNS STRING AS (
             r'ফায়ারফক্স',  -- firefox (Indic)
             r'মজিলা',  -- mozilla (Indic)
             r'মোজিলা',  -- mozilla (Indic)
-            r'ดฟายฟอก',  -- firefoak (Thai)
+            r'ดาวโหลดไฟฟ๊อก',  -- download firefox (Thai)
             r'ฟายฟอก',  -- bleach (Thai)
-            r'ฟายฟ๊อก',  -- firefox (Thai)
             r'ไฟ ฟอก',  -- fire bleach (Thai)
             r'ไฟฟ็อก',  -- fire fox (Thai)
             r'ไฟฟ๊อก',  -- fire fox (Thai)
@@ -120,25 +61,20 @@ RETURNS STRING AS (
             r'ไฟร์ฟอกซ์',  -- firefox (Thai)
             r'ไฟล์ฟอก',  -- bleaching file (Thai)
             r'ไฟลฟอก',  -- fire bleach (Thai)
-            r'หมาไฟ',  -- fire dog (Thai)
-            r'인터넷브라우저',  -- internet browser (Korean)
+            r'โหลดไฟฟอก',  -- load purifying light (Thai)
+            r'모질라',  -- mozilla (Korean)
             r'파이어',  -- fire (Korean)
-            r'파폭',  -- explosion (Korean)
             r'폭스',  -- fox (Korean)
-            r'ファイアーフォックス',  -- firefox (Japanese)
-            r'ふぁいあーふぉっくす',  -- fireworks (Japanese)
-            r'ファイアフォックス',  -- firefox (Japanese)
-            r'ふぁいあふぉっくす',  -- fireworks (Japanese)
-            r'ファイヤー フォックス',  -- fire fox (Japanese)
-            r'ふぁいやーふぉっくす',  -- faiya fox (Japanese)
-            r'ファイヤーフォックス',  -- firefox (Japanese)
-            r'ファイヤーホックス',  -- fire hox (Japanese)
-            r'ファイヤフォックス',  -- firefox (Japanese)
-            r'ファイやフォックス',  -- phi and fox (Japanese)
-            r'ふぃれふぉx',  -- firefox (Japanese)
-            r'ふぃれふぉｘ',  -- firefox (Japanese)
+            r'화이어',  -- fire (Korean)
+            r'ふぁいあ',  -- faia (Japanese)
+            r'ファイア',  -- fire (Japanese)
+            r'ファイや',  -- faiya (Japanese)
+            r'ファイヤ',  -- fire (Japanese)
+            r'ふぁいやー',  -- fire (Japanese)
+            r'ふぃれふぉ',  -- firefox (Japanese)
+            r'ふぉっくす',  -- fox (Japanese)
+            r'フォックス',  -- fox (Japanese)
             r'モジラ',  -- mozilla (Japanese)
-            r'火孤',  -- firefox (Chinese)
             r'火狐',  -- firefox (Chinese)
             r'狐狸'  -- fox (Chinese)
           ],

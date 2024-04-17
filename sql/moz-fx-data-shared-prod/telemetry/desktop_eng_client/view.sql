4f5127974fa345e784752fc1118d0597a9447d99
Katie Windau <kwindau@mozilla.com>
2024-04-17 12:03:27 -0500

    DENG-3186

--- sql/moz-fx-data-shared-prod/telemetry/desktop_eng_client/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/desktop_eng_client/view.sql
@@ -3,12 +3,16 @@ CREATE OR REPLACE VIEW
 AS
 SELECT
   a.*,
-  CASE 
-  WHEN first_seen_date = submission_date THEN 'new_profile'
-  WHEN date_diff(submission_date,first_seen_date, DAY) BETWEEN 1 AND 27 THEN 'repeat_user'
-  WHEN date_diff(submission_date,first_seen_date, DAY) >=28  THEN 'existing_user'
-  ELSE 'Unknown'
-  END 
-  AS lifecycle_stage
+  CASE
+    WHEN first_seen_date = submission_date
+      THEN 'new_profile'
+    WHEN DATE_DIFF(submission_date, first_seen_date, DAY)
+      BETWEEN 1
+      AND 27
+      THEN 'repeat_user'
+    WHEN DATE_DIFF(submission_date, first_seen_date, DAY) >= 28
+      THEN 'existing_user'
+    ELSE 'Unknown'
+  END AS lifecycle_stage
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.desktop_eng_client_v1` a

5044aae6c3637b8965c81bab1ab0c4c062903f59
Katie Windau <kwindau@mozilla.com>
2024-04-17 13:02:43 -0500

    DENG-3186 desktop engagement model

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_eng_agg_v1/query.sql
@@ -0,0 +1,31 @@
+SELECT
+  submission_date,
+  first_seen_date,
+  attribution_campaign,
+  attribution_content,
+  attribution_dlsource,
+  attribution_medium,
+  attribution_ua,
+  normalized_os,
+  normalized_os_version,
+  country,
+  lifecycle_stage,
+  SUM(CASE WHEN dau THEN 1 ELSE 0 END) AS dau,
+  SUM(CASE WHEN wau THEN 1 ELSE 0 END) AS wau,
+  SUM(CASE WHEN mau THEN 1 ELSE 0 END) AS mau
+FROM
+  `moz-fx-data-shared-prod.telemetry.desktop_eng_client`
+WHERE
+  submission_date = @submission_date
+GROUP BY
+  submission_date,
+  first_seen_date,
+  attribution_campaign,
+  attribution_content,
+  attribution_dlsource,
+  attribution_medium,
+  attribution_ua,
+  normalized_os,
+  normalized_os_version,
+  country,
+  lifecycle_stage
---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_eng_client_v1/query.sql
@@ -0,0 +1,27 @@
+SELECT
+  au.submission_date,
+  au.client_id,
+  cfs.first_seen_date,
+  cfs.attribution_campaign,
+  cfs.attribution_content,
+  cfs.attribution_dlsource,
+  cfs.attribution_medium,
+  cfs.attribution_ua,
+  c.normalized_os,
+  c.normalized_os_version,
+  c.country,
+  au.is_dau AS dau,
+  au.is_wau AS wau,
+  au.is_mau AS mau
+FROM
+  `moz-fx-data-shared-prod.telemetry.active_users` au
+LEFT JOIN
+  mozdata.telemetry.clients_first_seen cfs
+  ON au.client_id = cfs.client_id
+LEFT JOIN
+  mozdata.telemetry.clients_daily_v6 cd
+  ON au.client_id = cd.client_id
+  AND au.submission_date = cd.submission_date
+WHERE
+  au.app_name = 'Firefox'
+  AND au.submission_date = @submission_date

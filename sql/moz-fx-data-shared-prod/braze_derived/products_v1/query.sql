d586c3f2182240fc1265c5f3fbac410284de0939
Chelsey Beck <cbeck@mozilla.com>
2024-04-21 11:38:15 -0700

    joining on users to filter for active

--- sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
@@ -19,6 +19,12 @@ SELECT
       product_id
   ) AS products
 FROM
-  `moz-fx-data-shared-prod.subscription_platform_derived.stripe_subscriptions_v1`
+  `moz-fx-data-shared-prod.subscription_platform_derived.stripe_subscriptions_v1` AS subscriptions
+LEFT JOIN
+  `moz-fx-data-shared-prod.ctms_braze.ctms_fxa` AS fxa
+  ON subscriptions.fxa_uid = TO_HEX(SHA256(fxa.fxa_id))
+INNER JOIN
+  `moz-fx-data-shared-prod.braze_derived.users_v1` AS users
+  ON users.external_id = fxa.email_id
 GROUP BY
   fxa_id_sha256;

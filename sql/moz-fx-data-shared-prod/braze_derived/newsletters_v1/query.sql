e50cadad7f32dd7434e5232ba5803579895c77ea
Chelsey Beck <cbeck@mozilla.com>
2024-04-21 11:37:01 -0700

    joining on users table to filter for active and ensuring there is at least one subscription

--- sql/moz-fx-data-shared-prod/braze_derived/newsletters_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/newsletters_v1/query.sql
@@ -2,18 +2,22 @@ SELECT
   email_id AS external_id,
   ARRAY_AGG(
     STRUCT(
-      name AS newsletter_name,
-      subscribed,
-      lang AS newsletter_lang,
-      create_timestamp,
-      update_timestamp
+      newsletters.name AS newsletter_name,
+      newsletters.subscribed AS subscribed,
+      newsletters.lang AS newsletter_lang,
+      newsletters.create_timestamp AS create_timestamp,
+      newsletters.update_timestamp AS update_timestamp
     )
     ORDER BY
-      update_timestamp,
-      create_timestamp,
-      name
+      newsletters.update_timestamp,
+      newsletters.name
   ) AS newsletters
 FROM
-  `moz-fx-data-shared-prod.ctms_braze.ctms_newsletters`
+  `moz-fx-data-shared-prod.ctms_braze.ctms_newsletters` AS newsletters
+INNER JOIN
+  `moz-fx-data-shared-prod.braze_derived.users_v1` AS users
+  ON users.external_id = newsletters.email_id
 GROUP BY
-  email_id;
+  email_id
+HAVING
+  MAX(subscribed) = TRUE;

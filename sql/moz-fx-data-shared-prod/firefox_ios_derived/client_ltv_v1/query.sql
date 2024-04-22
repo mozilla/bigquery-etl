9f6569810f05c3c51a926e971f88c5886bed01b4
Katie Windau <kwindau@mozilla.com>
2024-04-22 16:52:10 -0500

    GROWTH-147 work in progress

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/client_ltv_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/client_ltv_v1/query.sql
@@ -1,9 +1,37 @@
--- Query for firefox_ios_derived.client_ltv_v1
-            -- For more information on writing queries see:
-            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+WITH new_data AS (
+  SELECT
+    client_id,
+    sample_id,
+    MAX(submission_date) AS as_of_date,
+    MAX_BY(ltv_states_v1, submission_date).* EXCEPT (client_id, sample_id, submission_date),
+  FROM
+    `moz-fx-data-shared-prod.firefox_ios_derived.ltv_states_v1` ltv_states_v1
+  WHERE
+      submission_date = @submission_date
+  GROUP BY
+    client_id,
+    sample_id
+),
+historic_data AS (
+  SELECT
+    *
+  FROM
+    firefox_ios_derived.client_ltv_v1
+)
 SELECT
-  *
+  (
+    CASE
+      WHEN new_data.as_of_date IS NULL
+        THEN historic_data
+      WHEN historic_data.as_of_date IS NULL
+        THEN new_data
+      WHEN new_data.as_of_date > historic_data.as_of_date
+        THEN new_data
+      ELSE historic_data
+    END
+  ).*
 FROM
-  table
-WHERE
-  submission_date = @submission_date
+  historic_data
+FULL OUTER JOIN
+  new_data
+  USING (sample_id, client_id)

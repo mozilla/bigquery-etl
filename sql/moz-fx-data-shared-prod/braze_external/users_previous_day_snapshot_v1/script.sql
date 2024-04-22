f74c9393ff5a3ccb7625e77eb233ee61571eee6b
Chelsey Beck <cbeck@mozilla.com>
2024-04-22 06:35:08 -0700

    adding snapshots to drop so that they can be deleted

--- sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
+++ sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
@@ -1,4 +1,7 @@
-DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_snapshot_v1`;
+--DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_snapshot_v1`;
+DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.active_subscriptions_v1`;
+DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.user_profiles_previous_day_snapshot_v1`;
+DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.test_snapshot`;
 
 CREATE SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_snapshot_v1` CLONE `moz-fx-data-shared-prod.braze_derived.users_v1`
 FOR SYSTEM_TIME AS OF

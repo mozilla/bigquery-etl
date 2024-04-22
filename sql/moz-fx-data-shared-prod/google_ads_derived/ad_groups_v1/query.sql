26b31d582e467fbbda6281011de0bcf251e02c0b
Frank Bertsch <frank.bertsch@gmail.com>
2024-04-22 18:24:36 -0400

    Reformat again

--- sql/moz-fx-data-shared-prod/google_ads_derived/ad_groups_v1/query.sql
+++ sql/moz-fx-data-shared-prod/google_ads_derived/ad_groups_v1/query.sql
@@ -3,7 +3,13 @@ WITH ad_group_names AS (
     campaign_id,
     id AS ad_group_id,
     name AS ad_group_name,
-    ROW_NUMBER() OVER (PARTITION BY id, campaign_id ORDER BY updated_at DESC) = 1 AS is_most_recent_record,
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        id,
+        campaign_id
+      ORDER BY
+        updated_at DESC
+    ) = 1 AS is_most_recent_record,
   FROM
     `moz-fx-data-bq-fivetran`.ads_google_mmc.ad_group_history
   QUALIFY

5fb46cd9936751f9feedefec79cf0e685a699dbb
Leli Schiestl <lschiestl@mozilla.com>
2024-04-22 22:49:39 +0200

    status quo

--- sql/moz-fx-data-shared-prod/marketing_suppression_list/campaign_monitor_suppression_list/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.marketing_suppression_list.campaign_monitor_suppression_list`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.marketing_suppression_list_derived.campaign_monitor_suppression_list_v1`

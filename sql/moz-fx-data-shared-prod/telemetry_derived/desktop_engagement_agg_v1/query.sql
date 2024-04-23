c36a02a6981e55836edc8dfdfa1b102d4808a6c8
Katie Windau <kwindau@mozilla.com>
2024-04-23 16:59:57 -0500

    DENG-3186 new desktop engagement model

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_agg_v1/query.sql
@@ -0,0 +1,33 @@
+SELECT
+  submission_date,
+  first_seen_date,
+  attribution_campaign,
+  attribution_content,
+  attribution_dlsource,
+  attribution_medium,
+  attribution_ua,
+  normalized_channel,
+  normalized_os,
+  normalized_os_version,
+  country,
+  lifecycle_stage,
+  SUM(CASE WHEN dau THEN 1 ELSE 0 END) AS dau,
+  SUM(CASE WHEN wau THEN 1 ELSE 0 END) AS wau,
+  SUM(CASE WHEN mau THEN 1 ELSE 0 END) AS mau
+FROM
+  `moz-fx-data-shared-prod.telemetry.desktop_engagement_client`
+WHERE
+  submission_date = @submission_date
+GROUP BY
+  submission_date,
+  first_seen_date,
+  attribution_campaign,
+  attribution_content,
+  attribution_dlsource,
+  attribution_medium,
+  attribution_ua,
+  normalized_channel,
+  normalized_os,
+  normalized_os_version,
+  country,
+  lifecycle_stage
---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_client_v1/query.sql
@@ -0,0 +1,32 @@
+SELECT
+  cls.submission_date,
+  cls.client_id,
+  cls.sample_id,
+  cfs.first_seen_date,
+  cfs.attribution_campaign,
+  cfs.attribution_content,
+  cfs.attribution_dlsource,
+  cfs.attribution_medium,
+  cfs.attribution_ua,
+  cd.normalized_channel,
+  mozfun.norm.os(cd.os) AS normalized_os,
+  COALESCE(
+    mozfun.norm.windows_version_info(cd.os, cd.os_version, cd.windows_build_number),
+    NULLIF(SPLIT(cd.normalized_os_version, ".")[SAFE_OFFSET(0)], "")
+  ) AS normalized_os_version,
+  cd.country,
+  cls.is_dau AS dau,
+  cls.is_wau AS wau,
+  cls.is_mau AS mau
+FROM
+  `moz-fx-data-shared-prod.telemetry.clients_last_seen_v2` cls
+LEFT JOIN
+  `mozdata.telemetry.clients_first_seen` cfs
+  ON cls.client_id = cfs.client_id
+LEFT JOIN
+  `mozdata.telemetry.clients_daily_v6` cd
+  ON cls.client_id = cd.client_id
+  AND cls.submission_date = cd.submission_date
+  AND cd.submission_date = @submission_date
+WHERE
+  cls.submission_date = @submission_date

d8edb09f322e16a6d76e24b3e3e413a422f5c1b2
Chelsey Beck <64881557+chelseybeck@users.noreply.github.com>
2024-04-23 12:41:44 -0700

    DENG-3008 Updating Braze Models (#5405)
    * updating create statement
    
    * joining on users table to filter for active and ensuring there is at least one subscription
    
    * joining on users to filter for active
    
    * adding dev subscription group
    
    * removing fxa_id in favor of has_fxa
    
    * bringing in update timestamp for downstream use
    
    * updating formatting and adding filter for active users
    
    * adding filter for one active newsletter
    
    * updating tests
    
    * adding fxa id back to users table to join to products
    
    * updating query
    
    * updating values
    
    * updating tests
    
    * fix test for subscriptions
    
    * changing schema to array
    
    * updating format
    
    * updating to pull in all subscriptions with statuses
    
    * removing create statement
    
    * updating subscriptions query to make it an array and updating associated tests
    
    * updating formatting and comment
    
    ---------
    
    Co-authored-by: Leli Schiestl <lschiestl@mozilla.com>

--- sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
@@ -1,24 +1,30 @@
 SELECT
-  fxa_uid AS fxa_id_sha256,
+  subscriptions.fxa_uid AS fxa_id_sha256,
   ARRAY_AGG(
     STRUCT(
-      plan_id,
-      product_id,
-      status,
-      plan_started_at,
-      plan_ended_at,
-      plan_interval,
-      plan_interval_count,
-      event_timestamp AS update_timestamp
+      subscriptions.plan_id,
+      subscriptions.product_id,
+      subscriptions.status,
+      subscriptions.plan_started_at,
+      subscriptions.plan_ended_at,
+      subscriptions.plan_interval,
+      subscriptions.plan_interval_count,
+      subscriptions.event_timestamp AS update_timestamp
     )
     ORDER BY
-      event_timestamp,
-      plan_started_at,
-      plan_ended_at,
-      plan_id,
-      product_id
+      subscriptions.event_timestamp,
+      subscriptions.plan_started_at,
+      subscriptions.plan_ended_at,
+      subscriptions.plan_id,
+      subscriptions.product_id
   ) AS products
 FROM
-  `moz-fx-data-shared-prod.subscription_platform_derived.stripe_subscriptions_v1`
+  `moz-fx-data-shared-prod.subscription_platform_derived.stripe_subscriptions_v1` AS subscriptions
+LEFT JOIN
+  `moz-fx-data-shared-prod.ctms_braze.ctms_fxa` AS fxa
+  ON subscriptions.fxa_uid = TO_HEX(SHA256(fxa.fxa_id))
+INNER JOIN
+  `moz-fx-data-shared-prod.braze_derived.users_v1` AS users
+  ON users.external_id = fxa.email_id
 GROUP BY
   fxa_id_sha256;

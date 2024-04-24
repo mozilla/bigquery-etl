f81f78780c0fbb41e214adb7c4fdd12494ba60e3
Leli Schiestl <lschiestl@mozilla.com>
2024-04-24 20:21:35 +0200

    DENG-3410 Main Suppression List

--- sql/moz-fx-data-shared-prod/marketing_suppression_list_derived/main_suppression_list_v1/query.sql
+++ sql/moz-fx-data-shared-prod/marketing_suppression_list_derived/main_suppression_list_v1/query.sql
@@ -1,28 +1,37 @@
-with
--- CTMS `has_opted_out_of_email` emails
-ctms_suppressions as (
-  select 1
-),
-
--- Acoustic Suppression List
-acoustic_suppressions AS (
-  select 1
-),
-
--- braze unsubscribes
-braze_unsubscribes AS (
-  select 1
-),
-
--- braze hard bounces
-braze_hard_bounces AS (
-  select 1
-),
-
-suppression_list AS (
+WITH suppressions AS (
+  -- CTMS `has_opted_out_of_email` emails
   SELECT
-    email,
-    external_id,
-    source,
-    reason
+    LOWER(primary_email) AS email,
+  FROM
+    `moz-fx-data-shared-prod.ctms_braze.ctms_emails`
+  WHERE
+    has_opted_out_of_email = TRUE
+  -- Acoustic Suppression List
+  UNION DISTINCT
+  SELECT
+    LOWER(email) AS email
+  FROM
+    `moz-fx-data-shared-prod.acoustic_external.suppression_list_v1`
+  -- braze unsubscribes
+  UNION DISTINCT
+  SELECT
+    email_address AS email
+  FROM
+    `moz-fx-data-shared-prod.braze_external.unsubscribes_v1`
+  -- braze hard bounces
+  UNION DISTINCT
+  SELECT
+    email_address AS email
+  FROM
+    `moz-fx-data-shared-prod.braze_external.hard_bounces_v1`
+-- MoFo suppression List
+  UNION DISTINCT
+  SELECT
+    email
+  FROM
+    `moz-fx-data-shared-prod.marketing_suppression_list_external.campaign_monitor_suppression_list_v1`
 )
+SELECT
+  *
+FROM
+  suppressions

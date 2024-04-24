bf44863a58f5474b6ea4c465ec5d59238febeed3
Katie Windau <kwindau@mozilla.com>
2024-04-24 11:20:30 -0500

    DENG-3186 switch to get country, os, os version, and build # from clients last seen and remove join to clients daily

--- sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_client_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_client_v1/query.sql
@@ -11,13 +11,13 @@ SELECT
   cfs.attribution_dlsource,
   cfs.attribution_medium,
   cfs.attribution_ua,
-  cd.normalized_channel,
-  mozfun.norm.os(cd.os) AS normalized_os,
+  cls.normalized_channel,
+  mozfun.norm.os(cls.os) AS normalized_os,
   COALESCE(
-    mozfun.norm.windows_version_info(cd.os, cd.os_version, cd.windows_build_number),
-    NULLIF(SPLIT(cd.normalized_os_version, ".")[SAFE_OFFSET(0)], "")
+    mozfun.norm.windows_version_info(cls.os, cls.os_version, cls.windows_build_number),
+    NULLIF(SPLIT(cls.normalized_os_version, ".")[SAFE_OFFSET(0)], "")
   ) AS normalized_os_version,
-  cd.country,
+  cls.country,
   cls.is_dau AS dau,
   cls.is_wau AS wau,
   cls.is_mau AS mau
@@ -26,10 +26,5 @@ FROM
 LEFT JOIN
   `mozdata.telemetry.clients_first_seen` cfs
   ON cls.client_id = cfs.client_id
-LEFT JOIN
-  `mozdata.telemetry.clients_daily_v6` cd
-  ON cls.client_id = cd.client_id
-  AND cls.submission_date = cd.submission_date
-  AND cd.submission_date = @submission_date
 WHERE
   cls.submission_date = @submission_date

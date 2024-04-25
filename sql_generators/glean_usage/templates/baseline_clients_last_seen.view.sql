95775509e27f78cad66266be14fe66db428a92da
kik-kik <kignasiak@mozilla.com>
2024-04-25 19:47:17 +0200

    feat: updated baseline_clients_last_seen view template

--- sql_generators/glean_usage/templates/baseline_clients_last_seen.view.sql
+++ sql_generators/glean_usage/templates/baseline_clients_last_seen.view.sql
@@ -17,24 +17,33 @@ SELECT
   mozfun.bits28.days_since_seen(days_seen_bits) < 28 AS is_monthly_user,
   CASE
     WHEN LOWER(isp) = 'browserstack'
-      THEN CONCAT(app_name, ' ', isp_name)
-    {% if app_name = "fenix" %}
+      THEN CONCAT({{ app_name }}, ' ', isp_name)
+    {% if app_name in ["fenix", "firefox_desktop"] %}
     WHEN LOWER(distribution_id) = 'mozillaonline'
-      THEN CONCAT(app_name, ' ', distribution_id)
+      THEN CONCAT({{ app_name }}, ' ', distribution_id)
     {% endif %}
-    ELSE app_name
-  END AS analysis_app_name,
-  NOT (
-    LOWER(isp) = "browserstack"
-    {% if app_name = "fenix" %}
-    AND LOWER(distribution_id) = "mozillaonline"
+    ELSE {{ app_name }}
+  END AS app_name,
+  {% if app_name not in ["klar_ios", "klar_android", "firefox_desktop"] %}
+  (
+    LOWER(IFNULL(isp, "")) <> "browserstack"
+    {% if app_name == "fenix" %}
+    AND LOWER(IFNULL(distribution_id, "")) <> "mozillaonline"
     {% endif %}
-  ) AS is_mobile, -- TODO: add logic to check the app is a mobile app
-  FALSE AS is_desktop, -- TODO: add logic to check the app is desktop
+  ){% endif %}{% else %} FALSE{% endif %} AS is_mobile, -- TODO: add logic to check the app is a mobile app
+  {% if app_name == "firefox_desktop" %}
+  (
+    LOWER(IFNULL(isp, "")) <> "browserstack"
+    AND LOWER(IFNULL(distribution_id, "")) <> "mozillaonline"
+  ){% else %} FALSE{% endif %} AS is_desktop,
   *
 FROM
   `{{ project_id }}.{{ last_seen_table }}`
-{% if app_name = "fenix" %}
+{% if app_name == "fenix" %}
 LEFT JOIN
   `moz-fx-data-shared-prod.fenix.firefox_android_clients` USING(client_id)
+{% endif %}
+{% if app_name == "firefox_desktop" %}
+-- LEFT JOIN
+--  `moz-fx-data-shared-prod.firefox_desktop.???` USING(client_id) -- Where can we get distribution_id
 {% endif %}

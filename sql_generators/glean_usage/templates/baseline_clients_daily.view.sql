d37fca7b982546b0644384445b8059a02d51162d
kik-kik <kignasiak@mozilla.com>
2024-04-25 15:30:40 +0200

    feat: add activity fields to support KPI metrics to last_seen views

--- sql_generators/glean_usage/templates/baseline_clients_daily.view.sql
+++ sql_generators/glean_usage/templates/baseline_clients_daily.view.sql
@@ -4,6 +4,12 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ daily_view }}`
 AS
 SELECT
-  *
+  *,
+  {% if app_name = "firefox_ios" %}
+  (
+    clients_daily.app_display_version = '107.2'
+    AND clients_daily.submission_date >= '2023-02-01'
+  ) AS is_suspicious_device_client,
+  {% endif %}
 FROM
   `{{ project_id }}.{{ daily_table }}`
--- sql_generators/glean_usage/templates/baseline_clients_last_seen.view.sql
+++ sql_generators/glean_usage/templates/baseline_clients_last_seen.view.sql
@@ -6,7 +6,35 @@ AS
 SELECT
   {% for ut in usage_types %}
     `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(days_{{ ut }}_bits) AS days_since_{{ ut }},
-  {% endfor %}
+  {% endfor %},
+  -- Metrics based on activity
+  mozfun.bits28.days_since_seen(days_active_bits) = 0 AS is_dau,
+  mozfun.bits28.days_since_seen(days_active_bits) < 7 AS is_wau,
+  mozfun.bits28.days_since_seen(days_active_bits) < 28 AS is_mau,
+  -- Metrics based on pings sent
+  mozfun.bits28.days_since_seen(days_seen_bits) = 0 AS is_daily_user,
+  mozfun.bits28.days_since_seen(days_seen_bits) < 7 AS is_weekly_user,
+  mozfun.bits28.days_since_seen(days_seen_bits) < 28 AS is_monthly_user,
+  CASE
+    WHEN LOWER(isp) = 'browserstack'
+      THEN CONCAT(app_name, ' ', isp_name)
+    {% if app_name = "fenix" %}
+    WHEN LOWER(distribution_id) = 'mozillaonline'
+      THEN CONCAT(app_name, ' ', distribution_id)
+    {% endif %}
+    ELSE app_name
+  END AS analysis_app_name,
+  NOT (
+    LOWER(isp) = "browserstack"
+    {% if app_name = "fenix" %}
+    AND LOWER(distribution_id) = "mozillaonline"
+    {% endif %}
+  ) AS is_mobile, -- TODO: add logic to check the app is a mobile app
+  FALSE AS is_desktop, -- TODO: add logic to check the app is desktop
   *
 FROM
   `{{ project_id }}.{{ last_seen_table }}`
+{% if app_name = "fenix" %}
+LEFT JOIN
+  `moz-fx-data-shared-prod.fenix.firefox_android_clients` USING(client_id)
+{% endif %}

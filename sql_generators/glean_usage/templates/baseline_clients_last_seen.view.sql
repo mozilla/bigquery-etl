590e05ba34150efbc7acd4f4f645cc20db262ac2
kik-kik <kignasiak@mozilla.com>
2024-04-26 11:26:09 +0200

    featL: move baseline_clients_last_seen modifications to cross_channel template

--- sql_generators/glean_usage/templates/baseline_clients_last_seen.view.sql
+++ sql_generators/glean_usage/templates/baseline_clients_last_seen.view.sql
@@ -6,44 +6,7 @@ AS
 SELECT
   {% for ut in usage_types %}
     `moz-fx-data-shared-prod`.udf.pos_of_trailing_set_bit(days_{{ ut }}_bits) AS days_since_{{ ut }},
-  {% endfor %},
-  -- Metrics based on activity
-  mozfun.bits28.days_since_seen(days_active_bits) = 0 AS is_dau,
-  mozfun.bits28.days_since_seen(days_active_bits) < 7 AS is_wau,
-  mozfun.bits28.days_since_seen(days_active_bits) < 28 AS is_mau,
-  -- Metrics based on pings sent
-  mozfun.bits28.days_since_seen(days_seen_bits) = 0 AS is_daily_user,
-  mozfun.bits28.days_since_seen(days_seen_bits) < 7 AS is_weekly_user,
-  mozfun.bits28.days_since_seen(days_seen_bits) < 28 AS is_monthly_user,
-  CASE
-    WHEN LOWER(isp) = 'browserstack'
-      THEN CONCAT({{ app_name }}, ' ', isp_name)
-    {% if app_name in ["fenix", "firefox_desktop"] %}
-    WHEN LOWER(distribution_id) = 'mozillaonline'
-      THEN CONCAT({{ app_name }}, ' ', distribution_id)
-    {% endif %}
-    ELSE {{ app_name }}
-  END AS app_name,
-  {% if app_name not in ["klar_ios", "klar_android", "firefox_desktop"] %}
-  (
-    LOWER(IFNULL(isp, "")) <> "browserstack"
-    {% if app_name == "fenix" %}
-    AND LOWER(IFNULL(distribution_id, "")) <> "mozillaonline"
-    {% endif %}
-  ){% endif %}{% else %} FALSE{% endif %} AS is_mobile, -- TODO: add logic to check the app is a mobile app
-  {% if app_name == "firefox_desktop" %}
-  (
-    LOWER(IFNULL(isp, "")) <> "browserstack"
-    AND LOWER(IFNULL(distribution_id, "")) <> "mozillaonline"
-  ){% else %} FALSE{% endif %} AS is_desktop,
+  {% endfor %}
   *
 FROM
   `{{ project_id }}.{{ last_seen_table }}`
-{% if app_name == "fenix" %}
-LEFT JOIN
-  `moz-fx-data-shared-prod.fenix.firefox_android_clients` USING(client_id)
-{% endif %}
-{% if app_name == "firefox_desktop" %}
--- LEFT JOIN
---  `moz-fx-data-shared-prod.firefox_desktop.???` USING(client_id) -- Where can we get distribution_id
-{% endif %}
--- sql_generators/glean_usage/templates/cross_channel.view.sql
+++ sql_generators/glean_usage/templates/cross_channel.view.sql
@@ -1,4 +1,6 @@
 -- Generated via ./bqetl generate glean_usage
+
+
 CREATE OR REPLACE VIEW
     `{{ project_id }}.{{ target_view }}`
 AS
@@ -7,13 +9,59 @@ AS
 UNION ALL
 {% endif -%}
 {% if app_name == "fenix" -%}
-SELECT 
+SELECT
     "{{ dataset }}" AS normalized_app_id,
     * REPLACE(mozfun.norm.fenix_app_info("{{ dataset }}", app_build).channel AS normalized_channel),
 {% else -%}
-SELECT 
+SELECT
     "{{ dataset }}" AS normalized_app_id,
-    * REPLACE("{{ channel }}" AS normalized_channel)
+    * REPLACE("{{ channel }}" AS normalized_channel),
 {% endif -%}
+{% if table == "baseline_clients_last_seen" %}
+    -- Metrics based on activity
+    CASE
+        WHEN BIT_COUNT(days_active_bits)
+            BETWEEN 1 AND 6
+                THEN 'infrequent_user'
+        WHEN BIT_COUNT(days_active_bits)
+            BETWEEN 7 AND 13
+                THEN 'casual_user'
+        WHEN BIT_COUNT(days_active_bits)
+            BETWEEN 14 AND 20
+                THEN 'regular_user'
+        WHEN BIT_COUNT(days_active_bits) >= 21
+            THEN 'core_user'
+        ELSE 'other'
+    END AS activity_segment,
+    mozfun.bits28.days_since_seen(days_active_bits) = 0 AS is_dau,
+    mozfun.bits28.days_since_seen(days_active_bits) < 7 AS is_wau,
+    mozfun.bits28.days_since_seen(days_active_bits) < 28 AS is_mau,
+    -- Metrics based on pings sent
+    mozfun.bits28.days_since_seen(days_seen_bits) = 0 AS is_daily_user,
+    mozfun.bits28.days_since_seen(days_seen_bits) < 7 AS is_weekly_user,
+    mozfun.bits28.days_since_seen(days_seen_bits) < 28 AS is_monthly_user,
+    CASE
+        WHEN LOWER(isp) = 'browserstack'
+            THEN CONCAT("{{ app_name }}", ' ', isp)
+        {% if app_name in ["fenix"] %}
+        WHEN LOWER(distribution_id) = 'mozillaonline'
+            THEN CONCAT("{{ app_name }}", ' ', distribution_id)
+        {% endif %}
+        ELSE "{{ app_name }}"
+    END AS app_name,
+    {% if app_name in ["klar_ios", "klar_android", "firefox_desktop"] -%}
+    FALSE
+    {% else -%}
+    (
+        LOWER(IFNULL(isp, "")) <> "browserstack"
+        {% if app_name == "fenix" %}
+        AND LOWER(IFNULL(distribution_id, "")) <> "mozillaonline"
+        {% endif %}
+    ){% endif %} AS is_mobile,  -- determined if should be used towards KPI calculations
 FROM `{{ project_id }}.{{ dataset }}.{{ table }}`
+{% if app_name == "fenix" %}
+LEFT JOIN
+  `moz-fx-data-shared-prod.fenix.firefox_android_clients` USING(client_id)
+{% endif %}
+{% endif -%}
 {% endfor %}

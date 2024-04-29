1f09d52ed986ad5c5966134cfc7b421037b10409
Dave Zeber <dzeber@mozilla.com>
2024-04-29 17:12:51 -0600

    Restructure view

--- sql_generators/serp_events_v2/templates/view.sql
+++ sql_generators/serp_events_v2/templates/view.sql
@@ -1,77 +1,156 @@
 CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ app_name }}.serp_events`
 AS
-WITH expanded AS (
+WITH with_ads_arrays AS (
+  -- prepare arrays for ad impressions and engagements with the same structure
+  -- to be combined in the next step
   SELECT
-    * EXCEPT (engagements, component_impressions),
-    (
-      -- use value table to compute multiple columns from unnest(engagements)
+    *,
+    ARRAY(
       SELECT AS STRUCT
-        -- inject ad component indicator into engagements array
-        ARRAY_AGG(STRUCT(component, action, num_engagements, is_ad_component)) AS engagements,
-        -- monetizable ad clicks
-        COALESCE(
-          SUM(IF(is_ad_component AND action = 'clicked', num_engagements, 0)),
-          0
-        ) AS num_ad_clicks,
-        -- non-ad link clicks
-        COALESCE(
-          SUM(IF(component = 'non_ads_link' AND action = 'clicked', num_engagements, 0)),
-          0
-        ) AS num_non_ad_link_clicks,
-        -- other types of engagement
-        COALESCE(
-          SUM(
-            IF(
-              action != 'clicked'
-              OR (action = 'clicked' AND NOT is_ad_component AND component != 'non_ads_link'),
-              num_engagements,
-              0
-            )
-          ),
-          0
-        ) AS num_other_engagements
+        component,
+        0 AS num_elements_loaded,
+        0 AS num_elements_visible,
+        0 AS num_elements_blocked,
+        0 AS num_elements_notshowing,
+        FALSE AS blocker_inferred,
+        IF(action = 'clicked', num_engagements, 0) AS num_clicks,
+        IF(action != 'clicked', num_engagements, 0) AS num_other_engagements
       FROM
-        (SELECT e.*, is_ad_component(e.component) AS is_ad_component FROM UNNEST(engagements) AS e)
-    ).*,
-    (
-      -- use value table to compute multiple columns from unnest(component_impressions)
+        UNNEST(engagements)
+      WHERE
+        is_ad_component(component)
+    ) AS _ad_engagements,
+    ARRAY(
       SELECT AS STRUCT
-        -- inject ad component and blocking indicators into components array
-        ARRAY_AGG(
-          STRUCT(
-            component,
-            num_elements_loaded,
-            num_elements_visible,
-            num_elements_blocked,
-            num_elements_notshowing,
-            is_ad_component,
-            blocker_inferred
-          )
-        ) AS component_impressions,
-        COALESCE(SUM(IF(is_ad_component, num_elements_loaded, 0)), 0) AS num_ads_loaded,
-        COALESCE(SUM(IF(is_ad_component, num_elements_visible, 0)), 0) AS num_ads_visible,
-        COALESCE(SUM(IF(is_ad_component, num_elements_blocked, 0)), 0) AS num_ads_blocked,
-        COALESCE(SUM(IF(is_ad_component, num_elements_notshowing, 0)), 0) AS num_ads_notshowing,
-        -- an ad blocker is inferred to be in use if all ads are blocked in at least one ad component
-        -- FALSE if no ads are loaded because we cannot infer
-        COALESCE(LOGICAL_OR(is_ad_component AND blocker_inferred), FALSE) AS ad_blocker_inferred
+        component,
+        num_elements_loaded,
+        num_elements_visible,
+        num_elements_blocked,
+        num_elements_notshowing,
+        ad_blocker_inferred(num_elements_loaded, num_elements_blocked) AS blocker_inferred,
+        0 AS num_clicks,
+        0 AS num_other_engagements
       FROM
-        (
-          SELECT
-            c.*,
-            is_ad_component(c.component) AS is_ad_component,
-            ad_blocker_inferred(c.num_elements_loaded, c.num_elements_blocked) AS blocker_inferred
-          FROM
-            UNNEST(component_impressions) AS c
-        )
-    ).*
+        UNNEST(component_impressions)
+      WHERE
+        is_ad_component(component)
+    ) AS _ad_impressions,
   FROM
     `{{ project_id }}.{{ app_name }}_derived.serp_events_v2`
-),
+)
 SELECT
-  * EXCEPT (num_ad_clicks),
-  -- clicks must refer to visible ads (impressions)
-  IF(num_ads_visible > 0, num_ad_clicks, 0) AS num_ad_clicks
+  * EXCEPT (engagements, component_impressions, _ad_engagements, _ad_impressions),
+  -- array for ad components "joining" impressions and engagements
+  -- aggregate to get 1 entry per ad component
+  ARRAY(
+    SELECT AS STRUCT
+      component,
+      SUM(num_elements_loaded) AS num_loaded,
+      SUM(num_elements_visible) AS num_visible,
+      SUM(num_elements_blocked) AS num_blocked,
+      SUM(num_elements_notshowing) AS num_notshowing,
+      SUM(num_clicks) AS num_clicks,
+      SUM(num_other_engagements) AS num_other_engagements,
+      LOGICAL_OR(blocker_inferred) AS blocker_inferred
+    FROM
+      UNNEST(ARRAY_CONCAT(_ad_impressions, _ad_engagements))
+    GROUP BY
+      component
+  ) AS ad_components,
+  -- remaining engagements for non-ad components
+  ARRAY(
+    SELECT
+      e
+    FROM
+      UNNEST(engagements) AS e
+    WHERE
+      NOT is_ad_component(e.component)
+  ) AS non_ad_engagements,
+  -- remaining impressions for non-ad components
+  ARRAY(
+    SELECT
+      c
+    FROM
+      UNNEST(component_impressions) AS c
+    WHERE
+      NOT is_ad_component(c.component)
+  ) AS non_ad_impressions,
+  -- total ad clicks for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_engagements), 0)
+    FROM
+      UNNEST(engagements)
+    WHERE
+      is_ad_component(component)
+      AND action = 'clicked'
+  ) AS num_ad_clicks,
+  -- total non-ad link clicks for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_engagements), 0)
+    FROM
+      UNNEST(engagements)
+    WHERE
+      component = 'non_ads_link'
+      AND action = 'clicked'
+  ) AS num_non_ad_link_clicks,
+  -- total other engagements for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_engagements), 0)
+    FROM
+      UNNEST(engagements)
+    WHERE
+      action != 'clicked'
+      OR (action = 'clicked' AND NOT is_ad_component(component) AND component != 'non_ads_link')
+  ) AS num_other_engagements,
+  -- total ads loaded for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_elements_loaded), 0)
+    FROM
+      UNNEST(component_impressions)
+    WHERE
+      is_ad_component(component)
+  ) AS num_ads_loaded,
+  -- total ads visible for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_elements_visible), 0)
+    FROM
+      UNNEST(component_impressions)
+    WHERE
+      is_ad_component(component)
+  ) AS num_ads_visible,
+  -- total ads blocked for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_elements_blocked), 0)
+    FROM
+      UNNEST(component_impressions)
+    WHERE
+      is_ad_component(component)
+  ) AS num_ads_blocked,
+  -- total ads not showing for the SERP impression
+  (
+    SELECT
+      COALESCE(SUM(num_elements_notshowing), 0)
+    FROM
+      UNNEST(component_impressions)
+    WHERE
+      is_ad_component(component)
+  ) AS num_ads_notshowing,
+  -- is ad blocker inferred?
+  -- true if blocker is inferred for at least 1 component
+  (
+    SELECT
+      COALESCE(LOGICAL_OR(ad_blocker_inferred(num_elements_loaded, num_elements_blocked)), FALSE)
+    FROM
+      UNNEST(component_impressions)
+    WHERE
+      is_ad_component(component)
+  ) AS ad_blocker_inferred,
 FROM
-  expanded
+  with_ads_arrays

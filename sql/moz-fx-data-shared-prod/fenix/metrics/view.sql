ec94a04e62fcd32cca38a30e6c8732fb70722a60
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-01 16:15:44 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/fenix/metrics/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/metrics/view.sql
@@ -783,7 +783,8 @@ SELECT
       metrics.timing_distribution.httpsfirst_downgrade_time_schemeless,
       metrics.timing_distribution.networking_http_content_cssloader_ondatafinished_to_onstop_delay,
       metrics.timing_distribution.cert_verifier_cert_trust_evaluation_time,
-      metrics.timing_distribution.networking_dns_native_https_call_time
+      metrics.timing_distribution.networking_dns_native_https_call_time,
+      metrics.timing_distribution.bounce_tracking_protection_purge_duration
     ) AS `timing_distribution`,
     metrics.memory_distribution,
     metrics.datetime,
@@ -1399,7 +1400,8 @@ SELECT
       metrics.timing_distribution.httpsfirst_downgrade_time_schemeless,
       metrics.timing_distribution.networking_http_content_cssloader_ondatafinished_to_onstop_delay,
       metrics.timing_distribution.cert_verifier_cert_trust_evaluation_time,
-      metrics.timing_distribution.networking_dns_native_https_call_time
+      metrics.timing_distribution.networking_dns_native_https_call_time,
+      metrics.timing_distribution.bounce_tracking_protection_purge_duration
     ) AS `timing_distribution`,
     metrics.memory_distribution,
     metrics.datetime,

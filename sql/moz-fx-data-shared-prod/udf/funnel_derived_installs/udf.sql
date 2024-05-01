7b5759094959067d4bcc5ad4a05504d864687b46
Katie Windau <kwindau@mozilla.com>
2024-05-01 09:05:25 -0500

    DENG-3371 reformat udf.sql

--- sql/moz-fx-data-shared-prod/udf/funnel_derived_installs/udf.sql
+++ sql/moz-fx-data-shared-prod/udf/funnel_derived_installs/udf.sql
@@ -15,12 +15,6 @@ RETURNS STRING AS (
 
 SELECT
   mozfun.assert.equals(
-    udf.funnel_derived_installs(
-      TRUE,
-      '2024-04-08 10:43:10.972298 UTC',
-      '123',
-      'ATTR1',
-      'DISTRO1'
-    ),
+    udf.funnel_derived_installs(TRUE, '2024-04-08 10:43:10.972298 UTC', '123', 'ATTR1', 'DISTRO1'),
     'hello'
   );

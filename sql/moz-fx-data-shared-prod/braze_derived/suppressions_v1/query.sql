ee665e1f221320793a39a91014e186fc2804ce15
Leli Schiestl <lschiestl@mozilla.com>
2024-05-02 23:59:43 +0200

    update table reference

--- sql/moz-fx-data-shared-prod/braze_derived/suppressions_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/suppressions_v1/query.sql
@@ -2,7 +2,7 @@ WITH suppressions AS (
   SELECT
     LOWER(email) AS email
   FROM
-    `moz-fx-data-shared-prod.acoustic_external.suppression_list_v1`
+    `moz-fx-data-shared-prod.marketing_suppression_list_derived.main_suppression_list_v1`
   UNION DISTINCT
   SELECT
     LOWER(primary_email) AS email

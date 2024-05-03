b2c9930bd6cfb35ab2f530d24d7e7b79a55005bc
Chelsey Beck <cbeck@mozilla.com>
2024-05-03 16:33:55 -0700

    formatting sql

--- sql/moz-fx-data-shared-prod/braze_derived/newsletters_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/newsletters_v1/checks.sql
@@ -1,26 +1,27 @@
 -- raw SQL checks
 -- checking to make sure there is at least one UPDATED_AT value
+
 #fail
-ASSERT
-  (
+ASSERT(
   SELECT
     COUNT(1)
   FROM
     `moz-fx-data-shared-prod.braze_external.changed_newsletters_sync_v1`
   WHERE
-    UPDATED_AT IS NOT NULL) > 0;
+    UPDATED_AT IS NOT NULL
+) > 0;
 
 -- checking to see if there is new data since the last run
 -- if not, fail or we will have blank sync tables
+
 #fail
-ASSERT
-  (
-  WITH
-    max_update AS (
+ASSERT(
+  WITH max_update AS (
     SELECT
       MAX(UPDATED_AT) AS max_updated_at
     FROM
-      `moz-fx-data-shared-prod.braze_external.changed_newsletters_sync_v1` )
+      `moz-fx-data-shared-prod.braze_external.changed_newsletters_sync_v1`
+  )
   SELECT
     COUNT(1)
   FROM
@@ -28,4 +29,5 @@ ASSERT
     UNNEST(newsletters) AS newsletters,
     max_update
   WHERE
-    newsletters.update_timestamp > max_update.max_updated_at) > 0;
+    newsletters.update_timestamp > max_update.max_updated_at
+) > 0;

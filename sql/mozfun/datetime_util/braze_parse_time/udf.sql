5e937e977326a3aea11cd09ca4eceff5dd67120d
Chelsey Beck <cbeck@mozilla.com>
2024-05-05 07:37:23 -0700

    adding udf to parse timestamp from json object

---
+++ sql/mozfun/datetime_util/braze_parse_time/udf.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION datetime_util.braze_parse_time(json_str STRING)
+  RETURNS STRING
+  LANGUAGE js AS """
+  try {
+    var json = JSON.parse(json_str);
+    return json['$time'];
+  } catch (e) {
+    return null;
+  }
+""";
+
+-- test
+  assert.equals("2024-05-03 16:14:09 UTC", mozfun.datetime_util.braze_parse_time("{"$time":"2024-05-03 16:14:09.000000 UTC"}")),

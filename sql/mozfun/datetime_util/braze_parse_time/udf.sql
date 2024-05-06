228231567dbe2589f867d34bd767480217d8ca0b
Chelsey Beck <cbeck@mozilla.com>
2024-05-06 06:54:27 -0700

    formatting

--- sql/mozfun/datetime_util/braze_parse_time/udf.sql
+++ sql/mozfun/datetime_util/braze_parse_time/udf.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE FUNCTION datetime_util.braze_parse_time(json_str STRING)
-  RETURNS STRING
-  LANGUAGE js AS """
+RETURNS STRING
+LANGUAGE js
+AS
+  """
   try {
     var json = JSON.parse(json_str);
     return json['$time'];
@@ -10,4 +12,7 @@ CREATE OR REPLACE FUNCTION datetime_util.braze_parse_time(json_str STRING)
 """;
 
 -- test
-  assert.equals("2024-05-03 16:14:09 UTC", mozfun.datetime_util.braze_parse_time("{"$time":"2024-05-03 16:14:09.000000 UTC"}")),
+assert.equals(
+  "2024-05-03 16:14:09 UTC",
+  mozfun.datetime_util.braze_parse_time("{" $ time ":" 2024 - 05 - 03 16 : 14 : 09.000000 UTC "}")
+),

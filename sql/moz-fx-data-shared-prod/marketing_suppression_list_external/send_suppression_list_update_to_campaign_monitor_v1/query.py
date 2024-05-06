452dfec3adeb660625b7aa5237623c48c2d62a20
Leli Schiestl <lschiestl@mozilla.com>
2024-05-06 22:19:03 +0200

    status quo

---
+++ sql/moz-fx-data-shared-prod/marketing_suppression_list_external/send_suppression_list_update_to_campaign_monitor_v1/query.py
@@ -0,0 +1,14 @@
+"""Upload changes in Suppression list to Campaign Monitor API."""
+
+# from pathlib import Path
+# from typing import Any, Dict, List
+
+# import requests
+# import rich_click as click
+# from google.cloud import bigquery
+# from requests import HTTPError
+# from requests.auth import HTTPBasicAuth
+
+# from bigquery_etl.schema import Schema
+
+BASE_URL = "https://api.createsend.com/api/v3.3/clients"

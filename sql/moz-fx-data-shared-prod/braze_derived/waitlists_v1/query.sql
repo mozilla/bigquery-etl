94cb8a68b8eddeb2e439e946921c8d9dbd16512a
Leli Schiestl <lschiestl@mozilla.com>
2024-05-07 19:31:07 +0200

    add lower() to waitlists platform and source

--- sql/moz-fx-data-shared-prod/braze_derived/waitlists_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/waitlists_v1/query.sql
@@ -3,9 +3,9 @@ SELECT
   ARRAY_AGG(
     STRUCT(
       name AS waitlist_name,
-      JSON_EXTRACT_SCALAR(fields, '$.geo') AS waitlist_geo,
-      JSON_EXTRACT_SCALAR(fields, '$.platform') AS waitlist_platform,
-      source AS waitlist_source,
+      LOWER(JSON_EXTRACT_SCALAR(fields, '$.geo')) AS waitlist_geo,
+      LOWER(JSON_EXTRACT_SCALAR(fields, '$.platform')) AS waitlist_platform,
+      LOWER(source) AS waitlist_source,
       create_timestamp,
       subscribed,
       unsub_reason,

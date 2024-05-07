b81bba5df9c68238088065e71cbfb759e76f5abf
Chelsey Beck <cbeck@mozilla.com>
2024-05-07 09:38:14 -0700

    removing time filter

--- sql/moz-fx-data-shared-prod/braze_external/changed_firefox_subscriptions_sync_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_external/changed_firefox_subscriptions_sync_v1/query.sql
@@ -1,39 +1,22 @@
--- CTE to determine the maximum update timestamp from changed_subscriptions_v1
-WITH max_update AS (
-  SELECT
-    MAX(subscriptions.update_timestamp) AS latest_subscription_updated_at
-  FROM
-    `moz-fx-data-shared-prod.braze_external.changed_subscriptions_v1` AS changed,
-    UNNEST(changed.subscriptions) AS subscriptions
-),
 -- Construct the JSON payload in Braze required format
-subscriptions_updated AS (
-  SELECT
-    CURRENT_TIMESTAMP() AS UPDATED_AT,
-    subscriptions.external_id AS EXTERNAL_ID,
-    TO_JSON(
-      STRUCT(
-        ARRAY_AGG(
-          STRUCT(
-            subscriptions_array.firefox_subscription_id AS subscription_group_id,
-            subscriptions_array.subscription_state AS subscription_state
-          )
-          ORDER BY
-            subscriptions_array.update_timestamp DESC
-        ) AS subscription_groups
-      )
-    ) AS PAYLOAD
-  FROM
-    `moz-fx-data-shared-prod.braze_external.changed_subscriptions_v1` AS subscriptions
-  CROSS JOIN
-    UNNEST(subscriptions.subscriptions) AS subscriptions_array
-  JOIN
-    max_update
-    ON subscriptions_array.update_timestamp > max_update.latest_subscription_updated_at
-  GROUP BY
-    subscriptions.external_id
-)
 SELECT
-  *
+  CURRENT_TIMESTAMP() AS UPDATED_AT,
+  subscriptions.external_id AS EXTERNAL_ID,
+  TO_JSON(
+    STRUCT(
+      ARRAY_AGG(
+        STRUCT(
+          subscriptions_array.firefox_subscription_id AS subscription_group_id,
+          subscriptions_array.subscription_state AS subscription_state
+        )
+        ORDER BY
+          subscriptions_array.update_timestamp DESC
+      ) AS subscription_groups
+    )
+  ) AS PAYLOAD
 FROM
-  subscriptions_updated;
+  `moz-fx-data-shared-prod.braze_external.changed_subscriptions_v1` AS subscriptions
+CROSS JOIN
+  UNNEST(subscriptions.subscriptions) AS subscriptions_array
+GROUP BY
+  subscriptions.external_id;

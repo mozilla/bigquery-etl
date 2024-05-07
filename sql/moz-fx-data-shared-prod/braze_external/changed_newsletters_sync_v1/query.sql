3df6f02cdb83de095d1ec40c5ee7bad1f3020c57
Chelsey Beck <64881557+chelseybeck@users.noreply.github.com>
2024-05-07 12:07:40 -0700

    DENG-3615 QA Fixes for Braze models (#5508)
    * changing structure of payload and structuring timestamps
    
    * updating time filter and standardizing
    
    * adding changed subscriptions model and updating time filter
    
    * moving udf to separate pr and changing source table
    
    * adding comments and moving max to timestamps
    
    * filtering out mofo users and fxa_account_deleted true"
    
    * updating filter on tables to pull most recent and adding acoustic event data
    
    * formatting and refactoring
    
    * formatting
    
    * updating format and adding comments
    
    * removing time filter
    
    * fix alias
    
    * reverting waitlists code back to original
    
    * add lower() to waitlists platform and source
    
    * ading inner join to users
    
    * fix test_braze_subscriptions
    
    * fix test_braze_waitlists
    
    ---------
    
    Co-authored-by: Leli Schiestl <lschiestl@mozilla.com>

--- sql/moz-fx-data-shared-prod/braze_external/changed_newsletters_sync_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_external/changed_newsletters_sync_v1/query.sql
@@ -1,9 +1,14 @@
+  -- Retrieves the maximum newsletter updated timestamp from the last run to only
+  -- select recently changed records
 WITH max_update AS (
   SELECT
-    MAX(UPDATED_AT) AS max_update_timestamp
+    MAX(
+      TIMESTAMP(JSON_VALUE(payload.newsletters_v1[0].update_timestamp, '$."$time"'))
+    ) AS latest_newsletter_updated_at
   FROM
     `moz-fx-data-shared-prod.braze_external.changed_newsletters_sync_v1`
 )
+  -- Construct the JSON payload in Braze required format
 SELECT
   CURRENT_TIMESTAMP() AS UPDATED_AT,
   newsletters.external_id AS EXTERNAL_ID,
@@ -22,14 +27,14 @@ SELECT
               newsletters_array.create_timestamp,
               'UTC'
             ) AS `$time`
-          ) AS create_timestamp,
+          ) AS created_at,
           STRUCT(
             FORMAT_TIMESTAMP(
               '%Y-%m-%d %H:%M:%E6S UTC',
               newsletters_array.update_timestamp,
               'UTC'
             ) AS `$time`
-          ) AS update_timestamp
+          ) AS updated_at
         )
         ORDER BY
           newsletters_array.update_timestamp DESC
@@ -41,6 +46,6 @@ FROM
 CROSS JOIN
   UNNEST(newsletters.newsletters) AS newsletters_array
 WHERE
-  newsletters_array.update_timestamp > (SELECT max_update_timestamp FROM max_update)
+  newsletters_array.update_timestamp > (SELECT latest_newsletter_updated_at FROM max_update)
 GROUP BY
   newsletters.external_id;

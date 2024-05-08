096f4810b6c17dc906ab809a4e3623a47f0a832f
Katie Windau <kwindau@mozilla.com>
2024-05-08 14:15:56 -0500

    DENG-3288 initial commit

---
+++ sql/moz-fx-data-shared-prod/telemetry/mobile_engagement/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.mobile_engagement`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.mobile_engagement_v1`
---
+++ sql/moz-fx-data-shared-prod/telemetry/mobile_engagement_clients/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.mobile_engagement_clients`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.mobile_engagement_clients_v1`

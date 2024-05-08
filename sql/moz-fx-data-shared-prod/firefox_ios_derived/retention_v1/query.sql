06985ef3103671a5c759bf1aeaafc8c1dac0b080
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-08 17:00:34 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/retention_v1/query.sql
@@ -0,0 +1,38 @@
+SELECT
+  metric_date,
+  first_seen_date,
+  app_name,
+  normalized_channel,
+  country,
+  app_version,
+  locale,
+  adjust_ad_group,
+  adjust_campaign,
+  adjust_creative,
+  adjust_network,
+  is_suspicious_device_profile,
+  COUNTIF(ping_sent_metric_date) AS ping_sent_metric_date,
+  COUNTIF(ping_sent_week_4) AS ping_sent_week_4,
+  COUNTIF(active_metric_date) AS active_metric_date,
+  COUNTIF(retained_week_4) AS retained_week_4,
+  COUNTIF(retained_week_4_new_profile) AS retained_week_4_new_profiles,
+  COUNTIF(new_profile_metric_date) AS new_profiles_metric_date,
+  COUNTIF(repeat_profile) AS repeat_profiles,
+FROM
+  firefox_ios.retention_clients
+WHERE
+  metric_date = DATE_SUB(@submission_date, INTERVAL 27 DAY)
+  AND submission_date = @submission_date
+GROUP BY
+  metric_date,
+  first_seen_date,
+  app_name,
+  normalized_channel,
+  country,
+  app_display_version,
+  locale,
+  adjust_ad_group,
+  adjust_campaign,
+  adjust_creative,
+  adjust_network,
+  is_suspicious_device_profile

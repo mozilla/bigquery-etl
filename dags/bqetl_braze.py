9414db08cfb95c92bca73645be923284a02c90f3
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-10 18:24:30 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_braze.py
+++ dags/bqetl_braze.py
@@ -135,6 +135,18 @@ with DAG(
         task_concurrency=1,
     )
 
+    braze_derived__subscriptions_map__v1 = bigquery_etl_query(
+        task_id="braze_derived__subscriptions_map__v1",
+        destination_table=None,
+        dataset_id="braze_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="cbeck@mozilla.com",
+        email=["cbeck@mozilla.com"],
+        date_partition_parameter=None,
+        depends_on_past=False,
+        sql_file_path="sql/moz-fx-data-shared-prod/braze_derived/subscriptions_map_v1/script.sql",
+    )
+
     braze_derived__suppressions__v1 = bigquery_etl_query(
         task_id="braze_derived__suppressions__v1",
         destination_table="suppressions_v1",
@@ -342,6 +354,19 @@ with DAG(
         retries=0,
     )
 
+    checks__fail_braze_derived__subscriptions_map__v1 = bigquery_dq_check(
+        task_id="checks__fail_braze_derived__subscriptions_map__v1",
+        source_table="subscriptions_map_v1",
+        dataset_id="braze_derived",
+        project_id="moz-fx-data-shared-prod",
+        is_dq_check_fail=True,
+        owner="cbeck@mozilla.com",
+        email=["cbeck@mozilla.com"],
+        depends_on_past=False,
+        task_concurrency=1,
+        retries=0,
+    )
+
     checks__fail_braze_derived__user_profiles__v1 = bigquery_dq_check(
         task_id="checks__fail_braze_derived__user_profiles__v1",
         source_table="user_profiles_v1",
@@ -507,6 +532,10 @@ with DAG(
     )
 
     braze_derived__subscriptions__v1.set_upstream(
+        checks__fail_braze_derived__subscriptions_map__v1
+    )
+
+    braze_derived__subscriptions__v1.set_upstream(
         checks__fail_braze_derived__user_profiles__v1
     )
 
@@ -532,6 +561,10 @@ with DAG(
 
     braze_derived__users__v1.set_upstream(
         wait_for_acoustic_external__raw_recipient_raw__v1
+    )
+
+    braze_derived__users__v1.set_upstream(
+        checks__fail_braze_derived__subscriptions_map__v1
     )
 
     braze_derived__users__v1.set_upstream(
@@ -584,6 +617,10 @@ with DAG(
 
     checks__fail_braze_derived__subscriptions__v1.set_upstream(
         braze_derived__subscriptions__v1
+    )
+
+    checks__fail_braze_derived__subscriptions_map__v1.set_upstream(
+        braze_derived__subscriptions_map__v1
     )
 
     checks__fail_braze_derived__user_profiles__v1.set_upstream(

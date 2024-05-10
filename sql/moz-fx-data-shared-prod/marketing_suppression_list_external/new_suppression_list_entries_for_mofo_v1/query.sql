8d1e3d355ab1b09d5fef7cdd259dfb8a5160cb21
Leli Schiestl <lschiestl@mozilla.com>
2024-05-10 19:16:13 +0200

    remove hard coded date

--- sql/moz-fx-data-shared-prod/marketing_suppression_list_external/new_suppression_list_entries_for_mofo_v1/query.sql
+++ sql/moz-fx-data-shared-prod/marketing_suppression_list_external/new_suppression_list_entries_for_mofo_v1/query.sql
@@ -6,5 +6,4 @@ LEFT JOIN
   `moz-fx-data-shared-prod.marketing_suppression_list_external.campaign_monitor_suppression_list_v1` AS current_mofo
   ON main.email = current_mofo.email
 WHERE
-  main.suppressed_timestamp > "2024-03-31"
-  AND current_mofo.email IS NULL
+  current_mofo.email IS NULL

61cc172396da07fd4b64343aea269b53fc7454a2
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-10 01:30:59 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_clients_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_clients_v1/query.sql
@@ -12,12 +12,15 @@ SELECT
   cfs.attribution_dlsource,
   cfs.attribution_medium,
   cfs.attribution_ua,
+  cfs.attribution_experiment,
+  cls.attribution.variation AS attribution_variation,
   cls.normalized_channel,
   mozfun.norm.os(cls.os) AS normalized_os,
   COALESCE(
     mozfun.norm.windows_version_info(cls.os, cls.os_version, cls.windows_build_number),
     NULLIF(SPLIT(cls.normalized_os_version, ".")[SAFE_OFFSET(0)], "")
   ) AS normalized_os_version,
+  cls.startup_profile_selection_reason_first AS startup_profile_selection_reason,
   cls.country,
   cls.is_dau,
   cls.is_wau,
@@ -25,7 +28,7 @@ SELECT
 FROM
   `moz-fx-data-shared-prod.telemetry.clients_last_seen_v2` cls
 LEFT JOIN
-  `mozdata.telemetry.clients_first_seen` cfs
+  `moz-fx-data-shared-prod.telemetry.clients_first_seen` cfs
   ON cls.client_id = cfs.client_id
 WHERE
   cls.submission_date = @submission_date

df82ac3f01b1fe74904bff5edb79032b25e71170
Sean Rose <srose@mozilla.com>
2024-05-14 16:29:34 -0700

    Change `classify_site_query` UDF to differentiate Firefox and Mozilla brand keywords.

--- sql/mozfun/google_search_console/classify_site_query/udf.sql
+++ sql/mozfun/google_search_console/classify_site_query/udf.sql
@@ -10,91 +10,96 @@ RETURNS STRING AS (
       THEN NULL
     WHEN query IS NULL
       THEN 'Anonymized'
-    WHEN site_domain_name = 'www.mozilla.org'
-      THEN IF(
-          REGEXP_CONTAINS(
-            query,
-            ARRAY_TO_STRING(
-              [
-                r'\bff\b',
-                r'\bm.z',
-                r'f.ref.x',
-                r'fier',
-                r'fire',
-                r'firf',
-                r'focus',
-                r'fokku',
-                r'fox',
-                r'il+a\b',
-                r'nightly',
-                r'quantum',
-                r'μοζ+ιλ+α',  -- moz+il+a (Greek)
-                r'μοτζιλα',  -- motzila (Greek)
-                r'лиса',  -- fox (Cyrillic)
-                r'мази',  -- mazi (Cyrillic)
-                r'мазі',  -- mazi (Cyrillic)
-                r'моз',  -- moz (Cyrillic)
-                r'муз',  -- muz (Cyrillic)
-                r'фаер',  -- fire (Cyrillic)
-                r'фаир',  -- fair (Cyrillic)
-                r'файер',  -- fire (Cyrillic)
-                r'файр',  -- fire (Cyrillic)
-                r'фире',  -- fire (Cyrillic)
-                r'фокс',  -- fox (Cyrillic)
-                r'фох',  -- fox (Cyrillic)
-                r'כןרקכםס',  -- yes, rakhems (Hebrew)
-                r'מוזילה',  -- mozilla (Hebrew)
-                r'פיירפוקס',  -- firefox (Hebrew)
-                r'فاکس',  -- fax (Arabic)
-                r'فاير',  -- fire (Arabic)
-                r'فایر',  -- fire (Arabic)
-                r'فكس',  -- fx (Arabic)
-                r'فوكس',  -- fox (Arabic)
-                r'فير',  -- fir (Arabic)
-                r'موزلا',  -- mozilla (Arabic)
-                r'موزيلا',  -- mozilla (Arabic)
-                r'موزیلا',  -- mozilla (Arabic)
-                r'फायरफक्स',  -- firefox (Indic)
-                r'फायरफॉक्स',  -- firefox (Indic)
-                r'फ़ायरफ़ॉक्स',  -- firefox (Indic)
-                r'मोजिला',  -- mozilla (Indic)
-                r'मोज़िला',  -- mozilla (Indic)
-                r'ফায়ারফক্স',  -- firefox (Indic)
-                r'মজিলা',  -- mozilla (Indic)
-                r'মোজিলা',  -- mozilla (Indic)
-                r'ฟายฟอก',  -- bleach (Thai)
-                r'ฟายฟ๊อก',  -- firefox (Thai)
-                r'ไฟ ฟอก',  -- fire bleach (Thai)
-                r'ไฟฟ็อก',  -- fire fox (Thai)
-                r'ไฟฟ๊อก',  -- fire fox (Thai)
-                r'ไฟฟอก',  -- purifying light (Thai)
-                r'ไฟร์ฟอกซ์',  -- firefox (Thai)
-                r'ไฟล์ฟอก',  -- bleaching file (Thai)
-                r'ไฟลฟอก',  -- fire bleach (Thai)
-                r'모질라',  -- mozilla (Korean)
-                r'파이어',  -- fire (Korean)
-                r'폭스',  -- fox (Korean)
-                r'화이어',  -- fire (Korean)
-                r'ふぁいあ',  -- faia (Japanese)
-                r'ファイア',  -- fire (Japanese)
-                r'ファイや',  -- faiya (Japanese)
-                r'ファイヤ',  -- fire (Japanese)
-                r'ふぁいやー',  -- fire (Japanese)
-                r'ふぃれふぉ',  -- firefox (Japanese)
-                r'ふぉっくす',  -- fox (Japanese)
-                r'フォックス',  -- fox (Japanese)
-                r'モジラ',  -- mozilla (Japanese)
-                r'火孤',  -- firefox (Chinese)
-                r'火狐',  -- firefox (Chinese)
-                r'狐狸'  -- fox (Chinese)
-              ],
-              '|'
-            )
-          ),
-          'Brand',
-          'Non-Brand'
+    WHEN REGEXP_CONTAINS(
+        query,
+        ARRAY_TO_STRING(
+          [
+            r'\bff\b',
+            r'f.ref.x',
+            r'fier',
+            r'fire',
+            r'firf',
+            r'focus',
+            r'fokku',
+            r'fox',
+            r'nightly',
+            r'quantum',
+            r'лиса',  -- fox (Cyrillic)
+            r'фаер',  -- fire (Cyrillic)
+            r'фаир',  -- fair (Cyrillic)
+            r'файер',  -- fire (Cyrillic)
+            r'файр',  -- fire (Cyrillic)
+            r'фире',  -- fire (Cyrillic)
+            r'фокс',  -- fox (Cyrillic)
+            r'фох',  -- fox (Cyrillic)
+            r'כןרקכםס',  -- yes, rakhems (Hebrew)
+            r'פיירפוקס',  -- firefox (Hebrew)
+            r'فاکس',  -- fax (Arabic)
+            r'فاير',  -- fire (Arabic)
+            r'فایر',  -- fire (Arabic)
+            r'فكس',  -- fx (Arabic)
+            r'فوكس',  -- fox (Arabic)
+            r'فير',  -- fir (Arabic)
+            r'फायरफक्स',  -- firefox (Indic)
+            r'फायरफॉक्स',  -- firefox (Indic)
+            r'फ़ायरफ़ॉक्स',  -- firefox (Indic)
+            r'ফায়ারফক্স',  -- firefox (Indic)
+            r'ฟายฟอก',  -- bleach (Thai)
+            r'ฟายฟ๊อก',  -- firefox (Thai)
+            r'ไฟ ฟอก',  -- fire bleach (Thai)
+            r'ไฟฟ็อก',  -- fire fox (Thai)
+            r'ไฟฟ๊อก',  -- fire fox (Thai)
+            r'ไฟฟอก',  -- purifying light (Thai)
+            r'ไฟร์ฟอกซ์',  -- firefox (Thai)
+            r'ไฟล์ฟอก',  -- bleaching file (Thai)
+            r'ไฟลฟอก',  -- fire bleach (Thai)
+            r'파이어',  -- fire (Korean)
+            r'폭스',  -- fox (Korean)
+            r'화이어',  -- fire (Korean)
+            r'ふぁいあ',  -- faia (Japanese)
+            r'ファイア',  -- fire (Japanese)
+            r'ファイや',  -- faiya (Japanese)
+            r'ファイヤ',  -- fire (Japanese)
+            r'ふぁいやー',  -- fire (Japanese)
+            r'ふぃれふぉ',  -- firefox (Japanese)
+            r'ふぉっくす',  -- fox (Japanese)
+            r'フォックス',  -- fox (Japanese)
+            r'火孤',  -- firefox (Chinese)
+            r'火狐',  -- firefox (Chinese)
+            r'狐狸'  -- fox (Chinese)
+          ],
+          '|'
         )
-    ELSE 'Unknown'
+      )
+      THEN 'Firefox Brand'
+    WHEN REGEXP_CONTAINS(
+        query,
+        ARRAY_TO_STRING(
+          [
+            r'\bm.z',
+            r'il+a\b',
+            r'μοζ+ιλ+α',  -- moz+il+a (Greek)
+            r'μοτζιλα',  -- motzila (Greek)
+            r'мази',  -- mazi (Cyrillic)
+            r'мазі',  -- mazi (Cyrillic)
+            r'моз',  -- moz (Cyrillic)
+            r'муз',  -- muz (Cyrillic)
+            r'מוזילה',  -- mozilla (Hebrew)
+            r'موزلا',  -- mozilla (Arabic)
+            r'موزيلا',  -- mozilla (Arabic)
+            r'موزیلا',  -- mozilla (Arabic)
+            r'मोजिला',  -- mozilla (Indic)
+            r'मोज़िला',  -- mozilla (Indic)
+            r'মজিলা',  -- mozilla (Indic)
+            r'মোজিলা',  -- mozilla (Indic)
+            r'모질라',  -- mozilla (Korean)
+            r'モジラ'  -- mozilla (Japanese)
+          ],
+          '|'
+        )
+      )
+      THEN 'Mozilla Brand'
+    ELSE 'Non-Brand'
   END
 );
 
@@ -121,17 +126,21 @@ SELECT
   ),
   assert.equals(
     google_search_console.classify_site_query('www.mozilla.org', 'mozilla', 'Web'),
-    'Brand'
+    'Mozilla Brand'
   ),
   assert.equals(
     google_search_console.classify_site_query('www.mozilla.org', 'firefox', 'Web'),
-    'Brand'
+    'Firefox Brand'
+  ),
+  assert.equals(
+    google_search_console.classify_site_query('www.mozilla.org', 'mozilla firefox', 'Web'),
+    'Firefox Brand'
   ),
   assert.equals(
     google_search_console.classify_site_query('www.mozilla.org', 'browser', 'Web'),
     'Non-Brand'
   ),
   assert.equals(
-    google_search_console.classify_site_query('addons.mozilla.org', 'mozilla', 'Web'),
-    'Unknown'
+    google_search_console.classify_site_query('addons.mozilla.org', 'firefox', 'Web'),
+    'Firefox Brand'
   ),

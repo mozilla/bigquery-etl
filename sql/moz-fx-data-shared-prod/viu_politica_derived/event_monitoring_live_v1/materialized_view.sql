102c1cbc7c8028f7b9493fb2a7395ff291fcac8c
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-16 21:35:37 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/viu_politica_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/viu_politica_derived/event_monitoring_live_v1/materialized_view.sql
@@ -50,7 +50,7 @@ IF
         client_info.app_display_version AS version,
         ping_info
       FROM
-        `moz-fx-data-shared-prod.viu_politica_live.main_events_v1`
+        `moz-fx-data-shared-prod.viu_politica_live.video_index_v1`
       UNION ALL
       SELECT
         submission_timestamp,
@@ -60,7 +60,7 @@ IF
         client_info.app_display_version AS version,
         ping_info
       FROM
-        `moz-fx-data-shared-prod.viu_politica_live.video_index_v1`
+        `moz-fx-data-shared-prod.viu_politica_live.main_events_v1`
     )
   CROSS JOIN
     UNNEST(events) AS event,

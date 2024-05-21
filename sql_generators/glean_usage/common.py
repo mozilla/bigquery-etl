f6ea4f7be7655eb058dcc4b065f903bccc142bb6
kik-kik <42538694+kik-kik@users.noreply.github.com>
2024-05-21 12:49:04 +0200

    fix: baseline_clients_daily to only grab distribution_id for fenix app (#5614)

--- sql_generators/glean_usage/common.py
+++ sql_generators/glean_usage/common.py
@@ -36,6 +36,8 @@ NO_BASELINE_PING_APPS = (
     "mozregression",
 )
 
+APPS_WITH_DISTRIBUTION_ID = ("fenix",)
+
 
 def write_dataset_metadata(output_dir, full_table_id, derived_dataset_metadata=False):
     """
@@ -238,6 +240,7 @@ class GleanTable:
             project_id=project_id,
             derived_dataset=derived_dataset,
             app_name=app_name,
+            has_distribution_id=app_name in APPS_WITH_DISTRIBUTION_ID,
         )
 
         render_kwargs.update(self.custom_render_kwargs)

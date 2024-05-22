024a705a683f31ca8c9eab576d95210ef6c1e0c6
Wil Stuckey <wstuckey@mozilla.com>
2024-05-22 15:01:30 -0500

    Add flipkart and home_depot views

---
+++ sql/moz-fx-data-shared-prod/fakespot/flipkart_products/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fakespot.flipkart_products`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.fakespot_syndicate.flipkart_products`
---
+++ sql/moz-fx-data-shared-prod/fakespot/home_depot_products/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fakespot.home_depot_products`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.fakespot_syndicate.home_depot_products`

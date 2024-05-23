279709b434026a00bb5ff09e8ddfdd2618720630
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-05-23 08:10:01 -0500

    DENG-2975 - Update view.sql

--- sql/moz-fx-data-shared-prod/telemetry/active_users/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/active_users/view.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.active_users`
 AS
--- Firefox Desktop
 SELECT
   submission_date,
   client_id,
@@ -22,7 +21,6 @@ FROM
 WHERE
   submission_date < current_date
 UNION ALL
--- Fenix
 SELECT
   submission_date,
   client_id,
@@ -39,111 +37,6 @@ SELECT
   is_desktop,
   is_mobile
 FROM
-  `moz-fx-data-shared-prod.fenix.active_users`
-WHERE
-  submission_date < current_date
-UNION ALL
--- Firefox iOS
-SELECT
-  submission_date,
-  client_id,
-  sample_id,
-  app_name,
-  days_seen_bits,
-  days_active_bits,
-  is_dau,
-  is_wau,
-  is_mau,
-  is_daily_user,
-  is_weekly_user,
-  is_monthly_user,
-  is_desktop,
-  is_mobile
-FROM
-  `moz-fx-data-shared-prod.firefox_ios.active_users`
-WHERE
-  submission_date < current_date
-UNION ALL
--- Focus Android
-SELECT
-  submission_date,
-  client_id,
-  sample_id,
-  app_name,
-  days_seen_bits,
-  days_active_bits,
-  is_dau,
-  is_wau,
-  is_mau,
-  is_daily_user,
-  is_weekly_user,
-  is_monthly_user,
-  is_desktop,
-  is_mobile
-FROM
-  `moz-fx-data-shared-prod.focus_android.active_users`
-WHERE
-  submission_date < current_date
-UNION ALL
--- Focus iOS
-SELECT
-  submission_date,
-  client_id,
-  sample_id,
-  app_name,
-  days_seen_bits,
-  days_active_bits,
-  is_dau,
-  is_wau,
-  is_mau,
-  is_daily_user,
-  is_weekly_user,
-  is_monthly_user,
-  is_desktop,
-  is_mobile
-FROM
-  `moz-fx-data-shared-prod.focus_ios.active_users`
-WHERE
-  submission_date < current_date
-UNION ALL
--- Klar Android
-SELECT
-  submission_date,
-  client_id,
-  sample_id,
-  app_name,
-  days_seen_bits,
-  days_active_bits,
-  is_dau,
-  is_wau,
-  is_mau,
-  is_daily_user,
-  is_weekly_user,
-  is_monthly_user,
-  is_desktop,
-  is_mobile
-FROM
-  `moz-fx-data-shared-prod.klar_android.active_users`
-WHERE
-  submission_date < current_date
-UNION ALL
--- Klar iOS
-SELECT
-  submission_date,
-  client_id,
-  sample_id,
-  app_name,
-  days_seen_bits,
-  days_active_bits,
-  is_dau,
-  is_wau,
-  is_mau,
-  is_daily_user,
-  is_weekly_user,
-  is_monthly_user,
-  is_desktop,
-  is_mobile
-FROM
-  `moz-fx-data-shared-prod.klar_ios.active_users`
+  `moz-fx-data-shared-prod.telemetry.active_users_mobile`
 WHERE
   submission_date < current_date

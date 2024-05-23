2cd72654e00a410ad7aefb2a8b37c348aa11340b
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-23 16:40:39 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/focus_android/events/view.sql
+++ sql/moz-fx-data-shared-prod/focus_android/events/view.sql
@@ -22,25 +22,6 @@ FROM
   `moz-fx-data-shared-prod.org_mozilla_focus.events`
 UNION ALL
 SELECT
-  "org_mozilla_focus_beta" AS normalized_app_id,
-  "beta" AS normalized_channel,
-  additional_properties,
-  client_info,
-  document_id,
-  events,
-  metadata,
-  metrics,
-  normalized_app_name,
-  normalized_country_code,
-  normalized_os,
-  normalized_os_version,
-  ping_info,
-  sample_id,
-  submission_timestamp
-FROM
-  `moz-fx-data-shared-prod.org_mozilla_focus_beta.events`
-UNION ALL
-SELECT
   "org_mozilla_focus_nightly" AS normalized_app_id,
   "nightly" AS normalized_channel,
   additional_properties,

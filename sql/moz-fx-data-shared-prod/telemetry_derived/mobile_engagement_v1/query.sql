f917857249f57ccd782373cb0b8ff2855a3e824d
Katie Windau <kwindau@mozilla.com>
2024-05-23 09:24:00 -0500

    DENG-3288 add is desktop, is mobile to agg table

--- sql/moz-fx-data-shared-prod/telemetry_derived/mobile_engagement_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/mobile_engagement_v1/query.sql
@@ -16,6 +16,8 @@ SELECT
   meta_attribution_app,
   install_source,
   is_suspicious_device_client,
+  is_desktop,
+  is_mobile,
   COUNTIF(is_dau) AS dau,
   COUNTIF(is_wau) AS wau,
   COUNTIF(is_mau) AS mau
@@ -40,4 +42,6 @@ GROUP BY
   play_store_attribution_medium,
   meta_attribution_app,
   install_source,
-  is_suspicious_device_client
+  is_suspicious_device_client,
+  is_desktop,
+  is_mobile

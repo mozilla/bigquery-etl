efbe3dc284ef51f6051f775b645dcf72cfd8e752
Katie Windau <kwindau@mozilla.com>
2024-05-23 11:35:20 -0500

    remove cls alias

--- sql/moz-fx-data-shared-prod/telemetry/active_users_desktop/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/active_users_desktop/view.sql
@@ -9,21 +9,21 @@ SELECT
   days_seen_bits,
   days_active_bits,
   CASE
-    WHEN cls.isp_name = 'BrowserStack'
-      THEN CONCAT(cls.app_name, ' ', cls.isp_name)
+    WHEN isp_name = 'BrowserStack'
+      THEN CONCAT(app_name, ' ', cls.isp_name)
     WHEN distribution_id = 'MozillaOnline'
-      THEN CONCAT(cls.app_name, ' ', cls.distribution_id)
+      THEN CONCAT(app_name, ' ', distribution_id)
     ELSE app_name
   END AS app_name,
-  IFNULL(mozfun.bits28.days_since_seen(cls.days_active_bits) = 0, FALSE) AS is_dau,
-  IFNULL(mozfun.bits28.days_since_seen(cls.days_active_bits) < 7, FALSE) AS is_wau,
-  IFNULL(mozfun.bits28.days_since_seen(cls.days_active_bits) < 28, FALSE) AS is_mau,
-  IFNULL(mozfun.bits28.days_since_seen(cls.days_seen_bits) = 0, FALSE) AS is_daily_user,
-  IFNULL(mozfun.bits28.days_since_seen(cls.days_seen_bits) < 7, FALSE) AS is_weekly_user,
-  IFNULL(mozfun.bits28.days_since_seen(cls.days_seen_bits) < 28, FALSE) AS is_monthly_user,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) = 0, FALSE) AS is_dau,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 7, FALSE) AS is_wau,
+  IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 28, FALSE) AS is_mau,
+  IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) = 0, FALSE) AS is_daily_user,
+  IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) < 7, FALSE) AS is_weekly_user,
+  IFNULL(mozfun.bits28.days_since_seen(days_seen_bits) < 28, FALSE) AS is_monthly_user,
   IF(
-    LOWER(IFNULL(cls.isp_name, '')) <> "browserstack"
-    AND LOWER(IFNULL(cls.distribution_id, '')) <> "mozillaonline",
+    LOWER(IFNULL(isp_name, '')) <> "browserstack"
+    AND LOWER(IFNULL(distribution_id, '')) <> "mozillaonline",
     TRUE,
     FALSE
   ) AS is_desktop

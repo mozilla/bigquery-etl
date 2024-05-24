9d1d9176f6c9998b341842db780e3b26eabc5250
Vanessa Sabino <vsabino@mozilla.com>
2024-05-24 17:25:18 -0400

    Add is_desktop to engagement and retention tables (#5658)
    * add is_desktop to engagement and retention tables
    
    * replace clients_last_seen by active_users
    
    * format
    
    * remove unused normalized_channel

--- sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_clients_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_engagement_clients_v1/query.sql
@@ -22,6 +22,7 @@ SELECT
   ) AS normalized_os_version,
   cls.startup_profile_selection_reason_first AS startup_profile_selection_reason,
   cls.country,
+  aud.is_desktop,
   aud.is_dau,
   aud.is_wau,
   aud.is_mau

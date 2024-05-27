01118a87ac88e75f352c27314c2e399339431b0d
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-05-27 15:46:17 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/org_mozilla_fenix_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_fenix_derived/event_monitoring_live_v1/materialized_view.sql
@@ -60,7 +60,7 @@ IF
   LEFT JOIN
     UNNEST(event.extra) AS event_extra
   WHERE
-    DATE(submission_timestamp) >= "2024-05-24"
+    DATE(submission_timestamp) >= "2024-05-27"
   GROUP BY
     submission_date,
     window_start,
--- sql/moz-fx-data-shared-prod/org_mozilla_firefoxreality_derived/events_stream_v1/query.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_firefoxreality_derived/events_stream_v1/query.sql
@@ -1,7 +1,73 @@
 -- Generated via bigquery_etl.glean_usage
+-- convert array of key value pairs to a json object, cast numbers and booleans if possible
+CREATE TEMP FUNCTION from_map_event_extra(input ARRAY<STRUCT<key STRING, value STRING>>)
+RETURNS json AS (
+  IF(
+    ARRAY_LENGTH(input) = 0,
+    NULL,
+    JSON_OBJECT(
+      ARRAY(SELECT key FROM UNNEST(input)),
+      ARRAY(
+        SELECT
+          CASE
+            WHEN SAFE_CAST(value AS NUMERIC) IS NOT NULL
+              THEN TO_JSON(SAFE_CAST(value AS NUMERIC))
+            WHEN SAFE_CAST(value AS BOOL) IS NOT NULL
+              THEN TO_JSON(SAFE_CAST(value AS BOOL))
+            ELSE TO_JSON(value)
+          END
+        FROM
+          UNNEST(input)
+      )
+    )
+  )
+);
+
+-- convert array of key value pairs to a json object
+-- values are nested structs and will be converted to json objects
+CREATE TEMP FUNCTION from_map_experiment(
+  input ARRAY<
+    STRUCT<key STRING, value STRUCT<branch STRING, extra STRUCT<type STRING, enrollment_id STRING>>>
+  >
+)
+RETURNS json AS (
+  IF(
+    ARRAY_LENGTH(input) = 0,
+    NULL,
+    JSON_OBJECT(ARRAY(SELECT key FROM UNNEST(input)), ARRAY(SELECT value FROM UNNEST(input)))
+  )
+);
+
+CREATE TEMP FUNCTION metrics_to_json(metrics JSON)
+RETURNS JSON AS (
+  JSON_STRIP_NULLS(
+    JSON_REMOVE(
+      -- labeled_* are the only ones that SHOULD show up as context for events pings,
+      -- thus we special-case them
+      --
+      -- The JSON_SET/JSON_EXTRACT shenanigans are needed
+      -- because those subfields might not exist, so accessing the columns would fail.
+      -- but accessing non-existent fields in a JSON object simply gives us NULL.
+      JSON_SET(
+        metrics,
+        '$.labeled_counter',
+        mozfun.json.from_nested_map(metrics.labeled_counter),
+        '$.labeled_string',
+        mozfun.json.from_nested_map(metrics.labeled_string),
+        '$.labeled_boolean',
+        mozfun.json.from_nested_map(metrics.labeled_boolean),
+        '$.url',
+        metrics.url2
+      ),
+      '$.url2'
+    ),
+    remove_empty => TRUE
+  )
+);
+
 WITH base AS (
   SELECT
-    * EXCEPT (metrics, events, name, category, extra, timestamp) REPLACE(
+    * REPLACE (
       STRUCT(
         client_info.app_build AS app_build,
         client_info.app_channel AS app_channel,
@@ -25,24 +91,14 @@ WITH base AS (
         ping_info.end_time,
         ping_info.parsed_end_time,
         ping_info.ping_type
-      ) AS ping_info
+      ) AS ping_info,
+      metrics_to_json(TO_JSON(metrics)) AS metrics
     ),
     client_info.client_id AS client_id,
     ping_info.reason AS reason,
-    `mozfun.json.from_map`(ping_info.experiments) AS experiments,
-    COALESCE(
-      SAFE.TIMESTAMP_MILLIS(SAFE_CAST(mozfun.map.get_key(event.extra, 'glean_timestamp') AS INT64)),
-      SAFE.TIMESTAMP_ADD(ping_info.parsed_start_time, INTERVAL event.timestamp MILLISECOND)
-    ) AS event_timestamp,
-    event.category AS event_category,
-    event.name AS event_name,
-    ARRAY_TO_STRING([event.category, event.name], '.') AS event, -- handles NULL values better
-    `mozfun.json.from_map`(event.extra) AS event_extra,
-    TO_JSON(metrics) AS metrics
+    from_map_experiment(ping_info.experiments) AS experiments,
   FROM
-    `org_mozilla_firefoxreality.events` AS e
-  CROSS JOIN
-    UNNEST(events) AS event
+    `org_mozilla_firefoxreality.events`
   WHERE
     {% if is_init() %}
       DATE(submission_timestamp) >= '2023-11-01'
@@ -52,34 +108,16 @@ WITH base AS (
 )
 --
 SELECT
-  * REPLACE (
-    -- expose as easy to access JSON column,
-    -- strip nulls,
-    -- translate nested array records into a JSON object,
-    -- rename url2 -> url
-    JSON_STRIP_NULLS(
-      JSON_REMOVE(
-        -- labeled_* are the only ones that SHOULD show up as context for events pings,
-        -- thus we special-case them
-        --
-        -- The JSON_SET/JSON_EXTRACT shenanigans are needed
-        -- because those subfields might not exist, so accessing the columns would fail.
-        -- but accessing non-existent fields in a JSON object simply gives us NULL.
-        JSON_SET(
-          metrics,
-          '$.labeled_counter',
-          mozfun.json.from_nested_map(metrics.labeled_counter),
-          '$.labeled_string',
-          mozfun.json.from_nested_map(metrics.labeled_string),
-          '$.labeled_boolean',
-          mozfun.json.from_nested_map(metrics.labeled_boolean),
-          '$.url',
-          metrics.url2
-        ),
-        '$.url2'
-      ),
-      remove_empty => TRUE
-    ) AS metrics
-  )
+  base.* EXCEPT (events),
+  COALESCE(
+    SAFE.TIMESTAMP_MILLIS(SAFE_CAST(mozfun.map.get_key(event.extra, 'glean_timestamp') AS INT64)),
+    SAFE.TIMESTAMP_ADD(ping_info.parsed_start_time, INTERVAL event.timestamp MILLISECOND)
+  ) AS event_timestamp,
+  event.category AS event_category,
+  event.name AS event_name,
+  ARRAY_TO_STRING([event.category, event.name], '.') AS event, -- handles NULL values better
+  from_map_event_extra(event.extra) AS event_extra,
 FROM
   base
+CROSS JOIN
+  UNNEST(events) AS event

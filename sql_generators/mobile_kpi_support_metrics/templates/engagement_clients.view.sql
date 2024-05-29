5b23aa1349b86551e510c3ef3e89b21c9f72a0e9
kik-kik <42538694+kik-kik@users.noreply.github.com>
2024-05-29 15:49:50 +0200

    feat: move mobile engagement logic into mobile kpi support metrics generator (#5661)
    * feat: move mobile_engagement SQL logic into mobile_kpi_support_metrics generator
    
    * feat: remove mobile_engagement queries from telemetry and telemetry_erived
    
    * feat: apply formatting to engagement_clients view and add checks template
    
    * feat: add engagement view template and add engagement templates to the generation logic
    
    * bug: fix engagement query errors
    
    * feat: sneak in removing of retention views inside telemetry (mobile kpi generator will overwrite these anyways)
    
    * feat: remove bqetl_mobile_engagement_model DAG from DAG list as no longer needed.
    
    * feat: fix an engagement_v1 query template and removed unecessary comment from the metadata template
    
    * fix: update engagement templates to ensure valid query is rendered

---
+++ sql_generators/mobile_kpi_support_metrics/templates/engagement_clients.view.sql
@@ -0,0 +1,88 @@
+{{ header }}
+CREATE OR REPLACE VIEW
+  `{{ project_id }}.{{ dataset }}.{{ name }}`
+AS
+WITH active_users AS (
+  SELECT
+    submission_date,
+    client_id,
+    sample_id,
+    first_seen_date,
+    app_name,
+    normalized_channel,
+    locale,
+    country,
+    isp,
+    app_display_version,
+    is_dau,
+    is_wau,
+    is_mau,
+    is_mobile,
+  FROM
+    `{{ project_id }}.{{ dataset }}.active_users`
+),
+attribution AS (
+  SELECT
+    client_id,
+    sample_id,
+    channel AS normalized_channel,
+    adjust_ad_group,
+    adjust_creative,
+    adjust_network,
+    {% if app_name == "fenix" %}
+      CASE
+        WHEN adjust_network IN ('Google Organic Search', 'Organic')
+          THEN ''
+        ELSE adjust_campaign
+      END AS adjust_campaign,
+    {% else %}
+      adjust_campaign,
+    {% endif %}
+    {% for field in product_specific_attribution_fields %}
+      {{ field.name if field.name != "adjust_campaign" }},
+    {% endfor %}
+  FROM
+    {% if app_name == "fenix" %}
+      `{{ project_id }}.{{ dataset }}_derived.firefox_android_clients_v1`
+    {% elif app_name == "firefox_ios" %}
+      `{{ project_id }}.{{ dataset }}_derived.firefox_ios_clients_v1`
+    {% endif %}
+)
+SELECT
+  submission_date,
+  client_id,
+  sample_id,
+  first_seen_date,
+  app_name,
+  normalized_channel,
+  app_display_version AS app_version,
+  locale,
+  country,
+  isp,
+  is_dau,
+  is_wau,
+  is_mau,
+  is_mobile,
+  attribution.adjust_ad_group,
+  attribution.adjust_campaign,
+  attribution.adjust_creative,
+  attribution.adjust_network,
+  {% for field in product_specific_attribution_fields %}
+    attribution.{{ field.name }},
+  {% endfor %}
+  CASE
+    WHEN active_users.submission_date = first_seen_date
+      THEN 'new_profile'
+    WHEN DATE_DIFF(active_users.submission_date, first_seen_date, DAY)
+      BETWEEN 1
+      AND 27
+      THEN 'repeat_user'
+    WHEN DATE_DIFF(active_users.submission_date, first_seen_date, DAY) >= 28
+      THEN 'existing_user'
+    ELSE 'Unknown'
+  END AS lifecycle_stage,
+FROM
+  active_users
+LEFT JOIN
+  attribution
+  USING (client_id, sample_id, normalized_channel)

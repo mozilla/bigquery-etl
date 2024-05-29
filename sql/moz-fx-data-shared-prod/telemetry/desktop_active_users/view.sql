4d98c82afab802de26753569e3a74f9e52a1f5d8
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2024-05-29 20:21:56 +0200

    Update desktop active_users view to make dimensions available for KPIs. (#5675)
    * Use desktop active_users view to make dimensions available for KPIs.
    
    * Naming adjustment

--- sql/moz-fx-data-shared-prod/telemetry/desktop_active_users/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/desktop_active_users/view.sql
@@ -5,6 +5,23 @@ SELECT
   submission_date,
   client_id,
   sample_id,
+  CASE
+    WHEN BIT_COUNT(days_active_bits)
+      BETWEEN 1
+      AND 6
+      THEN 'infrequent_user'
+    WHEN BIT_COUNT(days_active_bits)
+      BETWEEN 7
+      AND 13
+      THEN 'casual_user'
+    WHEN BIT_COUNT(days_active_bits)
+      BETWEEN 14
+      AND 20
+      THEN 'regular_user'
+    WHEN BIT_COUNT(days_active_bits) >= 21
+      THEN 'core_user'
+    ELSE 'other'
+  END AS activity_segment,
   days_seen_bits,
   days_active_bits,
   CASE
@@ -14,6 +31,22 @@ SELECT
       THEN CONCAT(app_name, ' ', distribution_id)
     ELSE app_name
   END AS app_name,
+  app_version,
+  normalized_channel,
+  country,
+  city,
+  locale,
+  first_seen_date,
+  os,
+  normalized_os_version,
+  windows_build_number,
+  scalar_parent_browser_engagement_total_uri_count_normal_and_private_mode_sum,
+  scalar_parent_browser_engagement_total_uri_count_sum,
+  is_default_browser,
+  distribution_id,
+  active_hours_sum,
+  attribution.source AS attribution_source,
+  attribution.medium AS attribution_medium,
   IFNULL(mozfun.bits28.days_since_seen(days_active_bits) = 0, FALSE) AS is_dau,
   IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 7, FALSE) AS is_wau,
   IFNULL(mozfun.bits28.days_since_seen(days_active_bits) < 28, FALSE) AS is_mau,

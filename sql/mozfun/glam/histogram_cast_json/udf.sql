c78c3e231a1aaeae604a74b48d1db129565360a8
Eduardo Filho <edugomfilho@gmail.com>
2024-05-30 19:51:25 -0400

    GLAM - Rewrite JS UDFs in SQL

--- sql/mozfun/glam/histogram_cast_json/udf.sql
+++ sql/mozfun/glam/histogram_cast_json/udf.sql
@@ -2,21 +2,30 @@
 CREATE OR REPLACE FUNCTION glam.histogram_cast_json(
   histogram ARRAY<STRUCT<key STRING, value FLOAT64>>
 )
-RETURNS STRING DETERMINISTIC
-LANGUAGE js
-AS
-  '''
-    let obj = {};
-    histogram.map(r => {
-        obj[r.key] = parseFloat(r.value.toFixed(4));
-    });
-    return JSON.stringify(obj);
-''';
+RETURNS STRING
+AS (
+  IF(
+    ARRAY_LENGTH(histogram) = 0,
+    "{}",
+    TO_JSON_STRING(
+      JSON_OBJECT(
+        ARRAY(SELECT key FROM UNNEST(histogram)),
+        ARRAY(SELECT ROUND(value, 4) FROM UNNEST(histogram))
+      )
+    )
+  )
+);
 
 SELECT
   assert.equals(
-    '{"0":0.1111,"1":0.6667,"2":0}',
+    '{"0":0.1111,"1":0.6667,"2":0.0}',
     glam.histogram_cast_json(
       ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 0.111111), ("1", 2.0 / 3), ("2", 0)]
     )
-  )
+  ),
+  assert.equals(
+    '{}',
+    glam.histogram_cast_json(
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[]
+    )
+  ),
--- sql/mozfun/glam/percentile/udf.sql
+++ sql/mozfun/glam/percentile/udf.sql
@@ -1,43 +1,57 @@
 -- udf_js.glean_percentile
 CREATE OR REPLACE FUNCTION glam.percentile(
-  percentile FLOAT64,
+  pct FLOAT64,
   histogram ARRAY<STRUCT<key STRING, value FLOAT64>>,
   type STRING
 )
-RETURNS FLOAT64 DETERMINISTIC
-LANGUAGE js
-AS
-  '''
-  if (percentile < 0 || percentile > 100) {
-      throw "percentile must be a value between 0 and 100";
-  }
-
-  let values = histogram.map(bucket => bucket.value);
-  let total = values.reduce((a, b) => a + b);
-  let normalized = values.map(value => value / total);
-
-  // Find the index into the cumulative distribution function that corresponds
-  // to the percentile. This undershoots the true value of the percentile.
-  let acc = 0;
-  let index = null;
-  for (let i = 0; i < normalized.length; i++) {
-      acc += normalized[i];
-      index = i;
-      if (acc >= percentile / 100) {
-          break;
-      }
-  }
-
-  // NOTE: we do not perform geometric or linear interpolation, but this would
-  // be the place to implement it.
-  return histogram[index].key;
-''';
-
+RETURNS FLOAT64
+AS (
+  (
+    WITH
+      check AS (
+        SELECT IF(pct >= 0 AND pct <= 100, TRUE, ERROR('percentile must be a value between 0 and 100')) pct_ok
+      ),
+      keyed_cum_sum AS (
+        SELECT
+          key,
+          SUM(value) OVER (ORDER BY key) / SUM(value) OVER () AS cum_sum
+        FROM
+          UNNEST(histogram)
+      )
+    SELECT
+      CAST(key AS FLOAT64)
+    FROM
+      keyed_cum_sum,
+      check
+    WHERE
+      check.pct_ok
+      AND cum_sum >= pct / 100
+    ORDER BY
+      cum_sum
+    LIMIT 1
+  )
+);
 SELECT
   assert.equals(
     2,
     glam.percentile(
       50.0,
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("2", 2), ("3", 1)],
+      "timing_distribution"
+    )
+  ),
+  assert.equals(
+    3,
+    glam.percentile(
+      100.0,
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("2", 2), ("3", 1)],
+      "timing_distribution"
+    )
+  ),
+  assert.equals(
+    0,
+    glam.percentile(
+      0.0,
       ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 1), ("2", 2), ("3", 1)],
       "timing_distribution"
     )

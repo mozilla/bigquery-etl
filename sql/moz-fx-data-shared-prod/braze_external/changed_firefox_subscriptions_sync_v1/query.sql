afbce8076550ebef0ecd4b34cffc62802b1082c5
Chelsey Beck <cbeck@mozilla.com>
2024-05-31 12:56:10 -0700

    updating firefox subscriptions query

--- sql/moz-fx-data-shared-prod/braze_external/changed_firefox_subscriptions_sync_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_external/changed_firefox_subscriptions_sync_v1/query.sql
@@ -1,15 +1,25 @@
--- Construct the JSON payload in Braze required format
-WITH max_timestamps AS (
+-- CTE to determine the maximum update timestamp from changed_subscriptions_v1
+WITH max_update AS (
   SELECT
-    subscriptions.external_id,
-    MAX(subscriptions_array.update_timestamp) AS max_update_timestamp
+    MAX(
+      CAST(JSON_EXTRACT_SCALAR(payload, '$.update_timestamp') AS TIMESTAMP)
+    ) AS latest_subscription_updated_at
   FROM
-    `moz-fx-data-shared-prod.braze_external.changed_subscriptions_v1` AS subscriptions
+    `moz-fx-data-shared-prod.braze_external.changed_firefox_subscriptions_sync_v1`
+),
+-- CTE to get the max update timestamp for each external_id in subscriptions_v1
+max_subscriptions AS (
+  SELECT
+    external_id,
+    MAX(subscriptions.update_timestamp) AS update_timestamp
+  FROM
+    `moz-fx-data-shared-prod.braze_derived.subscriptions_v1`
   CROSS JOIN
-    UNNEST(subscriptions.subscriptions) AS subscriptions_array
+    UNNEST(subscriptions) AS subscriptions
   GROUP BY
-    subscriptions.external_id
+    external_id
 )
+-- Main query to select all records from subscriptions_v1 that have been updated since the last sync
 SELECT
   CURRENT_TIMESTAMP() AS UPDATED_AT,
   subscriptions.external_id AS EXTERNAL_ID,
@@ -23,16 +33,18 @@ SELECT
         ORDER BY
           subscriptions_array.update_timestamp DESC
       ) AS subscription_groups,
-      max_timestamps.max_update_timestamp AS update_timestamp
+      max_subscriptions.update_timestamp
     )
   ) AS PAYLOAD
 FROM
-  `moz-fx-data-shared-prod.braze_external.changed_subscriptions_v1` AS subscriptions
+  `moz-fx-data-shared-prod.braze_derived.subscriptions_v1` AS subscriptions
 CROSS JOIN
   UNNEST(subscriptions.subscriptions) AS subscriptions_array
 JOIN
-  max_timestamps
-  ON subscriptions.external_id = max_timestamps.external_id
+  max_subscriptions
+  ON subscriptions.external_id = max_subscriptions.external_id
+WHERE
+  subscriptions_array.update_timestamp > (SELECT latest_subscription_updated_at FROM max_update)
 GROUP BY
   subscriptions.external_id,
-  max_timestamps.max_update_timestamp;
+  max_subscriptions.update_timestamp

5d43df11adb45144501ceb0a52c5f7df1397bf35
Chelsey Beck <cbeck@mozilla.com>
2024-05-31 11:32:54 -0700

    removing checks no longer needed

--- sql/moz-fx-data-shared-prod/braze_derived/waitlists_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/waitlists_v1/checks.sql
@@ -1,19 +1,4 @@
--- raw SQL checks
--- checking to see if there is new data since the last run
--- if not, fail or we will have blank sync tables
-
-#fail
-ASSERT(
-  SELECT
-    COUNT(1)
-  FROM
-    `moz-fx-data-shared-prod.braze_derived.waitlists_v1`,
-    UNNEST(waitlists) AS waitlists
-  WHERE
-    waitlists.update_timestamp > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 HOUR)
-) > 0
-AS
-  "No new records in the braze_derived.waitlists_v1 table in the last 7 hours";
+-- macro checks
 
 #fail
 {{ not_null(["external_id"]) }} -- to do: add array values

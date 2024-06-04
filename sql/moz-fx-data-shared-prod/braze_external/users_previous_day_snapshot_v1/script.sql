3608835d93c8d4b4099bd6fb35815b17f25ec3f2
Chelsey Beck <64881557+chelseybeck@users.noreply.github.com>
2024-06-04 09:38:08 -0700

    Update sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>

--- sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
+++ sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
@@ -2,5 +2,5 @@ DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_s
 
 CREATE SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_snapshot_v1` CLONE `moz-fx-data-shared-prod.braze_derived.users_v1` FOR SYSTEM_TIME AS OF TIMESTAMP_SUB(
   CURRENT_TIMESTAMP(),
-  INTERVAL {{ snapshot_interval }}
+  INTERVAL snapshot_interval HOUR
 );

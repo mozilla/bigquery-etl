c2fb92f7d944eb27895c0ee0d8b502d42425f66d
Katie Windau <kwindau@mozilla.com>
2024-06-04 07:36:46 -0500

    GROWTH-143 clean up query and schema

--- sql/moz-fx-data-shared-prod/google_ads_derived/conversion_event_categorization_v1/query.sql
+++ sql/moz-fx-data-shared-prod/google_ads_derived/conversion_event_categorization_v1/query.sql
@@ -1,10 +1,7 @@
-DECLARE first_cohort_date DATE DEFAULT DATE(2023, 11, 1);
-
-DECLARE last_cohort_date DATE DEFAULT DATE_SUB(
-  CURRENT_DATE,
-  INTERVAL 8 DAY
-); -- want to make sure the last cohort we are reporting has had their full first 7 days
-WITH clients_first_seen AS (
+--STEP 1: Get clients with a first seen date = submission date - 14 days
+--Note: Min cohort date is 2023-11-01 so backfilling will return nothing before then
+--Note: Max cohort date cannot be more than 7 days ago (to ensure we always have at least 7 days of data)
+WITH clients_first_seen_14_days_ago AS (
   SELECT
     client_id,
     first_seen_date,
@@ -17,29 +14,30 @@ WITH clients_first_seen AS (
   FROM
     `moz-fx-data-shared-prod.telemetry.clients_first_seen` --contains all new clients, including those that never sent a main ping
   WHERE
-    first_seen_date = DATE_SUB(@submission_date, INTERVAL 28 DAY)
+    first_seen_date = DATE_SUB(@submission_date, INTERVAL 14 DAY)
     AND first_seen_date
-    BETWEEN first_cohort_date
-    AND last_cohort_date
+    BETWEEN '2023-11-01'
+    AND DATE_SUB(CURRENT_DATE, INTERVAL 8 DAY)
 ),
-clients_last_seen AS (
+--STEP 2: For every client, get the first 7 days worth of main pings sent after their first main ping
+client_activity_first_7_days AS (
   SELECT
     client_id,
-  -- note that this is the date we got each client's first main ping, which is potentially a different date from the first_seen_date above.
-    ANY_VALUE(first_seen_date) AS first_seen_date,
-  -- just the country value from their first main ping day
-    ANY_VALUE(CASE WHEN first_seen_date = submission_date THEN country END) AS country,
-  -- the date we would report on their conversion events
     ANY_VALUE(
-      IF(submission_date = DATE_ADD(first_seen_date, INTERVAL 28 DAY), submission_date, NULL)
-    ) AS report_date,
-  -- their first week days of use, taken from their 7th day data
+      first_seen_date
+    ) AS first_seen_date, --date we got first main ping (potentially different than above first seen date)
+    ANY_VALUE(
+      CASE
+        WHEN first_seen_date = submission_date
+          THEN country
+      END
+    ) AS country, --any country from their first 7 days of main pings
     ANY_VALUE(
       CASE
         WHEN submission_date = DATE_ADD(first_seen_date, INTERVAL 6 DAY)
           THEN BIT_COUNT(days_visited_1_uri_bits & days_interacted_bits)
       END
-    ) AS dou,
+    ) AS dou, --total # of days of activity during their first 7 days of main pings
   -- if a client doesn't send a ping on `submission_date` their last active day's value will be carried forward
   -- so we only take measurements from days that they send a ping.
     SUM(
@@ -59,7 +57,7 @@ clients_last_seen AS (
   FROM
     `moz-fx-data-shared-prod.telemetry.clients_last_seen`
   WHERE
-    submission_date >= first_cohort_date
+    submission_date >= '2023-11-01' --first cohort date
     AND submission_date
     BETWEEN first_seen_date
     AND DATE_ADD(first_seen_date, INTERVAL 6 DAY)
@@ -69,54 +67,43 @@ clients_last_seen AS (
 combined AS (
   SELECT
     client_id,
-    -- we should use their first_seen_date from clients_first_seen as their cohort date
     cfs.first_seen_date,
     cfs.attribution_campaign,
     cfs.attribution_content,
     cfs.attribution_dltoken,
     cfs.attribution_medium,
     cfs.attribution_source,
+    @submission_date AS report_date,
+    IF(cls.first_seen_date IS NOT NULL, TRUE, FALSE) AS sent_main_ping_in_first_7_days,
     COALESCE(
-      cls.report_date,
-      IF(
-        (cls.report_date IS NULL)
-        AND (cls.first_seen_date IS NULL)
-        AND (DATE_DIFF(CURRENT_DATE, cfs.first_seen_date, DAY) >= 27),
-        DATE_ADD(cfs.first_seen_date, INTERVAL 27 DAY),
-        NULL
-      ) AS report_date,
-    -- not a strictly necessary field, used to see how many clients actually send a main ping
-      IF(cls.first_seen_date IS NOT NULL, TRUE, FALSE) AS sent_main_ping_in_first_7_days,
-    -- because the conversion events and ltv are based on their first observed country in CLS, use that country if its available.
-      COALESCE(cls.country, cfs.country) AS country,
-      COALESCE(dou, 0) AS dou,
-      COALESCE(active_hours_sum, 0) AS active_hours_sum,
-      COALESCE(search_with_ads_count_all, 0) AS search_with_ads_count_all
-      FROM
-        cfs
-      LEFT JOIN
-        cls
-        USING (client_id)
-    )
-  SELECT
-    client_id,
-    first_seen_date,
-    attribution_campaign,
-    attribution_content,
-    attribution_dltoken,
-    attribution_medium,
-    attribution_source,
-    report_date,
-    sent_main_ping_in_first_7_days,
-    country,
-    dou,
-    active_hours_sum,
-    search_with_ads_count_all,
-  --"strictest" event
-    IF(report_date IS NOT NULL, (search_with_ads_count_all > 0) AND (dou >= 5), NULL) AS event_1,
-  -- "medium" event
-    IF(report_date IS NOT NULL, (search_with_ads_count_all > 0) AND (dou >= 3), NULL) AS event_2,
-  -- "most_lenient" event
-    IF(report_date IS NOT NULL, (active_hours_sum >= 0.4) AND (dou >= 3), NULL) AS event_3,
+      cls.country,
+      cfs.country
+    ) AS country, -- Conversion events & LTV are based on their first observed country in CLS, use that country if its available
+    COALESCE(dou, 0) AS dou,
+    COALESCE(active_hours_sum, 0) AS active_hours_sum,
+    COALESCE(search_with_ads_count_all, 0) AS search_with_ads_count_all
   FROM
-    combined
+    clients_first_seen_14_days_ago AS cfs
+  LEFT JOIN
+    client_activity_first_7_days AS cls
+    USING (client_id)
+)
+SELECT
+  client_id,
+  first_seen_date,
+  attribution_campaign,
+  attribution_content,
+  attribution_dltoken,
+  attribution_medium,
+  attribution_source,
+  report_date,
+  sent_main_ping_in_first_7_days,
+  country,
+  dou,
+  active_hours_sum,
+  search_with_ads_count_all,
+  IF((search_with_ads_count_all > 0) AND (dou >= 5), TRUE, FALSE) AS event_1,
+  IF((search_with_ads_count_all > 0) AND (dou >= 3), TRUE, FALSE) AS event_2,
+  IF((active_hours_sum >= 0.4) AND (dou >= 3), TRUE, FALSE) AS event_3,
+FROM
+  combined

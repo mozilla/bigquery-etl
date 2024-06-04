f09c257384dd0db3b86b612f73bb4d8677c3955f
Chelsey Beck <cbeck@mozilla.com>
2024-06-04 09:22:50 -0700

    adding templating for config override

--- sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
+++ sql/moz-fx-data-shared-prod/braze_external/users_previous_day_snapshot_v1/script.sql
@@ -2,7 +2,5 @@ DROP SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_s
 
 CREATE SNAPSHOT TABLE `moz-fx-data-shared-prod.braze_external.users_previous_day_snapshot_v1` CLONE `moz-fx-data-shared-prod.braze_derived.users_v1` FOR SYSTEM_TIME AS OF TIMESTAMP_SUB(
   CURRENT_TIMESTAMP(),
-  INTERVAL 12 HOUR
+  INTERVAL {{ snapshot_interval }}
 );
-
-  -- Chose 12 hours here because it's split between when the job last ran and now (yesterday).

76de2d7e1ddf524540b0315c54abe3d1801921b1
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-06-05 18:14:59 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/monitoring_derived/event_monitoring_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/event_monitoring_aggregates_v1/query.sql
@@ -55,7 +55,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.firefox_desktop_stable.urlbar_potential_exposure_v1`
+      `moz-fx-data-shared-prod.firefox_desktop_stable.prototype_no_code_events_v1`
     UNION ALL
     SELECT
       submission_timestamp,
@@ -75,7 +75,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.firefox_desktop_stable.prototype_no_code_events_v1`
+      `moz-fx-data-shared-prod.firefox_desktop_stable.urlbar_potential_exposure_v1`
   )
 CROSS JOIN
   UNNEST(events) AS event,
@@ -1565,7 +1565,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.mozillavpn_stable.daemonsession_v1`
+      `moz-fx-data-shared-prod.mozillavpn_stable.main_v1`
     UNION ALL
     SELECT
       submission_timestamp,
@@ -1575,7 +1575,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.mozillavpn_stable.main_v1`
+      `moz-fx-data-shared-prod.mozillavpn_stable.daemonsession_v1`
   )
 CROSS JOIN
   UNNEST(events) AS event,
@@ -1656,7 +1656,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.org_mozilla_firefox_vpn_stable.daemonsession_v1`
+      `moz-fx-data-shared-prod.org_mozilla_firefox_vpn_stable.main_v1`
     UNION ALL
     SELECT
       submission_timestamp,
@@ -1666,7 +1666,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.org_mozilla_firefox_vpn_stable.main_v1`
+      `moz-fx-data-shared-prod.org_mozilla_firefox_vpn_stable.daemonsession_v1`
   )
 CROSS JOIN
   UNNEST(events) AS event,
@@ -1747,7 +1747,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_stable.daemonsession_v1`
+      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_stable.main_v1`
     UNION ALL
     SELECT
       submission_timestamp,
@@ -1757,7 +1757,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_stable.main_v1`
+      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_stable.daemonsession_v1`
   )
 CROSS JOIN
   UNNEST(events) AS event,
@@ -1838,7 +1838,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_network_extension_stable.daemonsession_v1`
+      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_network_extension_stable.main_v1`
     UNION ALL
     SELECT
       submission_timestamp,
@@ -1848,7 +1848,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_network_extension_stable.main_v1`
+      `moz-fx-data-shared-prod.org_mozilla_ios_firefoxvpn_network_extension_stable.daemonsession_v1`
   )
 CROSS JOIN
   UNNEST(events) AS event,
@@ -1990,7 +1990,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.bedrock_stable.non_interaction_v1`
+      `moz-fx-data-shared-prod.bedrock_stable.interaction_v1`
     UNION ALL
     SELECT
       submission_timestamp,
@@ -2000,7 +2000,7 @@ FROM
       client_info.app_display_version AS version,
       ping_info
     FROM
-      `moz-fx-data-shared-prod.bedrock_stable.interaction_v1`
+      `moz-fx-data-shared-prod.bedrock_stable.non_interaction_v1`
     UNION ALL
     SELECT
       submission_timestamp,

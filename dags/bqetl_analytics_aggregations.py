212192361d283d0d7de6b1cfcc801dad56d832a9
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-06-05 17:37:47 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_analytics_aggregations.py
+++ dags/bqetl_analytics_aggregations.py
@@ -54,20 +54,6 @@ with DAG(
     tags=tags,
 ) as dag:
 
-    wait_for_checks__fail_fenix_derived__firefox_android_clients__v1 = (
-        ExternalTaskSensor(
-            task_id="wait_for_checks__fail_fenix_derived__firefox_android_clients__v1",
-            external_dag_id="bqetl_analytics_tables",
-            external_task_id="checks__fail_fenix_derived__firefox_android_clients__v1",
-            execution_delta=datetime.timedelta(seconds=8100),
-            check_existence=True,
-            mode="reschedule",
-            allowed_states=ALLOWED_STATES,
-            failed_states=FAILED_STATES,
-            pool="DATA_ENG_EXTERNALTASKSENSOR",
-        )
-    )
-
     wait_for_checks__fail_telemetry_derived__unified_metrics__v1 = ExternalTaskSensor(
         task_id="wait_for_checks__fail_telemetry_derived__unified_metrics__v1",
         external_dag_id="bqetl_unified",
@@ -150,6 +136,20 @@ with DAG(
         allowed_states=ALLOWED_STATES,
         failed_states=FAILED_STATES,
         pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    wait_for_checks__fail_fenix_derived__firefox_android_clients__v1 = (
+        ExternalTaskSensor(
+            task_id="wait_for_checks__fail_fenix_derived__firefox_android_clients__v1",
+            external_dag_id="bqetl_analytics_tables",
+            external_task_id="checks__fail_fenix_derived__firefox_android_clients__v1",
+            execution_delta=datetime.timedelta(seconds=8100),
+            check_existence=True,
+            mode="reschedule",
+            allowed_states=ALLOWED_STATES,
+            failed_states=FAILED_STATES,
+            pool="DATA_ENG_EXTERNALTASKSENSOR",
+        )
     )
 
     wait_for_checks__fail_firefox_ios_derived__clients_activation__v1 = (
@@ -286,21 +286,6 @@ with DAG(
         allowed_states=ALLOWED_STATES,
         failed_states=FAILED_STATES,
         pool="DATA_ENG_EXTERNALTASKSENSOR",
-    )
-
-    active_users_aggregates_attribution_v1 = bigquery_etl_query(
-        task_id="active_users_aggregates_attribution_v1",
-        destination_table="active_users_aggregates_attribution_v1",
-        dataset_id="telemetry_derived",
-        project_id="moz-fx-data-shared-prod",
-        owner="lvargas@mozilla.com",
-        email=[
-            "gkaberere@mozilla.com",
-            "lvargas@mozilla.com",
-            "telemetry-alerts@mozilla.com",
-        ],
-        date_partition_parameter="submission_date",
-        depends_on_past=False,
     )
 
     active_users_aggregates_device_v1 = bigquery_etl_query(
@@ -726,14 +711,6 @@ with DAG(
         ],
         date_partition_parameter="submission_date",
         depends_on_past=False,
-    )
-
-    active_users_aggregates_attribution_v1.set_upstream(
-        wait_for_checks__fail_fenix_derived__firefox_android_clients__v1
-    )
-
-    active_users_aggregates_attribution_v1.set_upstream(
-        wait_for_checks__fail_telemetry_derived__unified_metrics__v1
     )
 
     active_users_aggregates_device_v1.set_upstream(

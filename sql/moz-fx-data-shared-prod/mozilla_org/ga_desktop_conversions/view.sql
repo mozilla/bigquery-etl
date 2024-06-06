aa17a62d96e9039d7c0d13150f81e00c11d594b5
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-06-06 16:22:38 -0500

    Update view.sql

--- sql/moz-fx-data-shared-prod/mozilla_org/ga_desktop_conversions/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org/ga_desktop_conversions/view.sql
@@ -2,9 +2,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.mozilla_org.ga_desktop_conversions`
 AS
 SELECT
-  activity_date AS activity_dt,
-  activity_datetime AS activity_date,
-  gclid,
-  conversion_name
+  *
 FROM
   `moz-fx-data-shared-prod.mozilla_org_derived.ga_desktop_conversions_v1`

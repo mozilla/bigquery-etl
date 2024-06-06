d4da730f928588891047e7247f9d0d9a09641fb7
Eduardo Filho <edugomfilho@gmail.com>
2024-06-06 13:49:06 -0400

    Fix histogram_cast_json

--- sql/mozfun/glam/histogram_cast_json/udf.sql
+++ sql/mozfun/glam/histogram_cast_json/udf.sql
@@ -1,25 +1,37 @@
--- udf_js_flatten
-CREATE OR REPLACE FUNCTION glam.histogram_cast_json(
-  histogram ARRAY<STRUCT<key STRING, value FLOAT64>>
-)
+/*
+Casts an ARRAY<STRUCT<key, value>> histogram to a JSON string.
+This implementation uses String concatenation instead of
+BigQuery native JSON (TO_JSON / JSON_OBJECT) functions to
+preserve order.
+https://cloud.google.com/bigquery/docs/reference/standard-sql/json_functions#json_object
+Order is important for GLAM histograms so other UDFs that
+operate on them, such as glam.percentile, can work correctly.
+*/
+CREATE OR REPLACE FUNCTION glam.histogram_cast_json(histogram ARRAY<STRUCT<key STRING, value FLOAT64>>)
 RETURNS STRING AS (
-  IF(
-    ARRAY_LENGTH(histogram) = 0,
-    "{}",
-    TO_JSON_STRING(
-      JSON_OBJECT(
-        ARRAY(SELECT key FROM UNNEST(histogram)),
-        ARRAY(SELECT ROUND(value, 4) FROM UNNEST(histogram))
+  (
+    SELECT
+      CONCAT(
+        '{',
+        STRING_AGG(CONCAT('"', key, '":', ROUND(value, 4)) ORDER BY CAST(key AS FLOAT64)),
+        '}'
       )
-    )
+    FROM
+      UNNEST(histogram)
   )
 );
 
 SELECT
   assert.equals(
-    '{"0":0.1111,"1":0.6667,"2":0.0}',
+    '{"0":0.1111,"1":0.6667,"2":0}',
     glam.histogram_cast_json(
       ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 0.111111), ("1", 2.0 / 3), ("2", 0)]
+    )
+  ),
+  assert.equals(
+    '{"0":0.1111,"1":0.6667,"2":0,"10":100}',
+    glam.histogram_cast_json(
+      ARRAY<STRUCT<key STRING, value FLOAT64>>[("0", 0.111111), ("1", 2.0 / 3), ("10", 100), ("2", 0)]
     )
   ),
   assert.equals('{}', glam.histogram_cast_json(ARRAY<STRUCT<key STRING, value FLOAT64>>[])),

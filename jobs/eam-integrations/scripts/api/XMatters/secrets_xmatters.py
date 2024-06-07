1d2e10715e8ffbd4553b5ba94b3ac1b495bccffe
JCMOSCON1976 <167822375+JCMOSCON1976@users.noreply.github.com>
2024-06-07 08:27:31 -0400

    Changed xm_password environment var name (#203)
    Co-authored-by: Julio Cezar Moscon <jcmoscon@gmail.com>

--- jobs/eam-integrations/scripts/api/XMatters/secrets_xmatters.py
+++ jobs/eam-integrations/scripts/api/XMatters/secrets_xmatters.py
@@ -4,7 +4,7 @@ config = {
     "proxies": {},
     "xm_client_id": os.environ.get("XMATTERS_CLIENT_ID", ""),
     "xm_username": os.environ.get("XMATTERS_USERNAME", ""),
-    "xm_password": os.environ.get("XMATTERS_PASSWORD", ""),
+    "xm_password": os.environ.get("XMATTERS_PASSWORD1", ""),
     "url": os.environ.get("XMATTERS_URL", ""),
     "supervisor_id": os.environ.get("XMATTERS_SUPERVISOR_ID", ""),
 }

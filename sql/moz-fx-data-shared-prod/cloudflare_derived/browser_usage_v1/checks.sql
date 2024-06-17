cf9083395700b93df2459a452a336d0812e8ace9
Katie Windau <kwindau@mozilla.com>
2024-06-17 12:16:51 -0500

    DENG-3096 fix QA check

--- sql/moz-fx-data-shared-prod/cloudflare_derived/browser_usage_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/cloudflare_derived/browser_usage_v1/checks.sql
@@ -1,2 +1,2 @@
 #warn
-{{ is_unique(["dte", "device_type", "user_type", "location", "browser", "operating_system"]) }}
+{{ is_unique(["dte", "device_type", "user_type", "location", "browser", "operating_system"], "dte = DATE_SUB(@date, INTERVAL 4 DAY)") }}

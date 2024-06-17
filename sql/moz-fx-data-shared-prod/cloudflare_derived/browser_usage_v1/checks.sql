efe863b4e8d7d0a07b21ae7870991933d016e7d2
Katie Windau <kwindau@mozilla.com>
2024-06-17 15:00:17 -0500

    DENG-3096 remove tests, do not work with a query.py file with parameters

--- sql/moz-fx-data-shared-prod/cloudflare_derived/browser_usage_v1/checks.sql
+++
@@ -1,2 +0,0 @@
-#warn
-{{ is_unique(["dte", "device_type", "user_type", "location", "browser", "operating_system"], "dte = DATE_SUB(@dte, INTERVAL 4 DAY)") }}
--- sql/moz-fx-data-shared-prod/cloudflare_derived/device_usage_v1/checks.sql
+++
@@ -1,2 +0,0 @@
-#warn
-{{ is_unique(["dte", "location", "user_type"], "dte = DATE_SUB(@dte, INTERVAL 4 DAY)") }}

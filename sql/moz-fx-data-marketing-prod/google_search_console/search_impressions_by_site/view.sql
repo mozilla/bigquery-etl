b876963ae0daa0fdfdcf4aa7cd9a20f25a0141fa
Sean Rose <srose@mozilla.com>
2024-06-20 17:29:51 -0700

    Temporarily add back Google Search Console views in `moz-fx-data-marketing-prod` to avoid breaking existing reporting.

---
+++ sql/moz-fx-data-marketing-prod/google_search_console/search_impressions_by_site/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-marketing-prod.google_search_console.search_impressions_by_site`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.google_search_console.search_impressions_by_site`

8d826d4abb0b401a573567f6bda28e6ca16bdf0f
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-06-24 21:25:57 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/fenix/bounce_tracking_protection/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/bounce_tracking_protection/view.sql
@@ -4,10 +4,9 @@ CREATE OR REPLACE VIEW
 AS
 SELECT
   "org_mozilla_firefox" AS normalized_app_id,
-  mozfun.norm.fenix_app_info(
-    "org_mozilla_firefox",
-    client_info.app_build
-  ).channel AS normalized_channel,
+    -- set app build to 21850000 since all affected pings are coming from versions older than that
+    -- abb build only used to differentiate between preview (pre 21850000) and nightly (everything after)
+  mozfun.norm.fenix_app_info("org_mozilla_firefox", '21850000').channel AS normalized_channel,
   additional_properties,
   document_id,
   events,
@@ -32,10 +31,9 @@ FROM
 UNION ALL
 SELECT
   "org_mozilla_firefox_beta" AS normalized_app_id,
-  mozfun.norm.fenix_app_info(
-    "org_mozilla_firefox_beta",
-    client_info.app_build
-  ).channel AS normalized_channel,
+    -- set app build to 21850000 since all affected pings are coming from versions older than that
+    -- abb build only used to differentiate between preview (pre 21850000) and nightly (everything after)
+  mozfun.norm.fenix_app_info("org_mozilla_firefox_beta", '21850000').channel AS normalized_channel,
   additional_properties,
   document_id,
   events,
@@ -60,10 +58,9 @@ FROM
 UNION ALL
 SELECT
   "org_mozilla_fenix" AS normalized_app_id,
-  mozfun.norm.fenix_app_info(
-    "org_mozilla_fenix",
-    client_info.app_build
-  ).channel AS normalized_channel,
+    -- set app build to 21850000 since all affected pings are coming from versions older than that
+    -- abb build only used to differentiate between preview (pre 21850000) and nightly (everything after)
+  mozfun.norm.fenix_app_info("org_mozilla_fenix", '21850000').channel AS normalized_channel,
   additional_properties,
   document_id,
   events,
@@ -88,10 +85,9 @@ FROM
 UNION ALL
 SELECT
   "org_mozilla_fenix_nightly" AS normalized_app_id,
-  mozfun.norm.fenix_app_info(
-    "org_mozilla_fenix_nightly",
-    client_info.app_build
-  ).channel AS normalized_channel,
+    -- set app build to 21850000 since all affected pings are coming from versions older than that
+    -- abb build only used to differentiate between preview (pre 21850000) and nightly (everything after)
+  mozfun.norm.fenix_app_info("org_mozilla_fenix_nightly", '21850000').channel AS normalized_channel,
   additional_properties,
   document_id,
   events,
@@ -116,10 +112,9 @@ FROM
 UNION ALL
 SELECT
   "org_mozilla_fennec_aurora" AS normalized_app_id,
-  mozfun.norm.fenix_app_info(
-    "org_mozilla_fennec_aurora",
-    client_info.app_build
-  ).channel AS normalized_channel,
+    -- set app build to 21850000 since all affected pings are coming from versions older than that
+    -- abb build only used to differentiate between preview (pre 21850000) and nightly (everything after)
+  mozfun.norm.fenix_app_info("org_mozilla_fennec_aurora", '21850000').channel AS normalized_channel,
   additional_properties,
   document_id,
   events,

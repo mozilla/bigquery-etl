3447f858114d7fa9b412d192da3c1ebffad4c54e
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-06-28 20:11:43 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_desktop/user_characteristics/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/user_characteristics/view.sql
@@ -3,11 +3,6 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.user_characteristics`
 AS
 SELECT
-  * REPLACE (
-    mozfun.norm.metadata(metadata) AS metadata,
-    mozfun.norm.glean_ping_info(ping_info) AS ping_info,
-    (SELECT AS STRUCT metrics.*, metrics.text2 AS text) AS metrics,
-    'Firefox' AS normalized_app_name
-  )
+  * REPLACE (mozfun.norm.metadata(metadata) AS metadata)
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_stable.user_characteristics_v1`

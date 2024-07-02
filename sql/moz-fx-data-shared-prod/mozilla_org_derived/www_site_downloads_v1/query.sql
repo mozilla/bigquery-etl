1959017265ddc6a0d79917b24f500ec9376dfd53
Katie Windau <kwindau@mozilla.com>
2024-07-02 17:36:50 -0500

    Add more GA3 tables in shared prod to prepare to remove from mktg prod

--- sql/moz-fx-data-shared-prod/mozilla_org_derived/www_site_downloads_v1/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org_derived/www_site_downloads_v1/query.sql
@@ -13,7 +13,7 @@ WITH download_event_count AS (
     browser,
     COUNTIF(event_action = 'Firefox Download') AS download_events,
   FROM
-    `moz-fx-data-shared-prod.ga_derived.www_site_hits_v1`
+    `moz-fx-data-shared-prod.mozilla_org_derived.www_site_hits_v1`
   WHERE
     date = @submission_date
     AND hit_type = 'EVENT'

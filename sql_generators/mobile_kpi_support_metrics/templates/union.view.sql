ecfd29c9cd2bd3a56df4f03cefab04834c9a075d
kik-kik <kignasiak@mozilla.com>
2024-07-04 17:23:48 +0200

    feat: move paid_vs_organic definition inside client level view

--- sql_generators/mobile_kpi_support_metrics/templates/union.view.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/union.view.sql
@@ -26,12 +26,6 @@ CREATE OR REPLACE VIEW
     CAST(NULL AS {{ field.type }}) AS {{ field.name }},
   {% endif %}
 {% endfor %}
-{% for field in product.all_possible_attribution_fields if field.exists and field.name == "adjust_network" %}
-  `moz-fx-data-shared-prod.udf.organic_vs_paid_mobile`(adjust_network) AS paid_vs_organic,
-{% else %}
-  "Organic" AS paid_vs_organic,
-{% endfor %}
-"{{ product.name }}" AS product_name,
 FROM
   `{{ project_id }}.{{ product.name }}.{{ name }}`
 {% endfor %}

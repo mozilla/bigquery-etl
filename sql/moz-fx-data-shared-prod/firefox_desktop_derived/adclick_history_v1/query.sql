4f8ecf8548dba1173b1b36495b4d556f6b8ba5b7
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-07-16 10:47:51 -0500

    Create new firefox_desktop_derived.adclick_history_v1 table and DAG (#5922)

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/query.sql
@@ -0,0 +1,28 @@
+WITH new_data AS (
+  SELECT
+    client_id,
+    sample_id,
+    ad_click AS ad_clicks
+  FROM
+    `moz-fx-data-shared-prod.search_derived.search_clients_daily_v8`
+  WHERE
+    submission_date = @submission_date
+    AND ad_click > 0
+),
+existing_data AS (
+  SELECT
+    client_id,
+    sample_id,
+    ad_click_history
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_derived.adclick_history_v1`
+)
+SELECT
+  client_id,
+  sample_id,
+  mozfun.map.set_key(ad_click_history, @submission_date, ad_clicks) AS ad_click_history
+FROM
+  new_data
+FULL OUTER JOIN
+  existing_data
+  USING (sample_id, client_id)

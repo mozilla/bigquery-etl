ae2a46aed35cd01404b78f0f1802b92978df3934
Katie Windau <kwindau@mozilla.com>
2024-07-16 16:42:45 -0500

    work in progress

--- sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
@@ -1,7 +1,24 @@
+DECLARE lookback INT64 DEFAULT 28;
+DECLARE death_time INT64 DEFAULT 168;
+DECLARE max_days INT64 DEFAULT 168;
+
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.ltv_states`
 AS
 SELECT
-  *
+a.client_id,
+a.sample_id,
+a.submission_date,
+a.first_seen_date,
+a.days_since_first_seen,
+a.days_since_active,
+a.first_reported_country, 
+a.attribution,
+a.active,
+a.ad_clicks,
+a.total_historic_ad_clicks
+BIT_COUNT(
+  `mozfun`.bytes.extract_bits(days_seen_bytes, - {{lookback}}, {{lookback}})
+  ) AS pattern,
 FROM
-  `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1`
+  `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1` a
--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/ltv_states_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/ltv_states_v1/query.sql
@@ -1,9 +1,42 @@
--- Query for firefox_desktop_derived.ltv_states_v1
-            -- For more information on writing queries see:
-            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+WITH base AS (
+  SELECT
+    cls.client_id,
+    cls.sample_id,
+    cls.submission_date,
+    DATE_DIFF(cls.submission_date, cfs.first_seen_date, DAY) AS days_since_first_seen, 
+    cfs.first_seen_date AS first_seen_date,
+    first_reported_country,
+    cfs.attribution,
+    `moz-fx-data-shared-prod`.udf.bits_to_days_since_seen(days_seen_bytes) AS days_since_active,
+    IF(((total_uri_count_sum > 0) AND (`moz-fx-data-shared-prod`.udf.bits_to_days_since_seen(days_seen_bytes) = 0) AND (cls.active_hours_sum > 0)), 1, 0) AS active,
+    IF(`moz-fx-data-shared-prod`.udf.bits_to_days_since_seen(days_seen_bytes) = 0, ad_click, 0) AS ad_clicks,
+    FROM `moz-fx-data-shared-prod.search.search_clients_last_seen_v2` cls
+    JOIN `moz-fx-data-shared-prod.telemetry_derived.clients_first_seen_v1` cfs
+    USING(client_id, sample_id)
+    WHERE sample_id >= min_sample_id
+    AND sample_id <= max_sample_id
+    AND cls.submission_date >= first_cohort_date
+    AND cls.submission_date <= last_observation_date
+    AND cfs.first_seen_date >= first_cohort_date -- clients newer than first cohort we care about
+    AND cfs.first_seen_date <= last_cohort_date -- clients not any newer than that last cohort
+    AND cls.submission_date >= cfs.first_seen_date  -- to account for some bad clients        
+    -- remove cases where the last time a client was seen was before their supposed first date
+    AND `moz-fx-data-shared-prod`.udf.bits_to_days_since_seen(days_seen_bytes) <= DATE_DIFF(cls.submission_date, cfs.first_seen_date, DAY)
+    AND days_seen_bytes IS NOT NULL
+)
+
 SELECT
-  *
-FROM
-  table
-WHERE
-  submission_date = @submission_date
+b.client_id,
+b.sample_id,
+b.submission_date,
+b.first_seen_date,
+b.days_since_first_seen,
+b.days_since_active,
+b.first_reported_country, 
+b.attribution,
+b.active,
+b.ad_clicks,
+h.total_historic_ad_clicks
+FROM base b
+LEFT JOIN `moz-fx-data-shared-prod.firefox_desktop_derived.adclick_history_v1` h
+USING (client_id)

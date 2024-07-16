235ef99a3ca8f8aaa9d1d7a944057cafb49ec2e6
Katie Windau <kwindau@mozilla.com>
2024-07-16 14:19:37 -0500

    GROWTH-90 initial commit

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.ltv_states`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1`
---
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/ltv_states_v1/query.sql
@@ -0,0 +1,9 @@
+-- Query for firefox_desktop_derived.ltv_states_v1
+            -- For more information on writing queries see:
+            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+SELECT
+  *
+FROM
+  table
+WHERE
+  submission_date = @submission_date

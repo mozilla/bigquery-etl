d8900e9a5c65f6fbf37aa91eb2421beadbdd7b4e
Katie Windau <kwindau@mozilla.com>
2024-07-18 14:01:47 -0500

    GROWTH-90 add missing struct column to view

--- sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
@@ -4,9 +4,32 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.ltv_states`
 AS
+WITH base AS (
+  SELECT
+    client_id,
+    sample_id,
+    submission_date,
+    first_seen_date,
+    days_since_first_seen,
+    days_since_active,
+    first_reported_country,
+    attribution,
+    active,
+    ad_clicks,
+    total_historic_ad_clicks,
+    days_seen_bytes,
+    BIT_COUNT(
+      `mozfun`.bytes.extract_bits(days_seen_bytes, - {{lookback}}, {{lookback}})
+    ) AS pattern,
+    {{death_time}} AS death_time,
+    {{max_days}} AS max_days
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1`
+)
 SELECT
   client_id,
   sample_id,
+  ,
   submission_date,
   first_seen_date,
   days_since_first_seen,
@@ -17,8 +40,21 @@ SELECT
   ad_clicks,
   total_historic_ad_clicks,
   days_seen_bytes,
-  BIT_COUNT(`mozfun`.bytes.extract_bits(days_seen_bytes, - {{lookback}}, {{lookback}})) AS pattern,
-  {{death_time}} AS death_time,
-  {{max_days}} AS max_days
+  pattern,
+  death_time,
+  max_days,
+  STRUCT(
+    mozfun.ltv.get_state_desktop_v1(
+      days_since_first_seen,
+      days_since_active,
+      submission_date,
+      first_seen_date,
+      death_time,
+      pattern,
+      active,
+      max_days,
+      lookback
+    ) AS desktop_states_v1
+  ) AS markov_state
 FROM
-  `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1`
+  base

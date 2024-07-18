35b8ec8a53cee61752e3f498b4b2d25a98f04cb7
skahmann3 <16420065+skahmann3@users.noreply.github.com>
2024-07-17 18:04:19 -0700

    remove old default engine dau, update engaged dau to remove default

--- sql/moz-fx-data-shared-prod/search_derived/search_revenue_levers_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/search_derived/search_revenue_levers_daily_v1/query.sql
@@ -161,8 +161,8 @@ desktop_data_ddg AS (
 -- Mobile DAU data -- merging baseline clients to AUA clients
 ## baseline ping -- mobile default search engine by client id
 mobile_baseline_engine AS (
-  SELECT DISTINCT
-    DATE(submission_timestamp) AS submission_date,
+  SELECT
+    DISTINCT DATE(submission_timestamp) AS submission_date,
     client_info.client_id,
     metrics.string.search_default_engine_code AS default_search_engine,
   FROM
@@ -170,8 +170,8 @@ mobile_baseline_engine AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
   UNION ALL
-  SELECT DISTINCT
-    DATE(submission_timestamp) AS submission_date,
+  SELECT
+    DISTINCT DATE(submission_timestamp) AS submission_date,
     client_info.client_id,
     metrics.string.search_default_engine AS default_search_engine
   FROM
@@ -179,8 +179,8 @@ mobile_baseline_engine AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
   UNION ALL
-  SELECT DISTINCT
-    DATE(submission_timestamp) AS submission_date,
+  SELECT
+    DISTINCT DATE(submission_timestamp) AS submission_date,
     client_info.client_id,
     metrics.string.browser_default_search_engine AS default_search_engine
   FROM
@@ -188,8 +188,8 @@ mobile_baseline_engine AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
   UNION ALL
-  SELECT DISTINCT
-    DATE(submission_timestamp) AS submission_date,
+  SELECT
+    DISTINCT DATE(submission_timestamp) AS submission_date,
     client_info.client_id,
     metrics.string.search_default_engine AS default_search_engine
   FROM
@@ -253,8 +253,8 @@ mobile_baseline_search AS (
 ),
 ## baseline-powered clients who qualify for KPI (activity filters applied)
 mobile_dau_data AS (
-  SELECT DISTINCT
-    submission_date,
+  SELECT
+    DISTINCT submission_date,
     "mobile" AS device,
     country,
     client_id
@@ -296,7 +296,6 @@ final_mobile_dau_counts AS (
       DISTINCT IF(
         search_count > 0
         AND normalized_engine = 'Google'
-        AND default_search_engine LIKE '%google%'
         AND (
           (submission_date < "2023-12-01" AND country NOT IN ('RU', 'UA', 'TR', 'BY', 'KZ', 'CN'))
           OR (submission_date >= "2023-12-01" AND country NOT IN ('RU', 'UA', 'BY', 'CN'))
@@ -309,13 +308,7 @@ final_mobile_dau_counts AS (
       DISTINCT IF(default_search_engine LIKE '%bing%', client_id, NULL)
     ) AS bing_dau_w_engine_as_default,
     COUNT(
-      DISTINCT IF(
-        search_count > 0
-        AND normalized_engine = 'Bing'
-        AND default_search_engine LIKE '%bing%',
-        client_id,
-        NULL
-      )
+      DISTINCT IF(search_count > 0 AND normalized_engine = 'Bing', client_id, NULL)
     ) AS bing_dau_engaged_w_sap,
     COUNT(
       DISTINCT IF(
@@ -326,22 +319,18 @@ final_mobile_dau_counts AS (
       )
     ) AS ddg_dau_w_engine_as_default,
     COUNT(
-      DISTINCT IF(
-        normalized_engine = "DuckDuckGo"
-        AND search_count > 0
-        AND (default_search_engine LIKE('%ddg%') OR default_search_engine LIKE('%duckduckgo%')),
-        client_id,
-        NULL
-      )
+      DISTINCT IF(normalized_engine = "DuckDuckGo" AND search_count > 0, client_id, NULL)
     ) AS ddg_dau_engaged_w_sap
   FROM
     mobile_dau_data
   LEFT JOIN
     mobile_baseline_engine
-    USING (submission_date, client_id)
+  USING
+    (submission_date, client_id)
   LEFT JOIN
     mobile_baseline_search
-    USING (submission_date, client_id, default_search_engine)
+  USING
+    (submission_date, client_id, default_search_engine)
   GROUP BY
     1,
     2
@@ -370,7 +359,8 @@ mobile_data_google AS (
     `moz-fx-data-shared-prod.search.mobile_search_clients_engines_sources_daily`
   INNER JOIN
     final_mobile_dau_counts
-    USING (submission_date, country)
+  USING
+    (submission_date, country)
   WHERE
     submission_date = @submission_date
     AND (
@@ -427,7 +417,8 @@ mobile_data_bing_ddg AS (
     `moz-fx-data-shared-prod.search.mobile_search_clients_engines_sources_daily`
   INNER JOIN
     final_mobile_dau_counts
-    USING (submission_date, country)
+  USING
+    (submission_date, country)
   WHERE
     submission_date = @submission_date
     AND (
@@ -548,7 +539,14 @@ SELECT
   ad_click_organic,
   search_with_ads_organic,
   monetizable_sap,
-  google_dau_w_engine_as_default AS dau_w_engine_as_default
+  # custom engine bug merged in v121
+  # null engine bug merged in v126
+  # remove default engine data prior to June 2024
+  IF(
+    submission_date >= "2024-06-01",
+    google_dau_w_engine_as_default,
+    NULL
+  ) AS dau_w_engine_as_default
 FROM
   mobile_data_google
 UNION ALL
@@ -569,7 +567,10 @@ SELECT
   bing_ad_click_organic,
   bing_search_with_ads_organic,
   bing_monetizable_sap,
-  bing_dau_w_engine_as_default AS dau_w_engine_as_default
+  # custom engine bug merged in v121
+  # null engine bug merged in v126
+  # remove default engine data prior to June 2024
+  IF(submission_date >= "2024-06-01", bing_dau_w_engine_as_default, NULL) AS dau_w_engine_as_default
 FROM
   mobile_data_bing_ddg
 UNION ALL
@@ -590,6 +591,9 @@ SELECT
   ddg_ad_click_organic,
   ddg_search_with_ads_organic,
   ddg_monetizable_sap,
-  ddg_dau_w_engine_as_default AS dau_w_engine_as_default
+  # custom engine bug merged in v121
+  # null engine bug merged in v126
+  # remove default engine data prior to June 2024
+  IF(submission_date >= "2024-06-01", ddg_dau_w_engine_as_default, NULL) AS dau_w_engine_as_default
 FROM
   mobile_data_bing_ddg

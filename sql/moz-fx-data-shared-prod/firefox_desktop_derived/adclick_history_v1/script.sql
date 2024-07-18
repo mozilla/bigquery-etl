24f98692e5015d315a9f9dc8f2b974f15344a6b8
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-07-18 08:55:55 -0500

    Remove sample ID

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/script.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/script.sql
@@ -3,13 +3,11 @@ MERGE INTO
   USING (
     SELECT
       s.client_id,
-      s.sample_id,
       mozfun.map.set_key(h.ad_click_history, @submission_date, ad_click) AS ad_click_history
     FROM
       (
         SELECT
           client_id,
-          sample_id,
           SUM(ad_click) AS ad_click
         FROM
           `moz-fx-data-shared-prod.search_derived.search_clients_daily_v8`
@@ -17,7 +15,6 @@ MERGE INTO
           submission_date = @submission_date
         GROUP BY
           client_id,
-          sample_id
         HAVING
           SUM(ad_click) > 0
       ) s
@@ -29,9 +26,9 @@ MERGE INTO
 WHEN NOT MATCHED
 THEN
   INSERT
-    (client_id, sample_id, ad_click_history)
+    (client_id, ad_click_history)
   VALUES
-    (S.client_id, S.sample_id, S.ad_click_history)
+    (S.client_id, S.ad_click_history)
   WHEN MATCHED
 THEN
   UPDATE

029cbfad29c6396992f3da57bbc3b149611d0338
Katie Windau <kwindau@mozilla.com>
2024-07-18 06:10:22 -0500

    fix grain

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/query.sql
@@ -2,21 +2,15 @@ WITH new_data AS (
   SELECT
     client_id,
     sample_id,
-    ad_click AS ad_clicks
+    sum(ad_click) AS ad_clicks
   FROM
     `moz-fx-data-shared-prod.search_derived.search_clients_daily_v8`
   WHERE
     submission_date = @submission_date
     AND ad_click > 0
-),
-existing_data AS (
-  SELECT
-    client_id,
-    sample_id,
-    ad_click_history
-  FROM
-    `moz-fx-data-shared-prod.firefox_desktop_derived.adclick_history_v1`
+    GROUP BY 1,2
 )
+    
 SELECT
   client_id,
   sample_id,
@@ -24,5 +18,5 @@ SELECT
 FROM
   new_data
 FULL OUTER JOIN
-  existing_data
+  `moz-fx-data-shared-prod.firefox_desktop_derived.adclick_history_v1`
   USING (sample_id, client_id)

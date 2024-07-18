716cb03374e2ceecda9eac5aa728c7b47f4b2c43
Katie Windau <kwindau@mozilla.com>
2024-07-18 11:57:02 -0500

    fix view.sql

--- sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/ltv_states/view.sql
@@ -5,20 +5,21 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.ltv_states`
 AS
 SELECT
-  a.client_id,
-  a.sample_id,
-  a.submission_date,
-  a.first_seen_date,
-  a.days_since_first_seen,
-  a.days_since_active,
-  a.first_reported_country,
-  a.attribution,
-  a.active,
-  a.ad_clicks,
-  a.total_historic_ad_clicks BIT_COUNT(
+  client_id,
+  sample_id,
+  submission_date,
+  first_seen_date,
+  days_since_first_seen,
+  days_since_active,
+  first_reported_country,
+  attribution,
+  active,
+  ad_clicks,
+  total_historic_ad_clicks,
+  BIT_COUNT(
     `mozfun`.bytes.extract_bits(days_seen_bytes, - {{lookback}}, {{lookback}})
   ) AS pattern,
   {{death_time}} AS death_time,
   {{max_days}} AS max_days
 FROM
-  `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1` a
+  `moz-fx-data-shared-prod.firefox_desktop_derived.ltv_states_v1`

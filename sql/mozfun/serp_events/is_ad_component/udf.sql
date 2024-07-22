20a63fe783d9e048322847795e279ff41323ccf5
Preethi Issac <102384487+pissac17@users.noreply.github.com>
2024-07-22 13:13:34 -0400

    Update udf.sql for serp_events: adding ads_uncategorized as an ad component (#5953)
    * Update udf.sql
    
    adding ads_uncategorized as an ad component
    
    * Update udf.sql

--- sql/mozfun/serp_events/is_ad_component/udf.sql
+++ sql/mozfun/serp_events/is_ad_component/udf.sql
@@ -1,7 +1,14 @@
 CREATE OR REPLACE FUNCTION serp_events.is_ad_component(component STRING)
 RETURNS BOOL AS (
   COALESCE(
-    component IN ('ad_carousel', 'ad_image_row', 'ad_link', 'ad_sidebar', 'ad_sitelink'),
+    component IN (
+      'ad_carousel', 
+      'ad_image_row', 
+      'ad_link', 
+      'ad_sidebar', 
+      'ad_sitelink', 
+      'ad_uncategorized'
+    ),
     FALSE
   )
 );
@@ -9,4 +16,4 @@ RETURNS BOOL AS (
 -- Tests
 SELECT
   assert.equals(TRUE, serp_events.is_ad_component('ad_link')),
-  assert.equals(FALSE, serp_events.is_ad_component('other'))
+  assert.equals(FALSE, serp_events.is_ad_component('other'));

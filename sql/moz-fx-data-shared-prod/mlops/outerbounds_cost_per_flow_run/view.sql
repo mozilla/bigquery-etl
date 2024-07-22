5206f93e08a73434574a40e31751b8acbb31649e
Chelsea Troy <chelsea.dommert@gmail.com>
2024-07-22 15:40:37 -0500

    Add views for seeing outerbounds job compute costs

---
+++ sql/moz-fx-data-shared-prod/mlops/outerbounds_cost_per_flow_run/view.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mlops.outerbounds_cost_per_flow_run`
+AS
+SELECT
+  DATE_TRUNC(usage_start_time, DAY) as invoice_day,
+  labels.value AS run_id,
+  labels.value,
+  ifnull(safe.left(labels.value, instr(labels.value, '-', -1) - 1), labels.value) as flow_name,
+  SUM(cost) as cost
+FROM
+  moz-fx-data-shared-prod.billing_syndicate.gcp_billing_export_resource_v1_01E7D5_97288E_E2EBA0
+JOIN
+  UNNEST(labels) AS labels
+ON
+  labels.key = "k8s-label/workflows.argoproj.io/workflow"
+WHERE
+  project.id = "moz-fx-mfouterbounds-prod-f98d"
+  AND usage_start_time >= '2024-07-10'
+GROUP BY
+  1,
+  2
+ORDER BY 3 DESC
+

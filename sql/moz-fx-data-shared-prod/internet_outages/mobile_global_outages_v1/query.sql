43bda73c4eba5846fecebcb125153a3d80ae7fca
kik-kik <kignasiak@mozilla.com>
2024-07-26 16:47:10 +0200

    feat: update mobile_global_outages query

--- sql/moz-fx-data-shared-prod/internet_outages/mobile_global_outages_v1/query.sql
+++ sql/moz-fx-data-shared-prod/internet_outages/mobile_global_outages_v1/query.sql
@@ -1,208 +1,170 @@
-WITH
--- Firefox for Android
-fenix_baseline AS (
+-- combine baseline pings FROM different products
+WITH product_union AS (
+  -- Firefox for Android
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS `datetime`,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.fenix.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
-),
--- Firefox for iOS
-firefox_ios_baseline AS (
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
+  UNION ALL
+  -- Firefox for iOS
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS `datetime`,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.firefox_ios.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
-),
--- Firefox Focus for Android
-focus_android_baseline AS (
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
+  UNION ALL
+  -- Firefox Focus for Android
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS `datetime`,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.focus_android.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
-),
--- Firefox Focus for iOS
-focus_ios_baseline AS (
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
+  UNION ALL
+  -- Firefox Focus for iOS
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS `datetime`,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.focus_ios.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
-),
--- Firefox Klar for Android
-klar_android_baseline AS (
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
+  UNION ALL
+  -- Firefox Klar for Android
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
     TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.klar_android.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
-),
--- Firefox Klar for iOS
-klar_ios_baseline AS (
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
+  UNION ALL
+  -- Firefox Klar for iOS
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS `datetime`,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.klar_ios.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
-),
--- Mozilla VPN
-mozilla_vpn_baseline AS (
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
+  UNION ALL
+  -- Mozilla VPN
   SELECT
     metadata.geo.country,
     metadata.geo.city,
     metadata.geo.subdivision1 AS geo_subdivision1,
     metadata.geo.subdivision2 AS geo_subdivision2,
-    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS submission_hour,
-    metadata.isp.name,
-    metadata.isp.organization,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) `datetime`,
+    metadata.isp.name AS isp_name,
+    ping_info.end_time,
+    submission_timestamp,
   FROM
     `moz-fx-data-shared-prod.mozilla_vpn.baseline`
   WHERE
-    DATE(submission_timestamp) = @submission_date
-        -- Country can be null if geoip lookup failed.
-        -- There's no point in adding these to the analyses.
-        -- Due to a bug in `telemetry.clients_daily` we need to
-        -- check for '??' AS well in addition to null.
-    AND metadata.geo.country IS NOT NULL
-    AND metadata.geo.country <> "??"
+    DATE(submission_timestamp)
+    BETWEEN "2024-06-20"
+    AND "2024-06-29"
+    AND (metadata.geo.country IS NOT NULL OR metadata.geo.country <> "??")
 ),
--- combine baseline pings FROM different products
-all_products_baseline AS (
+update_values AS (
   SELECT
-    *
+    * REPLACE (
+      IF(city = "??" OR city IS NULL, "unknown", city) AS city,
+      NULLIF(geo_subdivision1, "??") AS geo_subdivision1,
+      NULLIF(geo_subdivision2, "??") AS geo_subdivision2
+    ),
+    CASE
+      WHEN REGEXP_CONTAINS(
+          end_time,
+          "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}[-|+]\\d{2}:\\d{2}"
+        )
+        THEN TIMESTAMP(end_time)
+      WHEN REGEXP_CONTAINS(end_time, "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}[-|+]\\d{2}:\\d{2}")
+        THEN PARSE_TIMESTAMP("%Y-%m-%dT%H:%M%Ez", end_time)
+      ELSE NULL
+    END AS parsed_end_time,
   FROM
-    fenix_baseline
-  UNION ALL
-  SELECT
-    *
-  FROM
-    firefox_ios_baseline
-  UNION ALL
-  SELECT
-    *
-  FROM
-    focus_android_baseline
-  UNION ALL
-  SELECT
-    *
-  FROM
-    focus_ios_baseline
-  UNION ALL
-  SELECT
-    *
-  FROM
-    klar_android_baseline
-  UNION ALL
-  SELECT
-    *
-  FROM
-    klar_ios_baseline
-  UNION ALL
-  SELECT
-    *
-  FROM
-    mozilla_vpn_baseline
-),
-final AS (
-  SELECT
-    country,
-    IF(city IS NULL, "unknown", city) AS city,
-    geo_subdivision1,
-    geo_subdivision2,
-    submission_hour,
-    COUNT(*) AS ping_count
-  FROM
-    all_products_baseline
-  GROUP BY
-    country,
-    city,
-    geo_subdivision1,
-    geo_subdivision2,
-    submission_hour
-  HAVING
-    ping_count > 100
+    product_union
 )
 SELECT
-  *
+  country,
+  city,
+  geo_subdivision1,
+  geo_subdivision2,
+  isp_name,
+  `datetime`,
+  COUNT(*) AS ping_count,
+  AVG(
+    ABS(TIMESTAMP_DIFF(submission_timestamp, parsed_end_time, MINUTE))
+  ) AS avg_ping_arrival_delay_in_minutes,
 FROM
-  final
+  update_values
+GROUP BY
+  country,
+  city,
+  geo_subdivision1,
+  geo_subdivision2,
+  isp_name,
+  `datetime`
+HAVING
+  ping_count > 50

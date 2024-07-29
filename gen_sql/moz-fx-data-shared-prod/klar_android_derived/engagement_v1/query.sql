afe1766b59610a3716bfe995d2eb4de3ee812b5e
kik-kik <kignasiak@mozilla.com>
2024-07-29 18:25:51 +0200

    add generated sql from main branch

---
+++ gen_sql/moz-fx-data-shared-prod/klar_android_derived/engagement_v1/query.sql
@@ -0,0 +1,30 @@
+-- Query generated via `mobile_kpi_support_metrics` SQL generator.
+SELECT
+  submission_date,
+  first_seen_date,
+  normalized_channel,
+  app_name,
+  app_version,
+  country,
+  locale,
+  is_mobile,
+  COUNTIF(is_dau) AS dau,
+  COUNTIF(is_wau) AS wau,
+  COUNTIF(is_mau) AS mau,
+FROM
+  `moz-fx-data-shared-prod.klar_android.engagement_clients`
+WHERE
+  {% if is_init() %}
+    submission_date < CURRENT_DATE
+  {% else %}
+    submission_date = @submission_date
+  {% endif %}
+GROUP BY
+  submission_date,
+  first_seen_date,
+  normalized_channel,
+  app_name,
+  app_version,
+  country,
+  locale,
+  is_mobile

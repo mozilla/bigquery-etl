dbc1fe794286399934d60351711577fbde7e1674
kik-kik <kignasiak@mozilla.com>
2024-07-30 17:40:27 +0200

    feat: add new_profiles templates to mobile_kpi_support_metrics generator

---
+++ sql_generators/mobile_kpi_support_metrics/templates/new_profiles.query.sql
@@ -0,0 +1,44 @@
+{{ header }}
+SELECT
+  first_seen_date,
+  normalized_channel,
+  app_name,
+  app_version,
+  country,
+  locale,
+  isp,
+  os,
+  os_version,
+  device_manufacturer,
+  is_mobile,
+  {% for field in product_attribution_fields.values() if not field.name.endswith("_timestamp") %}
+  {{ field.name }},
+  {% endfor %}
+  COUNT(*) AS new_profiles,
+FROM
+  `{{ project_id }}.{{ dataset }}.new_profile_clients`
+WHERE
+  {% raw %}
+  {% if is_init() %}
+    first_seen_date < CURRENT_DATE
+  {% else %}
+    first_seen_date = @submission_date
+  {% endif %}
+  {% endraw %}
+GROUP BY
+  first_seen_date,
+  normalized_channel,
+  app_name,
+  app_version,
+  country,
+  locale,
+  isp,
+  os,
+  os_version,
+  device_manufacturer,
+  is_mobile
+  {% for field in product_attribution_fields.values() if not field.name.endswith("_timestamp") %}
+    {% if loop.first %},{% endif %}
+    {{ field.name }}
+    {% if not loop.last %},{% endif %}
+  {% endfor %}

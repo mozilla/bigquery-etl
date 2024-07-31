ccafe435da3471cae3ee7acbf2e926a10d377620
Katie Windau <kwindau@mozilla.com>
2024-07-31 16:05:04 -0500

    DENG-4194 - add filter that was missed in copy from marketing-prod

--- sql/moz-fx-data-shared-prod/mozilla_org/www_site_downloads/view.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org/www_site_downloads/view.sql
@@ -5,3 +5,5 @@ SELECT
   *
 FROM
   `moz-fx-data-shared-prod.mozilla_org_derived.www_site_downloads_v2`
+WHERE
+  `date` >= '2023-10-01' --filter out data earlier since downloads not fully set up before this date

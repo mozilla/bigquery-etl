210e44f797d22d37f61973fab7051b4e630af038
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-07-31 19:28:09 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_google_analytics_derived_ga4.py
+++ dags/bqetl_google_analytics_derived_ga4.py
@@ -386,6 +386,17 @@ with DAG(
         parameters=["conversion_window:INT64:30"],
     )
 
+    mozilla_org_derived__www_site_downloads__v2 = bigquery_etl_query(
+        task_id="mozilla_org_derived__www_site_downloads__v2",
+        destination_table="www_site_downloads_v2",
+        dataset_id="mozilla_org_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     mozilla_vpn_derived__site_metrics_summary__v2 = bigquery_etl_query(
         task_id="mozilla_vpn_derived__site_metrics_summary__v2",
         destination_table="site_metrics_summary_v2",
@@ -472,6 +483,8 @@ with DAG(
     mozilla_org_derived__gclid_conversions__v2.set_upstream(
         wait_for_telemetry_derived__clients_daily__v6
     )
+
+    mozilla_org_derived__www_site_downloads__v2.set_upstream(wait_for_wmo_events_table)
 
     mozilla_vpn_derived__site_metrics_summary__v2.set_upstream(
         wait_for_wmo_events_table

1f2d305838a1a9b505093b69020b7518d3229535
kik-kik <kignasiak@mozilla.com>
2024-08-01 16:09:57 +0200

    feat: remove isp from new_profiles query (due to too high cardinality) and add missing organic vs paid to the new_profiles view

--- sql_generators/mobile_kpi_support_metrics/templates/new_profiles.view.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/new_profiles.view.sql
@@ -3,6 +3,11 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ dataset }}.{{ name }}`
 AS
 SELECT
-  *
+  *,
+  {% if 'adjust_network' in product_attribution_fields %}
+    `moz-fx-data-shared-prod.udf.organic_vs_paid_mobile`(adjust_network) AS paid_vs_organic,
+  {% else %}
+    "Organic" AS paid_vs_organic,
+  {% endif %}
 FROM
   `{{ project_id }}.{{ dataset }}_derived.{{ name }}_{{ version }}`

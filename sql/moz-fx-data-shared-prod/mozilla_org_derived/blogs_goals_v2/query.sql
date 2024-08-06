508531222b658dbfa3ae24bb5ce1f3d55c3d5029
Marlene-M-Hirose <mhirose@mozilla.com>
2024-08-06 10:03:06 -1000

    actually overwrite metadata and query files. also change owner to mhirose

--- sql/moz-fx-data-shared-prod/mozilla_org_derived/blogs_goals_v2/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org_derived/blogs_goals_v2/query.sql
@@ -1,9 +1,17 @@
--- Query for mozilla_org_derived.blogs_goals_v2
-            -- For more information on writing queries see:
-            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
 SELECT
-  *
+  PARSE_DATE('%Y%m%d', event_date) AS `date`,
+  user_pseudo_id || '-' || CAST(e.value.int_value AS STRING) AS visit_identifier,
+  COUNTIF(event_name = 'download_click') AS downloads,
+  COUNTIF(event_name = 'social_share') AS social_share,
+  COUNTIF(event_name = 'newsletter_subscribe') AS newsletter_subscription
 FROM
-  table
+  `moz-fx-data-marketing-prod.analytics_314399816.events_*`
+JOIN
+  UNNEST(event_params) e
 WHERE
-  submission_date = @submission_date
+  _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d', @submission_date)
+  AND e.key = 'ga_session_id'
+  AND e.value.int_value IS NOT NULL
+GROUP BY
+  `date`,
+  visit_identifier

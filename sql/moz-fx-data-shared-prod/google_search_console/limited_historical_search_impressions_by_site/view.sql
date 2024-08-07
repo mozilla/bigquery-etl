257a6c9179044bab6f5151ec67d047ddb4f069e9
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-08-07 21:38:58 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/google_search_console/limited_historical_search_impressions_by_site/view.sql
+++ sql/moz-fx-data-shared-prod/google_search_console/limited_historical_search_impressions_by_site/view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.google_search_console.search_impressions_by_site`
+  `moz-fx-data-shared-prod.google_search_console.limited_historical_search_impressions_by_site`
 AS
 SELECT
   search_impressions.`date`,
@@ -11,7 +11,6 @@ SELECT
     search_impressions.query,
     search_impressions.search_type
   ) AS query_type,
-  search_impressions.is_anonymized,
   search_impressions.search_type,
   search_impressions.user_country_code,
   COALESCE(user_country.name, search_impressions.user_country_code) AS user_country,
@@ -22,7 +21,21 @@ SELECT
   search_impressions.clicks,
   search_impressions.average_top_position
 FROM
-  `moz-fx-data-shared-prod.google_search_console_derived.search_impressions_by_site_v2` AS search_impressions
+  `moz-fx-data-shared-prod.google_search_console_derived.search_impressions_by_site_v1` AS search_impressions
 LEFT JOIN
   `moz-fx-data-shared-prod.static.country_codes_v1` AS user_country
   ON search_impressions.user_country_code = user_country.code_3
+WHERE
+  -- Exclude the last two days of data for each site because the data for those days is incomplete.
+  CASE
+    WHEN site_domain_name IN (
+        'addons.mozilla.org',
+        'blog.mozilla.org',
+        'getpocket.com',
+        'support.mozilla.org',
+        'www.mozilla.org'
+      )
+      THEN `date` <= '2023-08-08'
+    WHEN site_domain_name = 'developer.mozilla.org'
+      THEN `date` <= '2024-04-29'
+  END

40ed1d24f504d28604ef30d20adf7a0a7aa2b03d
Sean Rose <1994030+sean-rose@users.noreply.github.com>
2024-08-07 14:09:03 -0700

    Move limited historical Google Search Console data into separate views (DENG-4329) (#6027)
    * Exclude limited historical Google Search Console data from the existing views.
    
    * Add views for limited historical Google Search Console data.

--- sql/moz-fx-data-shared-prod/google_search_console/limited_historical_search_impressions_by_page/view.sql
+++ sql/moz-fx-data-shared-prod/google_search_console/limited_historical_search_impressions_by_page/view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.google_search_console.search_impressions_by_page`
+  `moz-fx-data-shared-prod.google_search_console.limited_historical_search_impressions_by_page`
 AS
 SELECT
   search_impressions.date,
@@ -30,10 +30,7 @@ SELECT
     search_impressions.query,
     search_impressions.search_type
   ) AS query_type,
-  search_impressions.is_anonymized,
-  search_impressions.has_good_page_experience,
   search_impressions.search_type,
-  search_impressions.search_appearance,
   search_impressions.user_country_code,
   COALESCE(user_country.name, search_impressions.user_country_code) AS user_country,
   user_country.region_name AS user_region,
@@ -43,7 +40,7 @@ SELECT
   search_impressions.clicks,
   search_impressions.average_position
 FROM
-  `moz-fx-data-shared-prod.google_search_console_derived.search_impressions_by_page_v2` AS search_impressions
+  `moz-fx-data-shared-prod.google_search_console_derived.search_impressions_by_page_v1` AS search_impressions
 LEFT JOIN
   `moz-fx-data-shared-prod.static.language_codes_v1` AS localized_site_language
   ON search_impressions.localized_site_language_code = localized_site_language.code_2
@@ -53,3 +50,17 @@ LEFT JOIN
 LEFT JOIN
   `moz-fx-data-shared-prod.static.country_codes_v1` AS user_country
   ON search_impressions.user_country_code = user_country.code_3
+WHERE
+  -- Exclude the last two days of data for each site because the data for those days is incomplete.
+  CASE
+    WHEN site_domain_name IN (
+        'addons.mozilla.org',
+        'blog.mozilla.org',
+        'getpocket.com',
+        'support.mozilla.org',
+        'www.mozilla.org'
+      )
+      THEN `date` <= '2023-08-08'
+    WHEN site_domain_name = 'developer.mozilla.org'
+      THEN `date` <= '2024-04-29'
+  END

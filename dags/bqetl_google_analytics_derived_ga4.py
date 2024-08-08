2437f05319328c990c73c0ff76f13f201025c3cb
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-08-08 23:15:49 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_google_analytics_derived_ga4.py
+++ dags/bqetl_google_analytics_derived_ga4.py
@@ -371,6 +371,21 @@ with DAG(
         depends_on_past=False,
     )
 
+    mozilla_org_derived__blogs_daily_summary__v2 = bigquery_etl_query(
+        task_id="mozilla_org_derived__blogs_daily_summary__v2",
+        destination_table="blogs_daily_summary_v2",
+        dataset_id="mozilla_org_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="mhirose@mozilla.com",
+        email=[
+            "kwindau@mozilla.com",
+            "mhirose@mozilla.com",
+            "telemetry-alerts@mozilla.com",
+        ],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     mozilla_org_derived__blogs_goals__v2 = bigquery_etl_query(
         task_id="mozilla_org_derived__blogs_goals__v2",
         destination_table="blogs_goals_v2",
@@ -551,6 +566,14 @@ with DAG(
     ga_derived__www_site_metrics_summary__v2.set_upstream(wait_for_wmo_events_table)
 
     ga_derived__www_site_page_metrics__v2.set_upstream(ga_derived__www_site_hits__v2)
+
+    mozilla_org_derived__blogs_daily_summary__v2.set_upstream(
+        mozilla_org_derived__blogs_goals__v2
+    )
+
+    mozilla_org_derived__blogs_daily_summary__v2.set_upstream(
+        mozilla_org_derived__blogs_sessions__v2
+    )
 
     mozilla_org_derived__blogs_goals__v2.set_upstream(wait_for_blogs_events_table)
 

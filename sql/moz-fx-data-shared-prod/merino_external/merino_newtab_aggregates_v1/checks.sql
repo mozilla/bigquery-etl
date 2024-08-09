07da49963911dfafb2fbbe68d40c02b1b9a6bf11
Chelsey Beck <cbeck@mozilla.com>
2024-08-09 15:30:09 -0700

    adding dataset and table for merino exports

---
+++ sql/moz-fx-data-shared-prod/merino_external/merino_newtab_aggregates_v1/checks.sql
@@ -0,0 +1,15 @@
+-- macro checks
+
+#fail
+{{ not_null(["scheduled_corpus_item_id"]) }}
+
+{{ is_unique(["scheduled_corpus_item_id"]) }}
+
+#fail
+{{ not_null(["impression_count"]) }}
+
+#fail
+{{ not_null(["click_count"]) }}
+
+#fail
+{{ min_row_count(1) }}

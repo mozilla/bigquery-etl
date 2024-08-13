d112301edff5decc441483e8a6fcb562c0ea6a6e
Katie Windau <kwindau@mozilla.com>
2024-08-13 17:24:38 +0100

    DENG-3096 add country names to device usage view

--- sql/moz-fx-data-shared-prod/cloudflare/device_usage/view.sql
+++ sql/moz-fx-data-shared-prod/cloudflare/device_usage/view.sql
@@ -2,6 +2,22 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.cloudflare.device_usage`
 AS
 SELECT
-  *
+  device_usg.*,
+  country_codes.location_name,
+  country_codes.region_name
 FROM
-  `moz-fx-data-shared-prod.cloudflare_derived.device_usage_v1`
+  `moz-fx-data-shared-prod.cloudflare_derived.device_usage_v1` device_usg
+LEFT JOIN
+  (
+    SELECT
+      'ALL' AS code,
+      'Global' AS location_name,
+      'Global' AS region_name
+    UNION ALL
+    SELECT
+      code,
+      name AS location_name,
+      region_name
+    FROM
+      `mozdata.static.country_codes_v1`
+  ) country_codes

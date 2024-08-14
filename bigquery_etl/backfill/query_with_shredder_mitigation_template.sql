0240b5e5cae084298cd19c173ebfab5de6d67894
lucia-vargas-a <alvargasa@gmail.com>
2024-08-14 12:09:40 +0200

    Query template for shredder mitigation.

---
+++ bigquery_etl/backfill/query_with_shredder_mitigation_template.sql
@@ -0,0 +1,24 @@
+-- Query generated from a template that mitigates the effect of shredder.
+WITH
+{{ new_version_cte }} AS (
+  {{ new_version }}
+),
+{{ new_agg_cte }} AS (
+  {{ new_agg }}
+),
+{{ previous_agg_cte }} AS (
+  {{ previous_agg }}
+),
+{{  shredded_cte }} AS (
+  {{ shredded }}
+)
+SELECT
+    {{ final_select}}
+FROM
+    {{ new_version_cte }}
+UNION ALL
+SELECT
+  {{ final_select}}
+FROM
+    {{ shredded_cte }}
+;

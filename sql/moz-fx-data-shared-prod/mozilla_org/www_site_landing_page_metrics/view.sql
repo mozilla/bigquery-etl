5b1348f997d7bfc423dbba1ff22549381f37a301
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-08-27 22:23:46 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/mozilla_org/www_site_landing_page_metrics/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mozilla_org.www_site_landing_page_metrics`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.mozilla_org_derived.www_site_landing_page_metrics_v2`

f723d19ae577698118bfbd5404b4a4f6570ac68c
Katie Windau <kwindau@mozilla.com>
2024-08-30 15:33:29 -0500

    DENG-4688 add profile_group_id to adclick_history_v1

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/adclick_history_v1/query.sql
@@ -1,7 +1,8 @@
 WITH new_data AS (
   SELECT
     client_id,
-    SUM(ad_click) AS ad_clicks
+    SUM(ad_click) AS ad_clicks,
+    MAX(profile_group_id) AS profile_group_id
   FROM
     `moz-fx-data-shared-prod.search_derived.search_clients_daily_v8`
   WHERE
@@ -12,7 +13,8 @@ WITH new_data AS (
 )
 SELECT
   client_id,
-  mozfun.map.set_key(ad_click_history, @submission_date, ad_clicks) AS ad_click_history
+  mozfun.map.set_key(ad_click_history, @submission_date, ad_clicks) AS ad_click_history,
+  profile_group_id,
 FROM
   new_data
 FULL OUTER JOIN

642d81e0bd9cabe90d9fa5485b7735fe0b133ce8
Katie Windau <kwindau@mozilla.com>
2024-08-30 11:20:29 -0500

    DENG-4677 fix formatting of query.sql

--- sql/moz-fx-data-shared-prod/search_derived/search_clients_last_seen_v1/query.sql
+++ sql/moz-fx-data-shared-prod/search_derived/search_clients_last_seen_v1/query.sql
@@ -85,7 +85,7 @@ _grouped AS (
     `moz-fx-data-shared-prod.udf.aggregate_search_map`(
       ARRAY_AGG(engine_searches)
     ) AS engine_searches,
-  MAX(profile_group_id) AS profile_group_id,
+    MAX(profile_group_id) AS profile_group_id,
   FROM
     _derived_engine_searches
   GROUP BY

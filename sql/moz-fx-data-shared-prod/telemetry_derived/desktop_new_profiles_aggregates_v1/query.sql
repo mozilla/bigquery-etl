ce5af2ae215b88c9a8b197dc04cbc2eac226ee3c
Marlene-M-Hirose <mhirose@mozilla.com>
2024-09-06 08:10:18 -1000

    initial add of query.sql and metadata.yaml files

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/desktop_new_profiles_aggregates_v1/query.sql
@@ -0,0 +1,55 @@
+SELECT
+  cfs.first_seen_date,
+  cfs.is_desktop,
+  cfs.attribution_medium,
+  cfs.attribution_source,
+  cfs.attribution_campaign,
+  cfs.attribution_content,
+  cfs.attribution_dlsource,
+  cfs.attribution_ua,
+  (cfs.attribution_medium IS NOT NULL
+  OR cfs.attribution_source IS NOT NULL) AS attributed,
+  cfs.city,
+  cfs.country,
+  cfs.distribution_id,
+  EXTRACT( YEAR FROM cfs.first_seen_date) AS first_seen_year,
+  cfs.normalized_channel AS channel,
+  cfs.os,
+  cfs.os_version,
+  cfs.normalized_os,
+  cfs.normalized_os_version,
+  cfs.locale,
+  cfs.app_version,
+  cfs.windows_version,
+  cfs.windows_build_number,
+  au.is_dau,
+  count(cfs.client_id)
+FROM `moz-fx-data-shared-prod.telemetry.clients_first_seen` cfs
+LEFT JOIN `moz-fx-data-shared-prod.telemetry.active_users` au
+    ON au.client_id = cfs.client_id
+WHERE cfs.first_seen_date = au.submission_date
+  AND au.submission_date = @submission_date
+GROUP BY
+    cfs.first_seen_date,
+    cfs.is_desktop,
+    cfs.attribution_medium,
+    cfs.attribution_source,
+    cfs.attribution_campaign,
+    cfs.attribution_content,
+    cfs.attribution_dlsource,
+    cfs.attribution_ua,
+    attributed,
+    cfs.city,
+    cfs.country,
+    cfs.distribution_id,
+    first_seen_year,
+    channel,
+    cfs.os,
+    cfs.os_version,
+    normalized_os,
+    normalized_os_version,
+    cfs.locale,
+    cfs.app_version,
+    cfs.windows_version,
+    cfs.windows_build_number,
+    au.is_dau

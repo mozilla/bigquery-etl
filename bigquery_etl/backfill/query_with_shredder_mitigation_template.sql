7cc941a5cb4a626a2f6c8518bf9ca946b9c1dd77
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2024-09-10 15:47:11 +0200

    Add default values in template to fix sqlglot parsing error. (#6166)

--- bigquery_etl/backfill/query_with_shredder_mitigation_template.sql
+++ bigquery_etl/backfill/query_with_shredder_mitigation_template.sql
@@ -1,23 +1,22 @@
 -- Query generated using a template for shredder mitigation.
-WITH {{ new_version_cte }} AS (
-  {{ new_version }}
+WITH {{ new_version_cte | default('new_version') }} AS (
+  {{ new_version | default('SELECT 1')  }}
 ),
-{{ new_agg_cte }} AS (
-  {{ new_agg }}
+{{ new_agg_cte | default('new_agg') }} AS (
+  {{ new_agg | default('SELECT 1')  }}
 ),
-{{ previous_agg_cte }} AS (
-  {{ previous_agg }}
+{{ previous_agg_cte | default('previous_agg') }} AS (
+  {{ previous_agg | default('SELECT 1')  }}
 ),
-{{  shredded_cte }} AS (
-  {{ shredded }}
+{{  shredded_cte | default('shredded') }} AS (
+  {{ shredded | default('SELECT 1')  }}
 )
 SELECT
-    {{ final_select }}
+  {{ final_select | default('1') }}
 FROM
-    {{ new_version_cte }}
+  {{ new_version_cte | default('new_version') }}
 UNION ALL
 SELECT
-  {{ final_select}}
+  {{ final_select | default('1') }}
 FROM
-    {{ shredded_cte }}
-;
+  {{ shredded_cte | default('shredded') }};

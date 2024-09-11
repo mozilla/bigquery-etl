f2608d177db73f35daab0ec5ed495fa0c905284a
Katie Windau <kwindau@mozilla.com>
2024-09-11 16:40:40 -0500

    initial commit work in progress

---
+++ sql/moz-fx-data-shared-prod/sumo_ga_derived/ga4_events_v1/query.sql
@@ -0,0 +1,9 @@
+-- Query for sumo_ga_derived.ga4_events_v1
+            -- For more information on writing queries see:
+            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
+SELECT
+  *
+FROM
+  table
+WHERE
+  submission_date = @submission_date

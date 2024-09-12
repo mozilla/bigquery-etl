40bd47069a7b68f1a18c16c3491fb2d4299c4c22
Katie Windau <kwindau@mozilla.com>
2024-09-12 10:08:57 -0500

    DSRE-1681 remove clustering from both metadata.yaml files

--- sql/moz-fx-data-shared-prod/sumo_ga_derived/ga4_events_v1/query.sql
+++ sql/moz-fx-data-shared-prod/sumo_ga_derived/ga4_events_v1/query.sql
@@ -1,6 +1,3 @@
--- Query for sumo_ga_derived.ga4_events_v1
-            -- For more information on writing queries see:
-            -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
 SELECT
   ga.*,
   @submission_date AS submission_date

8f2d1acf2b78faae0676dbc8b0f7cb42f589c0fd
Katie Windau <kwindau@mozilla.com>
2024-09-23 21:31:11 -0500

    DENG-4883 fix handling for days with no data

--- sql/moz-fx-data-shared-prod/telemetry_derived/segmented_dau_28_day_rolling_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/segmented_dau_28_day_rolling_v1/query.sql
@@ -1,43 +1,119 @@
-WITH staging AS (
+WITH last_28_days AS (
+  SELECT DISTINCT
+    submission_date AS submission_dt
+  FROM
+    `moz-fx-data-shared-prod.telemetry.segmented_dau`
+  WHERE
+    submission_date
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 27 DAY)
+    AND @submission_date
+),
+raw_dau_last_28_days AS (
+  --note: even though table is already currently grouped by these, 
+  --here we re-doing the group by in case columns are added in 
+  --the future to this view, so as to not introduce dupes
   SELECT
     submission_date,
-    sd.country,
-    sd.app_name,
-    sd.adjust_network,
-    sd.attribution_medium,
-    sd.attribution_source,
-    sd.first_seen_year,
-    sd.channel,
-    sd.install_source,
-    sd.is_default_browser,
-    sd.os_grouped,
-    sd.segment,
-    sd.dau,
-    AVG(dau) OVER (
+    country,
+    app_name,
+    adjust_network,
+    attribution_medium,
+    attribution_source,
+    first_seen_year,
+    channel,
+    install_source,
+    is_default_browser,
+    os_grouped,
+    segment,
+    SUM(dau) AS dau
+  FROM
+    `moz-fx-data-shared-prod.telemetry.segmented_dau` sd
+  WHERE
+    submission_date
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 27 DAY)
+    AND @submission_date
+  GROUP BY
+    submission_date,
+    country,
+    app_name,
+    adjust_network,
+    attribution_medium,
+    attribution_source,
+    first_seen_year,
+    channel,
+    install_source,
+    is_default_browser,
+    os_grouped,
+    segment
+),
+all_combos_with_any_date_having_dau_in_last_28_days AS (
+  SELECT DISTINCT
+    country,
+    app_name,
+    adjust_network,
+    attribution_medium,
+    attribution_source,
+    first_seen_year,
+    channel,
+    install_source,
+    is_default_browser,
+    os_grouped,
+    segment,
+  FROM
+    raw_dau_last_28_days
+),
+daily_dau_last_28_days AS (
+  SELECT
+    a.country,
+    a.app_name,
+    a.adjust_network,
+    a.attribution_medium,
+    a.attribution_source,
+    a.first_seen_year,
+    a.channel,
+    a.install_source,
+    a.is_default_browser,
+    a.os_grouped,
+    a.segment,
+    b.submission_dt AS submission_date,
+    COALESCE(c.dau, 0) AS dau,
+    AVG(COALESCE(c.dau, 0)) OVER (
       PARTITION BY
-        country,
-        app_name,
-        adjust_network,
-        attribution_medium,
-        attribution_source,
-        first_seen_year,
-        channel,
-        install_source,
-        is_default_browser,
-        os_grouped,
-        segment
+        a.country,
+        a.app_name,
+        a.adjust_network,
+        a.attribution_medium,
+        a.attribution_source,
+        a.first_seen_year,
+        a.channel,
+        a.install_source,
+        a.is_default_browser,
+        a.os_grouped,
+        a.segment
       ORDER BY
-        submission_date
+        b.submission_dt
       ROWS BETWEEN
         27 PRECEDING
         AND CURRENT ROW
     ) AS ma_28_dau
   FROM
-    `moz-fx-data-shared-prod.telemetry.segmented_dau` sd
-  WHERE
-    submission_date
-    BETWEEN DATE_SUB(@submission_date, INTERVAL 30 DAY)
-    AND @submission_date
+    all_combos_with_any_date_having_dau_in_last_28_days a
+  CROSS JOIN
+    last_28_days b
+  LEFT JOIN
+    raw_dau_last_28_days c
+    ON a.country = c.country
+    AND a.app_name = c.app_name
+    AND a.adjust_network = c.adjust_network
+    AND a.attribution_medium = c.attribution_medium
+    AND a.attribution_source = c.attribution_source
+    AND a.first_seen_year = c.first_seen_year
+    AND a.channel = c.channel
+    AND a.install_source = c.install_source
+    AND a.is_default_browser = c.is_default_browser
+    AND a.os_grouped = c.os_grouped
+    AND a.segment = c.segment
+    AND b.submission_dt = c.submission_date
   QUALIFY
     RANK() OVER (ORDER BY submission_date DESC) = 1
 )
@@ -57,4 +133,4 @@ SELECT
   dau,
   ma_28_dau
 FROM
-  staging
+  daily_dau_last_28_days

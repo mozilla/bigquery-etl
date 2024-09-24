1c144dc9d82e65d7856528b6c84a274acda87da6
kik-kik <kignasiak@mozilla.com>
2024-09-24 20:46:10 +0200

    fix: trailing comma in the EXCEPT union view

--- sql_generators/mobile_kpi_support_metrics/templates/union.view.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/union.view.sql
@@ -8,25 +8,17 @@ UNION ALL
 {% endif %}
 SELECT
   *
-  {% for field in product.all_possible_attribution_fields if field.exists %}
+  {% for field in product.all_possible_attribution_fields if field.exists and template_grain == "AGGREGATE" and not field.client_only%}
     {% if loop.first %}EXCEPT({% endif %}
-    {% if not loop.last %}
-      {% if template_grain == "AGGREGATE" and not field.client_only %}
-        {% if field.exists %}
-        {{ field.name }},
-        {% else %}
-        {{ field.name }}
-        {% endif %}
-      {% elif template_grain == "CLIENT" %}
-        {% if field.exists %}
-        {{ field.name }},
-        {% else %}
-        {{ field.name }}
-        {% endif %}
-      {% endif %}
-    {% endif %}
-    {% if loop.last %}),{% endif %}
-  {% else %},{% endfor %}
+      {{ field.name }}{% if not loop.last %},{% endif %}
+    {% if loop.last %}){% endif %}
+  {% endfor %}
+  {% for field in product.all_possible_attribution_fields if field.exists and template_grain == "CLIENT"%}
+    {% if loop.first %}EXCEPT({% endif %}
+      {{ field.name }}{% if not loop.last %},{% endif %}
+    {% if loop.last %}){% endif %}
+  {% endfor %}
+  ,
 {% for field in product.all_possible_attribution_fields %}
   {% if template_grain == "AGGREGATE" and not field.client_only %}
     {% if field.exists %}

12677b6abcd8bcf5dec88e7b4611fc0714e337ac
kik-kik <42538694+kik-kik@users.noreply.github.com>
2024-10-15 19:41:17 +0200

    feat(DENG-4577): add monitoring bigeye_usage view to allow us to get more insight into bq usage and costs from bigeye (#6260)
    * feat: add monitoring bigeye_usage view to allow us to get more insight into bq usage and costs from bigeye
    
    * feat: add extract of bigeye table id checked in the bq information_schema
    
    * feat: remove usage of the query field when setting up bigeye usage
    
    * fix: invalid view definition for bigeye_usage view

---
+++ sql/moz-fx-data-shared-prod/monitoring/bigeye_usage/view.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.monitoring.bigeye_usage`
+AS
+SELECT
+  submission_date,
+  reference_project_id,
+  reference_dataset_id,
+  reference_table_id,
+  creation_date,
+  task_duration,
+  total_terabytes_processed,
+  total_terabytes_billed,
+  total_slot_ms,
+  cost,
+  job_id,
+  user_email AS service_account,
+FROM
+  `moz-fx-data-shared-prod.monitoring.bigquery_usage`
+WHERE
+  DATE(submission_date) = "2024-09-24"
+  AND user_type = "bigeye"
+  AND total_slot_ms IS NOT NULL

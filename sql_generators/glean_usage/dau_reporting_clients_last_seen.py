7d2157d54ead56508b3fc5d71c449b4bef6ee30e
kik-kik <kignasiak@mozilla.com>
2024-10-24 10:28:28 -0500

    feat: add dau_reporting template

---
+++ sql_generators/glean_usage/dau_reporting_clients_last_seen.py
@@ -0,0 +1,17 @@
+"""Generating and run dau_reporting_clients_last_seen queries for Glean apps."""
+
+from sql_generators.glean_usage.common import GleanTable
+
+TARGET_TABLE_ID = "dau_reporting_clients_last_seen_v1"
+PREFIX = "dau_reporting_clients_last_seen"
+
+
+class DauReportingClientsLastSeenTable(GleanTable):
+    """Represents generated dau_reporting_clients_last_seen table."""
+
+    def __init__(self):
+        """Initialize dau_reporting_clients_last_seen table."""
+        GleanTable.__init__(self)
+        self.target_table_id = TARGET_TABLE_ID
+        self.prefix = PREFIX
+        self.base_table_name = "dau_reporting_v1"

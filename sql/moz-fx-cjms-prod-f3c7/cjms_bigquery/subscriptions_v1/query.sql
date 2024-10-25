3aa93f80ec5b961dd1bc1053abb43418fed2fbbd
Sean Rose <1994030+sean-rose@users.noreply.github.com>
2024-10-24 17:16:20 -0700

    Add Stripe discounts ETLs combining data from Fivetran's new `discount` table with existing discounts data (DENG-2116) (#6387)
    * Add syndication ETLs for new Stripe `discount` tables from Fivetran.
    
    * Implement Stripe discounts ETLs that combine new `discount` table data with existing discounts data.
    
    * Update `nonprod_invoice_discount_v2` to match logic in `invoice_discount_v2`.
    
    * Use 2024-10-23 as the cutover date for the new Stripe discounts.

--- sql/moz-fx-cjms-prod-f3c7/cjms_bigquery/subscriptions_v1/query.sql
+++ sql/moz-fx-cjms-prod-f3c7/cjms_bigquery/subscriptions_v1/query.sql
@@ -68,7 +68,7 @@ initial_discounts AS (
   FROM
     initial_invoices
   JOIN
-    `moz-fx-data-shared-prod`.stripe_external.invoice_discount_v1 AS invoice_discounts
+    `moz-fx-data-shared-prod`.stripe_external.invoice_discount_v2 AS invoice_discounts
     USING (invoice_id)
   JOIN
     `moz-fx-data-shared-prod`.stripe_external.promotion_code_v1 AS promotion_codes

87d7e7372c74d624df2375c82055a135f9046338
bwu <bewu@mozilla.com>
2024-10-29 17:07:11 -0400

    fix view name

--- sql/moz-fx-data-shared-prod/telemetry_derived/test_view_read_materialized_view/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/test_view_read_materialized_view/view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry_derived.test_materialized_view`
+  `moz-fx-data-shared-prod.telemetry_derived.test_view_read_materialized_view`
 AS
 SELECT
   *

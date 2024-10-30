9af42259b0a15e3707ec78951a9f8b4ce34b67b7
bwu <bewu@mozilla.com>
2024-10-30 11:10:59 -0400

    remove test files

--- sql/moz-fx-data-shared-prod/telemetry_derived/test_table_read_materialized_view/query.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.test_materialized_view`
--- sql/moz-fx-data-shared-prod/telemetry_derived/test_view_read_materialized_view/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry_derived.test_view_read_materialized_view`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.test_materialized_view`

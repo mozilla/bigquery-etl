0cfc237c2a41d41b28e2580b10cf1e8ed2eb1f09
Katie Windau <kwindau@mozilla.com>
2024-11-12 12:30:35 -0600

    Make query scan clients daily less

--- sql/moz-fx-data-shared-prod/mozilla_org_derived/gclid_conversions_v3/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org_derived/gclid_conversions_v3/query.sql
@@ -51,83 +51,64 @@ new_conversion_events AS (
     AND report_date = @submission_date
     AND first_seen_date < @submission_date
 ),
---Step 5: Get all clients who had their first run on the report date
---WHAT IS WRONG WITH THIS COMPARED TO THE REST OF THEM????
-firefox_first_run_staging AS (
+--Step 5: Get, for each client, their first run date, first ad click date, first search date, the # of days running Firefox, and their most recent run date
+old_events_staging AS (
   SELECT
     client_id AS telemetry_client_id,
-    MIN(submission_date) AS activity_date
+    MIN(submission_date) AS firefox_first_run_date,
+    MIN(
+      CASE
+        WHEN IFNULL(ad_clicks_count_all, 0) > 0
+          THEN submission_date
+        ELSE NULL
+      END
+    ) AS firefox_first_ad_click_date,
+    MIN(
+      CASE
+        WHEN IFNULL(search_count_all, 0) > 0
+          THEN submission_date
+        ELSE NULL
+      END
+    ) AS firefox_first_search_date,
+    COUNT(DISTINCT(submission_date)) AS nbr_days_running_firefox,
+    MAX(submission_date) AS most_recent_date_running_firefox
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
   WHERE
     submission_date <= current_date
   GROUP BY
     1
-  HAVING
-    MIN(submission_date) = @submission_date
 ),
-firefox_first_run AS (
+--Step 6: Summarize this into the old event types
+old_events AS (
   SELECT
     telemetry_client_id,
-    activity_date,
-    TRUE AS firefox_first_run_event
+    @submission_date AS activity_date,
+    CASE
+      WHEN @submission_date = firefox_first_run_date
+        THEN TRUE
+      ELSE FALSE
+    END AS firefox_first_run,
+    CASE
+      WHEN @submission_date = firefox_first_ad_click_date
+        THEN TRUE
+      ELSE FALSE
+    END AS firefox_first_ad_click,
+    CASE
+      WHEN @submission_date = firefox_first_search_date
+        THEN TRUE
+      ELSE FALSE
+    END AS firefox_first_search,
+    CASE
+      WHEN most_recent_date_running_firefox = @submission_date
+        AND nbr_days_running_firefox = 2
+        THEN TRUE
+      ELSE FALSE
+    END AS returned_second_day
   FROM
-    firefox_first_run_staging
+    old_events_staging
 ),
---Step 6: Get all clients who had their first ever ad click on the report date
-firefox_first_ad_click AS (
-  SELECT
-    client_id AS telemetry_client_id,
-    TRUE AS firefox_first_ad_click_event,
-    MIN(submission_date) AS activity_date
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
-  WHERE
-    IFNULL(ad_clicks_count_all, 0) > 0
-    AND submission_date <= current_date
-  GROUP BY
-    1,
-    2
-  HAVING
-    MIN(submission_date) = @submission_date
-),
---Step 7: Get all clients who had their first ever search on the report date
-firefox_first_search AS (
-  SELECT
-    client_id AS telemetry_client_id,
-    TRUE AS firefox_first_search_event,
-    MIN(submission_date) AS activity_date
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
-  WHERE
-    IFNULL(search_count_all, 0) > 0
-    AND submission_date <= current_date
-  GROUP BY
-    1,
-    2
-  HAVING
-    MIN(submission_date) = @submission_date
-),
---Step 8: Get all clients who returned a second day for the first time on the report date
-returned_second_day AS (
-  SELECT
-    client_id AS telemetry_client_id,
-    TRUE AS returned_second_day_event,
-    CAST(@submission_date AS DATE) AS activity_date,
-    COUNT(DISTINCT(submission_date)) AS nbr_active_days
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
-  WHERE
-    submission_date <= current_date
-  GROUP BY
-    1,
-    2,
-    3
-  HAVING
-    COUNT(DISTINCT(submission_date)) = 2
-    AND MAX(submission_date) = @submission_date
-),
---Step 9: Get all clients who were active on the activity date, and the type of activity they had
+--Step 7: Get all clients who were active on the activity date, and the type of activity they had
 telemetry_id_to_activity_staging AS (
   SELECT
     client_id AS telemetry_client_id,
@@ -149,56 +130,22 @@ telemetry_id_to_activity_staging AS (
   SELECT
     telemetry_client_id,
     activity_date,
-    firefox_first_run_event AS firefox_first_run,
-    CAST(NULL AS BOOLEAN) AS firefox_first_ad_click,
-    CAST(NULL AS BOOLEAN) AS firefox_first_search,
-    CAST(NULL AS BOOLEAN) AS returned_second_day,
+    firefox_first_run,
+    firefox_first_ad_click,
+    firefox_first_search,
+    returned_second_day,
     CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
     CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_1_or_more_search_w_ads,
     CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_24_active_minutes,
   FROM
-    firefox_first_run
-  UNION ALL
-  SELECT
-    telemetry_client_id,
-    activity_date,
-    CAST(NULL AS BOOLEAN) AS firefox_first_run,
-    firefox_first_ad_click_event AS firefox_first_ad_click,
-    CAST(NULL AS BOOLEAN) AS firefox_first_search,
-    CAST(NULL AS BOOLEAN) AS returned_second_day,
-    CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
-    CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_1_or_more_search_w_ads,
-    CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_24_active_minutes,
-  FROM
-    firefox_first_ad_click
-  UNION ALL
-  SELECT
-    telemetry_client_id,
-    activity_date,
-    CAST(NULL AS BOOLEAN) AS firefox_first_run,
-    CAST(NULL AS BOOLEAN) AS firefox_first_ad_click,
-    firefox_first_search_event AS firefox_first_search,
-    CAST(NULL AS BOOLEAN) AS returned_second_day,
-    CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
-    CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_1_or_more_search_w_ads,
-    CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_24_active_minutes,
-  FROM
-    firefox_first_search
-  UNION ALL
-  SELECT
-    telemetry_client_id,
-    activity_date,
-    CAST(NULL AS BOOLEAN) AS firefox_first_run,
-    CAST(NULL AS BOOLEAN) AS firefox_first_ad_click,
-    CAST(NULL AS BOOLEAN) AS firefox_first_search,
-    returned_second_day_event AS returned_second_day,
-    CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
-    CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_1_or_more_search_w_ads,
-    CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_24_active_minutes,
-  FROM
-    returned_second_day
+    old_events
+  WHERE
+    firefox_first_run IS TRUE
+    OR firefox_first_ad_click IS TRUE
+    OR firefox_first_ad_click IS TRUE
+    OR returned_second_day IS TRUE
 ),
---Step 10 - Aggregate to 1 row per client ID/activity date
+--Step 8 - Aggregate to 1 row per client ID/activity date
 telemetry_id_to_activity AS (
   SELECT
     telemetry_client_id,
@@ -222,7 +169,7 @@ telemetry_id_to_activity AS (
     telemetry_client_id,
     activity_date
 )
---Step 11: Get Click IDs and associated events on this activity date
+--Step 9: Get Click IDs and associated events on this activity date
 SELECT
   activity_date,
   gclid,

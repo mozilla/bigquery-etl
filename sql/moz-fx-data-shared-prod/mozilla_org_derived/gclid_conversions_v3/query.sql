6b171d8207b12528358914e92b8eec26abe2dff6
Katie Windau <kwindau@mozilla.com>
2024-11-12 08:13:56 -0600

    Fix the SQL

--- sql/moz-fx-data-shared-prod/mozilla_org_derived/gclid_conversions_v3/query.sql
+++ sql/moz-fx-data-shared-prod/mozilla_org_derived/gclid_conversions_v3/query.sql
@@ -10,9 +10,9 @@ WITH gclids_to_ga_ids AS (
   CROSS JOIN
     UNNEST(all_reported_stub_session_ids) AS stub_session_id
   WHERE
-    session_date >= DATE_SUB(@submission_date, INTERVAL 30 DAY)
+    session_date >= DATE_SUB('2024-11-01', INTERVAL 30 DAY)
     -- Next line is needed for backfilling purposes
-    AND session_date <= @submission_date
+    AND session_date <= '2024-11-01'
     AND gclid IS NOT NULL
 ),
 --Step 2: Get all the download tokens associated with a known GA client ID & stub session ID
@@ -48,30 +48,37 @@ new_conversion_events AS (
     `moz-fx-data-shared-prod.google_ads_derived.conversion_event_categorization_v1`
   WHERE
     (event_1 IS TRUE OR event_2 IS TRUE OR event_3 IS TRUE)
-    AND report_date = @submission_date
-    AND first_seen_date < @submission_date
+    AND report_date = '2024-11-01'
+    AND first_seen_date < '2024-11-01'
 ),
 --Step 5: Get all clients who had their first run on the report date
-firefox_first_run AS (
+--WHAT IS WRONG WITH THIS COMPARED TO THE REST OF THEM????
+firefox_first_run_staging AS (
   SELECT
     client_id AS telemetry_client_id,
-    TRUE AS firefox_first_run,
     MIN(submission_date) AS activity_date
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
   WHERE
     submission_date <= current_date
   GROUP BY
-    1,
-    2
+    1
   HAVING
-    MIN(submission_date) = @submission_date
+    MIN(submission_date) = '2024-11-01'
+),
+firefox_first_run AS (
+  SELECT
+    telemetry_client_id,
+    activity_date,
+    TRUE AS firefox_first_run_event
+  FROM
+    firefox_first_run_staging
 ),
 --Step 6: Get all clients who had their first ever ad click on the report date
 firefox_first_ad_click AS (
   SELECT
     client_id AS telemetry_client_id,
-    TRUE AS firefox_first_ad_click,
+    TRUE AS firefox_first_ad_click_event,
     MIN(submission_date) AS activity_date
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
@@ -82,13 +89,13 @@ firefox_first_ad_click AS (
     1,
     2
   HAVING
-    MIN(submission_date) = @submission_date
+    MIN(submission_date) = '2024-11-01'
 ),
 --Step 7: Get all clients who had their first ever search on the report date
 firefox_first_search AS (
   SELECT
     client_id AS telemetry_client_id,
-    TRUE AS firefox_first_search,
+    TRUE AS firefox_first_search_event,
     MIN(submission_date) AS activity_date
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
@@ -99,14 +106,14 @@ firefox_first_search AS (
     1,
     2
   HAVING
-    MIN(submission_date) = @submission_date
+    MIN(submission_date) = '2024-11-01'
 ),
 --Step 8: Get all clients who returned a second day for the first time on the report date
 returned_second_day AS (
   SELECT
     client_id AS telemetry_client_id,
-    TRUE AS returned_second_day,
-    CAST(@submission_date AS DATE) AS activity_date,
+    TRUE AS returned_second_day_event,
+    CAST('2024-11-01' AS DATE) AS activity_date,
     COUNT(DISTINCT(submission_date)) AS nbr_active_days
   FROM
     `moz-fx-data-shared-prod.telemetry_derived.clients_daily_v6`
@@ -118,7 +125,7 @@ returned_second_day AS (
     3
   HAVING
     COUNT(DISTINCT(submission_date)) = 2
-    AND MAX(submission_date) = @submission_date
+    AND MAX(submission_date) = '2024-11-01'
 ),
 --Step 9: Get all clients who were active on the activity date, and the type of activity they had
 telemetry_id_to_activity_staging AS (
@@ -136,13 +143,13 @@ telemetry_id_to_activity_staging AS (
     `moz-fx-data-shared-prod.google_ads_derived.conversion_event_categorization_v1`
   WHERE
     (event_1 IS TRUE OR event_2 IS TRUE OR event_3 IS TRUE)
-    AND report_date = @submission_date
-    AND first_seen_date < @submission_date --needed since this is a required partition filter
+    AND report_date = '2024-11-01'
+    AND first_seen_date < '2024-11-01' --needed since this is a required partition filter
   UNION ALL
   SELECT
     telemetry_client_id,
     activity_date,
-    firefox_first_run,
+    firefox_first_run_event AS firefox_first_run,
     CAST(NULL AS BOOLEAN) AS firefox_first_ad_click,
     CAST(NULL AS BOOLEAN) AS firefox_first_search,
     CAST(NULL AS BOOLEAN) AS returned_second_day,
@@ -156,7 +163,7 @@ telemetry_id_to_activity_staging AS (
     telemetry_client_id,
     activity_date,
     CAST(NULL AS BOOLEAN) AS firefox_first_run,
-    firefox_first_ad_click,
+    firefox_first_ad_click_event AS firefox_first_ad_click,
     CAST(NULL AS BOOLEAN) AS firefox_first_search,
     CAST(NULL AS BOOLEAN) AS returned_second_day,
     CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
@@ -170,7 +177,7 @@ telemetry_id_to_activity_staging AS (
     activity_date,
     CAST(NULL AS BOOLEAN) AS firefox_first_run,
     CAST(NULL AS BOOLEAN) AS firefox_first_ad_click,
-    firefox_first_search,
+    firefox_first_search_event AS firefox_first_search,
     CAST(NULL AS BOOLEAN) AS returned_second_day,
     CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
     CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_1_or_more_search_w_ads,
@@ -184,7 +191,7 @@ telemetry_id_to_activity_staging AS (
     CAST(NULL AS BOOLEAN) AS firefox_first_run,
     CAST(NULL AS BOOLEAN) AS firefox_first_ad_click,
     CAST(NULL AS BOOLEAN) AS firefox_first_search,
-    returned_second_day,
+    returned_second_day_event AS returned_second_day,
     CAST(NULL AS BOOLEAN) AS first_wk_5_actv_days_and_1_or_more_search_w_ads,
     CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_1_or_more_search_w_ads,
     CAST(NULL AS BOOLEAN) AS first_wk_3_actv_days_and_24_active_minutes,

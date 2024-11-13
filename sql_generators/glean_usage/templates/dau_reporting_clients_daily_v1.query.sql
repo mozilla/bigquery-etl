24f16d721b9a920613f530adb6a1b06129b4d39b
kik-kik <kignasiak@mozilla.com>
2024-11-13 19:32:30 +0100

    feat: remove unused code from dau_reporting_clients_daily_v1.query.sql

--- sql_generators/glean_usage/templates/dau_reporting_clients_daily_v1.query.sql
+++ sql_generators/glean_usage/templates/dau_reporting_clients_daily_v1.query.sql
@@ -22,38 +22,11 @@ WITH base AS (
     {% endif %}
     CAST(NULL AS BOOLEAN) AS is_active,
     SAFE.PARSE_DATE('%F', SUBSTR(client_info.first_run_date, 1, 10)) AS first_run_date,
-    mozfun.glean.parse_datetime(ping_info.end_time) AS parsed_end_time,
-    -- TODO: add duration once it has been added to the dau_reporting ping.
-    -- udf.glean_timespan_seconds(metrics.timespan.glean_baseline_duration) AS duration,
   FROM
     `{{ project_id }}.{{ dau_reporting_stable_table }}`
   WHERE
     usage_profile_id IS NOT NULL
-),
---
-with_dates AS (
-  SELECT
-    *,
-    -- For explanation of session start time calculation, see Glean docs:
-    -- https://mozilla.github.io/glean/book/user/pings/baseline.html#contents
-    --
-    -- TODO: uncomment once duration is added to the dau_reporting ping
-    -- DATE(SAFE.TIMESTAMP_SUB(parsed_end_time, INTERVAL duration SECOND)) AS session_start_date,
-    DATE(parsed_end_time) AS session_end_date,
-  FROM
-    base
-),
---
-with_date_offsets AS (
-  SELECT
-    *,
-    -- TODO: uncomment once duration is added to the dau_reporting ping
-    -- DATE_DIFF(submission_date, session_start_date, DAY) AS session_start_date_offset,
-    DATE_DIFF(submission_date, session_end_date, DAY) AS session_end_date_offset,
-  FROM
-    with_dates
-),
---
+)
 SELECT
   submission_date,
   usage_profile_id,
@@ -70,27 +43,13 @@ SELECT
   udf.mode_last(ARRAY_AGG(distribution_id) OVER w1) AS distribution_id,
   {% if "_desktop" in app_name %}
   COALESCE(is_active, SUM(browser_engagement_uri_count) > 0 AND SUM(browser_engagement_active_ticks) > 0, False) AS is_active,
-  -- SUM(browser_engagement_uri_count) AS browser_engagement_uri_count,
-  -- SUM(browser_engagement_active_ticks) AS browser_engagement_active_ticks,
   {% else %}
   -- At the moment we do not have duration, default to True.
-  -- TODO: uncomment once duration is added to the dau_reporting ping
   -- COALESCE(is_active, SUM(IF(duration BETWEEN 0 AND 100000, duration, 0)) OVER w1 > 0, False) AS is_active,
-  True AS is_active
+  TRUE AS is_active
   {% endif %}
-  --
-  -- TODO: uncomment once duration is added to the dau_reporting ping
-  --
-  -- Bit patterns capturing activity dates relative to the submission date.
-  -- BIT_OR(
-  --   1 << IF(session_start_date_offset BETWEEN 0 AND 27, session_start_date_offset, NULL)
-  -- ) OVER w1 AS days_seen_session_start_bits,
-  -- BIT_OR(
-  --   1 << IF(session_end_date_offset BETWEEN 0 AND 27, session_end_date_offset, NULL)
-  -- ) OVER w1 AS days_seen_session_end_bits,
-  --
 FROM
-  with_date_offsets
+  base
 WHERE
   {% raw %}
   {% if is_init() %}

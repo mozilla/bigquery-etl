63009c9501efacdf7d3a9b6e73ed6778f97b018d
kik-kik <kignasiak@mozilla.com>
2024-11-27 17:48:12 +0100

    fix: mypy error

--- bigquery_etl/cli/monitoring.py
+++ bigquery_etl/cli/monitoring.py
@@ -182,7 +182,7 @@ def deploy(
                 metadata_file.parent / BIGCONFIG_FILE
                 for metadata_file in list(set(metadata_files))
             ]
-            + [Path(sql_dir, BIGCONFIG_FILE)],
+            + [f"{sql_dir}/{BIGCONFIG_FILE}"],
             output_path=Path(sql_dir).parent if sql_dir else None,
             apply=not dry_run,
             recursive=False,

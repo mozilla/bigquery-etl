73a431cef9dba303c51e35c08ad908a01b68a36f
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-12-04 22:31:44 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ dags/bqetl_firefox_installer_aggregates.py
@@ -0,0 +1,78 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from operators.gcp_container_operator import GKEPodOperator
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, bigquery_dq_check, bigquery_bigeye_check
+
+docs = """
+### bqetl_firefox_installer_aggregates
+
+Built from bigquery-etl repo, [`dags/bqetl_firefox_installer_aggregates.py`](https://github.com/mozilla/bigquery-etl/blob/generated-sql/dags/bqetl_firefox_installer_aggregates.py)
+
+#### Description
+
+This DAG schedules the build of a new aggregate table about Firefox Installs
+#### Owner
+
+kwindau@mozilla.com
+
+#### Tags
+
+* impact/tier_3
+* repo/bigquery-etl
+"""
+
+
+default_args = {
+    "owner": "kwindau@mozilla.com",
+    "start_date": datetime.datetime(2024, 12, 5, 0, 0),
+    "end_date": None,
+    "email": ["telemetry-alerts@mozilla.com", "kwindau@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+}
+
+tags = ["impact/tier_3", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_firefox_installer_aggregates",
+    default_args=default_args,
+    schedule_interval="0 15 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+
+    wait_for_copy_deduplicate_all = ExternalTaskSensor(
+        task_id="wait_for_copy_deduplicate_all",
+        external_dag_id="copy_deduplicate",
+        external_task_id="copy_deduplicate_all",
+        execution_delta=datetime.timedelta(seconds=50400),
+        check_existence=True,
+        mode="reschedule",
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    telemetry_derived__firefox_installer_aggregates__v1 = bigquery_etl_query(
+        task_id="telemetry_derived__firefox_installer_aggregates__v1",
+        destination_table="firefox_installer_aggregates_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    telemetry_derived__firefox_installer_aggregates__v1.set_upstream(
+        wait_for_copy_deduplicate_all
+    )

7815bae7a4d8f3ff5130c3c69f5bd8f75693beea
Katie Windau <kwindau@mozilla.com>
2024-12-06 15:26:15 -0600

    Fix SQL format

--- sql/moz-fx-data-shared-prod/google_ads_derived/daily_campaign_stats_v1/query.sql
+++ sql/moz-fx-data-shared-prod/google_ads_derived/daily_campaign_stats_v1/query.sql
@@ -1,6 +1,6 @@
 SELECT
   date_day AS `date`,
-  * EXCEPT (date_day), 
+  * EXCEPT (date_day),
   @submission_date AS last_updated_date
 FROM
   `moz-fx-data-bq-fivetran.ads_google_mmc_google_ads.google_ads__campaign_report`

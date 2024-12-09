6abb4b228832173418ffa09a85f4460ea5f63889
kik-kik <kignasiak@mozilla.com>
2024-12-09 12:46:29 +0100

    fix: remove date filter from view causing only a single partition to be returned by the view

--- sql/moz-fx-data-shared-prod/monitoring/bigeye_usage/view.sql
+++ sql/moz-fx-data-shared-prod/monitoring/bigeye_usage/view.sql
@@ -18,6 +18,5 @@ SELECT
 FROM
   `moz-fx-data-shared-prod.monitoring.bigquery_usage`
 WHERE
-  DATE(submission_date) = "2024-09-24"
-  AND user_type = "bigeye"
+  user_type = "bigeye"
   AND total_slot_ms IS NOT NULL

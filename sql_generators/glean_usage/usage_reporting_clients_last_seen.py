303a4bf5ced827d55cd5466f8e7d454129d95eea
kik-kik <kignasiak@mozilla.com>
2024-12-11 15:07:57 +0100

    feat: remove dau_reporting ping as we decided it should not be included in the usage_reporting derived tables

--- sql_generators/glean_usage/usage_reporting_clients_last_seen.py
+++ sql_generators/glean_usage/usage_reporting_clients_last_seen.py
@@ -1,16 +1,16 @@
-"""Generating and run dau_reporting_clients_last_seen queries for Glean apps."""
+"""Generating and run usage_reporting_clients_last_seen queries for Glean apps."""
 
 from sql_generators.glean_usage.common import GleanTable
 
-TARGET_TABLE_ID = "dau_reporting_clients_last_seen_v1"
-PREFIX = "dau_reporting_clients_last_seen"
+TARGET_TABLE_ID = "usage_reporting_clients_last_seen_v1"
+PREFIX = "usage_reporting_clients_last_seen"
 
 
 class UsageReportingClientsLastSeenTable(GleanTable):
-    """Represents generated dau_reporting_clients_last_seen table."""
+    """Represents generated usage_reporting_clients_last_seen table."""
 
     def __init__(self):
-        """Initialize dau_reporting_clients_last_seen table."""
+        """Initialize usage_reporting_clients_last_seen table."""
         GleanTable.__init__(self)
         self.target_table_id = TARGET_TABLE_ID
         self.prefix = PREFIX

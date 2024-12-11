fd7dea28be619b57c883e5cca3d681172c7226fa
kik-kik <kignasiak@mozilla.com>
2024-12-11 15:07:57 +0100

    feat: remove dau_reporting ping as we decided it should not be included in the usage_reporting derived tables

--- sql_generators/glean_usage/templates/usage_reporting_clients_daily_v1.query.sql
+++ sql_generators/glean_usage/templates/usage_reporting_clients_daily_v1.query.sql
@@ -34,54 +34,6 @@ WITH usage_reporting_base AS (
     {% endif %}
     {% endraw %}
     AND metrics.uuid.usage_profile_id IS NOT NULL
-),
-dau_reporting_base AS (
-  SELECT
-    submission_timestamp,
-    DATE(submission_timestamp) AS submission_date,
-    metrics.uuid.usage_profile_id,
-    normalized_channel,
-    -- client_info.app_display_version,
-    -- client_info.app_build,
-    normalized_os,
-    normalized_os_version,
-    -- client_info.locale,
-    normalized_country_code,
-    {% if has_distribution_id %}
-    metrics.string.metrics_distribution_id AS distribution_id,
-    {% else %}
-    CAST(NULL AS STRING) AS distribution_id,
-    {% endif %}
-    {% if "_desktop" in app_name %}
-    COALESCE(metrics.counter.browser_engagement_uri_count, 0) AS browser_engagement_uri_count,
-    COALESCE(metrics.counter.browser_engagement_active_ticks, 0) AS browser_engagement_active_ticks,
-    {% endif %}
-    CAST(NULL AS BOOLEAN) AS is_active,
-    -- SAFE.PARSE_DATE('%F', SUBSTR(client_info.first_run_date, 1, 10)) AS first_run_date,
-  FROM
-    `{{ project_id }}.{{ dau_reporting_stable_table }}`
-  WHERE
-    {% raw %}
-    {% if is_init() %}
-    DATE(submission_timestamp) >= '2024-10-10'
-    {% else %}
-    DATE(submission_timestamp) = @submission_date
-    {% endif %}
-    {% endraw %}
-    AND metrics.uuid.usage_profile_id IS NOT NULL
-),
--- We need to union with the old dau_reporting ping here as we want to make sure we include data
--- from clients that take longer to update.
-reporting_pings_union AS (
-  SELECT
-    *,
-    "usage_reporting" AS source_ping_name,
-  FROM usage_reporting_base
-  UNION ALL
-  SELECT
-    *,
-    "dau_reporting" AS source_ping_name,
-  FROM dau_reporting_base
 )
 SELECT
   submission_date,
@@ -98,11 +50,8 @@ SELECT
   -- Eventually is_active value will come from the client.
   COALESCE(LOGICAL_OR(is_active), TRUE) AS is_active,
   {% endif %}
-  STRUCT(
-    ARRAY_AGG(DISTINCT source_ping_name) AS source_pings
-  ),
 FROM
-  reporting_pings_union
+  usage_reporting_base
 GROUP BY
   submission_date,
   usage_profile_id

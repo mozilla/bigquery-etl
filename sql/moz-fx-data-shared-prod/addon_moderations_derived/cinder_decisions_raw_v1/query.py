ffe00d352a9c25202f6462794a761a6e7286da51
Marlene-M-Hirose <mhirose@mozilla.com>
2024-12-13 12:10:45 -1000

    update query.py to use load_file_from_json, eliminate unnecessary functions, add comments to describe reasons, take out partitions; update schema.yaml to reflect the schema.

--- sql/moz-fx-data-shared-prod/addon_moderations_derived/cinder_decisions_raw_v1/query.py
+++ sql/moz-fx-data-shared-prod/addon_moderations_derived/cinder_decisions_raw_v1/query.py
@@ -1,30 +1,14 @@
 """Cinder API Addon Moderations Decisions - download decisions, clean and upload to BigQuery."""
 
-import csv
 import json
 import os
 import tempfile
 from argparse import ArgumentParser
-from time import sleep
 
 import requests
 from google.cloud import bigquery
 
-CSV_FIELDS = [
-    "user",
-    "queue_slug",
-    "job_id",
-    "uuid",
-    "applied_policies",
-    "entity",
-    "entity_slug",
-    "entity_id",
-    "created_at",
-    "decision_type",
-    "job_assigned_at",
-    "typed_metadata",
-]
-
+"""Get the bearer token for Cinder from the environment"""
 cinder_bearer_token = os.environ.get("CINDER_TOKEN")
 
 
@@ -53,14 +37,6 @@ def read_json(filename: str) -> dict:
     return data
 
 
-def write_dict_to_csv(json_data, filename):
-    """Write a dictionary to a csv."""
-    with open(filename, "w") as out_file:
-        dict_writer = csv.DictWriter(out_file, CSV_FIELDS)
-        dict_writer.writeheader()
-        dict_writer.writerows(json_data)
-
-
 def cinder_addon_decisions_download(date, bearer_token):
     """Download data from Cinder - bearer_token are called here."""
     # getting overview metrics for different kpis / Deliverables
@@ -85,68 +61,20 @@ def check_json(cinder_addon_decisions_response_text):
     return query_export
 
 
-def clean_json(query_export, date):
-    """Turn the json file into a list to be input into a CSV for bq upload."""
-    fields_list = []
-    for item in query_export["items"]:
-        field_dict = {
-            "user": item["user"],
-            "queue_slug": item["queue_slug"],
-            "job_id": item["job_id"],
-            "uuid": item["uuid"],
-            "applied_policies": item["applied_policies"],
-            "entity": item["entity"],
-            "entity_slug": item["entity_slug"],
-            "entity_id": item["entity_id"],
-            "created_at": item["created_at"],
-            "decision_type": item["decision_type"],
-            "job_assigned_at": item["job_assigned_at"],
-            "typed_metadata": item["typed_metadata"],
-        }
-        fields_list.append(field_dict)
-    return fields_list
-
-
-def upload_to_bigquery(csv_data, project, dataset, table_name, date):
+def upload_to_bigquery(data, project, dataset, table_name, date):
     """Upload the data to bigquery."""
     date = date
-    print("writing json to csv")
-    partition = f"{date}".replace("-", "")
-    print(partition)
-    with tempfile.NamedTemporaryFile() as tmp_csv:
-        with open(tmp_csv.name, "w+b") as f_csv:
-            write_dict_to_csv(csv_data, f_csv.name)
-            client = bigquery.Client(project)
-            job_config = bigquery.LoadJobConfig(
-                create_disposition="CREATE_IF_NEEDED",
-                write_disposition="WRITE_TRUNCATE",
-                time_partitioning=bigquery.TimePartitioning(
-                    type_=bigquery.TimePartitioningType.DAY,
-                    field="date",
-                ),
-                skip_leading_rows=1,
-                schema=[
-                    bigquery.SchemaField("date", "DATE"),
-                    bigquery.SchemaField("user", "STRING"),
-                    bigquery.SchemaField("queue_slug", "STRING"),
-                    bigquery.SchemaField("job_id", "STRING"),
-                    bigquery.SchemaField("uuid", "STRING"),
-                    bigquery.SchemaField("applied_policies", "STRING"),
-                    bigquery.SchemaField("entity", "STRING"),
-                    bigquery.SchemaField("entity_slug", "STRING"),
-                    bigquery.SchemaField("entity_id", "STRING"),
-                    bigquery.SchemaField("created_at", "DATE"),
-                    bigquery.SchemaField("decision_type", "STRING"),
-                    bigquery.SchemaField("job_assigned_at", "STRING"),
-                    bigquery.SchemaField("typed_metadata", "STRING"),
-                ],
-            )
-            destination = f"{project}.{dataset}.{table_name}${partition}"
-            job = client.load_table_from_file(f_csv, destination, job_config=job_config)
-            print(
-                f"Writing Decisions data to {destination}. BigQuery job ID: {job.job_id}"
-            )
-            job.result()
+    # partition = f"{date}".replace("-", "")
+    client = bigquery.Client(project)
+    job_config = (
+        bigquery.LoadJobConfig(
+            create_disposition="CREATE_IF_NEEDED",
+            write_disposition="WRITE_TRUNCATE",
+        ),
+    )
+    destination = f"{project}.{dataset}.{table_name}"
+    job = client.load_table_from_json(data, destination, job_config=job_config)
+    job.result()
 
 
 def main():
@@ -165,20 +93,15 @@ def main():
     date = args.date
     bearer_token = cinder_bearer_token
 
-    data = []
+    cinder_data = []
 
     json_file = cinder_addon_decisions_download(date, bearer_token)
+    """Data returns as a dictionary with a key called 'items' and the value being a list of data"""
     query_export = check_json(json_file.text)
+    """Pull out the list from query_export["items"] and put that data into the cinder_data list"""
+    cinder_data = query_export["items"]
 
-    if query_export is not None:
-        # This section writes the tmp json data into a temp CSV file which will then be put into a BigQuery table
-        cinder_addon_decisions_data = clean_json(query_export, date)
-        data.extend(cinder_addon_decisions_data)
-    else:
-        print("no data for today")
-    sleep(5)
-
-    upload_to_bigquery(data, project, dataset, table_name, date)
+    upload_to_bigquery(cinder_data, project, dataset, table_name, date)
 
 
 if __name__ == "__main__":

258bc77af774554674ae41d23588ecbad23fd4b1
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-12-16 17:05:50 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/org_mozilla_ios_firefoxbeta_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefoxbeta_derived/event_monitoring_live_v1/materialized_view.sql
@@ -87,7 +87,7 @@ IF
   LEFT JOIN
     UNNEST(event.extra) AS event_extra
   WHERE
-    DATE(submission_timestamp) >= "2024-12-13"
+    DATE(submission_timestamp) >= "2024-12-16"
   GROUP BY
     submission_date,
     window_start,

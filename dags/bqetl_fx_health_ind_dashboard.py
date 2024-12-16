47f700424340bfefb19f55b901a3e79cfe6e4639
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2024-12-16 21:09:20 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_fx_health_ind_dashboard.py
+++ dags/bqetl_fx_health_ind_dashboard.py
@@ -183,6 +183,17 @@ with DAG(
         pool="DATA_ENG_EXTERNALTASKSENSOR",
     )
 
+    telemetry_derived__fx_health_ind_clients_daily_by_country__v1 = bigquery_etl_query(
+        task_id="telemetry_derived__fx_health_ind_clients_daily_by_country__v1",
+        destination_table="fx_health_ind_clients_daily_by_country_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     telemetry_derived__fx_health_ind_clients_daily_by_os__v1 = bigquery_etl_query(
         task_id="telemetry_derived__fx_health_ind_clients_daily_by_os__v1",
         destination_table="fx_health_ind_clients_daily_by_os_v1",
@@ -276,6 +287,10 @@ with DAG(
         date_partition_parameter=None,
         depends_on_past=False,
         parameters=["submission_date:DATE:{{macros.ds_add(ds, -1)}}"],
+    )
+
+    telemetry_derived__fx_health_ind_clients_daily_by_country__v1.set_upstream(
+        wait_for_telemetry_derived__clients_daily_joined__v1
     )
 
     telemetry_derived__fx_health_ind_clients_daily_by_os__v1.set_upstream(

b83319a013e82f797a0cc567f68063ddf6b72ce6
Marlene-M-Hirose <mhirose@mozilla.com>
2024-12-17 10:53:52 -1000

    add date to cinder downloaded data for partitioning, restore date partitioning to metadata.yaml, organize schema.yaml file to build table in a less haphazard way

--- sql/moz-fx-data-shared-prod/addon_moderations_derived/cinder_decisions_raw_v1/query.py
+++ sql/moz-fx-data-shared-prod/addon_moderations_derived/cinder_decisions_raw_v1/query.py
@@ -60,17 +60,42 @@ def check_json(cinder_addon_decisions_response_text):
     return query_export
 
 
+def add_date_to_json(query_export_contents, date):
+    """Add a date to the entries so we can partition table by this date."""
+    fields_list = []
+    for item in query_export_contents:
+        field_dict = {
+            "date": date,
+            "decision_type": item["decision_type"],
+            "user": item["user"],
+            "created_at": item["created_at"],
+            "entity_id": item["entity_id"],
+            "entity": item["entity"],
+            "uuid": item["uuid"],
+            "entity_slug": item["entity_slug"],
+            "job_id": item["job_id"],
+            "job_assigned_at": item["job_assigned_at"],
+            "typed_metadata": item["typed_metadata"],
+            "applied_policies": item["applied_policies"],
+        }
+        fields_list.append(field_dict)
+    return fields_list
+
+
 def upload_to_bigquery(data, project, dataset, table_name, date):
     """Upload the data to bigquery."""
     date = date
+    partition = f"{date}".replace("-", "")
     client = bigquery.Client(project)
-    job_config = (
-        bigquery.LoadJobConfig(
-            create_disposition="CREATE_IF_NEEDED",
-            write_disposition="WRITE_TRUNCATE",
+    job_config = bigquery.LoadJobConfig(
+        create_disposition="CREATE_IF_NEEDED",
+        write_disposition="WRITE_TRUNCATE",
+        time_partitioning=bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY,
+            field="date",
         ),
     )
-    destination = f"{project}.{dataset}.{table_name}"
+    destination = f"{project}.{dataset}.{table_name}${partition}"
     job = client.load_table_from_json(data, destination, job_config=job_config)
     job.result()
 
@@ -96,8 +121,10 @@ def main():
     json_file = cinder_addon_decisions_download(date, bearer_token)
     """Data returns as a dictionary with a key called 'items' and the value being a list of data"""
     query_export = check_json(json_file.text)
+    """Add date to each element in query_export for partitioning"""
+    query_export_contents = query_export["items"]
+    cinder_data = add_date_to_json(query_export_contents, date)
     """Pull out the list from query_export["items"] and put that data into the cinder_data list"""
-    cinder_data = query_export["items"]
 
     upload_to_bigquery(cinder_data, project, dataset, table_name, date)
 

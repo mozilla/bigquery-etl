56bbdb9c149caca3254a938a4cca17437a15ff2a
Chelsey Beck <cbeck@mozilla.com>
2024-12-18 08:45:23 -0800

    fixing error in group by causing abundance of records

--- sql/moz-fx-data-shared-prod/telemetry_derived/newtab_interactions_hourly_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/newtab_interactions_hourly_v1/query.sql
@@ -140,7 +140,7 @@ glean_summary AS (
     -- exclude suspicious activity
     NOT (user_event_count > 50)
   GROUP BY
-    submission_timestamp,
+    submission_date,
     recommendation_id,
     tile_id,
     position

a36feeca10cc822b4bf4410c1f0a9cbe91b78988
Gaurang Katre <gkatre@gmail.com>
2024-12-18 15:27:16 -0800

    formatting

--- sql/moz-fx-data-shared-prod/org_mozilla_broken_site_report_derived/broken_site_root_domain_daily_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_broken_site_report_derived/broken_site_root_domain_daily_aggregates_v1/query.sql
@@ -6,15 +6,23 @@ WITH reports AS (
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND metrics.text2.broken_site_report_description != "" -- Exclude empty descriptions
-  qualify row_number() over
-    (partition by document_id order by submission_timestamp) = 1 -- Only include the first submission for each document_id
+  QUALIFY
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        document_id
+      ORDER BY
+        submission_timestamp
+    ) = 1 -- Only include the first submission for each document_id
 )
-
 SELECT
   DATE(submission_timestamp) AS report_date,
-  REGEXP_EXTRACT(metrics.url2.broken_site_report_url, r'https?://(?:www\.)?([^/]+)') AS root_domain, -- Extract root domain
+  REGEXP_EXTRACT(
+    metrics.url2.broken_site_report_url,
+    r'https?://(?:www\.)?([^/]+)'
+  ) AS root_domain, -- Extract root domain
   COUNT(*) AS daily_domain_reports
 FROM
   reports
 GROUP BY
-  report_date, root_domain
+  report_date,
+  root_domain

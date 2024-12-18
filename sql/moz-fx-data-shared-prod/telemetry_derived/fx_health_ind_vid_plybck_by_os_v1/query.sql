5f689afb329256cd926d2ac238579a1d10eb92d8
Katie Windau <kwindau@mozilla.com>
2024-12-18 12:58:32 -0600

    Fix SQL formatting

--- sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_vid_plybck_by_os_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_vid_plybck_by_os_v1/query.sql
@@ -5,7 +5,7 @@ SELECT
     `moz-fx-data-shared-prod.udf.histogram_max_key_with_nonzero_value`(
       payload.processes.content.histograms.video_play_time_ms
     )
-  ) / count(DISTINCT client_id) / 60000 AS play_time_ratio
+  ) / COUNT(DISTINCT client_id) / 60000 AS play_time_ratio
 FROM
   `moz-fx-data-shared-prod.telemetry.main`
 WHERE

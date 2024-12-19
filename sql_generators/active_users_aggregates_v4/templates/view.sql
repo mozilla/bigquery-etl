c090d24b02a8341f98e377f27d71aba885baf5e3
kik-kik <kignasiak@mozilla.com>
2024-12-19 15:28:58 +0100

    feat: add partnership to telemetry.active_users_aggregates view instead of the template

--- sql_generators/active_users_aggregates_v4/templates/view.sql
+++ sql_generators/active_users_aggregates_v4/templates/view.sql
@@ -11,10 +11,5 @@ SELECT
   `mozfun.norm.browser_version_info`(app_version).patch_revision AS app_version_patch_revision,
   `mozfun.norm.browser_version_info`(app_version).is_major_release AS app_version_is_major_release,
   `mozfun.norm.os`(os) AS os_grouped,
-  CASE 
-    WHEN distribution_id LIKE "%vivo%"
-      THEN "vivo"
-    ELSE "other"
-  END AS partnership,
 FROM
   `{{ project_id }}.{{ app_name }}_derived.{{ table_name }}`

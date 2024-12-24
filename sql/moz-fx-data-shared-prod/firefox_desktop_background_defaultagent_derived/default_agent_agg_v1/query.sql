ca5643dbcf72654cffa5bb3dda4cc9986b9514d9
Winnie <wichan@mozilla.com>
2024-12-24 15:54:13 -0800

    Fixed column name

--- sql/moz-fx-data-shared-prod/firefox_desktop_background_defaultagent_derived/default_agent_agg_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_background_defaultagent_derived/default_agent_agg_v1/query.sql
@@ -1,6 +1,6 @@
 SELECT
   DATE(submission_timestamp) AS submission_date,
-  mozfun.norm.truncate_version(client_info.app_display_version, 'major') AS major_build_version,
+  mozfun.norm.truncate_version(client_info.app_display_version, 'major') AS build_version_major,
   normalized_channel,
   metrics.string.system_default_browser,
   metrics.string.system_default_previous_browser,

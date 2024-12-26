24ea91a8118c93df5f9509d6a4bd8baa6d56246e
alekhyamoz <akommasani@mozilla.com>
2024-12-26 12:22:53 -0500

    Add views

---
+++ sql/moz-fx-data-shared-prod/telemetry/fx_cert_error_unique_users_os_v1/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.fx_cert_error_unique_users_os`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.fx_cert_error_unique_users_os_v1`

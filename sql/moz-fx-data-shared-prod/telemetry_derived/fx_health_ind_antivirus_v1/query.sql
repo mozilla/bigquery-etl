5e949bc80b8d702c8df17e62c4d05c2207b43c22
Katie Windau <kwindau@mozilla.com>
2025-01-02 15:55:21 -0600

    Remove clustering

--- sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_antivirus_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_antivirus_v1/query.sql
@@ -1,7 +1,7 @@
 SELECT
   DATE(submission_timestamp) AS submission_date,
-  environment.system.sec.antivirus AS antivirus_name,
-  COUNT(DISTINCT client_id) AS dau
+  environment.system.sec.antivirus,
+  COUNT(DISTINCT client_id) AS unique_clients
 FROM
   `moz-fx-data-shared-prod.telemetry.main_1pct`
 WHERE
@@ -9,4 +9,4 @@ WHERE
   AND DATE(submission_timestamp) = @submission_date
 GROUP BY
   submission_date,
-  antivirus_name
+  environment.system.sec.antivirus

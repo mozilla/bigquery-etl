a23fb1124870ff325c2483a42ad2c7d62f03749d
Katie Windau <kwindau@mozilla.com>
2025-01-03 13:08:59 -0600

    Initial commit, create new agg table for Fx Health Ind dashboard

---
+++ sql/moz-fx-data-shared-prod/telemetry/fx_health_ind_np_by_install_typ/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.fx_health_ind_np_by_install_typ`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.fx_health_ind_np_by_install_typ_v1`
---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_np_by_install_typ_v1/query.sql
@@ -0,0 +1,61 @@
+WITH first_install_type AS (
+  SELECT
+    client_id,
+    event_object AS installer_type,
+    ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY submission_date ASC) AS rnk
+  FROM
+    `moz-fx-data-shared-prod.telemetry.events`
+  WHERE
+    event_category = 'installation'
+    AND sample_id = 0
+  QUALIFY
+    rnk = 1
+),
+staging AS (
+  SELECT
+    client_id,
+    first_seen_date,
+    submission_date,
+    days_interacted_bits & days_visited_1_uri_bits AS active_days_bits
+  FROM
+    `moz-fx-data-shared-prod.telemetry.clients_last_seen`
+  WHERE
+    first_seen_date = DATE_SUB(@submission_date, INTERVAL 7 DAY)
+    AND submission_date
+    BETWEEN DATE_SUB(@submission_date, INTERVAL 7 DAY)
+    AND DATE_SUB(@submission_date, INTERVAL 1 DAY)
+    AND sample_id = 0
+),
+cls AS (
+  SELECT
+    stg.client_id,
+    stg.first_seen_date,
+    fit.installer_type,
+    stg.submission_date,
+    stg.active_days_bits
+  FROM
+    staging stg
+  JOIN
+    first_install_type fit
+    ON stg.client_id = fit.client_id
+),
+final_stg AS (
+  SELECT
+    first_seen_date,
+    installer_type,
+    COUNT(DISTINCT(CLIENT_ID)) AS new_profiles,
+    SUM(BIT_COUNT(active_days_bits)) AS sum_active_days_bit_count_for_new_profiles
+  FROM
+    cls
+  GROUP BY
+    first_seen_date,
+    installer_type
+)
+SELECT
+  first_seen_date,
+  installer_type,
+  new_profiles,
+  sum_active_days_bit_count_for_new_profiles,
+  SAFE_DIVIDE(sum_active_days_bit_count_for_new_profiles, new_profiles) AS ratio_of_np_days_active_bits_vs_np_first_7_days
+FROM
+  final_stg

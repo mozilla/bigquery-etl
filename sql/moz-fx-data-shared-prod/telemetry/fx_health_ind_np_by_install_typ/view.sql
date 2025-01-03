4b15ccb7c3a8a4f8f9ee739a3a828cfa97485c95
Katie Windau <kwindau@mozilla.com>
2025-01-03 14:08:32 -0600

    Rename typ to type in table and view

--- sql/moz-fx-data-shared-prod/telemetry/fx_health_ind_np_by_install_typ/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.fx_health_ind_np_by_install_typ`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.fx_health_ind_np_by_install_typ_v1`

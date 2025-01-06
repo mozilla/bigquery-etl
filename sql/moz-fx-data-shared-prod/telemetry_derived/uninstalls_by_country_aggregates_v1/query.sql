f26856e59f2dbf6b59bf1abbbcdc3da485b3ca79
Katie Windau <kwindau@mozilla.com>
2025-01-06 15:13:31 -0600

    Add 1 more column to uninstalls_by_country_aggregates_v1

--- sql/moz-fx-data-shared-prod/telemetry_derived/uninstalls_by_country_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/uninstalls_by_country_aggregates_v1/query.sql
@@ -1,7 +1,8 @@
 SELECT
   DATE(submission_timestamp) AS submission_date,
   normalized_country_code,
-  COUNT(DISTINCT client_id) AS nbr_unique_fx_release_channel_clients_with_uninstalls
+  COUNT(DISTINCT client_id) AS nbr_unique_fx_release_channel_clients_with_uninstalls,
+  AVG(environment.profile.creation_date) AS avg_profile_creation_date
 FROM
   `moz-fx-data-shared-prod.telemetry.uninstall`
 WHERE

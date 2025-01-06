f8dc690e67775efbd5ac4eecb66582f307a73dd2
Katie Windau <kwindau@mozilla.com>
2025-01-06 10:23:17 -0600

    DENG-6883 - build share of user cancelled installs aggregate table

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/user_cancelled_install_share_v1/query.sql
@@ -0,0 +1,15 @@
+SELECT
+  DATE(submission_timestamp) AS submission_date,
+  installer_type,
+  update_channel,
+  COUNT(1) AS nbr_install_attempts,
+  ROUND(AVG(IF(User_cancelled = TRUE, 1, 0)) * 100, 1) AS user_cancelled,
+  ROUND(AVG(IF(Install_timeout = TRUE, 1, 0)) * 100, 1) AS timeout,
+FROM
+  `moz-fx-data-shared-prod.firefox_installer.install`
+WHERE
+  DATE(submission_timestamp) = @submission_date
+GROUP BY
+  DATE(submission_timestamp),
+  installer_type,
+  update_channel

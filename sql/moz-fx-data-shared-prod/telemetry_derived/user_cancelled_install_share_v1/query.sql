46a8d375229faf6c509ff30152f12b78883df796
Katie Windau <kwindau@mozilla.com>
2025-01-06 10:41:38 -0600

    Add backticks around timeout column

--- sql/moz-fx-data-shared-prod/telemetry_derived/user_cancelled_install_share_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/user_cancelled_install_share_v1/query.sql
@@ -4,7 +4,7 @@ SELECT
   update_channel,
   COUNT(1) AS nbr_install_attempts,
   ROUND(AVG(IF(User_cancelled = TRUE, 1, 0)) * 100, 1) AS user_cancelled,
-  ROUND(AVG(IF(Install_timeout = TRUE, 1, 0)) * 100, 1) AS timeout,
+  ROUND(AVG(IF(Install_timeout = TRUE, 1, 0)) * 100, 1) AS `timeout`,
 FROM
   `moz-fx-data-shared-prod.firefox_installer.install`
 WHERE

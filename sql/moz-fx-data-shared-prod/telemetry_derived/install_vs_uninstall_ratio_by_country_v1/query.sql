70e47f2bbb185d241bf370251587144aab6e7757
Katie Windau <kwindau@mozilla.com>
2025-01-07 13:52:30 -0600

    Fix SQL format

--- sql/moz-fx-data-shared-prod/telemetry_derived/install_vs_uninstall_ratio_by_country_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/install_vs_uninstall_ratio_by_country_v1/query.sql
@@ -1,37 +1,33 @@
-WITH installs
-AS
-  (
-    SELECT
-      DATE(submission_timestamp) AS submission_date,
-      normalized_country_code,
-      COUNT(*) AS installs_count
-    FROM
-      `moz-fx-data-shared-prod.firefox_installer.install`
-    WHERE
-      update_channel = 'release'
-      AND installer_type = 'stub'
-      AND DATE(submission_timestamp) = @submission_date
-    GROUP BY
-      submission_date,
-      normalized_country_code
-  ),
-  uninstalls
-AS
-  (
-    SELECT
-      DATE(submission_timestamp) AS submission_date,
-      normalized_country_code,
-      COUNT(DISTINCT client_id) AS uninstalls_count,
-    FROM
-      `moz-fx-data-shared-prod.telemetry.uninstall`
-    WHERE
-      DATE(submission_timestamp) = @submission_date
-      AND application.name = 'Firefox'
-      AND application.channel = 'release'
-    GROUP BY
-      submission_date,
-      normalized_country_code
-  )
+WITH installs AS (
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    normalized_country_code,
+    COUNT(*) AS installs_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_installer.install`
+  WHERE
+    update_channel = 'release'
+    AND installer_type = 'stub'
+    AND DATE(submission_timestamp) = @submission_date
+  GROUP BY
+    submission_date,
+    normalized_country_code
+),
+uninstalls AS (
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    normalized_country_code,
+    COUNT(DISTINCT client_id) AS uninstalls_count,
+  FROM
+    `moz-fx-data-shared-prod.telemetry.uninstall`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND application.name = 'Firefox'
+    AND application.channel = 'release'
+  GROUP BY
+    submission_date,
+    normalized_country_code
+)
 SELECT
   COALESCE(a.submission_date, b.submission_date) AS submission_date,
   COALESCE(a.normalized_country_code, b.normalized_country_code) AS normalized_country_code,

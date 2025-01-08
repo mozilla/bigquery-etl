de42071d2902813aa3e6e9a6af18edd4e1d2adfd
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-01-08 20:38:15 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/jira_service_desk/issue/view.sql
+++ sql/moz-fx-data-shared-prod/jira_service_desk/issue/view.sql
@@ -1,7 +1,69 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.jira_service_desk.issue`
 AS
+WITH requests AS (
+  -- Join requests related data
+  SELECT
+    * EXCEPT (id, description),
+    id AS request_id,
+    description AS request_description
+  FROM
+    (
+      SELECT
+        * EXCEPT (_fivetran_synced)
+      FROM
+        `moz-fx-data-shared-prod.jira_service_desk_syndicate.request`
+    ) AS request
+  LEFT JOIN
+    (
+      SELECT
+        * EXCEPT (_fivetran_synced, service_desk_id)
+      FROM
+        `moz-fx-data-shared-prod.jira_service_desk_syndicate.request_type`
+    ) AS request_type
+    ON request.request_type_id = request_type.id
+),
+fields AS (
+  -- Join fields related data
+  SELECT
+    * EXCEPT (id, field_id, description),
+    id AS field_id,
+    description AS field_description
+  FROM
+    (
+      SELECT
+        * EXCEPT (_fivetran_synced, _fivetran_deleted, name),
+        name AS field_name
+      FROM
+        `moz-fx-data-shared-prod.jira_service_desk_syndicate.field`
+    ) AS field
+  LEFT JOIN
+    (
+      SELECT
+        * EXCEPT (_fivetran_synced)
+      FROM
+        `moz-fx-data-shared-prod.jira_service_desk_syndicate.issue_field_history`
+    ) AS field_history
+    ON field_history.field_id = field.id
+  LEFT JOIN
+    (
+      SELECT
+        * EXCEPT (_fivetran_synced, id, name),
+        id AS field_option_id,
+        name AS field_option_name
+      FROM
+        `moz-fx-data-shared-prod.jira_service_desk_syndicate.field_option`
+    ) AS field_option
+    ON field.id = "customfield_" || field_option.field_option_id
+)
+-- Join requests and fields data with issues
 SELECT
-  *
+  * EXCEPT (issue_id, parent_id)
 FROM
-  `moz-fx-data-shared-prod.jira_service_desk_syndicate.issue`
+  `moz-fx-data-shared-prod.jira_service_desk_syndicate.issue` AS issue
+LEFT JOIN
+  requests
+  ON issue.id = requests.issue_id
+LEFT JOIN
+  fields
+  ON issue.id = fields.issue_id

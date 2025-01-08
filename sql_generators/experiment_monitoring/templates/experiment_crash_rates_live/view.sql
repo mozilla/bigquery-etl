f884702b66af4e08f3c415c6e40ec30b468f73e5
Anna Scholtz <anna@scholtzan.net>
2025-01-08 14:21:59 -0800

    Simplify experiment crash rates view logic

--- sql_generators/experiment_monitoring/templates/experiment_crash_rates_live/view.sql
+++ sql_generators/experiment_monitoring/templates/experiment_crash_rates_live/view.sql
@@ -2,7 +2,6 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.experiment_crash_rates_live`
 AS
-WITH crash_events AS (
 {% for app_dataset in applications %}
   SELECT
     experiment,
@@ -28,21 +27,3 @@ FROM
   `moz-fx-data-shared-prod.telemetry_derived.experiment_crash_aggregates_v1`
 WHERE
   window_start <= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
-)
-SELECT
-  crash_events.experiment AS experiment,
-  crash_events.branch AS branch,
-  window_start,
-  window_end,
-  crash_process_type,
-  crash_count,
-  crash_count / enrollments.value AS estimated_crash_rate
-  FROM
-    crash_events
-  LEFT JOIN
-    `moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_cumulative_population_estimate_v1` AS enrollments
-  ON enrollments.time = crash_events.window_start AND
-    enrollments.branch = crash_events.branch AND
-    enrollments.experiment = crash_events.experiment
-  WHERE
-    enrollments.time > TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))

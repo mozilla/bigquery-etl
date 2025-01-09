ae8150f7389cc1ade598f78979b35249f6ff02d1
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-01-09 08:01:28 -0600

    DENG-6883 & DENG-6884 - Create desktop_acquisition_funnel_aggregates table (#6780)
    * Create desktop_acquisition_funnel_aggregates_v1
    
    * Add minimum row count check
    
    * Fix YAML format, remove trailing space

---
+++ sql/moz-fx-data-shared-prod/telemetry/desktop_acquisition_funnel_aggregates/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.desktop_acquisition_funnel_aggregates`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.desktop_acquisition_funnel_aggregates_v1`

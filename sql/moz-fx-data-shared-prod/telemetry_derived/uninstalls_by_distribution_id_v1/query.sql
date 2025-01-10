75207bdf536c8a41d2dc61993d1eda6ea81f92fa
Katie Windau <kwindau@mozilla.com>
2025-01-10 12:37:46 -0600

    Create uninstalls_by_distribution_id_v1

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/uninstalls_by_distribution_id_v1/query.sql
@@ -0,0 +1,12 @@
+SELECT
+  DATE(submission_timestamp) AS submission_date,
+  environment.partner.distribution_id AS distribution_id,
+  COUNT(DISTINCT client_id) AS nbr_unique_clients_uninstalling,
+FROM
+  `moz-fx-data-shared-prod.telemetry.uninstall`
+WHERE
+  date(submission_timestamp) = @submission_date
+  AND application.name = 'Firefox'
+GROUP BY
+  submission_date,
+  distribution_id

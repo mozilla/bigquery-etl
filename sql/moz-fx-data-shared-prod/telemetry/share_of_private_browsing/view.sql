8d4f248658a72af2bc2b194b31669fea2fb64e61
Katie Windau <kwindau@mozilla.com>
2025-01-14 15:58:40 -0600

    Remove new table and consolidate with Alekhya's table

--- sql/moz-fx-data-shared-prod/telemetry/share_of_private_browsing/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.share_of_private_browsing`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.share_of_private_browsing_v1`

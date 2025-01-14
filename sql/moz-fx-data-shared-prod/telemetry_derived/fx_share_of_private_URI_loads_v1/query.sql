0347c4022784b44c337bfdf63cf37f82c734c8bd
Katie Windau <kwindau@mozilla.com>
2025-01-14 16:14:54 -0600

    Remove column since unlikely to be helpful at this grain

--- sql/moz-fx-data-shared-prod/telemetry_derived/fx_share_of_private_URI_loads_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_share_of_private_URI_loads_v1/query.sql
@@ -8,8 +8,7 @@ SELECT
   IF(total_uri_count_private_mode < 0, TRUE, FALSE) AS client_had_negative_uri_count_private,
   IF(total_uri_count_normal_mode < 0, TRUE, FALSE) AS client_negative_uri_count_normal,
   SUM(total_uri_count_private_mode) AS total_uri_count_private_mode,
-  SUM(total_uri_count_normal_mode) AS total_uri_count_normal_mode,
-  COUNT(DISTINCT(client_id)) AS nbr_clients
+  SUM(total_uri_count_normal_mode) AS total_uri_count_normal_mode
 FROM
   `moz-fx-data-shared-prod.telemetry.clients_daily`
 WHERE

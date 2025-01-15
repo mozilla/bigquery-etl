b81a7a09bb8a9c1c41f3961640622190c7041da1
kik-kik <42538694+kik-kik@users.noreply.github.com>
2025-01-15 11:37:26 +0100

    feat(DENG-6890): Update mobile kpi support metric aggregates to support dma analysis (#6783)
    * feat: add device_type and device_manufacturer to KPI support metrics datasets
    
    * feat: ensure all mobile kpi support metric aggregated dataset have device_type and device_manufacturer dimensions
    
    * feat: add device_type and device_manfacturer to the mobile kpi support metric aggregate queries
    
    * feat: tweak active_users view device_type value setting logic
    
    * feat: lower case device_manufacturer for some cases where the same manufacturer value has different casing
    
    * feat: bucket device_manufacturers into "other" bucket in case when they have a lower occurance to prevent from exploding the aggregate tables
    
    * fix: retention_clients some selection fields being ambiguous
    
    * feat: Use dense rank instead of count for bucketing all device_manufacturers into other bucket if they are rank 151 or more
    
    * fix: using incorrect source table for ranking device_manufacturers
    
    * feat: make sure that device_manufacturer is lowered prior to joining

--- sql_generators/mobile_kpi_support_metrics/templates/retention.query.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/retention.query.sql
@@ -1,4 +1,26 @@
 {{ header }}
+WITH device_manufacturer_counts AS (
+  SELECT
+    submission_date,
+    device_manufacturer,
+    RANK() OVER(PARTITION BY submission_date ORDER BY COUNT(*) DESC) AS manufacturer_rank,
+  FROM
+  `{{ project_id }}.{{ dataset }}.retention_clients`
+  WHERE
+    {% raw %}
+    {% if is_init() %}
+      metric_date < DATE_SUB(CURRENT_DATE, INTERVAL 27 DAY)
+      AND submission_date < CURRENT_DATE
+    {% else %}
+      metric_date = DATE_SUB(@submission_date, INTERVAL 27 DAY)
+      AND submission_date = @submission_date
+    {% endif %}
+    {% endraw %}
+  GROUP BY
+    submission_date,
+    device_manufacturer
+)
+
 SELECT
   metric_date,
   first_seen_date,
@@ -18,8 +40,14 @@ SELECT
   COUNTIF(retained_week_4_new_profile) AS retained_week_4_new_profiles,
   COUNTIF(new_profile_metric_date) AS new_profiles_metric_date,
   COUNTIF(repeat_profile) AS repeat_profiles,
+  device_type,
+  -- Bucket device manufacturers with low count prior to aggregation
+  IF(manufacturer_rank <= 150, device_manufacturer, "other") AS device_manufacturer,
 FROM
   `{{ project_id }}.{{ dataset }}.retention_clients`
+LEFT JOIN
+  device_manufacturer_counts
+  USING(submission_date, device_manufacturer)
 WHERE
   {% raw %}
   {% if is_init() %}
@@ -38,6 +66,8 @@ GROUP BY
   country,
   app_version,
   locale,
+  device_type,
+  device_manufacturer,
   is_mobile
   {% for field in product_attribution_fields.values() if not field.client_only %}
     {% if loop.first %},{% endif %}

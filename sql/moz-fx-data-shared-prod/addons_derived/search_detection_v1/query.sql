40ec6545cbefdec72138cbfdf5feacfc5145a574
kik-kik <kignasiak@mozilla.com>
2025-01-16 14:57:11 +0100

    feat: add search_detection_v1 aggregate

---
+++ sql/moz-fx-data-shared-prod/addons_derived/search_detection_v1/query.sql
@@ -0,0 +1,37 @@
+SELECT
+  submission_date,
+  normalized_channel,
+  country,
+  app_name,
+  app_version,
+  os_name,
+  os_version,
+  addon_id,
+  addon_version,
+  method,
+  `object`,
+  `value`,
+  udf.get_key(event_map_values, 'from') AS from_etld,
+  udf.get_key(event_map_values, 'to') AS to_etld,
+  COUNT(*) AS event_count,
+  COUNT(DISTINCT client_id) AS unique_clients_cnt,
+FROM
+  `moz-fx-data-shared-prod.tmp.addon_events_clients`
+WHERE
+  submission_date = @submission_date
+  AND event_category = "addonsSearchDetection"
+GROUP BY
+  submission_date,
+  normalized_channel,
+  country,
+  app_name,
+  app_version,
+  os_name,
+  os_version,
+  addon_id,
+  addon_version,
+  method,
+  `object`,
+  `value`,
+  from_etld,
+  to_etld

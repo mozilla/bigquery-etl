mozilla/bigquery-etl

commit 2cea3c6c9df336996c3b3573a8187f0fab901612

Fix ADM daily aggregates (#6845)
* Fix ADM daily aggregates

* Address feedback

* Reformat SQL

* Add mock data for quick_suggest_v1 to fix tests

Also change block IDs to be integers across the board

* Fix expected data

* More fixes to expected data

* Add quick_suggest_ping_type, and a click that should be ignored

* Move quick_suggest_context_id to uuid

* Fix expected data

sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_aggregates_v1/query.sql
@@ -5,7 +5,7 @@ SELECT
   COUNT(*) AS impressions,
   COUNTIF(is_clicked) AS clicks,
 FROM
-  `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v2`
+  `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v3`
 WHERE
   DATE(submission_timestamp) = @submission_date
   AND LENGTH(sanitized_query) > 0

sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/query.sql
@@ -34,6 +34,32 @@ WITH impressions AS (
     `moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_impression_v1`
   WHERE
     DATE(submission_timestamp) = @submission_date
+  UNION ALL
+  SELECT
+    TIMESTAMP_TRUNC(submission_timestamp, SECOND) AS submission_timestamp,
+    metrics.string.quick_suggest_request_id AS request_id,
+    NULL AS telemetry_query,
+    metrics.string.quick_suggest_advertiser AS advertiser,
+    SAFE_CAST(metrics.string.quick_suggest_block_id AS INT64) AS block_id,
+    metrics.uuid.quick_suggest_context_id AS context_id,
+    sample_id,
+    metrics.boolean.quick_suggest_is_clicked AS is_clicked,
+    client_info.locale AS locale,
+    metadata.geo.country,
+    metadata.geo.subdivision1 AS region,
+    normalized_os,
+    normalized_os_version,
+    normalized_channel,
+    metrics.quantity.quick_suggest_position AS position,
+    metrics.url2.quick_suggest_reporting_url AS reporting_url,
+    NULL AS scenario,
+    -- Truncate to just Firefox major version
+    SPLIT(client_info.app_display_version, '.')[SAFE_OFFSET(0)] AS version,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_stable.quick_suggest_v1`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND metrics.string.quick_suggest_ping_type = 'quicksuggest-impression'
 ),
 sanitized_queries AS (
   SELECT

tests/sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/test_single_day/expect.yaml
@@ -2,7 +2,7 @@
   request_id: rid1
   session_id: sid1
   sequence_no: 1
-  block_id: bid1
+  block_id: 101
   context_id: cid1
   country: US
   is_clicked: False
@@ -22,7 +22,7 @@
   submission_timestamp: 2021-01-01T12:00:00+00:00
   version: "94"
 - advertiser: awesome guitar store
-  block_id: bid2
+  block_id: 102
   context_id: cid2
   country: US
   is_clicked: true
@@ -36,7 +36,7 @@
   scenario: online
   submission_timestamp: 2021-01-01T12:03:00+00:00
   version: "94"
-- block_id: bid7
+- block_id: 107
   context_id: cid7
   request_id: rid7
   session_id: sid7
@@ -58,3 +58,61 @@
   scenario: online
   submission_timestamp: 2021-01-01T12:07:00+00:00
   version: "94"
+- advertiser: acme corp
+  block_id: 201
+  browser: firefox(134)
+  context_id: cid11
+  country: US
+  form_factor: desktop
+  is_clicked: false
+  locale: en-US
+  normalized_channel: release
+  normalized_os: macOS
+  normalized_os_version: 15.2
+  os_family: macos
+  position: 11
+  query: anvils
+  region: CA
+  reporting_url: https://example.com/acme-corp
+  request_id: rid11
+  sequence_no: 11
+  session_id: sid11
+  submission_timestamp: "2021-01-01T13:01:00+00:00"
+  timestamp: "2021-01-01T13:01:00+00:00"
+  version: "134"
+- advertiser: dunder mifflin
+  block_id: 202
+  browser: firefox(134)
+  context_id: cid12
+  country: US
+  form_factor: desktop
+  is_clicked: true
+  locale: en-US
+  normalized_channel: beta
+  normalized_os: macOS
+  normalized_os_version: 15.0
+  os_family: macos
+  position: 12
+  query: "best paper"
+  region: PA
+  reporting_url: https://example.com/dunder-mifflin
+  request_id: rid12
+  sequence_no: 12
+  session_id: sid12
+  submission_timestamp: "2021-01-01T13:02:00+00:00"
+  timestamp: "2021-01-01T13:02:00+00:00"
+  version: "134"
+- advertiser: duff beer
+  block_id: 203
+  context_id: cid13
+  country: US
+  is_clicked: false
+  locale: en-US
+  normalized_channel: nightly
+  normalized_os: Windows
+  normalized_os_version: 11.0
+  position: 13
+  region: OR
+  reporting_url: https://example.com/duff-beer
+  submission_timestamp: "2021-01-01T13:03:00+00:00"
+  version: "134"

tests/sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/test_single_day/moz-fx-data-shared-prod.contextual_services_stable.quicksuggest_impression_v1.yaml
@@ -2,7 +2,7 @@
   request_id: rid1
   search_query: null
   advertiser: shoe store
-  block_id: bid1
+  block_id: 101
   context_id: cid1
   sample_id: null
   is_clicked: false
@@ -22,7 +22,7 @@
   request_id: null
   search_query: null
   advertiser: awesome guitar store
-  block_id: bid2
+  block_id: 102
   context_id: cid2
   sample_id: null
   is_clicked: true
@@ -42,7 +42,7 @@
   request_id: rid7
   search_query: null
   advertiser: null
-  block_id: bid7
+  block_id: 107
   context_id: cid7
   sample_id: null
   is_clicked: false

tests/sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/test_single_day/moz-fx-data-shared-prod.firefox_desktop_stable.quick_suggest_v1.yaml
@@ -1,0 +1,104 @@
+- submission_timestamp: "2021-01-01 13:01:00"
+  client_info:
+    app_display_version: "134.0.1"
+    locale: en-US
+  metadata:
+    geo:
+      country: US
+      subdivision1: CA
+  metrics:
+    boolean:
+      quick_suggest_is_clicked: false
+    quantity:
+      quick_suggest_position: 11
+    string:
+      quick_suggest_advertiser: acme corp
+      quick_suggest_block_id: 201
+      quick_suggest_ping_type: "quicksuggest-impression"
+      quick_suggest_request_id: rid11
+    url2:
+      quick_suggest_reporting_url: "https://example.com/acme-corp"
+    uuid:
+      quick_suggest_context_id: cid11
+  normalized_channel: release
+  normalized_os: macOS
+  normalized_os_version: "15.2"
+  sample_id: null
+- submission_timestamp: "2021-01-01 13:02:00"
+  client_info:
+    app_display_version: "134.0.1"
+    locale: en-US
+  metadata:
+    geo:
+      country: US
+      subdivision1: PA
+  metrics:
+    boolean:
+      quick_suggest_is_clicked: true
+    quantity:
+      quick_suggest_position: 12
+    string:
+      quick_suggest_advertiser: dunder mifflin
+      quick_suggest_block_id: 202
+      quick_suggest_ping_type: "quicksuggest-impression"
+      quick_suggest_request_id: rid12
+    url2:
+      quick_suggest_reporting_url: "https://example.com/dunder-mifflin"
+    uuid:
+      quick_suggest_context_id: cid12
+  normalized_channel: beta
+  normalized_os: macOS
+  normalized_os_version: "15.0"
+  sample_id: null
+- submission_timestamp: "2021-01-01 13:03:00"
+  client_info:
+    app_display_version: "134.0.1"
+    locale: en-US
+  metadata:
+    geo:
+      country: US
+      subdivision1: OR
+  metrics:
+    boolean:
+      quick_suggest_is_clicked: false
+    quantity:
+      quick_suggest_position: 13
+    string:
+      quick_suggest_advertiser: duff beer
+      quick_suggest_block_id: 203
+      quick_suggest_ping_type: "quicksuggest-impression"
+      quick_suggest_request_id: null
+    url2:
+      quick_suggest_reporting_url: "https://example.com/duff-beer"
+    uuid:
+      quick_suggest_context_id: cid13
+  normalized_channel: nightly
+  normalized_os: Windows
+  normalized_os_version: "11.0"
+  sample_id: null
+- submission_timestamp: "2021-01-01 13:04:00"
+  client_info:
+    app_display_version: "134.0.1"
+    locale: en-US
+  metadata:
+    geo:
+      country: US
+      subdivision1: WA
+  metrics:
+    boolean:
+      quick_suggest_is_clicked: true
+    quantity:
+      quick_suggest_position: 14
+    string:
+      quick_suggest_advertiser: umbrella corp
+      quick_suggest_block_id: 204
+      quick_suggest_ping_type: "quicksuggest-click"
+      quick_suggest_request_id: rid14
+    url2:
+      quick_suggest_reporting_url: "https://example.com/umbrella-corp"
+    uuid:
+      quick_suggest_context_id: cid14
+  normalized_channel: release
+  normalized_os: Windows
+  normalized_os_version: "11.0"
+  sample_id: null

tests/sql/moz-fx-data-shared-prod/search_terms_derived/suggest_impression_sanitized_v3/test_single_day/moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3.yaml
@@ -31,3 +31,36 @@
   form_factor: desktop
   browser: firefox(96)
   os_family: macos
+- timestamp: "2021-01-01 13:01:00.000000"
+  request_id: rid11
+  session_id: sid11
+  sequence_no: 11
+  query: "anvils"
+  country: null
+  region: null
+  dma: null
+  form_factor: desktop
+  browser: firefox(134)
+  os_family: macos
+- timestamp: "2021-01-01 13:02:00.000000"
+  request_id: rid12
+  session_id: sid12
+  sequence_no: 12
+  query: "best paper"
+  country: null
+  region: null
+  dma: null
+  form_factor: desktop
+  browser: firefox(134)
+  os_family: macos
+- timestamp: "2021-01-01 13:04:00.000000"
+  request_id: rid14
+  session_id: sid14
+  sequence_no: 14
+  query: "adravil"
+  country: null
+  region: null
+  dma: null
+  form_factor: desktop
+  browser: firefox(134)
+  os_family: windows

commit eda800c5a912f4e67b2cc7e4abe0735440432d09

Fix adm_daily_aggregates_v1
suggest_impression_sanitized_v3 renames sanitized_query to query, which
broke this job after https://github.com/mozilla/bigquery-etl/pull/6845.

This should fix it.

sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_aggregates_v1/query.sql
@@ -1,6 +1,6 @@
 SELECT
   @submission_date AS submission_date,
-  sanitized_query AS query,
+  query,
   block_id,
   COUNT(*) AS impressions,
   COUNTIF(is_clicked) AS clicks,
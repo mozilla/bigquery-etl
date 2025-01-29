f520e16b6c7c52f630d6e686e21ebcc991287bbb
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-01-29 14:49:35 -0600

    DENG-7723 Clean up/optimize new Fx Health indicator tables (#6907)
    * Remove unnecessary filter
    
    * Remove unnecessary filter
    
    * Remove unnecessary filter
    
    * Remove unnecessary filter
    
    * Remove unnecessary filter
    
    * Remove unnecessary filter
    
    * Add channel to query to make table more usable for others, and update view to be release only so Fx Health dashboard still works as expected
    
    * Add normalized OS to the table to make it more usable for other use cases, and update view to be Windows only so Fx Health dashboard still works as expected
    
    * Update clustering

--- sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_searches_by_provider_v2/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_searches_by_provider_v2/query.sql
@@ -8,7 +8,6 @@ FROM
   `moz-fx-data-shared-prod.telemetry.clients_daily`
 WHERE
   submission_date_s3 = @submission_date
-  AND app_name = 'Firefox'
 GROUP BY
   submission_date,
   default_search_engine,
--- sql/moz-fx-data-shared-prod/telemetry_derived/fx_share_of_private_URI_loads_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_share_of_private_URI_loads_v1/query.sql
@@ -13,7 +13,6 @@ FROM
   `moz-fx-data-shared-prod.telemetry.clients_daily`
 WHERE
   submission_date = @submission_date
-  AND app_name = 'Firefox'
 GROUP BY
   submission_date,
   sample_id,

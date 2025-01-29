4b2824f2ef4e17949965346f7a2a79c34abf58bc
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-01-29 23:43:41 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/klar_ios/usage_deletion_request/view.sql
@@ -0,0 +1,28 @@
+-- Generated via ./bqetl generate glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.klar_ios.usage_deletion_request`
+AS
+SELECT
+  "org_mozilla_ios_klar" AS normalized_app_id,
+  normalized_channel,
+  additional_properties,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  sample_id,
+  submission_timestamp
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_klar.usage_deletion_request`

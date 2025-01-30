62b2f7331764d929597599994e2185d404699f68
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-01-30 13:33:42 -0600

    DENG-7723 Clean up/ optimize Firefox Health Indicator dashboards (#6925)
    * DENG-7723 remove filters from table fx_health_ind_win_instll_by_instll_typ_v1
    
    * Remove duplicate table, point view to other
    
    * Remove view

--- sql/moz-fx-data-shared-prod/telemetry/uninstalls_by_dflt_srch/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.uninstalls_by_dflt_srch`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.uninstalls_by_dflt_srch_v1`
--- sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_win_instll_by_instll_typ_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_health_ind_win_instll_by_instll_typ_v1/query.sql
@@ -1,12 +1,13 @@
 SELECT
   CAST(submission_timestamp AS DATE) AS submission_date,
+  update_channel,
   installer_type,
   COUNT(1) AS install_count,
 FROM
   `moz-fx-data-shared-prod.firefox_installer.install`
 WHERE
   DATE(submission_timestamp) = @submission_date
-  AND update_channel = 'release'
 GROUP BY
   CAST(submission_timestamp AS DATE),
+  update_channel,
   installer_type

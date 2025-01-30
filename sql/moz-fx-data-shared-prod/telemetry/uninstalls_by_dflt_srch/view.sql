2de37d59f2868d47d755a5373292aea77966ce8d
Katie Windau <kwindau@mozilla.com>
2025-01-30 12:10:12 -0600

    Remove duplicate table, point view to other

--- sql/moz-fx-data-shared-prod/telemetry/uninstalls_by_dflt_srch/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/uninstalls_by_dflt_srch/view.sql
@@ -2,6 +2,8 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.uninstalls_by_dflt_srch`
 AS
 SELECT
-  *
+  submission_date,
+  default_search_engine,
+  nbr_unique_clients_uninstalling AS uninstalls_count
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.uninstalls_by_dflt_srch_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.uninstalls_by_default_search_engine`

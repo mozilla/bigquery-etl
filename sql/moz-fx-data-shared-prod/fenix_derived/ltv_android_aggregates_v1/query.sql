1d5a040bd4e09fb5ada7b995513f0496be3ed571
m-d-bowerman <mbowerman@mozilla.com>
2025-01-30 09:45:29 -0800

    query tiles interactions from topsites_impressions

--- sql/moz-fx-data-shared-prod/fenix_derived/ltv_android_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/fenix_derived/ltv_android_aggregates_v1/query.sql
@@ -23,38 +23,15 @@ android_events AS (
     DATE(submission_timestamp) AS submission_date,
     client_info.client_id,
     COALESCE(
-      COUNTIF(event.category = 'pocket' AND event.name = 'home_recs_shown'),
-      0
-    ) AS pocket_section_impressions,
-    COALESCE(
-      COUNTIF(event.category = 'pocket' AND event.name = 'home_recs_spoc_shown'),
-      0
-    ) AS pocket_spoc_impressions,
-    COALESCE(
-      COUNTIF(event.category = 'pocket' AND event.name = 'home_recs_spoc_clicked'),
-      0
-    ) AS pocket_spoc_clicks,
-    COALESCE(
-      COUNTIF(
-        event.category = 'pocket'
-        AND event.name IN ('home_recs_story_shown', 'pocket_top_site_clicked')
-      ),
-      0
-    ) AS pocket_recs_clicks,
-    COALESCE(
       COUNTIF(event.category = 'top_sites' AND event.name = 'contile_impression'),
       0
     ) AS sponsored_tile_impressions,
     COALESCE(
       COUNTIF(event.category = 'top_sites' AND event.name = 'contile_click'),
       0
-    ) AS sponsored_tile_clicks,
-    COALESCE(
-      COUNTIF(event.category = 'top_sites' AND event.name IN ('open_default', 'open_frecency')),
-      0
-    ) AS organic_tile_clicks,
+    ) AS sponsored_tile_clicks
   FROM
-    `moz-fx-data-shared-prod.fenix.events`
+    `moz-fx-data-shared-prod.fenix.topsites_impression`
   CROSS JOIN
     UNNEST(events) event
   WHERE
@@ -72,7 +49,7 @@ android_search AS (
     ANY_VALUE(os) AS os,
     ANY_VALUE(channel) AS channel,
     ANY_VALUE(locale) AS locale,
-    MAX(mozfun.norm.extract_version(app_version, "major")) AS app_version,
+    CAST(MAX(mozfun.norm.extract_version(app_version, "major")) AS INT) AS app_version,
     ANY_VALUE(
       `moz-fx-data-shared-prod`.udf.normalize_search_engine(default_search_engine)
     ) AS default_search_engine,
@@ -109,13 +86,8 @@ android_joined AS (
     ad_click + COALESCE(sponsored_tile_clicks) > 0 AS client_with_ad_engagement,
     ad_click > 0 AS client_with_search_ad_engagement,
     COALESCE(sponsored_tile_clicks) > 0 AS client_with_homepage_ad_engagement,
-    COALESCE(pocket_section_impressions, 0) AS pocket_section_impressions,
-    COALESCE(pocket_spoc_impressions, 0) AS pocket_spoc_impressions,
-    COALESCE(pocket_spoc_clicks, 0) AS pocket_spoc_clicks,
-    COALESCE(pocket_recs_clicks, 0) AS pocket_recs_clicks,
     COALESCE(sponsored_tile_impressions, 0) AS sponsored_tile_impressions,
     COALESCE(sponsored_tile_clicks, 0) AS sponsored_tile_clicks,
-    COALESCE(organic_tile_clicks, 0) AS organic_tile_clicks,
     COALESCE(homepage_visits, 0) AS homepage_visits,
   FROM
     android_search s
@@ -163,20 +135,10 @@ SELECT
   STDDEV(search_with_ads_organic) AS search_with_ads_organic_stddev,
   AVG(homepage_visits) AS homepage_visits_avg,
   STDDEV(homepage_visits) AS homepage_visits_stddev,
-  AVG(pocket_section_impressions) AS pocket_section_impressions_avg,
-  STDDEV(pocket_section_impressions) AS pocket_section_impressions_stddev,
-  AVG(pocket_spoc_impressions) AS pocket_spoc_impressions_avg,
-  STDDEV(pocket_spoc_impressions) AS pocket_spoc_impressions_stddev,
-  AVG(pocket_spoc_clicks) AS pocket_spoc_clicks_avg,
-  STDDEV(pocket_spoc_clicks) AS pocket_spoc_clicks_stddev,
-  AVG(pocket_recs_clicks) AS pocket_recs_clicks_avg,
-  STDDEV(pocket_recs_clicks) AS pocket_recs_clicks_stddev,
   AVG(sponsored_tile_impressions) AS sponsored_tile_impressions_avg,
   STDDEV(sponsored_tile_impressions) AS sponsored_tile_impressions_stddev,
   AVG(sponsored_tile_clicks) AS sponsored_tile_clicks_avg,
   STDDEV(sponsored_tile_clicks) AS sponsored_tile_clicks_stddev,
-  AVG(organic_tile_clicks) AS organic_tile_clicks_avg,
-  STDDEV(organic_tile_clicks) AS organic_tile_clicks_stddev
 FROM
   android_joined
 GROUP BY

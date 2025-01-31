dc0dd8053111b138106193e391ed15267355e69e
Marlene-M-Hirose <mhirose@mozilla.com>
2025-01-30 17:23:43 -1000

    coerce type of received data to be strings - no need to worry about records of varying schema - explicitly define schema of table, take out import tempfile

--- sql/moz-fx-data-shared-prod/addon_moderations_derived/cinder_decisions_raw_v1/query.py
+++ sql/moz-fx-data-shared-prod/addon_moderations_derived/cinder_decisions_raw_v1/query.py
@@ -2,7 +2,6 @@
 
 import json
 import os
-import tempfile
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
 
@@ -28,7 +27,7 @@ def get_response(url, headers, params):
     if (response.status_code == 401) or (response.status_code == 400):
         print(f"***Error: {response.status_code}***")
         print(response.text)
-    return response
+    return response.json()
 
 
 def read_json(filename: str) -> dict:
@@ -57,37 +56,24 @@ def cinder_addon_decisions_download(date, bearer_token):
     return response
 
 
-def check_json(cinder_addon_decisions_response_text):
-    """Script will return an empty dictionary for apps on days when there is no data. Check for that here."""
-    with tempfile.NamedTemporaryFile() as tmp_json:
-        with open(tmp_json.name, "w") as f_json:
-            f_json.write(cinder_addon_decisions_response_text)
-            try:
-                query_export = read_json(f_json.name)
-            except (
-                ValueError
-            ):  # ex. json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
-                return None
-    return query_export
-
-
 def add_date_to_json(query_export_contents, date):
     """Add a date to the entries so we can partition table by this date."""
     fields_list = []
     for item in query_export_contents:
         field_dict = {
             "date": date,
-            "decision_type": item["decision_type"],
-            "user": item["user"],
-            "created_at": item["created_at"],
-            "entity_id": item["entity_id"],
-            "entity": item["entity"],
-            "uuid": item["uuid"],
-            "entity_slug": item["entity_slug"],
-            "job_id": item["job_id"],
-            "job_assigned_at": item["job_assigned_at"],
-            "typed_metadata": item["typed_metadata"],
-            "applied_policies": item["applied_policies"],
+            "decision_type": str(item["decision_type"]),
+            "user": str(item["user"]),
+            "created_at": str(item["created_at"]),
+            "entity_id": str(item["entity_id"]),
+            "entity": str(item["entity"]),
+            "uuid": str(item["uuid"]),
+            "entity_slug": str(item["entity_slug"]),
+            "job_id": str(item["job_id"]),
+            "job_assigned_at": str(item["job_assigned_at"]),
+            "queue_slug": str(item["queue_slug"]),
+            "typed_metadata": str(item["typed_metadata"]),
+            "applied_policies": str(item["applied_policies"]),
         }
         fields_list.append(field_dict)
     return fields_list
@@ -105,6 +91,21 @@ def upload_to_bigquery(data, project, dataset, table_name, date):
             type_=bigquery.TimePartitioningType.DAY,
             field="date",
         ),
+        schema=[
+            bigquery.SchemaField("date", "DATE"),
+            bigquery.SchemaField("decision_type", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("user", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("created_at", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("entity_id", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("entity", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("uuid", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("entity_slug", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("job_id", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("job_assigned_at", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("queue_slug", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("typed_metadata", "STRING", mode="NULLABLE"),
+            bigquery.SchemaField("applied_policies", "STRING", mode="NULLABLE"),
+        ],
     )
     destination = f"{project}.{dataset}.{table_name}${partition}"
     job = client.load_table_from_json(data, destination, job_config=job_config)
@@ -116,7 +117,7 @@ def main():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument("--date", required=True)
     parser.add_argument("--project", default="moz-fx-data-shared-prod")
-    parser.add_argument("--dataset", default="addon_moderations_derived")
+    parser.add_argument("--dataset", default="addon_moderations")
 
     args = parser.parse_args()
 
@@ -129,14 +130,12 @@ def main():
 
     cinder_data = []
 
-    json_file = cinder_addon_decisions_download(date, bearer_token)
+    query_export = cinder_addon_decisions_download(date, bearer_token)
     """Data returns as a dictionary with a key called 'items' and the value being a list of data"""
-    query_export = check_json(json_file.text)
-    """Add date to each element in query_export for partitioning"""
     query_export_contents = query_export["items"]
+    """Add date to each element in query_export for partitioning"""
     cinder_data = add_date_to_json(query_export_contents, date)
-    """Pull out the list from query_export["items"] and put that data into the cinder_data list"""
-
+    """Pull out[ the list from query_export["items"] and put that data into the cinder_data list"""
     upload_to_bigquery(cinder_data, project, dataset, table_name, date)
 
 

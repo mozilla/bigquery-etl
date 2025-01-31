5326ac734cd0d69e3f8c60b758695bf3ed1b3aaa
Katie Windau <kwindau@mozilla.com>
2025-01-31 15:53:18 -0600

    work in progress

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/rolling_cohorts_v2/query.sql
@@ -0,0 +1,63 @@
+--Desktop
+SELECT
+  au.client_id,
+  au.first_seen_date AS cohort_date,
+  au.activity_segment,
+  au.app_version,
+  CAST(NULL AS string) AS attribution_campaign, --FIX
+  CAST(NULL AS string) AS attribution_content, --FIX
+  CAST(NULL AS string) AS attribution_experiment, --FIX
+  au.attribution_medium,
+  au.attribution_source,
+  CAST(NULL AS string) AS attribution_variation, --FIX
+  au.city,
+  au.country,
+  ccls.device AS device_model,
+  au.distribution_id,
+  au.is_default_browser,
+  au.locale,
+  au.app_name AS normalized_app_name,
+  au.normalized_channel,
+  au.os AS normalized_os, --old one had it as normalized_os, do I need to add a transform of some kind to normalize?
+  au.normalized_os_version,
+  CAST(NULL AS INTEGER) AS os_version_major, --FIX
+  CAST(NULL AS INTEGER) AS os_version_minor, --FIX
+FROM
+  `moz-fx-data-shared-prod.telemetry.desktop_active_users` au
+LEFT JOIN
+  `moz-fx-data-shared-prod.telemetry.core_clients_last_seen` ccls
+  ON au.client_id = ccls.client_id
+  AND au.submission_date = ccls.submission_date
+WHERE
+  au.first_seen_date = @submission_date
+  AND au.submission_date = @submission_date
+UNION ALL
+--Mobile
+SELECT
+  au.client_id,
+  au.first_seen_date AS cohort_date,
+  au.activity_segment,
+  CAST(NULL AS STRING) AS app_version, --FIX
+  CAST(NULL AS string) AS attribution_campaign, --FIX
+  CAST(NULL AS string) AS attribution_content, --FIX
+  CAST(NULL AS string) AS attribution_experiment, --FIX
+  CAST(NULL AS string) AS attribution_medium, --FIX
+  CAST(NULL AS string) AS attribution_source, --FIX
+  CAST(NULL AS string) AS attribution_variation, --FIX
+  au.city,
+  au.country,
+  au.device_model,
+  au.distribution_id,
+  CAST(NULL AS BOOLEAN) AS is_default_browser,
+  au.locale,
+  CAST(NULL AS STRING) AS normalized_app_name,--FIX
+  au.normalized_channel,
+  CAST(NULL AS STRING) AS normalized_os, --FIX
+  CAST(NULL AS STRING) AS normalized_os_version, --FIX
+  CAST(NULL AS INTEGER) AS os_version_major, --FIX
+  CAST(NULL AS INTEGER) AS os_version_minor, --FIX
+FROM
+  `moz-fx-data-shared-prod.telemetry.mobile_active_users` au
+WHERE
+  au.first_seen_date = @submission_date
+  AND au.submission_date = @submission_date

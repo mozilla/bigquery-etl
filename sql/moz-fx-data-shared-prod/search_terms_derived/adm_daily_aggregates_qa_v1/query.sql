4e5d2d3ece838b2f65952fdb7fb043c1e3d51f14
Katie Windau <kwindau@mozilla.com>
2025-02-03 12:51:13 -0600

    Add hacky data check instead of normal one

---
+++ sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_aggregates_qa_v1/query.sql
@@ -0,0 +1,13 @@
+--Unable to do normal data check since "adm_daily_aggregates_v1" passes in parameter ALLOW_FIELD_EDITION, 
+--so this is a separate task to check each day has at least 100 rows and to fail the Airflow task if not
+SELECT
+  submission_date,
+  COUNT(1) AS num_rows
+FROM
+  `moz-fx-data-shared-prod.search_terms_derived.adm_daily_aggregates_v1`
+WHERE
+  submission_date = @submission_date
+GROUP BY
+  submission_date
+HAVING
+  COUNT(1) >= 100

0baf6481bb40bbec6def4ae53e5a8d3ab45186f7
Katie Windau <kwindau@mozilla.com>
2025-02-03 20:18:53 -0600

    work in progress

--- sql/moz-fx-data-shared-prod/external/holidays/view.sql
+++ sql/moz-fx-data-shared-prod/external/holidays/view.sql
@@ -4,6 +4,22 @@ AS
 SELECT
   submission_date,
   IF(calendar_month = 1 AND EXTRACT(day FROM submission_date) = 1, TRUE, FALSE) AS new_years_day,
+  CASE
+    WHEN submission_date IN (
+        '2020-01-25',
+        '2021-02-12',
+        '2022-02-01',
+        '2023-01-22',
+        '2024-02-10',
+        '2025-01-29',
+        '2026-02-17',
+        '2027-02-06',
+        '2028-01-26',
+        '2029-02-13'
+      )
+      THEN TRUE
+    ELSE FALSE
+  END AS lunar_new_year,
   IF(calendar_month = 2 AND EXTRACT(day FROM submission_date) = 14, TRUE, FALSE) AS valentines_day,
   CASE
     WHEN submission_date IN (
@@ -54,11 +70,44 @@ SELECT
   WHEN submission_date IN () THEN TRUE 
   ELSE FALSE 
   END AS us_thanksgiving,
+  CASE
+  WHEN submission_date IN () THEN TRUE 
+  ELSE FALSE 
+  END AS us_blackfriday,
+  CASE
+  WHEN submission_date IN () THEN TRUE 
+  ELSE FALSE 
+  END AS us_cybermonday,
   CASE 
   WHEN submission_date IN () THEN TRUE 
   ELSE FALSE 
   END AS ca_thanksgiving,
   */
+  CASE
+    WHEN submission_date IN (
+        '2020-10-13',
+        '2020-10-14',
+        '2021-06-21',
+        '2021-06-22',
+        '2021-10-10',
+        '2021-10-11',
+        '2022-07-12',
+        '2022-07-13',
+        '2022-10-11',
+        '2022-10-12',
+        '2023-07-11',
+        '2023-07-12',
+        '2023-10-10',
+        '2023-10-11',
+        '2024-07-16',
+        '2024-07-17',
+        '2024-10-08',
+        '2024-10-09'
+    --future dates unknown currently, will add as they are announced
+      )
+      THEN TRUE
+    ELSE FALSE
+  END AS amazon_prime_days_summer,
   IF(calendar_month = 10 AND EXTRACT(day FROM submission_date) = 3, TRUE, FALSE) AS de_unity_day,
   IF(calendar_month = 10 AND EXTRACT(day FROM submission_date) = 31, TRUE, FALSE) AS halloween,
   IF(calendar_month = 12 AND EXTRACT(day FROM submission_date) = 24, TRUE, FALSE) AS christmas_eve,

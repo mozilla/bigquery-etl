0a0574bf2eee4b6f0ca145c46683714547bf9616
Gaurang Katre <gkatre@users.noreply.github.com>
2025-02-04 09:11:57 -0800

    Update sql_generators/glean_usage/templates/baseline_clients_last_seen_v1.query.sql
    comment update
    
    Co-authored-by: Leif Oines <leifdoines@gmail.com>

--- sql_generators/glean_usage/templates/baseline_clients_last_seen_v1.query.sql
+++ sql_generators/glean_usage/templates/baseline_clients_last_seen_v1.query.sql
@@ -31,7 +31,7 @@ WITH _current AS (
     -- rightmost bit in 'days_since_seen' represents whether the user sent a
     -- baseline ping in the submission_date and similarly, the rightmost bit in
     -- days_active_bits represents whether the user counts as active on that date.
-    -- days_desktop_active_bits represents the official definition of used for desktop active user
+    -- days_desktop_active_bits represents the official definition of "active user" for desktop
     CAST(TRUE AS INT64) AS days_seen_bits,
     CAST(TRUE AS INT64) & CAST(durations > 0  AS INT64) AS days_active_bits,
     udf.days_since_created_profile_as_28_bits(

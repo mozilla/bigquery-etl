b5834b3e9d5f3f9908b8b56bceab89154e281576
Sean Rose <1994030+sean-rose@users.noreply.github.com>
2025-02-05 14:20:55 -0800

    feat: Add authorized views for Zoom data synced by Fivetran (DENG-6964) (#6972)
    * feat: Add authorized views for Zoom data synced by Fivetran (DENG-6964).
    
    * Add Zoom views to dryrun skip list.
    
    * Cast `meeting_reports.type` as an integer like it is in Zoom's API.
    
    * Simplify description of `meeting_participants.participant_user_id`.

---
+++ sql/moz-fx-data-shared-prod/zoom/meeting_reports/view.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.zoom.meeting_reports`
+AS
+SELECT
+  uuid,
+  meeting_id,
+  dept,
+  duration,
+  end_time,
+  participants_count,
+  start_time,
+  total_minutes,
+  SAFE_CAST(type AS INTEGER) AS type,
+  user_email,
+  user_name,
+  _fivetran_deleted AS is_deleted,
+FROM
+  `moz-fx-data-bq-fivetran.zoom.meeting_report`

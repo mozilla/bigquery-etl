6930fa2bac13091dfafab0bd5e1a0140045958fc
Katie Windau <kwindau@mozilla.com>
2025-02-05 09:01:51 -0600

    scratchwork

--- sql/moz-fx-data-shared-prod/external_derived/inflation_v1/query.py
+++ sql/moz-fx-data-shared-prod/external_derived/inflation_v1/query.py
@@ -7,94 +7,68 @@ from google.cloud import bigquery
 import time
 
 #Define countries to pull data for
-countries = ['US', 'DE']
+countries = ['US'] #, 'DE', 'FR', 'GB']
 
-
-#Define function to pull CPI data 
+#Define function to pull CPI data
 def pull_monthly_cpi_data_from_imf(country_code, start_month, end_month):
   """
   Inputs: 
-  Country Code - 2 letter country code, for example, US
-  Start Month - YYYY-MM - for example, 2023-10
-  End Month - YYYY-MM - for example, 2023-12
+      Country Code - 2 letter country code, for example, US
+      Start Month - YYYY-MM - for example, 2023-10
+      End Month - YYYY-MM - for example, 2023-12
 
   Output:
-  JSON with data for this country for the months between start month and end month
+    JSON with data for this country for the months between start month and end month
   """
   api_url = f"http://dataservices.imf.org/REST/SDMX_JSON.svc/CompactData/IFS/M.{country_code}.PCPI_IX.?startPeriod={start_month}&endPeriod={end_month}"
+  print('api_url')
+  print(api_url)
+
 
   response = requests.get(api_url)
-  data = response.json()
+  inflation_data = response.json()
 
+  print('inflation_data')
+  print(inflation_data)
 
-  #TEMP ABOVE 
+  series = inflation_data.get('CompactData', {}).get('DataSet', {}).get('Series', None)
+  if series is None:
+    raise KeyError("Expected 'Series' key not found in the API response.")
+  
+  print('series')
+  print(series)
+  
+  observations = series.get('Obs', [])
+  if not observations:
+    raise ValueError("No observations found for the specified parameters.")
+  
+  print('observations')
+  print(observations)
 
-
-    # try:
-    #     # Fetch data from the API
-    #     data_url = f"{base_url}{dataset_key}"
-    #     response = requests.get(data_url, params=params)
-    #     response.raise_for_status()
-    #     data = response.json()
-
-    #     # Extract the 'Series' element
-    #     series = data.get('CompactData', {}).get('DataSet', {}).get('Series', None)
-    #     if series is None:
-    #         raise KeyError("Expected 'Series' key not found in the API response.")
-        
-    #     # Extract observations
-    #     observations = series.get('Obs', [])
-    #     if not observations:
-    #         raise ValueError("No observations found for the specified parameters.")
-        
-    #     # Extract base year if available
-    #     base_year = series.get('@BASE_YEAR', "Unknown Base Year")
-
-    #     # Convert observations to a DataFrame
-    #     df = pd.DataFrame(observations)
-    #     df.rename(columns={'@TIME_PERIOD': 'Date', '@OBS_VALUE': 'Value'}, inplace=True)
-    #     df['Date'] = pd.to_datetime(df['Date'])
-    #     df['Value'] = pd.to_numeric(df['Value'], errors='coerce')
-    #     df['Frequency'] = frequency
-    #     df['Country'] = country_code
-    #     df['Base Year'] = base_year
-        
-    #     return df
-
-    # except requests.exceptions.RequestException as e:
-    #     print(f"An error occurred while fetching the data: {e}")
-    # except KeyError as e:
-    #     print(f"An error occurred while accessing the JSON structure: {e}")
-    # except ValueError as e:
-    #     print(f"Error: {e}")
-    # except Exception as e:
-    #     print(f"An unexpected error occurred: {e}")
-
-# Pull data
-
-
-# Load to table
+  #Now, convert the observations into a data frame
+  #observations_df = pd.DataFrame(observations)
+  #print('observations_df')
+  #print(observations_df)
 
 
 
 def main():
     """Call the API, save data to GCS, load to BQ staging, delete & load to BQ gold"""
-    parser = ArgumentParser(description=__doc__)
-    parser.add_argument("--date", required=True)
+    #parser = ArgumentParser(description=__doc__)
+    #parser.add_argument("--date", required=True)
 
     #Calculate start / end period based on submission date
 
     #For each country
     for country in countries: 
       #Pull the CPI data
-      results = pull_monthly_cpi_data_from_imf(country, start_month, end_month)
+      pull_monthly_cpi_data_from_imf(country, '2023-10', '2023-12') #start_month, end_month
       
       #Save the data to BQ
 
       #sleep for 10 seconds between each call since the API is rate limited
-      time.sleep(10)
+      #time.sleep(2)
 
-    
 
 if __name__ == "__main__":
   main()

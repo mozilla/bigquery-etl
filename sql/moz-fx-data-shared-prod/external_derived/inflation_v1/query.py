a141287dd17dbb445d350789e244831b1da5055d
Katie Windau <kwindau@mozilla.com>
2025-02-05 16:01:18 -0600

    work in progress

--- sql/moz-fx-data-shared-prod/external_derived/inflation_v1/query.py
+++ sql/moz-fx-data-shared-prod/external_derived/inflation_v1/query.py
@@ -2,19 +2,21 @@
 from datetime import datetime, timedelta
 import pandas as pd
 import requests
+import time
 from argparse import ArgumentParser
 from google.cloud import bigquery
-import time
-import pandas_gbq
+from google.cloud import storage
 
 # Set variables
-countries = ["CA", "CH", "GB", "FR", "ES", "US", "DE", "IT", "JP", "PL"]
+countries = ["CA",  'US'] #, "CH", "GB", "FR", "ES", "DE", "IT", "JP", "PL"] #AU
 START_LOOKBACK_DAYS = 1825
 END_LOOKBACK_DAYS = 15
 WAIT_TIME_SECONDS = 10
 TARGET_PROJECT = "moz-fx-data-shared-prod"
-TARGET_TABLE = "external_derived.inflation_v1"
-
+TARGET_TABLE = "moz-fx-data-shared-prod.external_derived.inflation_v1"
+GCS_BUCKET = "gs://moz-fx-data-prod-external-data/"
+GCS_BUCKET_NO_GS = "moz-fx-data-prod-external-data"
+RESULTS_FPATH = "IMF_CPI/imf_cpi_data_%s.csv"
 
 # Define function to pull CPI data
 def pull_monthly_cpi_data_from_imf(country_code, start_month, end_month):
@@ -36,9 +38,12 @@ def pull_monthly_cpi_data_from_imf(country_code, start_month, end_month):
         inflation_data.get("CompactData", {}).get("DataSet", {}).get("Series", None)
     )
 
+    base_year = series.get('@BASE_YEAR', "Unknown Base Year")
+
     observations = series.get("Obs", [])
     observations_df = pd.DataFrame(observations)
     observations_df["country"] = country_code
+    observations_df["base_year"] = base_year
 
     # Rename to friendlier names
     observations_df.rename(
@@ -48,7 +53,7 @@ def pull_monthly_cpi_data_from_imf(country_code, start_month, end_month):
 
     # Reorder cols to match schema order
     observations_df = observations_df[
-        ["report_period", "consumer_price_index", "country"]
+        ["report_period", "consumer_price_index", "country", "base_year"]
     ]
     return observations_df
 
@@ -77,6 +82,7 @@ def main():
 
     # For each country
     for country in countries:
+        print('pulling data for current country: ', country)
         # Pull the CPI data
         curr_country_infl_df = pull_monthly_cpi_data_from_imf(
             country, start_month, end_month
@@ -91,15 +97,30 @@ def main():
     # Add a column with the current date
     results_df["last_updated"] = curr_date
 
-    # Write the final results_df to BQ external_derived.inflation_v1 table
-    # This will always over-write with the latest data
-    pandas_gbq.to_gbq(
-        results_df,
-        TARGET_TABLE,
-        project_id=TARGET_PROJECT,
-        if_exists="replace",
-    )
+    # Write the final results_df to GCS bucket
+    final_results_fpath = GCS_BUCKET + RESULTS_FPATH % (curr_date)
+    print('final_results_fpath: ', final_results_fpath)
+    results_df.to_csv(final_results_fpath, index=False)
 
+    # Load the data from GCS into the table - do a full 
+    client = bigquery.Client(TARGET_PROJECT)
+    load_csv_to_inflation_v1_job = client.load_table_from_uri(
+        final_results_fpath,
+        TARGET_TABLE,
+        job_config=bigquery.LoadJobConfig(
+            create_disposition="CREATE_NEVER",
+            write_disposition="WRITE_TRUNCATE",
+            schema=[{"name": "report_period", 'type': "STRING", "mode" "NULLABLE"},
+                    {"name": "consumer_price_index", 'type': "NUMERIC", "mode" "NULLABLE"}
+                    {"name": "country", 'type': "STRING", "mode" "NULLABLE"}
+                    {"name": "base_year", 'type': "STRING", "mode" "NULLABLE"}
+                    {"name": "last_updated", 'type': "DATE", "mode" "NULLABLE"}
+                    ],
+            skip_leading_rows=1,
+            source_format=bigquery.SourceFormat.CSV,
+        ),
+
+    )
 
 if __name__ == "__main__":
     main()

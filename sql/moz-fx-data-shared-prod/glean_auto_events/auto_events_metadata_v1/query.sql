f6b426520687990d4ba6dd2cb8a85850410fc5b7
Eduardo Filho <edugomfilho@gmail.com>
2025-02-06 13:06:11 -0500

    Fix formatting

--- sql/moz-fx-data-shared-prod/glean_auto_events/auto_events_metadata_v1/query.sql
+++ sql/moz-fx-data-shared-prod/glean_auto_events/auto_events_metadata_v1/query.sql
@@ -1,18 +1,23 @@
 WITH events AS (
   SELECT
-    DATE(submission_timestamp) as submission_date,
+    DATE(submission_timestamp) AS submission_date,
     event,
     JSON_VALUE(event_extra.id) AS element_id,
     COUNT(*) AS count
-  FROM moz-fx-data-shared-prod.accounts_frontend.events_stream as e
+  FROM
+    moz - fx - data - shared - prod.accounts_frontend.events_stream AS e
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND event = 'glean.element_click'
-  GROUP BY submission_date, event, element_id
+  GROUP BY
+    submission_date,
+    event,
+    element_id
 )
 SELECT
   submission_date,
   "accounts_frontend" AS app,
   CONCAT(event, '.', events.element_id) name,
   count
-FROM events
+FROM
+  events

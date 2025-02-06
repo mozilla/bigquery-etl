574f398eea9253d93b5ddb933511ff733f371d5e
kik-kik <kignasiak@mozilla.com>
2025-02-06 15:08:08 +0100

    feat: add profile_dau_metrics_marketing_geo_testing_v1 for firefox_ios and fenix

---
+++ sql/moz-fx-data-shared-prod/firefox_ios/profile_dau_metrics_marketing_geo_testing/view.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_ios.profile_dau_metrics_marketing_geo_testing`
+AS
+SELECT
+  *,
+  `moz-fx-data-shared-prod.udf.organic_vs_paid_mobile`(adjust_network) AS paid_vs_organic,
+FROM
+  `moz-fx-data-shared-prod.firefox_ios_derived.profile_dau_metrics_marketing_geo_testing`

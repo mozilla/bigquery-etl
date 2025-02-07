c31a4db0a6cec5c8409218b5e09e24b125e897a3
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-02-07 14:46:49 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/external/calendar/view.sql
+++ sql/moz-fx-data-shared-prod/external/calendar/view.sql
@@ -2,6 +2,30 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.external.calendar`
 AS
 SELECT
-  *
+  submission_date,
+  CASE
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 1
+      THEN 'Sunday'
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 2
+      THEN 'Monday'
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 3
+      THEN 'Tuesday'
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 4
+      THEN 'Wednesday'
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 5
+      THEN 'Thursday'
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 6
+      THEN 'Friday'
+    WHEN EXTRACT(DAYOFWEEK FROM submission_date) = 7
+      THEN 'Saturday'
+    ELSE NULL
+  END AS day_of_week,
+  calendar_year AS `year`,
+  calendar_month AS `month`,
+  calendar_quarter AS `quarter`,
+  EXTRACT(WEEK FROM submission_date) AS week_number,
+  first_date_of_month,
+  first_date_of_quarter,
+  EXTRACT(DAYOFYEAR FROM submission_date) AS day_of_year
 FROM
   `moz-fx-data-shared-prod.external_derived.calendar_v1`

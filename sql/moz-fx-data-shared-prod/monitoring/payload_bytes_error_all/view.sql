8c7dfb97c447e50e183cbf261a0b3de4716da34b
whd <whd@users.noreply.github.com>
2025-02-07 13:50:33 -0800

    chore: authorize payload_bytes_error_all (#7003)

--- sql/moz-fx-data-shared-prod/monitoring/payload_bytes_error_all/view.sql
+++ sql/moz-fx-data-shared-prod/monitoring/payload_bytes_error_all/view.sql
@@ -1,16 +1,14 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.monitoring.payload_bytes_error_all`
 AS
--- payload_bytes_error.structured is restricted from most users,
--- so we have to reference an authorized view.
+-- We exclude the payload field for views that are accessible to
+-- most users.
 SELECT
   'structured' AS pipeline_family,
-  *
+  * EXCEPT (payload)
 FROM
-  `moz-fx-data-shared-prod.monitoring.payload_bytes_error_structured`
+  `moz-fx-data-shared-prod.payload_bytes_error.structured`
 UNION ALL
--- All other error tables are accessible, but we need to exclude
--- the payload field to match the schema of the authorized view above.
 SELECT
   'stub_installer' AS pipeline_family,
   * EXCEPT (payload)

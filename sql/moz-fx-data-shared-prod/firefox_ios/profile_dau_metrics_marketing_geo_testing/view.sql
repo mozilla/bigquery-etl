aabcd265b0567271c3d32bfd93059424222df980
kik-kik <kignasiak@mozilla.com>
2025-02-07 16:51:49 +0100

    fix: incorrect table reference in views

--- sql/moz-fx-data-shared-prod/firefox_ios/profile_dau_metrics_marketing_geo_testing/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios/profile_dau_metrics_marketing_geo_testing/view.sql
@@ -5,4 +5,4 @@ SELECT
   *,
   `moz-fx-data-shared-prod.udf.organic_vs_paid_mobile`(adjust_network) AS paid_vs_organic,
 FROM
-  `moz-fx-data-shared-prod.firefox_ios_derived.profile_dau_metrics_marketing_geo_testing`
+  `moz-fx-data-shared-prod.firefox_ios_derived.profile_dau_metrics_marketing_geo_testing_v1`

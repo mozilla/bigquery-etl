47a97efe7b7c131baaceeb538388f730d3bfe259
Ogonna Anaekwe <oanaekwe@Ogonnas-MacBook-Pro.local>
2025-02-11 12:27:04 -0500

    DENG-7839: fix linting errors

--- sql/moz-fx-data-shared-prod/backfills_staging_derived/baseline_clients_last_seen_v1_20211130/query.sql
+++ sql/moz-fx-data-shared-prod/backfills_staging_derived/baseline_clients_last_seen_v1_20211130/query.sql
@@ -11,7 +11,7 @@
   -- the two tables to validate.
     *
   FROM
-    `firefox_desktop_derived.baseline_clients_daily_v1`
+    `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_daily_v1`
   WHERE
   -- Output empty table and read no input rows
     FALSE
@@ -26,7 +26,7 @@
     -- days_desktop_active_bits represents the official definition of "active user" for desktop
       CAST(TRUE AS INT64) AS days_seen_bits,
       CAST(TRUE AS INT64) & CAST(durations > 0 AS INT64) AS days_active_bits,
-      udf.days_since_created_profile_as_28_bits(
+      `moz-fx-data-shared-prod.udf.days_since_created_profile_as_28_bits`(
         DATE_DIFF(submission_date, first_run_date, DAY)
       ) AS days_created_profile_bits,
       CAST(TRUE AS INT64) & CAST(browser_engagement_uri_count > 0 AS INT64) & CAST(
@@ -34,7 +34,7 @@
       ) AS days_desktop_active_bits,
       * EXCEPT (submission_date)
     FROM
-      `firefox_desktop_derived.baseline_clients_daily_v1`
+      `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_daily_v1`
     WHERE
       submission_date = @submission_date
       AND sample_id IS NOT NULL
@@ -54,38 +54,38 @@
         days_created_profile_bits
       ),
     FROM
-      `firefox_desktop_derived.baseline_clients_last_seen_v1`
+      `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_last_seen_v1`
     WHERE
       submission_date = DATE_SUB(@submission_date, INTERVAL 1 DAY)
     -- Filter out rows from yesterday that have now fallen outside the 28-day window.
-      AND udf.shift_28_bits_one_day(days_seen_bits) > 0
+      AND `moz-fx-data-shared-prod.udf.shift_28_bits_one_day`(days_seen_bits) > 0
       AND sample_id IS NOT NULL
   )
   --
   SELECT
     @submission_date AS submission_date,
     IF(_current.client_id IS NOT NULL, _current, _previous).* REPLACE (
-      udf.combine_adjacent_days_28_bits(
+      `moz-fx-data-shared-prod.udf.combine_adjacent_days_28_bits`(
         _previous.days_desktop_active_bits,
         _current.days_desktop_active_bits
       ) AS days_desktop_active_bits,
-      udf.combine_adjacent_days_28_bits(
+      `moz-fx-data-shared-prod.udf.combine_adjacent_days_28_bits`(
         _previous.days_seen_bits,
         _current.days_seen_bits
       ) AS days_seen_bits,
-      udf.combine_adjacent_days_28_bits(
+      `moz-fx-data-shared-prod.udf.combine_adjacent_days_28_bits`(
         _previous.days_active_bits,
         _current.days_active_bits
       ) AS days_active_bits,
-      udf.combine_adjacent_days_28_bits(
+      `moz-fx-data-shared-prod.udf.combine_adjacent_days_28_bits`(
         _previous.days_created_profile_bits,
         _current.days_created_profile_bits
       ) AS days_created_profile_bits,
-      udf.combine_adjacent_days_28_bits(
+      `moz-fx-data-shared-prod.udf.combine_adjacent_days_28_bits`(
         _previous.days_seen_session_start_bits,
         _current.days_seen_session_start_bits
       ) AS days_seen_session_start_bits,
-      udf.combine_adjacent_days_28_bits(
+      `moz-fx-data-shared-prod.udf.combine_adjacent_days_28_bits`(
         _previous.days_seen_session_end_bits,
         _current.days_seen_session_end_bits
       ) AS days_seen_session_end_bits

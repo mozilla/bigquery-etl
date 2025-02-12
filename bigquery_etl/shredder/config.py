c9ff08ccbb11e7f74bb451885d5d19cc4cb8332b
Katie Windau <kwindau@mozilla.com>
2025-02-12 14:50:11 -0600

    initial commit - add rolling_cohorts_v2 to desktop delete

--- bigquery_etl/shredder/config.py
+++ bigquery_etl/shredder/config.py
@@ -199,6 +199,9 @@ DELETE_TARGETS: DeleteIndex = {
     client_id_target(
         table="telemetry_derived.desktop_engagement_clients_v1"
     ): DESKTOP_SRC,
+        client_id_target(
+        table="telemetry_derived.rolling_cohorts_v2"
+    ): DESKTOP_SRC,
     client_id_target(table="search_derived.search_clients_last_seen_v1"): DESKTOP_SRC,
     client_id_target(table="telemetry_derived.clients_daily_v6"): DESKTOP_SRC,
     client_id_target(table="telemetry_derived.clients_daily_joined_v1"): DESKTOP_SRC,

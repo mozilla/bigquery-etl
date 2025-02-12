9fedbedbb4f44ba6f7bb6c611ce2baf6f6f14ff2
Katie Windau <kwindau@mozilla.com>
2025-02-12 16:57:56 -0600

    Initial commit

---
+++ sql/moz-fx-data-shared-prod/telemetry/fxa_accounts_active_daily_clients/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.fxa_accounts_active_daily_clients`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.fxa_accounts_active_daily_clients_v1`

dc7d1fbe8e1dba5f419671a0cec584957e5d8016
Gaurang Katre <gkatre@gmail.com>
2025-02-11 17:22:33 -0800

    query update with formatting, schema and metadata

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/desktop_dau_distribution_id_history/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/desktop_dau_distribution_id_history/query.sql
@@ -4,34 +4,46 @@
 -- This table creates a time series of distribution_ids from legacy (main) pings for desktop DAU based on the baseline
 -- ping.
 -- This would enable us to filter MozillaOnline (distribution_id='MozillaOnline') clients from the KPI purposes
-
-WIWITH baseline_users AS (
-   SELECT
-      submission_date,
-      client_id,
-      legacy_telemetry_client_id,
-      (browser_engagement_uri_count > 0) AND (browser_engagement_active_ticks > 0) AS is_dau_baseline,
-   FROM `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_daily_v1`
-   WHERE submission_date = @submission_date
-   AND LOWER(IFNULL(isp, '')) <> "browserstack"
+WITH baseline_users AS (
+  SELECT
+    submission_date,
+    client_id,
+    legacy_telemetry_client_id,
+    (browser_engagement_uri_count > 0)
+    AND (browser_engagement_active_ticks > 0) AS is_dau_baseline,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_daily_v1`
+  WHERE
+    submission_date = @submission_date
+    AND LOWER(IFNULL(isp, '')) <> "browserstack"
+    AND client_id IS NOT NULL
+    AND legacy_telemetry_client_id IS NOT NULL
 ),
-
 legacy_active_users AS (
-   SELECT
-      submission_date,
-      client_id AS legacy_telemetry_client_id,
-      is_dau AS is_dau_legacy,
-      distribution_id
-   FROM `moz-fx-data-shared-prod.telemetry.desktop_active_users`
-   WHERE submission_date = @submission_date
-   AND LOWER(IFNULL(isp_name, '')) <> "browserstack"
+  SELECT
+    submission_date,
+    client_id AS legacy_telemetry_client_id,
+    is_dau AS is_dau_legacy,
+    distribution_id
+  FROM
+    `moz-fx-data-shared-prod.telemetry.desktop_active_users`
+  WHERE
+    submission_date = @submission_date
+    AND LOWER(IFNULL(isp_name, '')) <> "browserstack"
    -- only clients with non-null distribution_ids, reduces the size of the table.
-   AND distribution_id IS NOT NULL
-   AND is_daily_user
+    AND distribution_id IS NOT NULL
+    AND is_daily_user
+    AND client_id IS NOT NULL
 )
-
 SELECT
-*
-FROM legacy_active_users
-JOIN baseline_users
-USING(submission_date, legacy_telemetry_client_id);
+  submission_date,
+  legacy_telemetry_client_id,
+  client_id,
+  is_dau_baseline,
+  is_dau_legacy,
+  distribution_id
+FROM
+  legacy_active_users
+JOIN
+  baseline_users
+  USING (submission_date, legacy_telemetry_client_id)

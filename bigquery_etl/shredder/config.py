88f59e7c111890a09b7eb874665095ca10040ccf
Katie Windau <kwindau@mozilla.com>
2025-02-12 20:21:13 -0600

    Add new table telemetry_derived.fx_accounts_active_daily_clients_v1 to shredder_mitigation

--- bigquery_etl/shredder/config.py
+++ bigquery_etl/shredder/config.py
@@ -256,6 +256,10 @@ DELETE_TARGETS: DeleteIndex = {
         DeleteSource(table="fenix.deletion_request", field=GLEAN_CLIENT_ID),
     ),
     DeleteTarget(
+        table="telemetry_derived.fx_accounts_active_daily_clients_v1",
+        field=("legacy_telemetry_client_id",),
+    ): (DESKTOP_SRC),
+    DeleteTarget(
         table="telemetry_derived.rolling_cohorts_v2",
         field=(
             CLIENT_ID,

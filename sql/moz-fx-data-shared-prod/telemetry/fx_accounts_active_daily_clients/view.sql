c1fbb57baaa3bf2d7d8bde88c6359e381ad026d6
Katie Windau <kwindau@mozilla.com>
2025-02-12 20:03:05 -0600

    Remove typo from table and view name

--- sql/moz-fx-data-shared-prod/telemetry/fx_accounts_active_daily_clients/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/fx_accounts_active_daily_clients/view.sql
@@ -4,4 +4,4 @@ AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.telemetry_derived.fxa_accounts_active_daily_clients_v1`
+  `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_active_daily_clients_v1`

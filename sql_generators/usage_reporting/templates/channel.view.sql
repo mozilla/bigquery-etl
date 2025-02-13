7313bc0067c05817881af2f3b968c31a6affd2b5
kik-kik <kignasiak@mozilla.com>
2025-02-13 19:28:27 +0100

    feat: tweaks for desktop views to be generated correctly

--- sql_generators/usage_reporting/templates/channel.view.sql
+++ sql_generators/usage_reporting/templates/channel.view.sql
@@ -3,6 +3,10 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ channel_dataset }}.{{ view_name }}`
 AS
 SELECT
-  *
+  {% if app_name == "firefox_desktop" %}
+  "{{ channel_dataset }}" AS normalized_app_id,
+  app_channel AS normalized_channel,
+  {% endif %}
+  *,
 FROM
   `{{ project_id }}.{{ channel_dataset }}_derived.{{ table_name }}`

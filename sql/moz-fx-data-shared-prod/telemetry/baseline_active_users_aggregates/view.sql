4237112e3f5dc7251fa3a94ffd78a65d18a357b8
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-02-19 13:50:36 -0600

    DENG-7860 - Delete new table, Kik to move to firefox_desktop_derived since it's a Glean based table (#7059)
    * Remove DAG
    
    * Delete files, Kik will rebuild in firefox_desktop_derived tomorrow

--- sql/moz-fx-data-shared-prod/telemetry/baseline_active_users_aggregates/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.baseline_active_users_aggregates`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.telemetry_derived.baseline_active_users_aggregates_v1`

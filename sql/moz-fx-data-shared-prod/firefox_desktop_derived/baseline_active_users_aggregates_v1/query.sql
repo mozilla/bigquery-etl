ab7eead48167ae4a8f819ffd91db9a11576794f9
kik-kik <kignasiak@mozilla.com>
2025-02-20 11:17:51 +0100

    feat: update baseline_active_users_aggregates_v1 to use active_users view instead of repeating the logic

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/baseline_active_users_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/baseline_active_users_aggregates_v1/query.sql
@@ -1,84 +1,3 @@
-WITH clients AS (
-  SELECT
-    client_id,
-    CASE
-      WHEN isp = 'BrowserStack'
-        THEN CONCAT('Firefox Desktop', ' ', isp)
-      WHEN distribution_id = 'MozillaOnline'
-        THEN CONCAT('Firefox Desktop', ' ', distribution_id)
-      ELSE 'Firefox Desktop'
-    END AS app_name,
-    app_display_version AS app_version,
-    normalized_channel AS channel,
-    IFNULL(country, '??') AS country,
-    IFNULL(city, '??') AS city,
-    COALESCE(REGEXP_EXTRACT(locale, r'^(.+?)-'), locale, NULL) AS locale,
-    normalized_os AS os,
-    COALESCE(
-      `mozfun.norm.windows_version_info`(
-        normalized_os,
-        normalized_os_version,
-        windows_build_number
-      ),
-      normalized_os_version
-    ) AS os_version_build,
-    COALESCE(
-      CAST(NULLIF(SPLIT(normalized_os_version, ".")[SAFE_OFFSET(0)], "") AS INTEGER),
-      0
-    ) AS os_version_major,
-    COALESCE(
-      CAST(NULLIF(SPLIT(normalized_os_version, ".")[SAFE_OFFSET(1)], "") AS INTEGER),
-      0
-    ) AS os_version_minor,
-    submission_date,
-    is_default_browser,
-    distribution_id,
-    CASE
-      WHEN BIT_COUNT(days_desktop_active_bits)
-        BETWEEN 1
-        AND 6
-        THEN 'infrequent_user'
-      WHEN BIT_COUNT(days_desktop_active_bits)
-        BETWEEN 7
-        AND 13
-        THEN 'casual_user'
-      WHEN BIT_COUNT(days_desktop_active_bits)
-        BETWEEN 14
-        AND 20
-        THEN 'regular_user'
-      WHEN BIT_COUNT(days_desktop_active_bits) >= 21
-        THEN 'core_user'
-      ELSE 'other'
-    END AS segment_dau,
-    CASE
-      WHEN BIT_COUNT(days_seen_bits)
-        BETWEEN 1
-        AND 6
-        THEN 'infrequent_user'
-      WHEN BIT_COUNT(days_seen_bits)
-        BETWEEN 7
-        AND 13
-        THEN 'casual_user'
-      WHEN BIT_COUNT(days_seen_bits)
-        BETWEEN 14
-        AND 20
-        THEN 'regular_user'
-      WHEN BIT_COUNT(days_seen_bits) >= 21
-        THEN 'core_user'
-      ELSE 'other'
-    END AS activity_segment,
-    EXTRACT(YEAR FROM first_seen_date) AS first_seen_year,
-    COALESCE(mozfun.bits28.days_since_seen(days_seen_bits) = 0, FALSE) AS is_daily_user,
-    COALESCE(mozfun.bits28.days_since_seen(days_seen_bits) < 7, FALSE) AS is_weekly_user,
-    COALESCE(mozfun.bits28.days_since_seen(days_seen_bits) < 28, FALSE) AS is_monthly_user,
-    COALESCE(mozfun.bits28.days_since_seen(days_desktop_active_bits) = 0, FALSE) AS is_dau,
-    COALESCE(mozfun.bits28.days_since_seen(days_desktop_active_bits) < 7, FALSE) AS is_wau,
-    COALESCE(mozfun.bits28.days_since_seen(days_desktop_active_bits) < 28, FALSE) AS is_mau
-  FROM
-    `moz-fx-data-shared-prod.firefox_desktop.baseline_clients_last_seen`
-  WHERE
-    submission_date = @submission_date
-)
 SELECT
   submission_date,
   app_name,
@@ -103,7 +22,7 @@ SELECT
   COUNTIF(is_wau) AS wau,
   COUNTIF(is_mau) AS mau
 FROM
-  clients
+  `moz-fx-data-shared-prod.firefox_desktop.baseline_active_users`
 GROUP BY
   submission_date,
   app_name,

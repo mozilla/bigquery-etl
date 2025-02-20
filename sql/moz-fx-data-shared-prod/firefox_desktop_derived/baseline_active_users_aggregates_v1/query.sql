ee21b8c85841997b5c794c62946901a042a4a76e
kik-kik <kignasiak@mozilla.com>
2025-02-20 18:25:06 +0100

    fix: remove dau_segment from baseline_active_users_aggregates and make sure the group and select order is the same

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/baseline_active_users_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/baseline_active_users_aggregates_v1/query.sql
@@ -1,8 +1,8 @@
 SELECT
   submission_date,
-  app_name,
-  app_version,
+  first_seen_year,
   channel,
+  app_name,
   country,
   city,
   locale,
@@ -10,26 +10,25 @@ SELECT
   os_version_build,
   os_version_major,
   os_version_minor,
+  app_version,
   is_default_browser,
   distribution_id,
-  segment_dau,
   activity_segment,
-  first_seen_year,
   COUNTIF(is_daily_user) AS daily_users,
   COUNTIF(is_weekly_user) AS weekly_users,
   COUNTIF(is_monthly_user) AS monthly_users,
   COUNTIF(is_dau) AS dau,
   COUNTIF(is_wau) AS wau,
-  COUNTIF(is_mau) AS mau
+  COUNTIF(is_mau) AS mau,
 FROM
   `moz-fx-data-shared-prod.firefox_desktop.baseline_active_users`
 WHERE
   submission_date = @submission_date
 GROUP BY
   submission_date,
-  app_name,
-  app_version,
+  first_seen_year,
   channel,
+  app_name,
   country,
   city,
   locale,
@@ -37,8 +36,7 @@ GROUP BY
   os_version_build,
   os_version_major,
   os_version_minor,
+  app_version,
   is_default_browser,
   distribution_id,
-  segment_dau,
-  activity_segment,
-  first_seen_year
+  activity_segment

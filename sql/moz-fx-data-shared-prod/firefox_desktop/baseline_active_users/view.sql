b485b35bacb23f0a634fdc2b7dbc492d6fc6d6f9
kik-kik <kignasiak@mozilla.com>
2025-02-21 10:50:43 +0100

    feat: use mozfun.norm.truncate_version for getting major and minor os versions

--- sql/moz-fx-data-shared-prod/firefox_desktop/baseline_active_users/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/baseline_active_users/view.sql
@@ -23,18 +23,13 @@ SELECT
   app_display_version AS app_version,
   normalized_channel AS channel,
   normalized_os AS os,
+  normalized_os_version AS os_version,
+  `mozfun.norm.truncate_version`(normalized_os_version, "major") AS os_version_major,
+  `mozfun.norm.truncate_version`(normalized_os_version, "minor") AS os_version_minor,
   COALESCE(
     `mozfun.norm.windows_version_info`(normalized_os, normalized_os_version, windows_build_number),
     normalized_os_version
   ) AS os_version_build,
-  COALESCE(
-    CAST(NULLIF(SPLIT(normalized_os_version, ".")[SAFE_OFFSET(0)], "") AS INTEGER),
-    0
-  ) AS os_version_major,
-  COALESCE(
-    CAST(NULLIF(SPLIT(normalized_os_version, ".")[SAFE_OFFSET(1)], "") AS INTEGER),
-    0
-  ) AS os_version_minor,
   CASE
     WHEN BIT_COUNT(days_desktop_active_bits)
       BETWEEN 1

b46be4f3a3e94b5ee7cd4372724a5b2ec015924d
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-02-21 19:05:42 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ dags/bqetl_client_attributes.py
@@ -0,0 +1,96 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from operators.gcp_container_operator import GKEPodOperator
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, bigquery_dq_check, bigquery_bigeye_check
+
+docs = """
+### bqetl_client_attributes
+
+Built from bigquery-etl repo, [`dags/bqetl_client_attributes.py`](https://github.com/mozilla/bigquery-etl/blob/generated-sql/dags/bqetl_client_attributes.py)
+
+#### Description
+
+Schedules daily level retention queries
+#### Owner
+
+kwindau@mozilla.com
+
+#### Tags
+
+* impact/tier_2
+* repo/bigquery-etl
+"""
+
+
+default_args = {
+    "owner": "kwindau@mozilla.com",
+    "start_date": datetime.datetime(2025, 2, 20, 0, 0),
+    "end_date": None,
+    "email": ["telemetry-alerts@mozilla.com", "kwindau@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1800),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+}
+
+tags = ["impact/tier_2", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_client_attributes",
+    default_args=default_args,
+    schedule_interval="40 19 * * *",
+    doc_md=docs,
+    tags=tags,
+) as dag:
+
+    wait_for_copy_deduplicate_all = ExternalTaskSensor(
+        task_id="wait_for_copy_deduplicate_all",
+        external_dag_id="copy_deduplicate",
+        external_task_id="copy_deduplicate_all",
+        execution_delta=datetime.timedelta(seconds=67200),
+        check_existence=True,
+        mode="reschedule",
+        poke_interval=datetime.timedelta(minutes=5),
+        allowed_states=ALLOWED_STATES,
+        failed_states=FAILED_STATES,
+        pool="DATA_ENG_EXTERNALTASKSENSOR",
+    )
+
+    checks__fail_telemetry_derived__fx_accounts_active_daily_clients__v1 = bigquery_dq_check(
+        task_id="checks__fail_telemetry_derived__fx_accounts_active_daily_clients__v1",
+        source_table="fx_accounts_active_daily_clients_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        is_dq_check_fail=True,
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        depends_on_past=False,
+        parameters=["submission_date:DATE:{{ds}}"],
+        retries=0,
+    )
+
+    telemetry_derived__fx_accounts_active_daily_clients__v1 = bigquery_etl_query(
+        task_id="telemetry_derived__fx_accounts_active_daily_clients__v1",
+        destination_table="fx_accounts_active_daily_clients_v1",
+        dataset_id="telemetry_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com", "telemetry-alerts@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
+    checks__fail_telemetry_derived__fx_accounts_active_daily_clients__v1.set_upstream(
+        telemetry_derived__fx_accounts_active_daily_clients__v1
+    )
+
+    telemetry_derived__fx_accounts_active_daily_clients__v1.set_upstream(
+        wait_for_copy_deduplicate_all
+    )

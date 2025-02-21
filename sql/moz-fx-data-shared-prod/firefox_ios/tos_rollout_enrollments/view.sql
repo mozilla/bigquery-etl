be64227e09f49adc35fba635d76ff4841d16661d
Daniel Berry <dberry@mozilla.com>
2025-02-21 17:15:52 -0500

    Creating ToS enrollments tables for monitoring of ToS rollout (#7079)
    * first attempt
    
    * Schemas and minor fixes
    
    ---------
    
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>

---
+++ sql/moz-fx-data-shared-prod/firefox_ios/tos_rollout_enrollments/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_ios.tos_rollout_enrollments`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_ios_derived.tos_rollout_enrollments_v1`

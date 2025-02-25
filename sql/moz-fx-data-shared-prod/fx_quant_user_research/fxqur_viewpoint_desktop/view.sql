9012767aebf6feb1ec1cffddef1b96f68defe1ea
ogzillla <197825729+ogzillla@users.noreply.github.com>
2025-02-25 13:28:01 -0500

    Deng 7935 create views for fx user research team (#7092)
    * Revert "DENG-6770: adding print statements for debugging"
    
    This reverts commit 6974cf1db628dbacca6a87728427d21b47559dcc.
    
    * DENG-7935: create view atop alchemer tables
    
    * DENG-7935: add schema files
    
    * DENG-7935: add schema files
    
    * DENG-7935: update wg access for data viewers

---
+++ sql/moz-fx-data-shared-prod/fx_quant_user_research/fxqur_viewpoint_desktop/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fx_quant_user_research.fxqur_viewpoint_desktop`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.fx_quant_user_research_derived.fxqur_viewpoint_desktop_v1`
---
+++ sql/moz-fx-data-shared-prod/fx_quant_user_research/fxqur_viewpoint_mobile/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fx_quant_user_research.fxqur_viewpoint_mobile`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.fx_quant_user_research_derived.fxqur_viewpoint_mobile_v1`

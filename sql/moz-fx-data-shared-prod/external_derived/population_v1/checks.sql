955523cfb39c9c2060029592fe3a537e16fe0ca9
Katie Windau <kwindau@mozilla.com>
2025-02-26 13:55:17 -0600

    Update checks.sql to include full primary key

--- sql/moz-fx-data-shared-prod/external_derived/population_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/external_derived/population_v1/checks.sql
@@ -1,1 +1,1 @@
-{{ is_unique(["location_id", "indicator_id", "time_label", "sex_id", "variant_id", "estimate_method_id", "age_id"])}}
+{{ is_unique(["location_id", "source_id", "indicator_id", "time_id", "revision", "sex_id", "variant_id", "estimate_method_id", "category_id","estimate_type_id","age_id"])}}

e466abeccedc2aaf1c1769cde308e286d32f5250
Katie Windau <kwindau@mozilla.com>
2025-02-27 16:04:14 -0600

    Initial commit

---
+++ sql/moz-fx-data-shared-prod/telemetry/cohort_weekly_statistics/view.sql
@@ -0,0 +1,79 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.cohort_weekly_statistics`
+AS
+WITH clients_first_seen_last_180_days AS (
+  SELECT
+    normalized_app_name,
+    normalized_channel,
+    app_version,
+    DATE_TRUNC(cohort_date, WEEK) AS cohort_date_week,
+    client_id
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.rolling_cohorts_v2`
+  WHERE
+    cohort_date >= DATE_SUB(CURRENT_DATE, INTERVAL 50 DAY)
+),
+submission_date_activity AS (
+  SELECT DISTINCT
+    client_id,
+    submission_date AS activity_date,
+    DATE_TRUNC(submission_date, WEEK) AS activity_date_week
+  FROM
+    `moz-fx-data-shared-prod.telemetry.active_users`
+  WHERE
+    submission_date > DATE_SUB(CURRENT_DATE, INTERVAL 50 DAY)
+    AND is_dau IS TRUE
+),
+clients_first_seen_in_last_180_days_and_activity_next_180_days AS (
+  SELECT
+    a.normalized_app_name,
+    a.normalized_channel,
+    a.app_version,
+    a.cohort_date_week,
+    b.activity_date_week,
+    COUNT(DISTINCT(b.client_id)) AS nbr_active_clients
+  FROM
+    clients_first_seen_last_180_days a
+  LEFT JOIN
+    submission_date_activity b
+    ON a.client_id = b.client_id
+  GROUP BY
+    a.normalized_app_name,
+    a.normalized_channel,
+    a.app_version,
+    a.cohort_date_week,
+    b.activity_date_week
+),
+--get # of unique clients by cohort start date week, normalized app name, channel, and app version
+initial_cohort_counts AS (
+  SELECT
+    normalized_app_name,
+    normalized_channel,
+    app_version,
+    cohort_date_week,
+    COUNT(DISTINCT(client_id)) AS nbr_clients_in_cohort
+  FROM
+    clients_first_seen_last_180_days
+  GROUP BY
+    normalized_app_name,
+    normalized_channel,
+    app_version,
+    cohort_date_week
+)
+SELECT
+  i.normalized_app_name,
+  i.normalized_channel,
+  i.app_version,
+  i.cohort_date_week,
+  i.nbr_clients_in_cohort,
+  a.activity_date_week,
+  a.nbr_active_clients,
+  SAFE_DIVIDE(a.nbr_active_clients, i.nbr_clients_in_cohort) AS pct_retained
+FROM
+  initial_cohort_counts AS i
+LEFT JOIN
+  clients_first_seen_in_last_180_days_and_activity_next_180_days AS a
+  ON COALESCE(i.normalized_app_name, 'NULL') = COALESCE(a.normalized_app_name, 'NULL')
+  AND COALESCE(i.normalized_channel, 'NULL') = COALESCE(a.normalized_channel, 'NULL')
+  AND COALESCE(i.app_version, 'NULL') = COALESCE(a.app_version, 'NULL')
+  AND i.cohort_date_week = a.cohort_date_week

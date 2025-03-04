a650a8f9e639359ff5072ec6dd31c0b8eae407c9
Katie Windau <kwindau@mozilla.com>
2025-03-04 15:56:30 -0600

    Initial commit

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/fx_accounts_linked_clients_v1/script.sql
@@ -0,0 +1,36 @@
+MERGE INTO
+  `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_linked_clients_v1` AS T
+  USING (
+    SELECT
+      n.client_id,
+      n.linked_client_id,
+      COALESCE(old.first_seen_linkage_date, n.submission_date) AS first_seen_linkage_date,
+      n.submission_date AS last_seen_linkage_date
+    FROM
+      (
+        SELECT
+          client_id,
+          linked_client_id,
+          submission_date
+        FROM
+          `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_linked_clients_staging_v1`
+        WHERE
+          submission_date = @submission_date
+      ) n
+    LEFT JOIN
+      `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_linked_clients_v1` old
+      ON a.client_id = old.client_id
+      AND a.linked_client_id = old.linked_client_id
+  ) AS S
+  ON T.client_id = S.client_id
+  AND T.linked_client_id = S.linked_client_id
+WHEN NOT MATCHED BY TARGET
+THEN
+  INSERT
+    (client_id, linked_client_id, first_seen_linkage_date, last_seen_linkage_date)
+  VALUES
+    (S.client_id, S.linked_client_id, S.first_seen_linkage_date, S.last_seen_linkage_date)
+  WHEN MATCHED
+THEN
+  UPDATE
+    SET T.last_seen_linkage_date = S.last_seen_linkage_date

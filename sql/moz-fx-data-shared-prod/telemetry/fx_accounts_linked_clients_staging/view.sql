ce8ffc6852553e63b04f36f8768ae555e25ce44c
Katie Windau <kwindau@mozilla.com>
2025-03-04 13:27:20 -0600

    feat(DENG-7982): Create fx_accounts_linked_clients_staging_v1

---
+++ sql/moz-fx-data-shared-prod/telemetry/fx_accounts_linked_clients_staging/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.fx_accounts_linked_clients_staging`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_linked_clients_staging_v1`

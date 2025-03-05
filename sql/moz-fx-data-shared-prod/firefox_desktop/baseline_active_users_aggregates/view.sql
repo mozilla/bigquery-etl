61ff446140016286fac4566388bc46d2cbc9a8b9
kik-kik <kignasiak@mozilla.com>
2025-03-05 16:55:49 +0100

    feat: add app_version breakdown fields to the view

--- sql/moz-fx-data-shared-prod/firefox_desktop/baseline_active_users_aggregates/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/baseline_active_users_aggregates/view.sql
@@ -9,5 +9,9 @@ SELECT
     `mozfun.norm.windows_version_info`(os, os_version, windows_build_number),
     os_version
   ) AS os_version_build,
+  `mozfun.norm.browser_version_info`(app_version).major_version AS app_version_major,
+  `mozfun.norm.browser_version_info`(app_version).minor_version AS app_version_minor,
+  `mozfun.norm.browser_version_info`(app_version).patch_revision AS app_version_patch_revision,
+  `mozfun.norm.browser_version_info`(app_version).is_major_release AS app_version_is_major_release,
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_active_users_aggregates_v1`

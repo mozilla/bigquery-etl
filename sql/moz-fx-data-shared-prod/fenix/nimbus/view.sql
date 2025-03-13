2b0f3fa2c2243ac6087c1f194ebb72000e57d700
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-03-13 13:48:34 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/fenix/nimbus/view.sql
@@ -0,0 +1,173 @@
+-- Generated via ./bqetl generate glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.fenix.nimbus`
+AS
+SELECT
+  "org_mozilla_firefox" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_firefox",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.object.nimbus_system_recorded_nimbus_context) AS `object`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox.nimbus`
+UNION ALL
+SELECT
+  "org_mozilla_firefox_beta" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_firefox_beta",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.object.nimbus_system_recorded_nimbus_context) AS `object`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_beta.nimbus`
+UNION ALL
+SELECT
+  "org_mozilla_fenix" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_fenix",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.object.nimbus_system_recorded_nimbus_context) AS `object`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix.nimbus`
+UNION ALL
+SELECT
+  "org_mozilla_fenix_nightly" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_fenix_nightly",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.object.nimbus_system_recorded_nimbus_context) AS `object`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fenix_nightly.nimbus`
+UNION ALL
+SELECT
+  "org_mozilla_fennec_aurora" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_fennec_aurora",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.object.nimbus_system_recorded_nimbus_context) AS `object`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fennec_aurora.nimbus`

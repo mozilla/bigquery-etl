80f495f5b92784bf091430c9dde8027e74d4e2e4
Katie Windau <kwindau@mozilla.com>
2025-03-13 21:03:11 -0500

    Fix view select lists

--- sql/moz-fx-data-shared-prod/telemetry/fx_accounts_linked_clients/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/fx_accounts_linked_clients/view.sql
@@ -5,10 +5,6 @@ SELECT
   client_id,
   linked_client_id,
   linkage_first_seen_date,
-  linkage_last_seen_date,
-  client_id_first_seen_date,
-  linked_client_id_first_seen_date,
-  client_id_last_seen_date,
-  linked_client_id_last_seen_date
+  linkage_last_seen_date
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_linked_clients_v1`
--- sql/moz-fx-data-shared-prod/telemetry/fx_accounts_linked_clients_ordered/view.sql
+++ sql/moz-fx-data-shared-prod/telemetry/fx_accounts_linked_clients_ordered/view.sql
@@ -2,6 +2,13 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.telemetry.fx_accounts_linked_clients_ordered`
 AS
 SELECT
-  *
+  client_id,
+  linked_client_id,
+  linkage_first_seen_date,
+  linkage_last_seen_date,
+  client_id_first_seen_date,
+  linked_client_id_first_seen_date,
+  client_id_last_seen_date,
+  linked_client_id_last_seen_date
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.fx_accounts_linked_clients_ordered_v1`

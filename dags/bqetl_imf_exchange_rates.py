51368052a8d1ccb5923e10143494d261fdb561e9
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-03-17 17:53:29 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ dags/bqetl_imf_exchange_rates.py
@@ -0,0 +1,82 @@
+# Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskMarker
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+import datetime
+from operators.gcp_container_operator import GKEPodOperator
+from utils.constants import ALLOWED_STATES, FAILED_STATES
+from utils.gcp import bigquery_etl_query, bigquery_dq_check, bigquery_bigeye_check
+
+docs = """
+### bqetl_imf_exchange_rates
+
+Built from bigquery-etl repo, [`dags/bqetl_imf_exchange_rates.py`](https://github.com/mozilla/bigquery-etl/blob/generated-sql/dags/bqetl_imf_exchange_rates.py)
+
+#### Description
+
+This DAG pulls exchange rate data from the International Monetary Fund API
+
+#### Owner
+
+kwindau@mozilla.com
+
+#### Tags
+
+* impact/tier_3
+* repo/bigquery-etl
+"""
+
+
+default_args = {
+    "owner": "kwindau@mozilla.com",
+    "start_date": datetime.datetime(2025, 1, 1, 0, 0),
+    "end_date": None,
+    "email": ["kwindau@mozilla.com"],
+    "depends_on_past": False,
+    "retry_delay": datetime.timedelta(seconds=1500),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+}
+
+tags = ["impact/tier_3", "repo/bigquery-etl"]
+
+with DAG(
+    "bqetl_imf_exchange_rates",
+    default_args=default_args,
+    schedule_interval="23 0 18 * *",
+    doc_md=docs,
+    tags=tags,
+    catchup=False,
+) as dag:
+
+    checks__fail_external_derived__exchange_rates__v1 = bigquery_dq_check(
+        task_id="checks__fail_external_derived__exchange_rates__v1",
+        source_table="exchange_rates_v1",
+        dataset_id="external_derived",
+        project_id="moz-fx-data-shared-prod",
+        is_dq_check_fail=True,
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com"],
+        depends_on_past=False,
+        parameters=["submission_date:DATE:{{ds}}"],
+        retries=0,
+    )
+
+    external_derived__exchange_rates__v1 = GKEPodOperator(
+        task_id="external_derived__exchange_rates__v1",
+        arguments=[
+            "python",
+            "sql/moz-fx-data-shared-prod/external_derived/exchange_rates_v1/query.py",
+        ]
+        + [],
+        image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
+        owner="kwindau@mozilla.com",
+        email=["kwindau@mozilla.com"],
+    )
+
+    checks__fail_external_derived__exchange_rates__v1.set_upstream(
+        external_derived__exchange_rates__v1
+    )

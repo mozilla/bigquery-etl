d1db7395ec9e95c3d466b3d292c362710c293a91
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-03-28 16:43:40 +0000

    Auto-push due to change on main branch [ci skip]

---
+++ sql/moz-fx-data-shared-prod/glam/events_stream/view.sql
@@ -0,0 +1,8 @@
+-- Generated via bigquery_etl.glean_usage
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.glam.events_stream`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.glam_derived.events_stream_v1`

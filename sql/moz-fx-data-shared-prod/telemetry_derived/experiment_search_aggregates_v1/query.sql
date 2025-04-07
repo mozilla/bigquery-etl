e460e0e4c2e365d6c057d87409418d37c5de99f4
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-04-07 22:42:06 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_search_aggregates_v1/query.sql
@@ -1,5 +1,85 @@
 -- Generated via ./bqetl generate experiment_monitoring
-WITH org_mozilla_firefox_beta AS (
+-- Filter out applications that have all search metrics definitions set to null.
+-- This reduces query complexity.
+WITH telemetry AS (
+  SELECT
+    submission_timestamp,
+    unnested_experiments.key AS experiment,
+    unnested_experiments.value AS branch,
+    SUM(
+      (
+        SELECT
+          SUM(value.value)
+        FROM
+          UNNEST(payload.processes.parent.keyed_scalars.browser_search_adclicks_urlbar) AS value
+      )
+    ) AS ad_clicks_count,
+    SUM(
+      (
+        SELECT
+          SUM(value.value)
+        FROM
+          UNNEST(payload.processes.parent.keyed_scalars.browser_search_withads_urlbar) AS value
+      )
+    ) AS search_with_ads_count,
+    SUM(
+      (
+        SELECT
+          SUM(`moz-fx-data-shared-prod`.udf.extract_histogram_sum(value.value))
+        FROM
+          UNNEST(payload.keyed_histograms.search_counts) AS value
+      )
+    ) AS search_count,
+  FROM
+    `moz-fx-data-shared-prod.telemetry_stable.main_v5`
+  LEFT JOIN
+    UNNEST(
+      ARRAY(SELECT AS STRUCT key, value.branch AS value FROM UNNEST(environment.experiments))
+    ) AS unnested_experiments
+  GROUP BY
+    submission_timestamp,
+    experiment,
+    branch
+),
+org_mozilla_ios_firefox AS (
+  SELECT
+    submission_timestamp,
+    experiment.key AS experiment,
+    experiment.value.branch AS branch,
+    SUM(0) AS ad_clicks_count,
+    SUM(0) AS search_with_ads_count,
+    SUM(
+      (SELECT SUM(value.value) FROM UNNEST(metrics.labeled_counter.search_counts) AS value)
+    ) AS search_count,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_ios_firefox_stable.metrics_v1`
+  LEFT JOIN
+    UNNEST(ping_info.experiments) AS experiment
+  GROUP BY
+    submission_timestamp,
+    experiment,
+    branch
+),
+org_mozilla_ios_firefoxbeta AS (
+  SELECT
+    submission_timestamp,
+    experiment.key AS experiment,
+    experiment.value.branch AS branch,
+    SUM(0) AS ad_clicks_count,
+    SUM(0) AS search_with_ads_count,
+    SUM(
+      (SELECT SUM(value.value) FROM UNNEST(metrics.labeled_counter.search_counts) AS value)
+    ) AS search_count,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta_stable.metrics_v1`
+  LEFT JOIN
+    UNNEST(ping_info.experiments) AS experiment
+  GROUP BY
+    submission_timestamp,
+    experiment,
+    branch
+),
+org_mozilla_firefox_beta AS (
   SELECT
     submission_timestamp,
     experiment.key AS experiment,
@@ -98,44 +178,6 @@ org_mozilla_firefox AS (
     experiment,
     branch
 ),
-org_mozilla_ios_firefox AS (
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    SUM(0) AS ad_clicks_count,
-    SUM(0) AS search_with_ads_count,
-    SUM(
-      (SELECT SUM(value.value) FROM UNNEST(metrics.labeled_counter.search_counts) AS value)
-    ) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.org_mozilla_ios_firefox_stable.metrics_v1`
-  LEFT JOIN
-    UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
-org_mozilla_ios_firefoxbeta AS (
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    SUM(0) AS ad_clicks_count,
-    SUM(0) AS search_with_ads_count,
-    SUM(
-      (SELECT SUM(value.value) FROM UNNEST(metrics.labeled_counter.search_counts) AS value)
-    ) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta_stable.metrics_v1`
-  LEFT JOIN
-    UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
 org_mozilla_ios_fennec AS (
   SELECT
     submission_timestamp,
@@ -150,46 +192,6 @@ org_mozilla_ios_fennec AS (
     `moz-fx-data-shared-prod.org_mozilla_ios_fennec_stable.metrics_v1`
   LEFT JOIN
     UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
-telemetry AS (
-  SELECT
-    submission_timestamp,
-    unnested_experiments.key AS experiment,
-    unnested_experiments.value AS branch,
-    SUM(
-      (
-        SELECT
-          SUM(value.value)
-        FROM
-          UNNEST(payload.processes.parent.keyed_scalars.browser_search_adclicks_urlbar) AS value
-      )
-    ) AS ad_clicks_count,
-    SUM(
-      (
-        SELECT
-          SUM(value.value)
-        FROM
-          UNNEST(payload.processes.parent.keyed_scalars.browser_search_withads_urlbar) AS value
-      )
-    ) AS search_with_ads_count,
-    SUM(
-      (
-        SELECT
-          SUM(`moz-fx-data-shared-prod`.udf.extract_histogram_sum(value.value))
-        FROM
-          UNNEST(payload.keyed_histograms.search_counts) AS value
-      )
-    ) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.telemetry_stable.main_v5`
-  LEFT JOIN
-    UNNEST(
-      ARRAY(SELECT AS STRUCT key, value.branch AS value FROM UNNEST(environment.experiments))
-    ) AS unnested_experiments
   GROUP BY
     submission_timestamp,
     experiment,
@@ -319,75 +321,22 @@ org_mozilla_focus_beta AS (
     experiment,
     branch
 ),
-org_mozilla_ios_klar AS (
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    SUM(0) AS ad_clicks_count,
-    SUM(0) AS search_with_ads_count,
-    SUM(0) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.org_mozilla_ios_klar_stable.metrics_v1`
-  LEFT JOIN
-    UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
-org_mozilla_ios_focus AS (
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    SUM(0) AS ad_clicks_count,
-    SUM(0) AS search_with_ads_count,
-    SUM(0) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.org_mozilla_ios_focus_stable.metrics_v1`
-  LEFT JOIN
-    UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
-monitor_cirrus AS (
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    SUM(0) AS ad_clicks_count,
-    SUM(0) AS search_with_ads_count,
-    SUM(0) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.monitor_cirrus_stable.metrics_v1`
-  LEFT JOIN
-    UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
-accounts_cirrus AS (
-  SELECT
-    submission_timestamp,
-    experiment.key AS experiment,
-    experiment.value.branch AS branch,
-    SUM(0) AS ad_clicks_count,
-    SUM(0) AS search_with_ads_count,
-    SUM(0) AS search_count,
-  FROM
-    `moz-fx-data-shared-prod.accounts_cirrus_stable.metrics_v1`
-  LEFT JOIN
-    UNNEST(ping_info.experiments) AS experiment
-  GROUP BY
-    submission_timestamp,
-    experiment,
-    branch
-),
 all_events AS (
+  SELECT
+    *
+  FROM
+    telemetry
+  UNION ALL
+  SELECT
+    *
+  FROM
+    org_mozilla_ios_firefox
+  UNION ALL
+  SELECT
+    *
+  FROM
+    org_mozilla_ios_firefoxbeta
+  UNION ALL
   SELECT
     *
   FROM
@@ -406,22 +355,7 @@ all_events AS (
   SELECT
     *
   FROM
-    org_mozilla_ios_firefox
-  UNION ALL
-  SELECT
-    *
-  FROM
-    org_mozilla_ios_firefoxbeta
-  UNION ALL
-  SELECT
-    *
-  FROM
     org_mozilla_ios_fennec
-  UNION ALL
-  SELECT
-    *
-  FROM
-    telemetry
   UNION ALL
   SELECT
     *
@@ -442,26 +376,6 @@ all_events AS (
     *
   FROM
     org_mozilla_focus_beta
-  UNION ALL
-  SELECT
-    *
-  FROM
-    org_mozilla_ios_klar
-  UNION ALL
-  SELECT
-    *
-  FROM
-    org_mozilla_ios_focus
-  UNION ALL
-  SELECT
-    *
-  FROM
-    monitor_cirrus
-  UNION ALL
-  SELECT
-    *
-  FROM
-    accounts_cirrus
 )
 SELECT
   experiment,

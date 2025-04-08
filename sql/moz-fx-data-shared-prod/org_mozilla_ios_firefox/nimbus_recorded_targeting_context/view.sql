d3804dc4be4a0166bae9d0be69da9f346b6bae1f
Charlie Humphreys <chumphreys@mozilla.com>
2025-04-08 11:30:02 -0500

    Add nimbus_recorded_targeting_context queries for fenix and firefox ios

---
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefox/nimbus_recorded_targeting_context/view.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefox.nimbus_recorded_targeting_context`
+AS
+SELECT
+  m.*
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefox_derived.nimbus_recorded_targeting_context_v1` m
+INNER JOIN (
+  SELECT
+    client_id,
+    MAX(submission_date) as latest_date
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_ios_firefox_derived.nimbus_recorded_targeting_context_v1`
+  GROUP BY client_id
+) ld
+ON
+  m.client_id = ld.client_id AND m.submission_date = ld.latest_date

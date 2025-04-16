85e656054cad49ff7a341f18dd707d3afe939516
Marlene-M-Hirose <mhirose@mozilla.com>
2025-04-16 12:44:18 -1000

    add cost_center_heirarchy_group to arguments

--- sql/moz-fx-data-shared-prod/udf/hr_cost_center_to_steering_committee/udf.sql
+++ sql/moz-fx-data-shared-prod/udf/hr_cost_center_to_steering_committee/udf.sql
@@ -2,13 +2,16 @@
 This is a stub implementation for use with tests in this repo
 Real implementation is in private-bigquery-etl
 */
-CREATE OR REPLACE FUNCTION udf.hr_cost_center_to_steering_committee(cost_center STRING)
+CREATE OR REPLACE FUNCTION udf.hr_cost_center_to_steering_committee(
+  cost_center STRING,
+  cost_center_hierarchy_group STRING
+)
 RETURNS STRING AS (
   'steering_committee'
 );
 
 SELECT
   mozfun.assert.equals(
-    udf.hr_cost_center_to_steering_committee('cost_center'),
+    udf.hr_cost_center_to_steering_committee('cost_center', 'cost_center_heirarchy_group'),
     'steering_committee'
   );

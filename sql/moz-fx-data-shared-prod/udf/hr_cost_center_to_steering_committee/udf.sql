faca7a8a37c5314e1af672f28bf2bd0949bf3099
Marlene-M-Hirose <mhirose@mozilla.com>
2025-04-16 11:01:52 -1000

    add argument to UDF

--- sql/moz-fx-data-shared-prod/udf/hr_cost_center_to_steering_committee/udf.sql
+++ sql/moz-fx-data-shared-prod/udf/hr_cost_center_to_steering_committee/udf.sql
@@ -2,10 +2,10 @@
 This is a stub implementation for use with tests in this repo
 Real implementation is in private-bigquery-etl
 */
-CREATE OR REPLACE FUNCTION udf.hr_cost_center_to_steering_committee()
+CREATE OR REPLACE FUNCTION udf.hr_cost_center_to_steering_committee(cost_center STRING)
 RETURNS STRING AS (
-  'helloworld'
+  'cost_center_group'
 );
 
 SELECT
-  mozfun.assert.equals(udf.hr_cost_center_to_steering_committee(), 'helloworld');
+  mozfun.assert.equals(udf.hr_cost_center_to_steering_committee(), 'cost_center_group');
--- sql/moz-fx-data-shared-prod/udf/hr_steering_committee_to_group_name/udf.sql
+++ sql/moz-fx-data-shared-prod/udf/hr_steering_committee_to_group_name/udf.sql
@@ -2,10 +2,10 @@
 This is a stub implementation for use with tests in this repo
 Real implementation is in private-bigquery-etl
 */
-CREATE OR REPLACE FUNCTION udf.hr_steering_committee_to_group_name()
+CREATE OR REPLACE FUNCTION udf.hr_steering_committee_to_group_name(steering_committee STRING)
 RETURNS STRING AS (
-  'helloworld'
+  'steering_committee_group'
 );
 
 SELECT
-  mozfun.assert.equals(udf.hr_steering_committee_to_group_name(), 'helloworld');
+  mozfun.assert.equals(udf.hr_steering_committee_to_group_name(), 'steering_committee_group');

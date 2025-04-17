b454a3ba01d57a69597fa3602b5a459478c7134e
Marlene Hirose <92952117+Marlene-M-Hirose@users.noreply.github.com>
2025-04-16 19:05:35 -0700

    add argument to UDF (#7326)
    * add argument to UDF
    
    * add arguments to assertion
    
    * update what strings are returned to match udf return
    
    * change quotation marks to ticks
    
    * change tick marks to single quotes
    
    * add cost_center_heirarchy_group to arguments

--- sql/moz-fx-data-shared-prod/udf/hr_cost_center_to_steering_committee/udf.sql
+++ sql/moz-fx-data-shared-prod/udf/hr_cost_center_to_steering_committee/udf.sql
@@ -2,10 +2,16 @@
 This is a stub implementation for use with tests in this repo
 Real implementation is in private-bigquery-etl
 */
-CREATE OR REPLACE FUNCTION udf.hr_cost_center_to_steering_committee()
+CREATE OR REPLACE FUNCTION udf.hr_cost_center_to_steering_committee(
+  cost_center STRING,
+  cost_center_hierarchy_group STRING
+)
 RETURNS STRING AS (
-  'helloworld'
+  'steering_committee'
 );
 
 SELECT
-  mozfun.assert.equals(udf.hr_cost_center_to_steering_committee(), 'helloworld');
+  mozfun.assert.equals(
+    udf.hr_cost_center_to_steering_committee('cost_center', 'cost_center_heirarchy_group'),
+    'steering_committee'
+  );

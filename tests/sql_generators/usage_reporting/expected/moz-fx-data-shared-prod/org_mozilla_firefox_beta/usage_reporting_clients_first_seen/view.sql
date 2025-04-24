5deea7f01ff5dc8d8f384135063a6fb4aef210f1
kik-kik <42538694+kik-kik@users.noreply.github.com>
2025-04-24 19:04:38 +0200

    feat(DENG-8074): Usage reporting generator - add tests (part 3) (#7353)
    * feat: move probe_scraper mock response into separate config
    
    * feat: add a test to usage_reporting to make sure contents generated are as expected to make a code refactor easier
    
    * feat: use symmetric_difference to get set delta instead of ^ operator
    
    * feat: use _ConfigLoader for getting test config

---
+++ tests/sql_generators/usage_reporting/expected/moz-fx-data-shared-prod/org_mozilla_firefox_beta/usage_reporting_clients_first_seen/view.sql
@@ -0,0 +1,8 @@
+-- Generated via `usage_reporting` SQL generator.
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.org_mozilla_firefox_beta.usage_reporting_clients_first_seen`
+AS
+SELECT
+  *,
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_firefox_beta_derived.usage_reporting_clients_first_seen_v1`

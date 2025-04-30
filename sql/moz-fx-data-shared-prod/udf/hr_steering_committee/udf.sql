b4397c74e53708669d3b2806c39db842f1b5094b
Marlene-M-Hirose <mhirose@mozilla.com>
2025-04-30 09:34:09 -1000

    remove stub files

--- sql/moz-fx-data-shared-prod/udf/hr_steering_committee/udf.sql
+++
@@ -1,8 +0,0 @@
-/*
-This is a stub implementation for use with tests in this repo
-Real implementation is in private-bigquery-etl
-*/
-CREATE OR REPLACE FUNCTION udf.hr_steering_committee(employee_id STRING)
-RETURNS STRING AS (
-  'steering_committee'
-);

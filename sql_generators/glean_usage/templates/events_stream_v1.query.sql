510186481b6861f6339d97486d916b996c0310c4
Anna Scholtz <anna@scholtzan.net>
2025-05-07 11:05:18 -0700

    Address feedback

--- sql_generators/glean_usage/templates/events_stream_v1.query.sql
+++ sql_generators/glean_usage/templates/events_stream_v1.query.sql
@@ -135,19 +135,21 @@ SELECT
 FROM
   base
 CROSS JOIN
+  {% if app_name == "firefox_desktop_background_update" %}
   -- See https://mozilla-hub.atlassian.net/browse/DENG-8432
   -- Filtering out nimbus and normandy events that are emitted due to 'invalid-feature'.
   -- The number of these events is too large to be processed, invalid-feature has also
   -- been removed in more recent versions.
-  {% if app_name == "firefox_desktop_background_update" %}
-  ARRAY(
-    SELECT event
-    FROM UNNEST(events) AS event
-    WHERE NOT (
-      app_version_major = 138 AND app_version_minor IN (0, 1)
-      AND (
-        (event.category = 'nimbus_events' AND event.name = 'validation_failed')
-        OR (event.category = 'normandy' AND event.name = 'validation_failed_nimbus_experiment')
+  UNNEST(
+    ARRAY(
+      SELECT event
+      FROM UNNEST(events) AS event
+      WHERE NOT (
+        app_version_major = 138 AND app_version_minor IN (0, 1)
+        AND (
+          (event.category = 'nimbus_events' AND event.name = 'validation_failed')
+          OR (event.category = 'normandy' AND event.name = 'validation_failed_nimbus_experiment')
+        )
       )
     )
   )

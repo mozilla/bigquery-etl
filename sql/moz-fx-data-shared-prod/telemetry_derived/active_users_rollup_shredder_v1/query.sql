cdf48a4975650ee2804ed6aacb77a556b6e4e165
Katie Windau <kwindau@mozilla.com>
2025-05-09 12:05:22 -0500

    Create telemetry.active_users_rollup_shredder to measure shredder impact over time

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/active_users_rollup_shredder_v1/query.sql
@@ -0,0 +1,23 @@
+SELECT
+  submission_date,
+  app_name,
+  @submission_date AS logical_dag_date,
+  CURRENT_DATE() AS run_date,
+  SUM(CAST(is_dau AS int)) AS dau,
+  SUM(CAST(is_wau AS int)) AS wau,
+  SUM(CAST(is_mau AS int)) AS mau,
+  SUM(CAST(is_daily_user AS int)) AS daily_users,
+  SUM(CAST(is_weekly_user AS int)) AS weekly_users,
+  SUM(CAST(is_monthly_user AS int)) AS monthly_users,
+  SUM(CAST(is_desktop AS int)) AS desktop,
+  SUM(CAST(is_mobile AS int)) AS mobile,
+  COUNT(DISTINCT(client_id)) AS unique_client_ids
+FROM
+  `moz-fx-data-shared-prod.telemetry.active_users`
+WHERE
+  submission_date >= '2023-01-01'
+GROUP BY
+  submission_date,
+  app_name,
+  logical_dag_date,
+  run_date

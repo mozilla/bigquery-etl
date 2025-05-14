e58f50a69c5e05d21d75437bf8a0987c7401d67b
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-05-14 14:09:20 -0500

    Break cohort_weekly_statistics_v1 into intermediate steps (#7441)
    * Break cohort_weekly_statistics_v1 into physicalized intermediate staging tables to improve effificiency
    
    * Break into separate physical tables to make more efficient
    
    * Add client level label
    
    * Add client level labels and retention limits
    
    * Fix typo

--- sql/moz-fx-data-shared-prod/telemetry_derived/cohort_weekly_statistics_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/cohort_weekly_statistics_v1/query.sql
@@ -1,74 +1,4 @@
-WITH clients_first_seen AS (
-  --Get 1 row per client ID, with all their attributes as of their first seen date
-  SELECT
-    normalized_app_name,
-    normalized_channel,
-    app_version,
-    attribution_campaign,
-    attribution_content,
-    attribution_experiment,
-    attribution_medium,
-    attribution_source,
-    attribution_variation,
-    country,
-    device_model,
-    distribution_id,
-    is_default_browser,
-    locale,
-    normalized_os,
-    normalized_os_version,
-    adjust_ad_group,
-    adjust_campaign,
-    adjust_creative,
-    adjust_network,
-    play_store_attribution_campaign,
-    play_store_attribution_medium,
-    play_store_attribution_source,
-    play_store_attribution_content,
-    play_store_attribution_term,
-    play_store_attribution_install_referrer_response,
-    DATE_TRUNC(cohort_date, WEEK) AS cohort_date_week,
-    client_id
-  FROM
-    `moz-fx-data-shared-prod.telemetry_derived.rolling_cohorts_v2`
-  WHERE
-    cohort_date >= DATE_TRUNC(
-      DATE_SUB(@submission_date, INTERVAL 768 day),
-      WEEK
-    ) --start of week for date 768 days ago
-    AND cohort_date <= DATE_SUB(
-      DATE_TRUNC(@submission_date, WEEK),
-      INTERVAL 1 DAY
-    ) --end of last completed week
-    AND LOWER(normalized_app_name) NOT LIKE '%browserstack'
-    AND LOWER(normalized_app_name) NOT LIKE '%mozillaonline'
-  QUALIFY
-    ROW_NUMBER() OVER (
-      PARTITION BY
-        client_id
-      ORDER BY
-        cohort_date ASC
-    ) = 1  --necessary due to mobile having some clients with multiple first seen dates
-),
-weekly_active_clients AS (
-  --Get 1 row per client ID & week where the client ID was a DAU at least 1 day during that week
-  SELECT DISTINCT
-    client_id,
-    DATE_TRUNC(submission_date, WEEK) AS activity_date_week
-  FROM
-    `moz-fx-data-shared-prod.telemetry.active_users`
-  WHERE
-    submission_date >= DATE_TRUNC(
-      DATE_SUB(@submission_date, INTERVAL 768 day),
-      WEEK
-    ) --start of week for date 768 days ago
-    AND submission_date <= DATE_SUB(
-      DATE_TRUNC(@submission_date, WEEK),
-      INTERVAL 1 DAY
-    ) --through end of last completed week
-    AND is_dau IS TRUE
-),
-unique_weeks AS (
+WITH unique_weeks AS (
   SELECT DISTINCT
     first_date_of_week AS activity_date_week
   FROM
@@ -114,7 +44,7 @@ initial_cohort_counts AS (
     cohort_date_week,
     COUNT(DISTINCT(client_id)) AS nbr_clients_in_cohort
   FROM
-    clients_first_seen
+    `moz-fx-data-shared-prod.telemetry_derived.cohort_weekly_cfs_staging_v1`
   GROUP BY
     normalized_app_name,
     normalized_channel,
@@ -212,9 +142,9 @@ weekly_active_agg AS (
     wac.activity_date_week,
     COUNT(DISTINCT(wac.client_id)) AS nbr_active_clients
   FROM
-    clients_first_seen cfs
+    `moz-fx-data-shared-prod.telemetry_derived.cohort_weekly_cfs_staging_v1` cfs
   JOIN
-    weekly_active_clients wac
+    `moz-fx-data-shared-prod.telemetry_derived.cohort_weekly_active_clients_v1` wac
     ON cfs.client_id = wac.client_id
     AND cfs.cohort_date_week <= wac.activity_date_week
   GROUP BY

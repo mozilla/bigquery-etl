a7f0dc66492cf56259660a28aca51ce62f3a566a
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-05-16 16:57:16 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_ios/temp_baseline/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios/temp_baseline/view.sql
@@ -39,6 +39,42 @@ FROM
   `moz-fx-data-shared-prod.org_mozilla_ios_firefox.temp_baseline`
 UNION ALL
 SELECT
+  "org_mozilla_ios_firefoxbeta" AS normalized_app_id,
+  "beta" AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(
+      metrics.timespan.baseline_validation_baseline_duration,
+      metrics.timespan.baseline_validation_startup_duration,
+      metrics.timespan.baseline_validation_visible_duration
+    ) AS `timespan`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch,
+  is_bot_generated
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta.temp_baseline`
+UNION ALL
+SELECT
   "org_mozilla_ios_fennec" AS normalized_app_id,
   "nightly" AS normalized_channel,
   additional_properties,

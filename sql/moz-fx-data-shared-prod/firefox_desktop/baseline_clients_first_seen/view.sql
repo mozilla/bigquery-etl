71a809203cd083260260de6fa992e6167274a757
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-05-20 19:31:32 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_desktop/baseline_clients_first_seen/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/baseline_clients_first_seen/view.sql
@@ -3,7 +3,18 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.baseline_clients_first_seen`
 AS
 SELECT
-  *
+  *,
+  IF(
+    LOWER(IFNULL(isp, '')) <> "browserstack"
+    AND LOWER(IFNULL(COALESCE(distribution_id, distribution.name), '')) <> "mozillaonline",
+    TRUE,
+    FALSE
+  ) AS is_desktop,
+  mozfun.norm.glean_windows_version_info(
+    normalized_os,
+    normalized_os_version,
+    windows_build_number
+  ) AS windows_version
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_daily_v1`
 WHERE

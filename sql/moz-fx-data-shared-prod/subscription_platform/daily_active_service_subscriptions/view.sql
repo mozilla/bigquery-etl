3da2437cf2241a68c4c68704e175bf440053bc41
Sean Rose <srose@mozilla.com>
2025-05-20 16:08:38 -0700

    Delay `daily_active_service_subscriptions_v1` ETL by 7 days.

--- sql/moz-fx-data-shared-prod/subscription_platform/daily_active_service_subscriptions/view.sql
+++ sql/moz-fx-data-shared-prod/subscription_platform/daily_active_service_subscriptions/view.sql
@@ -5,3 +5,15 @@ SELECT
   *
 FROM
   `moz-fx-data-shared-prod.subscription_platform_derived.daily_active_service_subscriptions_v1`
+WHERE
+  `date` < (
+    SELECT
+      COALESCE(MIN(`date`), '9999-12-31')
+    FROM
+      `moz-fx-data-shared-prod.subscription_platform_derived.recent_daily_active_service_subscriptions_v1`
+  )
+UNION ALL
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.subscription_platform_derived.recent_daily_active_service_subscriptions_v1`
--- sql/moz-fx-data-shared-prod/subscription_platform_derived/recent_daily_active_service_subscriptions_v1/query.sql
+++ sql/moz-fx-data-shared-prod/subscription_platform_derived/recent_daily_active_service_subscriptions_v1/query.sql
@@ -3,8 +3,4 @@ SELECT
 FROM
   `moz-fx-data-shared-prod.subscription_platform_derived.daily_active_service_subscriptions_v1_live`
 WHERE
-  {% if is_init() %}
-    `date` <= CURRENT_DATE() - 8
-  {% else %}
-    `date` = @date
-  {% endif %}
+  (`date` BETWEEN (CURRENT_DATE() - 7) AND (CURRENT_DATE() - 1))

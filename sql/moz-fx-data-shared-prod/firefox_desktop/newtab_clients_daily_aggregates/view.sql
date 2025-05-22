93e0df7b89a30ad7cb276c4f9994e3fbe8f5d476
Gaurang Katre <gkatre@gmail.com>
2025-05-22 10:54:52 -0700

    User facing view for newtab

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/newtab_clients_daily_aggregates/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.newtab_clients_daily_aggregates`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.newtab_clients_daily_aggregates_v2`

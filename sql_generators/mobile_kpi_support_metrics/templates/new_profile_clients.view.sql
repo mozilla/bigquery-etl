3cf9553429cdda501dd82d8124a969ccad2f2571
kik-kik <kignasiak@mozilla.com>
2025-05-28 17:35:38 +0200

    feat: add new_profile_clients query to the mobile_kpi_support_metrics generator

--- sql_generators/mobile_kpi_support_metrics/templates/new_profile_clients.view.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/new_profile_clients.view.sql
@@ -3,33 +3,11 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ dataset }}.{{ name }}`
 AS
 SELECT
-  client_id,
-  active_users.submission_date AS first_seen_date,
-  active_users.normalized_channel,
-  app_name,
-  app_display_version AS app_version,
-  country,
-  city,
-  geo_subdivision,
-  locale,
-  isp,
-  normalized_os AS os,
-  normalized_os_version AS os_version,
-  device_model,
-  device_manufacturer,
-  is_mobile,
-  {% for attribution_field in product_attribution_fields %}
-  attribution.{{ attribution_field }},
-  {% endfor %}
-  attribution.paid_vs_organic,
-  device_type,
+  *,
+  {% if 'adjust_network' in product_attribution_fields %}
+    `moz-fx-data-shared-prod.udf.organic_vs_paid_mobile`(adjust_network) AS paid_vs_organic,
+  {% else %}
+    "Organic" AS paid_vs_organic,
+  {% endif %}
 FROM
-  `{{ project_id }}.{{ dataset }}.active_users` AS active_users
-LEFT JOIN
-  `{{ project_id }}.{{ dataset }}.attribution_clients` AS attribution
-  USING(client_id) --temporarily removing normalized_channel until backfill finishes
-WHERE
-  active_users.submission_date < CURRENT_DATE
-  AND is_new_profile
-  AND is_daily_user
-  AND active_users.submission_date = active_users.first_seen_date
+  `{{ project_id }}.{{ dataset }}_derived.{{ name }}_{{ version }}`

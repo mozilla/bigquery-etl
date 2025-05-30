5ae7966d79b17883d23de52ef426fa006cef47f4
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-05-30 18:47:39 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/firefox_desktop/baseline_active_users_aggregates/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/baseline_active_users_aggregates/view.sql
@@ -2,6 +2,11 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.baseline_active_users_aggregates`
 AS
 SELECT
-  *
+  *,
+  `mozfun.norm.glean_windows_version_info`(
+    os_grouped,
+    os_version,
+    windows_build_number
+  ) AS windows_version
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_active_users_aggregates_v2`

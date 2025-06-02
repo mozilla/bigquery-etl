0f93c1be9d928eed82ee0bec31fa2a33b24ec0c6
Mike Williams <mwilliams@mozilla.com>
2025-06-02 13:50:43 -0400

    feat: create glean-only v2 versions of experiment datasets

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_other_events_overall_v2/view.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_other_events_overall_v2`
+AS
+WITH pivot AS (
+  SELECT
+    window_start,
+    experiment,
+    branch,
+    [
+      STRUCT("graduated" AS event, graduate_count AS count),
+      STRUCT("enroll_failed" AS event, enroll_failed_count AS count),
+      STRUCT("unenroll_failed" AS event, unenroll_failed_count AS count),
+      STRUCT("updated" AS event, update_count AS count),
+      STRUCT("update_failed" AS event, update_failed_count AS count),
+      STRUCT("disqualification" AS event, disqualification_count AS count),
+      STRUCT("validation_failed" AS event, validation_failed_count AS count)
+    ] AS events
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.experiment_enrollment_aggregates_live_v2`
+)
+SELECT
+  window_start AS `time`,
+  experiment,
+  branch,
+  e.event AS event,
+  SUM(e.count) AS value
+FROM
+  pivot
+CROSS JOIN
+  UNNEST(events) AS e
+GROUP BY
+  1,
+  2,
+  3,
+  4

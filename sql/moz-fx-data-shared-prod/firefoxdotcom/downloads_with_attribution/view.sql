95ddd7c14558b0fa93d7cea24978a8e53f24bc7f
Marlene-M-Hirose <mhirose@mozilla.com>
2025-06-03 09:38:31 -1000

    add files for firefoxdotcom downloads_with_attribution

---
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/downloads_with_attribution/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefoxdotcom.downloads_with_attribution`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.downloads_with_attribution_v1`

a25dc447d71fe255623e1093e2735323b6ce0e7f
kik-kik <kignasiak@mozilla.com>
2025-06-04 11:54:13 +0200

    feat: revert new_profile_clients view change until the table gets backfilled

--- sql_generators/mobile_kpi_support_metrics/templates/new_profile_clients.view.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/new_profile_clients.view.sql
@@ -1,13 +1,36 @@
 {{ header }}
+-- TODO: keeping this view as is for now until the new table get's backfilled.
 CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ dataset }}.{{ name }}`
 AS
 SELECT
-  *,
-  {% if 'adjust_network' in product_attribution_fields %}
-    `moz-fx-data-shared-prod.udf.organic_vs_paid_mobile`(adjust_network) AS paid_vs_organic,
-  {% else %}
-    "Organic" AS paid_vs_organic,
-  {% endif %}
+  client_id,
+  active_users.submission_date AS first_seen_date,
+  active_users.normalized_channel,
+  app_name,
+  app_display_version AS app_version,
+  country,
+  city,
+  geo_subdivision,
+  locale,
+  isp,
+  normalized_os AS os,
+  normalized_os_version AS os_version,
+  device_model,
+  device_manufacturer,
+  is_mobile,
+  {% for attribution_field in product_attribution_fields %}
+  attribution.{{ attribution_field }},
+  {% endfor %}
+  attribution.paid_vs_organic,
+  device_type,
 FROM
-  `{{ project_id }}.{{ dataset }}_derived.{{ name }}_{{ version }}`
+  `{{ project_id }}.{{ dataset }}.active_users` AS active_users
+LEFT JOIN
+  `{{ project_id }}.{{ dataset }}.attribution_clients` AS attribution
+  USING(client_id) --temporarily removing normalized_channel until backfill finishes
+WHERE
+  active_users.submission_date < CURRENT_DATE
+  AND is_new_profile
+  AND is_daily_user
+  AND active_users.submission_date = active_users.first_seen_date

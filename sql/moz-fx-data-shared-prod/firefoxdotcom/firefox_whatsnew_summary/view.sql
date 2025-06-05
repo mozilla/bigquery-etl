11f94f2b430e5c86539fc09c94aa129e2d84c332
Marlene Hirose <92952117+Marlene-M-Hirose@users.noreply.github.com>
2025-06-05 07:33:31 -0700

    add files for firefox_whatsnew_summary table and view (#7580)
    * add files for firefox_whatsnew_summary table and view
    
    * Update checks.sql
    
    ---------
    
    Co-authored-by: Katie Windau <153020235+kwindau@users.noreply.github.com>

---
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/firefox_whatsnew_summary/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefoxdotcom.firefox_whatsnew_summary`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.firefox_whatsnew_summary_v1`

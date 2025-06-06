c51b0fe2c00e2e3fdaa23e458292fba4deb8bb07
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-06-06 19:32:51 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_ga4_firefoxdotcom.py
+++ dags/bqetl_ga4_firefoxdotcom.py
@@ -304,6 +304,21 @@ with DAG(
         depends_on_past=False,
     )
 
+    firefoxdotcom_derived__www_site_page_metrics__v1 = bigquery_etl_query(
+        task_id="firefoxdotcom_derived__www_site_page_metrics__v1",
+        destination_table="www_site_page_metrics_v1",
+        dataset_id="firefoxdotcom_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="mhirose@mozilla.com",
+        email=[
+            "kwindau@mozilla.com",
+            "mhirose@mozilla.com",
+            "telemetry-alerts@mozilla.com",
+        ],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     firefoxdotcom_derived__wwww_site_metrics_summary__v1 = bigquery_etl_query(
         task_id="firefoxdotcom_derived__wwww_site_metrics_summary__v1",
         destination_table="wwww_site_metrics_summary_v1",
@@ -376,6 +391,10 @@ with DAG(
     )
 
     firefoxdotcom_derived__www_site_landing_page_metrics__v1.set_upstream(
+        firefoxdotcom_derived__www_site_hits__v1
+    )
+
+    firefoxdotcom_derived__www_site_page_metrics__v1.set_upstream(
         firefoxdotcom_derived__www_site_hits__v1
     )
 

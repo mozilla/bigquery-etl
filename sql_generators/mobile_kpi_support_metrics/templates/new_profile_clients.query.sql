1c6c4d573ded2714d57661997d3407071996dc8e
kik-kik <kignasiak@mozilla.com>
2025-06-06 15:04:36 +0200

    feat: update clustering settings, change ordering, and simplify the filtering date logic

--- sql_generators/mobile_kpi_support_metrics/templates/new_profile_clients.query.sql
+++ sql_generators/mobile_kpi_support_metrics/templates/new_profile_clients.query.sql
@@ -1,5 +1,5 @@
 SELECT
-  active_users.submission_date AS first_seen_date,
+  @submission_date AS first_seen_date,
   client_id,
   normalized_channel,
   app_name,
@@ -10,13 +10,13 @@ SELECT
   locale,
   normalized_os AS os,
   normalized_os_version AS os_version,
-  device_model,
   device_manufacturer,
+  device_model,
+  device_type,
   is_mobile,
   {% for attribution_field in product_attribution_fields %}
   attribution.{{ attribution_field }},
   {% endfor %}
-  device_type,
 FROM
   `{{ project_id }}.{{ dataset }}.active_users` AS active_users
 LEFT JOIN
@@ -24,6 +24,6 @@ LEFT JOIN
   USING(client_id, normalized_channel)
 WHERE
   active_users.submission_date = @submission_date
+  AND active_users.first_seen_date = @submission_date
   AND is_new_profile
   AND is_daily_user
-  AND active_users.submission_date = active_users.first_seen_date

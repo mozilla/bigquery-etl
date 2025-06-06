5f284af0f98dcb1e867086421972de2ce6d03054
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-06-06 09:23:01 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/org_mozilla_ios_firefoxvpn_network_extension_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_firefoxvpn_network_extension_derived/event_monitoring_live_v1/materialized_view.sql
@@ -187,7 +187,7 @@ SELECT
 FROM
   combined
 WHERE
-  DATE(submission_timestamp) >= "2025-06-05"
+  DATE(submission_timestamp) >= "2025-06-06"
 GROUP BY
   submission_date,
   window_start,
--- sql/moz-fx-data-shared-prod/thunderbird_desktop_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/thunderbird_desktop_derived/event_monitoring_live_v1/materialized_view.sql
@@ -59,7 +59,7 @@ SELECT
 FROM
   combined
 WHERE
-  DATE(submission_timestamp) >= "2025-06-05"
+  DATE(submission_timestamp) >= "2025-06-06"
 GROUP BY
   submission_date,
   window_start,

b1c3e9efd6b6715d40252d084bd5f82646fa6f2c
Katie Windau <kwindau@mozilla.com>
2025-06-12 13:58:21 -0500

    Add lifecycle_stage to OKR client level views

--- sql/moz-fx-data-shared-prod/firefox_desktop/desktop_engagement_clients/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/desktop_engagement_clients/view.sql
@@ -2,6 +2,17 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefox_desktop.desktop_engagement_clients`
 AS
 SELECT
-  *
+  *,
+  CASE
+    WHEN first_seen_date = submission_date
+      THEN 'new_profile'
+    WHEN DATE_DIFF(submission_date, first_seen_date, DAY)
+      BETWEEN 1
+      AND 27
+      THEN 'repeat_user'
+    WHEN DATE_DIFF(submission_date, first_seen_date, DAY) >= 28
+      THEN 'existing_user'
+    ELSE 'Unknown'
+  END AS lifecycle_stage
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_derived.desktop_engagement_clients_v1`

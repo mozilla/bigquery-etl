ae4204f5ccf5664bd3213515adaaab69203332c7
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-06-13 16:37:34 +0000

    Auto-push due to change on main branch [ci skip]

--- dags/bqetl_content_ml_daily.py
+++ dags/bqetl_content_ml_daily.py
@@ -53,6 +53,17 @@ with DAG(
     catchup=False,
 ) as dag:
 
+    snowflake_migration_derived__corpus_item_schedules_updated__v1 = bigquery_etl_query(
+        task_id="snowflake_migration_derived__corpus_item_schedules_updated__v1",
+        destination_table="corpus_item_schedules_updated_v1",
+        dataset_id="snowflake_migration_derived",
+        project_id="moz-fx-data-shared-prod",
+        owner="skamath@mozilla.com",
+        email=["rrando@mozilla.com", "skamath@mozilla.com"],
+        date_partition_parameter="submission_date",
+        depends_on_past=False,
+    )
+
     snowflake_migration_derived__corpus_items_updated__v1 = bigquery_etl_query(
         task_id="snowflake_migration_derived__corpus_items_updated__v1",
         destination_table="corpus_items_updated_v1",
@@ -129,6 +140,14 @@ with DAG(
         email=["rrando@mozilla.com", "skamath@mozilla.com"],
         date_partition_parameter="submission_date",
         depends_on_past=False,
+    )
+
+    snowflake_migration_derived__corpus_item_schedules_updated__v1.set_upstream(
+        snowflake_migration_derived__corpus_items_updated__v1
+    )
+
+    snowflake_migration_derived__corpus_item_schedules_updated__v1.set_upstream(
+        snowflake_migration_derived__stg_scheduled_corpus_items__v1
     )
 
     snowflake_migration_derived__corpus_items_updated__v1.set_upstream(

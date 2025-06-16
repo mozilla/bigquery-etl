b5304e402225a78b7456410b2ba77ab813ae6e7f
Luc Lisi <llisi@mozilla.com>
2025-06-16 12:03:42 -0700

    format

--- sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_dma_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_dma_aggregates_v1/query.sql
@@ -6,17 +6,15 @@ SELECT
   COUNT(sis.request_id) AS impressions,
   COUNTIF(sis.is_clicked) AS clicks,
 FROM
-  `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v3` as sis
+  `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v3` AS sis
 LEFT JOIN
-  `moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3` as mls
-ON
-  (sis.request_id = mls.request_id)
+  `moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3` AS mls
+  ON (sis.request_id = mls.request_id)
 WHERE
   DATE(sis.submission_timestamp) = @sis.submission_date
   AND LENGTH(sis.query) > 0
   AND sis.normalized_channel = 'release'
   AND DATE(sis.submission_timestamp) = @sis.submission_date
-
 GROUP BY
   sis.query,
   mls.dma,

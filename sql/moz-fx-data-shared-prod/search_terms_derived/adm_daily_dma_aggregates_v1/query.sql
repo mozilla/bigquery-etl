f808dd8a28e0ae573f28ea62a0b79b7581e337e6
Luc Lisi <llisi@mozilla.com>
2025-06-16 13:19:52 -0700

    query error w/ @submission_date

--- sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_dma_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_dma_aggregates_v1/query.sql
@@ -11,10 +11,10 @@ LEFT JOIN
   `moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3` AS mls
   ON (sis.request_id = mls.request_id)
 WHERE
-  DATE(sis.submission_timestamp) = @sis.submission_date
+  DATE(sis.submission_timestamp) = @submission_date
   AND LENGTH(sis.query) > 0
   AND sis.normalized_channel = 'release'
-  AND DATE(sis.submission_timestamp) = @sis.submission_date
+  AND DATE(sis.submission_timestamp) = @submission_date
 GROUP BY
   sis.query,
   mls.dma,

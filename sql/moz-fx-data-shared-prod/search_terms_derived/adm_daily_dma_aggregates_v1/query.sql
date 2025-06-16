8e7614e40ba4e6eec7b6d7475e864eb3f40bd7d9
Luc Lisi <llisi@mozilla.com>
2025-06-16 10:58:15 -0700

    Creating adm_daily_dma_aggregates_v1 with tests

---
+++ sql/moz-fx-data-shared-prod/search_terms_derived/adm_daily_dma_aggregates_v1/query.sql
@@ -0,0 +1,23 @@
+SELECT
+  @submission_date AS submission_date,
+  mls.dma,
+  sis.query,
+  sis.block_id,
+  COUNT(sis.request_id) AS impressions,
+  COUNTIF(sis.is_clicked) AS clicks,
+FROM
+  `moz-fx-data-shared-prod.search_terms_derived.suggest_impression_sanitized_v3` as sis
+LEFT JOIN
+  `moz-fx-data-shared-prod.search_terms_derived.merino_log_sanitized_v3` as mls
+ON
+  (sis.request_id == mls.request_id)
+WHERE
+  DATE(sis.submission_timestamp) = @sis.submission_date
+  AND LENGTH(sis.query) > 0
+  AND sis.normalized_channel = 'release'
+  AND DATE(sis.submission_timestamp) = @sis.submission_date
+
+GROUP BY
+  sis.query,
+  mls.dma,
+  sis.block_id

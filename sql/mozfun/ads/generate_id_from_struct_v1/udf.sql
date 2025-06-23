e9734deb721bd41abc23fe83e99f2a89c66353a7
Lucia <alvargasa@gmail.com>
2025-06-23 16:20:21 +0200

    UDF to generate id from a combination of fields as STRUCT.

--- sql/mozfun/ads/generate_id_from_struct_v1/udf.sql
+++ sql/mozfun/ads/generate_id_from_struct_v1/udf.sql
@@ -1,25 +1,34 @@
 -- Definition for ads.generate_int_id_from_struct
 -- For more information on writing UDFs see:
 -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
-CREATE OR REPLACE FUNCTION analysis.generate_id_from_struct_v1
-(
+CREATE OR REPLACE FUNCTION analysis.generate_id_from_struct_v1(
   key STRUCT<
-    adm_date DATE, product STRING, surface STRING, country_code STRING, advertiser STRING, position INT64, pricing_model STRING
+    adm_date DATE,
+    product STRING,
+    surface STRING,
+    country_code STRING,
+    advertiser STRING,
+    position INT64,
+    pricing_model STRING
   >
-) RETURNS STRUCT<id INT64, version STRING> AS (
-       STRUCT (
-       FARM_FINGERPRINT( TO_JSON_STRING(
-          STRUCT(
-            key.adm_date AS adm_date,
-            IFNULL(LOWER(TRIM(key.product)), "") AS product,
-            IFNULL(LOWER(TRIM(key.surface)), "") AS surface,
-            IFNULL(LOWER(TRIM(key.country_code)), "") AS country_code,
-            IFNULL(LOWER(TRIM(key.advertiser)), "") AS advertiser,
-            key.position AS position,
-            IFNULL(LOWER(TRIM(key.pricing_model)), "") AS pricing_model
-       ) ) ) AS id,
-       'v1' AS version
-    )
+)
+RETURNS STRUCT<id INT64, version STRING> AS (
+  STRUCT(
+    FARM_FINGERPRINT(
+      TO_JSON_STRING(
+        STRUCT(
+          key.adm_date AS adm_date,
+          IFNULL(LOWER(TRIM(key.product)), "") AS product,
+          IFNULL(LOWER(TRIM(key.surface)), "") AS surface,
+          IFNULL(LOWER(TRIM(key.country_code)), "") AS country_code,
+          IFNULL(LOWER(TRIM(key.advertiser)), "") AS advertiser,
+          key.position AS position,
+          IFNULL(LOWER(TRIM(key.pricing_model)), "") AS pricing_model
+        )
+      )
+    ) AS id,
+    'v1' AS version
+  )
 );
 
 -- Tests
@@ -27,7 +36,15 @@ WITH test_cases AS (
   -- Case 1: Baseline.
   SELECT
     "Base case" AS test_name,
-    STRUCT(DATE "2026-01-01", "Sponsored Tile", "Mobile", "AT", "Allegro", 1, "impressions") AS input,
+    STRUCT(
+      DATE "2026-01-01",
+      "Sponsored Tile",
+      "Mobile",
+      "AT",
+      "Allegro",
+      1,
+      "impressions"
+    ) AS input,
     -1178731585035914872 AS expected_id
   UNION ALL
   -- Case 2: Different values.
@@ -57,26 +74,38 @@ WITH test_cases AS (
   -- Case 1: Different Order without field name
   SELECT
     "Base case with fields product and surface in switched order",
-    STRUCT(DATE "2026-01-01", "Mobile", "Sponsored Tile","AT", "Allegro", 1, "impressions"),
+    STRUCT(DATE "2026-01-01", "Mobile", "Sponsored Tile", "AT", "Allegro", 1, "impressions"),
     -7757866412077111074
   UNION ALL
   -- Case 1: Different Order with field name
   SELECT
     "Base case with fields product and surface in switched order",
-    STRUCT(DATE "2026-01-01" AS adm_date, "Mobile" AS surface, "Sponsored Tile" AS product, "AT" AS country_code, "Allegro" AS advertiser, 1 AS position, "impressions" AS pricing_model),
+    STRUCT(
+      DATE "2026-01-01" AS adm_date,
+      "Mobile" AS surface,
+      "Sponsored Tile" AS product,
+      "AT" AS country_code,
+      "Allegro" AS advertiser,
+      1 AS position,
+      "impressions" AS pricing_model
+    ),
     -7757866412077111074
   UNION ALL
   -- Case 6: NULL in all fields.
   SELECT
     "All fields are NULL.",
-    STRUCT(CAST(NULL AS DATE), CAST(NULL AS STRING), CAST(NULL AS STRING), CAST(NULL AS STRING), CAST(NULL AS STRING), CAST(NULL AS INT64), CAST(NULL AS STRING)),
+    STRUCT(
+      CAST(NULL AS DATE),
+      CAST(NULL AS STRING),
+      CAST(NULL AS STRING),
+      CAST(NULL AS STRING),
+      CAST(NULL AS STRING),
+      CAST(NULL AS INT64),
+      CAST(NULL AS STRING)
+    ),
     -604032237644342624 AS expected
 )
 SELECT
-  `mozfun`.assert.equals(
-    (
-      SELECT analysis.generate_id_from_struct_v1(input).id
-    ),
-      expected_id
-    )
-FROM test_cases;
+  `mozfun`.assert.equals((SELECT analysis.generate_id_from_struct_v1(input).id), expected_id)
+FROM
+  test_cases;

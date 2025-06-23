f08728755cd58d04778a0abbede8f98fb1054777
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-06-23 18:19:28 +0000

    Auto-push due to change on main branch [ci skip]

--- sql/moz-fx-data-shared-prod/fenix/pageload/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/pageload/view.sql
@@ -137,3 +137,37 @@ SELECT
   is_bot_generated
 FROM
   `moz-fx-data-shared-prod.org_mozilla_fenix_nightly.pageload`
+UNION ALL
+SELECT
+  "org_mozilla_fennec_aurora" AS normalized_app_id,
+  mozfun.norm.fenix_app_info(
+    "org_mozilla_fennec_aurora",
+    client_info.app_build
+  ).channel AS normalized_channel,
+  additional_properties,
+  client_info,
+  document_id,
+  events,
+  metadata,
+  STRUCT(
+    STRUCT(
+      metrics.labeled_counter.glean_error_invalid_label,
+      metrics.labeled_counter.glean_error_invalid_overflow,
+      metrics.labeled_counter.glean_error_invalid_state,
+      metrics.labeled_counter.glean_error_invalid_value
+    ) AS `labeled_counter`,
+    STRUCT(metrics.string.glean_client_annotation_experimentation_id) AS `string`
+  ) AS `metrics`,
+  normalized_app_name,
+  normalized_country_code,
+  normalized_os,
+  normalized_os_version,
+  ping_info,
+  sample_id,
+  submission_timestamp,
+  app_version_major,
+  app_version_minor,
+  app_version_patch,
+  is_bot_generated
+FROM
+  `moz-fx-data-shared-prod.org_mozilla_fennec_aurora.pageload`

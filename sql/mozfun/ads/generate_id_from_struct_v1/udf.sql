bdaf2e8602d68743022a1d4cd35038dfe9224e65
Lucia <alvargasa@gmail.com>
2025-06-30 11:03:42 +0200

    Remove pricing model from id.

--- sql/mozfun/ads/generate_id_from_struct_v1/udf.sql
+++ sql/mozfun/ads/generate_id_from_struct_v1/udf.sql
@@ -1,15 +1,14 @@
 -- Definition for ads.generate_id_from_struct_v1
 -- For more information on writing UDFs see:
 -- https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html
-CREATE OR REPLACE FUNCTION ads.generate_id_from_struct_v1(
+CREATE OR REPLACE FUNCTION `moz-fx-dev-lvargas-dev.ads.generate_id_from_struct_v1`(
   key STRUCT<
     adm_date DATE,
     product STRING,
     surface STRING,
     country_code STRING,
     advertiser STRING,
-    position INT64,
-    pricing_model STRING
+    position INT64
   >
 )
 RETURNS STRUCT<id INT64, version STRING> AS (
@@ -22,8 +21,7 @@ RETURNS STRUCT<id INT64, version STRING> AS (
           IFNULL(LOWER(TRIM(key.surface)), "") AS surface,
           IFNULL(LOWER(TRIM(key.country_code)), "") AS country_code,
           IFNULL(LOWER(TRIM(key.advertiser)), "") AS advertiser,
-          key.position AS position,
-          IFNULL(LOWER(TRIM(key.pricing_model)), "") AS pricing_model
+          key.position AS position
         )
       )
     ) AS id,
@@ -36,38 +34,38 @@ WITH test_cases AS (
   -- Case 1: Baseline.
   SELECT
     "Base case" AS test_name,
-    STRUCT(DATE "2026-01-01", "Sponsored Tile", "Mobile", "AT", "Abcd", 1, "impressions") AS input,
-    288265945521826584 AS expected_id
+    STRUCT(DATE "2026-01-01", "Sponsored Tile", "Mobile", "AT", "Abcd", 1) AS input,
+    3164869416579476752 AS expected_id
   UNION ALL
   -- Case 2: Different values.
   SELECT
     "Change surface",
-    STRUCT(DATE "2026-01-01", "Sponsored Tile", "M", "AT", "Abcd", 1, "impressions"),
-    1334875948949342950
+    STRUCT(DATE "2026-01-01", "Sponsored Tile", "M", "AT", "Abcd", 1),
+    8870727342259587816
   UNION ALL
   -- Case 3: NULLs.
   SELECT
     "Set pricing model to NULL",
-    STRUCT(DATE "2026-01-01", "Sponsored Tile", "Mobile", "AT", "Abcd", 1, NULL),
-    2091622292415965945
+    STRUCT(DATE "2026-01-01", "Sponsored Tile", NULL, "AT", "Abcd", 1),
+    -7902997304503704096
   UNION ALL
   -- Case 4: Leading or trailing whitespaces.
   SELECT
     "Multiple white spaces in product.",
-    STRUCT(DATE "2026-01-01", "   Sponsored Tile ", "Mobile", "AT", "Abcd", 1, 'impressions'),
-    288265945521826584
+    STRUCT(DATE "2026-01-01", "   Sponsored Tile ", "Mobile", "AT", "Abcd", 1),
+    3164869416579476752
   UNION ALL
   -- Case 1: Deterministic.
   SELECT
     "Base case again shold return the same id.",
-    STRUCT(DATE "2026-01-01", "Sponsored Tile", "Mobile", "AT", "Abcd", 1, "impressions"),
-    288265945521826584
+    STRUCT(DATE "2026-01-01", "Sponsored Tile", "Mobile", "AT", "Abcd", 1),
+    3164869416579476752
   UNION ALL
   -- Case 1: Different Order without field name
   SELECT
     "Base case with fields product and surface in switched order",
-    STRUCT(DATE "2026-01-01", "Mobile", "Sponsored Tile", "AT", "Abcd", 1, "impressions"),
-    6374729218279076125
+    STRUCT(DATE "2026-01-01", "Mobile", "Sponsored Tile", "AT", "Abcd", 1),
+    -4679453810365301680
   UNION ALL
   -- Case 1: Different Order with field name
   SELECT
@@ -78,10 +76,9 @@ WITH test_cases AS (
       "Sponsored Tile" AS product,
       "AT" AS country_code,
       "Abcd" AS advertiser,
-      1 AS position,
-      "impressions" AS pricing_model
+      1 AS position
     ),
-    6374729218279076125
+    -4679453810365301680
   UNION ALL
   -- Case 6: NULL in all fields.
   SELECT
@@ -92,10 +89,9 @@ WITH test_cases AS (
       CAST(NULL AS STRING),
       CAST(NULL AS STRING),
       CAST(NULL AS STRING),
-      CAST(NULL AS INT64),
-      CAST(NULL AS STRING)
+      CAST(NULL AS INT64)
     ),
-    -604032237644342624 AS expected
+    506532424075948991
 )
 SELECT
   `mozfun`.assert.equals((SELECT ads.generate_id_from_struct_v1(input).id), expected_id)

78d810533dc6c658a8ab7127ad2474bb1fb7d623
Chelsey Beck <cbeck@mozilla.com>
2025-07-02 15:48:31 -0700

    adding temp table to sync add-ons developers for email send

---
+++ sql/moz-fx-data-shared-prod/braze_external/add_ons_devs_users_sync_v1/query.sql
@@ -0,0 +1,18 @@
+-- -- Construct the JSON payload in Braze required format
+-- SELECT
+--   CURRENT_TIMESTAMP() AS UPDATED_AT,
+--   external_id AS EXTERNAL_ID,
+--   TO_JSON(
+--     STRUCT(
+--       email AS email,
+--       email_subscribe AS email_subscribe,
+--       ARRAY_AGG(
+--         STRUCT(
+--           subscriptions_array.firefox_subscription_id AS subscription_group_id,
+--           subscriptions_array.subscription_state AS subscription_state
+--         )
+--         )
+--       ) AS subscription_groups
+--     )
+--   ) AS PAYLOAD
+select * from `moz-fx-data-shared-prod.braze_derived.users_v1`

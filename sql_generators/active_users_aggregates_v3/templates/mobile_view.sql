ba80395ac548ac2368077b13ec042841b40798ae
kik-kik <kignasiak@mozilla.com>
2025-07-08 12:21:38 +0200

    feat: update mobile_view templates inside active_users_aggregates generators so that focus products use composite dataset

--- sql_generators/active_users_aggregates_v3/templates/mobile_view.sql
+++ sql_generators/active_users_aggregates_v3/templates/mobile_view.sql
@@ -13,24 +13,42 @@ CREATE OR REPLACE VIEW
       UNION ALL
     {% endif %}
     SELECT
+    -- As per DENG-8914, we want to use composite tables for focus products
+    -- these fields do not exist in the composite view.
+      {% if app_dataset_id in [
+        focus_ios_dataset,
+        focus_android_dataset
+      ] %}
+      activity_segment AS segment,
+      CAST(NULL AS STRING) AS attribution_medium,
+      CAST(NULL AS STRING) AS attribution_source,
+      CAST(NULL AS BOOLEAN) AS attributed,
+      CAST(NULL AS STRING) AS city,
+      CAST(NULL AS STRING) AS locale,
+      CAST(NULL AS STRING) AS adjust_network,
+      CAST(NULL AS STRING) AS install_source,
+      `mozfun.norm.os`(os) AS os_grouped,
+      {% else %}
       segment,
       attribution_medium,
       attribution_source,
       attributed,
       city,
+      locale,
+      adjust_network,
+      install_source,
+      os_grouped,
+      {% endif %}
       country,
       distribution_id,
       first_seen_year,
       is_default_browser,
-      locale,
       channel,
       os,
       os_version,
       os_version_major,
       os_version_minor,
       submission_date,
-      adjust_network,
-      install_source,
       daily_users,
       weekly_users,
       monthly_users,
@@ -43,7 +61,14 @@ CREATE OR REPLACE VIEW
       app_version_minor,
       app_version_patch_revision,
       app_version_is_major_release,
-      os_grouped,
     FROM
+    -- As per DENG-8914, we want to use composite tables for focus products
+    {% if app_dataset_id in [
+      focus_ios_dataset,
+      focus_android_dataset
+    ] %}
+      `{{ project_id }}.{{ app_dataset_id }}.composite_active_users_aggregates`
+    {% else %}
       `{{ project_id }}.{{ app_dataset_id }}.active_users_aggregates`
+    {% endif %}
   {% endfor %}

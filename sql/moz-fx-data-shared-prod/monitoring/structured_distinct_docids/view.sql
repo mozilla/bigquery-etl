ac19b50e36df00a82a14ee30d613ef0c3f8683b2
Ben Wu <12437227+BenWu@users.noreply.github.com>
2025-07-08 17:19:35 -0400

    [DENG-8918] Delete deprecated monitoring tables (#7749)

--- sql/moz-fx-data-shared-prod/monitoring/structured_distinct_docids/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.monitoring.structured_distinct_docids`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.monitoring_derived.structured_distinct_docids_v1`

706d4a3da45ea785cb2571a5a913214b83332adc
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-07-09 10:45:33 -0500

    feat(DENG-9078): Add columns to firefox_desktop_derived.baseline_clients_first_seen_v1 (#7755)
    * feat(DENG-9078): Add normalized_channel & app_display_version to firefox_desktop_derived.baseline_clients_first_seen_v1
    
    * feat(DENG-9078): Use qualified selects with aliases
    
    * feat(DENG-9078): Use qualified selects
    
    * feat(DENG-9078): Add new columms to glean_baseline_clients_first_seen
    
    * feat(DENG-9078): Add normalized_os_version to firefox_desktop_derived.baseline_clients_first_seen_v1
    
    * feat(DENG-9078): Add ISP to firefox_desktop_derived.baseline_clients_first_seen_v1
    
    * feat(DENG-9078): Fully qualify select statement in baseline_active_users

--- sql/moz-fx-data-shared-prod/firefox_desktop/glean_baseline_clients_first_seen/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/glean_baseline_clients_first_seen/view.sql
@@ -21,6 +21,10 @@ SELECT
   distribution_id,
   windows_build_number,
   locale,
-  normalized_os
+  normalized_os,
+  app_display_version,
+  normalized_channel,
+  normalized_os_version,
+  isp,
 FROM
   `moz-fx-data-shared-prod.firefox_desktop_derived.baseline_clients_first_seen_v1`

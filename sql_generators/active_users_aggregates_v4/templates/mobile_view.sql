52278d84ee19f6ac0cba18a8e68b863d1bebabc8
kik-kik <kignasiak@mozilla.com>
2025-07-09 16:28:49 +0200

    feat: update active_users_aggregates v3 and v4 mobile_view templates to use a union between composite AUA and AUA for focus

--- sql_generators/active_users_aggregates_v4/templates/mobile_view.sql
+++ sql_generators/active_users_aggregates_v4/templates/mobile_view.sql
@@ -13,22 +13,6 @@ CREATE OR REPLACE VIEW
       UNION ALL
     {% endif %}
     SELECT
-    -- As per DENG-8914, we want to use composite tables for focus products
-    -- these fields do not exist in the composite view.
-      {% if app_dataset_id in [
-        focus_ios_dataset,
-        focus_android_dataset
-      ] %}
-      activity_segment AS segment,
-      CAST(NULL AS STRING) AS attribution_medium,
-      CAST(NULL AS STRING) AS attribution_source,
-      CAST(NULL AS BOOLEAN) AS attributed,
-      CAST(NULL AS STRING) AS city,
-      CAST(NULL AS STRING) AS locale,
-      CAST(NULL AS STRING) AS adjust_network,
-      CAST(NULL AS STRING) AS install_source,
-      `mozfun.norm.os`(os) AS os_grouped,
-      {% else %}
       segment,
       attribution_medium,
       attribution_source,
@@ -38,7 +22,6 @@ CREATE OR REPLACE VIEW
       adjust_network,
       install_source,
       os_grouped,
-      {% endif %}
       country,
       distribution_id,
       first_seen_year,
@@ -62,13 +45,51 @@ CREATE OR REPLACE VIEW
       app_version_patch_revision,
       app_version_is_major_release,
     FROM
-    -- As per DENG-8914, we want to use composite tables for focus products
-    {% if app_dataset_id in [
-      focus_ios_dataset,
-      focus_android_dataset
-    ] %}
-      `{{ project_id }}.{{ app_dataset_id }}.composite_active_users_aggregates`
-    {% else %}
       `{{ project_id }}.{{ app_dataset_id }}.active_users_aggregates`
+  -- As per DENG-8914, we want to use composite tables for focus products
+  -- these fields do not exist in the composite view.
+  {% if app_dataset_id in [
+    focus_ios_dataset,
+    focus_android_dataset
+  ] %}
+    WHERE
+      submission_date < "2025-03-27"
+    UNION ALL
+    SELECT
+      activity_segment AS segment,
+      CAST(NULL AS STRING) AS attribution_medium,
+      CAST(NULL AS STRING) AS attribution_source,
+      CAST(NULL AS BOOLEAN) AS attributed,
+      CAST(NULL AS STRING) AS city,
+      CAST(NULL AS STRING) AS locale,
+      CAST(NULL AS STRING) AS adjust_network,
+      CAST(NULL AS STRING) AS install_source,
+      `mozfun.norm.os`(os) AS os_grouped,
+      country,
+      distribution_id,
+      first_seen_year,
+      is_default_browser,
+      channel,
+      os,
+      os_version,
+      os_version_major,
+      os_version_minor,
+      submission_date,
+      daily_users,
+      weekly_users,
+      monthly_users,
+      dau,
+      wau,
+      mau,
+      app_name,
+      app_version,
+      app_version_major,
+      app_version_minor,
+      app_version_patch_revision,
+      app_version_is_major_release,
+    FROM
+      `{{ project_id }}.{{ app_dataset_id }}.composite_active_users_aggregates`
+    WHERE
+      submission_date >= "2025-03-27"
     {% endif %}
   {% endfor %}

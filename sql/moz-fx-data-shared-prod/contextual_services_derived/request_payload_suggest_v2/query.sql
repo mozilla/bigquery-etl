8bb47a9a58fd111a0cc489017fc8994b3114d57b
Curtis Morales <cmorales@mozilla.com>
2025-07-09 13:48:37 -0400

    Update suggest_impression_sanitized_v3 test

--- sql/moz-fx-data-shared-prod/contextual_services_derived/request_payload_suggest_v2/query.sql
+++ sql/moz-fx-data-shared-prod/contextual_services_derived/request_payload_suggest_v2/query.sql
@@ -22,12 +22,12 @@ ping_data AS (
     -- receives geo information from the client rather than from Glean ingestion's
     -- IP geolocation. We no longer send subdivision, only country.
     IF(
-      SAFE_CAST(metadata.user_agent.version AS INT64) >= 140,
+      SAFE_CAST(SPLIT(metadata.user_agent.version, '.')[0] AS INT64) >= 140,
       metrics.string.quick_suggest_country,
       metadata.geo.country
     ) AS country_code,
     IF(
-      SAFE_CAST(metadata.user_agent.version AS INT64) >= 140,
+      SAFE_CAST(SPLIT(metadata.user_agent.version, '.')[0] AS INT64) >= 140,
       CAST(NULL AS STRING),
       metadata.geo.subdivision1
     ) AS region_code,

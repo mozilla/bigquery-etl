ea40ee5afb763db0b054ef07aa45a3df12e32b46
Marlene-M-Hirose <mhirose@mozilla.com>
2025-07-09 15:26:28 -1000

    return name of table using marketing-prd, add view name to the skip dry-run section of bqetl_project.yaml file

--- sql/moz-fx-data-shared-prod/google_play_store/reviews/view.sql
+++ sql/moz-fx-data-shared-prod/google_play_store/reviews/view.sql
@@ -6,5 +6,4 @@ SELECT
   MAX(DATE(_PARTITIONTIME)) OVER () AS _LATEST_DATE,
   DATE(_PARTITIONTIME) AS _DATA_DATE
 FROM
--- The table below is `moz-fx-prod-marketing.google_play_store.p_Reviews_v1`
-  `444337733603.google_play_store.p_Reviews_v1`
+  `moz-fx-data-marketing-prod.google_play_store.p_Review_v1`

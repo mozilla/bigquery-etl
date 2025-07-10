d9986f4a303d09dd4fa55e29273e67ccd26f9677
Marlene-M-Hirose <mhirose@mozilla.com>
2025-07-09 15:20:43 -1000

    add comment describing what we see in bigquery as the table name, switch to the numbers name of the dataset

--- sql/moz-fx-data-shared-prod/google_play_store/reviews/view.sql
+++ sql/moz-fx-data-shared-prod/google_play_store/reviews/view.sql
@@ -6,4 +6,5 @@ SELECT
   MAX(DATE(_PARTITIONTIME)) OVER () AS _LATEST_DATE,
   DATE(_PARTITIONTIME) AS _DATA_DATE
 FROM
-  `moz-fx-data-marketing-prod.google_play_store.p_Reviews_v1`
+-- The table below is `moz-fx-prod-marketing.google_play_store.p_Reviews_v1`
+  `444337733603.google_play_store.p_Review_v1`

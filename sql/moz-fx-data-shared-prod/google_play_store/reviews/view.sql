f7cb662a0b989b553ee6b6bfccddc605a9b5b7e7
Marlene-M-Hirose <mhirose@mozilla.com>
2025-07-09 15:39:52 -1000

    add s in table name

--- sql/moz-fx-data-shared-prod/google_play_store/reviews/view.sql
+++ sql/moz-fx-data-shared-prod/google_play_store/reviews/view.sql
@@ -6,4 +6,4 @@ SELECT
   MAX(DATE(_PARTITIONTIME)) OVER () AS _LATEST_DATE,
   DATE(_PARTITIONTIME) AS _DATA_DATE
 FROM
-  `moz-fx-data-marketing-prod.google_play_store.p_Review_v1`
+  `moz-fx-data-marketing-prod.google_play_store.p_Reviews_v1`

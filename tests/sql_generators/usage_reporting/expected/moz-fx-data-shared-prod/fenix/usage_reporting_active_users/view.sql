94267019558306f996af9905226f58965342b13b
kik-kik <kignasiak@mozilla.com>
2025-07-11 23:35:49 +0200

    feat: remove app_name mapping from usage_reporting_active_users

--- tests/sql_generators/usage_reporting/expected/moz-fx-data-shared-prod/fenix/usage_reporting_active_users/view.sql
+++ tests/sql_generators/usage_reporting/expected/moz-fx-data-shared-prod/fenix/usage_reporting_active_users/view.sql
@@ -11,8 +11,8 @@ SELECT
   EXTRACT(YEAR FROM first_seen.first_seen_date) AS first_seen_year,
   CASE
     WHEN LOWER(distribution_id) = "mozillaonline"
-      THEN CONCAT("Fenix", " ", distribution_id)
-    ELSE "Fenix"
+      THEN CONCAT("fenix", " ", distribution_id)
+    ELSE "fenix"
   END AS app_name,
   -- Activity fields to support metrics built on top of activity
   CASE

3c0b6984e1a41792194cff89aee45fd8fd386fa3
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-07-15 19:06:47 +0000

    Auto-push due to change on main branch [ci skip]
    Created from 55a8189dc8223454df9197fc244cf6cf015ef97e

--- sql/moz-fx-data-shared-prod/telemetry/modules/view.sql
+++
@@ -1,9 +0,0 @@
--- Generated via ./bqetl generate stable_views
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.modules`
-AS
-SELECT
-  * REPLACE (mozfun.norm.metadata(metadata) AS metadata),
-  LOWER(IFNULL(metadata.isp.name, "")) = "browserstack" AS is_bot_generated,
-FROM
-  `moz-fx-data-shared-prod.telemetry_stable.modules_v4`

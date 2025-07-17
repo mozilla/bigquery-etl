6d89e12701bac63e589fd3ac15d2ad1ed1eb4d90
Philip Lee <plee@Philips-MBP.lan>
2025-07-17 04:34:22 -0400

    pull data from workspace service from bigeye api

---
+++ sql/moz-fx-data-shared-prod/bigeye_derived/workspace_service_v1/query.py
@@ -0,0 +1,77 @@
+"""Pull from BigEye API from dashboardService and upload to Big Query."""
+
+import logging
+import os
+from argparse import ArgumentParser
+from typing import Any, Dict
+
+import pandas as pd
+import requests
+from google.cloud import bigquery
+
+TARGET_TABLE = "moz-fx-data-shared-prod.bigeye_derived.workspaces"
+BIGEYE_API_KEY = os.environ["BIGEYE_API_KEY"]
+API_URL = "https://app.bigeye.com/api/v1/workspaces"
+
+
+def make_api_request() -> Dict[str, Any]:
+    """Make API request to Bigeye."""
+    headers = {
+        "accept": "application/json",
+        "authorization": BIGEYE_API_KEY,
+    }
+
+    response = requests.get(API_URL, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def process_response(response_data: Dict[str, Any]) -> pd.DataFrame:
+    """Process API response into a DataFrame."""
+    df = pd.json_normalize(
+        response_data["workspaces"],
+        record_path=["sources"],
+        meta=["id", "name", "isDefault"],
+        sep="_",
+        meta_prefix="workspace_",
+    )
+
+    df["refreshed_at"] = pd.Timestamp.now(tz="UTC")
+
+    return df
+
+
+def get_bigeye_data() -> pd.DataFrame:
+    """Fetch data from Bigeye API and combine into a single DataFrame."""
+    response_data = make_api_request()
+    df = process_response(response_data)
+
+    return df
+
+
+def load_to_bigquery(project_id, df: pd.DataFrame) -> None:
+    """Load DataFrame to BigQuery."""
+    client = bigquery.Client(project_id)
+
+    job = client.load_table_from_dataframe(
+        df,
+        TARGET_TABLE,
+        job_config=bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE"),
+    )
+    job.result()
+    logging.info(f"Successfully loaded data to {TARGET_TABLE}")
+
+
+def main() -> None:
+    """Pull from BigEye API dashboard service endpoint then upload to BigQuery."""
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("--project", default="moz-fx-data-shared-prod")
+    args = parser.parse_args()
+
+    df = get_bigeye_data()
+
+    load_to_bigquery(args.project, df)
+
+
+if __name__ == "__main__":
+    main()

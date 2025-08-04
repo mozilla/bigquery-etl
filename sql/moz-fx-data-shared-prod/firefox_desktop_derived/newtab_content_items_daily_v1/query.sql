78b46fb71cd22bf2a2a039445ec74b9883c08a03
Liam McFall <lmcfall@mozilla.com>
2025-08-04 15:12:40 -0400

    updated model with new fields

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/newtab_content_items_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/newtab_content_items_daily_v1/query.sql
@@ -1,8 +1,96 @@
-WITH events_unnested AS (
+WITH newtab_events_unnested AS (
   SELECT
     DATE(submission_timestamp) AS submission_date,
-    -- mozfun.norm.browser_version_info(client_info.app_display_version).major_version AS app_version,
-    140 AS app_version,  -- Placeholder for app_version, adjust as needed
+    mozfun.norm.browser_version_info(client_info.app_display_version).major_version AS app_version,
+    normalized_channel AS channel,
+    metrics.string.newtab_locale AS locale,
+    normalized_country_code AS country,
+    timestamp AS event_timestamp,
+    category AS event_category,
+    name AS event_name,
+    extra AS event_details,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_stable.newtab_v1`,
+    UNNEST(events)
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND category IN ('pocket')
+    AND name IN ('impression', 'click', 'dismiss')
+    AND mozfun.norm.browser_version_info(
+      client_info.app_display_version
+    ).major_version >= 121 -- the [Pocket team started using Glean](https://github.com/Pocket/dbt-snowflake/pull/459) from this version on. This prevents duplicates for previous releases.
+),
+newtab_flattened_events AS (
+  SELECT
+    submission_date,
+    SAFE_CAST(app_version AS INT64) AS app_version,
+    channel,
+    locale,
+    country,
+    event_category,
+    event_name,
+    mozfun.map.get_key(event_details, 'corpus_item_id') AS corpus_item_id,
+    SAFE_CAST(mozfun.map.get_key(event_details, 'position') AS INT64) AS position,
+    SAFE_CAST(mozfun.map.get_key(event_details, 'is_sponsored') AS BOOLEAN) AS is_sponsored,
+    SAFE_CAST(
+      mozfun.map.get_key(event_details, 'is_section_followed') AS BOOLEAN
+    ) AS is_section_followed,
+    mozfun.map.get_key(event_details, 'matches_selected_topic') AS matches_selected_topic,
+    mozfun.map.get_key(event_details, 'newtab_visit_id') AS newtab_visit_id,
+    SAFE_CAST(mozfun.map.get_key(event_details, 'received_rank') AS INT64) AS received_rank,
+    mozfun.map.get_key(event_details, 'section') AS section,
+    SAFE_CAST(mozfun.map.get_key(event_details, 'section_position') AS INT64) AS section_position,
+    mozfun.map.get_key(event_details, 'topic') AS topic,
+    mozfun.map.get_key(event_details, 'content_redacted') AS content_redacted,
+    NULL AS newtab_content_ping_version
+  FROM
+    newtab_events_unnested
+),
+newtab_daily_agg AS (
+  SELECT
+    submission_date,
+    channel,
+    country,
+    '' AS newtab_content_surface_id,
+    corpus_item_id,
+    position,
+    is_sponsored,
+    is_section_followed,
+    matches_selected_topic,
+    received_rank,
+    section,
+    section_position,
+    topic,
+    content_redacted,
+    newtab_content_ping_version,
+    COUNTIF(event_name = 'impression') AS impression_count,
+    COUNTIF(event_name = 'click') AS click_count,
+    COUNTIF(event_name = 'dismiss') AS dismiss_count
+  FROM
+    newtab_flattened_events
+  WHERE
+    content_redacted = 'FALSE'
+  GROUP BY
+    submission_date,
+    app_version,
+    channel,
+    country,
+  -- newtab_content_surface_id,
+    corpus_item_id,
+    position,
+    is_sponsored,
+    is_section_followed,
+    matches_selected_topic,
+    received_rank,
+    section,
+    section_position,
+    topic,
+    content_redacted,
+    newtab_content_ping_version
+),
+newtab_content_events_unnested AS (
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
     normalized_channel AS channel,
     normalized_country_code AS country,
     metrics.string.newtab_content_surface_id AS newtab_content_surface_id,
@@ -10,6 +98,7 @@ WITH events_unnested AS (
     category AS event_category,
     name AS event_name,
     extra AS event_details,
+    metrics.quantity.newtab_content_ping_version AS newtab_content_ping_version
   FROM
     `moz-fx-data-shared-prod.firefox_desktop.newtab_content`,
     UNNEST(events)
@@ -17,15 +106,11 @@ WITH events_unnested AS (
     DATE(submission_timestamp) = @submission_date
     AND category IN ('newtab_content')
     AND name IN ('impression', 'click', 'dismiss')
-    -- AND mozfun.norm.browser_version_info(
-    --   client_info.app_display_version
-    -- ).major_version >= 140 -- Transitioned to the newtab-content ping in version 140, so we only want to include events after that version for this ping.
 ),
-flattened_events AS (
+newtab_content_flattened_events AS (
   SELECT
     submission_date,
-    -- SAFE_CAST(app_version AS INT64) AS app_version,
-    app_version,  -- Use the placeholder directly
+    NULL AS app_version,
     channel,
     country,
     newtab_content_surface_id,
@@ -35,48 +120,64 @@ flattened_events AS (
     SAFE_CAST(mozfun.map.get_key(event_details, 'position') AS INT64) AS position,
     SAFE_CAST(mozfun.map.get_key(event_details, 'is_sponsored') AS BOOLEAN) AS is_sponsored,
     SAFE_CAST(
-      mozfun.map.get_key(event_details, 'is_secton_followed') AS BOOLEAN
+      mozfun.map.get_key(event_details, 'is_section_followed') AS BOOLEAN
     ) AS is_section_followed,
     mozfun.map.get_key(event_details, 'matches_selected_topic') AS matches_selected_topic,
     SAFE_CAST(mozfun.map.get_key(event_details, 'received_rank') AS INT64) AS received_rank,
     mozfun.map.get_key(event_details, 'section') AS section,
     SAFE_CAST(mozfun.map.get_key(event_details, 'section_position') AS INT64) AS section_position,
     mozfun.map.get_key(event_details, 'topic') AS topic,
+    '' AS content_redacted,
+    newtab_content_ping_version
   FROM
-    events_unnested
+    newtab_content_events_unnested
+),
+newtab_content_daily_agg AS (
+  SELECT
+    submission_date,
+    channel,
+    country,
+    newtab_content_surface_id,
+    corpus_item_id,
+    position,
+    is_sponsored,
+    is_section_followed,
+    matches_selected_topic,
+    received_rank,
+    section,
+    section_position,
+    topic,
+    content_redacted,
+    newtab_content_ping_version,
+    COUNTIF(event_name = 'impression') AS impression_count,
+    COUNTIF(event_name = 'click') AS click_count,
+    COUNTIF(event_name = 'dismiss') AS dismiss_count
+  FROM
+    newtab_content_flattened_events
+  GROUP BY
+    submission_date,
+    app_version,
+    channel,
+    country,
+    newtab_content_surface_id,
+    corpus_item_id,
+    position,
+    is_sponsored,
+    is_section_followed,
+    matches_selected_topic,
+    received_rank,
+    section,
+    section_position,
+    topic,
+    content_redacted,
+    newtab_content_ping_version
 )
 SELECT
-  submission_date,
-  app_version,
-  channel,
-  country,
-  newtab_content_surface_id,
-  corpus_item_id,
-  position,
-  is_sponsored,
-  is_section_followed,
-  matches_selected_topic,
-  received_rank,
-  section,
-  section_position,
-  topic,
-  COUNTIF(event_name = 'impression') AS impression_count,
-  COUNTIF(event_name = 'click') AS click_count,
-  COUNTIF(event_name = 'dismiss') AS dismiss_count
+  *
 FROM
-  flattened_events
-GROUP BY
-  submission_date,
-  app_version,
-  channel,
-  country,
-  newtab_content_surface_id,
-  corpus_item_id,
-  position,
-  is_sponsored,
-  is_section_followed,
-  matches_selected_topic,
-  received_rank,
-  section,
-  section_position,
-  topic
+  newtab_daily_agg
+UNION ALL
+SELECT
+  *
+FROM
+  newtab_content_daily_agg

fd996377cd1d8ffe8b480a20e9bc83502fe07963
kik-kik <kignasiak@mozilla.com>
2025-08-20 13:00:38 +0200

    feat: add comments to the views and remove timezone converversion

--- sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_ios_derived/app_store_funnel_v1/query.sql
@@ -1,3 +1,8 @@
+-- https://developer.apple.com/help/app-store-connect/view-sales-and-trends/download-and-view-reports
+-- "Time zone: Reports are based on Pacific Time (PT). A day includes transactions that happened from 12:00 a.m. to 11:59 p.m. PT.
+-- However, the `date` timestamp field appear to always show midnight meaning if we do timezone conversion
+-- we will end up moving all results 1 day back if we attempt conversion to UTC.
+-- This is why we are not doing timezone converstions here.
 WITH historical_store_data AS (
   WITH impression_data AS (
     SELECT

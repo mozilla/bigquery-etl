78d00c48e8e0df69f883dde3e431ddabb594d3c0
kik-kik <kignasiak@mozilla.com>
2025-08-20 18:46:49 +0200

    feat: add view for enterprise_and_consumer_metric_segment_aggregates dataset

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/enterprise_and_consumer_metric_segment_aggregates/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.enterprise_and_consumer_metric_segment_aggregates`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.enterprise_and_consumer_metric_segment_aggregates_v1`

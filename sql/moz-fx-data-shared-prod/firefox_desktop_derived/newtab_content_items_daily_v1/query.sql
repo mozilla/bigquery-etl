c71e20075107aacf173146497f5ea23251cc5a8d
Liam McFall <lmcfall@mozilla.com>
2025-08-28 12:51:23 -0400

    create combined model and new view to join latest corpus item values

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/newtab_content_items_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/newtab_content_items_daily_v1/query.sql
@@ -1,6 +1,3 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_desktop_derived.newtab_content_items_daily_v1`
-AS
 WITH newtab_events_unnested AS (
   SELECT
     DATE(submission_timestamp) AS submission_date,
@@ -16,7 +13,8 @@ WITH newtab_events_unnested AS (
     `moz-fx-data-shared-prod.firefox_desktop_stable.newtab_v1`,
     UNNEST(events)
   WHERE
-    category IN ('pocket')
+    DATE(submission_timestamp) = @submission_date
+    AND category IN ('pocket')
     AND name IN ('impression', 'click', 'dismiss')
     AND mozfun.norm.browser_version_info(
       client_info.app_display_version
@@ -51,10 +49,11 @@ newtab_flattened_events AS (
 newtab_daily_agg AS (
   SELECT
     submission_date,
+    app_version,
     channel,
     country,
     '' AS newtab_content_surface_id,
-    newtab_flattened_events.corpus_item_id,
+    corpus_item_id,
     position,
     is_sponsored,
     is_section_followed,
@@ -65,20 +64,14 @@ newtab_daily_agg AS (
     topic,
     content_redacted,
     newtab_content_ping_version,
-    corpus_items.title AS title,
-    corpus_items.url AS recommendation_url,
-    corpus_items.authors AS authors,
-    corpus_items.publisher AS publisher,
     COUNTIF(event_name = 'impression') AS impression_count,
     COUNTIF(event_name = 'click') AS click_count,
     COUNTIF(event_name = 'dismiss') AS dismiss_count
   FROM
     newtab_flattened_events
-  LEFT OUTER JOIN
-    `moz-fx-data-shared-prod.snowflake_migration_derived.corpus_items_current_v1` AS corpus_items
-    ON newtab_flattened_events.corpus_item_id = corpus_items.corpus_item_id
   WHERE
-    content_redacted = 'FALSE'
+    -- Only options are 'true' and null, so this filters to just non-redacted events
+    content_redacted != 'true'
   GROUP BY
     submission_date,
     app_version,
@@ -95,11 +88,7 @@ newtab_daily_agg AS (
     section_position,
     topic,
     content_redacted,
-    newtab_content_ping_version,
-    title,
-    recommendation_url,
-    authors,
-    publisher
+    newtab_content_ping_version
 ),
 newtab_content_events_unnested AS (
   SELECT
@@ -116,7 +105,8 @@ newtab_content_events_unnested AS (
     `moz-fx-data-shared-prod.firefox_desktop.newtab_content`,
     UNNEST(events)
   WHERE
-    category IN ('newtab_content')
+    DATE(submission_timestamp) = @submission_date
+    AND category IN ('newtab_content')
     AND name IN ('impression', 'click', 'dismiss')
 ),
 newtab_content_flattened_events AS (
@@ -147,10 +137,11 @@ newtab_content_flattened_events AS (
 newtab_content_daily_agg AS (
   SELECT
     submission_date,
+    app_version,
     channel,
     country,
     newtab_content_surface_id,
-    newtab_content_flattened_events.corpus_item_id,
+    corpus_item_id,
     position,
     is_sponsored,
     is_section_followed,
@@ -161,18 +152,11 @@ newtab_content_daily_agg AS (
     topic,
     content_redacted,
     newtab_content_ping_version,
-    corpus_items.title AS title,
-    corpus_items.url AS recommendation_url,
-    corpus_items.authors AS authors,
-    corpus_items.publisher AS publisher,
     COUNTIF(event_name = 'impression') AS impression_count,
     COUNTIF(event_name = 'click') AS click_count,
     COUNTIF(event_name = 'dismiss') AS dismiss_count
   FROM
     newtab_content_flattened_events
-  LEFT OUTER JOIN
-    `moz-fx-data-shared-prod.snowflake_migration_derived.corpus_items_current_v1` AS corpus_items
-    ON newtab_content_flattened_events.corpus_item_id = corpus_items.corpus_item_id
   GROUP BY
     submission_date,
     app_version,
@@ -189,11 +173,7 @@ newtab_content_daily_agg AS (
     section_position,
     topic,
     content_redacted,
-    newtab_content_ping_version,
-    title,
-    recommendation_url,
-    authors,
-    publisher
+    newtab_content_ping_version
 )
 SELECT
   *

7e3a7ffea707d5ba5d925d7c2a0fce1a4bb6ba71
Marlene Hirose <92952117+Marlene-M-Hirose@users.noreply.github.com>
2025-09-03 13:01:09 -0700

    Update bigquery_etl/shredder/config.py
    Co-authored-by: Ben Wu <12437227+BenWu@users.noreply.github.com>

--- bigquery_etl/shredder/config.py
+++ bigquery_etl/shredder/config.py
@@ -324,20 +324,9 @@ DELETE_TARGETS: DeleteIndex = {
         *FOCUS_ADDITIONAL_DELETIONS,
         *LEGACY_MOBILE_SOURCES,
     ),
-    DeleteTarget(
+    client_id_target(
         table="glean_telemetry_derived.cohort_weekly_active_clients_staging_v1",
-        field=(CLIENT_ID,) * 15,
-    ): (
-        DESKTOP_GLEAN_SRC,
-        DeleteSource(table="focus_android.deletion_request", field=GLEAN_CLIENT_ID),
-        DeleteSource(table="firefox_ios.deletion_request", field=GLEAN_CLIENT_ID),
-        DeleteSource(table="fenix.deletion_request", field=GLEAN_CLIENT_ID),
-        DeleteSource(table="klar_ios.deletion_request", field=GLEAN_CLIENT_ID),
-        DeleteSource(table="focus_ios.deletion_request", field=GLEAN_CLIENT_ID),
-        DeleteSource(table="klar_android.deletion_request", field=GLEAN_CLIENT_ID),
-        *FOCUS_ADDITIONAL_DELETIONS,
-        *LEGACY_MOBILE_SOURCES,
-    ),
+    ): DESKTOP_GLEAN_SRC,
     DeleteTarget(
         table="telemetry_derived.cohort_weekly_active_clients_v1",
         field=(CLIENT_ID,) * 15,

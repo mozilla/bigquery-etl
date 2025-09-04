337a099a31501925611d6da59dcfba26b4f463b7
Claas Augner <caugner@mozilla.com>
2025-09-04 20:17:22 +0200

    chore(mdn): format events_stream_v1

--- sql/moz-fx-data-shared-prod/mdn/events_stream_v1/query.sql
+++ sql/moz-fx-data-shared-prod/mdn/events_stream_v1/query.sql
@@ -1,5 +1,12 @@
-CREATE OR REPLACE VIEW `moz-fx-data-shared-prod.mdn.events_stream_v1` AS
-  (SELECT *
-   FROM `moz-fx-data-shared-prod.mdn_fred.events_stream`
-   UNION ALL SELECT *
-   FROM `moz-fx-data-shared-prod.mdn_yari.events_stream`)
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.mdn.events_stream_v1` AS (
+    SELECT
+      *
+    FROM
+      `moz-fx-data-shared-prod.mdn_fred.events_stream`
+    UNION ALL
+    SELECT
+      *
+    FROM
+      `moz-fx-data-shared-prod.mdn_yari.events_stream`
+  )

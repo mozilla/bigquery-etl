e2bce3e750f0810485b3f026af1295fc7a20f8a7
Katie Windau <kwindau@mozilla.com>
2025-09-28 18:29:03 -0500

    feat(DENG-9765): Initial commit, work in progress

---
+++ sql/moz-fx-data-shared-prod/mozilla_org_derived/blog_performance_v1/query.sql
@@ -0,0 +1,61 @@
+--note to self - still need to add engagement rate & figure out key events list
+WITH staging AS (
+  SELECT
+    event_date,
+    event_name,
+    CAST(
+      (
+        SELECT
+          `value`
+        FROM
+          UNNEST(event_params)
+        WHERE
+          key = 'ga_session_id'
+        LIMIT
+          1
+      ).int_value AS STRING
+    ) AS ga_session_id,
+    user_pseudo_id,
+    (
+      SELECT
+        `value`
+      FROM
+        UNNEST(event_params)
+      WHERE
+        key = 'page_title'
+      LIMIT
+        1
+    ).string_value AS page_title,
+    COUNTIF(event_name = 'page_view') AS page_views
+  FROM
+    `moz-fx-data-marketing-prod.analytics_314399816.events_*`
+  WHERE
+    _TABLE_SUFFIX = FORMAT_DATE('%Y%m%d', @submission_date)
+    AND LOWER(
+      (SELECT `value` FROM UNNEST(event_params) WHERE key = 'page_location' LIMIT 1).string_value
+    ) LIKE '%blog.mozilla.org%'
+  GROUP BY
+    event_date,
+    event_name,
+    ga_session_id,
+    user_pseudo_id,
+    page_title
+)
+SELECT
+  PARSE_DATE('%Y%m%d', event_date) AS event_date,
+  page_title,
+  --placeholder, waiting for list of actual key events from Stephen
+  COUNTIF(
+    event_name IN ('link_click', 'download_click', 'scroll', 'cta_click', 'click', 'search')
+  ) AS nbr_key_events,
+  SUM(page_views) AS page_views,
+  COUNT(DISTINCT(ga_session_id || ' - ' || user_pseudo_id)) AS nbr_sessions,
+  SAFE_DIVIDE(
+    SUM(page_views),
+    COUNT(DISTINCT(ga_session_id || ' - ' || user_pseudo_id))
+  ) AS views_per_session
+FROM
+  staging
+GROUP BY
+  event_date,
+  page_title

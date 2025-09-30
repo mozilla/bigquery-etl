84857ec64b0cb3e5951497dcb82202f0a6b19e6b
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-09-30 19:47:54 +0000

    Auto-push due to change on main branch [ci skip]
    Created from 87e410471c5016eb09803880ec89f9da60b32b0a

--- sql/moz-fx-data-shared-prod/firefox_desktop_derived/metrics_clients_daily_v1/query.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/metrics_clients_daily_v1/query.sql
@@ -5,7 +5,94 @@ SELECT
   "release" AS normalized_channel,
   COUNT(*) AS n_metrics_ping,
   1 AS days_sent_metrics_ping_bits,
-  ANY_VALUE(metrics.uuid.legacy_telemetry_profile_group_id) AS profile_group_id
+  ANY_VALUE(metrics.uuid.legacy_telemetry_profile_group_id) AS profile_group_id,
+  SUM(
+    COALESCE(
+      (SELECT SUM(kv.value) FROM UNNEST(metrics.labeled_counter.browser_search_withads_urlbar) kv),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_urlbar_searchmode) kv
+      ),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_contextmenu) kv
+      ),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_about_home) kv
+      ),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_about_newtab) kv
+      ),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_searchbar) kv
+      ),
+      0
+    ) + COALESCE(
+      (SELECT SUM(kv.value) FROM UNNEST(metrics.labeled_counter.browser_search_withads_system) kv),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_webextension) kv
+      ),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_tabhistory) kv
+      ),
+      0
+    ) + COALESCE(
+      (SELECT SUM(kv.value) FROM UNNEST(metrics.labeled_counter.browser_search_withads_reload) kv),
+      0
+    ) + COALESCE(
+      (SELECT SUM(kv.value) FROM UNNEST(metrics.labeled_counter.browser_search_withads_unknown) kv),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_urlbar_handoff) kv
+      ),
+      0
+    ) + COALESCE(
+      (
+        SELECT
+          SUM(kv.value)
+        FROM
+          UNNEST(metrics.labeled_counter.browser_search_withads_urlbar_persisted) kv
+      ),
+      0
+    )
+  ) AS search_with_ads_count_all
 FROM
   `moz-fx-data-shared-prod.firefox_desktop.metrics` AS m
 WHERE

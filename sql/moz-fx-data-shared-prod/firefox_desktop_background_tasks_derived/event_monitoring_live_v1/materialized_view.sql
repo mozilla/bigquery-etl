fcc8ac47bd746dac8684143447e975a19db6fa86
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-10-03 03:28:04 +0000

    Auto-push due to change on main branch [ci skip]
    Created from cc90750feeaba698428ef7483b2853578b8edfd4

--- sql/moz-fx-data-shared-prod/firefox_desktop_background_tasks_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop_background_tasks_derived/event_monitoring_live_v1/materialized_view.sql
@@ -86,7 +86,7 @@ SELECT
 FROM
   combined
 WHERE
-  DATE(submission_timestamp) >= "2025-10-02"
+  DATE(submission_timestamp) >= "2025-10-03"
 GROUP BY
   submission_date,
   window_start,
--- sql/moz-fx-data-shared-prod/net_thunderbird_android_daily_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/net_thunderbird_android_daily_derived/event_monitoring_live_v1/materialized_view.sql
@@ -53,7 +53,7 @@ SELECT
 FROM
   combined
 WHERE
-  DATE(submission_timestamp) >= "2025-10-02"
+  DATE(submission_timestamp) >= "2025-10-03"
 GROUP BY
   submission_date,
   window_start,

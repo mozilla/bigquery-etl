4164f19f0376fda7c61502cbfee2f8c7037508c5
Anna Scholtz <anna@scholtzan.net>
2025-10-09 12:42:14 -0700

    [DENG-9376] Publish DOH adoption and usage rates

---
+++ sql/moz-fx-data-shared-prod/telemetry_derived/doh_adoption_rate_v1/query.sql
@@ -0,0 +1,262 @@
+WITH unioned AS (
+  /* ---------- doh.evaluate_v2_heuristics ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'doh.evaluate_v2_heuristics' AS metric,
+    "value" AS key,
+    COALESCE(extra.value, '') AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN normalized_country_code
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events` AS e,
+    UNNEST(e.events) AS event,
+    UNNEST(event.extra) AS extra
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND event.category = 'doh'
+    AND event.name = 'evaluate_v2_heuristics'
+    AND extra.key = 'value'
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    normalized_country_code
+  UNION ALL
+  /* ---------- doh.state_disabled ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'doh.state_disabled' AS metric,
+    '' AS key,
+    '' AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN normalized_country_code
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events` AS e,
+    UNNEST(e.events) AS event
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND event.category = 'doh'
+    AND event.name = 'state_disabled'
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    normalized_country_code
+  UNION ALL
+  /* ---------- doh.state_enabled ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'doh.state_enabled' AS metric,
+    '' AS key,
+    '' AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN normalized_country_code
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events` AS e,
+    UNNEST(e.events) AS event
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND event.category = 'doh'
+    AND event.name = 'state_enabled'
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    normalized_country_code
+  UNION ALL
+  /* ---------- doh.state_manually_disabled ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'doh.state_manually_disabled' AS metric,
+    '' AS key,
+    '' AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN normalized_country_code
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events` AS e,
+    UNNEST(e.events) AS event
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND event.category = 'doh'
+    AND event.name = 'state_manually_disabled'
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    normalized_country_code
+  UNION ALL
+  /* ---------- doh.state_policy_disabled ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'doh.state_policy_disabled' AS metric,
+    '' AS key,
+    '' AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN normalized_country_code
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events` AS e,
+    UNNEST(e.events) AS event
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND event.category = 'doh'
+    AND event.name = 'state_policy_disabled'
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    normalized_country_code
+  UNION ALL
+  /* ---------- networking.doh_heuristics_attempts ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'networking.doh_heuristics_attempts' AS metric,
+    'networking.doh_heuristics_attempts' AS key,
+    CAST(metrics.counter.networking_doh_heuristics_attempts AS STRING) AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN metadata.geo.country
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.metrics`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND metrics.counter.networking_doh_heuristics_attempts IS NOT NULL
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    metadata.geo.country
+  UNION ALL
+  /* ---------- networking.doh_heuristics_pass_count ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'networking.doh_heuristics_pass_count' AS metric,
+    'networking.doh_heuristics_attempts' AS key,
+    CAST(metrics.counter.networking_doh_heuristics_pass_count AS STRING) AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN metadata.geo.country
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.metrics`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND metrics.counter.networking_doh_heuristics_pass_count IS NOT NULL
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    metadata.geo.country
+  UNION ALL
+  /* ---------- networking.doh_heuristics_result ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'networking.doh_heuristics_result' AS metric,
+    'networking.doh_heuristics_result' AS key,
+    CAST(metrics.quantity.networking_doh_heuristics_result AS STRING) AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN metadata.geo.country
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.metrics`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND metrics.quantity.networking_doh_heuristics_result IS NOT NULL
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    metadata.geo.country
+  UNION ALL
+  /* ---------- security.doh.settings.provider_choice_value ---------- */
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    'security.doh.settings.provider_choice_value' AS metric,
+    "value" AS key,
+    COALESCE(extra.value, '') AS value,
+    CASE
+      WHEN COUNT(DISTINCT client_info.client_id) >= 5000
+        THEN normalized_country_code
+      ELSE 'OTHER'
+    END AS country_code,
+    COUNT(DISTINCT client_info.client_id) AS client_count
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events` AS e,
+    UNNEST(e.events) AS event,
+    UNNEST(event.extra) AS extra
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND client_info.app_channel = 'release'
+    AND event.category = 'security.doh.settings'
+    AND event.name = 'provider_choice_value'
+    AND extra.key = 'value'
+  GROUP BY
+    submission_date,
+    key,
+    value,
+    normalized_country_code
+)
+SELECT
+  submission_date,
+  metric key,
+  value,
+  country_code,
+  SUM(client_count) AS total_client_count
+FROM
+  unioned
+GROUP BY
+  submission_date,
+  metric,
+  key,
+  value,
+  country_code
+ORDER BY
+  submission_date DESC,
+  metric,
+  country_code,
+  key,
+  value

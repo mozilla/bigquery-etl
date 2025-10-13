da8f56bdc539ffe2356fe3914ef7ea7dacb4a73d
Katie Windau <kwindau@mozilla.com>
2025-10-13 15:57:08 -0500

    feat(DENG-9450): Create firefoxdotcom.glean_ga_desktop_conversions

---
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/glean_ga_desktop_conversions/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefoxdotcom.glean_ga_desktop_conversions`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.glean_ga_desktop_conversions_v1`

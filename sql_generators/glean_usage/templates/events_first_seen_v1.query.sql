993430e33828b2209ed6248359f220cbe7014156
Kiran Bammarito <kbammarito@gmail.com>
2025-10-15 12:15:25 -0400

    remove unnecessary app_name from jinja and re-add is_init block

--- sql_generators/glean_usage/templates/events_first_seen_v1.query.sql
+++ sql_generators/glean_usage/templates/events_first_seen_v1.query.sql
@@ -1,3 +1,52 @@
+{{ header }}
+
+{% raw %}
+{% if is_init() %}
+{% endraw %}
+
+WITH eventsstream AS (
+  SELECT
+    client_id,
+    profile_group_id,
+    sample_id,
+    event_category,
+    event_name,
+    `event`,
+    CAST(NULL AS string) AS criteria,
+    MIN(submission_timestamp) AS first_submission_timestamp,
+    MIN(event_timestamp) AS first_event_timestamp,
+    min_by(event_extra, submission_timestamp) AS event_extra,
+    min_by(app_version_major, submission_timestamp) AS app_version_major,
+    min_by(normalized_channel, submission_timestamp) AS normalized_channel,
+    min_by(normalized_country_code, submission_timestamp) AS normalized_country_code,
+    min_by(normalized_os, submission_timestamp) AS normalized_os,
+    min_by(normalized_os_version, submission_timestamp) AS normalized_os_version,
+  FROM
+    `{{ project_id }}.{{ app_name }}_derived.events_stream_v1`
+  WHERE
+    -- initialize by looking over all of history
+    DATE(submission_timestamp) >= '2023-01-01'
+    AND profile_group_id IS NOT NULL
+    AND event_category NOT IN ('media.playback', 'nimbus_events', 'uptake.remotecontent.result')
+    AND sample_id = @sample_id
+  GROUP BY
+    client_id,
+    profile_group_id,
+    sample_id,
+    event_category,
+    event_name,
+    `event`,
+    criteria
+)
+SELECT
+  *
+FROM
+  eventsstream
+
+{% raw %}
+{% else %}
+{% endraw %}
+
 WITH _current AS (
   SELECT
     @submission_date AS submission_date,
@@ -18,7 +67,7 @@ WITH _current AS (
     min_by(normalized_os, submission_timestamp) AS normalized_os,
     min_by(normalized_os_version, submission_timestamp) AS normalized_os_version,
   FROM
-    `{{ project_id }}.{{ app_name }}_derived.{{ base_table }}`
+    `{{ project_id }}.{{ app_name }}_derived.events_stream_v1`
   WHERE
     DATE(submission_timestamp) = @submission_date
     AND profile_group_id IS NOT NULL
@@ -55,7 +104,7 @@ _previous AS (
     normalized_os,
     normalized_os_version
   FROM
-    `{{ project_id }}.{{ app_name }}_derived.{{ events_first_seen_table }}`
+    `{{ project_id }}.{{ events_first_seen_table }}`
   WHERE
     event_first_seen_date > '2023-01-01'
     AND event_first_seen_date < @submission_date
@@ -80,3 +129,9 @@ SELECT
   *
 FROM
   _joined
+
+from _joined
+
+{% raw %}
+{% endif %}
+{% endraw %}

a6bf55a063f09649857da9a49ec6a84e0d3d7c0a
Kiran Bammarito <kbammarito@gmail.com>
2025-10-15 12:12:31 -0400

    remove unnecessary app_name from jinja

--- sql_generators/glean_usage/templates/events_first_seen.view.sql
+++ sql_generators/glean_usage/templates/events_first_seen.view.sql
@@ -1,9 +1,9 @@
 {{ header }}
 
 CREATE OR REPLACE VIEW
-  `{{ project_id }}.{{ app_name }}.{{ events_first_seen_view }}`
+  `{{ project_id }}.{{ events_first_seen_view }}`
 AS
 SELECT
   *
 FROM
-  `{{ project_id }}.{{ app_name }}_derived.{{ events_first_seen_table }}`
+  `{{ project_id }}.{{ events_first_seen_table }}`

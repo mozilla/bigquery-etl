e6d84c9570dec54a2d5881c8aaba1540c2a60c58
kik-kik <kignasiak@mozilla.com>
2025-10-17 20:24:54 +0200

    feat: drop amo_glean namespaces and add addons derived datasets to addons_derived based on glean data to understand addons usage

---
+++ sql/moz-fx-data-shared-prod/addons_derived/fenix_addons_by_client_v1/query.sql
@@ -0,0 +1,76 @@
+WITH base AS (
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    client_info.client_id,
+    sample_id,
+    normalized_channel,
+    normalized_country_code,
+    client_info.locale,
+    normalized_os,
+    -- merging active_addons and addons_theme into a single addons object
+    ARRAY_CONCAT(
+      -- COALESCE to make sure array concat returns results if active_addons is empty and addons_theme is not
+      COALESCE(JSON_QUERY_ARRAY(metrics.object.addons_active_addons, '$'), []),
+      [
+        JSON_QUERY(metrics.object.addons_theme, '$')
+      ] -- wrapping the json object in array to enable array concat
+    ) AS active_addons,
+    -- Accepts formats: 80.0 80.0.0 80.0.0a1 80.0.0b1
+    IF(
+      REGEXP_CONTAINS(client_info.app_display_version, r'^(\d+\.\d+(\.\d+)?([ab]\d+)?)$'),
+      client_info.app_display_version,
+      NULL
+    ) AS app_version,
+  FROM
+    `moz-fx-data-shared-prod.fenix.addons`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND client_info.client_id IS NOT NULL
+    AND (
+      (
+        metrics.object.addons_active_addons IS NOT NULL
+        AND ARRAY_LENGTH(JSON_QUERY_ARRAY(metrics.object.addons_active_addons, '$')) > 0
+      )
+      OR metrics.object.addons_theme IS NOT NULL
+    )
+),
+per_client AS (
+  SELECT
+    submission_date,
+    client_id,
+    sample_id,
+    normalized_channel,
+    ARRAY_CONCAT_AGG(active_addons) AS active_addons,
+    -- We always want to take the most recent seen version per
+    -- https://bugzilla.mozilla.org/show_bug.cgi?id=1693308
+    ARRAY_AGG(app_version ORDER BY mozfun.norm.truncate_version(app_version, "minor") DESC)[
+      SAFE_OFFSET(0)
+    ] AS app_version,
+    `moz-fx-data-shared-prod.udf.mode_last`(ARRAY_AGG(normalized_country_code)) AS country,
+    `moz-fx-data-shared-prod.udf.mode_last`(ARRAY_AGG(locale)) AS locale,
+    `moz-fx-data-shared-prod.udf.mode_last`(ARRAY_AGG(normalized_os)) AS app_os,
+  FROM
+    base
+  GROUP BY
+    ALL
+)
+SELECT
+  * EXCEPT (active_addons),
+  ARRAY(
+    SELECT AS STRUCT
+      JSON_VALUE(addon, "$.id") AS id,
+      -- Same methodology as for app_version above.
+      ARRAY_AGG(
+        JSON_VALUE(addon, "$.version")
+        ORDER BY
+          mozfun.norm.truncate_version(JSON_VALUE(addon, "$.version"), "minor") DESC
+      )[SAFE_OFFSET(0)] AS `version`,
+    FROM
+      UNNEST(active_addons) AS addon
+    WHERE
+      addon IS NOT NULL
+    GROUP BY
+      id
+  ) AS addons
+FROM
+  per_client
---
+++ sql/moz-fx-data-shared-prod/addons_derived/firefox_desktop_stats_installs_v1/query.sql
@@ -0,0 +1,152 @@
+CREATE TEMP FUNCTION extract_addon_info(addon_info ANY TYPE)
+RETURNS STRUCT<
+  hashed_addon_id STRING,
+  utm_content STRING,
+  utm_campaign STRING,
+  utm_source STRING,
+  utm_medium STRING
+> AS (
+  STRUCT(
+    mozfun.map.get_key(addon_info, "hashed_addon_id") AS hashed_addon_id,
+    mozfun.map.get_key(addon_info, "utm_content") AS utm_content,
+    mozfun.map.get_key(addon_info, "utm_campaign") AS utm_campaign,
+    mozfun.map.get_key(addon_info, "utm_source") AS utm_source,
+    mozfun.map.get_key(addon_info, "utm_medium") AS utm_medium
+  )
+);
+
+WITH install_stats AS (
+  SELECT
+    DATE(submission_timestamp) AS submission_date,
+    client_info.client_id,
+    extract_addon_info(event.extra).*,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop.events`,
+    UNNEST(events) AS `event`
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+    AND event.category = "addons_manager"
+    AND event.name = "install_stats"
+  GROUP BY
+    ALL
+),
+per_source AS (
+  SELECT
+    hashed_addon_id,
+    submission_date,
+    ARRAY_AGG(STRUCT(`key`, `value`) ORDER BY `value` DESC) AS downloads_per_source
+  FROM
+    (
+      SELECT
+        hashed_addon_id,
+        submission_date,
+        utm_source AS `key`,
+        COUNT(DISTINCT client_id) AS `value`
+      FROM
+        install_stats
+      GROUP BY
+        submission_date,
+        hashed_addon_id,
+        `key`
+    )
+  GROUP BY
+    submission_date,
+    hashed_addon_id
+),
+per_content AS (
+  SELECT
+    hashed_addon_id,
+    submission_date,
+    ARRAY_AGG(STRUCT(`key`, `value`) ORDER BY `value` DESC) AS downloads_per_content
+  FROM
+    (
+      SELECT
+        hashed_addon_id,
+        submission_date,
+        utm_content AS `key`,
+        COUNT(DISTINCT client_id) AS `value`
+      FROM
+        install_stats
+      GROUP BY
+        submission_date,
+        hashed_addon_id,
+        `key`
+    )
+  GROUP BY
+    submission_date,
+    hashed_addon_id
+),
+per_medium AS (
+  SELECT
+    hashed_addon_id,
+    submission_date,
+    ARRAY_AGG(STRUCT(`key`, `value`) ORDER BY `value` DESC) AS downloads_per_medium
+  FROM
+    (
+      SELECT
+        hashed_addon_id,
+        submission_date,
+        utm_medium AS `key`,
+        COUNT(DISTINCT client_id) AS `value`
+      FROM
+        install_stats
+      GROUP BY
+        submission_date,
+        hashed_addon_id,
+        `key`
+    )
+  GROUP BY
+    submission_date,
+    hashed_addon_id
+),
+per_campaign AS (
+  SELECT
+    hashed_addon_id,
+    submission_date,
+    ARRAY_AGG(STRUCT(`key`, `value`) ORDER BY `value` DESC) AS downloads_per_campaign
+  FROM
+    (
+      SELECT
+        hashed_addon_id,
+        submission_date,
+        utm_campaign AS `key`,
+        COUNT(DISTINCT client_id) AS `value`
+      FROM
+        install_stats
+      GROUP BY
+        submission_date,
+        hashed_addon_id,
+        `key`
+    )
+  GROUP BY
+    submission_date,
+    hashed_addon_id
+),
+--
+total_downloads AS (
+  SELECT
+    hashed_addon_id,
+    submission_date,
+    COUNT(DISTINCT client_id) AS total_downloads
+  FROM
+    install_stats
+  GROUP BY
+    hashed_addon_id,
+    submission_date
+)
+SELECT
+  *
+FROM
+  total_downloads
+LEFT JOIN
+  per_source
+  USING (submission_date, hashed_addon_id)
+LEFT JOIN
+  per_content
+  USING (submission_date, hashed_addon_id)
+LEFT JOIN
+  per_medium
+  USING (submission_date, hashed_addon_id)
+LEFT JOIN
+  per_campaign
+  USING (submission_date, hashed_addon_id)

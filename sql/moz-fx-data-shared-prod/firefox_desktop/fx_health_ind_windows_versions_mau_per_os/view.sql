1d1d3f70753492cf9d4f52a5bbb8941defb70dd6
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-10-28 12:16:00 -0500

    feat(DENG-9984): Create Glean fx_health_ind_desktop_dau_by_device_type & fx_health_ind_windows_versions_mau_per_os (#8355)
    * feat(DENG-9984): Create fx_health_ind_windows_versions_mau_per_os
    
    * feat(DENG-9984): Create fx_health_ind_desktop_dau_by_device_type

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/fx_health_ind_windows_versions_mau_per_os/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.fx_health_ind_windows_versions_mau_per_os`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.fx_health_ind_windows_versions_mau_per_os_v1`

f3b549f2887d535b1a9dad3ab887aa96a2776d7f
kik-kik <kignasiak@mozilla.com>
2025-10-29 16:22:50 +0100

    feat: add enterprise_metrics_clients_legacy virw

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/enterprise_metrics_clients_legacy/view.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.enterprise_metrics_clients_legacy`
+AS
+SELECT
+  *,
+  CASE
+    WHEN normalized_channel = "release"
+      AND ((policies_count > 1) OR (policies_is_enterprise = TRUE))
+      THEN "enterprise_release"
+    WHEN normalized_channel = "release"
+      AND (
+        (distribution_id IS NOT NULL)
+        OR (policies_count = 0)
+        OR (policies_is_enterprise = FALSE)
+      )
+      THEN "consumer_release"
+    WHEN normalized_channel = "esr"
+      AND ((policies_count > 1) AND (distribution_id IS NULL))
+      THEN "enterprise_esr"
+    WHEN normalized_channel = "esr"
+      AND (
+        (distribution_id IS NOT NULL)
+        OR (policies_count = 0)
+        OR (policies_is_enterprise = FALSE)
+      )
+      THEN "consumer_esr"
+    WHEN normalized_channel = "release"
+      THEN "unknown_release"
+    WHEN normalized_channel = "esr"
+      THEN "unknown_esr"
+    ELSE "unexpected_classification" -- TODO: we should set up an alert for this, but not fail the query.
+  END AS enterprise_classification,
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.enterprise_metrics_clients_legacy_v1`

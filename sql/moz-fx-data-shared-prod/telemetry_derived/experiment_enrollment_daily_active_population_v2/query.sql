dee2a4d24e5aaf4725380541bb5d4290a5181881
Mike Williams <mwilliams@mozilla.com>
2025-10-30 16:33:45 -0400

    feat(DENG-10023): add channel to enrollment daily population tables

--- sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_daily_active_population_v2/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/experiment_enrollment_daily_active_population_v2/query.sql
@@ -2,6 +2,7 @@ SELECT
   CAST(DATE_ADD(submission_date, INTERVAL 1 day) AS timestamp) AS time,
   experiment_id AS experiment,
   branch,
+  normalized_channel,
   SUM(active_clients) AS value
 FROM
   `moz-fx-data-shared-prod.telemetry_derived.experiments_daily_active_clients_v2`

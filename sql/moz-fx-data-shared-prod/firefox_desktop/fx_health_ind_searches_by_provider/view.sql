7d28e45df0b69370a5c6e74ec6bc2848d2314236
Katie Windau <kwindau@mozilla.com>
2025-11-10 14:33:17 -0600

    feat(DENG-9984): Create fx_health_ind_searches_by_provider

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/fx_health_ind_searches_by_provider/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.fx_health_ind_searches_by_provider`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.fx_health_ind_searches_by_provider_v1`

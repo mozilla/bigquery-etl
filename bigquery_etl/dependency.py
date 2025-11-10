91027003224c307171c012ee84382532235ab340
Sean Rose <srose@mozilla.com>
2025-11-10 11:42:19 -0800

    fix(dependency): Implement workaround for tobymao/sqlglot#6298.

--- bigquery_etl/dependency.py
+++ bigquery_etl/dependency.py
@@ -58,7 +58,16 @@ def extract_table_references(sql: str) -> List[str]:
             for expr in statement.find_all(sqlglot.exp.Create)
         }
         tables |= (
-            {_raw_table_name(table) for table in statement.find_all(sqlglot.exp.Table)}
+            {
+                _raw_table_name(table)
+                for table in statement.find_all(sqlglot.exp.Table)
+                # Starting in sqlglot v26.9.0 the identifiers for UDFs created by `CREATE TEMP FUNCTION`
+                # statements get parsed into `Table` expressions (https://github.com/tobymao/sqlglot/pull/4829),
+                # so we need to exclude those (note that `Table` expressions in the bodies of UDFs won't have
+                # the `UserDefinedFunction` expression as their parent).  We reported this as a sqlglot bug
+                # (https://github.com/tobymao/sqlglot/issues/6298) but were told this is expected behavior.
+                if not isinstance(table.parent, sqlglot.exp.UserDefinedFunction)
+            }
             # ignore references created in this query
             - creates
             # ignore CTEs created in this statement

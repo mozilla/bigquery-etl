11d40add4f2549e10404386a0138c1106c03b2cb
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-11-11 14:31:31 +0000

    Auto-push due to change on main branch [ci skip]
    Created from 7c82bae4606eeead1d1b0d695d79cb43bdd41002

--- sql/moz-fx-data-shared-prod/accounts_frontend/events_stream/view.sql
+++ sql/moz-fx-data-shared-prod/accounts_frontend/events_stream/view.sql
@@ -5,12 +5,27 @@ AS
 SELECT
   *,
   STRUCT(
-    STRUCT(LAX_BOOL(event_extra.third_party_links) AS `third_party_links`) AS `boolean`,
     STRUCT(
+      LAX_BOOL(event_extra.cg) AS `cg`,
+      LAX_BOOL(event_extra.cpu_arm) AS `cpu_arm`,
+      LAX_BOOL(event_extra.hyb) AS `hyb`,
+      LAX_BOOL(event_extra.ppa) AS `ppa`,
+      LAX_BOOL(event_extra.prf) AS `prf`,
+      LAX_BOOL(event_extra.rel) AS `rel`,
+      LAX_BOOL(event_extra.supported) AS `supported`,
+      LAX_BOOL(event_extra.third_party_links) AS `third_party_links`,
+      LAX_BOOL(event_extra.uvpa) AS `uvpa`
+    ) AS `boolean`,
+    STRUCT(
+      JSON_VALUE(event_extra.browser_family) AS `browser_family`,
+      JSON_VALUE(event_extra.browser_major) AS `browser_major`,
       JSON_VALUE(event_extra.choice) AS `choice`,
+      JSON_VALUE(event_extra.error_reason) AS `error_reason`,
       JSON_VALUE(event_extra.id) AS `id`,
       JSON_VALUE(event_extra.label) AS `label`,
       JSON_VALUE(event_extra.nimbus_user_id) AS `nimbus_user_id`,
+      JSON_VALUE(event_extra.os_family) AS `os_family`,
+      JSON_VALUE(event_extra.os_major) AS `os_major`,
       JSON_VALUE(event_extra.reason) AS `reason`,
       JSON_VALUE(event_extra.referrer) AS `referrer`,
       JSON_VALUE(event_extra.title) AS `title`,
--- sql/moz-fx-data-shared-prod/org_mozilla_ios_klar_derived/event_monitoring_live_v1/materialized_view.sql
+++ sql/moz-fx-data-shared-prod/org_mozilla_ios_klar_derived/event_monitoring_live_v1/materialized_view.sql
@@ -53,7 +53,7 @@ SELECT
 FROM
   combined
 WHERE
-  DATE(submission_timestamp) >= "2025-11-10"
+  DATE(submission_timestamp) >= "2025-11-11"
 GROUP BY
   submission_date,
   window_start,

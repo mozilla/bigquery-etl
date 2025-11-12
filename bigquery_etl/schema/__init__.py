817c8301fe35b6b57f7e6c7bc88d2b9c8e098577
Anna Scholtz <anna@scholtzan.net>
2025-11-12 10:13:00 -0800

    Revert "[DENG-9396] Skip dry running query when getting table schema (#8416)"
    This reverts commit 915b5429c85c8af5ce4e5c468eafe471042f0365.

--- bigquery_etl/schema/__init__.py
+++ bigquery_etl/schema/__init__.py
@@ -61,11 +61,16 @@ class Schema:
     @classmethod
     def for_table(cls, project, dataset, table, partitioned_by=None, *args, **kwargs):
         """Get the schema for a BigQuery table."""
+        query = f"SELECT * FROM `{project}.{dataset}.{table}`"
+
+        if partitioned_by:
+            query += f" WHERE DATE(`{partitioned_by}`) = DATE('2020-01-01')"
+
         try:
             return cls(
                 dryrun.DryRun(
                     os.path.join(project, dataset, table, "query.sql"),
-                    None,
+                    query,
                     project=project,
                     dataset=dataset,
                     table=table,

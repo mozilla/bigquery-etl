267f45c02dae2f25c6a9037fce07b9a8128c6d41
Katie Windau <153020235+kwindau@users.noreply.github.com>
2025-11-13 14:16:04 -0600

    feat(DENG-10047): Add join to lookup table in firefox_whatsnew_summary view (#8437)

--- sql/moz-fx-data-shared-prod/firefoxdotcom/firefox_whatsnew_summary/view.sql
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/firefox_whatsnew_summary/view.sql
@@ -2,6 +2,22 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefoxdotcom.firefox_whatsnew_summary`
 AS
 SELECT
-  *
+  wns.*,
+  map.english_text
 FROM
-  `moz-fx-data-shared-prod.firefoxdotcom_derived.firefox_whatsnew_summary_v2`
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.firefox_whatsnew_summary_v2` wns
+LEFT JOIN
+  (
+  --this is an extra safeguard, there should only ever be 1 row per UID & locale
+    SELECT
+      data_cta_uid,
+      locale,
+      english_text,
+      ROW_NUMBER() OVER (PARTITION BY data_cta_uid, locale ORDER BY RAND() ASC) AS rnk
+    FROM
+      `moz-fx-data-shared-prod.firefoxdotcom_derived.firefox_data_cta_uid_map_v1`
+    QUALIFY
+      rnk = 1
+  ) map
+  ON wns.cta_click_uid = map.data_cta_uid
+  AND wns.page_location_locale = map.locale

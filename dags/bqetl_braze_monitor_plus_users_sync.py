5d4af5d213df5157801684bcc5be9332ecab2810
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-11-18 19:19:33 +0000

    Auto-push due to change on main branch [ci skip]
    Created from e8ae15a48d259017634ed40ebd6adfe127eb1714

--- dags/bqetl_braze_monitor_plus_users_sync.py
+++ dags/bqetl_braze_monitor_plus_users_sync.py
@@ -1,6 +1,10 @@
 # Generated via https://github.com/mozilla/bigquery-etl/blob/main/bigquery_etl/query_scheduling/generate_airflow_dags.py
 
 from airflow import DAG
+from airflow.providers.google.cloud.sensors.bigquery import (
+    BigQueryTableExistenceSensor,
+    BigQueryTablePartitionExistenceSensor,
+)
 from airflow.sensors.external_task import ExternalTaskMarker
 from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.utils.task_group import TaskGroup
@@ -52,9 +56,41 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    braze_external__monitor_plus_sync__v1 = bigquery_etl_query(
-        task_id="braze_external__monitor_plus_sync__v1",
-        destination_table="monitor_plus_sync_v1",
+    wait_for_braze_monitor_plus_users_v1 = BigQueryTableExistenceSensor(
+        task_id="wait_for_braze_monitor_plus_users_v1",
+        project_id="moz-fx-data-shared-prod",
+        dataset_id="braze_derived",
+        table_id="monitor_plus_users_v1",
+        gcp_conn_id="google_cloud_shared_prod",
+        deferrable=True,
+        poke_interval=datetime.timedelta(minutes=5),
+        timeout=datetime.timedelta(seconds=36000),
+        retries=1,
+        retry_delay=datetime.timedelta(seconds=1800),
+    )
+
+    braze_derived__monitor_plus_users__v1 = GKEPodOperator(
+        task_id="braze_derived__monitor_plus_users__v1",
+        arguments=[
+            "python",
+            "sql/moz-fx-data-shared-prod/braze_derived/monitor_plus_users_v1/query.py",
+        ]
+        + [
+            "--destination-project=moz-fx-data-shared-prod",
+            "--destination-dataset=braze_derived",
+            "--destination-table=monitor_plus_users_v1",
+            "--source-bucket=moz-fx-data-marketing-prod-braze-firefox",
+            "--source-prefix=monitor-sunset",
+            "--source-files=all_monitor_plus,ppp_auto_renewing,ppp_non_renewing",
+        ],
+        image="gcr.io/moz-fx-data-airflow-prod-88e0/bigquery-etl:latest",
+        owner="sherrera@mozilla.com",
+        email=["sherrera@mozilla.com", "telemetry-alerts@mozilla.com"],
+    )
+
+    braze_external__monitor_user_sync__v1 = bigquery_etl_query(
+        task_id="braze_external__monitor_user_sync__v1",
+        destination_table="monitor_user_sync_v1",
         dataset_id="braze_external",
         project_id="moz-fx-data-shared-prod",
         owner="sherrera@mozilla.com",
@@ -66,4 +102,12 @@ with DAG(
         date_partition_parameter=None,
         depends_on_past=False,
         task_concurrency=1,
+    )
+
+    braze_external__monitor_user_sync__v1.set_upstream(
+        braze_derived__monitor_plus_users__v1
+    )
+
+    braze_external__monitor_user_sync__v1.set_upstream(
+        wait_for_braze_monitor_plus_users_v1
     )

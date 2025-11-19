78ec7c5ef20e104a9dd9268ecd6232d45f3e7aa5
Liam McFall <lmcfall@mozilla.com>
2025-11-19 15:05:56 -0500

    feat: create braze<>fxa table to evaluate braze assets

--- sql/moz-fx-data-shared-prod/braze_external/braze_send_open_clicks_combined_v1/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.braze_external.braze_send_open_clicks_combined_v1`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.braze_external_derived.braze_send_open_clicks_combined_v1_v1`
---
+++ sql/moz-fx-data-shared-prod/braze_external/braze_sends_combined_v1/query.sql
@@ -0,0 +1,38 @@
+WITH sends AS (
+  SELECT
+    *
+  FROM
+    `moz-fx-data-shared-prod.braze_external.braze_sends_v1`
+  WHERE
+    DATE(send_time) = DATE(TIMESTAMP_SUB(click_time, INTERVAL 30 DAY))
+),
+opens AS (
+  SELECT
+    *
+  FROM
+    `moz-fx-data-shared-prod.braze_external.braze_opens_v1`
+  WHERE
+    DATE(open_time) = DATE(TIMESTAMP_SUB(click_time, INTERVAL 30 DAY))
+),
+clicks AS (
+  SELECT
+    *
+  FROM
+    `moz-fx-data-shared-prod.braze_external.braze_clicks_v1`
+  WHERE
+    DATE(click_time) = @click_time
+)
+SELECT
+  clicks.*,
+  sends.send_time,
+  sends.message_extras,
+  opens.open_id,
+  opens.machine_open
+FROM
+  clicks
+LEFT JOIN
+  sends
+  ON clicks.dispatch_id = sends.dispatch_id
+LEFT JOIN
+  opens
+  ON clicks.dispatch_id = opens.dispatch_id

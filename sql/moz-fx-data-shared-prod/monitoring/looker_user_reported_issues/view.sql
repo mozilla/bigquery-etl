5b095ae17a1a6f2ea150d0ff9a0486937b4266cc
Anna Scholtz <anna@scholtzan.net>
2025-11-21 14:22:17 -0800

    [DENG-9717] Connect sheet with user reported Looker issues to BQ

---
+++ sql/moz-fx-data-shared-prod/monitoring/looker_user_reported_issues/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.monitoring.looker_user_reported_issues`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.monitoring_derived.looker_user_reported_issues_v1`

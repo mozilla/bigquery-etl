009b1fbcab792abe47ea084131ed7aca43396ce9
Anna Scholtz <anna@scholtzan.net>
2025-11-24 10:30:15 -0800

    Remove view

--- sql/moz-fx-data-shared-prod/monitoring/looker_user_reported_issues/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.monitoring.looker_user_reported_issues`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.monitoring_derived.looker_user_reported_issues_v1`

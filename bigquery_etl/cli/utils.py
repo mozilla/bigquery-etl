f676ed789a8671fa3b7978860cb9220e3228239c
Winnie Chan <10429026+wwyc@users.noreply.github.com>
2025-11-24 16:00:55 -0800

    Update bigquery_etl/cli/utils.py
    Co-authored-by: Ben Wu <12437227+BenWu@users.noreply.github.com>

--- bigquery_etl/cli/utils.py
+++ bigquery_etl/cli/utils.py
@@ -254,6 +254,9 @@ def temp_dataset_option(
     )
 
 
+from functools import cache
+
+@cache
 def get_glean_channel_to_app_name_mapping() -> Dict[str, str]:
     """Return a dict where key is the channel app id and the value is the shared app name.
 

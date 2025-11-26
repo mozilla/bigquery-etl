c0267f5a7419064d16cff39a7e55d968b372f5d1
Chelsey Beck <cbeck@mozilla.com>
2025-11-25 16:01:01 -0800

    moving to monitoring_derived dataset

--- sql/moz-fx-data-shared-prod/monitoring_derived/zyte_rss_load_job_monitoring_v1/bigeye_custom_rules.sql
+++ sql/moz-fx-data-shared-prod/monitoring_derived/zyte_rss_load_job_monitoring_v1/bigeye_custom_rules.sql
@@ -1,5 +1,5 @@
 -- Alert when zyte_cache failures exceed the BigQuery change table limit
--- {'name': 'zyte_cache_failures_critical', 'alert_type': 'count', 'range': {'max': 1400}, 'collections': ['Operational Checks'], 'schedule': 'Default Schedule - 13:00 UTC'}
+-- {'name': 'zyte_cache_failures_critical', 'alert_type': 'count', 'range': {'max': 1400}, 'collections': ['Home Newtab'], 'schedule': 'Default Schedule - 13:00 UTC'}
 SELECT
   zyte_cache_failure AS value
 FROM
@@ -9,7 +9,7 @@ WHERE
   AND zyte_cache_failure > 1400;
 
 -- Alert when rss_feed_items failures exceed the BigQuery change table limit
--- {'name': 'rss_feed_items_failures_critical', 'alert_type': 'count', 'range': {'max': 1400}, 'collections': ['Operational Checks'], 'schedule': 'Default Schedule - 13:00 UTC'}
+-- {'name': 'rss_feed_items_failures_critical', 'alert_type': 'count', 'range': {'max': 1400}, 'collections': ['Home Newtab'], 'schedule': 'Default Schedule - 13:00 UTC'}
 SELECT
   rss_feed_items_failure AS value
 FROM

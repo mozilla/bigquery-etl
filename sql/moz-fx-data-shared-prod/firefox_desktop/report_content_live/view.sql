226620921e50ea4bd4b83df3b9d8773ea85ff3b3
Liam McFall <lmcfall@mozilla.com>
2025-12-02 16:06:36 -0500

    initialize hourly model

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop/report_content_live/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefox_desktop.report_content_live`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefox_desktop_derived.report_content_live_v1`

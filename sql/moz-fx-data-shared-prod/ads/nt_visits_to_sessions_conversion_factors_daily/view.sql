cf0a92c84e0457289ff15fce83bf7da95ed86d5e
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2025-12-03 14:12:05 -0500

    Deprecate nt_visits_to_sessions_conversion_factors_daily (#8048)

--- sql/moz-fx-data-shared-prod/ads/nt_visits_to_sessions_conversion_factors_daily/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.ads.nt_visits_to_sessions_conversion_factors_daily`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.ads_derived.nt_visits_to_sessions_conversion_factors_daily_v1`

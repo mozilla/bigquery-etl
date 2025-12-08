6d55ed3120a32dc01074ff6d478534d1c2c88d3e
Katie Windau <kwindau@mozilla.com>
2025-12-08 09:23:49 -0600

    work in progress

--- bigquery_etl/metadata/add_view_metadata_if_missing.py
+++ bigquery_etl/metadata/add_view_metadata_if_missing.py
@@ -3,6 +3,7 @@
 # Load libraries
 import os
 import re
+import yaml
 
 # Set directory to loop through
 root_directory = '../../sql/moz-fx-data-shared-prod'
@@ -41,13 +42,9 @@ for derived_dir in derived_top_level_directories:
 
     view_folder_fpath = os.path.join(root_directory, view_dir)
 
-    print('view_folder_fpath: ', view_folder_fpath)
     view_folders = get_top_level_dirs(view_folder_fpath)
     
-    print('view folders:')
-    print(view_folders)
     for view_folder in view_folders:
-        print(view_folder)
         # if metadata.yaml doesn't exist in folder, check if derived one has a copy
         view_level_fpath = os.path.join(view_folder_fpath, view_folder)
         print(view_level_fpath)
@@ -56,8 +53,6 @@ for derived_dir in derived_top_level_directories:
             continue
 
         files_in_view_folder = os.listdir(view_level_fpath)
-        print('files_in_view_folder: ')
-        print(files_in_view_folder)
 
         if 'metadata.yaml' not in files_in_view_folder:
             print("Check if derived table has metadata.yaml since view doesn't")
@@ -65,11 +60,69 @@ for derived_dir in derived_top_level_directories:
 
             derived_dir_fpath = os.path.join(root_directory, derived_dir)
             print('derived_dir_fpath: ', derived_dir_fpath)
-            # Calculate the corresponding derived view folder
 
-            # Get it's files
+            # Find matching derived folders with version suffix (e.g., view_folder_v1, view_folder_v2)
+            if os.path.isdir(derived_dir_fpath):
+                all_derived_folders = os.listdir(derived_dir_fpath)
 
-            # Check if it has a metadata.yaml
+                # Find all folders that match the pattern: view_folder_v#
+                matching_folders = []
+                pattern = re.compile(rf'^{re.escape(view_folder)}_v(\d+)$')
+
+                for folder in all_derived_folders:
+                    match = pattern.match(folder)
+                    if match:
+                        version = int(match.group(1))
+                        matching_folders.append((version, folder))
+
+                if matching_folders:
+                    # Sort by version number and get the highest
+                    matching_folders.sort(reverse=True)
+                    highest_version, highest_version_folder = matching_folders[0]
+
+                    derived_view_folder_fpath = os.path.join(derived_dir_fpath, highest_version_folder)
+                    print(f'Checking derived folder (v{highest_version}): {derived_view_folder_fpath}')
+
+                    # Check if the folder is a directory
+                    if os.path.isdir(derived_view_folder_fpath):
+                        # Get its files
+                        files_in_derived_folder = os.listdir(derived_view_folder_fpath)
+                        print(f'files_in_derived_folder: {files_in_derived_folder}')
+
+                        # Check if it has a metadata.yaml
+                        if 'metadata.yaml' in files_in_derived_folder:
+                            print(f'✓ Found metadata.yaml in derived folder: {derived_view_folder_fpath}')
+
+                            # Read the derived metadata.yaml
+                            derived_metadata_path = os.path.join(derived_view_folder_fpath, 'metadata.yaml')
+                            with open(derived_metadata_path, 'r') as f:
+                                derived_metadata = yaml.safe_load(f)
+
+                            # Extract only the specified keys
+                            view_metadata = {}
+                            keys_to_copy = ['friendly_name', 'description', 'owners']
+
+                            for key in keys_to_copy:
+                                if key in derived_metadata:
+                                    view_metadata[key] = derived_metadata[key]
+
+                            # Only create the file if we have at least one key to copy
+                            if view_metadata:
+                                view_metadata_path = os.path.join(view_level_fpath, 'metadata.yaml')
+                                with open(view_metadata_path, 'w') as f:
+                                    yaml.dump(view_metadata, f, default_flow_style=False, sort_keys=False)
+                                print(f'✓ Created metadata.yaml in view folder: {view_metadata_path}')
+                                print(f'  Copied keys: {list(view_metadata.keys())}')
+                            else:
+                                print(f'✗ No relevant keys found in derived metadata to copy')
+                        else:
+                            print(f'✗ No metadata.yaml found in derived folder: {derived_view_folder_fpath}')
+                    else:
+                        print(f'✗ Path is not a directory: {derived_view_folder_fpath}')
+                else:
+                    print(f'✗ No matching derived folder found for pattern: {view_folder}_v#')
+            else:
+                print(f'✗ Derived directory does not exist: {derived_dir_fpath}')
 
     # Get all the directories within this directory
 

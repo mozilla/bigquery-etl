8fa23519a5c74d909c1893c9aaec5038edc1851d
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2025-12-11 19:18:56 +0000

    Auto-push due to change on main branch [ci skip]
    Created from e5b159bfc8a788fa95047890321dc5714d5ac81a

--- sql/moz-fx-data-shared-prod/fenix/addons/view.sql
+++ sql/moz-fx-data-shared-prod/fenix/addons/view.sql
@@ -129,16 +129,110 @@ SELECT
     client_info.app_build
   ).channel AS normalized_channel,
   additional_properties,
-  client_info,
+  STRUCT(
+    client_info.android_sdk_version,
+    client_info.app_build,
+    client_info.app_channel,
+    client_info.app_display_version,
+    client_info.architecture,
+    STRUCT(
+      client_info.attribution.campaign,
+      client_info.attribution.content,
+      client_info.attribution.medium,
+      client_info.attribution.source,
+      client_info.attribution.term,
+      client_info.attribution.ext
+    ) AS `attribution`,
+    client_info.build_date,
+    client_info.client_id,
+    client_info.device_manufacturer,
+    client_info.device_model,
+    STRUCT(client_info.distribution.name, client_info.distribution.ext) AS `distribution`,
+    client_info.first_run_date,
+    client_info.locale,
+    client_info.os,
+    client_info.os_version,
+    client_info.session_count,
+    client_info.session_id,
+    client_info.telemetry_sdk_build,
+    client_info.windows_build_number
+  ) AS `client_info`,
   document_id,
-  events,
-  metadata,
+  ARRAY(
+    SELECT
+      STRUCT(
+        events.category,
+        ARRAY(
+          SELECT
+            STRUCT(extra.key, extra.value)
+          FROM
+            UNNEST(events.extra) AS `extra`
+        ) AS `extra`,
+        events.name,
+        events.timestamp
+      )
+    FROM
+      UNNEST(events) AS `events`
+  ) AS `events`,
   STRUCT(
     STRUCT(
-      metrics.labeled_counter.glean_error_invalid_label,
-      metrics.labeled_counter.glean_error_invalid_overflow,
-      metrics.labeled_counter.glean_error_invalid_state,
-      metrics.labeled_counter.glean_error_invalid_value
+      metadata.geo.city,
+      metadata.geo.country,
+      metadata.geo.db_version,
+      metadata.geo.subdivision1,
+      metadata.geo.subdivision2
+    ) AS `geo`,
+    STRUCT(
+      metadata.header.date,
+      metadata.header.dnt,
+      metadata.header.x_debug_id,
+      metadata.header.x_foxsec_ip_reputation,
+      metadata.header.x_lb_tags,
+      metadata.header.x_pingsender_version,
+      metadata.header.x_source_tags,
+      metadata.header.x_telemetry_agent,
+      metadata.header.parsed_date,
+      metadata.header.parsed_x_source_tags,
+      STRUCT(
+        metadata.header.parsed_x_lb_tags.tls_version,
+        metadata.header.parsed_x_lb_tags.tls_cipher_hex
+      ) AS `parsed_x_lb_tags`
+    ) AS `header`,
+    STRUCT(metadata.isp.db_version, metadata.isp.name, metadata.isp.organization) AS `isp`,
+    STRUCT(
+      metadata.user_agent.browser,
+      metadata.user_agent.os,
+      metadata.user_agent.version
+    ) AS `user_agent`
+  ) AS `metadata`,
+  STRUCT(
+    STRUCT(
+      ARRAY(
+        SELECT
+          STRUCT(glean_error_invalid_label.key, glean_error_invalid_label.value)
+        FROM
+          UNNEST(metrics.labeled_counter.glean_error_invalid_label) AS `glean_error_invalid_label`
+      ) AS `glean_error_invalid_label`,
+      ARRAY(
+        SELECT
+          STRUCT(glean_error_invalid_overflow.key, glean_error_invalid_overflow.value)
+        FROM
+          UNNEST(
+            metrics.labeled_counter.glean_error_invalid_overflow
+          ) AS `glean_error_invalid_overflow`
+      ) AS `glean_error_invalid_overflow`,
+      ARRAY(
+        SELECT
+          STRUCT(glean_error_invalid_state.key, glean_error_invalid_state.value)
+        FROM
+          UNNEST(metrics.labeled_counter.glean_error_invalid_state) AS `glean_error_invalid_state`
+      ) AS `glean_error_invalid_state`,
+      ARRAY(
+        SELECT
+          STRUCT(glean_error_invalid_value.key, glean_error_invalid_value.value)
+        FROM
+          UNNEST(metrics.labeled_counter.glean_error_invalid_value) AS `glean_error_invalid_value`
+      ) AS `glean_error_invalid_value`
     ) AS `labeled_counter`,
     STRUCT(
       metrics.object.addons_active_addons,
@@ -152,7 +246,27 @@ SELECT
   normalized_country_code,
   normalized_os,
   normalized_os_version,
-  ping_info,
+  STRUCT(
+    ping_info.end_time,
+    ARRAY(
+      SELECT
+        STRUCT(
+          experiments.key,
+          STRUCT(
+            experiments.value.branch,
+            STRUCT(experiments.value.extra.enrollment_id, experiments.value.extra.type) AS `extra`
+          ) AS `value`
+        )
+      FROM
+        UNNEST(ping_info.experiments) AS `experiments`
+    ) AS `experiments`,
+    ping_info.ping_type,
+    ping_info.reason,
+    ping_info.seq,
+    ping_info.start_time,
+    ping_info.parsed_start_time,
+    ping_info.parsed_end_time
+  ) AS `ping_info`,
   sample_id,
   submission_timestamp,
   app_version_major,

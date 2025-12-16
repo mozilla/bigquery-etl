201e8c2e3ff03e5b0910c0466efd46b86a089c46
Lucia <30448600+lucia-vargas-a@users.noreply.github.com>
2025-12-16 16:13:19 -0500

    Allow for subfolders in get_table_dir function (#8614)
    * Allow for subfolders in get_table_dir function.
    
    * isort

--- tests/util/test_common.py
+++ tests/util/test_common.py
@@ -7,6 +7,7 @@ from click.testing import CliRunner
 from bigquery_etl.cli.utils import is_valid_dir
 from bigquery_etl.util.common import (
     extract_last_group_by_from_query,
+    get_table_dir,
     project_dirs,
     qualify_table_references_in_file,
     render,
@@ -513,4 +514,33 @@ class TestUtilCommon:
         assert ["COLUMN"] == extract_last_group_by_from_query(
             sql_text="""WITH cte1 AS (SELECT COLUMN FROM test_table GROUP BY COLUMN),
             cte2 AS (SELECT COLUMN FROM test_table group by COLUMN) SELECT * FROM cte2;"""
+        )
+
+    def test_get_table_dir(self):
+        """Test cases using a path and table."""
+        assert Path("sql/project1/dataset1/table1") == get_table_dir(
+            output_dir=Path("sql/project1"),
+            full_table_id="dataset1.table1",
+        )
+        assert Path("sql/project1/dataset1/table1/subfolder1") == get_table_dir(
+            output_dir=Path("sql/project1"),
+            full_table_id="project1.dataset1.table1.subfolder1",
+            parts=3,
+        )
+        assert Path(
+            "sql/project1/dataset1/table1/subfolder1/subfolder2/subfolder3"
+        ) == get_table_dir(
+            output_dir=Path("sql/project1"),
+            full_table_id="project1.dataset1.table1.subfolder1.subfolder2.subfolder3",
+            parts=5,
+        )
+        # 'parts' is incorrect or missing.
+        assert Path("sql/project1/table1/subfolder1") == get_table_dir(
+            output_dir=Path("sql/project1"),
+            full_table_id="project1.dataset1.table1.subfolder1",
+        )
+        assert Path("sql/project1/project1/dataset1/table1") == get_table_dir(
+            output_dir=Path("sql/project1"),
+            full_table_id="project1.dataset1.table1",
+            parts=3,
         )

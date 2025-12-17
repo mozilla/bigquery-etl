dc45fde3367a0fe6c7f77ac7261276a7ad251bee
Travis Long <tlong@mozilla.com>
2025-12-17 09:19:11 -0600

    Bug 2006093 - Glean Telemetry Health ETL, better normalize application channel for Fenix Nightly (#8629)
    Fenix was reporting clients as "release" from the nightly application id, so this should better normalize the channel using the existing UDF.

---
+++ sql_health_test/moz-fx-data-shared-prod/monitoring/telemetry_health_sequence_holes/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.monitoring.telemetry_health_sequence_holes`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.monitoring_derived.telemetry_health_sequence_holes_v1`

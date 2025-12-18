8ea3c423dbff8ef28399c0acedfaa9cee7f1eae6
Marlene-M-Hirose <mhirose@mozilla.com>
2025-12-17 22:24:41 -1000

    use constants not declare statements

--- sql/moz-fx-data-shared-prod/telemetry_derived/crashing_users_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/crashing_users_aggregates_v1/query.sql
@@ -1,26 +1,18 @@
-DECLARE PROCESS_TYPE_MAIN INT64 DEFAULT 1 << 0;
-
-DECLARE PROCESS_TYPE_CONTENT INT64 DEFAULT 1 << 1;
-
-DECLARE PROCESS_TYPE_GMPLUGIN INT64 DEFAULT 1 << 2;
-
-DECLARE PROCESS_TYPE_GPU INT64 DEFAULT 1 << 3;
-
-DECLARE PROCESS_TYPE_RDD INT64 DEFAULT 1 << 4;
-
-DECLARE PROCESS_TYPE_SOCKET INT64 DEFAULT 1 << 5;
-
-DECLARE PROCESS_TYPE_UTILITY INT64 DEFAULT 1 << 6;
-
-DECLARE CLASSIFY_NONE INT64 DEFAULT 1 << 0;
-
-DECLARE CLASSIFY_OOM INT64 DEFAULT 1 << 1;
-
-DECLARE CLASSIFY_BACKGROUND_TASK INT64 DEFAULT 1 << 2;
-
-DECLARE CLASSIFY_SHUTDOWN_HANG INT64 DEFAULT 1 << 3;
-
-WITH crashes AS (
+WITH constants AS (
+  SELECT
+    1 << 0 AS PROCESS_TYPE_MAIN,
+    1 << 1 AS PROCESS_TYPE_CONTENT,
+    1 << 2 AS PROCESS_TYPE_GMPLUGIN,
+    1 << 3 AS PROCESS_TYPE_GPU,
+    1 << 4 AS PROCESS_TYPE_RDD,
+    1 << 5 AS PROCESS_TYPE_SOCKET,
+    1 << 6 AS PROCESS_TYPE_UTILITY,
+    1 << 0 AS CLASSIFY_NONE,
+    1 << 1 AS CLASSIFY_OOM,
+    1 << 2 AS CLASSIFY_BACKGROUND_TASK,
+    1 << 3 AS CLASSIFY_SHUTDOWN_HANG
+),
+crashes AS (
   SELECT
     client_info.client_id AS client_id,
     DATE(submission_timestamp) AS submission_date,
@@ -31,32 +23,32 @@ WITH crashes AS (
       CASE
         metrics.string.crash_process_type
         WHEN 'main'
-          THEN PROCESS_TYPE_MAIN
+          THEN 1 << 0  -- PROCESS_TYPE_MAIN
         WHEN 'content'
-          THEN PROCESS_TYPE_CONTENT
+          THEN 1 << 1  -- PROCESS_TYPE_CONTENT
         WHEN 'gmplugin'
-          THEN PROCESS_TYPE_GMPLUGIN
+          THEN 1 << 2  -- PROCESS_TYPE_GMPLUGIN
         WHEN 'gpu'
-          THEN PROCESS_TYPE_GPU
+          THEN 1 << 3  -- PROCESS_TYPE_GPU
         WHEN 'rdd'
-          THEN PROCESS_TYPE_RDD
+          THEN 1 << 4  -- PROCESS_TYPE_RDD
         WHEN 'socket'
-          THEN PROCESS_TYPE_SOCKET
+          THEN 1 << 5  -- PROCESS_TYPE_SOCKET
         WHEN 'utility'
-          THEN PROCESS_TYPE_UTILITY
+          THEN 1 << 6  -- PROCESS_TYPE_UTILITY
         ELSE 0
       END
     ) AS process_type_bit,
     (
       CASE
         WHEN metrics.quantity.memory_oom_allocation_size IS NOT NULL
-          THEN CLASSIFY_OOM
+          THEN 1 << 1  -- CLASSIFY_OOM
         WHEN metrics.string.crash_background_task_name IS NOT NULL
-          THEN CLASSIFY_BACKGROUND_TASK
+          THEN 1 << 2  -- CLASSIFY_BACKGROUND_TASK
         WHEN metrics.object.crash_async_shutdown_timeout IS NOT NULL
           OR metrics.object.crash_quota_manager_shutdown_timeout IS NOT NULL
-          THEN CLASSIFY_SHUTDOWN_HANG
-        ELSE CLASSIFY_NONE
+          THEN 1 << 3  -- CLASSIFY_SHUTDOWN_HANG
+        ELSE 1 << 0  -- CLASSIFY_NONE
       END
     ) AS classification_bit,
     (
@@ -87,9 +79,8 @@ client_daily_crashes AS (
     BIT_OR(process_type_bit) AS process_types_bitset,
     major_version,
     BIT_OR(classification_bit) AS classifications_bitset,
-    -- Main crashes without a minidump will be double counted because they are submitted from the crash reporter and firefox, so we distinguish those clients
     no_minidump
-    AND process_type_bit = PROCESS_TYPE_MAIN AS main_no_minidump
+    AND process_type_bit = (1 << 0) AS main_no_minidump  -- PROCESS_TYPE_MAIN
   FROM
     crashes
   GROUP BY
@@ -102,7 +93,6 @@ SELECT
   major_version,
   process_types_bitset,
   classifications_bitset,
-    -- Count main crashes without a minidump separately to halve them.
   COUNT(DISTINCT IF(main_no_minidump, client_id, NULL)) / 2 + COUNT(
     DISTINCT IF(main_no_minidump, NULL, client_id)
   ) AS crashing_users

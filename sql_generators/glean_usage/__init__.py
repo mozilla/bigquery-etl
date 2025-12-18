65f239f2371021e5d7498ad94da5ef0f422b3411
kbammarito <kbammarito@users.noreply.github.com>
2025-12-18 17:27:06 -0500

    DENG-8767: Create events first seen table (#8638)

--- sql_generators/glean_usage/__init__.py
+++ sql_generators/glean_usage/__init__.py
@@ -21,6 +21,7 @@ from sql_generators.glean_usage import (
     event_error_monitoring,
     event_flow_monitoring,
     event_monitoring_live,
+    events_first_seen,
     events_stream,
     events_unnested,
     glean_app_ping_views,
@@ -42,6 +43,7 @@ GLEAN_TABLES = [
     event_monitoring_live.EventMonitoringLive(),
     event_error_monitoring.EventErrorMonitoring(),
     event_flow_monitoring.EventFlowMonitoring(),
+    events_first_seen.EventsFirstSeenTable(),
     events_stream.EventsStreamTable(),
 ]
 

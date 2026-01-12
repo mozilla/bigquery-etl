70855b5b172a4e0664a4f914b71c477f359b0c40
Anna Scholtz <anna@scholtzan.net>
2026-01-12 10:39:36 -0800

    Address review feedback

--- bigquery_etl/metadata/parse_metadata.py
+++ bigquery_etl/metadata/parse_metadata.py
@@ -136,9 +136,9 @@ class SchemaDerivedMetadata:
 class SchemaMetadata:
     """Metadata related to additional schema information."""
 
-    derived_from: List[SchemaDerivedMetadata] = attr.ib([])
+    derived_from: List[SchemaDerivedMetadata] = attr.ib(None)
     # indicates that the schema might change over time and should be updated even with --skip-existing
-    allow_field_addition: bool = attr.ib(False)
+    allow_field_addition: bool = attr.ib(None)
 
 
 @attr.s(auto_attribs=True)

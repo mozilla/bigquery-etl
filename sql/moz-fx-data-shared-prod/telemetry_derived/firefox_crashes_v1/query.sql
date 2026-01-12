612b8df073fb460298a14dc13301b145c148e11a
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2026-01-12 19:44:10 +0000

    Auto-push due to change on main branch [ci skip]
    Created from 9691ee380d136c41baa5fd34cbd69dadc852cc0f

--- sql/moz-fx-data-shared-prod/telemetry_derived/firefox_crashes_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/firefox_crashes_v1/query.sql
@@ -267,7 +267,22 @@ unioned_pings AS (
       STRUCT(client_info.distribution.name, client_info.distribution.ext) AS `distribution`
     ) AS `client_info`,
     document_id,
-    events,
+    ARRAY(
+      SELECT
+        STRUCT(
+          events.category,
+          ARRAY(
+            SELECT
+              STRUCT(extra.key, extra.value)
+            FROM
+              UNNEST(events.extra) AS `extra`
+          ) AS `extra`,
+          events.name,
+          events.timestamp
+        )
+      FROM
+        UNNEST(events) AS `events`
+    ) AS `events`,
     STRUCT(
       STRUCT(
         metadata.geo.city,
@@ -287,17 +302,46 @@ unioned_pings AS (
         metadata.header.x_telemetry_agent,
         metadata.header.parsed_date,
         metadata.header.parsed_x_source_tags,
-        metadata.header.parsed_x_lb_tags
+        STRUCT(
+          metadata.header.parsed_x_lb_tags.tls_version,
+          metadata.header.parsed_x_lb_tags.tls_cipher_hex
+        ) AS `parsed_x_lb_tags`
       ) AS `header`,
       STRUCT(metadata.isp.db_version, metadata.isp.name, metadata.isp.organization) AS `isp`,
-      metadata.user_agent
+      STRUCT(
+        metadata.user_agent.browser,
+        metadata.user_agent.os,
+        metadata.user_agent.version
+      ) AS `user_agent`
     ) AS `metadata`,
     STRUCT(
       STRUCT(
-        metrics.labeled_counter.glean_error_invalid_label,
-        metrics.labeled_counter.glean_error_invalid_overflow,
-        metrics.labeled_counter.glean_error_invalid_state,
-        metrics.labeled_counter.glean_error_invalid_value
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_label.key, glean_error_invalid_label.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_label) AS `glean_error_invalid_label`
+        ) AS `glean_error_invalid_label`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_overflow.key, glean_error_invalid_overflow.value)
+          FROM
+            UNNEST(
+              metrics.labeled_counter.glean_error_invalid_overflow
+            ) AS `glean_error_invalid_overflow`
+        ) AS `glean_error_invalid_overflow`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_state.key, glean_error_invalid_state.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_state) AS `glean_error_invalid_state`
+        ) AS `glean_error_invalid_state`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_value.key, glean_error_invalid_value.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_value) AS `glean_error_invalid_value`
+        ) AS `glean_error_invalid_value`
       ) AS `labeled_counter`,
       STRUCT(
         metrics.boolean.crash_startup,
@@ -329,7 +373,16 @@ unioned_pings AS (
         metrics.string.memory_js_out_of_memory,
         metrics.string.crash_cause
       ) AS `string`,
-      STRUCT(metrics.timespan.crash_uptime, metrics.timespan.environment_uptime) AS `timespan`,
+      STRUCT(
+        STRUCT(
+          metrics.timespan.crash_uptime.time_unit,
+          metrics.timespan.crash_uptime.value
+        ) AS `crash_uptime`,
+        STRUCT(
+          metrics.timespan.environment_uptime.time_unit,
+          metrics.timespan.environment_uptime.value
+        ) AS `environment_uptime`
+      ) AS `timespan`,
       STRUCT(
         metrics.object.crash_async_shutdown_timeout,
         metrics.object.crash_quota_manager_shutdown_timeout,
@@ -366,7 +419,27 @@ unioned_pings AS (
     normalized_country_code,
     normalized_os,
     normalized_os_version,
-    ping_info,
+    STRUCT(
+      ping_info.end_time,
+      ARRAY(
+        SELECT
+          STRUCT(
+            experiments.key,
+            STRUCT(
+              experiments.value.branch,
+              STRUCT(experiments.value.extra.type, experiments.value.extra.enrollment_id) AS `extra`
+            ) AS `value`
+          )
+        FROM
+          UNNEST(ping_info.experiments) AS `experiments`
+      ) AS `experiments`,
+      ping_info.ping_type,
+      ping_info.reason,
+      ping_info.seq,
+      ping_info.start_time,
+      ping_info.parsed_start_time,
+      ping_info.parsed_end_time
+    ) AS `ping_info`,
     sample_id,
     submission_timestamp,
     app_version_major,
@@ -408,7 +481,22 @@ unioned_pings AS (
       STRUCT(client_info.distribution.name, client_info.distribution.ext) AS `distribution`
     ) AS `client_info`,
     document_id,
-    events,
+    ARRAY(
+      SELECT
+        STRUCT(
+          events.category,
+          ARRAY(
+            SELECT
+              STRUCT(extra.key, extra.value)
+            FROM
+              UNNEST(events.extra) AS `extra`
+          ) AS `extra`,
+          events.name,
+          events.timestamp
+        )
+      FROM
+        UNNEST(events) AS `events`
+    ) AS `events`,
     STRUCT(
       STRUCT(
         metadata.geo.city,
@@ -428,17 +516,46 @@ unioned_pings AS (
         metadata.header.x_telemetry_agent,
         metadata.header.parsed_date,
         metadata.header.parsed_x_source_tags,
-        metadata.header.parsed_x_lb_tags
+        STRUCT(
+          metadata.header.parsed_x_lb_tags.tls_version,
+          metadata.header.parsed_x_lb_tags.tls_cipher_hex
+        ) AS `parsed_x_lb_tags`
       ) AS `header`,
       STRUCT(metadata.isp.db_version, metadata.isp.name, metadata.isp.organization) AS `isp`,
-      metadata.user_agent
+      STRUCT(
+        metadata.user_agent.browser,
+        metadata.user_agent.os,
+        metadata.user_agent.version
+      ) AS `user_agent`
     ) AS `metadata`,
     STRUCT(
       STRUCT(
-        metrics.labeled_counter.glean_error_invalid_label,
-        metrics.labeled_counter.glean_error_invalid_overflow,
-        metrics.labeled_counter.glean_error_invalid_state,
-        metrics.labeled_counter.glean_error_invalid_value
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_label.key, glean_error_invalid_label.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_label) AS `glean_error_invalid_label`
+        ) AS `glean_error_invalid_label`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_overflow.key, glean_error_invalid_overflow.value)
+          FROM
+            UNNEST(
+              metrics.labeled_counter.glean_error_invalid_overflow
+            ) AS `glean_error_invalid_overflow`
+        ) AS `glean_error_invalid_overflow`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_state.key, glean_error_invalid_state.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_state) AS `glean_error_invalid_state`
+        ) AS `glean_error_invalid_state`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_value.key, glean_error_invalid_value.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_value) AS `glean_error_invalid_value`
+        ) AS `glean_error_invalid_value`
       ) AS `labeled_counter`,
       STRUCT(
         metrics.boolean.crash_startup,
@@ -470,7 +587,16 @@ unioned_pings AS (
         metrics.string.memory_js_out_of_memory,
         metrics.string.crash_cause
       ) AS `string`,
-      STRUCT(metrics.timespan.crash_uptime, metrics.timespan.environment_uptime) AS `timespan`,
+      STRUCT(
+        STRUCT(
+          metrics.timespan.crash_uptime.time_unit,
+          metrics.timespan.crash_uptime.value
+        ) AS `crash_uptime`,
+        STRUCT(
+          metrics.timespan.environment_uptime.time_unit,
+          metrics.timespan.environment_uptime.value
+        ) AS `environment_uptime`
+      ) AS `timespan`,
       STRUCT(
         metrics.object.crash_async_shutdown_timeout,
         metrics.object.crash_quota_manager_shutdown_timeout,
@@ -507,7 +633,27 @@ unioned_pings AS (
     normalized_country_code,
     normalized_os,
     normalized_os_version,
-    ping_info,
+    STRUCT(
+      ping_info.end_time,
+      ARRAY(
+        SELECT
+          STRUCT(
+            experiments.key,
+            STRUCT(
+              experiments.value.branch,
+              STRUCT(experiments.value.extra.type, experiments.value.extra.enrollment_id) AS `extra`
+            ) AS `value`
+          )
+        FROM
+          UNNEST(ping_info.experiments) AS `experiments`
+      ) AS `experiments`,
+      ping_info.ping_type,
+      ping_info.reason,
+      ping_info.seq,
+      ping_info.start_time,
+      ping_info.parsed_start_time,
+      ping_info.parsed_end_time
+    ) AS `ping_info`,
     sample_id,
     submission_timestamp,
     app_version_major,
@@ -520,12 +666,111 @@ unioned_pings AS (
   UNION ALL
   SELECT
     additional_properties,
-    client_info,
-    document_id,
-    events,
-    metadata,
     STRUCT(
-      metrics.labeled_counter,
+      client_info.android_sdk_version,
+      client_info.app_build,
+      client_info.app_channel,
+      client_info.app_display_version,
+      client_info.architecture,
+      client_info.build_date,
+      client_info.client_id,
+      client_info.device_manufacturer,
+      client_info.device_model,
+      client_info.first_run_date,
+      client_info.locale,
+      client_info.os,
+      client_info.os_version,
+      client_info.telemetry_sdk_build,
+      client_info.windows_build_number,
+      client_info.session_count,
+      client_info.session_id,
+      STRUCT(
+        client_info.attribution.campaign,
+        client_info.attribution.content,
+        client_info.attribution.medium,
+        client_info.attribution.source,
+        client_info.attribution.term,
+        client_info.attribution.ext
+      ) AS `attribution`,
+      STRUCT(client_info.distribution.name, client_info.distribution.ext) AS `distribution`
+    ) AS `client_info`,
+    document_id,
+    ARRAY(
+      SELECT
+        STRUCT(
+          events.category,
+          ARRAY(
+            SELECT
+              STRUCT(extra.key, extra.value)
+            FROM
+              UNNEST(events.extra) AS `extra`
+          ) AS `extra`,
+          events.name,
+          events.timestamp
+        )
+      FROM
+        UNNEST(events) AS `events`
+    ) AS `events`,
+    STRUCT(
+      STRUCT(
+        metadata.geo.city,
+        metadata.geo.country,
+        metadata.geo.db_version,
+        metadata.geo.subdivision1,
+        metadata.geo.subdivision2
+      ) AS `geo`,
+      STRUCT(
+        metadata.header.date,
+        metadata.header.dnt,
+        metadata.header.x_debug_id,
+        metadata.header.x_foxsec_ip_reputation,
+        metadata.header.x_lb_tags,
+        metadata.header.x_pingsender_version,
+        metadata.header.x_source_tags,
+        metadata.header.x_telemetry_agent,
+        metadata.header.parsed_date,
+        metadata.header.parsed_x_source_tags,
+        STRUCT(
+          metadata.header.parsed_x_lb_tags.tls_version,
+          metadata.header.parsed_x_lb_tags.tls_cipher_hex
+        ) AS `parsed_x_lb_tags`
+      ) AS `header`,
+      STRUCT(metadata.isp.db_version, metadata.isp.name, metadata.isp.organization) AS `isp`,
+      STRUCT(
+        metadata.user_agent.browser,
+        metadata.user_agent.os,
+        metadata.user_agent.version
+      ) AS `user_agent`
+    ) AS `metadata`,
+    STRUCT(
+      STRUCT(
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_label.key, glean_error_invalid_label.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_label) AS `glean_error_invalid_label`
+        ) AS `glean_error_invalid_label`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_overflow.key, glean_error_invalid_overflow.value)
+          FROM
+            UNNEST(
+              metrics.labeled_counter.glean_error_invalid_overflow
+            ) AS `glean_error_invalid_overflow`
+        ) AS `glean_error_invalid_overflow`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_state.key, glean_error_invalid_state.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_state) AS `glean_error_invalid_state`
+        ) AS `glean_error_invalid_state`,
+        ARRAY(
+          SELECT
+            STRUCT(glean_error_invalid_value.key, glean_error_invalid_value.value)
+          FROM
+            UNNEST(metrics.labeled_counter.glean_error_invalid_value) AS `glean_error_invalid_value`
+        ) AS `glean_error_invalid_value`
+      ) AS `labeled_counter`,
       STRUCT(
         metrics.boolean.crash_startup,
         metrics.boolean.crash_is_garbage_collecting,
@@ -534,7 +779,7 @@ unioned_pings AS (
         CAST(NULL AS BOOLEAN) AS `dll_blocklist_user32_loaded_before`,
         metrics.boolean.environment_headless_mode
       ) AS `boolean`,
-      metrics.datetime,
+      STRUCT(metrics.datetime.crash_time, metrics.datetime.raw_crash_time) AS `datetime`,
       STRUCT(
         metrics.string.crash_process_type,
         metrics.string.glean_client_annotation_experimentation_id,
@@ -556,7 +801,16 @@ unioned_pings AS (
         metrics.string.memory_js_out_of_memory,
         metrics.string.crash_cause
       ) AS `string`,
-      metrics.timespan,
+      STRUCT(
+        STRUCT(
+          metrics.timespan.crash_uptime.time_unit,
+          metrics.timespan.crash_uptime.value
+        ) AS `crash_uptime`,
+        STRUCT(
+          metrics.timespan.environment_uptime.time_unit,
+          metrics.timespan.environment_uptime.value
+        ) AS `environment_uptime`
+      ) AS `timespan`,
       STRUCT(
         metrics.object.crash_async_shutdown_timeout,
         metrics.object.crash_quota_manager_shutdown_timeout,
@@ -593,7 +847,27 @@ unioned_pings AS (
     normalized_country_code,
     normalized_os,
     normalized_os_version,
-    ping_info,
+    STRUCT(
+      ping_info.end_time,
+      ARRAY(
+        SELECT
+          STRUCT(
+            experiments.key,
+            STRUCT(
+              experiments.value.branch,
+              STRUCT(experiments.value.extra.type, experiments.value.extra.enrollment_id) AS `extra`
+            ) AS `value`
+          )
+        FROM
+          UNNEST(ping_info.experiments) AS `experiments`
+      ) AS `experiments`,
+      ping_info.ping_type,
+      ping_info.reason,
+      ping_info.seq,
+      ping_info.start_time,
+      ping_info.parsed_start_time,
+      ping_info.parsed_end_time
+    ) AS `ping_info`,
     sample_id,
     submission_timestamp,
     app_version_major,

80556acea88672735f031be4792143ee647b30b8
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2026-01-13 16:35:26 +0000

    Auto-push due to change on main branch [ci skip]
    Created from 9a7e80152718b4287499786b5750956435809da0

--- sql/moz-fx-data-shared-prod/telemetry/saved_session_use_counter/view.sql
+++
@@ -1,9 +0,0 @@
--- Generated via ./bqetl generate stable_views
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.telemetry.saved_session_use_counter`
-AS
-SELECT
-  * REPLACE (mozfun.norm.metadata(metadata) AS metadata),
-  LOWER(IFNULL(metadata.isp.name, "")) = "browserstack" AS is_bot_generated,
-FROM
-  `moz-fx-data-shared-prod.telemetry_stable.saved_session_use_counter_v4`

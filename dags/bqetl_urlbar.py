16ec6e7405e026d0d17bbcfdf60d02e497a77da9
CircleCI generate-sql job <dataops+generated-sql@mozilla.com>
2026-01-22 21:24:53 +0000

    Auto-push due to change on main branch [ci skip]
    Created from d71c32d032df364856aeee5f3061c1e0160ac01f

--- dags/bqetl_urlbar.py
+++ dags/bqetl_urlbar.py
@@ -99,6 +99,20 @@ with DAG(
         depends_on_past=False,
     )
 
+    with TaskGroup(
+        "firefox_desktop_derived__urlbar_events_daily__v1_external",
+    ) as firefox_desktop_derived__urlbar_events_daily__v1_external:
+        ExternalTaskMarker(
+            task_id="private_bqetl_ads__wait_for_firefox_desktop_derived__urlbar_events_daily__v1",
+            external_dag_id="private_bqetl_ads",
+            external_task_id="wait_for_firefox_desktop_derived__urlbar_events_daily__v1",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=82800)).isoformat() }}",
+        )
+
+        firefox_desktop_derived__urlbar_events_daily__v1_external.set_upstream(
+            firefox_desktop_derived__urlbar_events_daily__v1
+        )
+
     firefox_desktop_derived__urlbar_events_daily_engagement_by_position__v1 = bigquery_etl_query(
         task_id="firefox_desktop_derived__urlbar_events_daily_engagement_by_position__v1",
         destination_table="urlbar_events_daily_engagement_by_position_v1",
@@ -147,6 +161,20 @@ with DAG(
         date_partition_parameter="submission_date",
         depends_on_past=False,
     )
+
+    with TaskGroup(
+        "firefox_desktop_urlbar_events__v2_external",
+    ) as firefox_desktop_urlbar_events__v2_external:
+        ExternalTaskMarker(
+            task_id="private_bqetl_ads__wait_for_firefox_desktop_urlbar_events__v2",
+            external_dag_id="private_bqetl_ads",
+            external_task_id="wait_for_firefox_desktop_urlbar_events__v2",
+            execution_date="{{ (execution_date - macros.timedelta(days=-1, seconds=82800)).isoformat() }}",
+        )
+
+        firefox_desktop_urlbar_events__v2_external.set_upstream(
+            firefox_desktop_urlbar_events__v2
+        )
 
     telemetry_derived__urlbar_clients_daily__v1 = bigquery_etl_query(
         task_id="telemetry_derived__urlbar_clients_daily__v1",

73bb0fef99a51ee99a0c5e9112b0c10cf0743170
stephendherrera <sherrera@mozilla.com>
2026-02-02 14:34:28 -0700

    feat(firefoxdotcom): added site_engagement_events to enable a looker performance DB. (#8796)
    * feat(firefoxdotcom): added site_engagement_events to enable a looker performance DB.
    
    * feat(firefoxdotcom): added site_engagement_events to enable a looker performance DB, removed depends on logic.
    
    * chore(site_engagement_events_v1): updates to depends on logic

---
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/site_engagement_events/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.firefoxdotcom.site_engagement_events`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.site_engagement_events_v1`

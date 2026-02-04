42136ccf670a1db84cfcf56cd5d1197edb063ca1
stephendherrera <sherrera@mozilla.com>
2026-02-04 14:06:50 -0700

    feat(site_engagement_events): updates to site_engagement_events view as well as completing the v1 backfill from reviewed stage data

--- sql/moz-fx-data-shared-prod/firefoxdotcom/site_engagement_events/view.sql
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/site_engagement_events/view.sql
@@ -2,6 +2,14 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefoxdotcom.site_engagement_events`
 AS
 SELECT
-  *
+  e.ga_client_id || "-" || e.ga_session_id AS visit_identifier,
+  e.*,
+  s.engaged_session
 FROM
-  `moz-fx-data-shared-prod.firefoxdotcom_derived.site_engagement_events_v1`
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.site_engagement_events_v1` e
+JOIN
+  `moz-fx-data-shared-prod.firefoxdotcom_derived.ga_sessions_v2` s
+  ON e.ga_client_id = s.ga_client_id
+  AND e.ga_session_id = s.ga_session_id
+WHERE
+  REGEXP_CONTAINS(s.ga_client_id || '-' || s.ga_session_id, r"^[0-9]+\.{1}[0-9]+\-{1}[0-9]+$")

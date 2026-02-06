ed290340f5d752d4bdcc6d39c201ffb2296b6504
kik-kik <kignasiak@mozilla.com>
2026-02-06 20:25:36 +0100

    feat: schedule telemetry_derived/firefox_desktop_marketing_funnel_v1/ to run with a 28 day lag

--- sql/moz-fx-data-shared-prod/telemetry_derived/firefox_desktop_marketing_funnel_v1/query.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/firefox_desktop_marketing_funnel_v1/query.sql
@@ -53,7 +53,7 @@ WITH top_of_funnel_base AS (
     `moz-fx-data-shared-prod.static.country_names_v1` AS country_names
     ON ga4.country = country_names.name
   WHERE
-    session_date = @submission_date
+    session_date = DATE_SUB(@submission_date, INTERVAL 27 day)
     AND device_category = 'desktop'
     -- Exclude existing Firefox users - we want new acquisition only
     AND COALESCE(browser, '') NOT IN ('Firefox', 'Mozilla')
@@ -168,7 +168,7 @@ windows_installer_installs_base AS (
   FROM
     `moz-fx-data-shared-prod.firefox_installer.install` AS installs
   WHERE
-    DATE(submission_timestamp) = @submission_date
+    DATE(submission_timestamp) = DATE_SUB(@submission_date, INTERVAL 27 day)
     AND succeeded  -- Only successful installs
     AND NOT had_old_install  -- Exclude upgrades/reinstalls (new installs only)
   -- Only count installs from tracked mozilla.org downloads
@@ -225,7 +225,7 @@ fresh_download_profiles AS (
     USING (client_id)
   WHERE
     cfs.funnel_derived IN ('mozorg windows funnel', 'mozorg mac funnel')
-    AND cfs.first_seen_date = @submission_date
+    AND cfs.first_seen_date = DATE_SUB(@submission_date, INTERVAL 27 day)
     AND cfs.is_desktop
     AND cfs.attribution_dltoken IS NOT NULL
   -- Download must have occurred within 30 days before profile creation
@@ -299,7 +299,7 @@ desktop_funnels_telemetry AS (
     ga4_attr_by_dltoken AS ga4_attr
     USING (client_id)
   WHERE
-    cfs28.first_seen_date = @submission_date
+    cfs28.first_seen_date = DATE_SUB(@submission_date, INTERVAL 27 day)
     AND cfs28.is_desktop
     AND cfs28.normalized_channel = 'release'
     -- Exclude EU for core funnels, but keep partner funnel metrics

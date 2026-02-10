198128c787edcc9eabca5eeee93d2d8aa8158501
Anna Scholtz <anna@scholtzan.net>
2026-02-10 08:56:16 -0800

    Apply suggestion from @scholtzan

--- sql_generators/rust_component_metrics/__init__.py
+++ sql_generators/rust_component_metrics/__init__.py
@@ -164,7 +164,7 @@ def generate(target_project, output_dir, use_cloud_function):
     for metric_group in all_metric_groups():
         for metric in metric_group.metrics:
             full_table_id = f"{target_project}.rust_components_derived.{metric.name}_v1"
-            full_view_id = f"{target_project}.rust_component_metrics.{metric.name}"
+            full_view_id = f"{target_project}.rust_components.{metric.name}"
             metric_data = get_metric_data(metric)
 
             query_sql_parts = [

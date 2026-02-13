3c7f8b79530f4705a4e13643cbcbf5b3972b7020
Chelsey Beck <cbeck@mozilla.com>
2026-02-12 19:56:19 -0800

    casting installs as integer to match schema

--- sql/moz-fx-data-shared-prod/google_ads_derived/campaign_conversions_by_date_v1/query.sql
+++ sql/moz-fx-data-shared-prod/google_ads_derived/campaign_conversions_by_date_v1/query.sql
@@ -3,10 +3,10 @@ WITH conversion_counts AS (
     date,
     customer_id AS account_id,
     campaign_id,
-    SUM(biddable_app_install_conversions) AS installs,
+    CAST(SUM(biddable_app_install_conversions) AS INTEGER) AS installs,
     SUM(biddable_app_post_install_conversions) AS conversions,
   FROM
-    `moz-fx-data-bq-fivetran`.ads_google_mmc.campaign_conversions_by_date
+    `moz-fx-data-bq-fivetran.ads_google_mmc.campaign_conversions_by_date`
   GROUP BY
     date,
     account_id,

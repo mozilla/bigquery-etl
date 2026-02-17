e20d803ac6371c76a8f4bfaa7d49a2b6a2149a10
Philip Lee <plee@Philips-MacBook-Pro.local>
2026-02-17 14:47:13 -0500

    refactor udf to use like any pattern

--- sql/mozfun/norm/subplat_utm_to_channel_group/udf.sql
+++ sql/mozfun/norm/subplat_utm_to_channel_group/udf.sql
@@ -2,84 +2,85 @@ CREATE OR REPLACE FUNCTION norm.subplat_utm_to_channel_group(utm_source STRING)
 RETURNS STRING AS (
   CASE
     -- Marketing Owned
-    WHEN utm_source LIKE '%mozilla.org-whatsnew%'
-      OR utm_source LIKE '%mozilla.org-welcome%'
-      OR utm_source LIKE '%blog.mozilla.org%'
-      OR utm_source LIKE '%fxnews%'
-      OR utm_source LIKE '%fxavpn%'
-      OR utm_source LIKE '%fxatips%'
-      OR utm_source LIKE '%fxakip%'
-      OR utm_source LIKE '%vpnwaitlist%'
-      OR utm_source LIKE '%monitor%'
-      OR utm_source LIKE '%twitter.com%'
-      OR utm_source LIKE '%fxaonboardingemail%'
-      OR utm_source LIKE '%invite%'
-      OR utm_source LIKE '%pockethits%'
-      OR utm_source LIKE '%pkt-hits%'
-      OR utm_source LIKE '%news%'
-      OR utm_source LIKE '%sync-onboarding%'
-      OR utm_source LIKE '%stage.fxprivaterelay.nonprod.cloudops.mozgcp.net%'
-      OR utm_source LIKE '%instagram%'
-      OR utm_source LIKE '%wrapped_email%'
-      OR utm_source LIKE '%relay-onboarding%'
-      OR utm_source LIKE '%oim%'
-      OR utm_source LIKE '%firefox-desktop%'
+    WHEN utm_source LIKE ANY(
+        '%mozilla.org-whatsnew%',
+        '%mozilla.org-welcome%',
+        '%blog.mozilla.org%',
+        '%fxnews%',
+        '%fxavpn%',
+        '%fxatips%',
+        '%fxakip%',
+        '%vpnwaitlist%',
+        '%monitor%',
+        '%twitter.com%',
+        '%fxaonboardingemail%',
+        '%invite%',
+        '%pockethits%',
+        '%pkt-hits%',
+        '%news%',
+        '%sync-onboarding%',
+        '%stage.fxprivaterelay.nonprod.cloudops.mozgcp.net%',
+        '%instagram%',
+        '%wrapped_email%',
+        '%relay-onboarding%',
+        '%oim%',
+        '%firefox-desktop%'
+      )
       THEN 'Marketing Owned'
     -- Direct
-    WHEN utm_source = 'www.mozilla.org-vpn-product-page'
-      OR utm_source = 'google-play'
-      OR utm_source = 'product'
+    WHEN utm_source IN ('www.mozilla.org-vpn-product-page', 'google-play', 'product')
       THEN 'Direct'
     -- Product Owned
-    WHEN utm_source LIKE '%about-prefs%'
-      OR utm_source LIKE '%leanplum-push-notification%'
-      OR utm_source LIKE '%firefox-browser%'
-      OR utm_source LIKE '%newtab%'
-      OR utm_source LIKE '%monitor.firefox.com%'
-      OR utm_source LIKE '%fx-monitor%'
-      OR utm_source LIKE '%relay-firefox-com.translate.goog%'
-      OR utm_source LIKE '%accounts.firefox.com%'
-      OR utm_source LIKE '%privatebrowser%'
-      OR utm_source LIKE '%pocket%'
-      OR utm_source LIKE '%fx-vpn-windows%'
-      OR utm_source LIKE '%toolbar%'
-      OR utm_source LIKE '%spotlight-modal%'
-      OR utm_source LIKE '%thunderbird%'
-      OR utm_source LIKE '%sponsoredtile%'
-      OR utm_source LIKE '%activity-stream%'
-      OR utm_source LIKE '%mozilla.org-firefox-accounts%'
-      OR utm_source LIKE '%mozilla.org-firefox_home%'
-      OR utm_source LIKE '%send.firefox.com%'
-      OR utm_source LIKE '%addons.mozilla.org%'
-      OR utm_source LIKE '%premium.firefox.com%'
-      OR utm_source LIKE '%fpn.firefox.com%'
-      OR utm_source LIKE '%leanplum-push-qa%'
-      OR utm_source LIKE '%fx-ios-vpn%'
-      OR utm_source LIKE '%modal%'
-      OR utm_source LIKE '%fx-relay-addon%'
-      OR utm_source LIKE '%fx-relay%'
-      OR utm_source LIKE '%mozilla.org-firefox-browsers%'
-      OR utm_source LIKE '%new-tab-ad%'
-      OR utm_source LIKE '%firefox%'
-      OR utm_source LIKE '%pocket_saves%'
-      OR utm_source LIKE '%www.mozilla.org-vpn-or-proxy%'
-      OR utm_source LIKE '%fx-vpn-iOSs%'
+    WHEN utm_source LIKE ANY(
+        '%about-prefs%',
+        '%leanplum-push-notification%',
+        '%firefox-browser%',
+        '%newtab%',
+        '%monitor.firefox.com%',
+        '%fx-monitor%',
+        '%relay-firefox-com.translate.goog%',
+        '%accounts.firefox.com%',
+        '%privatebrowser%',
+        '%pocket%',
+        '%fx-vpn-windows%',
+        '%toolbar%',
+        '%spotlight-modal%',
+        '%thunderbird%',
+        '%sponsoredtile%',
+        '%activity-stream%',
+        '%mozilla.org-firefox-accounts%',
+        '%mozilla.org-firefox_home%',
+        '%send.firefox.com%',
+        '%addons.mozilla.org%',
+        '%premium.firefox.com%',
+        '%fpn.firefox.com%',
+        '%leanplum-push-qa%',
+        '%fx-ios-vpn%',
+        '%modal%',
+        '%fx-relay-addon%',
+        '%fx-relay%',
+        '%mozilla.org-firefox-browsers%',
+        '%new-tab-ad%',
+        '%firefox%',
+        '%pocket_saves%',
+        '%www.mozilla.org-vpn-or-proxy%',
+        '%fx-vpn-iOSs%'
+      )
       THEN 'Product Owned'
     -- Marketing Paid
-    WHEN utm_source LIKE '%facebook%'
-      OR utm_source LIKE '%instagram%'
-      OR utm_source LIKE '%google%'
-      OR utm_source = 'reddit'
-      OR utm_source = 'dv360'
-      OR utm_source LIKE '%saasworthy.com%'
-      OR utm_source LIKE '%youtube%'
+    WHEN utm_source LIKE ANY(
+        '%facebook%',
+        '%instagram%',
+        '%google%',
+        '%saasworthy.com%',
+        '%youtube%'
+      )
+      OR utm_source IN ('reddit', 'dv360')
       THEN 'Marketing Paid'
     -- Miscellaneous (catch-all for remaining patterns)
-    WHEN utm_source LIKE '%relay%'
-      OR utm_source LIKE '%desktop-signup-flow%'
-      OR utm_source = 'invalid'
-      OR utm_source LIKE '%multi.account.containers%'
+    WHEN utm_source LIKE ANY('%relay%', '%desktop-signup-flow%', '%multi.account.containers%')
       OR utm_source IN (
+        'invalid',
         'yahoo',
         'bdmtools',
         'private-relay',
@@ -110,8 +111,7 @@ RETURNS STRING AS (
       )
       THEN 'Miscellaneous'
     -- Default for any unmapped values (based on the data, this should be Marketing Owned for most mozilla domains)
-    WHEN utm_source LIKE '%mozilla.org%'
-      OR utm_source LIKE '%.mozilla.org%'
+    WHEN utm_source LIKE ANY('%mozilla.org%', '%.mozilla.org%')
       THEN 'Marketing Owned'
     -- Final fallback
     ELSE 'Miscellaneous'

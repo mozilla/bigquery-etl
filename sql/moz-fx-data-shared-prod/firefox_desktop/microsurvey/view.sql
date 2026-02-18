68a3c52683c9cb057d6c1416652b1cf146998325
Kiran Bammarito <kbammarito@mozilla.com>
2026-02-18 18:37:01 -0500

    list fields

--- sql/moz-fx-data-shared-prod/firefox_desktop/microsurvey/view.sql
+++ sql/moz-fx-data-shared-prod/firefox_desktop/microsurvey/view.sql
@@ -32,7 +32,41 @@ replace_and_lower AS (
 ),
 final AS (
   SELECT
-    *
+    additional_properties,
+    document_id,
+    events.category,
+    events.extra.key,
+    events.extra.value,
+    events.name,
+    events.timestamp,
+    metadata.geo.city,
+    metadata.geo.country,
+    metadata.geo.db_version,
+    metadata.geo.subdivision1,
+    metadata.geo.subdivision2,
+    metadata.header.date,
+    metadata.header.dnt,
+    metadata.header.x_debug_id,
+    metadata.header.x_foxsec_ip_reputation,
+    metadata.header.x_lb_tags,
+    metadata.header.x_pingsender_version,
+    metadata.header.x_source_tags,
+    metadata.header.x_telemetry_agent,
+    metadata.isp.db_version,
+    metadata.isp.name,
+    metadata.isp.organization,
+    metadata.user_agent.browser,
+    metadata.user_agent.os,
+    metadata.user_agent.version,
+    metrics,
+    normalized_app_name,
+    normalized_channel,
+    normalized_country_code,
+    normalized_os,
+    normalized_os_version,
+    sample_id,
+    subimssion_timestamp,
+    is_bot_generated
   FROM
     replace_and_lower
 )

3cdf6865f3e0fdcd2ba2ce4e9a2a08bad16e3cae
kik-kik <kignasiak@mozilla.com>
2026-02-18 17:46:24 +0100

    feat: update desktop_conversion_events + glean to only include specific countries

--- sql/moz-fx-data-shared-prod/firefoxdotcom/glean_desktop_conversion_events/view.sql
+++ sql/moz-fx-data-shared-prod/firefoxdotcom/glean_desktop_conversion_events/view.sql
@@ -1,27 +1,24 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.firefoxdotcom.glean_desktop_conversion_events`
 AS
--- Get all clicks from the countries without web cookie consent and the first session date associated with that click
-WITH firefoxdotcom_sessions AS (
+-- Get all clicks from specific countries (US, Canada, Mexico, Australia, Japan) and the first session associated with that click
+WITH firefoxdotcom_first_sessions AS (
   SELECT
     gclid,
     MIN(session_date) AS first_session_date
   FROM
     `moz-fx-data-shared-prod.firefoxdotcom_derived.ga_sessions_v2` AS ga_sessions_v2,
     UNNEST(gclid_array) AS gclid
-  LEFT JOIN
+  INNER JOIN
     `moz-fx-data-shared-prod.static.country_names_v1` AS country_names
     ON ga_sessions_v2.country = country_names.name
-  LEFT JOIN
-    `moz-fx-data-shared-prod.static.marketing_country_tier_mapping_v1` AS marketing_tier_mapping
-    ON country_names.code = marketing_tier_mapping.country_code
   WHERE
-    NOT COALESCE(marketing_tier_mapping.has_web_cookie_consent, FALSE)
+    country_names.code IN ("US", "CA", "MX", "AU", "JP")
   GROUP BY
     gclid
 )
 -- Get all conversion events and associated clicks in the last 28 days
--- where the click ID originated from a country without a web cookie consent
+-- where the click ID is present inside the firefoxdotcom_first_sessions CTE
 -- and the click's first seen session date is more recent than 28 days ago
 SELECT
   gclid,
@@ -32,8 +29,8 @@ SELECT
   ) AS activity_date_timestamp
 FROM
   `moz-fx-data-shared-prod.firefoxdotcom_derived.glean_ga_desktop_conversions_v1`
-JOIN
-  firefoxdotcom_sessions
+INNER JOIN
+  firefoxdotcom_first_sessions
   USING (gclid)
 WHERE
   first_session_date >= DATE_SUB(CURRENT_DATE, INTERVAL 28 day)

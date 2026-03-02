da6c8c961887f3878af4c2f896581df389f49ba8
Anna Scholtz <anna@scholtzan.net>
2026-03-02 14:58:57 -0800

    Update bigquery_etl/public_data/publish_json.py
    Co-authored-by: Ben Wu <12437227+BenWu@users.noreply.github.com>

--- bigquery_etl/public_data/publish_json.py
+++ bigquery_etl/public_data/publish_json.py
@@ -145,7 +145,7 @@ class JsonPublisher:
         try:
             if order_by_field:
                 ordered_temp_table = (
-                    f"{self.project_id}.tmp.{self.table}_{self.version}_"
+                    f"{self.project_id}.tmp.publish_public_{self.table}_{self.version}_"
                     + "".join(random.choice(string.ascii_lowercase) for _ in range(12))
                     + "_ordered_temp"
                 )

97525e9eda482b8e676457a285d45339bcf3ed2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-06 00:05:39 +0100

    Remove jira_issues_etl and merge its code into the jira quer.py files

--- sql/moz-fx-data-shared-prod/jira_tickets_derived/ffxp_epic_issues_v1/query.py
+++ sql/moz-fx-data-shared-prod/jira_tickets_derived/ffxp_epic_issues_v1/query.py
@@ -1,13 +1,40 @@
-from pathlib import Path
+import logging
 import sys
+from argparse import ArgumentParser
+from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from jira_issues_etl import main
-
+from bigquery_etl.jira import JiraIssueBigQueryIntegration
 
 if __name__ == "__main__":
-    main(
-        default_destination="moz-fx-data-shared-prod.jira_tickets_derived.ffxp_epic_issues_v1",
-        default_jql="project = FFXP AND issuetype = Epic ORDER BY created DESC",
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--destination",
+        dest="destination",
+        default="moz-fx-data-shared-prod.jira_tickets_derived.ffxp_epic_issues_v1",
+        required=False,
     )
+    parser.add_argument(
+        "--base-url",
+        dest="base_jira_url",
+        default="https://mozilla-hub.atlassian.net",
+        required=False,
+    )
+    parser.add_argument(
+        "--jql",
+        dest="jql",
+        default="project = FFXP AND issuetype = Epic ORDER BY created DESC",
+        required=False,
+    )
+
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        format="%(asctime)s:\t%(name)s.%(funcName)s()[%(filename)s:%(lineno)s]:\t%(levelname)s: %(message)s",
+        level=logging.INFO,
+        encoding="utf-8",
+    )
+
+    integration = JiraIssueBigQueryIntegration()
+    integration.run(args)

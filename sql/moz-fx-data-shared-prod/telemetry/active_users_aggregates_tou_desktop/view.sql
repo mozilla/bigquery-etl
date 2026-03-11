fd9c8e1dae1fdb814e1ff330be4f96a4182b676c
Marlene-M-Hirose <mhirose@mozilla.com>
2026-03-11 10:17:44 -1000

    add active_users_aggregates_tou_desktop view

---
+++ sql/moz-fx-data-shared-prod/telemetry/active_users_aggregates_tou_desktop/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.telemetry.active_users_aggregates_tou_desktop`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.telemetry_derived.active_users_aggregates_tou_desktop_v1`

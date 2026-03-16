d4f2b47159440e8a5cc76c9cb210ef38d4d78e06
kik-kik <kignasiak@mozilla.com>
2026-03-16 11:50:34 +0100

    feat: update firefoxdotcom_derived/fb_desktop_conversions_v1 query.py

--- sql/moz-fx-data-shared-prod/firefoxdotcom_derived/fb_desktop_conversions_v1/query.py
+++ sql/moz-fx-data-shared-prod/firefoxdotcom_derived/fb_desktop_conversions_v1/query.py
@@ -22,18 +22,15 @@ from bigquery_etl.schema import Schema
 API_URI = "https://graph.facebook.com/"
 API_VERSION = "v25.0"
 API_ENDPOINT = "events"
-FBC_FORMAT_TEMPLATE = "fbc.{subdomain_index}.{creation_time}.{fbclid}"
-SUBDOMAIN_INDEX = 1
-PARTITION_FIELD = "activity_date"
 
 DEFAULT_TABLE_NAME = Path(__file__).parent
 
 SCHEMA_FILE = Path(__file__).parent / "schema.yaml"
 SCHEMA = Schema.from_schema_file(SCHEMA_FILE).to_bigquery_schema()
+PARTITION_FIELD = "activity_date"
 
 
 # Both contain different conversion events so union should be fine.
-# TODO: should we be checking if a specific fbclid and conversion_name combination already been sent to Facebook before?
 UNION_QUERY = """
 SELECT activity_date, fbclid, ga_event_timestamp, ga_country, conversion_name,
 FROM `moz-fx-data-shared-prod.firefoxdotcom_derived.fbclid_conversion_events_day_2_v1`
@@ -44,65 +41,59 @@ FROM `moz-fx-data-shared-prod.firefoxdotcom_derived.fbclid_conversion_events_day
 WHERE activity_date = @activity_date AND ga_country IN ("United States", "India")
 """
 
+# Expected fbc format: "fbc.{subdomain_index}.{creation_time}.{fbclid}"
+DATA_SELECT_QUERY = """
+SELECT activity_date, CONCAT("fbc.0", CAST(ga_event_timestamp AS STRING), ".", fbclid) AS fbc, conversion_name,
+FROM `{destination_table}`
+WHERE activity_date = @activity_date
+"""
 
-# TODO: still needs to be completed.
-def run_query(bq_client, activity_date, query):
-    """TODO."""
+
+def update_table(bq_client, activity_date, query, destination_table):
+    """Run query and store the result inside BQ (destination) table to act as store for data export."""
     query_job = bq_client.query(
-        query,
+        query=query,
         job_config=bigquery.QueryJobConfig(
-            bigquery.ScalarQueryParameter("activity_date", "DATE", activity_date),
-        ),
-    )
-    query_job.result()
-
-    print("The query data:")
-    for row in query_job:
-        print(row)
-
-
-# TODO: this needs a bit more work.
-def load_to_bigquery(bq_client, project_id, dataset, table_name, activity_date, data):
-    """Load DataFrame to BigQuery."""
-    target_table = (
-        f"{project_id}.{dataset}.{table_name}${activity_date.replace('-', '')}"
-    )
-
-    job = bq_client.load_table_from_dataframe(
-        data,
-        target_table,
-        job_config=bigquery.LoadJobConfig(
+            destination=destination_table,
             time_partitioning=bigquery.TimePartitioning(field=PARTITION_FIELD),
             write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            use_legacy_sql=False,
             schema=SCHEMA,
+            query_parameters=[
+                bigquery.ScalarQueryParameter("activity_date", "DATE", activity_date),
+            ],
         ),
     )
-    job.result()
-    print(f"Successfully loaded data to {target_table}")
+
+    query_job.result()
+
+
+def get_results_from_bigquery(bq_client, activity_date, query):
+    """Prepare and retrieve data from a BQ table that should be sent to the Conversions API."""
+    return bq_client.query(
+        query=query,
+        query_parameters=[
+            bigquery.ScalarQueryParameter("activity_date", "DATE", activity_date),
+        ],
+    ).to_dataframe()
 
 
 # Looks like there's a library for building event:
 # https://developers.facebook.com/documentation/ads-commerce/conversions-api/guides/business-sdk-features#http-service-interface
-def create_event(
-    event_name: str, event_timestamp: int, fbclid: str, fbclid_creation_timestamp: int
-) -> Event:
-    """TODO."""
+def create_event(event_name: str, event_timestamp: int, fbc: str) -> Event:
+    """Format data into Event."""
     return Event(
         event_name=event_name,
         event_time=event_timestamp,
         user_data=UserData(
-            fbc=FBC_FORMAT_TEMPLATE.format(
-                subdomain_index=SUBDOMAIN_INDEX,
-                creation_time=fbclid_creation_timestamp,
-                fbclid=fbclid,
-            )
+            fbc=fbc,
         ),
         action_source=ActionSource.OTHER,
     )
 
 
 def execute_request(pixel_id: int, events: List[Event]):
-    """TODO."""
+    """Send event info to Conversions API."""
     event_request = EventRequest(
         pixel_id=pixel_id,
         events=events,
@@ -118,7 +109,7 @@ def main(
     access_token: str,
     pixel_id: int,
 ) -> None:
-    """TODO."""
+    """Update table to include data to be pushed to Conversions API, retrieve data for a specific activity_date, format and send it to Conversions API."""
     if not (pixel_id and access_token):
         raise Exception(
             "Missing required test config. Got pixel_id: '{pixel_id}', access_token: '{access_token}'".format(
@@ -128,22 +119,30 @@ def main(
 
     bq_client = bigquery.Client(project_id)
 
-    query_results = run_query(bq_client, activity_date, UNION_QUERY)
+    table_partition = f"{table_name}${activity_date.replace('-', '')}"
+    destination_table = f"{project_id}.{dataset}.{table_partition}"
+
+    update_table(bq_client, activity_date, UNION_QUERY, destination_table)
+    result_df = get_results_from_bigquery(
+        bq_client,
+        activity_date,
+        DATA_SELECT_QUERY.format(source_table=destination_table),
+    )
+    print(result_df)
+
     conversion_events = [
-        create_event(conversion_event) for conversion_event in query_results
+        create_event(conversion_event) for conversion_event in result_df
     ]
+    print(conversion_events)
+    return
 
     FacebookAdsApi.init(access_token=access_token, crash_log=False)
 
     # TODO: check this batching method works as intended.
     for batch in batched(conversion_events, 1000):
-        response = execute_request([batch])
-        # TODO: Investigate if can determine which fclid conversions got successfully uploaded and write to table?
+        response = execute_request(pixel_id=pixel_id, events=[batch])
+        # TODO: Investigate what the response looks like and if there's some interesting information we may want to persist?
         print(response)
-
-    load_to_bigquery(
-        bq_client, project_id, dataset, table_name, activity_date, query_results
-    )
 
 
 if __name__ == "__main__":
@@ -158,7 +157,7 @@ if __name__ == "__main__":
         "--access_token",
         type=str,
         default=os.getenv("FB_ACCESS_TOKEN", None),
-    )  # TODO: need access token
+    )
     parser.add_argument(
         "--pixel_id",
         type=str,
@@ -168,10 +167,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(
-        args.activity_date,
-        args.project_id,
-        args.dataset,
-        args.table_name,
-        args.access_token,
-        args.pixel_id,
+        activity_date=args.activity_date,
+        project_id=args.project_id,
+        dataset=args.dataset,
+        table_name=args.table_name,
+        access_token=args.access_token,
+        pixel_id=args.pixel_id,
     )

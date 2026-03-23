95ab86c8c1e7b86178232311f00e6011eb1f2cbe
Marlene-M-Hirose <mhirose@mozilla.com>
2026-03-23 13:41:23 -1000

    deng_10290 add files for acct front end event streams + nimbus data

---
+++ sql/moz-fx-data-shared-prod/accounts_frontend_derived/events_stream_nimbus_enrollments_v1/query.sql
@@ -0,0 +1,54 @@
+WITH cirrus_data AS (
+  SELECT DISTINCT
+    DATE(submission_timestamp) AS submission_date,
+    mozfun.map.get_key(e.extra, "experiment") AS experiment_id,
+    mozfun.map.get_key(e.extra, "branch") AS branch,
+    mozfun.map.get_key(e.extra, "nimbus_user_id") AS client_id,
+    normalized_channel
+  FROM
+    `moz-fx-data-shared-prod.accounts_cirrus.enrollment` AS enrollment
+  CROSS JOIN
+    UNNEST(events) AS e
+  WHERE
+    DATE(submission_timestamp) = @submission_date
+)
+SELECT
+  acct_fr.event_id,
+  acct_fr.additional_properties,
+  acct_fr.client_info,
+  acct_fr.document_id,
+  acct_fr.metadata,
+  acct_fr.normalized_app_name,
+  acct_fr.normalized_channel,
+  acct_fr.normalized_country_code,
+  acct_fr.normalized_os,
+  acct_fr.normalized_os_version,
+  acct_fr.ping_info,
+  acct_fr.sample_id,
+  acct_fr.submission_timestamp,
+  acct_fr.client_id,
+  acct_fr.reason,
+  acct_fr.experiments,
+  acct_fr.event_timestamp,
+  acct_fr.event_category,
+  acct_fr.event_name,
+  acct_fr.event,
+  acct_fr.event_extra,
+  acct_fr.metrics,
+  acct_fr.profile_group_id,
+  acct_fr.legacy_telemetry_client_id,
+  acct_fr.app_version_major,
+  acct_fr.app_version_minor,
+  acct_fr.app_version_patch,
+  acct_fr.is_bot_generated,
+  acct_fr.document_event_number,
+  nimbus.experiment_id,
+  nimbus.branch,
+  @submission_date AS submission_date
+FROM
+  `moz-fx-data-shared-prod.accounts_frontend.events_stream` AS acct_fr
+LEFT JOIN
+  cirrus_data nimbus
+  USING (client_id)
+WHERE
+  DATE(submission_timestamp) = @submission_date

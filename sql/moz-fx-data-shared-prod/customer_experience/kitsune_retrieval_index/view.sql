0177631a55d8f88b8e809980160f549beb2499ed
Lucia <alvargasa@gmail.com>
2026-04-02 22:39:48 +0200

    Skip formatting query.sql for now, as sqlglot does not parse BigQuery ML syntax (AI.GENERATE_TABLE(MODEL ...)

--- sql/moz-fx-data-shared-prod/customer_experience/kitsune_retrieval_index/view.sql
+++ sql/moz-fx-data-shared-prod/customer_experience/kitsune_retrieval_index/view.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.customer_experience.kitsune_embeddings`
+  `moz-fx-data-shared-prod.customer_experience.kitsune_retrieval_index`
 AS
 SELECT
   *
 FROM
-  `moz-fx-data-shared-prod.customer_experience_derived.kitsune_embeddings_v1`
+  `moz-fx-data-shared-prod.customer_experience_derived.kitsune_retrieval_index_v1`

fe1c52acabdba60bf0545202b6c688b14b0584a4
kik-kik <kignasiak@mozilla.com>
2026-04-07 14:49:55 +0200

    feat: add dev_amo views for amo_stats_dau and amo_stats_installs

---
+++ sql/moz-fx-data-shared-prod/addons/dev_amo_stats_installs/view.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.addons.dev_amo_stats_installs`
+AS
+-- The addon_id is hashed due to string size limitations in events;
+-- we create a "unhashing" lookup table here from the DAU table, which should
+-- include all the same addon_ids that we see in install_stats events.
+WITH addon_id_lookup AS (
+  SELECT DISTINCT
+    addon_id,
+    TO_HEX(SHA256(addon_id)) AS hashed_addon_id
+  FROM
+    `moz-fx-data-shared-prod.addons.amo_stats_dau`
+)
+SELECT
+  installs.*,
+FROM
+  `moz-fx-data-shared-prod.addons.amo_stats_installs` AS installs
+-- This join will filter out all addon_ids that are not already present
+-- in the dev dau table, so we don't need to duplicate the list of addons
+-- approved for dev here.
+INNER JOIN
+  addon_id_lookup
+  USING (hashed_addon_id)

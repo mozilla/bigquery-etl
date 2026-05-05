e4f00b59ca955b2a41f76cbf8e4f1d0e297997bc
Kiran Bammarito <kbammarito@mozilla.com>
2026-05-05 10:37:49 -0400

    select all in view except some fields

--- sql_generators/urlbar_events/templates/view.sql
+++ sql_generators/urlbar_events/templates/view.sql
@@ -3,6 +3,6 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ app_name }}.urlbar_events`
 AS
 SELECT
-  *
+  * except (product_engaged_result_type, engaged_result_type)
 FROM
   `{{ project_id }}.{{ app_name }}_derived.urlbar_events_v2`

a972aea9b7edb05e1575bf64ffd27931a3814105
Kiran Bammarito <kbammarito@mozilla.com>
2026-05-05 13:12:58 -0400

    format sql

--- sql/mozfun/norm/result_type_to_product_name/udf.sql
+++ sql/mozfun/norm/result_type_to_product_name/udf.sql
@@ -4,11 +4,11 @@ RETURNS STRING AS (
     WHEN res IS NULL
       THEN NULL
     WHEN res IN (
-      'autofill_origin',
-      'autofill_url',
-      'autofill_adaptive_origin',
-      'autofill_adaptive_url',
-      'history_autofill_fallback_origin'
+        'autofill_origin',
+        'autofill_url',
+        'autofill_adaptive_origin',
+        'autofill_adaptive_url',
+        'history_autofill_fallback_origin'
       )
       THEN 'autofill'
     WHEN res IN ('addon')

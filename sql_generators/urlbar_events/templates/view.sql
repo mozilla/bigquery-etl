0bcf2002b50d689c7026f1d42781931b0fa3fb31
Kiran Bammarito <kbammarito@mozilla.com>
2026-05-11 13:06:07 -0400

    update excluded fields

--- sql_generators/urlbar_events/templates/view.sql
+++ sql_generators/urlbar_events/templates/view.sql
@@ -3,6 +3,6 @@ CREATE OR REPLACE VIEW
   `{{ project_id }}.{{ app_name }}.urlbar_events`
 AS
 SELECT
-  * except (product_engaged_result_type, engaged_result_type)
+  * except (product_engaged_result_type, engaged_result_type, annoyance_signal_type)
 FROM
   `{{ project_id }}.{{ app_name }}_derived.urlbar_events_v2`

30892d13cfc762206bd090811af48363b2b477ea
Lucia <alvargasa@gmail.com>
2026-05-12 14:05:11 +0200

    Add Zendesk retrieval index table.

---
+++ sql/moz-fx-data-shared-prod/customer_experience/zendesk_retrieval_index/view.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.customer_experience.zendesk_retrieval_index`
+AS
+SELECT
+  *,
+  EXP(-DATE_DIFF(CURRENT_DATE(), creation_date, DAY) / 30) AS recency_score
+FROM
+  `moz-fx-data-shared-prod.customer_experience_derived.zendesk_retrieval_index_v1`
+WHERE
+  metadata.embedding_succeeded

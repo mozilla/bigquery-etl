7b8c3bda2810f5aa2dc5eee9fcaa855b91af5b53
Philip Lee <phlee@mozilla.com>
2026-05-20 11:29:39 -0400

    address issues 9383 and 9391

--- sql/moz-fx-data-shared-prod/sumo_metrics_derived/sumo_forum_kpis/view.sql
+++ sql/moz-fx-data-shared-prod/sumo_metrics_derived/sumo_forum_kpis/view.sql
@@ -2,16 +2,15 @@ CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.sumo_metrics_derived.sumo_forum_kpis`
 AS
 SELECT
-  DATE,
+  created_date,
+  first_reply_date,
   product,
   locale,
   questions_created,
   questions_replied,
-  SAFE_DIVIDE(questions_replied, questions_created) AS reply_rate,
   avg_ttfr_hrs,
   total_ttfr_hrs,
   total_helpful_votes,
-  total_unhelpful_votes,
-  SAFE_DIVIDE(total_helpful_votes, total_helpful_votes + total_unhelpful_votes) AS helpfulness_rate
+  total_unhelpful_votes
 FROM
   `moz-fx-data-shared-prod.sumo_metrics_derived.kitsune_forum_metrics_daily_v1`

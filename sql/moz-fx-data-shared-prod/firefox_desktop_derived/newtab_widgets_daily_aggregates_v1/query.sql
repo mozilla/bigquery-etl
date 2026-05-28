006aee6799f65a685b3f31653be476af0323e2d6
Landry Edi <ledi@mozilla.com>
2026-05-28 12:23:30 -0500

    # This is a combination of 4 commits. # This is the 1st commit message:
    DENG-11155 - Create Newtab Widget aggregation table
    
    # This is the commit message #2:
    
    Add group by
    
    # This is the commit message #3:
    
    User client table
    
    # This is the commit message #4:
    
    User client table

---
+++ sql/moz-fx-data-shared-prod/firefox_desktop_derived/newtab_widgets_daily_aggregates_v1/query.sql
@@ -0,0 +1,156 @@
+WITH action_counts_per_widget AS (
+    -- unnest each visit's user_action_counts and sum per user_action to client-day grain
+  SELECT
+    submission_date,
+    widget_name,
+    app_version,
+    os,
+    channel,
+    country,
+    locale,
+    uac.user_action,
+    SUM(uac.count) AS action_count,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_derived.widgets_client_daily_v1`,
+    UNNEST(user_action_counts) AS uac
+  WHERE
+    submission_date = @submission_date
+  GROUP BY
+    submission_date,
+    widget_name,
+    app_version,
+    os,
+    channel,
+    country,
+    locale,
+    uac.user_action
+),
+action_counts_summary AS (
+  SELECT
+    submission_date,
+    widget_name,
+    app_version,
+    os,
+    channel,
+    country,
+    locale,
+    SUM(
+      IF(user_action IN ('learn_more', 'provider_link_click'), action_count, 0)
+    ) AS widget_link_click_count,
+    SUM(
+      IF(
+        user_action IN (
+          'change_hour_format',
+          'change_location',
+          'change_size',
+          'change_temperature_units',
+          'change_weather_display',
+          'collapse',
+          'detect_location',
+          'expand'
+        ),
+        action_count,
+        0
+      )
+    ) AS widget_setting_change_count,
+    SUM(
+      IF(
+        user_action IN (
+          'add_clock',
+          'add_nickname',
+          'edit_clock',
+          'follow_teams',
+          'list_copy',
+          'list_create',
+          'list_delete',
+          'list_edit',
+          'open_match_search',
+          'remove_clock',
+          'save_teams',
+          'task_complete',
+          'task_create',
+          'task_delete',
+          'task_edit',
+          'timer_end',
+          'timer_pause',
+          'timer_play',
+          'timer_reset',
+          'timer_set',
+          'timer_toggle_break',
+          'timer_toggle_focus',
+          'timer_toggle_play',
+          'view_key_dates',
+          'view_schedule',
+          'view_upcoming'
+        ),
+        action_count,
+        0
+      )
+    ) AS widget_utility_action_count,
+    SUM(IF(user_action = 'opt_in_accepted', action_count, 0)) AS widget_optin_accept_count,
+    ARRAY_AGG(STRUCT(user_action, action_count)) AS widget_user_action_counts
+  FROM
+    action_counts_per_widget
+  GROUP BY
+    submission_date,
+    widget_name,
+    app_version,
+    os,
+    channel,
+    country,
+    locale
+),
+widget_metrics AS (
+  SELECT
+    submission_date,
+    widget_name,
+    app_version,
+    os,
+    channel,
+    country,
+    locale,
+    COUNT(DISTINCT IF(is_widget_enabled, client_id, NULL)) AS widget_enabled_clients,
+    COUNT(
+      DISTINCT IF(is_widget_enabled AND all_visits > 0, client_id, NULL)
+    ) AS widget_visited_clients,
+    COUNT(
+      DISTINCT IF(user_event_count + enabled_count > 0, client_id, NULL)
+    ) AS widget_engaged_clients,
+    COUNT(DISTINCT IF(default_ui_visits > 0, client_id, NULL)) AS default_ui_clients,
+    SUM(enabled_count) AS widget_enabled_count,
+    SUM(disabled_count) AS widget_disabled_count,
+    SUM(impression_count) AS widget_impression_count,
+    SUM(user_event_count) AS widget_user_event_count,
+    SUM(all_visits) AS all_visits,
+    SUM(default_ui_visits) AS default_ui_visits,
+    SUM(widget_impression_visits) AS widget_impression_visits,
+    SUM(widget_user_event_visits) AS widget_user_event_visits,
+    SUM(widget_enabled_visits) AS widget_enabled_visits,
+    SUM(widget_disabled_visits) AS widget_disabled_visits,
+    SUM(impression_count) AS impression_count,
+    SUM(user_event_count) AS user_event_count,
+    SUM(change_size_or_learn_more_count) AS change_size_or_learn_more_count,
+    SUM(enabled_count) AS enabled_count,
+    SUM(disabled_count) AS disabled_count,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_derived.widgets_client_daily_v1`
+  WHERE
+    submission_date = @submission_date
+  GROUP BY
+    submission_date,
+    client_id,
+    widget_name,
+    widget_size
+)
+SELECT
+  widget_metrics.*,
+  action_counts_summary.widget_link_click_count,
+  action_counts_summary.widget_setting_change_count,
+  action_counts_summary.widget_utility_action_count,
+  action_counts_summary.widget_optin_accept_count,
+  action_counts_summary.widget_user_action_counts
+FROM
+  widget_metrics
+LEFT JOIN
+  action_counts_summary
+  USING (submission_date, widget_name, app_version, os, channel, country, locale)

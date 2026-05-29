1139c458e9f5aaf4e732f2dc8a837bb2511d19b0
Chelsey Klein <64881557+chelseyklein@users.noreply.github.com>
2026-05-29 14:53:13 -0700

    DENG-11188 Refactors braze_products_v1 (#9470)
    * removing and updating tests, unscheduling all tasks except products, and simplifying logic
    
    * removing scheduling from downstream tasks
    
    * updating external_id description
    
    * unscheduling braze_clicks_v1
    
    ---------
    
    Co-authored-by: Chelsey Klein <chelseyklein@Chelseys-MacBook-Pro.local>

--- sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/products_v1/query.sql
@@ -1,68 +1,51 @@
 SELECT
-  fxa.email_id AS external_id,
-  ARRAY_AGG(
-    STRUCT(
-      subscriptions.provider AS provider,
-      subscriptions.payment_provider AS payment_provider,
-      subscriptions.product_name AS product_name,
-      services.name AS service_name,
-      subscriptions.provider_plan_id AS subscription_plan_id,
-      subscriptions.provider_product_id AS subscription_product_id,
-      subscriptions.started_at AS subscription_started_at,
-      subscriptions.ended_at AS subscription_ended_at,
-      subscriptions.plan_summary AS subscription_summary,
-      subscriptions.plan_interval_type AS subscription_billing_interval,
-      subscriptions.plan_interval_count AS subscription_interval_count,
-      subscriptions.plan_currency AS subscription_currency,
-      subscriptions.plan_amount AS subscription_amount,
-      subscriptions.is_bundle AS is_bundle,
-      subscriptions.is_trial AS is_trial,
-      subscriptions.is_active AS is_active,
-      subscriptions.provider_status AS subscription_status,
-      LOWER(subscriptions.country_code) AS subscription_country_code,
-      subscriptions.country_name AS subscription_country_name,
-      subscriptions.current_period_started_at AS current_period_started_at,
-      subscriptions.current_period_ends_at AS current_period_ends_at,
-      subscriptions.auto_renew AS is_auto_renew,
-      subscriptions.auto_renew_disabled_at AS auto_renew_disabled_at,
-      subscriptions.has_refunds AS has_refunds,
-      subscriptions.has_fraudulent_charges AS has_fraudulent_charges,
-      CAST(NULL AS TIMESTAMP) AS first_touch_impression_at,
-      CAST(NULL AS STRING) AS first_touch_entrypoint,
-      CAST(NULL AS STRING) AS first_touch_entrypoint_experiment,
-      CAST(NULL AS STRING) AS first_touch_entrypoint_variation,
-      CAST(NULL AS STRING) AS first_touch_utm_campaign,
-      CAST(NULL AS STRING) AS first_touch_utm_content,
-      CAST(NULL AS STRING) AS first_touch_utm_medium,
-      CAST(NULL AS STRING) AS first_touch_utm_source,
-      CAST(NULL AS STRING) AS first_touch_utm_term,
-      subscriptions.last_touch_attribution.impression_at AS last_touch_impression_at,
-      subscriptions.last_touch_attribution.entrypoint AS last_touch_entrypoint,
-      subscriptions.last_touch_attribution.entrypoint_experiment AS last_touch_entrypoint_experiment,
-      subscriptions.last_touch_attribution.entrypoint_variation AS last_touch_entrypoint_variation,
-      subscriptions.last_touch_attribution.utm_campaign AS last_touch_utm_campaign,
-      subscriptions.last_touch_attribution.utm_content AS last_touch_utm_content,
-      subscriptions.last_touch_attribution.utm_medium AS last_touch_utm_medium,
-      subscriptions.last_touch_attribution.utm_source AS last_touch_utm_source,
-      subscriptions.last_touch_attribution.utm_term AS last_touch_utm_term,
-      subscriptions.provider_subscription_created_at AS subscription_created_at,
-      subscriptions.provider_subscription_updated_at AS subscription_updated_at
-    )
-    ORDER BY
-      subscriptions.product_name,
-      subscriptions.provider_subscription_updated_at,
-      subscriptions.started_at,
-      subscriptions.ended_at
-  ) AS products
+  mozilla_account_id AS external_id,
+  provider AS provider,
+  payment_provider AS payment_provider,
+  product_name AS product_name,
+  services.name AS service_name,
+  provider_plan_id AS subscription_plan_id,
+  provider_product_id AS subscription_product_id,
+  started_at AS subscription_started_at,
+  ended_at AS subscription_ended_at,
+  plan_summary AS subscription_summary,
+  plan_interval_type AS subscription_billing_interval,
+  plan_interval_count AS subscription_interval_count,
+  plan_currency AS subscription_currency,
+  plan_amount AS subscription_amount,
+  is_bundle AS is_bundle,
+  is_trial AS is_trial,
+  is_active AS is_active,
+  provider_status AS subscription_status,
+  LOWER(country_code) AS subscription_country_code,
+  country_name AS subscription_country_name,
+  current_period_started_at AS current_period_started_at,
+  current_period_ends_at AS current_period_ends_at,
+  auto_renew AS is_auto_renew,
+  auto_renew_disabled_at AS auto_renew_disabled_at,
+  has_refunds AS has_refunds,
+  has_fraudulent_charges AS has_fraudulent_charges,
+  CAST(NULL AS TIMESTAMP) AS first_touch_impression_at,
+  CAST(NULL AS STRING) AS first_touch_entrypoint,
+  CAST(NULL AS STRING) AS first_touch_entrypoint_experiment,
+  CAST(NULL AS STRING) AS first_touch_entrypoint_variation,
+  CAST(NULL AS STRING) AS first_touch_utm_campaign,
+  CAST(NULL AS STRING) AS first_touch_utm_content,
+  CAST(NULL AS STRING) AS first_touch_utm_medium,
+  CAST(NULL AS STRING) AS first_touch_utm_source,
+  CAST(NULL AS STRING) AS first_touch_utm_term,
+  last_touch_attribution.impression_at AS last_touch_impression_at,
+  last_touch_attribution.entrypoint AS last_touch_entrypoint,
+  last_touch_attribution.entrypoint_experiment AS last_touch_entrypoint_experiment,
+  last_touch_attribution.entrypoint_variation AS last_touch_entrypoint_variation,
+  last_touch_attribution.utm_campaign AS last_touch_utm_campaign,
+  last_touch_attribution.utm_content AS last_touch_utm_content,
+  last_touch_attribution.utm_medium AS last_touch_utm_medium,
+  last_touch_attribution.utm_source AS last_touch_utm_source,
+  last_touch_attribution.utm_term AS last_touch_utm_term,
+  provider_subscription_created_at AS subscription_created_at,
+  provider_subscription_updated_at AS subscription_updated_at
 FROM
-  `moz-fx-data-shared-prod.subscription_platform.logical_subscriptions` AS subscriptions
-LEFT JOIN
-  `moz-fx-data-shared-prod.ctms_braze.ctms_fxa` AS fxa
-  ON subscriptions.mozilla_account_id_sha256 = TO_HEX(SHA256(fxa.fxa_id))
-INNER JOIN
-  `moz-fx-data-shared-prod.braze_derived.users_v1` AS users
-  ON users.external_id = fxa.email_id
+  `moz-fx-data-shared-prod.subscription_platform.logical_subscriptions`
 CROSS JOIN
   UNNEST(services) AS services
-GROUP BY
-  email_id;

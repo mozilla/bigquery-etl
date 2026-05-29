c706b17f038e410e12c42dfcc338a866655f1b8c
Chelsey Klein <chelseyklein@Chelseys-MacBook-Pro.local>
2026-05-29 13:43:16 -0700

    removing and updating tests, unscheduling all tasks except products, and simplifying logic

--- sql/moz-fx-data-shared-prod/braze_derived/products_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/braze_derived/products_v1/checks.sql
@@ -1,10 +1,7 @@
 -- macro checks
 
 #fail
-{{ not_null(["external_id"]) }} -- to do: add array values
-
-#fail
-{{ is_unique(["external_id"]) }}
+{{ not_null(["external_id"]) }}
 
 #fail
 {{ min_row_count(1) }}

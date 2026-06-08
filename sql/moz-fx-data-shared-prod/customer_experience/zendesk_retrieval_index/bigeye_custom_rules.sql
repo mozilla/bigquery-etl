79365750fe7cc4170c0accff10a7b72d3e36edf1
Lucia <alvargasa@gmail.com>
2026-06-08 16:28:56 +0200

    feat(customer_experience): normalize retrieval index product via cx_normalize_product. Add incremental Bigeye custom rules that alert when product is 'unclassified'.

---
+++ sql/moz-fx-data-shared-prod/customer_experience/zendesk_retrieval_index/bigeye_custom_rules.sql
@@ -0,0 +1,8 @@
+-- {'name': 'zendesk_retrieval_index_unclassified_product', 'alert_type': 'count', 'range': {'max': 0}, 'collections': ['Operational Checks'], 'schedule': 'Default Schedule - 13:00 UTC'}
+SELECT
+  COUNT(*) AS unclassified_product_count
+FROM
+  `{{ project_id }}.{{ dataset_id }}.{{ table_name }}`
+WHERE
+  creation_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
+  AND product = 'unclassified';

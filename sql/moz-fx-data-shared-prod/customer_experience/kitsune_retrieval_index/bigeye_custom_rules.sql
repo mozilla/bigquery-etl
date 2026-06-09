649e507be9f8936a24ce1e668424b0113d1939f6
Lucia <alvargasa@gmail.com>
2026-06-09 16:05:55 +0200

    Change custom rule for BigEye metric and enable deploying metrics for the views.

--- sql/moz-fx-data-shared-prod/customer_experience/kitsune_retrieval_index/bigeye_custom_rules.sql
+++
@@ -1,8 +0,0 @@
--- {'name': 'kitsune_retrieval_index_unclassified_product', 'alert_type': 'count', 'range': {'max': 0}, 'collections': ['Operational Checks'], 'schedule': 'Default Schedule - 13:00 UTC'}
-SELECT
-  COUNT(*) AS unclassified_product_count
-FROM
-  `{{ project_id }}.{{ dataset_id }}.{{ table_name }}`
-WHERE
-  creation_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
-  AND product = 'unclassified';

e1b1a7c6c4c48cb8aca74fdba225c574b34d4034
Jari Jalkanen <jjalkanen@mozilla.com>
2026-06-10 10:33:01 +0200

    Bug 1990006 - Change signatures table to view and allow aggregating per app.

--- sql_generators/gecko_trace/templates/aggregates/signatures/view.sql
+++ sql_generators/gecko_trace/templates/aggregates/signatures/view.sql
@@ -1,11 +1,14 @@
 CREATE OR REPLACE VIEW
   `{{ target_project }}.gecko_trace_aggregates.signatures` AS
-{% for app_id in applications -%}
 SELECT
-  *
+  MIN(submission_date) AS submission_date,
+  app_id,
+  signature,
+  AVG(duration_nano) AS average_duration_nano,
+  COUNT(*) AS hits
 FROM
-  `{{ target_project }}.{{ app_id }}_derived.gecko_trace_signatures_v1`
-{%- if not loop.last %}
-UNION ALL
-{% endif -%}
-{% endfor %}
+  `{{ target_project }}.gecko_trace_aggregates.traces`
+GROUP BY
+  DATE(submission_date),
+  app_id,
+  signature

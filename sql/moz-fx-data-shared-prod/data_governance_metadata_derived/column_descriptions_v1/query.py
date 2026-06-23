b3060b393512118734920a2d87b828c6e455c89f
Chelsey Klein <chelseyklein@Chelseys-MacBook-Pro.local>
2026-06-23 10:28:19 -0700

    updating schema and adding readme

--- sql/moz-fx-data-shared-prod/data_governance_metadata_derived/column_descriptions_v1/query.py
+++
@@ -1,5 +0,0 @@
-"""Stub for column_descriptions_v1.
-
-This table is populated via the schema enricher agent at:
-https://github.com/mozilla/data-shared-llm-agents/tree/main/agents/schema_enricher
-"""

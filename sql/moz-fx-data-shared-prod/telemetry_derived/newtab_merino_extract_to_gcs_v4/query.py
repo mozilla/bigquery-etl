81992063ce4b65c14b9ae72675fdf87e2180533f
Mitchell Plyler <mlplyler@mozilla.com>
2026-07-17 13:13:35 -0400

    Use propensity v2 for Merino newtab extract

--- sql/moz-fx-data-shared-prod/telemetry_derived/newtab_merino_extract_to_gcs_v4/query.py
+++
@@ -1,6 +0,0 @@
-"""Extract New Tab engagement query results and write the combined JSON to a single file."""
-
-from bigquery_etl.newtab_merino import export_newtab_merino_table_to_gcs
-
-if __name__ == "__main__":
-    export_newtab_merino_table_to_gcs()

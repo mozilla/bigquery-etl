b657eaf5b5768bd75d6caefc2c819315894be1d0
Jared Snyder <jsnyder@mozilla.com>
2026-08-18 12:03:58 -0500

    delete view

--- sql/moz-fx-data-shared-prod/firefox_desktop/newtab_component_content/view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW
-  `moz-fx-data-shared-prod.firefox_desktop.newtab_component_content`
-AS
-SELECT
-  *
-FROM
-  `moz-fx-data-shared-prod.firefox_desktop_derived.newtab_component_content_v1`

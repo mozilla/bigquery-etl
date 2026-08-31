66e3cab8795a6a7b2f0dfdc109941757309514f8
Jari Jalkanen <jjalkanen@mozilla.com>
2026-08-31 18:04:10 +0200

    Bug 1990006 - Apply SQL formatting to gecko trace queries.

--- sql_generators/gecko_trace/templates/aggregates/events/view.sql
+++ sql_generators/gecko_trace/templates/aggregates/events/view.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW
-  `{{ target_project }}.gecko_trace_aggregates.events` AS
+  `{{ target_project }}.gecko_trace_aggregates.events`
+AS
 SELECT
   app_id,
   stable_event_id,
@@ -10,18 +11,19 @@ SELECT
   SUM(hit_count) AS hit_count,
   MIN(submission_date) AS first_seen_date,
   MAX(submission_date) AS latest_seen_date
-FROM (
-{% for app_id in applications -%}
-  SELECT
-    "{{ app_id }}" AS app_id,
-    *
-  FROM
-    `{{ target_project }}.{{ app_id }}_derived.gecko_trace_events_v1`
-  {%- if not loop.last %}
-  UNION ALL
-  {% endif -%}
-{% endfor %}
-)
+FROM
+  (
+    {% for app_id in applications -%}
+      SELECT
+        "{{ app_id }}" AS app_id,
+        *
+      FROM
+        `{{ target_project }}.{{ app_id }}_derived.gecko_trace_events_v1`
+        {%- if not loop.last %}
+          UNION ALL
+        {% endif -%}
+    {% endfor %}
+  )
 GROUP BY
   app_id,
   stable_event_id,

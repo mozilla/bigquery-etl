9d720a5f5b68c1baa021f9d00c11b035939de41e
Chelsey Klein <cbeck@mozilla.com>
2026-09-08 12:45:22 -0700

    added view on top of schema_source_coverage_v1

---
+++ sql/moz-fx-data-shared-prod/data_governance_metadata/schema_source_coverage/view.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW
+  `moz-fx-data-shared-prod.data_governance_metadata.schema_source_coverage`
+AS
+SELECT
+  *
+FROM
+  `moz-fx-data-shared-prod.data_governance_metadata_derived.schema_source_coverage_v1`

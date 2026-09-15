fc78d45f372aaa217f1123e05e8dd034c2de12ee
kbammarito <kbammarito@users.noreply.github.com>
2026-09-15 18:38:45 +0000

    feat(DENG-8178): add stub for new udf (#9872)

---
+++ sql/moz-fx-data-shared-prod/udf/normalize_search_engine_glean/udf.sql
@@ -0,0 +1,29 @@
+/*
+Return normalized engine name for recognized engines, for Glean data only
+
+This is a stub implementation for use with tests in this repo
+Real implementation is in private-bigquery-etl
+*/
+CREATE OR REPLACE FUNCTION udf.normalize_search_engine_glean(engine STRING) AS (
+  CASE
+    WHEN engine IS NULL
+      THEN NULL
+    WHEN STARTS_WITH(LOWER(engine), 'engine1')
+      THEN 'Engine1'
+    WHEN STARTS_WITH(LOWER(engine), 'engine2')
+      THEN 'Engine2'
+    WHEN STARTS_WITH(LOWER(engine), 'engine3')
+      THEN 'Engine3'
+    WHEN STARTS_WITH(LOWER(engine), 'engine4')
+      THEN 'Engine4'
+    ELSE 'Other'
+  END
+);
+
+-- Test
+SELECT
+  mozfun.assert.equals('Engine1', udf.normalize_search_engine_glean('engine1')),
+  mozfun.assert.equals('Engine2', udf.normalize_search_engine_glean('Engine2-abc')),
+  mozfun.assert.equals('Other', udf.normalize_search_engine_glean('not-Engine1')),
+  mozfun.assert.equals('Other', udf.normalize_search_engine_glean('engine')),
+  mozfun.assert.null(udf.normalize_search_engine_glean(NULL))

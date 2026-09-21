5c02b8e65dddf8c693507d206e287c4cf6805bd1
Curtis Morales <cmorales@mozilla.com>
2026-09-21 15:28:24 -0400

    Ignore overactive user filter for NULL context_id

--- sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
+++ sql/moz-fx-data-shared-prod/contextual_services_derived/event_aggregates_v1/query.sql
@@ -427,7 +427,7 @@ FROM
 WHERE
   submission_date = @submission_date
   -- Filter out events associated with suspiciously active clients.
-  AND NOT (user_event_count > 50 AND event_type = 'click')
+  AND NOT (user_event_count > 50 AND event_type = 'click' AND context_id IS NOT NULL)
   AND IF(
     DATE_DIFF(CURRENT_DATE(), @submission_date, DAY) > 30,
     ERROR("Data older than 30 days has been removed"),

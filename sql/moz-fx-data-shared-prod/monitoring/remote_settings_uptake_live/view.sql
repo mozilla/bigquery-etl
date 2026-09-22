83d7d0ab2b08afd9dab91447335aca043c2591b8
Mathieu Leplatre <mathieu@mozilla.com>
2026-09-22 18:56:34 +0200

    RMST-503: Include all implementations in Uptake Live view

--- sql/moz-fx-data-shared-prod/monitoring/remote_settings_uptake_live/view.sql
+++ sql/moz-fx-data-shared-prod/monitoring/remote_settings_uptake_live/view.sql
@@ -1,29 +1,170 @@
 CREATE OR REPLACE VIEW
   `moz-fx-data-shared-prod.monitoring.remote_settings_uptake_live`
 AS
+WITH all_implementations AS (
+-- Desktop (Gecko / Rust) -- one dataset covers every channel
+  SELECT
+    'desktop' AS platform,
+    'firefox_desktop' AS normalized_app_id,
+    IF(e.category = 'remote_settings', 'rust', 'gecko') AS implementation,
+    submission_timestamp,
+    client_info,
+    normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.firefox_desktop_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON (e.category = 'uptake.remotecontent.result' OR e.category = 'remote_settings')
+    AND e.name = 'uptake_remotesettings'
+  UNION ALL
+-- Android (Gecko / Rust) -- release
+  SELECT
+    'android' AS platform,
+    'org_mozilla_firefox' AS normalized_app_id,
+    IF(e.category = 'remote_settings', 'rust', 'gecko') AS implementation,
+    submission_timestamp,
+    client_info,
+    mozfun.norm.fenix_app_info(
+      'org_mozilla_firefox',
+      client_info.app_build
+    ).channel AS normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_firefox_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON (e.category = 'uptake.remotecontent.result' OR e.category = 'remote_settings')
+    AND e.name = 'uptake_remotesettings'
+  UNION ALL
+-- Android (Gecko / Rust) -- beta
+  SELECT
+    'android' AS platform,
+    'org_mozilla_firefox_beta' AS normalized_app_id,
+    IF(e.category = 'remote_settings', 'rust', 'gecko') AS implementation,
+    submission_timestamp,
+    client_info,
+    mozfun.norm.fenix_app_info(
+      'org_mozilla_firefox_beta',
+      client_info.app_build
+    ).channel AS normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_firefox_beta_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON (e.category = 'uptake.remotecontent.result' OR e.category = 'remote_settings')
+    AND e.name = 'uptake_remotesettings'
+  UNION ALL
+-- Android (Gecko / Rust) -- nightly
+  SELECT
+    'android' AS platform,
+    'org_mozilla_fenix' AS normalized_app_id,
+    IF(e.category = 'remote_settings', 'rust', 'gecko') AS implementation,
+    submission_timestamp,
+    client_info,
+    mozfun.norm.fenix_app_info(
+      'org_mozilla_fenix',
+      client_info.app_build
+    ).channel AS normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_fenix_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON (e.category = 'uptake.remotecontent.result' OR e.category = 'remote_settings')
+    AND e.name = 'uptake_remotesettings'
+  UNION ALL
+-- iOS (Rust) -- release
+  SELECT
+    'ios' AS platform,
+    'org_mozilla_ios_firefox' AS normalized_app_id,
+    'rust' AS implementation,
+    submission_timestamp,
+    client_info,
+    'release' AS normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_ios_firefox_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON e.category = 'remote_settings'
+    AND e.name = 'uptake_remotesettings'
+  UNION ALL
+-- iOS (Rust) -- beta
+  SELECT
+    'ios' AS platform,
+    'org_mozilla_ios_firefoxbeta' AS normalized_app_id,
+    'rust' AS implementation,
+    submission_timestamp,
+    client_info,
+    'beta' AS normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_ios_firefoxbeta_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON e.category = 'remote_settings'
+    AND e.name = 'uptake_remotesettings'
+  UNION ALL
+-- iOS (Rust) -- nightly
+  SELECT
+    'ios' AS platform,
+    'org_mozilla_ios_fennec' AS normalized_app_id,
+    'rust' AS implementation,
+    submission_timestamp,
+    client_info,
+    'nightly' AS normalized_channel,
+    normalized_os,
+    normalized_os_version,
+    normalized_country_code,
+    e.extra AS event_extra,
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_ios_fennec_live.events_v1`
+  INNER JOIN
+    UNNEST(events) AS e
+    ON e.category = 'remote_settings'
+    AND e.name = 'uptake_remotesettings'
+)
 SELECT
   submission_timestamp,
+  platform,
+  normalized_app_id,
+  implementation,
   SAFE_CAST(
     mozfun.norm.truncate_version(client_info.app_display_version, 'major') AS INTEGER
   ) AS major_version,
   client_info.client_id AS client_id,
-  'desktop' AS platform,
   normalized_channel,
   normalized_os,
   normalized_os_version,
   normalized_country_code,
   -- Extra attributes
   -- See https://searchfox.org/firefox-main/rev/1427c88632d1474d/services/common/metrics.yaml
-  mozfun.map.get_key(e.extra, 'value') AS extra_status, -- It's 'status' in our uptake.
-  mozfun.map.get_key(e.extra, 'trigger') AS extra_trigger,
-  mozfun.map.get_key(e.extra, 'source') AS extra_source,
-  mozfun.map.get_key(e.extra, 'errorName') AS extra_errorname,
-  mozfun.map.get_key(e.extra, 'timestamp') AS extra_timestamp,
-  SAFE_CAST(mozfun.map.get_key(e.extra, 'age') AS DECIMAL) AS extra_age,
-  SAFE_CAST(mozfun.map.get_key(e.extra, 'duration') AS INT64) AS extra_duration,
+  mozfun.map.get_key(event_extra, 'value') AS extra_status, -- It's 'status' in our uptake.
+  mozfun.map.get_key(event_extra, 'trigger') AS extra_trigger,
+  mozfun.map.get_key(event_extra, 'source') AS extra_source,
+  mozfun.map.get_key(event_extra, 'errorName') AS extra_errorname,
+  mozfun.map.get_key(event_extra, 'timestamp') AS extra_timestamp,
+  SAFE_CAST(mozfun.map.get_key(event_extra, 'age') AS DECIMAL) AS extra_age,
+  SAFE_CAST(mozfun.map.get_key(event_extra, 'duration') AS INT64) AS extra_duration,
 FROM
-  `moz-fx-data-shared-prod.firefox_desktop_live.events_v1`
-INNER JOIN
-  UNNEST(events) AS e
-  ON e.category = 'uptake.remotecontent.result'
-  AND e.name = 'uptake_remotesettings'
+  all_implementations
